32e7a6bafce2ebd7da95df8471517bc040729a83
Mahesh Sivasubramanian <msivasub@codeaurora.org>
2014-09-18 20:33:55 -0600

    ANDROID: GKI: ARM64: smp: Prevent cluster LPM modes when pending IPIs on cluster CPUs
    LPM modes can fail if there is a pending IPI interrupt at GIC CPU
    interface. On some usecases frequent failure of LPM modes can cause
    power and performance degradation. Hence, prevent cluster low power
    modes when there is a pending IPI on cluster CPUs.
    
    This patch has been combined with the following patches:
    	- 'commit c3d3af5dce1fb52 ("ARM: smp: Remove use of
    	  IPI_CALL_FUNC_SINGLE")'
    	- 'commit 0a061eca16bf0d8 ("ARM: kernel: smp: Clear Pending IPI
    	  flag after handling")'
    
    Bug: 140652396
    Change-Id: Id8a0ac24e4867ef824e0a6f11d989f1e1a2b0e93
    Signed-off-by: Mahesh Sivasubramanian <msivasub@codeaurora.org>
    Signed-off-by: Murali Nalajala <mnalajal@codeaurora.org>
    [satyap: trivial merge conflict resolution]
    Signed-off-by: Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>
    (cherry picked from commit 4b490f2e15cbb77f2be83b3b3043c395d5322ff7)
    Signed-off-by: Will McVicker <willmcvicker@google.com>
    [willmcvicker: fixed merged conflicts]

--- arch/arm/kernel/smp.c
+++ arch/arm/kernel/smp.c
@@ -541,6 +541,18 @@ static void smp_cross_call(const struct cpumask *target, unsigned int ipinr)
 	__smp_cross_call(target, ipinr);
 }
 
+DEFINE_PER_CPU(bool, pending_ipi);
+static void smp_cross_call_common(const struct cpumask *cpumask,
+						unsigned int func)
+{
+	unsigned int cpu;
+
+	for_each_cpu(cpu, cpumask)
+		per_cpu(pending_ipi, cpu) = true;
+
+	smp_cross_call(cpumask, func);
+}
+
 void show_ipi_list(struct seq_file *p, int prec)
 {
 	unsigned int cpu, i;
@@ -569,31 +581,32 @@ u64 smp_irq_stat_cpu(unsigned int cpu)
 
 void arch_send_call_function_ipi_mask(const struct cpumask *mask)
 {
-	smp_cross_call(mask, IPI_CALL_FUNC);
+	smp_cross_call_common(mask, IPI_CALL_FUNC);
 }
 
 void arch_send_wakeup_ipi_mask(const struct cpumask *mask)
 {
-	smp_cross_call(mask, IPI_WAKEUP);
+	smp_cross_call_common(mask, IPI_WAKEUP);
 }
 
 void arch_send_call_function_single_ipi(int cpu)
 {
-	smp_cross_call(cpumask_of(cpu), IPI_CALL_FUNC);
+	smp_cross_call_common(cpumask_of(cpu), IPI_CALL_FUNC);
 }
 
 #ifdef CONFIG_IRQ_WORK
 void arch_irq_work_raise(void)
 {
 	if (arch_irq_work_has_interrupt())
-		smp_cross_call(cpumask_of(smp_processor_id()), IPI_IRQ_WORK);
+		smp_cross_call_common(cpumask_of(smp_processor_id()),
+				IPI_IRQ_WORK);
 }
 #endif
 
 #ifdef CONFIG_GENERIC_CLOCKEVENTS_BROADCAST
 void tick_broadcast(const struct cpumask *mask)
 {
-	smp_cross_call(mask, IPI_TIMER);
+	smp_cross_call_common(mask, IPI_TIMER);
 }
 #endif
 
@@ -711,6 +724,8 @@ void handle_IPI(int ipinr, struct pt_regs *regs)
 
 	if ((unsigned)ipinr < NR_IPI)
 		trace_ipi_exit_rcuidle(ipi_types[ipinr]);
+
+	per_cpu(pending_ipi, cpu) = false;
 	set_irq_regs(old_regs);
 }
 
@@ -718,7 +733,7 @@ void smp_send_reschedule(int cpu)
 {
 	if (__smp_update_ipi_history_cb)
 		__smp_update_ipi_history_cb(cpu);
-	smp_cross_call(cpumask_of(cpu), IPI_RESCHEDULE);
+	smp_cross_call_common(cpumask_of(cpu), IPI_RESCHEDULE);
 }
 
 void smp_send_stop(void)
@@ -729,7 +744,7 @@ void smp_send_stop(void)
 	cpumask_copy(&mask, cpu_online_mask);
 	cpumask_clear_cpu(smp_processor_id(), &mask);
 	if (!cpumask_empty(&mask))
-		smp_cross_call(&mask, IPI_CPU_STOP);
+		smp_cross_call_common(&mask, IPI_CPU_STOP);
 
 	/* Wait up to one second for other CPUs to stop */
 	timeout = USEC_PER_SEC;
@@ -817,7 +832,7 @@ core_initcall(register_cpufreq_notifier);
 
 static void raise_nmi(cpumask_t *mask)
 {
-	__smp_cross_call(mask, IPI_CPU_BACKTRACE);
+	smp_cross_call_common(mask, IPI_CPU_BACKTRACE);
 }
 
 void arch_trigger_cpumask_backtrace(const cpumask_t *mask, bool exclude_self)
