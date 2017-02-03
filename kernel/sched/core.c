af6539cf72f7d035e9976d0f59b3f688ba82f92a
Joonwoo Park <joonwoop@codeaurora.org>
2017-02-03 11:15:31 -0800

    sched: WALT: account cumulative window demand
    Energy cost estimation has been a long lasting challenge for WALT
    because WALT guides CPU frequency based on the CPU utilization of
    previous window.  Consequently it's not possible to know newly
    waking-up task's energy cost until WALT's end of the current window.
    
    The WALT already tracks 'Previous Runnable Sum' (prev_runnable_sum)
    and 'Cumulative Runnable Average' (cr_avg).  They are designed for
    CPU frequency guidance and task placement but unfortunately both
    are not suitable for the energy cost estimation.
    
    It's because using prev_runnable_sum for energy cost calculation would
    make us to account CPU and task's energy solely based on activity in the
    previous window so for example, any task didn't have an activity in the
    previous window will be accounted as a 'zero energy cost' task.
    Energy estimation with cr_avg is what energy_diff() relies on at present.
    However cr_avg can only represent instantaneous picture of energy cost
    thus for example, if a CPU was fully occupied for an entire WALT window
    and became idle just before window boundary, and if there is a wake-up,
    energy_diff() accounts that CPU is a 'zero energy cost' CPU.
    
    As a result, introduce a new accounting unit 'Cumulative Window Demand'.
    The cumulative window demand tracks all the tasks' demands have seen in
    current window which is neither instantaneous nor actual execution time.
    Because task demand represents estimated scaled execution time when the
    task runs a full window, accumulation of all the demands represents
    predicted CPU load at the end of window.
    
    Thus we can estimate CPU's frequency at the end of current WALT window
    with the cumulative window demand.
    
    The use of prev_runnable_sum for the CPU frequency guidance and cr_avg
    for the task placement have not changed and these are going to be used
    for both purpose while this patch aims to add an additional statistics.
    
    Change-Id: I9908c77ead9973a26dea2b36c001c2baf944d4f5
    Signed-off-by: Joonwoo Park <joonwoop@codeaurora.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2170,6 +2170,10 @@ static void __sched_fork(unsigned long clone_flags, struct task_struct *p)
 	p->se.prev_sum_exec_runtime	= 0;
 	p->se.nr_migrations		= 0;
 	p->se.vruntime			= 0;
+#ifdef CONFIG_SCHED_WALT
+	p->last_sleep_ts		= 0;
+#endif
+
 	INIT_LIST_HEAD(&p->se.group_node);
 	walt_init_new_task_load(p);
 
@@ -3371,6 +3375,10 @@ static void __sched notrace __schedule(bool preempt)
 	rq->clock_skip_update = 0;
 
 	if (likely(prev != next)) {
+#ifdef CONFIG_SCHED_WALT
+		if (!prev->on_rq)
+			prev->last_sleep_ts = wallclock;
+#endif
 		rq->nr_switches++;
 		rq->curr = next;
 		++*switch_count;
@@ -9052,6 +9060,12 @@ void walt_exit_task(struct task_struct *p)
 	wallclock = walt_ktime_clock();
 	walt_update_task_ravg(rq->curr, rq, TASK_UPDATE, wallclock, 0);
 	dequeue_task(rq, p, 0);
+	/*
+	 * task's contribution is already removed from the
+	 * cumulative window demand in dequeue. As the
+	 * task's stats are reset, the next enqueue does
+	 * not change the cumulative window demand.
+	 */
 	reset_task_stats(p);
 	p->ravg.mark_start = wallclock;
 	p->ravg.sum_history[0] = WALT_EXITING_TASK_MARKER;
