bd1dcd826071affc2668be7619b0cfe573fec819
Dietmar Eggemann <dietmar.eggemann@arm.com>
2017-03-22 18:23:13 +0000

    ANDROID: sched/fair: Add eas (& cas) specific rq, sd and task stats
    The statistic counter are placed in the eas (& cas) wakeup path. Each
    of them has one representation for the runqueue (rq), the sched_domain
    (sd) and the task.
    A task counter is always incremented. A rq counter is always
    incremented for the rq the scheduler is currently running on. A sd
    counter is only incremented if a relation to a sd exists.
    
    The counters are exposed:
    
    (1) In /proc/schedstat for rq's and sd's:
    
    $ cat /proc/schedstat
    ...
    cpu0 71422 0 2321254 ...
    eas  44144 0 0 19446 0 24698 568435 51621 156932 133 222011 17459 120279 516814 83 0 156962 359235 176439 139981
      <- runqueue for cpu0
    ...
    domain0 3 42430 42331 ...
    eas 0 0 0 14200 0 0 0 0 0 0 0 0 0 0 0 0 0 0 66355 0  <- MC sched domain for cpu0
    ...
    
    The per-cpu eas vector has the following elements:
    
    sis_attempts  sis_idle   sis_cache_affine sis_suff_cap    sis_idle_cpu    sis_count               ||
    secb_attempts secb_sync  secb_idle_bt     secb_insuff_cap secb_no_nrg_sav secb_nrg_sav secb_count ||
    fbt_attempts  fbt_no_cpu fbt_no_sd        fbt_pref_idle   fbt_count                               ||
    cas_attempts  cas_count
    
    The following relations exist between these counters (from cpu0 eas
    vector above):
    
    sis_attempts = sis_idle + sis_cache_affine + sis_suff_cap + sis_idle_cpu + sis_count
    
    44144        = 0        + 0                + 19446        + 0            + 24698
    
    secb_attempts = secb_sync + secb_idle_bt + secb_insuff_cap + secb_no_nrg_sav + secb_nrg_sav + secb_count
    
    568435        = 51621     + 156932       + 133             + 222011          + 17459        + 120279
    
    fbt_attempts = fbt_no_cpu + fbt_no_sd + fbt_pref_idle + fbt_count + (return -1)
    
    516814       = 83         + 0         + 156962        + 359235    + (534)
    
    cas_attempts = cas_count + (return -1 or smp_processor_id())
    
    176439       = 139981    + (36458)
    
    (2) In /proc/$PROCESS_PID/task/$TASK_PID/sched for a task.
    
    example: main thread of system_server
    
    $ cat /proc/1083/task/1083/sched
    
    ...
    se.statistics.nr_wakeups_sis_attempts        :                  945
    se.statistics.nr_wakeups_sis_idle            :                    0
    se.statistics.nr_wakeups_sis_cache_affine    :                    0
    se.statistics.nr_wakeups_sis_suff_cap        :                  219
    se.statistics.nr_wakeups_sis_idle_cpu        :                    0
    se.statistics.nr_wakeups_sis_count           :                  726
    se.statistics.nr_wakeups_secb_attempts       :                10376
    se.statistics.nr_wakeups_secb_sync           :                 1462
    se.statistics.nr_wakeups_secb_idle_bt        :                 6984
    se.statistics.nr_wakeups_secb_insuff_cap     :                    3
    se.statistics.nr_wakeups_secb_no_nrg_sav     :                  927
    se.statistics.nr_wakeups_secb_nrg_sav        :                  206
    se.statistics.nr_wakeups_secb_count          :                  794
    se.statistics.nr_wakeups_fbt_attempts        :                 8914
    se.statistics.nr_wakeups_fbt_no_cpu          :                    0
    se.statistics.nr_wakeups_fbt_no_sd           :                    0
    se.statistics.nr_wakeups_fbt_pref_idle       :                 6987
    se.statistics.nr_wakeups_fbt_count           :                 1554
    se.statistics.nr_wakeups_cas_attempts        :                 3107
    se.statistics.nr_wakeups_cas_count           :                 1195
    ...
    
    The same relation between the counters as in the per-cpu case apply.
    
    Change-Id: Ie7d01267c78a3f41f60a3ef52917d5a5d463f195
    Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    [fixed schedstat macros calls to match modifications
    made in commit ae92882e5646d8661a3ca182ba988752fe4b773f]
    Signed-off-by: Quentin Perret <quentin.perret@arm.com>
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6339,7 +6339,14 @@ find_idlest_group_cpu(struct sched_group *group, struct task_struct *p, int this
 static inline int find_idlest_cpu(struct sched_domain *sd, struct task_struct *p,
 				  int cpu, int prev_cpu, int sd_flag)
 {
+	int wu = sd_flag & SD_BALANCE_WAKE;
+	int cas_cpu = -1;
 	int new_cpu = cpu;
+
+	if (wu) {
+		schedstat_inc(p->se.statistics.nr_wakeups_cas_attempts);
+		schedstat_inc(this_rq()->eas_stats.cas_attempts);
+	}
 
 	if (!cpumask_intersects(sched_domain_span(sd), &p->cpus_allowed))
 		return prev_cpu;
@@ -6348,6 +6355,9 @@ static inline int find_idlest_cpu(struct sched_domain *sd, struct task_struct *p
 		struct sched_group *group;
 		struct sched_domain *tmp;
 		int weight;
+
+		if (wu)
+			schedstat_inc(sd->eas_stats.cas_attempts);
 
 		if (!(sd->flags & sd_flag)) {
 			sd = sd->child;
@@ -6368,7 +6378,7 @@ static inline int find_idlest_cpu(struct sched_domain *sd, struct task_struct *p
 		}
 
 		/* Now try balancing at a lower domain level of new_cpu */
-		cpu = new_cpu;
+		cpu = cas_cpu = new_cpu;
 		weight = sd->span_weight;
 		sd = NULL;
 		for_each_domain(cpu, tmp) {
@@ -6378,6 +6388,11 @@ static inline int find_idlest_cpu(struct sched_domain *sd, struct task_struct *p
 				sd = tmp;
 		}
 		/* while loop will break here if sd == NULL */
+	}
+
+	if (wu && (cas_cpu >= 0)) {
+		schedstat_inc(p->se.statistics.nr_wakeups_cas_count);
+		schedstat_inc(this_rq()->eas_stats.cas_count);
 	}
 
 	return new_cpu;
@@ -6569,14 +6584,20 @@ static inline int __select_idle_sibling(struct task_struct *p, int prev, int tar
 	struct sched_domain *sd;
 	int i;
 
-	if (idle_cpu(target))
+	if (idle_cpu(target)) {
+		schedstat_inc(p->se.statistics.nr_wakeups_sis_idle);
+		schedstat_inc(this_rq()->eas_stats.sis_idle);
 		return target;
+	}
 
 	/*
 	 * If the previous cpu is cache affine and idle, don't be stupid.
 	 */
-	if (prev != target && cpus_share_cache(prev, target) && idle_cpu(prev))
+	if (prev != target && cpus_share_cache(prev, target) && idle_cpu(prev)) {
+		schedstat_inc(p->se.statistics.nr_wakeups_sis_cache_affine);
+		schedstat_inc(this_rq()->eas_stats.sis_cache_affine);
 		return prev;
+	}
 
 	sd = rcu_dereference(per_cpu(sd_llc, target));
 	if (!sd)
@@ -6636,6 +6657,9 @@ static inline int select_idle_sibling_cstate_aware(struct task_struct *p, int pr
 				 * intended to use this CPU, just proceed
 				 */
 				if (i == target && new_usage <= capacity_curr_of(target)) {
+					schedstat_inc(p->se.statistics.nr_wakeups_sis_suff_cap);
+					schedstat_inc(this_rq()->eas_stats.sis_suff_cap);
+					schedstat_inc(sd->eas_stats.sis_suff_cap);
 					return target;
 				}
 
@@ -6664,10 +6688,18 @@ static inline int select_idle_sibling_cstate_aware(struct task_struct *p, int pr
 
 static int select_idle_sibling(struct task_struct *p, int prev, int target)
 {
-	if (!sysctl_sched_cstate_aware)
-		return __select_idle_sibling(p, prev, target);
+	schedstat_inc(p->se.statistics.nr_wakeups_sis_attempts);
+	schedstat_inc(this_rq()->eas_stats.sis_attempts);
 
-	return select_idle_sibling_cstate_aware(p, prev, target);
+	if (!sysctl_sched_cstate_aware)
+		target = __select_idle_sibling(p, prev, target);
+	else
+		target = select_idle_sibling_cstate_aware(p, prev, target);
+
+	schedstat_inc(p->se.statistics.nr_wakeups_sis_count);
+	schedstat_inc(this_rq()->eas_stats.sis_count);
+
+	return target;
 }
 
 static inline unsigned long task_util(struct task_struct *p)
@@ -6777,10 +6809,15 @@ static int select_energy_cpu_brute(struct task_struct *p, int prev_cpu, int sync
 	struct sched_domain *sd;
 	struct energy_env *eenv;
 
+	schedstat_inc(p->se.statistics.nr_wakeups_secb_attempts);
+	schedstat_inc(this_rq()->eas_stats.secb_attempts);
+
 	if (sysctl_sched_sync_hint_enable && sync) {
 		int cpu = smp_processor_id();
 
 		if (cpumask_test_cpu(cpu, &p->cpus_allowed)) {
+			schedstat_inc(p->se.statistics.nr_wakeups_secb_sync);
+			schedstat_inc(this_rq()->eas_stats.secb_sync);
 			return cpu;
 		}
 	}
@@ -6832,8 +6869,19 @@ no_possible_energy_placement:
 	put_eenv(eenv);
 
 unlock:
-	if (energy_cpu == prev_cpu && !cpu_overutilized(prev_cpu))
+	if (energy_cpu == prev_cpu && !cpu_overutilized(prev_cpu)) {
+		schedstat_inc(p->se.statistics.nr_wakeups_secb_no_nrg_sav);
+		schedstat_inc(this_rq()->eas_stats.secb_no_nrg_sav);
 		return prev_cpu;
+	}
+
+	if (energy_cpu != prev_cpu) {
+		schedstat_inc(p->se.statistics.nr_wakeups_secb_nrg_sav);
+		schedstat_inc(this_rq()->eas_stats.secb_nrg_sav);
+	} else {
+		schedstat_inc(p->se.statistics.nr_wakeups_secb_count);
+		schedstat_inc(this_rq()->eas_stats.secb_count);
+	}
 
 	return energy_cpu != prev_cpu ? energy_cpu : spare_cpu;
 }
