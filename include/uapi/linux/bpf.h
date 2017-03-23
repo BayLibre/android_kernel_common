59b0c29b756c312e09a1c2072945cf01b9984a68
Chenbo Feng <fengc@google.com>
2017-03-22 17:27:35 -0700

    BACKPORT: [UPSTREAM] Add a eBPF helper function to retrieve socket uid
    Cherry-pick from commit 6acc5c2910689fc6ee181bf63085c5efff6a42bd
    
    Returns the owner uid of the socket inside a sk_buff. This is useful to
    perform per-UID accounting of network traffic or per-UID packet
    filtering. The socket need to be a fullsock otherwise overflowuid is
    returned.
    
    Signed-off-by: Chenbo Feng <fengc@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Bug: 30950746
    Change-Id: Idc00947ccfdd4e9f2214ffc4178d701cd9ead0ac

--- include/uapi/linux/bpf.h
+++ include/uapi/linux/bpf.h
@@ -437,6 +437,7 @@ enum bpf_func_id {
 	BPF_FUNC_csum_update,
 
 
+
 	/**
 	 * bpf_set_hash_invalid(skb)
 	 * Invalidate current skb>hash.
@@ -452,6 +453,15 @@ enum bpf_func_id {
 	 *     field is missing inside sk_buff
 	 */
 	BPF_FUNC_get_socket_cookie,
+
+	/**
+	 * u32 bpf_get_socket_uid(skb)
+	 *     Get the owner uid of the socket stored inside sk_buff.
+	 *     @skb: pointer to skb
+	 *     Return: uid of the socket owner on success or 0 if the socket pointer
+	 *     inside sk_buff is NULL
+	 */
+	BPF_FUNC_get_socket_uid,
 
 	__BPF_FUNC_MAX_ID,
 };
