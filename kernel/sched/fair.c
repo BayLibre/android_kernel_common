cf7b6fe0f1c909997ad94dbc58f06aa087523400
Leo Yan <leo.yan@linaro.org>
2017-03-27 15:00:14 +0100

    ANDROID: sched/fair: kick nohz idle balance for misfit task
    If there have misfit task on one CPU, current code does not handle this
    situation for nohz idle balance. As result, we can see the misfit task
    stays run on little core for long time.
    
    So this patch check if the CPU has misfit task or not. If has misfit
    task then kick nohz idle balance so finally can execute active balance.
    
    Change-Id: I117d3b7404296f8de11cb960a87a6b9a54a9f348
    Signed-off-by: Leo Yan <leo.yan at linaro.org>
    [taken from https://lists.linaro.org/pipermail/eas-dev/2016-September/000551.html]
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -9924,6 +9924,10 @@ static inline bool nohz_kick_needed(struct rq *rq)
 	    (!energy_aware() || cpu_overutilized(cpu)))
 		return true;
 
+	/* Do idle load balance if there have misfit task */
+	if (energy_aware() && rq->misfit_task)
+		return true;
+
 	rcu_read_lock();
 	sds = rcu_dereference(per_cpu(sd_llc_shared, cpu));
 	if (sds && !energy_aware()) {
