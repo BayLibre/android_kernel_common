2c1ef3235cbd0352e1f324491b74f21406f59052
Lingfeng Yang <lfy@google.com>
2017-03-28 09:05:26 -0700

    ANDROID: goldfish: Get latest goldfish drivers
    This simply resets the drivers/platform/goldfish and
    drivers/staging/goldfish to the ToT of android-goldfish-3.10,
    which is:
    
    c2dbe7bbc MIPS: ranchu: enable hwbinder
    
    Also tries to leave goldfish_sync alone as thats been updated
    for 4.9 already.
    
    Change-Id: I54e9db2a49f08a18038e1cb18bd31be142710cdb
    Signed-off-by: Lingfeng Yang <lfy@google.com>

--- drivers/platform/goldfish/goldfish_pipe.h
+++ drivers/platform/goldfish/goldfish_pipe.h
@@ -73,7 +73,7 @@ struct goldfish_pipe_dev {
 
 	/* Pointers to the buffers host uses for interaction with this driver */
 	struct goldfish_pipe_dev_buffers *buffers;
-
+	
 	/* Head of a doubly linked list of signalled pipes */
 	struct goldfish_pipe *first_signalled_pipe;
 
@@ -84,8 +84,14 @@ struct goldfish_pipe_dev {
 
 	/* v1-specific access parameters */
 	struct access_params *aps;
+
+	/* DMA info */
+	u64 dma_alloc_total;
+
+	/* ptr to platform device's device struct */
+	struct device *pdev_dev;
 };
 
-extern struct goldfish_pipe_dev pipe_dev[1];
+extern struct goldfish_pipe_dev goldfish_pipe_dev[1];
 
 #endif /* GOLDFISH_PIPE_H */
