45a92642f50cd75f92fd492994a2f3e8339b8190
Pavankumar Kondeti <pkondeti@codeaurora.org>
2017-06-28 12:00:31 +0530

    ANDROID: softirq: defer softirq processing to ksoftirqd if CPU is busy with RT
    Defer the softirq processing to ksoftirqd if a RT task is running
    or queued on the current CPU. This complements the RT task placement
    algorithm which tries to find a CPU that is not currently busy with
    softirqs.
    
    Currently NET_TX, NET_RX, BLOCK and TASKLET softirqs are only deferred
    as they can potentially run for long time.
    
    Bug: 168521633
    Change-Id: I9828d641e92e40a798e899b60aba83dbc59675bb
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    [satyap@codeaurora.org: trivial merge conflict resolution.]
    Signed-off-by: Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>
    [elavila: Port to mainline, squash with bugfix]
    Signed-off-by: J. Avila <elavila@google.com>
    Signed-off-by: Shaleen Agrawal <shalagra@codeaurora.org>

--- kernel/softirq.c
+++ kernel/softirq.c
@@ -515,6 +515,16 @@ static inline bool lockdep_softirq_start(void) { return false; }
 static inline void lockdep_softirq_end(bool in_hardirq) { }
 #endif
 
+#define softirq_deferred_for_rt(pending)		\
+({							\
+	__u32 deferred = 0;				\
+	if (cpupri_check_rt()) {			\
+		deferred = pending & LONG_SOFTIRQ_MASK; \
+		pending &= ~LONG_SOFTIRQ_MASK;		\
+	}						\
+	deferred;					\
+})
+
 asmlinkage __visible void __softirq_entry __do_softirq(void)
 {
 	unsigned long end = jiffies + MAX_SOFTIRQ_TIME;
@@ -522,6 +532,7 @@ asmlinkage __visible void __softirq_entry __do_softirq(void)
 	int max_restart = MAX_SOFTIRQ_RESTART;
 	struct softirq_action *h;
 	bool in_hardirq;
+	__u32 deferred;
 	__u32 pending;
 	int softirq_bit;
 
@@ -534,13 +545,15 @@ asmlinkage __visible void __softirq_entry __do_softirq(void)
 
 	pending = local_softirq_pending();
 
+	deferred = softirq_deferred_for_rt(pending);
 	softirq_handle_begin();
 	in_hardirq = lockdep_softirq_start();
 	account_softirq_enter(current);
 
 restart:
 	/* Reset the pending bitmask before enabling irqs */
-	set_softirq_pending(0);
+
+	set_softirq_pending(deferred);
 
 	local_irq_enable();
 
@@ -577,13 +590,18 @@ restart:
 	local_irq_disable();
 
 	pending = local_softirq_pending();
+	deferred = softirq_deferred_for_rt(pending);
+
 	if (pending) {
 		if (time_before(jiffies, end) && !need_resched() &&
 		    --max_restart)
 			goto restart;
-
-		wakeup_softirqd();
 	}
+
+#ifdef CONFIG_RT_SOFTINT_OPTIMIZATION
+	if (pending | deferred)
+		wakeup_softirqd();
+#endif
 
 	account_softirq_exit(current);
 	lockdep_softirq_end(in_hardirq);
