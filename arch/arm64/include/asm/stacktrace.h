d9908a3bbea5121cedca2a747317e8056a0567cf
Mark Rutland <mark.rutland@arm.com>
2017-07-31 21:17:03 +0100

    BACKPORT: arm64: use an irq stack pointer
    We allocate our IRQ stacks using a percpu array. This allows us to generate our
    IRQ stack pointers with adr_this_cpu, but bloats the kernel Image with the boot
    CPU's IRQ stack. Additionally, these are packed with other percpu variables,
    and aren't guaranteed to have guard pages.
    
    When we enable VMAP_STACK we'll want to vmap our IRQ stacks also, in order to
    provide guard pages and to permit more stringent alignment requirements. Doing
    so will require that we use a percpu pointer to each IRQ stack, rather than
    allocating a percpu IRQ stack in the kernel image.
    
    This patch updates our IRQ stack code to use a percpu pointer to the base of
    each IRQ stack. This will allow us to change the way the stack is allocated
    with minimal changes elsewhere. In some cases we may try to backtrace before
    the IRQ stack pointers are initialised, so on_irq_stack() is updated to account
    for this.
    
    In testing with cyclictest, there was no measureable difference between using
    adr_this_cpu (for irq_stack) and ldr_this_cpu (for irq_stack_ptr) in the IRQ
    entry path.
    
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Reviewed-by: Will Deacon <will.deacon@arm.com>
    Tested-by: Laura Abbott <labbott@redhat.com>
    Cc: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: James Morse <james.morse@arm.com>
    (cherry picked from commit f60fe78f133243e6de0f05fdefc3ed2f3c5085ca)
    [astrachan: minor rediff against android-4.9 changes]
    Bug: 117194687
    Change-Id: I9e97d4a00417bd611b352bb1d73a5f221ea1e04e
    Signed-off-by: Alistair Strachan <astrachan@google.com>

--- arch/arm64/include/asm/stacktrace.h
+++ arch/arm64/include/asm/stacktrace.h
@@ -30,5 +30,27 @@ struct stackframe {
 extern int unwind_frame(struct task_struct *tsk, struct stackframe *frame);
 extern void walk_stackframe(struct task_struct *tsk, struct stackframe *frame,
 			    int (*fn)(struct stackframe *, void *), void *data);
+extern void dump_backtrace(struct pt_regs *regs, struct task_struct *tsk);
+
+DECLARE_PER_CPU(unsigned long *, irq_stack_ptr);
+
+static inline bool on_irq_stack(unsigned long sp)
+{
+	unsigned long low = (unsigned long)raw_cpu_read(irq_stack_ptr);
+	unsigned long high = low + IRQ_STACK_SIZE;
+
+	if (!low)
+		return false;
+
+	return (low <= sp && sp < high);
+}
+
+static inline bool on_task_stack(struct task_struct *tsk, unsigned long sp)
+{
+	unsigned long low = (unsigned long)task_stack_page(tsk);
+	unsigned long high = low + THREAD_SIZE;
+
+	return (low <= sp && sp < high);
+}
 
 #endif	/* __ASM_STACKTRACE_H */
