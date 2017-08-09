b88ed30c22d16cf6cbf13d3c91552ee2d1b457ce
Leo Yan <leo.yan@linaro.org>
2017-08-09 10:14:32 +0800

    sched/fair: take account of margin for finding capacity index
    CPUFreq governors use 20% margin as tipping point to increase frequency,
    "schedfreq" and "schedutil" governors both leave 20% margin to trigger
    higher frequency so can leave head room for performance.
    
    But the energy calculation doesn't take account of 20% margin, so this
    underestimates the frequency when place task on one CPU. Finally this
    is not alignment between energy calculation and real frequency
    selection in CPUFreq governors.
    
    This patch changes to take account of 20% margin for finding capacity
    index, so it can be more sensitive for frequency when calculate energy.
    
    Change-Id: I28c438eaecd3b1367c59a64138700cb812e816c3
    Signed-off-by: Leo Yan <leo.yan@linaro.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5387,7 +5387,7 @@ static int find_new_capacity(struct energy_env *eenv,
 	unsigned long util = group_max_util(eenv);
 
 	for (idx = 0; idx < sge->nr_cap_states; idx++) {
-		if (sge->cap_states[idx].cap >= util)
+		if (cmp_capacity_margin(sge->cap_states[idx].cap, util))
 			break;
 	}
 
