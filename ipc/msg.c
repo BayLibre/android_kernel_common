4d969df2b4b0514052dfb270407d9bbd4dcf6b7a
Prashant Malani <pmalani@google.com>
2017-08-17 13:17:10 -0700

    ipc namespace: Add ipc priv data per namepsace
    This patch is based off of:
      https://lwn.net/Articles/577957/
    
    The patch adds the facility for ipc mechanisms to store private data
    within an ipc namespace.
    
    First, (un)register_per_ipc_ops() routines are provided which an ipc
    driver should use to register itself to store ipc data.
    
    Functions to create and store the necessary data, as well as to remove
    it should be provided via struct ipc_priv_ops which is passed to the
    register call. Thereafter, ipc_access_generic() and ipc_assign_generic()
    are used to access the modify the data.
    
    Change-Id: Ie060a69d1f0edebe0759f3373be32b6c7ad1c59a
    Signed-off-by: Prashant Malani <pmalani@google.com>

--- ipc/msg.c
+++ ipc/msg.c
@@ -1057,6 +1057,7 @@ static int sysvipc_msg_proc_show(struct seq_file *s, void *it)
 void __init msg_init(void)
 {
 	msg_init_ns(&init_ipc_ns);
+	INIT_HLIST_HEAD(&init_ipc_ns.ipc_priv_data_list);
 
 	ipc_init_proc_interface("sysvipc/msg",
 				"       key      msqid perms      cbytes       qnum lspid lrpid   uid   gid  cuid  cgid      stime      rtime      ctime\n",
