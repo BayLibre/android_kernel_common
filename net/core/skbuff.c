a977a979bdea3f30044051e817ce44c016655c91
Steffen Klassert <steffen.klassert@secunet.com>
2017-08-31 07:22:35 +0200

    BACKPORT: skbuff: Reset secpath only on xnet when scrub a packet
    Signed-off-by: Benedict Wong <benedictwong@google.com>
    (cherry picked from commit 3542f3f0523bd7d19a4e8677d953d8e04476262c)

--- net/core/skbuff.c
+++ net/core/skbuff.c
@@ -4393,7 +4393,6 @@ void skb_scrub_packet(struct sk_buff *skb, bool xnet)
 	skb->skb_iif = 0;
 	skb->ignore_df = 0;
 	skb_dst_drop(skb);
-	secpath_reset(skb);
 	nf_reset(skb);
 	nf_reset_trace(skb);
 
@@ -4401,6 +4400,7 @@ void skb_scrub_packet(struct sk_buff *skb, bool xnet)
 		return;
 
 	ipvs_reset(skb);
+	secpath_reset(skb);
 	skb_orphan(skb);
 	skb->mark = 0;
 }
