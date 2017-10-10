89e0528e42dad63bcaac8baa2761d15188ab791c
Daniel Rosenberg <drosen@google.com>
2017-10-10 14:57:01 -0700

    ANDROID: staging: Create pkglist for configuration
    This moves the configuration for sdcardfs into an
    independent driver, allowing the code to be reused
    for alternate approaches or implementations. It
    also provides a way to swap out the implementation
    if others are desired.
    
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    Change-Id: I466bdb280d5a215a06f699b0c8c1dba0efac94c8

---
+++ drivers/staging/android/pkglist.h
@@ -0,0 +1,43 @@
+#ifndef _PKGLIST_H_
+#define _PKGLIST_H_
+
+#include <linux/dcache.h>
+
+struct hashtable_entry {
+	struct hlist_node hlist;
+	struct hlist_node dlist; /* for deletion cleanup */
+	struct qstr key;
+	atomic_t value;
+};
+
+#define QSTR_LITERAL(string) QSTR_INIT(string, sizeof(string)-1)
+
+static inline bool str_case_eq(const char *s1, const char *s2)
+{
+	return !strcasecmp(s1, s2);
+}
+
+static inline bool str_n_case_eq(const char *s1, const char *s2, size_t len)
+{
+	return !strncasecmp(s1, s2, len);
+}
+
+static inline bool qstr_case_eq(const struct qstr *q1, const struct qstr *q2)
+{
+	return q1->len == q2->len && str_case_eq(q1->name, q2->name);
+}
+
+#define BY_NAME		BIT(0)
+#define BY_USERID	BIT(1)
+
+struct pkg_list {
+	struct list_head list;
+	void (*update)(int flags, const struct qstr *name, unsigned int userid);
+};
+kuid_t pkglist_get_appid(const char *key);
+kgid_t pkglist_get_ext_gid(const char *key);
+bool pkglist_user_is_excluded(const char *key, uid_t user);
+kuid_t pkglist_get_allowed_appid(const char *key, uid_t user);
+void pkglist_register_update_listener(struct pkg_list *pkg);
+void pkglist_unregister_update_listener(struct pkg_list *pkg);
+#endif
