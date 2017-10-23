0ebe589c2c7aaa48a2c56249cb959467ebf232ea
Leo Yan <leo.yan@linaro.org>
2017-10-23 15:14:34 +0800

    sched: schedutil: Add trace event for requested util
    The schedutil governor accumulates the RT and CFS classes utilization to
    change CPU frequency. This commit adds trace event to track the
    utilization from RT, CFS and total value. The trace event can be used
    for detailed analysis which part contributes for frequency change.
    
    Change-Id: I1e4d46eeb37634a10180294c1f9df03ca3fa9eeb
    Signed-off-by: Leo Yan <leo.yan@linaro.org>

--- include/trace/events/power.h
+++ include/trace/events/power.h
@@ -554,6 +554,32 @@ DEFINE_EVENT(dev_pm_qos_request, dev_pm_qos_remove_request,
 
 	TP_ARGS(name, type, new_value)
 );
+
+TRACE_EVENT(sched_sugov_util,
+
+	TP_PROTO(int cpu, unsigned long rt, unsigned long cfs,
+		 unsigned long total),
+
+	TP_ARGS(cpu, rt, cfs, total),
+
+	TP_STRUCT__entry(
+		__field( int,	cpu				)
+		__field( unsigned long,	rt_util			)
+		__field( unsigned long,	cfs_util		)
+		__field( unsigned long,	total_util		)
+	),
+
+	TP_fast_assign(
+		__entry->cpu			= cpu;
+		__entry->rt_util		= rt;
+		__entry->cfs_util		= cfs;
+		__entry->total_util		= total;
+	),
+
+	TP_printk("cpu=%d rt_util=%lu cfs_util=%lu total_util=%lu",
+		  __entry->cpu, __entry->rt_util, __entry->cfs_util,
+		  __entry->total_util)
+);
 #endif /* _TRACE_POWER_H */
 
 /* This part must be outside protection */
