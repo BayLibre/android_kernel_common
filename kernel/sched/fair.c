88becbedd54d8436ff0e2bf51eb6e3e03a2a218e
Joel Fernandes <joelaf@google.com>
2017-10-25 15:24:42 +0100

    ANDROID: sched: fair: Bypass energy-aware wakeup for prefer-idle tasks
    Use the upstream slow path to find an idle cpu for prefer-idle tasks.
    This slow-path is actually faster than the EAS path we are currently
    going through (select_energy*) which is really slow.
    
    No performance degradation is seen with this and it reduces the delta
    quite a bit between upstream and out of tree code.
    
    It's not clear yet if using the mainline slow path task placement when
    a task has the schedtune attribute prefer_idle=1 is the right thing to
    do for products. Put the option to disable this behind a sched feature
    so we can try out both options.
    
    Change-Id: Icf762a101c92c0e3f9e61df0370247fa15455581
    Signed-off-by: Joel Fernandes <joelaf@google.com>
    (refactored for 4.14 version)
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6707,6 +6707,7 @@ static void nohz_balancer_kick(bool only_update);
 static inline int wake_energy(struct task_struct *p, int prev_cpu, int sd_flag, int wake_flags)
 {
 	struct sched_domain *sd = NULL;
+	int sync = wake_flags & WF_SYNC;
 
 	rcu_read_lock();
 	sd = rcu_dereference_sched(cpu_rq(prev_cpu)->sd);
@@ -6723,6 +6724,14 @@ static inline int wake_energy(struct task_struct *p, int prev_cpu, int sd_flag, 
 	if (sd_overutilized(sd))
 		return false;
 
+	if(sched_feat(MAINLINE_PREFER_IDLE)){
+		/*
+		 * Force prefer-idle tasks into the slow path, this may not happen
+		 * if none of the sd flags matched.
+		 */
+		if (schedtune_prefer_idle(p) > 0 && !sync)
+			return false;
+	}
 	return true;
 }
 
