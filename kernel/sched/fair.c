efd062d01c4ceac5abbce3f321bfd1a49ac28bab
Chris Redpath <chris.redpath@arm.com>
2017-10-28 11:08:29 +0100

    ANDROID: sched: EAS: Select correct capacity state for energy_diff
    The util returned from group_max_util is not capped at the max util
    present in the group. This means it can be larger than the capacity
    stored in the array. Ensure that when this happens, we always use
    the last entry in the array to fetch energy from.
    
    Change-Id: I6f7e8d93c4a161564e37b0038ac8e776732ed0b2
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5503,14 +5503,16 @@ static int find_new_capacity(struct energy_env *eenv,
 	int idx;
 	unsigned long util = group_max_util(eenv);
 
+	eenv->cap_idx = sge->nr_cap_states - 1;
+
 	for (idx = 0; idx < sge->nr_cap_states; idx++) {
-		if (sge->cap_states[idx].cap >= util)
+		if (sge->cap_states[idx].cap >= util) {
+			eenv->cap_idx = idx;
 			break;
+		}
 	}
 
-	eenv->cap_idx = idx;
-
-	return idx;
+	return eenv->cap_idx;
 }
 
 static int group_idle_state(struct sched_group *sg)
