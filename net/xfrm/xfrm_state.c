9ca36445f13cb4b1feeb03867b7bb1e4e340fc93
Lorenzo Colitti <lorenzo@google.com>
2017-11-16 19:50:08 +0900

    net: xfrm: allow clearing socket xfrm policies.
    Currently it is possible to add or update socket policies, but
    not clear them. Therefore, once a socket policy has been applied,
    the socket cannot be used for unencrypted traffic.
    
    This patch allows (privileged) users to clear socket policies by
    passing in a zero-length argument to {IP,IPV6}_XFRM_POLICY.
    
    This results in both the incoming and outgoing policies being
    cleared.  The simple approach taken in this patch cannot support
    clearing socket policies in only one direction, because the
    direction is part of the xfrm_user_policy object.
    
    Bug: 65857891
    Tested: https://android-review.googlesource.com/539816
    Change-Id: Ib3d5c7c6179979d62a0fb66e36258a57aaa7b382

--- net/xfrm/xfrm_state.c
+++ net/xfrm/xfrm_state.c
@@ -1883,7 +1883,14 @@ int xfrm_user_policy(struct sock *sk, int optname, u8 __user *optval, int optlen
 	struct xfrm_mgr *km;
 	struct xfrm_policy *pol = NULL;
 
-	if (optlen <= 0 || optlen > PAGE_SIZE)
+	if (optlen == 0) {
+		xfrm_sk_policy_insert(sk, XFRM_POLICY_IN, NULL);
+		xfrm_sk_policy_insert(sk, XFRM_POLICY_OUT, NULL);
+		__sk_dst_reset(sk);
+		return 0;
+	}
+
+	if (optlen < 0 || optlen > PAGE_SIZE)
 		return -EMSGSIZE;
 
 	data = kmalloc(optlen, GFP_KERNEL);
