114bc4e9b511fab47ed1114eef63b1c78d49c804
Sami Tolvanen <samitolvanen@google.com>
2018-05-02 11:05:22 -0700

    ANDROID: arm64: implement Shadow Call Stack
    Bug: 112277034
    Change-Id: Ie18cb3738861828f7befef73b9c957340b0370d1
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>

--- arch/arm64/include/asm/thread_info.h
+++ arch/arm64/include/asm/thread_info.h
@@ -43,6 +43,10 @@ struct thread_info {
 	u64			ttbr0;		/* saved TTBR0_EL1 */
 #endif
 	int			preempt_count;	/* 0 => preemptable, <0 => bug */
+#ifdef CONFIG_SHADOW_CALL_STACK
+	void			*shadow_call_stack;
+	void			*shadow_call_stack_base;
+#endif
 };
 
 #define INIT_THREAD_INFO(tsk)						\
