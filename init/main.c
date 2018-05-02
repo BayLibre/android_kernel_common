a087fe547a826bb530a1801f0ce627234a279505
Sami Tolvanen <samitolvanen@google.com>
2018-05-02 10:44:59 -0700

    ANDROID: add support for clang Shadow Call Stack (SCS)
    This change adds generic support for clang's Shadow Call Stack, which
    uses a shadow stack to protect return addresses from being overwritten
    by an attacker. Details are available here:
    
      https://clang.llvm.org/docs/ShadowCallStack.html
    
    Bug: 112277034
    Change-Id: Idd553b7c978b0673ab533a68980fb9a654f4510c
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>

--- init/main.c
+++ init/main.c
@@ -92,6 +92,7 @@
 #include <linux/rodata_test.h>
 #include <linux/jump_label.h>
 #include <linux/mem_encrypt.h>
+#include <linux/scs.h>
 
 #include <asm/io.h>
 #include <asm/bugs.h>
@@ -534,6 +535,8 @@ asmlinkage __visible void __init start_kernel(void)
 	char *after_dashes;
 
 	set_task_stack_end_magic(&init_task);
+	scs_set_init_magic(&init_task);
+
 	smp_setup_processor_id();
 	debug_objects_early_init();
 
