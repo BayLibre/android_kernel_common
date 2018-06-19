f48c1372df1c348dbb9d52f0ac17b11044479be7
Kyle Tso <kyletso@google.com>
2018-06-19 21:10:25 +0800

    ANDROID: usb: typec: tcpm: Notify TCPC when usb_comm_capable is set
    Cherry-pick of <4c023575d40e37f7ce6fdca3dd10648a5456fe23>
    
    If HVDCP_TIMEOUT is skipped in smb-lib, PD state machine will start
    before APSD finishes. If device is UFP and PD port partner supports USB
    data capability, device will not start USB controller because the
    charger type is unknown. To fix this, notify TCPC of the USB capability
    of the port partner. TCPC will start USB controller regardless of the
    charger type if usb_comm_capable is set.
    
    Bug: 158509499
    Signed-off-by: Kyle Tso <kyletso@google.com>
    Signed-off-by: Badhri Jagan Sridharan <badhri@google.com>
    (cherry picked from commit 95a81992da27e2f2c101eaab97063730f9b7455c)
    (cherry picked from commit 94c0b1549f9e9283b331c4e45b46bd1b8d85e74b)
    Signed-off-by: Will McVicker <willmcvicker@google.com>
    (cherry picked from commit 6d955c6b7746088a10e56388c4a27df856aae8a7)
    Signed-off-by: Will McVicker <willmcvicker@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I813ffe37e1284d859fefc24b31b9403b8265c23c

--- drivers/usb/typec/tcpm/tcpm.c
+++ drivers/usb/typec/tcpm/tcpm.c
@@ -3910,6 +3910,8 @@ static void run_state_machine(struct tcpm_port *port)
 			tcpm_pd_send_control(port, PD_CTRL_ACCEPT);
 			tcpm_set_partner_usb_comm_capable(port,
 							  !!(port->sink_request & RDO_USB_COMM));
+			/* Notify TCPC of usb_comm_capable. */
+			tcpm_set_attached_state(port, true);
 			tcpm_set_state(port, SRC_TRANSITION_SUPPLY,
 				       PD_T_SRC_TRANSITION);
 		}
@@ -4145,6 +4147,8 @@ static void run_state_machine(struct tcpm_port *port)
 		tcpm_set_pd_capable(port, true);
 		tcpm_set_partner_usb_comm_capable(port,
 						  !!(port->source_caps[0] & PDO_FIXED_USB_COMM));
+		/* Notify TCPC of usb_comm_capable. */
+		tcpm_set_attached_state(port, true);
 		port->hard_reset_count = 0;
 		ret = tcpm_pd_send_request(port);
 		if (ret < 0) {
