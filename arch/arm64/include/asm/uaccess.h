70ba31f2b5cfd8e8542b62e40847ab6d81e9f365
Andrey Konovalov <andreyknvl@google.com>
2018-08-30 13:41:06 +0200

    FROMLIST: arm64: add type casts to untagged_addr macro
    (from https://lore.kernel.org/patchwork/patch/979329/)
    
    This patch makes the untagged_addr macro accept all kinds of address types
    (void *, unsigned long, etc.) and allows not to specify type casts in each
    place where it is used. This is done by using __typeof__.
    
    Bug: 112461694
    Change-Id: I4471e564990d46b77e24a83d2fa43c0eb475c68d
    Signed-off-by: Andrey Konovalov <andreyknvl@google.com>

--- arch/arm64/include/asm/uaccess.h
+++ arch/arm64/include/asm/uaccess.h
@@ -126,7 +126,8 @@ static inline void set_fs(mm_segment_t fs)
  * up with a tagged userland pointer. Clear the tag to get a sane pointer to
  * pass on to access_ok(), for instance.
  */
-#define untagged_addr(addr)		sign_extend64(addr, 55)
+#define untagged_addr(addr)		\
+	((__typeof__(addr))sign_extend64((__u64)(addr), 55))
 
 #define access_ok(type, addr, size)	__range_ok(addr, size)
 #define user_addr_max			get_fs
