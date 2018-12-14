4f07a1d79b77fa2f3ff588241c87650641fa8566
Jaegeuk Kim <jaegeuk@kernel.org>
2018-12-14 11:14:38 -0800

    FROMLIST: loop: drop caches if offset or block_size are changed
    If we don't drop caches used in old offset or block_size, we can get old data
    from new offset/block_size, which gives unexpected data to user.
    
    For example, Martijn found a loopback bug in the below scenario.
    1) LOOP_SET_FD loads first two pages on loop file
    2) LOOP_SET_STATUS64 changes the offset on the loop file
    3) mount is failed due to the cached pages having wrong superblock
    
    Link: https://www.spinics.net/lists/linux-block/msg35051.html
    Change-Id: Iffb7e1f04ab587e1a8785bc862a425efb654be24
    Cc: Jens Axboe <axboe@kernel.dk>
    Cc: linux-block@vger.kernel.org
    Reported-by: Martijn Coenen <maco@google.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- drivers/block/loop.c
+++ drivers/block/loop.c
@@ -1137,11 +1137,18 @@ loop_set_status(struct loop_device *lo, const struct loop_info64 *info)
 		goto exit;
 
 	if (lo->lo_offset != info->lo_offset ||
-	    lo->lo_sizelimit != info->lo_sizelimit)
+	    lo->lo_sizelimit != info->lo_sizelimit) {
+		struct block_device *bdev = lo->lo_device;
+
+		/* drop stale caches used in old offset */
+		sync_blockdev(bdev);
+		kill_bdev(bdev);
+
 		if (figure_loop_size(lo, info->lo_offset, info->lo_sizelimit)) {
 			err = -EFBIG;
 			goto exit;
 		}
+	}
 
 	loop_config_discard(lo);
 
@@ -1357,6 +1364,15 @@ static int loop_set_block_size(struct loop_device *lo, unsigned long arg)
 
 	blk_queue_logical_block_size(lo->lo_queue, arg);
 	loop_update_dio(lo);
+
+	/* Don't change the size if it is same as current */
+	if (lo->lo_queue->limits.logical_block_size != arg) {
+		struct block_device *bdev = lo->lo_device;
+
+		/* drop stale caches likewise set_blocksize */
+		sync_blockdev(bdev);
+		kill_bdev(bdev);
+	}
 
 	blk_mq_unfreeze_queue(lo->lo_queue);
 
