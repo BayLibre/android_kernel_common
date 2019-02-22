93c262878313cc6a2ded26a8a36233e580fe125c
Arve Hjønnevåg <arve@android.com>
2019-02-22 13:02:56 -0800

    wip: trusty: Add hook to share memory with trusty
    TODO: revoke shared memory
    If trusty runs in an environment where it does not have access to all
    memory, any memory passed to trusty must be explicitly shared. Add a
    hook to the custom smc struct to allow this and update code to call it.
    
    Change-Id: I42c8acad0e77ab6e19c76ed1ee0777c9e091e023

--- drivers/trusty/trusty-mem.c
+++ drivers/trusty/trusty-mem.c
@@ -113,6 +113,7 @@ int trusty_call32_mem_buf(struct device *dev, u32 smcnr,
 {
 	int ret;
 	struct ns_mem_page_info pg_inf;
+	phys_addr_t paddr;
 
 	if (!dev || !page)
 		return -EINVAL;
@@ -120,6 +121,14 @@ int trusty_call32_mem_buf(struct device *dev, u32 smcnr,
 	ret = trusty_encode_page_info(&pg_inf, page, pgprot);
 	if (ret)
 		return ret;
+
+	paddr = page_to_phys(page);
+	ret = trusty_share_memory(dev, paddr, size, 0);
+	if (ret) {
+		dev_err(dev, "trusty_share_memory failed: %d %pa, smc 0x%x\n",
+			ret, &paddr, smcnr);
+		return ret;
+	}
 
 	if (SMC_IS_FASTCALL(smcnr)) {
 		return trusty_fast_call32(dev, smcnr,
