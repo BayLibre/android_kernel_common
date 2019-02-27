df29c63bc7878950f5061afd60400a62214e6d50
Quentin Perret <quentin.perret@arm.com>
2019-02-27 10:29:47 +0000

    ANDROID: sched/fair: Remove android wake-up tweaks
    Remove all of the Android-specific tweaks to the wake-up path, including
    find_best_target() and other related functions. While at it, also revert
    
      FROMLIST: sched/fair: Use wake_q length as a hint for wake_wide
    
    which isn't used in Android. This brings the wake-up path very close to
    mainline, and provides a clean start to re-implement the whole thing in
    a much cleaner and concise way.
    
    Change-Id: Ia1887affc0dad05bafc224016180859be2fbc876
    Signed-off-by: Quentin Perret <quentin.perret@arm.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -961,7 +961,6 @@ struct rq {
 #ifdef CONFIG_CPU_IDLE
 	/* Must be inspected within a rcu lock section */
 	struct cpuidle_state	*idle_state;
-	int			idle_state_idx;
 #endif
 };
 
@@ -1681,8 +1680,7 @@ struct sched_class {
 	void (*put_prev_task)(struct rq *rq, struct task_struct *p);
 
 #ifdef CONFIG_SMP
-	int  (*select_task_rq)(struct task_struct *p, int task_cpu, int sd_flag, int flags,
-			       int subling_count_hint);
+	int  (*select_task_rq)(struct task_struct *p, int task_cpu, int sd_flag, int flags);
 	void (*migrate_task_rq)(struct task_struct *p, int new_cpu);
 
 	void (*task_woken)(struct rq *this_rq, struct task_struct *task);
@@ -1770,17 +1768,6 @@ static inline struct cpuidle_state *idle_get_state(struct rq *rq)
 
 	return rq->idle_state;
 }
-
-static inline void idle_set_state_idx(struct rq *rq, int idle_state_idx)
-{
-	rq->idle_state_idx = idle_state_idx;
-}
-
-static inline int idle_get_state_idx(struct rq *rq)
-{
-	WARN_ON(!rcu_read_lock_held());
-	return rq->idle_state_idx;
-}
 #else
 static inline void idle_set_state(struct rq *rq,
 				  struct cpuidle_state *idle_state)
@@ -1790,15 +1777,6 @@ static inline void idle_set_state(struct rq *rq,
 static inline struct cpuidle_state *idle_get_state(struct rq *rq)
 {
 	return NULL;
-}
-
-static inline void idle_set_state_idx(struct rq *rq, int idle_state_idx)
-{
-}
-
-static inline int idle_get_state_idx(struct rq *rq)
-{
-	return -1;
 }
 #endif
 
