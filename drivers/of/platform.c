ded4a216d0fa1316472684cd01eaecae0d150a25
Saravana Kannan <saravanak@google.com>
2019-05-14 14:25:31 -0700

    of/platform: Add functional dependency link from "depends-on" property
    Add a generic "depends-on" property that allows specifying mandatory
    functional dependencies between devices.
    
    This property is intentionally explicit and separate from existing DT
    properties that specify phandles of other devices (Eg: clocks,
    pinctrl, regulators, etc). This is because not all resources referred
    to by existing DT properties are mandatory functional dependencies.
    Some devices/drivers might be able to operate with reduced
    functionality when some of the resources aren't available.
    
    For example, a device could operating in polling mode if no IRQ is
    available, a device could skip doing power management if clock or
    voltage control isn't available and they are left on, etc.
    
    So, adding mandatory functional dependency links between devices by
    looking at referred phandles in DT properties won't work as it would
    prevent probing devices that could be probed. By having an explicit
    depends-on property, we can handle these cases correctly.
    
    Having functional dependencies explicitly called out in DT and
    automatically added before the devices are probed, provides the
    following benefits:
    
    - This allows optimizing device probe order and reduces time wasted by
      attempting probes of devices that will not probe successfully because
      their suppliers haven't probed yet.
    
    - Supplier devices like clock providers, regulators providers, etc need
      to keep the resources they provide active and at a particular state
      during boot up even if their current set of consumers don't request
      the resource to be active. This is because the rest of the consumers
      might not have probed yet and turning off the resource before all the
      consumers have probed could lead to a hang or undesired user
      experience.
    
      Some frameworks (Eg: regulator) handle this today by turning off
      "unused" resources at late_initcall_sync and hoping all the devices
      have probed by then.  This is not a valid assumption for systems
      with loadable modules.  Other frameworks (Eg: clock) just don't
      handle this due to the lack of a clear signal for when they can turn
      off resources. This leads to downstream hacks to handle cases like
      this that can easily be solved in the upstream kernel.
    
      By linking devices before they are probed, we give suppliers a clear
      count of the number of dependent consumers. Once all of the consumers
      are active, the suppliers can turn off the unused resources without
      making assumptions about the number of consumers.
    
    By default we just add functional dependency links to track "driver
    presence". If any other functionality provided by device links are
    needed, it is left to the consumer/supplier devices to change the link
    when they probe.
    
    Change-Id: I44c9e282bc960627d772c0db702395cdb537a280
    Signed-off-by: Saravana Kannan <saravanak@google.com>

--- drivers/of/platform.c
+++ drivers/of/platform.c
@@ -68,6 +68,73 @@ struct platform_device *of_find_device_by_node(struct device_node *np)
 EXPORT_SYMBOL(of_find_device_by_node);
 
 #ifdef CONFIG_OF_ADDRESS
+
+static LIST_HEAD(wait_for_suppliers);
+static DEFINE_MUTEX(wfs_lock);
+static bool link_waiting_consumers_enable;
+
+static int __of_link_to_suppliers(struct device *dev)
+{
+	struct device_node *sup_node;
+	struct device *sup_dev;
+	unsigned int i = 0, links = 0;
+	u32 dl_flags = DL_FLAG_AUTOPROBE_CONSUMER;
+
+	while ((sup_node = of_parse_phandle(dev->of_node, "depends-on", i))) {
+		sup_dev = &of_find_device_by_node(sup_node)->dev;
+		if (device_link_add(dev, sup_node->dev, dl_flags))
+			links++;
+		put_device(sup_dev);
+		i++;
+	}
+	if (links < i)
+		return -ENODEV;
+	return 0;
+}
+
+static int of_link_to_suppliers(struct device *dev)
+{
+	if (unlikely(!dev->of_node))
+		return 0;
+
+	if (__of_link_to_suppliers(dev)) {
+		mutex_lock(&wfs_lock);
+		list_add_tail(&dev->links.needs_suppliers, &wait_for_suppliers);
+		mutex_unlock(&wfs_lock);
+		return -ENODEV;
+	}
+
+	return 0;
+}
+
+static int of_link_waiting_consumers(void)
+{
+	struct device *dev, *tmp;
+
+	mutex_lock(&wfs_lock);
+	list_for_each_entry_safe(dev, tmp, &wait_for_suppliers,
+				 links.needs_suppliers)
+		if (!__of_link_to_suppliers(dev))
+			list_del_init(&dev->links.needs_suppliers);
+	mutex_unlock(&wfs_lock);
+
+	return 0;
+}
+
+static void link_waiting_consumers_func(struct work_struct *work)
+{
+	of_link_waiting_consumers();
+}
+static DECLARE_WORK(link_waiting_consumers_work, link_waiting_consumers_func);
+
+static void link_waiting_consumers_trigger(void)
+{
+	if (!link_waiting_consumers_enable)
+		return;
+
+	schedule_work(&link_waiting_consumers_work);
+}
+
 /*
  * The following routines scan a subtree and registers a device for
  * each applicable node.
@@ -199,6 +266,7 @@ static struct platform_device *of_platform_device_create_pdata(
 	dev->dev.platform_data = platform_data;
 	of_msi_configure(&dev->dev, dev->dev.of_node);
 
+	of_link_to_suppliers(&dev->dev);
 	if (of_device_add(dev) != 0) {
 		platform_device_put(dev);
 		goto err_clear_flag;
@@ -206,6 +274,7 @@ static struct platform_device *of_platform_device_create_pdata(
 	spin_lock(&of_dev_lock);
 	np->dev = &dev->dev;
 	spin_unlock(&of_dev_lock);
+	link_waiting_consumers_trigger();
 
 	return dev;
 
@@ -554,6 +623,10 @@ static int __init of_platform_default_populate_init(void)
 	/* Populate everything else. */
 	of_platform_default_populate(NULL, NULL, NULL);
 
+	/* Make the device-links between producers and consumers */
+	link_waiting_consumers_enable = true;
+	of_link_waiting_consumers();
+
 	return 0;
 }
 arch_initcall_sync(of_platform_default_populate_init);
@@ -572,6 +645,9 @@ int of_platform_device_destroy(struct device *dev, void *data)
 	spin_lock(&of_dev_lock);
 	dev->of_node->dev = NULL;
 	spin_unlock(&of_dev_lock);
+	mutex_lock(&wfs_lock);
+	list_del(&dev->links.needs_suppliers);
+	mutex_unlock(&wfs_lock);
 	of_node_clear_flag(dev->of_node, OF_POPULATED);
 	of_node_clear_flag(dev->of_node, OF_POPULATED_BUS);
 
