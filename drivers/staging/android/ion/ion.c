e568be779b10e12b4c96318162b7f893c30d14b8
John Stultz <john.stultz@linaro.org>
2019-10-24 00:37:41 +0000

    staging: ion: Try to fix another off by one issue with heap id assignment
    When testing with HiKey960, gralloc could not seemingly find the
    "linux,cma" heap we use for HWFB buffers.
    
    Digging into why this was happening, it seemed when we were
    adding the CMA heaps, the HiKey960 has multiple cma heaps,
    where "linux,cma" is the second one added.
    
    In ion_assign_heap_id(), we would scan the heap_ids for an empty
    bit. For reasons I'm not quite understanding, it subtracts one
    from that id_bit value before calling test_and_set_bit.
    
    Then when we try to assign a second cma heap, we find the same
    empty bit as before, and the test_and_set_bit() call on id_bit-1
    fails.
    
    This patch tries to simplify the logic, by avoiding the id_bit-1
    logic and just using the id_bit value directly, which seems to work
    better.
    
    Signed-off-by: John Stultz <john.stultz@linaro.org>
    Change-Id: I5463ec98884c3253313c5b25ea747e37bdeae9a4

--- drivers/staging/android/ion/ion.c
+++ drivers/staging/android/ion/ion.c
@@ -284,9 +284,9 @@ static int ion_assign_heap_id(struct ion_heap *heap, struct ion_device *dev)
 		}
 	}
 
-	if (test_and_set_bit(id_bit - 1, dev->heap_ids))
+	if (test_and_set_bit(id_bit, dev->heap_ids))
 		return -EEXIST;
-	heap->id = id_bit - 1;
+	heap->id = id_bit;
 	dev->heap_cnt++;
 
 	return 0;
