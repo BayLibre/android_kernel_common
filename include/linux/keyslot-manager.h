b2d6b9884191ccc6cc4afddf74428d6f124c2545
Eric Biggers <ebiggers@google.com>
2019-10-31 09:21:43 -0700

    ANDROID: block: introduce struct blk_crypto_key
    Introduce struct blk_crypto_key to represent a (raw_key, crypto_mode,
    data_unit_size) tuple, i.e. the information that can be programmed into
    a keyslot.  Add a function blk_crypto_alloc_key() which upper layers can
    call to pack this information into a blk_crypto_key.
    
    This simplifies a lot of function prototypes, since now we pass the
    blk_crypto_key instead of the raw_key, crypto_mode, and data_unit_size
    separately.  It will also make it possible for the keyslot manager to
    efficiently start using a hash table without having to add a key_hash
    parameter to lots of functions.
    
    Bug: 137270441
    Test: tested as series, see I618949de4670828bfa3957c1162737d04b59167b
    Change-Id: I1146fd272a7a9b9dc2ecf71bfa3d6a0891eb12e8
    Signed-off-by: Eric Biggers <ebiggers@google.com>

--- include/linux/keyslot-manager.h
+++ include/linux/keyslot-manager.h
@@ -12,12 +12,11 @@
 
 /**
  * struct keyslot_mgmt_ll_ops - functions to manage keyslots in hardware
- * @keyslot_program:	Program the specified key and algorithm into the
- *			specified slot in the inline encryption hardware.
+ * @keyslot_program:	Program the specified key into the specified slot in the
+ *			inline encryption hardware.
  * @keyslot_evict:	Evict key from the specified keyslot in the hardware.
- *			The key, crypto_mode and data_unit_size are also passed
- *			down so that e.g. dm layers can evict keys from
- *			the devices that they map over.
+ *			The key is provided so that e.g. dm layers can evict
+ *			keys from the devices that they map over.
  *			Returns 0 on success, -errno otherwise.
  * @crypto_mode_supported:	Check whether a crypto_mode and data_unit_size
  *				combo is supported.
@@ -27,13 +26,11 @@
  * manager will use to manipulate keyslots in the hardware.
  */
 struct keyslot_mgmt_ll_ops {
-	int (*keyslot_program)(void *ll_priv_data, const u8 *key,
-			       enum blk_crypto_mode_num crypto_mode,
-			       unsigned int data_unit_size,
+	int (*keyslot_program)(void *ll_priv_data,
+			       const struct blk_crypto_key *key,
 			       unsigned int slot);
-	int (*keyslot_evict)(void *ll_priv_data, const u8 *key,
-			     enum blk_crypto_mode_num crypto_mode,
-			     unsigned int data_unit_size,
+	int (*keyslot_evict)(void *ll_priv_data,
+			     const struct blk_crypto_key *key,
 			     unsigned int slot);
 	bool (*crypto_mode_supported)(void *ll_priv_data,
 				      enum blk_crypto_mode_num crypto_mode,
@@ -49,9 +46,7 @@ extern struct keyslot_manager *keyslot_manager_create(unsigned int num_slots,
 
 extern int
 keyslot_manager_get_slot_for_key(struct keyslot_manager *ksm,
-				 const u8 *key,
-				 enum blk_crypto_mode_num crypto_mode,
-				 unsigned int data_unit_size);
+				 const struct blk_crypto_key *key);
 
 extern void keyslot_manager_get_slot(struct keyslot_manager *ksm,
 				     unsigned int slot);
@@ -70,9 +65,7 @@ keyslot_manager_rq_crypto_mode_supported(struct request_queue *q,
 					 unsigned int data_unit_size);
 
 extern int keyslot_manager_evict_key(struct keyslot_manager *ksm,
-				     const u8 *key,
-				     enum blk_crypto_mode_num crypto_mode,
-				     unsigned int data_unit_size);
+				     const struct blk_crypto_key *key);
 
 extern void keyslot_manager_reprogram_all_keys(struct keyslot_manager *ksm);
 
