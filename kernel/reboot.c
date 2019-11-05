515918b2809b8d439fa22819cc1e5da88c1cfbef
Elliot Berman <eberman@codeaurora.org>
2019-11-05 14:09:44 -0800

    reboot: Export reboot_mode
    Export reboot_mode to support SoC DLKMs wishing to modify reboot_mode.
    
    Change-Id: I8d25aec75331a57fab85c12b8527a329e2a4f19d
    Signed-off-by: Elliot Berman <eberman@codeaurora.org>

--- kernel/reboot.c
+++ kernel/reboot.c
@@ -32,7 +32,9 @@ EXPORT_SYMBOL(cad_pid);
 #define DEFAULT_REBOOT_MODE
 #endif
 enum reboot_mode reboot_mode DEFAULT_REBOOT_MODE;
+EXPORT_SYMBOL(reboot_mode);
 enum reboot_mode panic_reboot_mode = REBOOT_UNDEFINED;
+EXPORT_SYMBOLE(panic_reboot_mode);
 
 /*
  * This variable is used privately to keep track of whether or not
