efbf92a534ebc4aa805282d1cd861900239784e1
Will Deacon <will@kernel.org>
2019-12-05 19:48:48 +0000

    KVM: arm64: Add debug UART hacks at EL2
    Provide a standalone mechanism for debug UART access at EL2. It's a
    debug hack, so please try not to get too offended by it.
    
    [ qperret: re-map UART at EL2 in protected mode ]
    
    Change-Id: I851a875201f224afcbf4e6491189af7dbcce2b08
    Signed-off-by: Will Deacon <will@kernel.org>
    Signed-off-by: Quentin Perret <qperret@google.com>

--- arch/arm64/kernel/image-vars.h
+++ arch/arm64/kernel/image-vars.h
@@ -112,6 +112,10 @@ KVM_NVHE_ALIAS(__hyp_event_ids_end);
 /* pKVM static key */
 KVM_NVHE_ALIAS(kvm_protected_mode_initialized);
 
+#ifdef CONFIG_KVM_ARM_HYP_DEBUG_UART
+KVM_NVHE_ALIAS(kvm_hyp_debug_uart_set_basep);
+#endif
+
 #endif /* CONFIG_KVM */
 
 #ifdef CONFIG_EFI_ZBOOT
