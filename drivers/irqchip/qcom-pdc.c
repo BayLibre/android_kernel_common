c4ccf368b6659d66a406fc04734060939ad6eb45
Greg Kroah-Hartman <gregkh@google.com>
2019-12-09 12:44:11 +0100

    ANDROID: fix up merge error in drivers/irqchip/qcom-pdc.c
    I messed up the merge in drivers/irqchip/qcom-pdc.c and left two copies
    of qcom_pdc_gic_get_irqchip_state() in the file :(
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I50528b34ec05f03c04b16f4f649bb58971914590

--- drivers/irqchip/qcom-pdc.c
+++ drivers/irqchip/qcom-pdc.c
@@ -108,15 +108,6 @@ static void qcom_pdc_gic_disable(struct irq_data *d)
 	irq_chip_disable_parent(d);
 }
 
-static int qcom_pdc_gic_get_irqchip_state(struct irq_data *d,
-		enum irqchip_irq_state which, bool *state)
-{
-	if (d->hwirq == GPIO_NO_WAKE_IRQ)
-		return 0;
-
-	return irq_chip_get_parent_state(d, which, state);
-}
-
 static int qcom_pdc_gic_set_irqchip_state(struct irq_data *d,
 		enum irqchip_irq_state which, bool value)
 {
