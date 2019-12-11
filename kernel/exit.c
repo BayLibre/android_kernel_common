cbb26287bdd4c4cc6f96861f1aa286cf0793ae42
chenqiwu <chenqiwu@xiaomi.com>
2019-12-11 19:04:47 +0800

    This is a test for kernel.
    Signed-off-by: chenqiwu <chenqiwu@xiaomi.com>

--- kernel/exit.c
+++ kernel/exit.c
@@ -4,7 +4,7 @@
  *  Copyright (C) 1991, 1992  Linus Torvalds
  */
 
-#include <linux/mm.h>
+//#include <linux/mm.h>
 #include <linux/slab.h>
 #include <linux/sched/autogroup.h>
 #include <linux/sched/mm.h>
