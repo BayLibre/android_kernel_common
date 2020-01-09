eaca9e9c4946767a934c58634ba92ae68b031243
Eric Biggers <ebiggers@google.com>
2020-01-08 16:00:46 -0800

    scsi: ufs-qcom: add inline encryption (ICE) support
    Add support for inline encryption using Qualcomm Inline Cryptographic
    Engine (ICE).
    
    Beyond the standards-compliant part which is already handled by
    ufshcd-crypto.c, ICE requires vendor-specific initialization sequences
    using vendor-specific registers, and it requires that keys are
    programmed and evicted using vendor-specific SMC calls.  But these
    details are hidden in the driver; users of the block layer (e.g.
    filesystems via fscrypt) can just use the generic blk-crypto interface.
    
    I've tested this on Dragonboard 845c, which is a publicly available
    development board which uses the Snapdragon 845 SoC, which is the same
    SoC used in the Pixel 3 and Pixel 3 XL phones.  This testing included
    using it with ext4 encryption and verifying that the expected ciphertext
    was produced, given the key and IV that should have been used.
    
    This is based very loosely on the vendor-provided driver, but I've
    greatly simplified it.  Also, for now I've only included support for
    major version 3 of ICE, since that's all I have the hardware to test
    with the mainline Linux kernel.
    
    Change-Id: I5a036531bb5e83c14746f06d80e5b436dcabb827
    Signed-off-by: Eric Biggers <ebiggers@google.com>

--- drivers/scsi/ufs/ufs-qcom.c
+++ drivers/scsi/ufs/ufs-qcom.c
@@ -364,7 +364,7 @@ static int ufs_qcom_hce_enable_notify(struct ufs_hba *hba,
 		/* check if UFS PHY moved from DISABLED to HIBERN8 */
 		err = ufs_qcom_check_hibern8(hba);
 		ufs_qcom_enable_hw_clk_gating(hba);
-
+		ufs_qcom_ice_enable(host);
 		break;
 	default:
 		dev_err(hba->dev, "%s: invalid status %d\n", __func__, status);
@@ -614,6 +614,10 @@ static int ufs_qcom_resume(struct ufs_hba *hba, enum ufs_pm_op pm_op)
 		if (err)
 			return err;
 	}
+
+	err = ufs_qcom_ice_resume(host);
+	if (err)
+		return err;
 
 	hba->is_sys_suspended = false;
 	return 0;
@@ -1030,13 +1034,6 @@ static void ufs_qcom_advertise_quirks(struct ufs_hba *hba)
 				| UFSHCD_QUIRK_DME_PEER_ACCESS_AUTO_MODE
 				| UFSHCD_QUIRK_BROKEN_PA_RXHSUNTERMCAP);
 	}
-
-	/*
-	 * Inline crypto is currently broken with ufs-qcom at least because the
-	 * device tree doesn't include the crypto registers.  There are likely
-	 * to be other issues that will need to be addressed too.
-	 */
-	hba->quirks |= UFSHCD_QUIRK_BROKEN_CRYPTO;
 }
 
 static void ufs_qcom_set_caps(struct ufs_hba *hba)
@@ -1264,6 +1261,13 @@ static int ufs_qcom_init(struct ufs_hba *hba)
 
 	ufs_qcom_set_caps(hba);
 	ufs_qcom_advertise_quirks(hba);
+
+	err = ufs_qcom_ice_init(host);
+	if (err) {
+		if (err != -ENODEV)
+			goto out_variant_clear;
+		hba->quirks |= UFSHCD_QUIRK_BROKEN_CRYPTO;
+	}
 
 	ufs_qcom_set_bus_vote(hba, true);
 	ufs_qcom_setup_clocks(hba, true, POST_CHANGE);
@@ -1689,6 +1693,7 @@ static const struct ufs_hba_variant_ops ufs_hba_qcom_vops = {
 	.resume			= ufs_qcom_resume,
 	.dbg_register_dump	= ufs_qcom_dump_dbg_regs,
 	.device_reset		= ufs_qcom_device_reset,
+	.program_key		= ufs_qcom_ice_program_key,
 };
 
 /**
