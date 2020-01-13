d2739f785a0a381c46b2810f32c4776630ff38e3
Eric Biggers <ebiggers@google.com>
2020-01-13 09:41:43 -0800

    ANDROID: dm: add support for passing through inline crypto support
    Update the device-mapper core to support exposing the inline crypto
    support of the underlying device(s) through the device-mapper device.
    
    This works by creating a "passthrough keyslot manager" for the dm
    device, which declares support for the set of (crypto_mode,
    data_unit_size) combos which all the underlying devices support.  When a
    supported combo is used, the bio cloning code handles cloning the crypto
    context to the bios for all the underlying devices.  When an unsupported
    combo is used, the blk-crypto fallback is used as usual.
    
    Crypto support on each underlying device is ignored unless the
    corresponding dm target opts into exposing it.  This is needed because
    for inline crypto to semantically operate on the original bio, the data
    must not be transformed by the dm target.  Thus, targets like dm-linear
    can expose crypto support of the underlying device, but targets like
    dm-crypt can't.  (dm-crypt could use inline crypto itself, though.)
    
    When a key is evicted from the dm device, it is evicted from all
    underlying devices.
    
    Bug: 137270441
    Bug: 147814592
    Change-Id: If28b574f2e28268db5eb9f325d4cf8f96cb63e3f
    Signed-off-by: Eric Biggers <ebiggers@google.com>

--- include/linux/keyslot-manager.h
+++ include/linux/keyslot-manager.h
@@ -8,6 +8,8 @@
 
 #include <linux/bio.h>
 
+#ifdef CONFIG_BLK_INLINE_ENCRYPTION
+
 struct keyslot_manager;
 
 /**
@@ -61,5 +63,13 @@ struct keyslot_manager *keyslot_manager_create_passthrough(
 	const struct keyslot_mgmt_ll_ops *ksm_ops,
 	const unsigned int crypto_mode_supported[BLK_ENCRYPTION_MODE_MAX],
 	void *ll_priv_data);
+
+void keyslot_manager_get_supported_modes(struct keyslot_manager *ksm,
+		 unsigned int crypto_mode_supported[BLK_ENCRYPTION_MODE_MAX]);
+
+void keyslot_manager_set_supported_modes(struct keyslot_manager *ksm,
+	 const unsigned int crypto_mode_supported[BLK_ENCRYPTION_MODE_MAX]);
+
+#endif /* CONFIG_BLK_INLINE_ENCRYPTION */
 
 #endif /* __LINUX_KEYSLOT_MANAGER_H */
