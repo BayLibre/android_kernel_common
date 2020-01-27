b0971f0885d61ebe98448d029baa5064054ccd22
Vinayak Menon <vinmenon@codeaurora.org>
2020-01-27 17:05:10 +0530

    ANDROID: mm: shmem: interface to get shmem swap pages count
    For in-kernel users of shmem, add an interface to get
    the swapped pages in an shmem address_space.
    
    Bug: 187798288
    Change-Id: Icb16650dbcb91793af426e24fc2f56022b020e9b
    Signed-off-by: Vinayak Menon <vinmenon@codeaurora.org>

--- include/linux/shmem_fs.h
+++ include/linux/shmem_fs.h
@@ -85,6 +85,7 @@ extern bool shmem_huge_enabled(struct vm_area_struct *vma);
 extern unsigned long shmem_swap_usage(struct vm_area_struct *vma);
 extern unsigned long shmem_partial_swap_usage(struct address_space *mapping,
 						pgoff_t start, pgoff_t end);
+extern unsigned long shmem_swap_pages(struct address_space *mapping);
 
 /* Flag allocation requirements to shmem_getpage */
 enum sgp_type {
