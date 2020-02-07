a24d4167dcb25a9bd5ebb11f82a81c1f514e82b0
Connor O'Brien <connoro@google.com>
2020-02-07 10:01:49 -0800

    UPSTREAM: security: selinux: allow per-file labeling for bpffs
    Add support for genfscon per-file labeling of bpffs files. This allows
    for separate permissions for different pinned bpf objects, which may
    be completely unrelated to each other.
    
    Signed-off-by: Connor O'Brien <connoro@google.com>
    Signed-off-by: Steven Moreland <smoreland@google.com>
    Acked-by: Stephen Smalley <sds@tycho.nsa.gov>
    Signed-off-by: Paul Moore <paul@paul-moore.com>
    (cherry picked from commit 4ca54d3d3022ce27170b50e4bdecc3a42f05dbdc)
    [which is v5.6-rc1-10-g4ca54d3d3022 and thus already included in 5.10]
    Bug: 200440527
    Change-Id: I8234b9047f29981b8140bd81bb2ff070b3b0b843
    (cherry picked from commit d52ac987ad2ae16ff313d7fb6185bc412cb221a4)

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -871,6 +871,11 @@ static int selinux_set_mnt_opts(struct super_block *sb,
 	    !strcmp(sb->s_type->name, "tracefs") ||
 	    !strcmp(sb->s_type->name, "sysfs") ||
 	    !strcmp(sb->s_type->name, "pstore") ||
+<<<<<<< HEAD   (3a843c Merge 4.19.215 into android-4.19-q)
+=======
+	    !strcmp(sb->s_type->name, "binder") ||
+	    !strcmp(sb->s_type->name, "bpf") ||
+>>>>>>> CHANGE (65de97 UPSTREAM: security: selinux: allow per-file labeling for bpf)
 	    !strcmp(sb->s_type->name, "cgroup") ||
 	    !strcmp(sb->s_type->name, "cgroup2"))
 		sbsec->flags |= SE_SBGENFS;
