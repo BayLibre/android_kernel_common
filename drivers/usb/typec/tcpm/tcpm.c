ed2aba383325706fa3df39b357aae9c302464c83
Badhri Jagan Sridharan <badhri@google.com>
2020-02-06 17:29:04 -0800

    ANDROID: usb: typec: tcpm: Add a callback notify pd capable partner
    Low level drivers related to charging have to make adjustments
    such as disabling aicl when a pd capable partner is detected.
    Introduce a callback to notify the low level driver whenever
    the port is connected to a pd capable partner.
    
    Bug: 138685578
    Signed-off-by: Badhri Jagan Sridharan <Badhri@google.com>
    (cherry picked from commit 782c3054cbf77225dff204790614fbdacbd1d363)
    (cherry picked from commit 681ffa49754cc8e7064ffb87ce4ab31b94f2fd65)
    [willmcvicker: resolve merge conflicts]
    Signed-off-by: Will McVicker <willmcvicker@google.com>
    (cherry picked from commit 1970df121bd804deab0d5cb6d94bcaec54f8f12d)
    [willmcvicker: resolved conflicts]
    Signed-off-by: Will McVicker <willmcvicker@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I94c2ab0f76ed482f25a98c4b1a0514be151814b6

--- drivers/usb/typec/tcpm/tcpm.c
+++ drivers/usb/typec/tcpm/tcpm.c
@@ -734,6 +734,14 @@ static void tcpm_set_cc(struct tcpm_port *port, enum typec_cc_status cc)
 	port->tcpc->set_cc(port->tcpc, cc);
 }
 
+static void tcpm_set_pd_capable(struct tcpm_port *port, bool capable)
+{
+	tcpm_log(port, "Setting pd capable %s", capable ? "true" : "false");
+
+	if (port->tcpc->set_pd_capable)
+		port->tcpc->set_pd_capable(port->tcpc, capable);
+}
+
 /*
  * Determine RP value to set based on maximum current supported
  * by a port if configured as source.
@@ -3427,7 +3435,7 @@ static int tcpm_src_attach(struct tcpm_port *port)
 	if (ret < 0)
 		goto out_disable_vconn;
 
-	port->pd_capable = false;
+	tcpm_set_pd_capable(port, false);
 
 	port->partner = NULL;
 
@@ -3483,7 +3491,7 @@ static void tcpm_reset_port(struct tcpm_port *port)
 	tcpm_unregister_altmodes(port);
 	tcpm_typec_disconnect(port);
 	port->attached = false;
-	port->pd_capable = false;
+	tcpm_set_pd_capable(port, false);
 	port->pps_data.supported = false;
 
 	/*
@@ -3554,7 +3562,7 @@ static int tcpm_snk_attach(struct tcpm_port *port)
 	if (ret < 0)
 		return ret;
 
-	port->pd_capable = false;
+	tcpm_set_pd_capable(port, false);
 
 	port->partner = NULL;
 
@@ -3808,7 +3816,8 @@ static void run_state_machine(struct tcpm_port *port)
 			 */
 			/* port->hard_reset_count = 0; */
 			port->caps_count = 0;
-			port->pd_capable = true;
+
+			tcpm_set_pd_capable(port, true);
 			tcpm_set_state_cond(port, SRC_SEND_CAPABILITIES_TIMEOUT,
 					    PD_T_SEND_SOURCE_CAP);
 		}
@@ -4081,7 +4090,7 @@ static void run_state_machine(struct tcpm_port *port)
 		}
 		break;
 	case SNK_NEGOTIATE_CAPABILITIES:
-		port->pd_capable = true;
+		tcpm_set_pd_capable(port, true);
 		port->hard_reset_count = 0;
 		ret = tcpm_pd_send_request(port);
 		if (ret < 0) {
