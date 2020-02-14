9674c11a0925f294ad44f4ef8656792a8c236a2a
John Stultz <john.stultz@linaro.org>
2020-02-14 00:29:35 +0000

    WIP: driver core: Extend returning EPROBE_DEFER for 30 seconds after late_initcall
    Due to commit e01afc3250255 ("PM / Domains: Stop deferring probe
    at the end of initcall"), along with commit 25b4e70dcce9
    ("driver core: allow stopping deferred probe after init") after
    late_initcall, drivers will stop getting EPROBE_DEFER, and
    instead see an error causing the driver to fail to load.
    
    That change causes trouble when trying to use many clk drivers
    as modules, as the clk modules may not load until much later
    after init has started. If a dependent driver loads and gets an
    error instead of EPROBE_DEFER, it won't try to reload later when
    the dependency is met, and will thus fail to load.
    
    Instead of reverting that patch, this patch tries to extend the
    time that EPROBE_DEFER is retruned by 30 seconds, to (hopefully)
    ensure that everything has had a chance to load.
    
    30 seconds was chosen to match the similar timeout used by the
    regulator code here in commit 55576cf18537 ("regulator: Defer
    init completion for a while after late_initcall")
    
    Specifically, on db845c, this change allows us to set
    SDM_GPUCC_845, QCOM_CLK_RPMH and COMMON_CLK_QCOM as modules and
    get a working system, where as without it the display will fail
    to load.
    
    Cc: Rob Herring <robh@kernel.org>
    Cc: "Rafael J. Wysocki" <rjw@rjwysocki.net>
    Cc: Kevin Hilman <khilman@kernel.org>
    Cc: Ulf Hansson <ulf.hansson@linaro.org>
    Cc: Pavel Machek <pavel@ucw.cz>
    Cc: Len Brown <len.brown@intel.com>
    Cc: Todd Kjos <tkjos@google.com>
    Cc: Bjorn Andersson <bjorn.andersson@linaro.org>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: linux-pm@vger.kernel.org
    Fixes: e01afc3250255 ("PM / Domains: Stop deferring probe at the end of initcall")
    Fixes: 25b4e70dcce9 ("driver core: allow stopping deferred probe after init")
    Signed-off-by: John Stultz <john.stultz@linaro.org>
    Change-Id: Ie7560fc7274c5b8581656ea3bd4c46406ff7489d
    Bug: 149414941
    Link: https://lore.kernel.org/lkml/CALAqxLVukbQETMSfjc3OsEE4BGt-uXxw-NTqgLxA=ja4JAVD4w@mail.gmail.com/T/#t
    ---
    v2:
    * Add calls to driver_deferred_probe_trigger() after the two minute timeout,
      as suggested by Bjorn
    * Minor whitespace cleanups
    * Switch to 30 second timeout to match what the regulator code is doing as
      suggested by Rob.

--- drivers/base/dd.c
+++ drivers/base/dd.c
@@ -311,6 +311,15 @@ static void deferred_probe_timeout_work_func(struct work_struct *work)
 }
 static DECLARE_DELAYED_WORK(deferred_probe_timeout_work, deferred_probe_timeout_work_func);
 
+static void deferred_initcall_done_work_func(struct work_struct *work)
+{
+	initcalls_done = true;
+	driver_deferred_probe_trigger();
+	flush_work(&deferred_probe_work);
+}
+static DECLARE_DELAYED_WORK(deferred_initcall_done_work,
+			    deferred_initcall_done_work_func);
+
 /**
  * deferred_probe_initcall() - Enable probing of deferred devices
  *
@@ -327,7 +336,8 @@ static int deferred_probe_initcall(void)
 	driver_deferred_probe_trigger();
 	/* Sort as many dependencies as possible before exiting initcalls */
 	flush_work(&deferred_probe_work);
-	initcalls_done = true;
+	schedule_delayed_work(&deferred_initcall_done_work,
+			      msecs_to_jiffies(30000));
 
 	/*
 	 * Trigger deferred probe again, this time we won't defer anything
