5a2d7a3fd2df383c663980036c617258abbb1d70
David Stevens <stevensd@chromium.org>
2020-03-02 17:10:01 +0900

    FROMLIST: drm/prime: add support for virtio exported objects
    This change exposes dma-buf's get_uuid callback to PRIME drivers.
    
    Signed-off-by: David Stevens <stevensd@chromium.org>
    
    BUG=b:136269340
    TEST=boot ARCVM and launch play store
    
    Signed-off-by: Keiichi Watanabe <keiichiw@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2090951
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Gurchetan Singh <gurchetansingh@chromium.org>
    Tested-by: David Stevens <stevensd@chromium.org>
    Commit-Queue: David Stevens <stevensd@chromium.org>
    Bug: 153580313
    Link: https://lore.kernel.org/lkml/20200311112004.47138-3-stevensd@chromium.org/
    Change-Id: I0599ce4cafee3dc705805c1ad97295bea264cfdf
    Signed-off-by: Lingfeng Yang <lfy@google.com>

--- drivers/gpu/drm/drm_prime.c
+++ drivers/gpu/drm/drm_prime.c
@@ -362,8 +362,41 @@ static void drm_gem_dmabuf_vunmap(struct dma_buf *dma_buf, void *vaddr)
 	dev->driver->gem_prime_vunmap(obj, vaddr);
 }
 
+<<<<<<< HEAD   (3be023 FROMLIST: dma-buf: add support for virtio exported objects)
 static void *drm_gem_dmabuf_kmap_atomic(struct dma_buf *dma_buf,
 					unsigned long page_num)
+=======
+/**
+ * drm_gem_dmabuf_get_uuid - dma_buf get_uuid implementation for GEM
+ * @dma_buf: buffer to query
+ * @uuid: uuid outparam
+ *
+ * Queries the buffer's virtio UUID. This can be used as the
+ * &dma_buf_ops.get_uuid callback. Calls into &drm_driver.gem_prime_get_uuid.
+ *
+ * Returns 0 on success or a negative error code on failure.
+ */
+int drm_gem_dmabuf_get_uuid(struct dma_buf *dma_buf, uuid_t *uuid)
+{
+	struct drm_gem_object *obj = dma_buf->priv;
+	struct drm_device *dev = obj->dev;
+
+	if (!dev->driver->gem_prime_get_uuid)
+		return -ENODEV;
+
+	return dev->driver->gem_prime_get_uuid(obj, uuid);
+}
+EXPORT_SYMBOL(drm_gem_dmabuf_get_uuid);
+
+/**
+ * drm_gem_dmabuf_kmap - map implementation for GEM
+ * @dma_buf: buffer to be mapped
+ * @page_num: page number within the buffer
+ *
+ * Not implemented. This can be used as the &dma_buf_ops.map callback.
+ */
+void *drm_gem_dmabuf_kmap(struct dma_buf *dma_buf, unsigned long page_num)
+>>>>>>> CHANGE (c90793 FROMLIST: drm/prime: add support for virtio exported objects)
 {
 	return NULL;
 }
@@ -410,6 +443,7 @@ static const struct dma_buf_ops drm_gem_prime_dmabuf_ops =  {
 	.mmap = drm_gem_dmabuf_mmap,
 	.vmap = drm_gem_dmabuf_vmap,
 	.vunmap = drm_gem_dmabuf_vunmap,
+	.get_uuid = drm_gem_dmabuf_get_uuid,
 };
 
 /**
