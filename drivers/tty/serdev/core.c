f4dde5005e7aca87276ea1e6aa2b22a1d13d8a3f
Alistair Delva <adelva@google.com>
2020-03-04 12:03:23 -0800

    ANDROID: serdev: restrict claim of platform devices
    Make the fallback path for claiming platform devices trigger only if a
    new module parameter is specified:
    
      serdev_ttyport.pdev_tty_port=ttyS2
    
    Bug: 146517987
    Change-Id: Ibf331ad6e6d8712a405921530f217f7122428b13
    Signed-off-by: Alistair Delva <adelva@google.com>

--- drivers/tty/serdev/core.c
+++ drivers/tty/serdev/core.c
@@ -452,16 +452,22 @@ static int platform_serdev_register_devices(struct serdev_controller *ctrl)
 	return err;
 }
 
+
 /**
- * serdev_controller_add() - Add an serdev controller
+ * serdev_controller_add_platform() - Add an serdev controller
  * @ctrl:	controller to be registered.
+ * @platform:	whether to permit fallthrough to platform device probe
  *
  * Register a controller previously allocated via serdev_controller_alloc() with
- * the serdev core.
+ * the serdev core. Optionally permit probing via a platform device fallback.
  */
-int serdev_controller_add(struct serdev_controller *ctrl)
+int serdev_controller_add_platform(struct serdev_controller *ctrl, bool platform)
 {
+<<<<<<< HEAD   (167db7 ANDROID: scsi: ufs: allow ufs variants to override sg entry )
 	int ret_of, ret_platform, ret;
+=======
+	int ret, ret_of, ret_acpi, ret_platform = -ENODEV;
+>>>>>>> CHANGE (f6af9b ANDROID: serdev: restrict claim of platform devices)
 
 	/* Can't register until after driver model init */
 	if (WARN_ON(!is_registered))
@@ -471,10 +477,21 @@ int serdev_controller_add(struct serdev_controller *ctrl)
 	if (ret)
 		return ret;
 
+<<<<<<< HEAD   (167db7 ANDROID: scsi: ufs: allow ufs variants to override sg entry )
 	ret_platform = platform_serdev_register_devices(ctrl);
 	ret_of = of_serdev_register_devices(ctrl);
 	if (ret_of && ret_platform) {
 		dev_dbg(&ctrl->dev, "no devices registered: of:%d "
+=======
+	pm_runtime_enable(&ctrl->dev);
+
+	ret_of = of_serdev_register_devices(ctrl);
+	ret_acpi = acpi_serdev_register_devices(ctrl);
+	if (platform)
+		ret_platform = platform_serdev_register_devices(ctrl);
+	if (ret_of && ret_acpi && ret_platform) {
+		dev_dbg(&ctrl->dev, "no devices registered: of:%d acpi:%d "
+>>>>>>> CHANGE (f6af9b ANDROID: serdev: restrict claim of platform devices)
 				    "platform:%d\n",
 				    ret_of, ret_platform);
 		ret = -ENODEV;
@@ -489,7 +506,7 @@ out_dev_del:
 	device_del(&ctrl->dev);
 	return ret;
 };
-EXPORT_SYMBOL_GPL(serdev_controller_add);
+EXPORT_SYMBOL_GPL(serdev_controller_add_platform);
 
 /* Remove a device associated with a controller */
 static int serdev_remove_device(struct device *dev, void *data)
