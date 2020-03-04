c4dacec90deb7671d34a251b3020184b7354559e
John Stultz <john.stultz@linaro.org>
2020-03-04 23:10:34 +0000

    FROMLIST: soc: qcom: rpmpd: Allow RPMPD driver to be loaded as a module
    Allow the rpmpd driver to be loaded as a module.
    
    Cc: Todd Kjos <tkjos@google.com>
    Cc: Saravana Kannan <saravanak@google.com>
    Cc: Andy Gross <agross@kernel.org>
    Cc: Bjorn Andersson <bjorn.andersson@linaro.org>
    Cc: linux-arm-msm@vger.kernel.org
    Signed-off-by: John Stultz <john.stultz@linaro.org>
    Link: https://lore.kernel.org/lkml/20200305054244.128950-1-john.stultz@linaro.org/T/#u
    Change-Id: Ic9140609813f4c03f8f676de6c76a749d16e8479

--- drivers/soc/qcom/rpmpd.c
+++ drivers/soc/qcom/rpmpd.c
@@ -5,6 +5,7 @@
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/mutex.h>
+#include <linux/module.h>
 #include <linux/pm_domain.h>
 #include <linux/of.h>
 #include <linux/of_device.h>
@@ -226,6 +227,7 @@ static const struct of_device_id rpmpd_match_table[] = {
 	{ .compatible = "qcom,qcs404-rpmpd", .data = &qcs404_desc },
 	{ }
 };
+MODULE_DEVICE_TABLE(of, rpmpd_match_table);
 
 static int rpmpd_send_enable(struct rpmpd *pd, bool enable)
 {
@@ -421,4 +423,5 @@ static int __init rpmpd_init(void)
 {
 	return platform_driver_register(&rpmpd_driver);
 }
-core_initcall(rpmpd_init);
+module_init(rpmpd_init);
+MODULE_LICENSE("GPL");
