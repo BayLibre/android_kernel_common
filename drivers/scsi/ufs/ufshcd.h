ef3cc1948a6fd586c40f7fb31e72429926afe379
Can Guo <cang@codeaurora.org>
2020-04-01 00:07:43 -0700

    BACKPORT: FROMGIT: scsi: ufshcd: Let vendor override devfreq parameters
    Vendor drivers may have a need to update the polling interval and
    thresholds.  Provide a vops for vendor drivers to use.
    
    Bug: 152781094
    (cherry picked from commit 2c75f9a5be53501a6e733cd72c1c46281eaa49a0
    https://git.kernel.org/pub/scm/linux/kernel/git/mkp/scsi.git/log/?h=5.7/scsi-queue)
    Link: https://lore.kernel.org/r/acd79e00396cff855256adad47f615ccdbde85ac.1585160616.git.asutoshd@codeaurora.org
    Acked-by: Avri Altman <Avri.Altman@wdc.com>
    Signed-off-by: Asutosh Das <asutoshd@codeaurora.org>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    
    Change-Id: Iaa41b1861a95d686fa0db6d5b7eb045ed14b531f
    Signed-off-by: Can Guo <cang@codeaurora.org>

--- drivers/scsi/ufs/ufshcd.h
+++ drivers/scsi/ufs/ufshcd.h
@@ -55,6 +55,7 @@
 #include <linux/clk.h>
 #include <linux/completion.h>
 #include <linux/regulator/consumer.h>
+#include <linux/devfreq.h>
 #include "unipro.h"
 
 #include <asm/irq.h>
@@ -339,6 +340,9 @@ struct ufs_hba_variant_ops {
 	void	(*device_reset)(struct ufs_hba *hba);
 	int	(*program_key)(struct ufs_hba *hba,
 			       const union ufs_crypto_cfg_entry *cfg, int slot);
+	void	(*config_scaling_param)(struct ufs_hba *hba,
+					struct devfreq_dev_profile *profile,
+					void *data);
 };
 
 struct keyslot_mgmt_ll_ops;
@@ -1159,6 +1163,14 @@ static inline void ufshcd_vops_device_reset(struct ufs_hba *hba)
 		hba->vops->device_reset(hba);
 		ufshcd_update_reg_hist(&hba->ufs_stats.dev_reset, 0);
 	}
+}
+
+static inline void ufshcd_vops_config_scaling_param(struct ufs_hba *hba,
+						    struct devfreq_dev_profile
+						    *profile, void *data)
+{
+	if (hba->vops && hba->vops->config_scaling_param)
+		hba->vops->config_scaling_param(hba, profile, data);
 }
 
 extern struct ufs_pm_lvl_states ufs_pm_lvl_states[];
