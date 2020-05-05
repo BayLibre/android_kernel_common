834c3a0bae703734f0cceb98167a911a4316ba7f
John Stultz <john.stultz@linaro.org>
2020-05-05 18:40:52 +0000

    ANDROID: dma-heap: Provide accessors so that in-kernel drivers can allocate dmabufs from specific heaps
    This allows drivers who don't want to create their own
    DMA-BUF exporter to be able to allocate DMA-BUFs directly
    from existing DMA-BUF Heaps.
    
    There is some concern that the premise of DMA-BUF heaps is
    that userland knows better about what type of heap memory
    is needed for a pipeline, so it would likely be best for
    drivers to import and fill DMA-BUFs allocated by userland
    instead of allocating one themselves, but this is still
    up for debate.
    
    Unfortunately we don't have any public users of this, so
    we cannot push it upstream. However, vendors have asked
    for this, so I'm submitting to Andorid Common it in the
    hopes that we can eventually find what vendors are doing.
    
    Signed-off-by: John Stultz <john.stultz@linaro.org>
    Change-Id: I7b5ef690cefd2bfbbc2e01220ada49e44ef4fa04
    Bug: 154341375

--- drivers/dma-buf/dma-heap.c
+++ drivers/dma-buf/dma-heap.c
@@ -50,7 +50,7 @@ static dev_t dma_heap_devt;
 static struct class *dma_heap_class;
 static DEFINE_XARRAY_ALLOC(dma_heap_minors);
 
-static struct dma_heap *dma_heap_find(const char *name)
+struct dma_heap *dma_heap_find(const char *name)
 {
 	struct dma_heap *h;
 
@@ -65,10 +65,12 @@ static struct dma_heap *dma_heap_find(const char *name)
 	mutex_unlock(&heap_list_lock);
 	return NULL;
 }
+EXPORT_SYMBOL_GPL(dma_heap_find);
 
-static int dma_heap_buffer_alloc(struct dma_heap *heap, size_t len,
-				 unsigned int fd_flags,
-				 unsigned int heap_flags)
+
+int dma_heap_buffer_alloc(struct dma_heap *heap, size_t len,
+			  unsigned int fd_flags,
+			  unsigned int heap_flags)
 {
 	if (fd_flags & ~DMA_HEAP_VALID_FD_FLAGS)
 		return -EINVAL;
@@ -85,6 +87,7 @@ static int dma_heap_buffer_alloc(struct dma_heap *heap, size_t len,
 
 	return heap->ops->allocate(heap, len, fd_flags, heap_flags);
 }
+EXPORT_SYMBOL_GPL(dma_heap_buffer_alloc);
 
 static int dma_heap_open(struct inode *inode, struct file *file)
 {
