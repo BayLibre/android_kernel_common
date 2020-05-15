0defa19071afb8fbc2f27195c432140bdae7a5c6
Frank Lee <frank@allwinnertech.com>
2020-05-15 16:54:44 +0800

    FROMLIST: clk: sunxi-ng: add support for the Allwinner A100 CCU
    Add support for a100 in the sunxi-ng CCU framework.
    
    Change-Id: I2eb73fd43a25296a636874f2feb767aa89ad0219
    Link: https://lkml.org/lkml/2020/5/21/1230
    Signed-off-by: Frank Lee <frank@allwinnertech.com>

---
+++ drivers/clk/sunxi-ng/ccu-sun50i-a100.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2020 Frank Lee <frank@allwinner.com>
+ */
+
+#ifndef _CCU_SUN50I_A100_H_
+#define _CCU_SUN50I_A100_H_
+
+#include <dt-bindings/clock/sun50i-a100-ccu.h>
+#include <dt-bindings/reset/sun50i-a100-ccu.h>
+
+#define CLK_NUMBER		(CLK_CSI_ISP + 1)
+
+#endif /* _CCU_SUN50I_A100_H_ */
