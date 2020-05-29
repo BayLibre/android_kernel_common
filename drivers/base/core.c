c65b3bc758a62305f1bea9d1de6b7b7f00fa12dd
Saravana Kannan <saravanak@google.com>
2020-05-28 17:09:59 -0700

    ANDROID: GKI: driver core: Flush deferred probe device list on fw_devlink_resume()
    The initcall sequence does something like:
    initcall 1 -> register driver 1
    initcall 2 -> register driver 2
    initcall 3 -> register driver 3
    initcall 4 -> register driver 4
    
    Some drivers (say driver 3), seem to assume devices probed by earlier
    drivers (driver 1 and driver 2) would never have their corresponding
    devices defer their probes.
    
    Instead of fixing all those drivers, this change ensures the probe
    ordering doesn't change compared to before fw_devlink_pause/resume() was
    added.
    
    Bug: 157691602
    Signed-off-by: Saravana Kannan <saravanak@google.com>
    Change-Id: If34ac15e986d741a66315bbb7fc9e754a95bd8fe

--- drivers/base/core.c
+++ drivers/base/core.c
@@ -1290,6 +1290,7 @@ void fw_devlink_resume(void)
 	driver_deferred_probe_force_trigger();
 out:
 	mutex_unlock(&defer_fw_devlink_lock);
+	driver_deferred_probe_flush();
 }
 /* Device links support end. */
 
