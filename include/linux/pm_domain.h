d3f326922d190eb07e78f31f8bb4bfb1263aedd3
Lina Iyer <ilina@codeaurora.org>
2020-06-26 17:10:24 -0600

    FROMLIST: PM / Domains: use device's next wakeup to determine domain idle state
    Currently, a PM domain's idle state is determined based on whether the
    QoS requirements are met. This may not save power, if the idle state
    residency requirements are not met.
    
    CPU PM domains use the next timer wakeup for the CPUs in the domain to
    determine the sleep duration of the domain. This is compared with the
    idle state residencies to determine the optimal idle state. For other PM
    domains, determining the sleep length is not that straight forward. But
    if the device's next_event is available, we can use that to determine
    the sleep duration of the PM domain.
    
    Let's update the domain governor logic to check for idle state residency
    based on the next wakeup of devices as well as QoS constraints.
    
    Bug: 170654157
    Link: https://lore.kernel.org/linux-pm/20201015193807.17423-3-ilina@codeaurora.org/T/#u
    Signed-off-by: Lina Iyer <ilina@codeaurora.org>
    Change-Id: I937f299eb4f279c325f06d6a35b29af3f0ad9f1a

--- include/linux/pm_domain.h
+++ include/linux/pm_domain.h
@@ -119,6 +119,7 @@ struct generic_pm_domain {
 				     unsigned int state);
 	struct gpd_dev_ops dev_ops;
 	s64 max_off_time_ns;	/* Maximum allowed "suspended" time. */
+	ktime_t next_wakeup;
 	bool max_off_time_changed;
 	bool cached_power_down_ok;
 	bool cached_power_down_state_idx;
