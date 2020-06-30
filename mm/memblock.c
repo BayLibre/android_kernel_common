9fd42e52dcf3502e092a1794d0f3d3d68a8aaef1
Jaewon Kim <jaewon31.kim@samsung.com>
2020-06-30 10:32:13 +0900

    FROMLIST: memblock: track kernel size on memsize
    Some memory regions are already being tracked by previous patches. But
    there are many memory allocations from memblock and frees to memblock
    during the boot time.
    
    This patch tracks the memblock size used for the common kernel. To to
    this, tracking memblock size is disabled for some memory handling logics
    like early param, device tree, and default cma size.
    
    For precise kernel size, this patch counts not actually freed size to
    buddy at boot time, and does not count freed size from ramdisk and init
    section.
    
    Additionally this patch does one important thing. This patch blocks
    memblock_add_range of memblock_remove_range not to update memsize if
    free pages were already released to the buddy allocator.
    
    This is an example. The kernel size is newly added by this patch.
    
     .kernel    :  135137 KB
     .unusable  :  788073 KB
     .reusable  :  294912 KB
    
    Bug: 340432773
    Signed-off-by: Jaewon Kim <jaewon31.kim@samsung.com>
    Link: https://lore.kernel.org/linux-mm/20240521023957.2587005-8-jaewon31.kim@samsung.com/
    Change-Id: Ice0c2aa568385490a3468f7567b4b8c083e043ec

--- mm/memblock.c
+++ mm/memblock.c
@@ -137,6 +137,11 @@ struct memblock_type physmem = {
 };
 #endif
 
+#ifdef CONFIG_MEMBLOCK_MEMSIZE
+static long memsize_kinit;
+static bool memblock_memsize_tracking __initdata_memblock = true;
+#endif
+
 /*
  * keep a pointer to &memblock.memory in the text section to use it in
  * __next_mem_range() and its helpers.
@@ -595,6 +600,7 @@ static int __init_memblock memblock_add_range(struct memblock_type *type,
 	phys_addr_t end = base + memblock_cap_size(base, &size);
 	int idx, nr_new, start_rgn = -1, end_rgn;
 	struct memblock_region *rgn;
+	phys_addr_t new_size = 0;
 
 	if (!size)
 		return 0;
@@ -607,7 +613,8 @@ static int __init_memblock memblock_add_range(struct memblock_type *type,
 		type->regions[0].flags = flags;
 		memblock_set_region_node(&type->regions[0], nid);
 		type->total_size = size;
-		return 0;
+		new_size = size;
+		goto done;
 	}
 
 	/*
@@ -654,6 +661,7 @@ repeat:
 				memblock_insert_region(type, idx++, base,
 						       rbase - base, nid,
 						       flags);
+				new_size += rbase - base;
 			}
 		}
 		/* area below @rend is dealt with, forget about it */
@@ -669,6 +677,7 @@ repeat:
 			end_rgn = idx + 1;
 			memblock_insert_region(type, idx, base, end - base,
 					       nid, flags);
+			new_size += end - base;
 		}
 	}
 
@@ -687,8 +696,18 @@ repeat:
 		goto repeat;
 	} else {
 		memblock_merge_regions(type, start_rgn, end_rgn);
-		return 0;
 	}
+done:
+#ifdef CONFIG_MEMBLOCK_MEMSIZE
+	if (memblock_memsize_tracking) {
+		if (new_size && type == &memblock.reserved) {
+			memblock_dbg("%s: kernel %lu %+ld\n", __func__,
+				     memsize_kinit, (unsigned long)new_size);
+			memsize_kinit += size;
+		}
+	}
+#endif
+	return 0;
 }
 
 /**
@@ -824,6 +843,15 @@ static int __init_memblock memblock_remove_range(struct memblock_type *type,
 
 	for (i = end_rgn - 1; i >= start_rgn; i--)
 		memblock_remove_region(type, i);
+#ifdef CONFIG_MEMBLOCK_MEMSIZE
+	if (memblock_memsize_tracking) {
+		if (type == &memblock.reserved) {
+			memblock_dbg("%s: kernel %lu %+ld\n", __func__,
+				     memsize_kinit, (unsigned long)size);
+			memsize_kinit -= size;
+		}
+	}
+#endif
 	return 0;
 }
 
@@ -1991,6 +2019,21 @@ struct memsize_rgn_struct {
 static struct memsize_rgn_struct memsize_rgn[CONFIG_MAX_MEMBLOCK_MEMSIZE] __initdata_memblock;
 static int memsize_rgn_count __initdata_memblock;
 
+void __init memblock_memsize_enable_tracking(void)
+{
+	memblock_memsize_tracking = true;
+}
+
+void __init memblock_memsize_disable_tracking(void)
+{
+	memblock_memsize_tracking = false;
+}
+
+void memblock_memsize_mod_kernel_size(long size)
+{
+	memsize_kinit += size;
+}
+
 static void __init_memblock memsize_get_valid_name(char *valid_name, const char *name)
 {
 	char *head, *tail, *found;
@@ -2301,6 +2344,19 @@ static unsigned long __init __free_memory_core(phys_addr_t start,
 	unsigned long end_pfn = min_t(unsigned long,
 				      PFN_DOWN(end), max_low_pfn);
 
+#ifdef CONFIG_MEMBLOCK_MEMSIZE
+	unsigned long start_align_up = PFN_ALIGN(start);
+	unsigned long end_align_down = PFN_PHYS(end_pfn);
+
+	if (start_pfn >= end_pfn) {
+		memblock_memsize_mod_kernel_size(end - start);
+	} else {
+		if (start_align_up > start)
+			memblock_memsize_mod_kernel_size(start_align_up - start);
+		if (end_pfn != max_low_pfn && end_align_down < end)
+			memblock_memsize_mod_kernel_size(end - end_align_down);
+	}
+#endif
 	if (start_pfn >= end_pfn)
 		return 0;
 
@@ -2400,6 +2456,8 @@ void __init memblock_free_all(void)
 
 	pages = free_low_memory_core_early();
 	totalram_pages_add(pages);
+
+	memblock_memsize_disable_tracking();
 }
 
 #if defined(CONFIG_DEBUG_FS) && defined(CONFIG_ARCH_KEEP_MEMBLOCK)
@@ -2506,7 +2564,6 @@ static inline void memblock_memsize_check_size(struct memsize_rgn_struct *rgn)
 	if (rgn->reusable || rgn->nomap)
 		return;
 
-	/* check the first page of each 1 MB */
 	phy = rgn->base;
 	end = rgn->base + rgn->size;
 	while (phy < end) {
@@ -2525,6 +2582,8 @@ static inline void memblock_memsize_check_size(struct memsize_rgn_struct *rgn)
 
 		if (has_freed && (phy + SZ_64K >= end))
 			memblock_memsize_free(freed, end - freed);
+
+		/* check the first page only */
 		phy += SZ_64K;
 	}
 }
@@ -2560,6 +2619,8 @@ static int memblock_memsize_show(struct seq_file *m, void *private)
 	}
 
 	seq_puts(m, "\n");
+	seq_printf(m, " .kernel    : %7lu KB\n",
+		   DIV_ROUND_UP(memsize_kinit, SZ_1K));
 	seq_printf(m, " .unusable  : %7lu KB\n",
 		   DIV_ROUND_UP(reserved, SZ_1K));
 	seq_printf(m, " .reusable  : %7lu KB\n",
