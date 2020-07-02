c0cac3479c0321a0771ea64211cc9c4ed837db39
Greg Kroah-Hartman <gregkh@google.com>
2020-07-02 12:51:03 +0200

    ANDROID: GKI: set vfs-only exports into their own namespace
    We have namespaces, so use them for all vfs-exported namespaces so that
    filesystems can use them, but not anything else.
    
    Bug: 157965270
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: Iaf6140baf3a18a516ab2d5c3966235c42f3f70de

--- fs/dcache.c
+++ fs/dcache.c
@@ -1976,7 +1976,7 @@ void d_instantiate(struct dentry *entry, struct inode * inode)
 		spin_unlock(&inode->i_lock);
 	}
 }
-EXPORT_SYMBOL(d_instantiate);
+EXPORT_SYMBOL_NS(d_instantiate, ANDROID_GKI_VFS_EXPORT_ONLY);
 
 /*
  * This should be equivalent to d_instantiate() + unlock_new_inode(),
@@ -2110,7 +2110,7 @@ struct dentry *d_obtain_alias(struct inode *inode)
 {
 	return __d_obtain_alias(inode, true);
 }
-EXPORT_SYMBOL(d_obtain_alias);
+EXPORT_SYMBOL_NS(d_obtain_alias, ANDROID_GKI_VFS_EXPORT_ONLY);
 
 /**
  * d_obtain_root - find or allocate a dentry for a given inode
@@ -2184,7 +2184,7 @@ struct dentry *d_add_ci(struct dentry *dentry, struct inode *inode,
 	}
 	return found;
 }
-EXPORT_SYMBOL(d_add_ci);
+EXPORT_SYMBOL_NS(d_add_ci, ANDROID_GKI_VFS_EXPORT_ONLY);
 
 
 static inline bool d_same_name(const struct dentry *dentry,
@@ -3065,7 +3065,7 @@ out:
 	__d_add(dentry, inode);
 	return NULL;
 }
-EXPORT_SYMBOL(d_splice_alias);
+EXPORT_SYMBOL_NS(d_splice_alias, ANDROID_GKI_VFS_EXPORT_ONLY);
 
 /*
  * Test whether new_dentry is a subdirectory of old_dentry.
