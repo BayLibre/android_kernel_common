266c5138bac2b2e03169296c69583f41669ceff5
JianMin Liu <jian-min.liu@mediatek.com>
2020-07-21 13:36:22 +0800

    ANDROID: rwsem: Add vendor hook to the rw-semaphore
    Add the hook to apply vendor's performance tune for owner
    of rwsem.
    
    Add the hook for the waiter list of rwsem to allow
    vendor perform waiting queue enhancement
    
    ANDROID_VENDOR_DATA added to rw_semaphore
    
    Bug: 319399115
    
    Signed-off-by: JianMin Liu <jian-min.liu@mediatek.com>
    Signed-off-by: Guanwun Chen <guan-wun.chen@mediatek.com>
    Change-Id: I007a5e26f3db2adaeaf4e5ccea414ce7abfa83b8

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -44,6 +44,7 @@
 #include <trace/hooks/ftrace_dump.h>
 #include <trace/hooks/dtask.h>
 #include <trace/hooks/power.h>
+#include <trace/hooks/rwsem.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -137,3 +138,7 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ep_create_wakeup_source);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_timerfd_create);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_try_to_freeze_todo);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_try_to_freeze_todo_unfrozen);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rwsem_init);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rwsem_wake);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rwsem_write_finished);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_alter_rwsem_list_add);
