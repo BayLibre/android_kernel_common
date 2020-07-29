17a3129469be836e6d3ebdd8c5ae4e62177e03d7
Stephen Dickey <dickey@codeaurora.org>
2020-07-28 17:06:35 -0700

    ANDROID: sched cpu and cpumask: create paused cpu mask
    Create a cpu mask struct to track paused cpu status.
    
    Paused cpus are essentially cpus removed from running
    tasks or interrupts (unless they are pinned). Unpinned
    timers and hrtimers too are moved away. This allows the
    paused cpus to enter deep idle states thereby saving a
    lot of power. The exit from paused state is lightweight
    where a scheduling ipi is sent to the paused cpu and it
    come back up again.
    
    This mask will be used later in various kernel paths
    to identify and react to paused cpus.
    
    Bug: 161210528
    Change-Id: I6e53d0c635495fece422846556faf2a5d2923485
    Signed-off-by: Stephen Dickey <dickey@codeaurora.org>
    Signed-off-by: Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -2449,6 +2449,9 @@ EXPORT_SYMBOL(__cpu_present_mask);
 struct cpumask __cpu_active_mask __read_mostly;
 EXPORT_SYMBOL(__cpu_active_mask);
 
+struct cpumask __cpu_paused_mask __read_mostly;
+EXPORT_SYMBOL(__cpu_paused_mask);
+
 atomic_t __num_online_cpus __read_mostly;
 EXPORT_SYMBOL(__num_online_cpus);
 
