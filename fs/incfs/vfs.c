fb41a05a2b73d83f564f079b79d1eaf3a6dc6974
Paul Lawrence <paullawrence@google.com>
2020-08-11 13:19:49 -0700

    ANDROID: Incremental fs: Add FS_IOC_GETFLAGS
    Bug: 160634504
    Test: incfs_test passes
    Signed-off-by: Paul Lawrence <paullawrence@google.com>
    Change-Id: Id79add0db0d66f604ca0f222fe5faec91450ade5

--- fs/incfs/vfs.c
+++ fs/incfs/vfs.c
@@ -764,6 +764,8 @@ static long dispatch_ioctl(struct file *f, unsigned int req, unsigned long arg)
 		return ioctl_get_block_count(f, (void __user *)arg);
 	case FS_IOC_ENABLE_VERITY:
 		return fsverity_ioctl_enable(f, (const void __user *)arg);
+	case FS_IOC_GETFLAGS:
+		return incfs_verity_get_flags(f, (void __user *) arg);
 	default:
 		return -EINVAL;
 	}
