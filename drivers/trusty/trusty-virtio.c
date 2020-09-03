71729b388eb2fd743793b844b24e051a2333d6e5
Tri Vo <trong@google.com>
2020-09-03 12:35:08 -0700

    ANDROID: trusty: Declare trusty-virtio module dependency on trusty-ipc
    Bug: 140053385
    Change-Id: I9a0244de23019c393b34eb62de8de970449a8d6b
    Signed-off-by: Tri Vo <trong@google.com>

--- drivers/trusty/trusty-virtio.c
+++ drivers/trusty/trusty-virtio.c
@@ -843,3 +843,10 @@ module_platform_driver(trusty_virtio_driver);
 
 MODULE_LICENSE("GPL v2");
 MODULE_DESCRIPTION("Trusty virtio driver");
+/*
+ * TODO(b/140053385): trusty-virtio and trusty-ipc should be independent.
+ * However, trusty-virtio is not completely generic and is aware of trusty-ipc.
+ * See header includes. Particularly, trusty-virtio.ko can't be loaded before
+ * trusty-ipc.ko.
+ */
+MODULE_SOFTDEP("pre: trusty-ipc");
