5317749d127a402131f8358d715d4e608e815d28
Badhri Jagan Sridharan <badhri@google.com>
2020-09-11 12:40:55 -0700

    ANDROID: usb: typec: tcpci: allow enabing autodischarge diconnect in runtime
    There are chip level dependencies for enabling autodischarge mechanism.
    To allow hardware validation, allow enabling autodischarge mechanism
    in runtime.
    
    Bug: 169086146
    Signed-off-by: Badhri Jagan Sridharan <badhri@google.com>
    Change-Id: I764e25c098ff91b18843e2af5a0b5fcc2aab261f
    (cherry picked from commit e08c44acbf23087cbcb3e078f743e9480f11548b)
    (cherry picked from commit 46f2e76735a946860dbcb4ad076a7914bcf5d8f6)
    Signed-off-by: Will McVicker <willmcvicker@google.com>
    (cherry picked from commit 182bdbf85ab781ba15f1094031aa21d2d1cad9d5)
    Signed-off-by: Will McVicker <willmcvicker@google.com>

--- drivers/usb/typec/tcpm/tcpci.c
+++ drivers/usb/typec/tcpm/tcpci.c
@@ -729,6 +729,21 @@ static int tcpci_parse_config(struct tcpci *tcpci)
 	return 0;
 }
 
+void tcpci_auto_discharge_update(struct tcpci *tcpci)
+{
+	if (!tcpci || !tcpci->data)
+		return;
+	if (tcpci->data->auto_discharge_disconnect) {
+		tcpci->tcpc.enable_auto_vbus_discharge = tcpci_enable_auto_vbus_discharge;
+		tcpci->tcpc.set_auto_vbus_discharge_threshold =
+			tcpci_set_auto_vbus_discharge_threshold;
+	} else {
+		tcpci->tcpc.enable_auto_vbus_discharge = NULL;
+		tcpci->tcpc.set_auto_vbus_discharge_threshold = NULL;
+	}
+}
+EXPORT_SYMBOL_GPL(tcpci_auto_discharge_update);
+
 struct tcpci *tcpci_register_port(struct device *dev, struct tcpci_data *data)
 {
 	struct tcpci *tcpci;
@@ -759,12 +774,6 @@ struct tcpci *tcpci_register_port(struct device *dev, struct tcpci_data *data)
 	tcpci->tcpc.frs_sourcing_vbus = tcpci_frs_sourcing_vbus;
 	tcpci->tcpc.set_current_limit = tcpci_set_current_limit;
 	tcpci->tcpc.set_pd_capable = tcpci_set_pd_capable;
-
-	if (tcpci->data->auto_discharge_disconnect) {
-		tcpci->tcpc.enable_auto_vbus_discharge = tcpci_enable_auto_vbus_discharge;
-		tcpci->tcpc.set_auto_vbus_discharge_threshold =
-			tcpci_set_auto_vbus_discharge_threshold;
-	}
 
 	err = tcpci_parse_config(tcpci);
 	if (err < 0)
