0d33d0df6c3a0d7f806306f4c1953fc61fb5a696
Eric Biggers <ebiggers@google.com>
2020-09-21 09:20:20 -0700

    ANDROID: trusty-ipc: make TIPC_IOC_CONNECT reject too-long names
    Reject service names longer than 256 bytes (including terminating null)
    rather than silently truncate them.
    
    Otherwise it may be impossible to increase the maximum supported length
    in the future because userspace could start relying on the truncation.
    
    Change-Id: I681a3cf4ae07afa81ecd7cea315ef6e40f0e42d2
    Signed-off-by: Eric Biggers <ebiggers@google.com>

--- drivers/trusty/trusty-ipc.c
+++ drivers/trusty/trusty-ipc.c
@@ -1072,19 +1072,20 @@ err_vds_lookup:
 
 static int dn_connect_ioctl(struct tipc_dn_chan *dn, char __user *usr_name)
 {
-	int err;
+	int res;
 	char name[MAX_SRV_NAME_LEN];
 
 	/* copy in service name from user space */
-	err = strncpy_from_user(name, usr_name, sizeof(name));
-	if (err < 0)
-		return err;
-	name[sizeof(name)-1] = '\0';
+	res = strncpy_from_user(name, usr_name, sizeof(name));
+	if (res < 0)
+		return res;
+	if (res == sizeof(name))
+		return -ENAMETOOLONG;
 
 	/* send connect request */
-	err = tipc_chan_connect(dn->chan, name);
-	if (err)
-		return err;
+	res = tipc_chan_connect(dn->chan, name);
+	if (res)
+		return res;
 
 	/* and wait for reply */
 	return dn_wait_for_reply(dn, REPLY_TIMEOUT);
