032abd6b8e7974f53bcc19c2532644f881e20c80
Paul Lawrence <paullawrence@google.com>
2020-10-06 14:36:01 -0700

    ANDROID: Incremental fs: Add FS_IOC_GETFLAGS
    Bug: 160634504
    Test: incfs_test passes
    Signed-off-by: Paul Lawrence <paullawrence@google.com>
    Change-Id: Id79add0db0d66f604ca0f222fe5faec91450ade5

--- fs/incfs/verity.h
+++ fs/incfs/verity.h
@@ -6,6 +6,18 @@
 #ifndef _INCFS_VERITY_H
 #define _INCFS_VERITY_H
 
+#ifdef CONFIG_FS_VERITY
+
 int incfs_ioctl_enable_verity(struct file *filp, const void __user *uarg);
+
+#else /* !CONFIG_FS_VERITY */
+
+static inline int incfs_ioctl_enable_verity(struct file *filp,
+					    const void __user *uarg)
+{
+	return -EOPNOTSUPP;
+}
+
+#endif /* !CONFIG_FS_VERITY */
 
 #endif
