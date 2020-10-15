a2ce179ab079e61c6b4415dbe9139ef83f03b482
Rick Yiu <rickyiu@google.com>
2020-10-15 20:41:52 +0800

    ANDROID: sched: Add vendor hooks for find_energy_efficient_cpu
    Add hooks for vendor specific find_energy_efficient_cpu logic.
    
    Bug: 170507310
    Bug: 265387484
    
    Signed-off-by: Rick Yiu <rickyiu@google.com>
    Change-Id: I064b501017e32d4f22f8128bed8bf3a1508ab15b
    (cherry picked from commit 2f108e2ec6e89609cbae32c5d13d6ad9f2e858cb)
    Signed-off-by: Will McVicker <willmcvicker@google.com>
    (cherry picked from commit 147a9b3d9eab750d90678af8a70ce80a69f6d510)
    Signed-off-by: Han Lin <han.lin@mediatek.com>
    (cherry picked from commit cbcd3a6460e3b14b200b268b79e90155229d472c)

--- kernel/sched/vendor_hooks.c
+++ kernel/sched/vendor_hooks.c
@@ -85,3 +85,5 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_set_wake_flags);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_post_init_entity_util_avg);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_effective_cpu_util);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_dup_task_struct);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_find_new_ilb);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_find_energy_efficient_cpu);
