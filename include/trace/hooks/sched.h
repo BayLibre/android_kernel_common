a97210ac26e726bec6da3c06e99d62dd11aedc62
Amir Vajid <avajid@codeaurora.org>
2020-10-20 15:58:14 -0700

    ANDROID: vendor_hooks: Add hook for jiffies updates
    Create a vendor hook for jiffies updates by the
    tick_do_timer_cpu.
    
    Bug: 148928265
    Change-Id: Ia442e20d446b8ce4f2b3f2be76655e72919c76eb
    Signed-off-by: Amir Vajid <avajid@codeaurora.org>

--- include/trace/hooks/sched.h
+++ include/trace/hooks/sched.h
@@ -70,6 +70,10 @@ struct sched_group;
 DECLARE_RESTRICTED_HOOK(android_rvh_find_busiest_group,
 	TP_PROTO(struct sched_group *busiest, struct rq *dst_rq, int *out_balance),
 		TP_ARGS(busiest, dst_rq, out_balance), 1);
+
+DECLARE_HOOK(android_vh_jiffies_update,
+	TP_PROTO(void *unused),
+	TP_ARGS(unused));
 #else
 #define trace_android_rvh_select_task_rq_fair(p, prev_cpu, sd_flag, wake_flags, new_cpu)
 #define trace_android_rvh_select_task_rq_rt(p, prev_cpu, sd_flag, wake_flags, new_cpu)
@@ -85,6 +89,7 @@ DECLARE_RESTRICTED_HOOK(android_rvh_find_busiest_group,
 #define trace_android_rvh_set_user_nice(p, nice)
 #define trace_android_rvh_setscheduler(p)
 #define trace_android_rvh_find_busiest_group(busiest, dst_rq, out_balance)
+#define trace_android_vh_jiffies_update()
 #endif
 #endif /* _TRACE_HOOK_SCHED_H */
 /* This part must be outside protection */
