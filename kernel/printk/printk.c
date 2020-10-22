893d0bf65c9b297fb5154d3a0541118ab5439e61
Prasad Sodagudi <psodagud@codeaurora.org>
2020-10-22 04:26:09 -0700

    ANDROID: printk: add vendor hook for console flush
    Add vendor hook for skipping console flush in cpu hotplug.
    
    Bug: 165340180
    Change-Id: I167e1595bbb50e57371bfabfde638624761d5f8a
    Signed-off-by: Prasad Sodagudi <psodagud@codeaurora.org>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -54,6 +54,8 @@
 #include <trace/events/initcall.h>
 #define CREATE_TRACE_POINTS
 #include <trace/events/printk.h>
+#undef CREATE_TRACE_POINTS
+#include <trace/hooks/printk.h>
 
 #include "console_cmdline.h"
 #include "braille.h"
@@ -2294,6 +2296,12 @@ void resume_console(void)
  */
 static int console_cpu_notify(unsigned int cpu)
 {
+	int flag = 0;
+
+	trace_android_vh_printk_hotplug(&flag);
+	if (!flag)
+		return 0;
+
 	if (!cpuhp_tasks_frozen) {
 		/* If trylock fails, someone else is doing the printing */
 		if (console_trylock())
