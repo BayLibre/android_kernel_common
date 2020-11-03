48b1269cfda4ca123d32bc4e5c640061afc0ed5c
Saravana Kannan <saravanak@google.com>
2020-11-02 23:50:47 -0800

    driver core: Use device's fwnode to check if it is waiting for suppliers
    To check if a device is still waiting for its supplier devices to be
    added, we used to check if the devices is in a global
    waiting_for_suppliers list. Since the global list will be deleted in subsequent patches, this patch
    stops using this check.
    
    Instead, this patch uses a more device specific check. It checks if the
    device's fwnode has any fwnode links that haven't been converted to
    device links yet.
    
    Signed-off-by: Saravana Kannan <saravanak@google.com>
    
     Conflicts:
    	drivers/base/core.c
    
    Change-Id: Ic267ff9d7b37a2bf0947d6560d875a054f322d1e

--- drivers/base/core.c
+++ drivers/base/core.c
@@ -50,6 +50,7 @@ static DEFINE_MUTEX(wfs_lock);
 static LIST_HEAD(deferred_sync);
 static unsigned int defer_sync_state_count = 1;
 static DEFINE_MUTEX(fwnode_link_lock);
+static bool fw_devlink_is_permissive(void);
 
 /**
  * fwnode_link_add - Create a link between two fwnode_handles.
@@ -988,13 +989,12 @@ int device_links_check_suppliers(struct device *dev)
 	 * Device waiting for supplier to become available is not allowed to
 	 * probe.
 	 */
-	mutex_lock(&wfs_lock);
-	if (!list_empty(&dev->links.needs_suppliers) &&
-	    dev->links.need_for_probe) {
-		mutex_unlock(&wfs_lock);
+	mutex_lock(&fwnode_link_lock);
+	if (dev->fwnode && !list_empty(&dev->fwnode->suppliers)) {
+		mutex_unlock(&fwnode_link_lock);
 		return -EPROBE_DEFER;
 	}
-	mutex_unlock(&wfs_lock);
+	mutex_unlock(&fwnode_link_lock);
 
 	device_links_write_lock();
 
@@ -1160,10 +1160,9 @@ static ssize_t waiting_for_supplier_show(struct device *dev,
 	bool val;
 
 	device_lock(dev);
-	mutex_lock(&wfs_lock);
-	val = !list_empty(&dev->links.needs_suppliers)
-	      && dev->links.need_for_probe;
-	mutex_unlock(&wfs_lock);
+	mutex_lock(&fwnode_link_lock);
+	val = !list_empty(&dev->fwnode->suppliers);
+	mutex_unlock(&fwnode_link_lock);
 	device_unlock(dev);
 	return sprintf(buf, "%u\n", val);
 }
@@ -2188,9 +2187,11 @@ static int device_add_attrs(struct device *dev)
 			goto err_remove_dev_groups;
 	}
 
-	error = device_create_file(dev, &dev_attr_waiting_for_supplier);
-	if (error)
-		goto err_remove_dev_online;
+	if (dev->fwnode) {
+		error = device_create_file(dev, &dev_attr_waiting_for_supplier);
+		if (error)
+			goto err_remove_dev_online;
+	}
 
 	return 0;
 
