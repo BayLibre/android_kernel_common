3af77fc5648b32b21c88cbfcbaad88c6dbf9c840
Murali Nalajala <mnalajal@codeaurora.org>
2020-11-10 14:21:37 -0800

    ANDROID: sched_clock: Add vendor hook to print epoch values
    Add vendor hook to print epoch values when system enter and exit
    out of suspend and resume. These epoch values are useful to know
    how long the device is in suspend state. These values can be used
    to synchronize various subsystem timestamps and have an unique
    timestamp to correlate between various subsystems.
    
    Bug: 172945021
    Change-Id: I82a01e348d05a46c9c3921869cc9d2fc0fd28867
    Signed-off-by: Murali Nalajala <mnalajal@codeaurora.org>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -22,6 +22,7 @@
 #include <trace/hooks/debug.h>
 #include <trace/hooks/sysrqcrash.h>
 #include <trace/hooks/printk.h>
+#include <trace/hooks/epoch.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -67,3 +68,5 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_sysrq_crash);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_dump_throttled_rt_tasks);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_printk_hotplug);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_jiffies_update);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_show_suspend_epoch_val);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_show_resume_epoch_val);
