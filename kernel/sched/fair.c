2dab1a84772dbb5b3914c88bdab20437bc602f79
Shaleen Agrawal <shalagra@codeaurora.org>
2020-11-19 15:57:16 -0800

    ANDROID: Sched: Add restricted vendor hooks for scheduler
    Add vendors hooks for to facilitate various scheduler value adds.
    
    Bug: 200103201
    Change-Id: I92b1d121d9f23fed5e1ee0ead4c4ae1fdfef1a84
    Signed-off-by: Shaleen Agrawal <shalagra@codeaurora.org>
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    Signed-off-by: Ashay Jaiswal <ashayj@codeaurora.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4120,7 +4120,10 @@ static inline int task_fits_capacity(struct task_struct *p, long capacity)
 
 static inline void update_misfit_status(struct task_struct *p, struct rq *rq)
 {
-	if (!static_branch_unlikely(&sched_asym_cpucapacity))
+	bool need_update = true;
+
+	trace_android_rvh_update_misfit_status(p, rq, &need_update);
+	if (!static_branch_unlikely(&sched_asym_cpucapacity) || !need_update)
 		return;
 
 	if (!p || p->nr_cpus_allowed == 1) {
@@ -8485,6 +8488,7 @@ static void update_cpu_capacity(struct sched_domain *sd, int cpu)
 	if (!capacity)
 		capacity = 1;
 
+	trace_android_rvh_update_cpu_capacity(cpu, &capacity);
 	cpu_rq(cpu)->cpu_capacity = capacity;
 	trace_sched_cpu_capacity_tp(cpu_rq(cpu));
 
