9e01b5e51613617508c94ac250d4d5c13ad0a475
Hridya Valsaraju <hridya@google.com>
2020-11-26 15:04:16 -0800

    FROMGIT: dma-buf: Use struct dma_buf_map in dma_buf_vmap() interfaces
    This patch updates dma_buf_vmap() and dma-buf's vmap callback to use
    struct dma_buf_map.
    
    The interfaces used to return a buffer address. This address now gets
    stored in an instance of the structure that is given as an additional
    argument. The functions return an errno code on errors.
    
    Users of the functions are updated accordingly. This is only an interface
    change. It is currently expected that dma-buf memory can be accessed with
    system memory load/store operations.
    
    v3:
    	* update fastrpc driver (kernel test robot)
    v2:
    	* always clear map parameter in dma_buf_vmap() (Daniel)
    	* include dma-buf-heaps and i915 selftests (kernel test robot)
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Acked-by: Sumit Semwal <sumit.semwal@linaro.org>
    Acked-by: Christian König <christian.koenig@amd.com>
    Acked-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    
    Bug: 173440990
    (cherry picked from commit 6619ccf1bb1d0ebb071f758111efa83918b216fc
    git: //anongit.freedesktop.org/drm/drm-misc
    tags/drm-misc-next-2020-11-27)
    Change-Id: Ice4b427b9c08783724cbe953edf423e2bb9d327d
    Signed-off-by: Hridya Valsaraju <hridya@google.com>

--- include/linux/dma-buf.h
+++ include/linux/dma-buf.h
@@ -323,7 +323,7 @@ struct dma_buf_ops {
 	 */
 	int (*mmap)(struct dma_buf *, struct vm_area_struct *vma);
 
-	void *(*vmap)(struct dma_buf *);
+	int (*vmap)(struct dma_buf *dmabuf, struct dma_buf_map *map);
 	void (*vunmap)(struct dma_buf *, void *vaddr);
 
 	/**
@@ -598,8 +598,8 @@ int dma_buf_end_cpu_access_partial(struct dma_buf *dma_buf,
 
 int dma_buf_mmap(struct dma_buf *, struct vm_area_struct *,
 		 unsigned long);
-void *dma_buf_vmap(struct dma_buf *);
-void dma_buf_vunmap(struct dma_buf *, void *vaddr);
+int dma_buf_vmap(struct dma_buf *dmabuf, struct dma_buf_map *map);
+void dma_buf_vunmap(struct dma_buf *dmabuf, void *vaddr);
 int dma_buf_get_flags(struct dma_buf *dmabuf, unsigned long *flags);
 int dma_buf_get_uuid(struct dma_buf *dmabuf, uuid_t *uuid);
 
