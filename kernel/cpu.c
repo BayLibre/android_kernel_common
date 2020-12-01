991c65e29afabb6699b69637171184989f7596d8
Stephen Dickey <dickey@codeaurora.org>
2020-12-01 11:36:27 -0800

    ANDROID: cpu/hotplug: vendor hook for resume cpus
    Incorporate a vendor hook in the resume cpus path
    so that vendor specific activities may take place.
    
    Bug: 161210528
    Change-Id: I74d03247491b004e891dbcfe06a478d00a95ba9f
    Signed-off-by: Stephen Dickey <dickey@codeaurora.org>

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -1256,6 +1256,10 @@ int resume_cpus(struct cpumask *cpus)
 	for_each_cpu(cpu, cpus)
 		set_cpu_active(cpu, true);
 
+	trace_android_rvh_resume_cpus(cpus, &err);
+	if (err)
+		goto err_cpu_maps_update;
+
 	/* Lazy Resume.  Build domains immediately instead of scheduling
 	 * a workqueue.  This is so that the cpu can pull load when
 	 * sent a load balancing kick.
