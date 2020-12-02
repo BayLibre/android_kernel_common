f4e855a775bcbbaa40e73cd9d086724eeeeedf0b
Mark Rutland <mark.rutland@arm.com>
2020-12-02 13:15:57 +0000

    BACKPORT: arm64: uaccess: remove redundant PAN toggling
    Some code (e.g. futex) needs to make privileged accesses to userspace
    memory, and uses uaccess_{enable,disable}_privileged() in order to
    permit this. All other uaccess primitives use LDTR/STTR, and never need
    to toggle PAN.
    
    Remove the redundant PAN toggling.
    
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Cc: Christoph Hellwig <hch@lst.de>
    Cc: James Morse <james.morse@arm.com>
    Cc: Will Deacon <will@kernel.org>
    Link: https://lore.kernel.org/r/20201202131558.39270-12-mark.rutland@arm.com
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    
    Bug: 170327579
    Bug: 172318110
    (cherry picked from commit 7cf283c7bd6260ae43a74cd213f5ec9d665a19b5)
    [glider: resolved minor conflicts in arch/arm64/include/asm/uaccess.h]
    Signed-off-by: Alexander Potapenko <glider@google.com>
    Change-Id: I9eb8e774edf0b0a97da5c711ae07759e8dfcf3fd

--- arch/arm64/include/asm/cpucaps.h
+++ arch/arm64/include/asm/cpucaps.h
@@ -17,7 +17,6 @@
 #define ARM64_WORKAROUND_834220			7
 #define ARM64_HAS_NO_HW_PREFETCH		8
 #define ARM64_HAS_UAO				9
-#define ARM64_ALT_PAN_NOT_UAO			10
 #define ARM64_HAS_VIRT_HOST_EXTN		11
 #define ARM64_WORKAROUND_CAVIUM_27456		12
 /* Unreliable: use system_supports_32bit_el0() instead. */
