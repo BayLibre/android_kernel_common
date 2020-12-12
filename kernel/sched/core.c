30184496feb368d9738b86e9e705b9bbf080cadf
Stephen Dickey <dickey@codeaurora.org>
2020-12-11 17:10:32 -0800

    ANDROID: sched/pause: prevent timers on paused cpus
    The pause feature is designed to keep CPUs idle as
    much as possible.
    
    When the timers migration feature is enabled (with
    sysctl_timer_migration = 1 ) timer/hrtimer can be
    migrated to other CPUs than the CPU currently
    queueing the CPU. For example, a pinned kworker
    runs on a paused CPU and queued the timer. The
    other use case is an interrupt coming on paused CPU
    and queueing the time.
    
    Update the get nohz cpu path to use the active
    and online masks to prevent paused cpus from
    being woken up to queue a timer.
    
    Bug: 161210528
    Change-Id: I3707164ab6ea35cb6202b6c6b2018e1583c3e95c
    Signed-off-by: Stephen Dickey <dickey@codeaurora.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -666,7 +666,7 @@ int get_nohz_timer_target(void)
 	struct sched_domain *sd;
 
 	if (housekeeping_cpu(cpu, HK_FLAG_TIMER)) {
-		if (!idle_cpu(cpu))
+		if (!idle_cpu(cpu) && !cpu_active(cpu))
 			return cpu;
 		default_cpu = cpu;
 	}
@@ -685,8 +685,16 @@ int get_nohz_timer_target(void)
 		}
 	}
 
-	if (default_cpu == -1)
-		default_cpu = housekeeping_any_cpu(HK_FLAG_TIMER);
+	if (default_cpu == -1) {
+		default_cpu = cpumask_any(cpu_active_mask);
+		if (default_cpu >= nr_cpu_ids) {
+			default_cpu = cpumask_any(cpu_online_mask);
+			if (default_cpu >= nr_cpu_ids)
+				default_cpu =
+					housekeeping_any_cpu(HK_FLAG_TIMER);
+		}
+	}
+
 	cpu = default_cpu;
 unlock:
 	rcu_read_unlock();
