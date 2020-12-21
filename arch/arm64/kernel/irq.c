8b9222259ec5c7a35758e6f98f7f6028e8956444
Prasad Sodagudi <psodagud@codeaurora.org>
2020-12-21 13:13:19 -0800

    ANDROID: arm64: Export irq stack pointer
    Export percpu IRQ stack point symbols for loadable
    vendor modules.
    
    Bug: 176114457
    Change-Id: I57845ad0589d4c86a1d113693df571ff81c92ca8
    Signed-off-by: Prasad Sodagudi <psodagud@codeaurora.org>

--- arch/arm64/kernel/irq.c
+++ arch/arm64/kernel/irq.c
@@ -26,6 +26,7 @@
 DEFINE_PER_CPU(struct nmi_ctx, nmi_contexts);
 
 DEFINE_PER_CPU(unsigned long *, irq_stack_ptr);
+EXPORT_SYMBOL_GPL(irq_stack_ptr);
 
 #ifdef CONFIG_VMAP_STACK
 static void init_irq_stacks(void)
