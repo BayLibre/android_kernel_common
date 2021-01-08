c054c414e89d40eb8faade9f5e856282469a96e6
Kalesh Singh <kaleshsingh@google.com>
2021-01-08 12:10:08 -0800

    ANDROID: dmabuf: Add mmap_count to struct dmabuf
    mmap_count can be used to  efficiently calculate the
    PSS for any DMA buffer that the process has mapped.
    
    Bug: 167141117
    Change-Id: I8296981f0135a5532969aca3a8baa5da61dfe39f
    Signed-off-by: Kalesh Singh <kaleshsingh@google.com>

--- include/linux/dma-buf.h
+++ include/linux/dma-buf.h
@@ -381,6 +381,7 @@ struct dma_buf_ops {
  * @sysfs_entry: for exposing information about this buffer in sysfs.
  * The attachment_uid member of @sysfs_entry is protected by dma_resv lock
  * and is incremented on each attach.
+ * @mmap_count: number of times buffer has been mmapped.
  *
  * This represents a shared buffer, created by calling dma_buf_export(). The
  * userspace representation is a normal file descriptor, which can be created by
@@ -424,6 +425,10 @@ struct dma_buf {
 		unsigned int attachment_uid;
 		struct kset *attach_stats_kset;
 	} *sysfs_entry;
+	int mmap_count;
+	struct vm_operations_struct vm_ops;
+	void (*vma_open)(struct vm_area_struct *area);
+	void (*vma_close)(struct vm_area_struct *area);
 #endif
 };
 
