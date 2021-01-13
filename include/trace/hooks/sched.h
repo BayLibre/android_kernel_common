23a68a85b4122dcfd11d937f4f0605441adec24a
Yun Hsiang <yun.hsiang@mediatek.com>
2021-01-13 17:00:22 +0800

    ANDROID: schedutil: add vendor hook for adjusting util to freq calculation
    Currently, the frequency is calculated by max freq * 1.25 * util / max cap.
    Add a vendor hook to adjust the frequency when the calculation
    overestimate.
    
    android_vh_map_util_freq
    	adjust util to freq calculation
    
    Bug: 177392207
    
    Signed-off-by: Yun Hsiang <yun.hsiang@mediatek.com>
    Change-Id: I9aa9079f00af7d3380b19f2fe21b75cddd107d15
    (cherry picked from commit 3122e3ec9672036384304fdeaa1b1815f60ba817)

--- include/trace/hooks/sched.h
+++ include/trace/hooks/sched.h
@@ -190,6 +190,11 @@ struct cgroup_taskset;
 DECLARE_RESTRICTED_HOOK(android_rvh_cpu_cgroup_attach,
 	TP_PROTO(struct cgroup_taskset *tset),
 	TP_ARGS(tset), 1);
+
+DECLARE_HOOK(android_vh_map_util_freq,
+	TP_PROTO(unsigned long util, unsigned long freq,
+		unsigned long cap, unsigned long *next_freq),
+	TP_ARGS(util, freq, cap, next_freq));
 #else
 #define trace_android_rvh_select_task_rq_fair(p, prev_cpu, sd_flag, wake_flags, new_cpu)
 #define trace_android_rvh_select_task_rq_rt(p, prev_cpu, sd_flag, wake_flags, new_cpu)
@@ -233,6 +238,7 @@ DECLARE_RESTRICTED_HOOK(android_rvh_cpu_cgroup_attach,
 #define trace_android_rvh_update_cpu_capacity(cpu, capacity)
 #define trace_android_rvh_update_misfit_status(p, rq, need_update)
 #define trace_android_rvh_cpu_cgroup_attach(tset)
+#define trace_android_vh_map_util_freq(util, freq, cap, next_freq)
 #endif
 #endif /* _TRACE_HOOK_SCHED_H */
 /* This part must be outside protection */
