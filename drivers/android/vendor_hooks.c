67105d0e4c504948c8af0cf34caf4ef07416d69d
Sangmoon Kim <sangmoon.kim@samsung.com>
2021-01-19 21:19:01 +0900

    ANDROID: softlockup: add vendor hook for a softlockup task
    Add hook to gather data of softlockup and summarize it with
    other information.
    
    Bug: 177483057
    
    Signed-off-by: Sangmoon Kim <sangmoon.kim@samsung.com>
    Change-Id: I42b906f17ad689176f0cc5a1a46acd0b5971d6c5

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -30,6 +30,7 @@
 #include <trace/hooks/ftrace_dump.h>
 #include <trace/hooks/pm_domain.h>
 #include <trace/hooks/bug.h>
+#include <trace/hooks/softlockup.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -133,3 +134,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_allow_domain_state);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_map_util_freq);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_report_bug);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_em_cpu_energy);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_watchdog_timer_softlockup);
