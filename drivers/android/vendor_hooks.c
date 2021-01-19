d6eb05673085d78cabcbb8644e406d5cff54481a
Sangmoon Kim <sangmoon.kim@samsung.com>
2021-01-19 21:16:39 +0900

    ANDROID: sched: add vendor hooks for bad scheduling
    Add hooks to gather data of bad scheduling and summarize it with
    other information.
    
    Bug: 177483057
    
    Signed-off-by: Sangmoon Kim <sangmoon.kim@samsung.com>
    Change-Id: I08a7097b60dd8eebc5c0205b31c463a36f576121

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -138,3 +138,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_em_cpu_energy);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_watchdog_timer_softlockup);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_try_to_freeze_todo);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_try_to_freeze_todo_unfrozen);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_dequeue_task_idle);
--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4333,7 +4333,7 @@ static noinline void __schedule_bug(struct task_struct *prev)
 	if (panic_on_warn)
 		panic("scheduling while atomic\n");
 
-	trace_android_rvh_schedule_bug(NULL);
+	trace_android_rvh_schedule_bug(prev);
 
 	dump_stack();
 	add_taint(TAINT_WARN, LOCKDEP_STILL_OK);
