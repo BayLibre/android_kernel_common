63129aee27255990b01b6da6c23126252893719c
Sangmoon Kim <sangmoon.kim@samsung.com>
2021-01-19 21:24:58 +0900

    ANDROID: arm64: add vendor hooks for unusal abort cases
    Add hooks to gather data of unsual aborts and summarize it with
    other information.
    
    Bug: 177483057
    
    Signed-off-by: Sangmoon Kim <sangmoon.kim@samsung.com>
    Change-Id: I5f44b12770ae4976a6e3b098bd95a05dea347764

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -34,6 +34,7 @@
 #include <trace/hooks/rcu.h>
 #include <trace/hooks/softlockup.h>
 #include <trace/hooks/fault.h>
+#include <trace/hooks/traps.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -147,3 +148,6 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_die_kernel_fault);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_do_sea);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_do_mem_abort);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_do_sp_pc_abort);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_do_undefinstr);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_bad_mode);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_arm64_serror_panic);
