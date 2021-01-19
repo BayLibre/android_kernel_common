5d04e5372a02129f7ea38fd6f54a2456a1c88368
Sangmoon Kim <sangmoon.kim@samsung.com>
2021-01-19 21:23:13 +0900

    ANDROID: arm64: add vendor hooks for kernel fault cases
    Add hooks to gather data of kernel fault and summarize it with
    other information.
    
    Bug: 222638752
    
    Signed-off-by: Sangmoon Kim <sangmoon.kim@samsung.com>
    Change-Id: I7d6a66837f2e896a413bd8d878f26928669d96e6

---
+++ include/trace/hooks/fault.h
@@ -0,0 +1,31 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#undef TRACE_SYSTEM
+#define TRACE_SYSTEM fault
+#define TRACE_INCLUDE_PATH trace/hooks
+
+#if !defined(_TRACE_HOOK_FAULT_H) || defined(TRACE_HEADER_MULTI_READ)
+#define _TRACE_HOOK_FAULT_H
+#include <linux/tracepoint.h>
+#include <trace/hooks/vendor_hooks.h>
+
+struct pt_regs;
+DECLARE_RESTRICTED_HOOK(android_rvh_die_kernel_fault,
+	TP_PROTO(struct pt_regs *regs, unsigned int esr, unsigned long addr, const char *msg),
+	TP_ARGS(regs, esr, addr, msg), 1);
+
+DECLARE_RESTRICTED_HOOK(android_rvh_do_sea,
+	TP_PROTO(struct pt_regs *regs, unsigned int esr, unsigned long addr, const char *msg),
+	TP_ARGS(regs, esr, addr, msg), 1);
+
+DECLARE_RESTRICTED_HOOK(android_rvh_do_mem_abort,
+	TP_PROTO(struct pt_regs *regs, unsigned int esr, unsigned long addr, const char *msg),
+	TP_ARGS(regs, esr, addr, msg), 1);
+
+DECLARE_RESTRICTED_HOOK(android_rvh_do_sp_pc_abort,
+	TP_PROTO(struct pt_regs *regs, unsigned int esr, unsigned long addr, bool user),
+	TP_ARGS(regs, esr, addr, user),
+	TP_CONDITION(!user));
+
+#endif /* _TRACE_HOOK_FAULT_H */
+/* This part must be outside protection */
+#include <trace/define_trace.h>
