834ab229c70494259bc713e260eb8d3cd30f6a6f
Huang Yiwei <hyiwei@codeaurora.org>
2021-01-29 10:59:25 +0800

    ANDROID: timer: Add vendor hook for timer calc index
    Since we're expecting timers more precisely in short period, add
    a vendor hook to calc_index when adding timers. Then we can modify
    the index this timer used to make it accurate.
    
    Bug: 178758017
    Change-Id: I13cdf541e4c1bd426ce28b7a8a17cb8381eb2a92
    Signed-off-by: Huang Yiwei <quic_hyiwei@quicinc.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -36,6 +36,7 @@
 #include <trace/hooks/syscall_check.h>
 #include <trace/hooks/remoteproc.h>
 #include <trace/hooks/hung_task.h>
+#include <trace/hooks/timer.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -152,3 +153,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rproc_recovery);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rproc_recovery_set);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_check_uninterruptible_tasks);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_check_uninterruptible_tasks_dn);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_timer_calc_index);
