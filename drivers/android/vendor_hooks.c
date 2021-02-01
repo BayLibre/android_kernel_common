81b61ded0f11284d9637c4bbafc948e0234c21fd
davidchao <davidchao@google.com>
2021-02-01 16:46:13 +0800

    ANDROID: thermal: Add vendor thermal genl event function
    Add vendor enable_thermal_genl_check logic.
    To avoid thermal-hal being woken up all the time by thermal genl events,
    only the selected thermal_zone and cooling_device can send events from
    kernel.
    
    Bug: 170682696
    Test: boot and thermal-hal can receive thermal genl events from kernel
    Change-Id: I8f49d67963bed9d6d1f11c74bd274dc8967407c1
    Signed-off-by: davidchao <davidchao@google.com>
    (cherry picked from commit 88133a5b0b63a55dfb138e2150a6fd22b81c65a2)
    Signed-off-by: J. Avila <elavila@google.com>
    (cherry picked from commit ea8600cea83a77a6f512578ebf02de43bf680c1e)

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -51,6 +51,8 @@
 #include <trace/hooks/fault.h>
 #include <trace/hooks/traps.h>
 #include <trace/hooks/fips140.h>
+#include <trace/hooks/iommu.h>
+#include <trace/hooks/thermal.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -247,3 +249,9 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_sha256);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_aes_expandkey);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_aes_encrypt);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_aes_decrypt);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_iommu_setup_dma_ops);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_pick_next_entity);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_check_preempt_wakeup);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rmqueue);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_pagecache_get_page);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_enable_thermal_genl_check);
