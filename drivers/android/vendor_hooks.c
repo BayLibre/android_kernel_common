99eb50308d444cfe3ecfe45e63eb3fd413998e33
davidchao <davidchao@google.com>
2021-02-01 16:46:13 +0800

    ANDROID: thermal: Add vendor thermal genl event function
    Add vendor enable_thermal_genl_check logic.
    To avoid thermal-hal being woken up all the time by thermal genl events,
    only the selected thermal_zone and cooling_device can send events from
    kernel.
    
    Bug: 170682696
    Test: boot and thermal-hal can receive thermal genl events from kernel
    Change-Id: I8f49d67963bed9d6d1f11c74bd274dc8967407c1
    Signed-off-by: davidchao <davidchao@google.com>
    (cherry picked from commit 88133a5b0b63a55dfb138e2150a6fd22b81c65a2)
    Signed-off-by: Palmer Dabbelt <palmerdabbelt@google.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -29,6 +29,7 @@
 #include <trace/hooks/preemptirq.h>
 #include <trace/hooks/ftrace_dump.h>
 #include <trace/hooks/ufshcd.h>
+#include <trace/hooks/thermal.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -113,3 +114,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ufs_prepare_command);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ufs_update_sysfs);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ufs_send_command);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ufs_compl_command);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_enable_thermal_genl_check);
