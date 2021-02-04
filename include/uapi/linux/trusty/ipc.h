0a0e29eb8aa7bb7657c45d223ffd7ad351ca9f40
David LeGare <legare@google.com>
2021-02-03 17:40:10 -0800

    ANDROID: trusty: Add trace points
    Bug: 179312495
    Signed-off-by: David LeGare <legare@google.com>
    Signed-off-by: Padmanabha V <padmanabhav@google.com>
    Change-Id: I93c51b8e675c4b6faa231e51c681f82dd2c9a38f

--- include/uapi/linux/trusty/ipc.h
+++ include/uapi/linux/trusty/ipc.h
@@ -27,9 +27,10 @@
  * Trusty. Options may be valid only for certain kinds of fds.
  */
 enum transfer_kind {
-	TRUSTY_SHARE = 0,
-	TRUSTY_LEND = 1,
-	TRUSTY_SEND_SECURE = 2,
+       TRUSTY_SHARE = 0,
+       TRUSTY_LEND = 1,
+       TRUSTY_SEND_SECURE = 2,
+       TRUSTY_NO_SHM = 3,
 };
 
 /**
