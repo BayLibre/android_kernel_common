a6992c73c9ccba9c1c0ffc1f5598f195ccc1c9e8
Mukesh Ojha <mojha@codeaurora.org>
2021-02-04 15:09:58 +0530

    pstore: vendor hook for pstore on oops
    Add following vendor hooks in pstore ram oops path to allow
    vendor modules to get debugging logs like dmesg, pmsg,
    ftrace etc.
    
    1) android_vh_pstore_mem_type
    2) android_vh_pstore_buf_check
    
    Bug: 179108912
    Change-Id: I2d04a8bef336df29f6e8a1b51c4ee11e5ff88d68
    Signed-off-by: Mukesh Ojha <mojha@codeaurora.org>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -32,6 +32,7 @@
 #include <trace/hooks/pm_domain.h>
 #include <trace/hooks/bug.h>
 #include <trace/hooks/timer.h>
+#include <trace/hooks/pstore.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -139,3 +140,5 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_cpu_up);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_cpu_down);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_sched_balance_rt);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_timer_calc_index);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_pstore_mem_type);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_pstore_buf_check);
