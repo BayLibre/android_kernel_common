2fe67bcaffc82a8805ccf50f5875d19ae05526b6
Chiawei Wang <chiaweiwang@google.com>
2021-02-18 10:43:50 +0800

    ANDROID: mm: Add vendor hook in rmqueue()
    Add a vendor hook for costly order page counting
    and other vendor specific functions.
    
    Bug: 174521902
    Bug: 172987241
    Signed-off-by: Chiawei Wang <chiaweiwang@google.com>
    Change-Id: I89206727a462548cc3500b695d85c83ff003eec7
    Signed-off-by: Richard Chang <richardycc@google.com>
    (cherry picked from commit 369de3780428a17e9afece2f5747f03619d589b6)
    Signed-off-by: liangjlee <liangjlee@google.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -608,3 +608,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_wq_queue_work);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_wq_wake_idle_worker);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_dma_buf_release);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_send_signal_locked);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rmqueue);
