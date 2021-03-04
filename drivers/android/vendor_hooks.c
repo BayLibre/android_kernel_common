088e37996e5ea666725e5b0fe96b427b28dcc3ca
Pavankumar Kondeti <quic_pkondeti@quicinc.com>
2021-03-05 04:31:50 +0530

    ANDROID: cpufreq: Add a restricted vendor hook for freq transition
    A restricted vendor hook for cpufreq transition. The hook is
    invoked per policy after changing the policy->cur and passes
    policy as an argument.
    
    Bug: 181889516
    Change-Id: I6f89850779fdd63e178e227c293f42b7f66fd78d
    Signed-off-by: Pavankumar Kondeti <quic_pkondeti@quicinc.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -166,3 +166,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ufs_send_command);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ufs_compl_command);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_do_wake_up_sync);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_set_wake_flags);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_cpufreq_transition);
