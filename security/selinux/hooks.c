dc097ca9a8258694c07a40e354f0b0e17b76f3c5
Greg Kroah-Hartman <gregkh@google.com>
2021-03-05 15:29:16 +0100

    Revert "FROMLIST: Add flags option to get xattr method paired to __vfs_getxattr"
    This reverts commit 3484eba91d6b529cc606486a2db79513f3db6c67.
    
    It's causing too many merge issues, so revert it for now and hopefully
    bring it back later...
    
    Cc: Mark Salyzyn <salyzyn@android.com>
    Bug: 133515582
    Bug: 136124883
    Bug: 129319403
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: Id70a37d9471eff58f0027a1371538858136604d3

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -1361,14 +1361,12 @@ static int inode_doinit_use_xattr(struct inode *inode, struct dentry *dentry,
 		return -ENOMEM;
 
 	context[len] = '\0';
-	rc = __vfs_getxattr(dentry, inode, XATTR_NAME_SELINUX, context, len,
-			    XATTR_NOSECURITY);
+	rc = __vfs_getxattr(dentry, inode, XATTR_NAME_SELINUX, context, len);
 	if (rc == -ERANGE) {
 		kfree(context);
 
 		/* Need a larger buffer.  Query for the right size. */
-		rc = __vfs_getxattr(dentry, inode, XATTR_NAME_SELINUX, NULL, 0,
-				    XATTR_NOSECURITY);
+		rc = __vfs_getxattr(dentry, inode, XATTR_NAME_SELINUX, NULL, 0);
 		if (rc < 0)
 			return rc;
 
@@ -1379,7 +1377,7 @@ static int inode_doinit_use_xattr(struct inode *inode, struct dentry *dentry,
 
 		context[len] = '\0';
 		rc = __vfs_getxattr(dentry, inode, XATTR_NAME_SELINUX,
-				    context, len, XATTR_NOSECURITY);
+				    context, len);
 	}
 	if (rc < 0) {
 		kfree(context);
