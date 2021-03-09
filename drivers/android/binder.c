e3108166a72edc7d66f7f4306c31d408fd183299
Zhuguangqing <zhuguangqing@xiaomi.com>
2021-03-09 15:47:43 +0800

    ANDROID: andd vendor hook to binder.
    Add vendor hook to get the binder message for vendor-specific tuning.
    
    Signed-off-by: Zhuguangqing <zhuguangqing@xiaomi.com>
    Change-Id: Id47e59c4e3ccd07b26eef758ada147b98cd1964e

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -3070,6 +3070,7 @@ static void binder_transaction(struct binder_proc *proc,
 		target_proc = target_thread->proc;
 		target_proc->tmp_ref++;
 		binder_inner_proc_unlock(target_thread->proc);
+		trace_android_vh_binder_reply(target_proc, proc, thread, tr);
 	} else {
 		if (tr->target.handle) {
 			struct binder_ref *ref;
@@ -3122,6 +3123,7 @@ static void binder_transaction(struct binder_proc *proc,
 			goto err_dead_binder;
 		}
 		e->to_node = target_node->debug_id;
+		trace_android_vh_binder_trans(target_proc, proc, thread, tr);
 		if (security_binder_transaction(proc->tsk,
 						target_proc->tsk) < 0) {
 			return_error = BR_FAILED_REPLY;
@@ -4240,6 +4242,7 @@ static int binder_wait_for_work(struct binder_thread *thread,
 		prepare_to_wait(&thread->wait, &wait, TASK_INTERRUPTIBLE);
 		if (binder_has_work_ilocked(thread, do_proc_work))
 			break;
+		trace_android_vh_binder_wait_for_work(thread, proc);
 		if (do_proc_work)
 			list_add(&thread->waiting_thread_node,
 				 &proc->waiting_threads);
@@ -5413,7 +5416,7 @@ static int binder_open(struct inode *nodp, struct file *filp)
 	}
 	hlist_add_head(&proc->proc_node, &binder_procs);
 	mutex_unlock(&binder_procs_lock);
-
+	trace_android_vh_binder_open_get_hhead_and_lock(&binder_procs, &binder_procs_lock);
 	if (binder_debugfs_dir_entry_proc && !existing_pid) {
 		char strbuf[11];
 
