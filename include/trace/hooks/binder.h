1c5f5bf1f2d9a4a4430838fff4aa04218119f9b0
Zhuguangqing <zhuguangqing@xiaomi.com>
2021-03-09 15:47:43 +0800

    ANDROID: Add vendor hook to binder.
    Add vendor hook to get the binder message for vendor-specific tuning.
    
    Bug: 182496370
    
    Signed-off-by: Zhuguangqing <zhuguangqing@xiaomi.com>
    Change-Id: Id47e59c4e3ccd07b26eef758ada147b98cd1964e

--- include/trace/hooks/binder.h
+++ include/trace/hooks/binder.h
@@ -13,6 +13,10 @@
 struct binder_transaction;
 struct task_struct;
 struct seq_file;
+struct binder_alloc;
+struct binder_proc;
+struct binder_thread;
+struct binder_transaction_data;
 DECLARE_HOOK(android_vh_binder_transaction_init,
 	TP_PROTO(struct binder_transaction *t),
 	TP_ARGS(t));
@@ -74,6 +78,21 @@ DECLARE_HOOK(android_vh_binder_read_done,
 DECLARE_HOOK(android_vh_binder_has_work_ilocked,
 	TP_PROTO(struct binder_thread *thread, bool do_proc_work, int *ret),
 	TP_ARGS(thread, do_proc_work, ret));
+DECLARE_HOOK(android_vh_binder_alloc_new_buf_locked,
+	TP_PROTO(size_t size, struct binder_alloc *alloc, int is_async),
+	TP_ARGS(size, alloc, is_async));
+DECLARE_HOOK(android_vh_binder_reply,
+	TP_PROTO(struct binder_proc *target_proc, struct binder_proc *proc,
+		struct binder_thread *thread, struct binder_transaction_data *tr),
+	TP_ARGS(target_proc, proc, thread, tr));
+DECLARE_HOOK(android_vh_binder_trans,
+	TP_PROTO(struct binder_proc *target_proc, struct binder_proc *proc,
+		struct binder_thread *thread, struct binder_transaction_data *tr),
+	TP_ARGS(target_proc, proc, thread, tr));
+DECLARE_HOOK(android_vh_binder_preset,
+	TP_PROTO(struct hlist_head *hhead, struct mutex *lock),
+	TP_ARGS(hhead, lock));
+
 
 #endif /* _TRACE_HOOK_BINDER_H */
 /* This part must be outside protection */
