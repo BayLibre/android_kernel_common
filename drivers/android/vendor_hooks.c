3e1fc7372b57fcdde806f579ba256cabc925be61
xieliujie <xieliujie@oppo.com>
2021-03-09 22:15:47 +0800

    ANDROID: vendor_hooks: Add hooks for rwsem and mutex
    Add hooks to apply oem's optimization of rwsem and mutex
    
    Bug: 182237112
    Signed-off-by: xieliujie <xieliujie@oppo.com>
    Change-Id: I6332623732e2d6826b8b61087ca74e55393e0c3d
    (cherry picked from commit 0902cc73b793f8b8cc2a80943d85d4ca9b98278b)

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -150,3 +150,5 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_mmc_resume);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_mmc_suspend);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mmc_update_mmc_queue);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rwsem_downgrade_wake_finish);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_alter_mutex_list_add);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mutex_unlock_slowpath);
