d6f4fa6ec253a52f3ae89402da1e1debe7d3c086
Naina Mehta <nainmeht@codeaurora.org>
2021-03-10 10:03:29 +0530

    ANDROID: gpiolib: Add vendor hook for gpio read
    Add android_vh_gpio_read vendor hook to bypass
    initial gpio read.
    
    Bug: 182716666
    Change-Id: Ifda23ae50d90573698bc88fc9bbdfd428fdeae1a
    Signed-off-by: Naina Mehta <nainmeht@codeaurora.org>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -31,6 +31,7 @@
 #include <trace/hooks/ufshcd.h>
 #include <trace/hooks/cgroup.h>
 #include <trace/hooks/sys.h>
+#include <trace/hooks/gpiolib.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -118,3 +119,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ufs_send_command);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ufs_compl_command);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_cgroup_set_task);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_syscall_prctl_finished);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_gpio_block_read);
