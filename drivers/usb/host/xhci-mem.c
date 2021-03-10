67c1f69440b79397b818488108c6c336c0b51709
Jack Pham <jackp@codeaurora.org>
2021-03-10 00:27:11 -0800

    ANDROID: usb: host: export additional xhci symbols for ring management
    Export the following xhci symbols which will allow vendor modules
    to perform their own ring management. These will be used to create
    and manage additional secondary rings that are used to offload to a
    separate coprocessor which interacts with the xHC controller.
    
    xhci_ring_alloc
        - Allocate a struct xhci_ring.
    
    xhci_alloc_erst
    xhci_free_erst
        - Allocate and free event ring segment tables.
    
    xhci_trb_virt_to_dma
        - Used to retrieve the DMA address of a TRB
    
    xhci_ring_cmd_db
        - Notify the controller when a new command is issued
    
    xhci_alloc_command
    xhci_free_command
        - Allocate and free a struct xhci_command
    
    xhci_queue_stop_endpoint
        - Issue a stop endpoint command to the controller
    
    Bug: 182336717
    Change-Id: I9d4c5884108e656e890aca8137d4ef580bcd7a12
    Signed-off-by: Jack Pham <jackp@codeaurora.org>
    (cherry picked from commit 6c997d153d7c1e11d51aeb1b7c46002d5daa760b)
    Signed-off-by: Carlos Llamas <cmllamas@google.com>

--- drivers/usb/host/xhci-mem.c
+++ drivers/usb/host/xhci-mem.c
@@ -408,6 +408,7 @@ fail:
 	kfree(ring);
 	return NULL;
 }
+EXPORT_SYMBOL_GPL(xhci_ring_alloc);
 
 void xhci_free_endpoint_ring(struct xhci_hcd *xhci,
 		struct xhci_virt_device *virt_dev,
@@ -1757,6 +1758,7 @@ struct xhci_command *xhci_alloc_command(struct xhci_hcd *xhci,
 	INIT_LIST_HEAD(&command->cmd_list);
 	return command;
 }
+EXPORT_SYMBOL_GPL(xhci_alloc_command);
 
 struct xhci_command *xhci_alloc_command_with_ctx(struct xhci_hcd *xhci,
 		bool allocate_completion, gfp_t mem_flags)
@@ -1790,6 +1792,7 @@ void xhci_free_command(struct xhci_hcd *xhci,
 	kfree(command->completion);
 	kfree(command);
 }
+EXPORT_SYMBOL_GPL(xhci_free_command);
 
 int xhci_alloc_erst(struct xhci_hcd *xhci,
 		    struct xhci_ring *evt_ring,
@@ -1820,6 +1823,7 @@ int xhci_alloc_erst(struct xhci_hcd *xhci,
 
 	return 0;
 }
+EXPORT_SYMBOL_GPL(xhci_alloc_erst);
 
 void xhci_free_erst(struct xhci_hcd *xhci, struct xhci_erst *erst)
 {
@@ -1833,6 +1837,7 @@ void xhci_free_erst(struct xhci_hcd *xhci, struct xhci_erst *erst)
 				erst->erst_dma_addr);
 	erst->entries = NULL;
 }
+EXPORT_SYMBOL_GPL(xhci_free_erst);
 
 void xhci_mem_cleanup(struct xhci_hcd *xhci)
 {
