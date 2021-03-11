4a28136cc7feca1212b3f9f816068110d7419490
Liujie Xie <xieliujie@oppo.com>
2021-03-11 20:40:06 +0800

    ANDROID: vendor_hooks: Add hooks for memory
    when debug Add vendors hooks for recording memory used
    
    Vendor modules allocate and manages the memory itself.
    
    These memories might not be included in kernel memory
    statistics. Also, detailed references and vendor-specific
    information are managed only inside modules. When
    various problems such as memory leaks occurs, these
    information should be showed in real-time.
    
    Bug: 182443489
    Bug: 234407991
    Bug: 277799025
    
    Change-Id: I623d68dd1056517024b0db2bf2fd9a63a92893e6
    Signed-off-by: Liujie Xie <xieliujie@oppo.com>
    Signed-off-by: Hyesoo Yu <hyesoo.yu@samsung.com>

--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -96,6 +96,23 @@ DECLARE_HOOK(android_vh_try_to_unmap_one,
 	TP_PROTO(struct folio *folio, struct vm_area_struct *vma,
 		unsigned long addr, void *arg, bool ret),
 	TP_ARGS(folio, vma, addr, arg, ret));
+DECLARE_HOOK(android_vh_meminfo_proc_show,
+	TP_PROTO(struct seq_file *m),
+	TP_ARGS(m));
+DECLARE_HOOK(android_vh_exit_mm,
+	TP_PROTO(struct mm_struct *mm),
+	TP_ARGS(mm));
+DECLARE_HOOK(android_vh_show_mem,
+	TP_PROTO(unsigned int filter, nodemask_t *nodemask),
+	TP_ARGS(filter, nodemask));
+DECLARE_HOOK(android_vh_print_slabinfo_header,
+	TP_PROTO(struct seq_file *m),
+	TP_ARGS(m));
+struct slabinfo;
+DECLARE_HOOK(android_vh_cache_show,
+	TP_PROTO(struct seq_file *m, struct slabinfo *sinfo, struct kmem_cache *s),
+	TP_ARGS(m, sinfo, s));
+
 #endif /* _TRACE_HOOK_MM_H */
 
 /* This part must be outside protection */
