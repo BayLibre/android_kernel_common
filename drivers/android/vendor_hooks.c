5b7ed23f3babac60139d7499378c84b27668676b
Rick Yiu <rickyiu@google.com>
2021-03-12 16:33:30 +0800

    ANDROID: sched: Add vendor hook for uclamp_eff_get
    Vendor may have their own behavior for determing uclamp value. Also
    uclamp_eff_get() is used in uclamp_rq_inc_id() when task is enqueued,
    and it is contained in uclamp_eff_value(), there is no much benefit to
    hook uclamp_eff_value(), so remove the hook in uclamp_eff_value().
    
    Bug: 170507972
    Bug: 180859906
    Signed-off-by: Rick Yiu <rickyiu@google.com>
    Change-Id: Ibf7c043188a77c40baf5b4040b198a4197236089
    Signed-off-by: Will McVicker <willmcvicker@google.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -198,3 +198,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_check_file_open);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_check_bpf_syscall);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_post_init_entity_util_avg);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_find_new_ilb);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_uclamp_eff_get);
