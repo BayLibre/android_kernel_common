8e08192612151fe2713f0ab26599c9ae134ed96e
xieliujie <xieliujie@oppo.com>
2021-03-16 20:48:42 +0800

    ANDROID: vendor_hooks: Add hooks for improving binder trans
    Recognize important binder proc & binder thread and improve their sched
    lantency.
    
    Bug: 182952552
    Signed-off-by: xieliujie <xieliujie@oppo.com>
    Change-Id: I174949bf90a4215a6d27f24abbc7d324a321e662

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -957,7 +957,8 @@ static void binder_wakeup_poll_threads_ilocked(struct binder_proc *proc,
 		thread = rb_entry(n, struct binder_thread, rb_node);
 		if (thread->looper & BINDER_LOOPER_STATE_POLL &&
 		    binder_available_for_proc_work_ilocked(thread)) {
-			trace_android_vh_binder_wakeup_ilocked(thread->task);
+			trace_android_vh_binder_wakeup_ilocked(thread->task, sync,
+				proc->tsk, proc->max_threads);
 			if (sync)
 				wake_up_interruptible_sync(&thread->wait);
 			else
@@ -1017,7 +1018,8 @@ static void binder_wakeup_thread_ilocked(struct binder_proc *proc,
 	assert_spin_locked(&proc->inner_lock);
 
 	if (thread) {
-		trace_android_vh_binder_wakeup_ilocked(thread->task);
+		trace_android_vh_binder_wakeup_ilocked(thread->task, sync,
+			proc->tsk, proc->max_threads);
 		if (sync)
 			wake_up_interruptible_sync(&thread->wait);
 		else
@@ -4243,6 +4245,7 @@ static int binder_wait_for_work(struct binder_thread *thread,
 		if (do_proc_work)
 			list_add(&thread->waiting_thread_node,
 				 &proc->waiting_threads);
+		trace_android_vh_binder_wait_for_work(do_proc_work, thread->task, proc->tsk);
 		binder_inner_proc_unlock(proc);
 		schedule();
 		binder_inner_proc_lock(proc);
@@ -4608,6 +4611,7 @@ retry:
 			trd->sender_pid =
 				task_tgid_nr_ns(sender,
 						task_active_pid_ns(current));
+			trace_android_vh_sync_txn_recvd(thread->task, t_from->task);
 		} else {
 			trd->sender_pid = 0;
 		}
