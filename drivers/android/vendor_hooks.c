f65e755adadbab73e3e5354e51e7bf38e5e9aa3c
Rick Yiu <rickyiu@google.com>
2021-03-16 10:07:31 +0800

    ANDROID: sched: Add vendor hook for util_est_update
    Vendor may have its own estimated utilization.
    
    Bug: 170508405
    Signed-off-by: Rick Yiu <rickyiu@google.com>
    Change-Id: I6055907de75ace4586c3ad854d40f42e3bf40147
    Signed-off-by: Will McVicker <willmcvicker@google.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -199,3 +199,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_check_bpf_syscall);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_post_init_entity_util_avg);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_find_new_ilb);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_uclamp_eff_get);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_util_est_update);
