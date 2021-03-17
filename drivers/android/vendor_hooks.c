afa6262e843b539cd99ed5b500b73feb60021501
Badhri Jagan Sridharan <badhri@google.com>
2021-03-17 15:02:21 -0700

    ANDROID: usb: typec: tcpci: Add vendor hooks for tcpci interface
    Needed to implement chip specific features that are not defined
    in the Type-C spec.
    
    Remove the include of linux/tracepoint.h to follow the changes in
    commit 0ef21e1c1ae5 ("ANDROID: vendor_hooks: Reduce pointless modversions CRC churn")
    
    OOT_bug:
    Bug: 169213252
    Bug: 168245874
    Bug: 173252019
    Signed-off-by: Badhri Jagan Sridharan <badhri@google.com>
    Change-Id: I36fe75dddd8cd4e2054db01ed4fee7ea08dd8702
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Signed-off-by: Kyle Tso <kyletso@google.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -68,6 +68,7 @@
 #include <trace/hooks/ipv4.h>
 #include <trace/hooks/signal.h>
 #include <trace/hooks/cfg80211.h>
+#include <trace/hooks/typec.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -404,3 +405,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_read_done);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_preset);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_gic_v2_resume);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_exit_signal);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_typec_tcpci_override_toggling);
