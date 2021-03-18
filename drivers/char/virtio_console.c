664dd3036dab926f134f0fe5692797b41f687125
bohu <bohu@google.com>
2021-03-18 15:20:22 -0700

    ANDROID: hack register hvc tty
    work in progress
    it appears to be working
    
    BUG: 180435101
    
    Signed-off-by: Bo Hu <bohu@google.com>
    Change-Id: I41e0c45e395a95426639d57ca3dc8448b6f4d7b8

--- drivers/char/virtio_console.c
+++ drivers/char/virtio_console.c
@@ -16,16 +16,26 @@
 #include <linux/init.h>
 #include <linux/list.h>
 #include <linux/poll.h>
+#include <linux/serdev.h>
 #include <linux/sched.h>
 #include <linux/slab.h>
 #include <linux/spinlock.h>
 #include <linux/virtio.h>
+#include <linux/tty.h>
+#include <linux/tty_driver.h>
 #include <linux/virtio_console.h>
 #include <linux/wait.h>
 #include <linux/workqueue.h>
 #include <linux/module.h>
 #include <linux/dma-mapping.h>
 #include "../tty/hvc/hvc_console.h"
+
+static char *hvc_tty_port;
+module_param(hvc_tty_port, charp, 0644);
+MODULE_PARM_DESC(hvc_tty_port, "hvc device tty port to claim");
+
+extern struct gnss_serial *gnss_serial_allocate(struct serdev_device *gserial, size_t data_size);
+extern int gnss_serial_register(struct gnss_serial *gserial);
 
 #define is_rproc_enabled IS_ENABLED(CONFIG_REMOTEPROC)
 
@@ -1253,6 +1263,20 @@ static int init_port_console(struct port *port)
 		port->cons.hvc = NULL;
 		return ret;
 	}
+	port->cons.hvc->dev = &(port->portdev->vdev->dev);
+	struct hvc_struct *hp = port->cons.hvc;
+	if (hp && hvc_tty_port) {
+		unsigned long hvc_idx;
+		if (!kstrtoul(hvc_tty_port, 10, &hvc_idx) && hvc_idx == hp->index) {
+            struct device* mydev = serdev_tty_port_register(&(hp->port), hp->dev, hp->driver, hp->index);
+            if (PTR_ERR(mydev) != -ENODEV) {
+	            struct serdev_controller* myserctrl = container_of(mydev, struct serdev_controller, dev);
+                gnss_serial_register(gnss_serial_allocate(myserctrl->serdev, 0));
+                tty_unregister_device(hp->driver, hp->index);
+            }
+		}
+	}
+
 	spin_lock_irq(&pdrvdata_lock);
 	pdrvdata.next_vtermno++;
 	list_add_tail(&port->cons.list, &pdrvdata.consoles);
@@ -2283,4 +2307,5 @@ module_init(init);
 module_exit(fini);
 
 MODULE_DESCRIPTION("Virtio console driver");
+MODULE_SOFTDEP("pre: gnss_serial");
 MODULE_LICENSE("GPL");
