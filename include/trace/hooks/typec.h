763a3aea01ad5df398439ddd828d95212b48d2f5
Badhri Jagan Sridharan <badhri@google.com>
2021-03-18 16:15:41 -0700

    ANDROID: usb: typec: tcpci: Add vendor hook to mask vbus present
    OOT_bug:
    Bug: 183149028
    Bug: 168245874
    Bug: 173252019
    Signed-off-by: Badhri Jagan Sridharan <badhri@google.com>
    Change-Id: I2ca994d49b37bf8600c5913b892fde9acd0dc896

--- include/trace/hooks/typec.h
+++ include/trace/hooks/typec.h
@@ -20,6 +20,11 @@ DECLARE_HOOK(android_vh_typec_tcpci_override_toggling,
 DECLARE_HOOK(android_vh_typec_tcpci_check_contaminant,
 	TP_PROTO(struct tcpci *tcpci, struct tcpci_data *data, int *ret),
 	TP_ARGS(tcpci, data, ret));
+
+DECLARE_HOOK(android_vh_typec_tcpci_get_vbus,
+	TP_PROTO(struct tcpci *tcpci, struct tcpci_data *data, int *vbus, int *bypass),
+	TP_ARGS(tcpci, data, vbus, bypass));
+
 #endif /* _TRACE_HOOK_UFSHCD_H */
 /* This part must be outside protection */
 #include <trace/define_trace.h>
