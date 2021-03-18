43236e6a937594aa6e7d9d1b73982c9275a71442
Manish Varma <varmam@google.com>
2021-03-18 13:53:57 -0700

    ANDROID: i2c: Add vendor hook to allow assign dev_name to I2C devices
    "dev_name" for I2C devices won't have any value set, unless they are
    instantiated thru ACPI interface. As a result of this, I2C driver will
    assign some dynamic name in the format "%d-%04x" (e.g. 1-001f), and
    further this device names are used for kernel wakelocks.
    
    This dynamic names are difficult to associate with actual device and
    hence it will help to have an ability where vendor can assign the
    device name through vendor hooks.
    
    Bug: 147496295
    
    Signed-off-by: Manish Varma <varmam@google.com>
    Change-Id: Idb417ef5330002063a9763544f9f0364f9581276

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -27,6 +27,7 @@
 #include <trace/hooks/epoch.h>
 #include <trace/hooks/cpufreq.h>
 #include <trace/hooks/fs.h>
+#include <trace/hooks/i2c.h>
 #include <trace/hooks/mm.h>
 #include <trace/hooks/preemptirq.h>
 #include <trace/hooks/ftrace_dump.h>
@@ -262,3 +263,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_dequeue_task_fair);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_prepare_update_load_avg_se);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_finish_update_load_avg_se);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_selinux_is_initialized);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_of_i2c_get_board_info);
