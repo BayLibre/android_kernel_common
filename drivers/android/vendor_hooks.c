5de6e157ce062c6a53fc0d5d93dae73e71b7187b
Liujie Xie <xieliujie@oppo.com>
2021-03-22 02:24:32 +0800

    ANDROID: vendor_hooks: Add hooks for account process tick
    Add a hook in account_process_tick, which help us to get information
    about the high load task and the cpu they running on.
    
    Bug: 183260319
    Change-Id: I54162ce3c65bd69e08d2d4747e4d4883efe4c442
    Signed-off-by: Liujie Xie <xieliujie@oppo.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -176,3 +176,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_uclamp_eff_value);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_cpufreq_transition);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_cgroup_set_task);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_syscall_prctl_finished);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_account_task_time);
--- include/trace/hooks/sched.h
+++ include/trace/hooks/sched.h
@@ -275,6 +275,9 @@ DECLARE_RESTRICTED_HOOK(android_rvh_uclamp_eff_value,
 		 struct uclamp_se *uclamp_default, unsigned long *ret),
 	TP_ARGS(p, clamp_id, uclamp_default, ret), 1);
 
+DECLARE_HOOK(android_vh_account_task_time,
+	TP_PROTO(struct task_struct *p, struct rq *rq, int user_tick),
+	TP_ARGS(p, rq, user_tick));
 /* macro versions of hooks are no longer required */
 
 #endif /* _TRACE_HOOK_SCHED_H */
