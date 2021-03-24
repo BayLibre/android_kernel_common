487d1aa7cf8289a8888a155f7c14359da7424620
lijianzhong <lijianzhong@xiaomi.com>
2021-03-24 15:44:55 +0800

    ANDROID: sched: Add vendor hooks for cpu affinity.
    add vendor hooks for cpu affinity to support oem's feature.
    
    Bug: 183674818
    
    Signed-off-by: lijianzhong <lijianzhong@xiaomi.com>
    Change-Id: I3402abec4d9faa08f564409bfb8db8d7902f3aa2

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1884,6 +1884,7 @@ void set_cpus_allowed_common(struct task_struct *p, const struct cpumask *new_ma
 {
 	cpumask_copy(&p->cpus_mask, new_mask);
 	p->nr_cpus_allowed = cpumask_weight(new_mask);
+	trace_android_rvh_set_cpus_allowed_comm(p, new_mask);
 }
 
 void do_set_cpus_allowed(struct task_struct *p, const struct cpumask *new_mask)
@@ -6096,6 +6097,9 @@ long sched_setaffinity(pid_t pid, const struct cpumask *in_mask)
 		rcu_read_unlock();
 	}
 
+	trace_android_vh_sched_setaffinity_early(p, in_mask, &retval);
+	if (!retval)
+		goto out_free_new_mask;
 	retval = security_task_setscheduler(p);
 	if (retval)
 		goto out_free_new_mask;
