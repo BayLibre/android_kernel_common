b2b2dfce31f721bffd7a66d1ac84afbe29ce75b3
Vivek Goyal <vgoyal@redhat.com>
2021-03-25 11:18:22 -0400

    fuse: extend FUSE_SETXATTR request
    Fuse client needs to send additional information to file server when it
    calls SETXATTR(system.posix_acl_access), so add extra flags field to the
    structure.
    
    Signed-off-by: Vivek Goyal <vgoyal@redhat.com>
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Change-Id: I24f1acf2499e53e47d68f302420df9520e42ad0c

--- fs/fuse/inode.c
+++ fs/fuse/inode.c
@@ -1060,6 +1060,8 @@ static void process_init_reply(struct fuse_mount *fm, struct fuse_args *args,
 				fm->sb->s_stack_depth =
 					FILESYSTEM_MAX_STACK_DEPTH;
 			}
+			if (arg->flags & FUSE_SETXATTR_EXT)
+				fc->setxattr_ext = 1;
 		} else {
 			ra_pages = fc->max_read / PAGE_SIZE;
 			fc->no_lock = 1;
@@ -1103,7 +1105,8 @@ void fuse_send_init(struct fuse_mount *fm)
 		FUSE_PARALLEL_DIROPS | FUSE_HANDLE_KILLPRIV | FUSE_POSIX_ACL |
 		FUSE_ABORT_ERROR | FUSE_MAX_PAGES | FUSE_CACHE_SYMLINKS |
 		FUSE_NO_OPENDIR_SUPPORT | FUSE_EXPLICIT_INVAL_DATA |
-		FUSE_HANDLE_KILLPRIV_V2 | FUSE_PASSTHROUGH;
+		FUSE_PASSTHROUGH |
+		FUSE_HANDLE_KILLPRIV_V2 | FUSE_SETXATTR_EXT;
 #ifdef CONFIG_FUSE_DAX
 	if (fm->fc->dax)
 		ia->in.flags |= FUSE_MAP_ALIGNMENT;
