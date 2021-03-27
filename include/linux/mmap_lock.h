1a387bf818ae984e45d3c5863fe99d96116ec0f1
Michel Lespinasse <michel@lespinasse.org>
2021-03-27 08:51:51 -0700

    BACKPORT: FROMGIT: mm: add per-mm mmap sequence counter for speculative page fault handling.
    The counter's write side is hooked into the existing mmap locking API:
    mmap_write_lock() increments the counter to the next (odd) value, and
    mmap_write_unlock() increments it again to the next (even) value.
    
    The counter's speculative read side is supposed to be used as follows:
    
    seq = mmap_seq_read_start(mm);
    if (seq & 1)
    	goto fail;
    .... speculative handling here ....
    if (!mmap_seq_read_check(mm, seq)
    	goto fail;
    
    This API guarantees that, if none of the "fail" tests abort
    speculative execution, the speculative code section did not run
    concurrently with any mmap writer.
    
    This is very similar to a seqlock, but both the writer and speculative
    readers are allowed to block. In the fail case, the speculative reader
    does not spin on the sequence counter; instead it should fall back to
    a different mechanism such as grabbing the mmap lock read side.
    
    Signed-off-by: Michel Lespinasse <walken@google.com>
    
    (cherry picked from commit 8d5a66905432a0bb0364ba26b0d9117001916fac
    https://github.com/lespinasse/linux.git)
    
    Conflicts:
        include/linux/mmap_lock.h
    
    1. Trivial merge conflicts.
    
    Bug: 183885439
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Change-Id: I316acfe3b7d9582990f6b2ed8fdcc3b5946694c4

--- include/linux/mmap_lock.h
+++ include/linux/mmap_lock.h
@@ -3,22 +3,70 @@
 
 #include <linux/mmdebug.h>
 
-#define MMAP_LOCK_INITIALIZER(name) \
-	.mmap_lock = __RWSEM_INITIALIZER((name).mmap_lock),
+#ifdef CONFIG_SPECULATIVE_PAGE_FAULT
+#define MMAP_LOCK_SEQ_INITIALIZER(name) \
+	.mmap_seq = 0,
+#else
+#define MMAP_LOCK_SEQ_INITIALIZER(name)
+#endif
+
+#define MMAP_LOCK_INITIALIZER(name)				\
+	.mmap_lock = __RWSEM_INITIALIZER((name).mmap_lock),	\
+	MMAP_LOCK_SEQ_INITIALIZER(name)
 
 static inline void mmap_init_lock(struct mm_struct *mm)
 {
 	init_rwsem(&mm->mmap_lock);
+#ifdef CONFIG_SPECULATIVE_PAGE_FAULT
+	mm->mmap_seq = 0;
+#endif
 }
+
+static inline void __mmap_seq_write_lock(struct mm_struct *mm)
+{
+#ifdef CONFIG_SPECULATIVE_PAGE_FAULT
+	VM_BUG_ON_MM(mm->mmap_seq & 1, mm);
+	mm->mmap_seq++;
+	smp_wmb();
+#endif
+}
+
+static inline void __mmap_seq_write_unlock(struct mm_struct *mm)
+{
+#ifdef CONFIG_SPECULATIVE_PAGE_FAULT
+	smp_wmb();
+	mm->mmap_seq++;
+	VM_BUG_ON_MM(mm->mmap_seq & 1, mm);
+#endif
+}
+
+#ifdef CONFIG_SPECULATIVE_PAGE_FAULT
+static inline unsigned long mmap_seq_read_start(struct mm_struct *mm)
+{
+	unsigned long seq;
+
+	seq = READ_ONCE(mm->mmap_seq);
+	smp_rmb();
+	return seq;
+}
+
+static inline bool mmap_seq_read_check(struct mm_struct *mm, unsigned long seq)
+{
+	smp_rmb();
+	return seq == READ_ONCE(mm->mmap_seq);
+}
+#endif
 
 static inline void mmap_write_lock(struct mm_struct *mm)
 {
 	down_write(&mm->mmap_lock);
+	__mmap_seq_write_lock(mm);
 }
 
 static inline void mmap_write_lock_nested(struct mm_struct *mm, int subclass)
 {
 	down_write_nested(&mm->mmap_lock, subclass);
+	__mmap_seq_write_lock(mm);
 }
 
 static inline int mmap_write_lock_killable(struct mm_struct *mm)
@@ -26,6 +74,8 @@ static inline int mmap_write_lock_killable(struct mm_struct *mm)
 	int error;
 
 	error = down_write_killable(&mm->mmap_lock);
+	if (likely(!error))
+		__mmap_seq_write_lock(mm);
 	return error;
 }
 
@@ -34,16 +84,20 @@ static inline bool mmap_write_trylock(struct mm_struct *mm)
 	bool ok;
 
 	ok = down_write_trylock(&mm->mmap_lock) != 0;
+	if (likely(ok))
+		__mmap_seq_write_lock(mm);
 	return ok;
 }
 
 static inline void mmap_write_unlock(struct mm_struct *mm)
 {
+	__mmap_seq_write_unlock(mm);
 	up_write(&mm->mmap_lock);
 }
 
 static inline void mmap_write_downgrade(struct mm_struct *mm)
 {
+	__mmap_seq_write_unlock(mm);
 	downgrade_write(&mm->mmap_lock);
 }
 
