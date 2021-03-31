07a660d5f2fcedb095c1f701e0a3c455f34eb181
Badhri Jagan Sridharan <badhri@google.com>
2021-03-31 10:56:12 -0700

    ANDROID: usb: typec: tcpm: vendor hook for timer adjustments
    linux/usb/pd.h has a bunch of timers for which the Type-C spec defines
    a range of values. These values have to be tuned based on the latency
    observed in a specific architecture. However, linux opensource sets
    them to a specific value without providing a mechanism to set board
    specific values. While a generic way is figured out, a vendor hook
    is needed in the interim.
    
    For instance, tCCDebounce can have a value between 100msec - 200msec.
    
    OOT_bug:
    Bug: 184308605
    Bug: 168245874
    Bug: 173252019
    Change-Id: I278b34654a7e48990b6ebe25fbe17e3aa4165098
    Signed-off-by: Badhri Jagan Sridharan <badhri@google.com>
    Signed-off-by: Kyle Tso <kyletso@google.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -408,3 +408,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_exit_signal);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_typec_tcpci_override_toggling);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_typec_tcpci_chk_contaminant);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_typec_tcpci_get_vbus);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_typec_tcpm_get_timer);
