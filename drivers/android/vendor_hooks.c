e50cf816b948d10868f4c050d99f38c0d39d436c
Ard Biesheuvel <ardb@google.com>
2021-04-12 13:05:54 +0200

    ANDROID: crypto: lib/aes - add vendor hooks for AES library routines
    Add vendor hooks that will allow the FIPS140 kernel module to override
    the implementations of the AES library routines. The FIPS 140 versions
    are identical to the normal ones, but their code and rodata will have been
    integrity checked at module load time.
    
    Bug: 153614920
    Bug: 188620248
    Change-Id: I5711fc42eced903565fd3c8d41ca7cdd82641148
    Signed-off-by: Ard Biesheuvel <ardb@google.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -310,3 +310,6 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_clear_reserved_fmt_fields);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_fill_ext_fmtdesc);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_clear_mask_adjust);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_sha256);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_aes_expandkey);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_aes_encrypt);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_aes_decrypt);
