5f740ee982721a8455bcced92bb66ae24fcbbfc2
Ard Biesheuvel <ardb@google.com>
2021-04-12 12:51:16 +0200

    ANDROID: crypto: lib/sha256 - add vendor hook for sha256() routine
    Add a vendor hook that will allow the FIPS140 kernel module to override
    the implementation of the sha256() library routine. The FIPS 140 version
    is identical to the normal one, but its code and rodata will have been
    integrity checked at module load time.
    
    Bug: 153614920
    Bug: 188620248
    Change-Id: I8ccc4f0cc8206af39fa922134b438dacac2a614a
    Signed-off-by: Ard Biesheuvel <ardb@google.com>

--- lib/crypto/sha256.c
+++ lib/crypto/sha256.c
@@ -17,6 +17,7 @@
 #include <linux/string.h>
 #include <crypto/sha.h>
 #include <asm/unaligned.h>
+#include <trace/hooks/libsha256.h>
 
 static inline u32 Ch(u32 x, u32 y, u32 z)
 {
@@ -283,6 +284,14 @@ EXPORT_SYMBOL(sha224_final);
 void sha256(const u8 *data, unsigned int len, u8 *out)
 {
 	struct sha256_state sctx;
+
+#if defined(CONFIG_CRYPTO_FIPS140_INTEGRITY) && !defined(BUILD_FIPS140_KO)
+	int ret = 0;
+
+	trace_android_vh_sha256(data, &len, out, &ret);
+	if (ret)
+		return;
+#endif
 
 	sha256_init(&sctx);
 	sha256_update(&sctx, data, len);
