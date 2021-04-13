ab9c01505a935ec5baae5e3950f398b3621ab63a
Mukesh Ojha <mojha@codeaurora.org>
2021-04-13 09:32:41 +0530

    ANDROID: logbuf: vendor hook for logbuf dump on oops
    Initial kernel bootup logs get overwritten after running
    for a long time, and there can be debugging scenario where
    we need initial ~100s bootup logs for debugging.
    'android_vh_initial_logbuf' vendor hook is helping in
    achieving this purpose.
    
    Bug: 185182649
    Change-Id: I706824aeb566c09ecaf4b5900973d6cee8a2f35b
    Signed-off-by: Mukesh Ojha <mojha@codeaurora.org>
    (cherry picked from commit 34fde61f723b3387e12116390fa38540b18ced2e)
    Signed-off-by: Yogesh Lal <ylal@codeaurora.org>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -37,6 +37,7 @@
 #include <trace/hooks/pm_domain.h>
 #include <trace/hooks/cpuidle_psci.h>
 #include <trace/hooks/vmscan.h>
+#include <trace/hooks/logbuf.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -174,3 +175,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_dequeue_entity);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_entity_tick);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_enqueue_task_fair);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_dequeue_task_fair);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_logbuf);
