4a537e8138cf46e60f7740ad60b756b26bf3aa41
xiaofeng <xiaofeng5@xiaomi.com>
2021-04-15 15:02:58 +0800

    ANDROID: vendor_hooks: tune reclaim swappiness or scan type
    Add hooks for reclaim
    
    Bug: 185438290
    Change-Id: Ib9eec302b1df4da7e98c77b94541af28c34a8613
    Signed-off-by: xiaofeng <xiaofeng5@xiaomi.com>
    (cherry picked from commit 35dafe72dd7f7802f941f441f32b9d85efce8803)

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -316,3 +316,5 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_blk_rq_ctx_init);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mmap_region);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_try_to_unmap_one);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_shrink_node_memcgs);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_tune_scan_type);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_tune_swappiness);
