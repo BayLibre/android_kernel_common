c556d7893cf3ade5ac7bd36cc27414225cad8cc6
Quentin Perret <qperret@google.com>
2021-04-23 15:03:03 +0000

    Revert "ANDROID: sched: add vendor hooks to handle scheduling priority"
    This reverts commit 3b773b24e67678aae79072c2d5b7783e7d5dc6a1.
    It is not listed in any symbol list and cannot be used, so let's not
    carry dead code in ACK.
    
    Bug: 163431711
    Bug: 186209366
    Cc: JianMin Liu <jian-min.liu@mediatek.com>
    Signed-off-by: Quentin Perret <qperret@google.com>
    Change-Id: Ie658db3f8d57c6803093cc0ef7a0033a15fdf073

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -27,9 +27,6 @@
 #undef CREATE_TRACE_POINTS
 #include <trace/hooks/dtask.h>
 
-#undef CREATE_TRACE_POINTS
-#include <trace/hooks/sched.h>
-
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
  * associated with them) to allow external modules to probe them.
@@ -2944,7 +2941,6 @@ int sched_fork(unsigned long clone_flags, struct task_struct *p)
 	 * Make sure we do not leak PI boosting priority to the child.
 	 */
 	p->prio = current->normal_prio;
-	trace_android_rvh_prepare_prio_fork(p);
 
 	uclamp_fork(p);
 
@@ -2977,7 +2973,6 @@ int sched_fork(unsigned long clone_flags, struct task_struct *p)
 		p->sched_class = &fair_sched_class;
 
 	init_entity_runnable_average(&p->se);
-	trace_android_rvh_finish_prio_fork(p);
 
 	/*
 	 * The child is not yet in the pid-hash so no cgroup attach races,
@@ -4476,7 +4471,6 @@ void rt_mutex_setprio(struct task_struct *p, struct task_struct *pi_task)
 	struct rq_flags rf;
 	struct rq *rq;
 
-	trace_android_rvh_rtmutex_prepare_setprio(p, pi_task);
 	/* XXX used to be waiter->prio, not waiter->task->prio */
 	prio = __rt_effective_prio(pi_task, p->normal_prio);
 
@@ -4608,7 +4602,6 @@ void set_user_nice(struct task_struct *p, long nice)
 	 */
 	rq = task_rq_lock(p, &rf);
 	update_rq_clock(rq);
-	trace_android_rvh_set_user_nice(p, &nice);
 
 	/*
 	 * The RT priorities are set via sched_setscheduler(), but we still
@@ -4832,8 +4825,6 @@ static void __setscheduler(struct rq *rq, struct task_struct *p,
 		p->sched_class = &rt_sched_class;
 	else
 		p->sched_class = &fair_sched_class;
-
-	trace_android_rvh_setscheduler(p);
 }
 
 /*
