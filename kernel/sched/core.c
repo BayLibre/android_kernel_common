b63a17c1f116b0e3222e9fb89b7403412899e1b6
Quentin Perret <qperret@google.com>
2021-04-23 15:01:32 +0000

    Revert "ANDROID: sched: move vendor hook to check scheduling nice value"
    This reverts commit 66da798d24956e2b2818cbed78589c8227fe7bd2.
    
    Bug: 163431711
    Bug: 186209366
    Cc: Frankie Chang <frankie.chang@mediatek.com>
    Signed-off-by: Quentin Perret <qperret@google.com>
    Change-Id: I6cc89201293b5ec6f1e9740c16589544a5d6110c

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4595,13 +4595,12 @@ static inline int rt_effective_prio(struct task_struct *p, int prio)
 
 void set_user_nice(struct task_struct *p, long nice)
 {
-	bool queued, running, allowed = false;
+	bool queued, running;
 	int old_prio, delta;
 	struct rq_flags rf;
 	struct rq *rq;
 
-	trace_android_rvh_set_user_nice(p, &nice, &allowed);
-	if ((task_nice(p) == nice || nice < MIN_NICE || nice > MAX_NICE) && !allowed)
+	if (task_nice(p) == nice || nice < MIN_NICE || nice > MAX_NICE)
 		return;
 	/*
 	 * We have to be careful, if called from sys_setpriority(),
@@ -4609,6 +4608,7 @@ void set_user_nice(struct task_struct *p, long nice)
 	 */
 	rq = task_rq_lock(p, &rf);
 	update_rq_clock(rq);
+	trace_android_rvh_set_user_nice(p, &nice);
 
 	/*
 	 * The RT priorities are set via sched_setscheduler(), but we still
