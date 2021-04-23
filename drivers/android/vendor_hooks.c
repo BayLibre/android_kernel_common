ba8383318313f2f2413abfcd7d5f9f0037fab577
Quentin Perret <qperret@google.com>
2021-04-23 15:06:20 +0000

    Revert "ANDROID: sched: add restrict vendor hook to modify task placement policy in EAS"
    This reverts commit 93e073732696c4aa81f531dfe84f7d6da44d8f0d.
    It is not listed in any symbol list and cannot be used, so let's not
    carry dead code in ACK.
    
    Bug: 165660734
    Bug: 186209366
    Cc: Yun Hsiang <yun.hsiang@mediatek.com>
    Signed-off-by: Quentin Perret <qperret@google.com>
    Change-Id: Ib38aafc353c6b13b23be3c76517bdbebff5fcaf2

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -8,7 +8,6 @@
 
 #define CREATE_TRACE_POINTS
 #include <trace/hooks/vendor_hooks.h>
-#include <trace/hooks/sched.h>
 #include <trace/hooks/dtask.h>
 #include <trace/hooks/net.h>
 #include <trace/hooks/fpsimd.h>
@@ -24,7 +23,6 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ipi_stop);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_wq_lockup_pool);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ptype_head);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_kfree_skb);
-EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_select_task_rq_fair);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mutex_wait_start);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mutex_wait_finish);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rwsem_read_wait_start);
