de2c97d44ae0376368fc2b1c3b136372fac2c281
wudean <dean.wu@vivo.com>
2021-04-27 17:40:41 +0800

    ANDROID: vendor_hooks: bypass shrink slab
    Add hooks for bypass shrink slab.
    
    Bug: 185951972
    Bug: 395197442
    Change-Id: I343e02ae5cd6d076d525d0e4bfc09ecdfeda1d7b
    Signed-off-by: wudean <dean.wu@vivo.com>
    Signed-off-by: wengle <wengle@oppo.com>
    (cherry picked from commit 6dd826c7924662e8d3fa040c8c414c23b929cea5)

--- include/trace/hooks/vmscan.h
+++ include/trace/hooks/vmscan.h
@@ -22,6 +22,9 @@ DECLARE_HOOK(android_vh_tune_swappiness,
 DECLARE_HOOK(android_vh_async_psi_bypass,
 	TP_PROTO(bool *bypass),
 	TP_ARGS(bypass));
+DECLARE_HOOK(android_vh_shrink_slab_bypass,
+	TP_PROTO(gfp_t gfp_mask, int nid, struct mem_cgroup *memcg, int priority, bool *bypass),
+	TP_ARGS(gfp_mask, nid, memcg, priority, bypass));
 #endif /* _TRACE_HOOK_VMSCAN_H */
 /* This part must be outside protection */
 #include <trace/define_trace.h>
