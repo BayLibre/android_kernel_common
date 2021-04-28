8e4bc764d3a16d84e2264bafc5679922dcaabf07
zhengding chen <chenzhengding@oppo.com>
2021-04-28 19:24:57 +0800

    ANDROID: vendor_hooks: Add hooks for binder reference.
    When servicemanager process added service proxy from other process
    register the service, we want to know the matching relation between
    handle in the process and service name.
    
    Bug: 186604985
    Change-Id: I466ae200cf17bc821c61bd92544a8fbe6b220a25
    Signed-off-by: zhengding chen <chenzhengding@oppo.com>
    Signed-off-by: shenshen mao <maoshenshen@oppo.com>
    Signed-off-by: xiaosa liang <liangxiaosa@oppo.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -451,3 +451,5 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_direct_reclaim_begin);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_direct_reclaim_end);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_do_read_fault);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_bd_link_disk_holder);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_new_ref);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_del_ref);
