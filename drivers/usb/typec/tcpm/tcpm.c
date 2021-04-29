bf5f026b96f191b45f4b666a0ec255197024076f
Kyle Tso <kyletso@google.com>
2021-04-29 22:13:26 +0800

    usb: typec: tcpm: Fix wrong handling for incoming Not_Supported in VDM AMS
    Bug: 174860685
    Signed-off-by: Kyle Tso <kyletso@google.com>
    Change-Id: Ide8dc1df186febf3576172c4f0911656c53a31d9

--- drivers/usb/typec/tcpm/tcpm.c
+++ drivers/usb/typec/tcpm/tcpm.c
@@ -2585,6 +2585,15 @@ static void tcpm_pd_ctrl_request(struct tcpm_port *port,
 			port->sink_cap_done = true;
 			tcpm_set_state(port, ready_state(port), 0);
 			break;
+		case SRC_READY:
+		case SNK_READY:
+			if (port->vdm_state == VDM_STATE_BUSY) {
+				port->vdm_state = VDM_STATE_DONE;
+				port->vdm_sm_running = false;
+				tcpm_ams_finish(port);
+				break;
+			}
+			fallthrough;
 		default:
 			tcpm_pd_handle_state(port,
 					     port->pwr_role == TYPEC_SOURCE ?
