368532f3a52e987b4a4beaa478acf6e5409fd1f3
Liujie Xie <xieliujie@oppo.com>
2021-05-11 19:05:32 +0800

    ANDROID: vendor_hooks: add hooks for slab memory leak debugging
    Add hooks and additional fields in vm_struct and track structs
    to store and report additional information for slab memory leak
    debugging.
    
    Bug: 184928480
    Bug: 395197442
    Change-Id: I6897a6a98d4eaaea492673cefd4111a7ba741940
    Signed-off-by: Liujie Xie <xieliujie@oppo.com>
    Signed-off-by: wengle <wengle@oppo.com>
    Signed-off-by: zhaorou <v-zhaorou@oppo.com>
    (cherry picked from commit 293d51412c2228067345bd9d9804c35987909be4)

--- mm/vmalloc.c
+++ mm/vmalloc.c
@@ -2004,6 +2004,7 @@ static inline void setup_vmalloc_vm(struct vm_struct *vm,
 	vm->size = vm->requested_size = va_size(va);
 	vm->caller = caller;
 	va->vm = vm;
+	trace_android_vh_save_vmalloc_stack(flags, vm);
 }
 
 /*
@@ -5127,6 +5128,7 @@ static int vmalloc_info_show(struct seq_file *m, void *p)
 			if (IS_ENABLED(CONFIG_NUMA))
 				show_numa_info(m, v, counters);
 
+			trace_android_vh_show_stack_hash(m, v);
 			seq_putc(m, '\n');
 		}
 		spin_unlock(&vn->busy.lock);
