52d7b54c949d7eb54af12742c14efa1d2d7eba1f
Siddharth Gupta <sidgup@codeaurora.org>
2021-05-17 16:08:13 -0700

    FROMLIST: remoteproc: core: Invoke subdev callbacks in list order
    Subdevices at the beginning of the subdev list should have
    higher priority than those at the end of the list. Reverse
    traversal of the list causes priority inversion, which can
    impact the performance of the device.
    
    For example a device adds the glink, sysmon and ssr subdevs
    to its list. During a crash the ssr notification would go
    before the glink and sysmon notifications. This can cause a
    degraded response when a client driver waits for a response
    from the crashed rproc.
    
    Signed-off-by: Siddharth Gupta <sidgup@codeaurora.org>
    
    Bug: 188764827
    Link: https://lore.kernel.org/linux-arm-msm/1621292893-18827-1-git-send-email-sidgup@codeaurora.org/
    Change-Id: Iee635c0a229fae58923075c50c3aa259e20718c7
    Signed-off-by: Siddharth Gupta <quic_sidgup@quicinc.com>

--- drivers/remoteproc/remoteproc_core.c
+++ drivers/remoteproc/remoteproc_core.c
@@ -1168,7 +1168,7 @@ static int rproc_handle_resources(struct rproc *rproc,
 
 static int rproc_prepare_subdevices(struct rproc *rproc)
 {
-	struct rproc_subdev *subdev;
+	struct rproc_subdev *subdev, *itr;
 	int ret;
 
 	list_for_each_entry(subdev, &rproc->subdevs, node) {
@@ -1182,9 +1182,11 @@ static int rproc_prepare_subdevices(struct rproc *rproc)
 	return 0;
 
 unroll_preparation:
-	list_for_each_entry_continue_reverse(subdev, &rproc->subdevs, node) {
-		if (subdev->unprepare)
-			subdev->unprepare(subdev);
+	list_for_each_entry(itr, &rproc->subdevs, node) {
+		if (itr == subdev)
+			break;
+		if (itr->unprepare)
+			itr->unprepare(subdev);
 	}
 
 	return ret;
@@ -1192,7 +1194,7 @@ unroll_preparation:
 
 static int rproc_start_subdevices(struct rproc *rproc)
 {
-	struct rproc_subdev *subdev;
+	struct rproc_subdev *subdev, *itr;
 	int ret;
 
 	list_for_each_entry(subdev, &rproc->subdevs, node) {
@@ -1206,9 +1208,11 @@ static int rproc_start_subdevices(struct rproc *rproc)
 	return 0;
 
 unroll_registration:
-	list_for_each_entry_continue_reverse(subdev, &rproc->subdevs, node) {
-		if (subdev->stop)
-			subdev->stop(subdev, true);
+	list_for_each_entry(itr, &rproc->subdevs, node) {
+		if (itr == subdev)
+			break;
+		if (itr->stop)
+			itr->stop(itr, true);
 	}
 
 	return ret;
@@ -1218,7 +1222,7 @@ static void rproc_stop_subdevices(struct rproc *rproc, bool crashed)
 {
 	struct rproc_subdev *subdev;
 
-	list_for_each_entry_reverse(subdev, &rproc->subdevs, node) {
+	list_for_each_entry(subdev, &rproc->subdevs, node) {
 		if (subdev->stop)
 			subdev->stop(subdev, crashed);
 	}
@@ -1228,7 +1232,7 @@ static void rproc_unprepare_subdevices(struct rproc *rproc)
 {
 	struct rproc_subdev *subdev;
 
-	list_for_each_entry_reverse(subdev, &rproc->subdevs, node) {
+	list_for_each_entry(subdev, &rproc->subdevs, node) {
 		if (subdev->unprepare)
 			subdev->unprepare(subdev);
 	}
