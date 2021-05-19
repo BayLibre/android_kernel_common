aac1c188197793391fa9ee8b031d8cbcacef9a4d
qupanwang <qupanwang@xiaomi.com>
2021-05-19 09:59:52 +0800

    ANDROID: power suspend resume get clk info vendor hook
    add mi power resume clk infos for further debug
    
    Bug: 188452033
    
    Signed-off-by: panwang qu <qupanwang@xiaomi.com>
    Change-Id: I6ea27bce049298a9c9e5df9b3c0bde1dc6a52eeb

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -56,6 +56,7 @@
 #include <trace/hooks/psi.h>
 #include <trace/hooks/selinux.h>
 #include <trace/hooks/hung_task.h>
+#include <trace/hooks/mi_power.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -286,3 +287,5 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_cgroup_attach);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mm_dirty_limits);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_check_uninterruptible_tasks);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_check_uninterruptible_tasks_dn);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_clk_debug_init);
+
