fdfea1523fba083e5aff95d104edbc5bccc6c68b
Lokesh Gidra <lokeshgidra@google.com>
2021-05-20 09:24:59 -0700

    BACKPORT: userfaultfd: selftests: add write-protect test
    Add uffd tests for write protection.
    
    Instead of introducing new tests for it, let's simply squashing uffd-wp
    tests into existing uffd-missing test cases.  Changes are:
    
    (1) Bouncing tests
    
      We do the write-protection in two ways during the bouncing test:
    
      - By using UFFDIO_COPY_MODE_WP when resolving MISSING pages: then
        we'll make sure for each bounce process every single page will be
        at least fault twice: once for MISSING, once for WP.
    
      - By direct call UFFDIO_WRITEPROTECT on existing faulted memories:
        To further torture the explicit page protection procedures of
        uffd-wp, we split each bounce procedure into two halves (in the
        background thread): the first half will be MISSING+WP for each
        page as explained above.  After the first half, we write protect
        the faulted region in the background thread to make sure at least
        half of the pages will be write protected again which is the first
        half to test the new UFFDIO_WRITEPROTECT call.  Then we continue
        with the 2nd half, which will contain both MISSING and WP faulting
        tests for the 2nd half and WP-only faults from the 1st half.
    
    (2) Event/Signal test
    
      Mostly previous tests but will do MISSING+WP for each page.  For
      sigbus-mode test we'll need to provide standalone path to handle the
      write protection faults.
    
    For all tests, do statistics as well for uffd-wp pages.
    
    Signed-off-by: Peter Xu <peterx@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Cc: Andrea Arcangeli <aarcange@redhat.com>
    Cc: Bobby Powers <bobbypowers@gmail.com>
    Cc: Brian Geffon <bgeffon@google.com>
    Cc: David Hildenbrand <david@redhat.com>
    Cc: Denis Plotnikov <dplotnikov@virtuozzo.com>
    Cc: "Dr . David Alan Gilbert" <dgilbert@redhat.com>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: Jerome Glisse <jglisse@redhat.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: "Kirill A . Shutemov" <kirill@shutemov.name>
    Cc: Martin Cracauer <cracauer@cons.org>
    Cc: Marty McFadden <mcfadden8@llnl.gov>
    Cc: Maya Gokhale <gokhale2@llnl.gov>
    Cc: Mel Gorman <mgorman@suse.de>
    Cc: Mike Kravetz <mike.kravetz@oracle.com>
    Cc: Mike Rapoport <rppt@linux.vnet.ibm.com>
    Cc: Pavel Emelyanov <xemul@openvz.org>
    Cc: Rik van Riel <riel@redhat.com>
    Cc: Shaohua Li <shli@fb.com>
    Link: http://lkml.kernel.org/r/20200220163112.11409-20-peterx@redhat.com
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit 9b12488a7711b9aa2d0915f6949a8ad2069eb072)
    
    Note: This patch introduces a write-protect test. In addition it also
    introduces uffd_stats_report(), which prints the stats in uniform a
    manner. We only require uffd_stats_report() as subsequent patches depend
    on it. Write-protect test is omitted as this kernel doesn't have
    userfaultfd's write-protect feature.
    Since 'minor_faults' was introduced in the previous patch as a backport,
    we make the corresponding change in uffd_stats_report().
    
    Signed-off-by: Lokesh Gidra <lokeshgidra@google.com>
    Bug: 187930641
    Change-Id: Iad7012e086016e4a67a283c4997d8f7be92eea74

--- tools/testing/selftests/vm/userfaultfd.c
+++ tools/testing/selftests/vm/userfaultfd.c
@@ -146,6 +146,25 @@ static void uffd_stats_reset(struct uffd_stats *uffd_stats,
 	}
 }
 
+static void uffd_stats_report(struct uffd_stats *stats, int n_cpus)
+{
+	int i;
+	unsigned long long miss_total = 0, minor_total = 0;
+
+	for (i = 0; i < n_cpus; i++) {
+		miss_total += stats[i].missing_faults;
+		minor_total += stats[i].minor_faults;
+	}
+
+	printf("userfaults: %llu missing (", miss_total);
+	for (i = 0; i < n_cpus; i++)
+		printf("%lu+", stats[i].missing_faults);
+	printf("\b), %llu minor (", minor_total);
+	for (i = 0; i < n_cpus; i++)
+		printf("%lu+", stats[i].minor_faults);
+	printf("\b)\n");
+}
+
 static int anon_release_pages(char *rel_area)
 {
 	int ret = 0;
@@ -973,7 +992,8 @@ static int userfaultfd_events_test(void)
 		return 1;
 
 	close(uffd);
-	printf("userfaults: %ld\n", stats.missing_faults);
+
+	uffd_stats_report(&stats, 1);
 
 	return stats.missing_faults != nr_pages;
 }
@@ -1136,7 +1156,8 @@ static int userfaultfd_minor_test(void)
 		return 1;
 
 	close(uffd);
-	printf("userfaults: %ld\n", stats.missing_faults + stats.minor_faults);
+
+	uffd_stats_report(&stats, 1);
 
 	return stats.missing_faults != 0 || stats.minor_faults != nr_pages;
 }
@@ -1322,10 +1343,7 @@ static int userfaultfd_stress(void)
 		area_src_alias = area_dst_alias;
 		area_dst_alias = tmp_area;
 
-		printf("userfaults:");
-		for (cpu = 0; cpu < nr_cpus; cpu++)
-			printf(" %lu", uffd_stats[cpu].missing_faults);
-		printf("\n");
+		uffd_stats_report(uffd_stats, nr_cpus);
 	}
 
 	if (err)
