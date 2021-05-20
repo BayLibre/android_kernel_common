5f595c2e2e45949c6458d37fa7132c1ef517484c
Jone Chou <jonechou@google.com>
2021-05-20 10:01:53 +0800

    ANDROID: kernel: Fix debug_kinfo warning message
    Fix warning message as CONFIG_DEBUG_VIRTUAL=y
    virt_to_phys used for non-linear address: (____ptrval____) (swapper_pg_dir+0x0/0x1000)
    
    Bug: 188614550
    Signed-off-by: Jone Chou <jonechou@google.com>
    Change-Id: I14098e6ed3363ae18fa30d4072fc06cc03664c9f

--- drivers/staging/android/debug_kinfo.c
+++ drivers/staging/android/debug_kinfo.c
@@ -153,7 +153,7 @@ static int debug_kinfo_probe(struct platform_device *pdev)
 	info->_token_index_pa = (u64)virt_to_phys((volatile void *)kallsyms_token_index);
 	info->_markers_pa = (u64)virt_to_phys((volatile void *)kallsyms_markers);
 	info->thread_size = THREAD_SIZE;
-	info->swapper_pg_dir_pa = (u64)virt_to_phys(swapper_pg_dir);
+	info->swapper_pg_dir_pa = (u64)__pa_symbol(swapper_pg_dir);
 	strlcpy(info->last_uts_release, init_utsname()->release, sizeof(info->last_uts_release));
 	info->enabled_modules_tree_lookup = IS_ENABLED(CONFIG_MODULES_TREE_LOOKUP);
 	info->mod_core_layout_offset = offsetof(struct module, core_layout);
