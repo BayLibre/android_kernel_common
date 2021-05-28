856e2276587a2f993301dfb14513510803dde0b1
qupanwang <qupanwang@xiaomi.com>
2021-05-28 17:06:25 +0800

    ANDROID: GKI: Add wakeup reason vendor hooks
    add wakeup reason vendor hook info and add some info to print
    
    Bug: 189518444
    
    Signed-off-by: panwang qu <qupanwang@xiaomi.com>
    Change-Id: I0fa47f26f67d81b18613a4d9c5b1fc2d144633c3

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -298,3 +298,6 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_save_vmalloc_stack);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_show_stack_hash);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_save_track_hash);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_set_task_comm);
+
+
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_wakeup_reason_init);
