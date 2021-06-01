1e7924a436d46c2955e2a359c96528d80a5ad665
fengmingli <mingli.feng@vivo.com>
2021-06-01 10:52:19 +0800

    ANDROID: usb: add EXPORT_TRACE_SYMBOL to export tracepoint
    To use the tracepoint in kernel module, add EXPORT_TRACE_SYMBOL_GPL to
    export the dwc3_ctrl_req tracepoint
    
    Bug: 189130101
    
    Change-Id: Ie3245474fbd0cc18c6d41036dcf17c7bbe460814
    Signed-off-by: fengmingli <mingli.feng@vivo.com>
    (cherry picked from commit 0c811ab6f73851ece0203f00f43b8e7458225c5a)

--- drivers/usb/dwc3/trace.c
+++ drivers/usb/dwc3/trace.c
@@ -13,4 +13,9 @@
 EXPORT_TRACEPOINT_SYMBOL_GPL(dwc3_ep_queue);
 EXPORT_TRACEPOINT_SYMBOL_GPL(dwc3_readl);
 EXPORT_TRACEPOINT_SYMBOL_GPL(dwc3_writel);
+<<<<<<< HEAD   (7b90f9 ANDROID: softirq: add tasklet to LONG_SOFTIRQ_MASK)
 EXPORT_TRACEPOINT_SYMBOL_GPL(dwc3_event);
+=======
+EXPORT_TRACEPOINT_SYMBOL_GPL(dwc3_event);
+EXPORT_TRACEPOINT_SYMBOL_GPL(dwc3_ctrl_req);
+>>>>>>> CHANGE (0c811a ANDROID: usb: add EXPORT_TRACE_SYMBOL to export tracepoint)
