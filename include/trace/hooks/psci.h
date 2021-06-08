a315aa89ee41941587626431513bf5ecd528fa20
Maulik Shah <mkshah@codeaurora.org>
2021-06-08 14:33:31 +0530

    ANDROID: gic: Add vendor hook for s2idle enter and exit
    Add vendor hook for s2idle to allow vendor GIC enhancement.
    
    Bug: 190353898
    Change-Id: I02b1393e6119e32eae96010ba5951405dbdada2b
    Signed-off-by: Maulik Shah <mkshah@codeaurora.org>

---
+++ include/trace/hooks/psci.h
@@ -0,0 +1,24 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#undef TRACE_SYSTEM
+#define TRACE_SYSTEM psci
+#define TRACE_INCLUDE_PATH trace/hooks
+#if !defined(_TRACE_HOOK_PSCI_H) || defined(TRACE_HEADER_MULTI_READ)
+#define _TRACE_HOOK_PSCI_H
+#include <linux/tracepoint.h>
+#include <trace/hooks/vendor_hooks.h>
+/*
+ * Following tracepoints are not exported in tracefs and provide a
+ * mechanism for vendor modules to hook and extend functionality
+ */
+
+DECLARE_HOOK(android_vh_s2idle_enter,
+	TP_PROTO(unsigned int cpu),
+	TP_ARGS(cpu));
+
+DECLARE_HOOK(android_vh_s2idle_exit,
+	TP_PROTO(unsigned int cpu),
+	TP_ARGS(cpu));
+
+#endif /* _TRACE_HOOK_PSCI_H */
+/* This part must be outside protection */
+#include <trace/define_trace.h>
