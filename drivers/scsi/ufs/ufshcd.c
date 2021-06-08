bd10cef779c6078cd595835338f068cdfc5b312b
Liujie Xie <xieliujie@oppo.com>
2021-06-08 12:23:13 +0800

    ANDROID: vendor_hooks: Add vendor hooks for recording ufs device infomation and power status
    Add hooks to support oem's getting ufs device information and the status of ufs power mode.
    
    Bug: 190416018
    
    Signed-off-by: Liujie Xie <xieliujie@oppo.com>
    Change-Id: I9af3f81f15287087582ff0aa4e2bc143db61e623

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -7275,6 +7275,7 @@ static int ufshcd_scsi_add_wlus(struct ufs_hba *hba)
 remove_sdev_ufs_device:
 	scsi_remove_device(hba->sdev_ufs_device);
 out:
+	trace_android_vh_ufs_gen_devinfo(hba);
 	return ret;
 }
 
@@ -8504,6 +8505,8 @@ static int ufshcd_set_dev_pwr_mode(struct ufs_hba *hba,
 		if (driver_byte(ret) == DRIVER_SENSE)
 			scsi_print_sense_hdr(sdp, NULL, &sshdr);
 	}
+
+	trace_android_vh_ufs_pwr_mode(hba, pwr_mode);
 
 	if (!ret)
 		hba->curr_dev_pwr_mode = pwr_mode;
