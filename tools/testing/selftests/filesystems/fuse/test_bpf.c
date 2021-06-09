e6a6dd87143565cc7cec6f57c88221ce0f012ae3
Paul Lawrence <paullawrence@google.com>
2021-06-09 13:45:53 -0700

    Build bpf files for testing
    Change-Id: Id6ab6e7a7d16f440fd7f348e28acc73000568866

---
+++ tools/testing/selftests/filesystems/fuse/test_bpf.c
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-2.0 OR BSD-3-Clause
+// Copyright (c) 2021 Google
+
+#include <stdint.h>
+
+#define SEC(NAME) __attribute__((section(NAME), used))
+
+static long (*bpf_trace_printk)(const char *fmt, uint32_t fmt_size, ...)
+	= (void *) 6;
+
+#define bpf_printk(fmt, ...)					\
+	({                                                      \
+		char ____fmt[] = fmt;                           \
+	        bpf_trace_printk(____fmt, sizeof(____fmt),      \
+		                 ##__VA_ARGS__);                \
+	})
+
+SEC("test_trace")
+int trace()
+{
+	bpf_printk("Hello Paul\n");
+	return 1;
+}
+
