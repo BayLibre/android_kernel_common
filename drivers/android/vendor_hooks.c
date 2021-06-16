f920cb8521e00153c5a94269568c575fb5fb95e2
zhang chuang <zhangchuang3@xiaomi.com>
2021-06-16 19:11:38 +0800

    ANDROID: Add hook to show vendor info for transactions
    When watchdog or anr occur, we need to read
    dev/binderfs/binder_logs/proc/pid or dev/binderfs/binder_logs/state
    node to know the time-consuming information of the binder call.
    We need to add the time-consuming information of binder transaction.
    
    Bug: 190413570
    
    Signed-off-by: zhang chuang <zhangchuang3@xiaomi.com>
    Change-Id: I0423d4e821d5cd725a848584133dc7245cbc233a
    (cherry picked from commit eabe9707f264c859d5b0b5b1740a97f8eef97d67)
    Signed-off-by: Carlos Llamas <cmllamas@google.com>
    [cmllamas: fix trivial merge conflict in vendor_hooks.c]
    (cherry picked from commit 1502f0ea7c022dffaf6f25f048a10d021c1e50da)

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -155,3 +155,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_post_init_entity_util_avg);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_uclamp_eff_get);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_util_est_update);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_setscheduler_uclamp);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_print_transaction_info);
