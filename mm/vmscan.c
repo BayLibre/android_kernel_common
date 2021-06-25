998f211bf03e4d7ec21b42ff7e2662e72a0d495d
Liujie Xie <xieliujie@oppo.com>
2021-06-25 22:21:35 +0800

    ANDROID: Allow vendor module to reclaim a memcg
    Export try_to_free_mem_cgroup_pages function to allow vendor modules to reclaim a memory cgroup.
    
    Bug: 192052083
    
    Signed-off-by: Liujie Xie <xieliujie@oppo.com>
    (cherry picked from commit a8385d61f27b57d98fb6245a23477c6ed5db4a7c)
    (cherry picked from commit 1ed025b9a1c8dc1420ccf1a656797b85eacd2bdb)
    
    Change-Id: Iec6ef50f5c71c62d0c9aa6de90e56a143dac61c1
    Signed-off-by: lvwenhuan <lvwenhuan@oppo.com>
    Signed-off-by: huzhanyuan <huzhanyuan@oppo.com>
    (cherry picked from commit 5e18ec3497343e3a5d671c7bc692828f09cbfa19)
    Signed-off-by: yipeng xiang <yipengxiang@honor.corp-partner.google.com>
    (cherry picked from commit 202ad18f829b811890b392aa7551d7eecf11d8b4)
    Signed-off-by: gao xu <gaoxu2@honor.corp-partner.google.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -6758,6 +6758,7 @@ unsigned long try_to_free_mem_cgroup_pages(struct mem_cgroup *memcg,
 	return 0;
 }
 #endif
+EXPORT_SYMBOL_GPL(try_to_free_mem_cgroup_pages);
 
 static void kswapd_age_node(struct pglist_data *pgdat, struct scan_control *sc)
 {
