42f684752ee6a5f090092f579a22558c6af48259
Liujie Xie <xieliujie@oppo.com>
2021-06-25 22:04:36 +0800

    ANDROID: vendor_hooks: add fields in mem_cgroup
    Add more parameters to record more memory info for memory optimization.
    
    Bug: 192052083
    
    Signed-off-by: Liujie Xie <xieliujie@oppo.com>
    Change-Id: I28c8bc1c2455d53e68a05555b57b76ded27af98a

--- include/linux/memcontrol.h
+++ include/linux/memcontrol.h
@@ -331,6 +331,7 @@ struct mem_cgroup {
 	struct deferred_split deferred_split_queue;
 #endif
 
+	ANDROID_VENDOR_DATA_ARRAY(1, 64);
 	struct mem_cgroup_per_node *nodeinfo[0];
 	/* WARNING: nodeinfo must be the last member here */
 };
