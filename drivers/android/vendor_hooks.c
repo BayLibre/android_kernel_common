589ed179fde68efea83257d99f850b73abddccdd
Mukesh Ojha <mojha@codeaurora.org>
2021-06-27 15:26:40 +0530

    ANDROID: logbuf: Add new logbuf vendor hook to support pr_cont()
    Add new logbuf vendor hook android_vh_logbuf_pr_cont()
    to capture pr_cont logs.
    
    Bug: 185182649
    Change-Id: I76b310fc9caac71b344b6cc25ea36f7f81cb7148
    Signed-off-by: Mukesh Ojha <mojha@codeaurora.org>
    (cherry picked from commit 01f2392e13dbdb17d7a22290fba48207059a7f5a)
    Signed-off-by: Yogesh Lal <ylal@codeaurora.org>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -176,3 +176,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_entity_tick);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_enqueue_task_fair);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_dequeue_task_fair);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_logbuf);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_logbuf_pr_cont);
