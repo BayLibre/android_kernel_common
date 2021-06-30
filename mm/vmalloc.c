e355ecba14993313bd607541b70e184605d1633c
Marc Zyngier <maz@kernel.org>
2021-06-30 13:24:32 +0100

    ANDROID: mm/vmalloc: Add arch-specific callbacks to track io{remap,unmap} physical pages
    Add a pair of hooks (ioremap_phys_range_hook/iounmap_phys_range_hook)
    that can be implemented by an architecture. Contrary to the existing
    arch_sync_kernel_mappings(), this one tracks things at the physical
    address level.
    
    This is specially useful in these virtualised environments where
    the guest has to tell the host whether (and how) it intends to use
    a MMIO device.
    
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    
    TODO: tabba@ resolved conflict in remove_vm_area(), placement of
    iounmap_phys_range_hook()
    Bug: 278749606
    Change-Id: I970c2e632cb2b01060d5e66e4194fa9248188f43

--- mm/vmalloc.c
+++ mm/vmalloc.c
@@ -39,6 +39,7 @@
 #include <linux/pgtable.h>
 #include <linux/uaccess.h>
 #include <linux/hugetlb.h>
+#include <linux/io.h>
 #include <linux/sched/mm.h>
 #include <asm/tlbflush.h>
 #include <asm/shmparam.h>
@@ -317,12 +318,17 @@ int ioremap_page_range(unsigned long addr, unsigned long end,
 {
 	int err;
 
-	err = vmap_range_noflush(addr, end, phys_addr, pgprot_nx(prot),
+	prot = pgprot_nx(prot);
+	err = vmap_range_noflush(addr, end, phys_addr, prot,
 				 ioremap_max_page_shift);
 	flush_cache_vmap(addr, end);
 	if (!err)
 		err = kmsan_ioremap_page_range(addr, end, phys_addr, prot,
 					       ioremap_max_page_shift);
+
+	if (IS_ENABLED(CONFIG_ARCH_HAS_IOREMAP_PHYS_HOOKS) && !err)
+		ioremap_phys_range_hook(phys_addr, end - addr, prot);
+
 	return err;
 }
 
@@ -2605,6 +2611,10 @@ struct vm_struct *remove_vm_area(const void *addr)
 
 		va->vm = NULL;
 		spin_unlock(&vmap_area_lock);
+
+		if (IS_ENABLED(CONFIG_ARCH_HAS_IOREMAP_PHYS_HOOKS) &&
+		    vm->flags & VM_IOREMAP)
+			iounmap_phys_range_hook(vm->phys_addr, get_vm_area_size(vm));
 
 		kasan_free_module_shadow(vm);
 		free_unmap_vmap_area(va);
