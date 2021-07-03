d2ff848393f1497a1392f8476dc1ce28f7cd651a
Liujie Xie <xieliujie@oppo.com>
2021-07-03 12:09:04 +0800

    ANDROID: vendor_hooks: add hooks in mem_cgroup subsystem
    Add hooks to tune memory policy based on mem_cgroup.
    
    Bug: 192052083
    
    Signed-off-by: Liujie Xie <xieliujie@oppo.com>
    (cherry picked from commit 1cdcf76b1532ca8092bb6601f45d27c1ed19f448)
    (cherry picked from commit 7af5027889c760a4e02abf7cbd1b95685af4b233)
    
    Change-Id: Ica1a5409eed86fbd466edd2c7557f94972a40175
    Signed-off-by: lvwenhuan <lvwenhuan@oppo.com>

--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -64,7 +64,23 @@ struct slabinfo;
 DECLARE_HOOK(android_vh_cache_show,
 	TP_PROTO(struct seq_file *m, struct slabinfo *sinfo, struct kmem_cache *s),
 	TP_ARGS(m, sinfo, s));
-
+struct mem_cgroup;
+DECLARE_HOOK(android_vh_mem_cgroup_alloc,
+	TP_PROTO(struct mem_cgroup *memcg),
+	TP_ARGS(memcg));
+DECLARE_HOOK(android_vh_mem_cgroup_free,
+	TP_PROTO(struct mem_cgroup *memcg),
+	TP_ARGS(memcg));
+DECLARE_HOOK(android_vh_mem_cgroup_id_remove,
+	TP_PROTO(struct mem_cgroup *memcg),
+	TP_ARGS(memcg));
+struct cgroup_subsys_state;
+DECLARE_HOOK(android_vh_mem_cgroup_css_online,
+	TP_PROTO(struct cgroup_subsys_state *css, struct mem_cgroup *memcg),
+	TP_ARGS(css, memcg));
+DECLARE_HOOK(android_vh_mem_cgroup_css_offline,
+	TP_PROTO(struct cgroup_subsys_state *css, struct mem_cgroup *memcg),
+	TP_ARGS(css, memcg));
 #endif /* _TRACE_HOOK_MM_H */
 
 /* This part must be outside protection */
