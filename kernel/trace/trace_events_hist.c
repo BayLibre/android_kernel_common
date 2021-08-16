dab357ce0ed48e730a1fecfbb70beca535507f71
Kalesh Singh <kaleshsingh@google.com>
2021-08-16 05:02:13 +0000

    tracing: Add division and multiplication support for hist triggers
    Adds basic support for division and multiplication operations for
    hist trigger variable expressions.
    
    For simplicity this patch only supports, division and multiplication
    for a single operation expression (e.g. x=$a/$b), as currently
    expressions are always evaluated right to left. This can lead to some
    incorrect results:
    
    	e.g. echo 'hist:keys=common_pid:a=8,b=4,c=2,x=$a-$b-$c' \
                       >> event/trigger
    
    	     8-4-2 should evaluate to 2 i.e. (8-4)-2
    	     but currently x evaluate to  6 i.e. 8-(4-2)
    
    Multiplication and division in sub-expressions will work correctly, once
    correct operator precedence support is added (See next patch in this
    series).
    
    For the undefined case of division by 0, the histogram expression
    evaluates to (u64)(-1). Since this cannot be detected when the
    expression is created, it is the responsibility of the user to be
    aware and account for this possibility.
    
    Examples:
    	echo 'hist:keys=common_pid:a=8,b=4,x=$a/$b' \
                       >> event/trigger
    
    	echo 'hist:keys=common_pid:a=8,b=4,x=$a*$b' \
                       >> event/trigger
    
    Signed-off-by: Kalesh Singh <kaleshsingh@google.com>
    Change-Id: Ie08cb3f3a3518e24ee3e1413cb72345b5e30bb93

--- kernel/trace/trace_events_hist.c
+++ kernel/trace/trace_events_hist.c
@@ -95,6 +95,8 @@ enum field_op_id {
 	FIELD_OP_PLUS,
 	FIELD_OP_MINUS,
 	FIELD_OP_UNARY_MINUS,
+	FIELD_OP_DIV,
+	FIELD_OP_MULT,
 };
 
 /*
@@ -257,6 +259,40 @@ static u64 hist_field_minus(struct hist_field *hist_field,
 	u64 val2 = operand2->fn(operand2, elt, buffer, rbe, event);
 
 	return val1 - val2;
+}
+
+static u64 hist_field_div(struct hist_field *hist_field,
+			   struct tracing_map_elt *elt,
+			   struct trace_buffer *buffer,
+			   struct ring_buffer_event *rbe,
+			   void *event)
+{
+	struct hist_field *operand1 = hist_field->operands[0];
+	struct hist_field *operand2 = hist_field->operands[1];
+
+	u64 val1 = operand1->fn(operand1, elt, buffer, rbe, event);
+	u64 val2 = operand2->fn(operand2, elt, buffer, rbe, event);
+
+	/* Return -1 for the undefined case */
+	if (!val2)
+		return -1;
+
+	return val1 / val2;
+}
+
+static u64 hist_field_mult(struct hist_field *hist_field,
+			   struct tracing_map_elt *elt,
+			   struct trace_buffer *buffer,
+			   struct ring_buffer_event *rbe,
+			   void *event)
+{
+	struct hist_field *operand1 = hist_field->operands[0];
+	struct hist_field *operand2 = hist_field->operands[1];
+
+	u64 val1 = operand1->fn(operand1, elt, buffer, rbe, event);
+	u64 val2 = operand2->fn(operand2, elt, buffer, rbe, event);
+
+	return val1 * val2;
 }
 
 static u64 hist_field_unary_minus(struct hist_field *hist_field,
@@ -1550,6 +1586,12 @@ static char *expr_str(struct hist_field *field, unsigned int level)
 	case FIELD_OP_PLUS:
 		strcat(expr, "+");
 		break;
+	case FIELD_OP_DIV:
+		strcat(expr, "/");
+		break;
+	case FIELD_OP_MULT:
+		strcat(expr, "*");
+		break;
 	default:
 		kfree(expr);
 		return NULL;
@@ -1565,7 +1607,7 @@ static int contains_operator(char *str)
 	enum field_op_id field_op = FIELD_OP_NONE;
 	char *op;
 
-	op = strpbrk(str, "+-");
+	op = strpbrk(str, "+-/*");
 	if (!op)
 		return FIELD_OP_NONE;
 
@@ -1585,6 +1627,12 @@ static int contains_operator(char *str)
 		break;
 	case '+':
 		field_op = FIELD_OP_PLUS;
+		break;
+	case '/':
+		field_op = FIELD_OP_DIV;
+		break;
+	case '*':
+		field_op = FIELD_OP_MULT;
 		break;
 	default:
 		break;
@@ -2322,8 +2370,24 @@ static struct hist_field *parse_expr(struct hist_trigger_data *hist_data,
 	case FIELD_OP_PLUS:
 		sep = "+";
 		break;
+	case FIELD_OP_DIV:
+		sep = "/";
+		break;
+	case FIELD_OP_MULT:
+		sep = "*";
+		break;
 	default:
 		goto free;
+	}
+
+	/*
+	 * Multiplication and division are only supported is single operator
+	 * expressions, since the expression is always evaluated from right
+	 * to left`
+	 */
+	if ((field_op == FIELD_OP_DIV || field_op == FIELD_OP_MULT) && level > 0) {
+		hist_err(file->tr, HIST_ERR_TOO_MANY_SUBEXPR, errpos(str));
+		return ERR_PTR(-EINVAL);
 	}
 
 	operand1_str = strsep(&str, sep);
@@ -2396,6 +2460,12 @@ static struct hist_field *parse_expr(struct hist_trigger_data *hist_data,
 		break;
 	case FIELD_OP_PLUS:
 		expr->fn = hist_field_plus;
+		break;
+	case FIELD_OP_DIV:
+		expr->fn = hist_field_div;
+		break;
+	case FIELD_OP_MULT:
+		expr->fn = hist_field_mult;
 		break;
 	default:
 		ret = -EINVAL;
