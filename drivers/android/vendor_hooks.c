6129d3f8208afc162847f598fb54824e105635c4
Jiewen Wang <jiewen.wang@vivo.com>
2021-09-06 16:55:03 +0800

    ANDROID: vendor_hooks: Add hook in try_to_unmap_one()
    Add hook in try_to_unmap_one() to trace this function for debug memory
    swap bugs.
    
    Bug: 431139963
    Change-Id: I1fdbe60e09bb491b949e06a07133710453ecca03
    Signed-off-by: Jiewen Wang <jiewen.wang@vivo.com>
    (cherry picked from commit 5a70133febf92e1269f98b533ce9f188c2e3b135)

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -560,3 +560,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_alloc_uid);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_free_user);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_pr_set_vma_name_bypass);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_swap_writepage);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_try_to_unmap_one);
