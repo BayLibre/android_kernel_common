85e7d5036575eac97ffd557840143ef0f382d186
Jiewen Wang <jiewen.wang@vivo.com>
2021-09-06 16:50:35 +0800

    ANDROID: vendor_hooks: Add hook in mmap_region()
    Add hook in mmap_region() to record the vma and address information
    of monitored processes.
    
    Bug: 198385827
    Change-Id: I0bde29113b47ca7f4a9f5d42a54188e791ca3b7e
    Signed-off-by: Jiewen Wang <jiewen.wang@vivo.com>
    (cherry picked from commit 73c9d4a9d575107b90a6d9f415fa56f963264d06)

--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -303,6 +303,9 @@ DECLARE_HOOK(android_vh_try_to_unmap_one,
 DECLARE_HOOK(android_vh_swap_writepage,
 	TP_PROTO(unsigned long *sis_flags, struct page *page),
 	TP_ARGS(sis_flags, page));
+DECLARE_HOOK(android_vh_mmap_region,
+	TP_PROTO(struct vm_area_struct *vma, unsigned long addr),
+	TP_ARGS(vma, addr));
 #endif /* _TRACE_HOOK_MM_H */
 
 /* This part must be outside protection */
