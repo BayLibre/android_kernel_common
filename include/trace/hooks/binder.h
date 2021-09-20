bc0e9879e6e3438f04a652ea025f8d03071d4b65
Shaleen Agrawal <shalagra@codeaurora.org>
2021-09-20 09:21:41 -0700

    ANDROID: sched: Add restricted vendor hook to binder trans
    Need restricted hook so that it can sleep.
    
    Bug: 200542217
    Change-Id: I850410a7c1a97d89f4e9011910f6bb7c2c2a8a9b
    Signed-off-by: Shaleen Agrawal <shalagra@codeaurora.org>

--- include/trace/hooks/binder.h
+++ include/trace/hooks/binder.h
@@ -73,6 +73,13 @@ DECLARE_HOOK(android_vh_binder_print_transaction_info,
 DECLARE_RESTRICTED_HOOK(android_rvh_binder_wakeup_ilocked,
 	TP_PROTO(struct task_struct *task, bool sync, struct binder_proc *proc),
 	TP_ARGS(task, sync, proc), 1);
+DECLARE_RESTRICTED_HOOK(android_rvh_binder_set_priority,
+	TP_PROTO(struct binder_transaction *t, struct task_struct *task),
+	TP_ARGS(t, task), 1);
+DECLARE_RESTRICTED_HOOK(android_rvh_binder_restore_priority,
+	TP_PROTO(struct binder_transaction *t, struct task_struct *task),
+	TP_ARGS(t, task), 1);
+
 /* macro versions of hooks are no longer required */
 
 #endif /* _TRACE_HOOK_BINDER_H */
