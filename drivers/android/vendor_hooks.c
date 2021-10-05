60cfaa95f18821c14e002223a35bb540c5b22adc
Kuan-Ying Lee <Kuan-Ying.Lee@mediatek.com>
2021-10-05 17:46:15 +0800

    ANDROID: syscall_check: add vendor hook for bpf syscall
    Through this vendor hook, we can get the timing to check
    current running task for the validation of its credential
    and bpf operations.
    
    Bug: 191291287
    
    Signed-off-by: Kuan-Ying Lee <Kuan-Ying.Lee@mediatek.com>
    Change-Id: Ie4ed8df7ad66df2486fc7e52a26d9191fc0c176e
    (cherry picked from commit 38abaebab795d87a4e5bbf2ce00e7e37a3db3847)

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -182,3 +182,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_set_module_permit_after_init);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_selinux_is_initialized);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_check_mmap_file);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_check_file_open);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_check_bpf_syscall);
