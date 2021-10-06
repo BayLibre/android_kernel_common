46da6326be8ec446ed7498458c2538fdbd0ddd9d
Chris Goldsworthy <quic_cgoldswo@quicinc.com>
2021-10-05 23:02:29 -0700

    ANDROID: arm64/mm: Add command line option to make ZONE_DMA32 empty
    ZONE_DMA32 is enabled by default on android12-5.10, yet it is not
    needed for all devices, nor is it desirable to have if not needed. For
    instance, if a partner in GKI 1.0 did not use ZONE_DMA32, memory can
    be lower for ZONE_NORMAL relative to older targets, such that memory
    would run out more quickly in ZONE_NORMAL leading kswapd to be invoked
    unnecessarily.
    
    Correspondingly, provide a means of making ZONE_DMA32 empty via the
    kernel command line when it is compiled in via CONFIG_ZONE_DMA32.
    
    Bug: 199917449
    Change-Id: I70ec76914b92e518d61a61072f0b3cb41cb28646
    Signed-off-by: Chris Goldsworthy <quic_cgoldswo@quicinc.com>

--- kernel/dma/pool.c
+++ kernel/dma/pool.c
@@ -56,6 +56,20 @@ static void dma_atomic_pool_size_add(gfp_t gfp, size_t size)
 		pool_size_kernel += size;
 }
 
+static inline bool zone_dma32_is_empty()
+{
+	pg_data_t *pgdat;
+	int node;
+
+	for_each_node(node) {
+		pgdat = NODE_DATA(node);
+		if (zone_is_empty(&pgdat->node_zones[ZONE_DMA32]))
+			return true;
+	}
+
+	return false;
+}
+
 static bool cma_in_zone(gfp_t gfp)
 {
 	unsigned long size;
@@ -74,7 +88,7 @@ static bool cma_in_zone(gfp_t gfp)
 	end = cma_get_base(cma) + size - 1;
 	if (IS_ENABLED(CONFIG_ZONE_DMA) && (gfp & GFP_DMA))
 		return end <= DMA_BIT_MASK(zone_dma_bits);
-	if (IS_ENABLED(CONFIG_ZONE_DMA32) && (gfp & GFP_DMA32))
+	if (IS_ENABLED(CONFIG_ZONE_DMA32) && (gfp & GFP_DMA32) && !zone_dma32_is_empty())
 		return end <= DMA_BIT_MASK(32);
 	return true;
 }
@@ -156,7 +170,7 @@ static void atomic_pool_work_fn(struct work_struct *work)
 	if (IS_ENABLED(CONFIG_ZONE_DMA))
 		atomic_pool_resize(atomic_pool_dma,
 				   GFP_KERNEL | GFP_DMA);
-	if (IS_ENABLED(CONFIG_ZONE_DMA32))
+	if (IS_ENABLED(CONFIG_ZONE_DMA32) && !zone_dma32_is_empty())
 		atomic_pool_resize(atomic_pool_dma32,
 				   GFP_KERNEL | GFP_DMA32);
 	atomic_pool_resize(atomic_pool_kernel, GFP_KERNEL);
@@ -212,7 +226,7 @@ static int __init dma_atomic_pool_init(void)
 		if (!atomic_pool_dma)
 			ret = -ENOMEM;
 	}
-	if (IS_ENABLED(CONFIG_ZONE_DMA32)) {
+	if (IS_ENABLED(CONFIG_ZONE_DMA32) && !zone_dma32_is_empty()) {
 		atomic_pool_dma32 = __dma_atomic_pool_init(atomic_pool_size,
 						GFP_KERNEL | GFP_DMA32);
 		if (!atomic_pool_dma32)
@@ -227,7 +241,7 @@ postcore_initcall(dma_atomic_pool_init);
 static inline struct gen_pool *dma_guess_pool(struct gen_pool *prev, gfp_t gfp)
 {
 	if (prev == NULL) {
-		if (IS_ENABLED(CONFIG_ZONE_DMA32) && (gfp & GFP_DMA32))
+		if (IS_ENABLED(CONFIG_ZONE_DMA32) && (gfp & GFP_DMA32) && !zone_dma32_is_empty())
 			return atomic_pool_dma32;
 		if (IS_ENABLED(CONFIG_ZONE_DMA) && (gfp & GFP_DMA))
 			return atomic_pool_dma;
