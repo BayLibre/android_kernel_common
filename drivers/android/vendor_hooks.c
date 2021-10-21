85318a2bea8a8f8ce64a14ca0a2f0ac4df2cd80b
Shaleen Agrawal <shalagra@codeaurora.org>
2021-10-21 14:30:17 -0700

    ANDROID: arch_topology: Add android_rvh_update_thermal_stats
    Add trace hook so modules can track thermal changes to process
    vendor value adds.
    
    Bug: 203763121
    Change-Id: I5c54b313d3c92c0514f18b07797dd20c9c6f5161
    Signed-off-by: Shaleen Agrawal <shalagra@codeaurora.org>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -404,3 +404,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_read_done);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_preset);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_gic_v2_resume);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_exit_signal);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_update_thermal_stats);
