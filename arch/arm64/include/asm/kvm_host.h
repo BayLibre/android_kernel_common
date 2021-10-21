4183ed870b43246bdb9c70fbf271e67ad8272527
Marc Zyngier <maz@kernel.org>
2021-10-21 14:10:35 +0100

    FROMGIT: KVM: arm64: Introduce flag shadowing TIF_FOREIGN_FPSTATE
    We currently have to maintain a mapping the thread_info structure
    at EL2 in order to be able to check the TIF_FOREIGN_FPSTATE flag.
    
    In order to eventually get rid of this, start with a vcpu flag that
    shadows the thread flag on each entry into the hypervisor.
    
    Reviewed-by: Mark Brown <broonie@kernel.org>
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    (cherry picked from commit af9a0e21d817f40595aa629de32d3bd96582abef
     git://git.kernel.org/pub/scm/linux/kernel/git/kvmarm/kvmarm.git next)
    Signed-off-by: Quentin Perret <qperret@google.com>
    Bug: 209580772
    Change-Id: I72d174eb364f6e66be61ace31de335b224cf20f1

--- arch/arm64/include/asm/kvm_host.h
+++ arch/arm64/include/asm/kvm_host.h
@@ -454,6 +454,7 @@ struct kvm_vcpu_arch {
 
 #define KVM_ARM64_DEBUG_STATE_SAVE_SPE	(1 << 12) /* Save SPE context if active  */
 #define KVM_ARM64_DEBUG_STATE_SAVE_TRBE	(1 << 13) /* Save TRBE context if active  */
+#define KVM_ARM64_FP_FOREIGN_FPSTATE	(1 << 14)
 
 #define vcpu_has_sve(vcpu) (system_supports_sve() &&			\
 			    ((vcpu)->arch.flags & KVM_ARM64_GUEST_HAS_SVE))
@@ -754,6 +755,7 @@ long kvm_vm_ioctl_mte_copy_tags(struct kvm *kvm,
 /* Guest/host FPSIMD coordination helpers */
 int kvm_arch_vcpu_run_map_fp(struct kvm_vcpu *vcpu);
 void kvm_arch_vcpu_load_fp(struct kvm_vcpu *vcpu);
+void kvm_arch_vcpu_ctxflush_fp(struct kvm_vcpu *vcpu);
 void kvm_arch_vcpu_ctxsync_fp(struct kvm_vcpu *vcpu);
 void kvm_arch_vcpu_put_fp(struct kvm_vcpu *vcpu);
 
