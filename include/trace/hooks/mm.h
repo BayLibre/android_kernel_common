85dcbf6b95d13daff268a4abeccc9b7f75efe381
Greg Kroah-Hartman <gregkh@google.com>
2021-10-22 16:19:05 +0200

    Revert "ANDROID: vendor_hooks: Add hook in mmap_region()"
    This reverts commit 878e0caa773151b8a9445f24d3e059d0ece5566d.
    
    The hook android_vh_mmap_region is not used by any vendor, so remove
    it to help with merge issues with future LTS releases.
    
    If this is needed by any real user, it can easily be reverted to add it
    back and then the symbol should be added to the abi list at the same
    time to prevent it from being removed again later.
    
    Bug: 203756332
    Bug: 198385827
    Cc: Jiewen Wang <jiewen.wang@vivo.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: Iaffa6bb13c5e69eb99cf11a7f4b93aac0b8416fb

--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -82,9 +82,6 @@ DECLARE_HOOK(android_vh_vmpressure,
 DECLARE_HOOK(android_vh_kmalloc_slab,
 	TP_PROTO(unsigned int index, gfp_t flags, struct kmem_cache **s),
 	TP_ARGS(index, flags, s));
-DECLARE_HOOK(android_vh_mmap_region,
-	TP_PROTO(struct vm_area_struct *vma, unsigned long addr),
-	TP_ARGS(vma, addr));
 DECLARE_HOOK(android_vh_try_to_unmap_one,
 	TP_PROTO(struct vm_area_struct *vma, struct page *page, unsigned long addr, bool ret),
 	TP_ARGS(vma, page, addr, ret));
