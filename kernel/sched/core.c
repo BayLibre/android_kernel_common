f0766f94c1723fb6600549401afdb003b7763253
Greg Kroah-Hartman <gregkh@google.com>
2021-10-25 10:57:19 +0200

    Revert "ANDROID: sched: add vendor hooks to handle scheduling priority"
    This reverts commit 5aeda5a3ccb1823eec337c21c1e56e19e16b8e94.
    
    The hooks android_rvh_finish_prio_fork, android_rvh_prepare_prio_fork,
    android_rvh_rtmutex_prepare_setprio, android_rvh_set_user_nice, and
    android_rvh_setscheduler, are not used by any vendor, so remove them to
    help with merge issues with future LTS releases.
    
    If this is needed by any real user, it can easily be reverted to add it
    back and then the symbol should be added to the abi list at the same
    time to prevent it from being removed again later.
    
    Bug: 203756332
    Bug: 163431711
    Cc: JianMin Liu <jian-min.liu@mediatek.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I3b93f8138d3c9b9efc15065b144bb144881217dd

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -27,9 +27,6 @@
 #undef CREATE_TRACE_POINTS
 #include <trace/hooks/dtask.h>
 
-#undef CREATE_TRACE_POINTS
-#include <trace/hooks/sched.h>
-
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
  * associated with them) to allow external modules to probe them.
@@ -3052,7 +3049,6 @@ int sched_fork(unsigned long clone_flags, struct task_struct *p)
 	 * Make sure we do not leak PI boosting priority to the child.
 	 */
 	p->prio = current->normal_prio;
-	trace_android_rvh_prepare_prio_fork(p);
 
 	uclamp_fork(p);
 
@@ -3085,7 +3081,6 @@ int sched_fork(unsigned long clone_flags, struct task_struct *p)
 		p->sched_class = &fair_sched_class;
 
 	init_entity_runnable_average(&p->se);
-	trace_android_rvh_finish_prio_fork(p);
 
 	/*
 	 * The child is not yet in the pid-hash so no cgroup attach races,
@@ -4589,7 +4584,6 @@ void rt_mutex_setprio(struct task_struct *p, struct task_struct *pi_task)
 	struct rq_flags rf;
 	struct rq *rq;
 
-	trace_android_rvh_rtmutex_prepare_setprio(p, pi_task);
 	/* XXX used to be waiter->prio, not waiter->task->prio */
 	prio = __rt_effective_prio(pi_task, p->normal_prio);
 
@@ -4713,7 +4707,6 @@ void set_user_nice(struct task_struct *p, long nice)
 	struct rq_flags rf;
 	struct rq *rq;
 
-	trace_android_rvh_set_user_nice(p, &nice, &allowed);
 	if ((task_nice(p) == nice || nice < MIN_NICE || nice > MAX_NICE) && !allowed)
 		return;
 	/*
@@ -4945,8 +4938,6 @@ static void __setscheduler(struct rq *rq, struct task_struct *p,
 		p->sched_class = &rt_sched_class;
 	else
 		p->sched_class = &fair_sched_class;
-
-	trace_android_rvh_setscheduler(p);
 }
 
 /*
