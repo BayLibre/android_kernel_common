0ed4766d19085548a78e3babdb61fa79dbfd3bf7
Stephen Dickey <quic_dickey@quicinc.com>
2021-11-04 15:26:15 -0700

    ANDROID: sched: add hook to rto_next_cpu
    Restricted vendor hook to modify the cpu selected in rto_next_cpu,
    which is needed for the implementation of CPU Pause.
    
    Bug: 200103201
    Change-Id: I0dc675e54f7f116d538840262fbb0ba6d28246f4
    Signed-off-by: Stephen Dickey <quic_dickey@quicinc.com>
    Signed-off-by: Ashay Jaiswal <quic_ashayj@quicinc.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -177,3 +177,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_uclamp_eff_get);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_util_est_update);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_setscheduler_uclamp);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_set_cpus_allowed_ptr_locked);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_rto_next_cpu);
