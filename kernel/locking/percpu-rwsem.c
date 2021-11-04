3a8a3c10a2d3248f74e55bbfa43de0775af1677a
Suren Baghdasaryan <surenb@google.com>
2021-11-04 13:42:56 -0700

    ANDROID: percpu-rwsem: enable percpu_sem destruction in atomic context
    Calling percpu_free_rwsem in atomic context results in "scheduling while
    atomic" bug being triggered:
    
    BUG: scheduling while atomic: klogd/158/0x00000002
    ...
      __schedule_bug+0x191/0x290
      schedule_debug+0x97/0x180
      __schedule+0xdc/0xba0
      schedule+0xda/0x250
      schedule_timeout+0x92/0x2d0
      __wait_for_common+0x25b/0x430
      wait_for_completion+0x1f/0x30
      rcu_barrier+0x440/0x4f0
      rcu_sync_dtor+0xaa/0x190
      percpu_free_rwsem+0x41/0x80
    
    Introduce percpu_rwsem_destroy function to defer percpu_rwsem destruction
    with percpu_free_rwsem being called in a worker thread if called in
    atomic context. When called from non-atomic context the destruction
    will be performed immediately in the current context. percpu_rwsem_destroy
    allows to pass a destructor function in case percpu_rw_semaphore is
    embedded in a structure and can't be freed independently.
    
    Bug: 161210518
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Change-Id: I74ac65c2a9962492cd5002d7a019d2aa13a21a8c

--- kernel/locking/percpu-rwsem.c
+++ kernel/locking/percpu-rwsem.c
@@ -7,6 +7,7 @@
 #include <linux/rcupdate.h>
 #include <linux/sched.h>
 #include <linux/sched/task.h>
+#include <linux/slab.h>
 #include <linux/errno.h>
 
 int __percpu_init_rwsem(struct percpu_rw_semaphore *sem,
@@ -268,3 +269,47 @@ void percpu_up_write(struct percpu_rw_semaphore *sem)
 	rcu_sync_exit(&sem->rss);
 }
 EXPORT_SYMBOL_GPL(percpu_up_write);
+
+static LIST_HEAD(destroy_list);
+static DEFINE_SPINLOCK(destroy_list_lock);
+
+static void destroy_list_workfn(struct work_struct *work)
+{
+	struct percpu_rw_semaphore *sem, *sem2;
+	LIST_HEAD(to_destroy);
+
+	spin_lock(&destroy_list_lock);
+	list_splice_init(&destroy_list, &to_destroy);
+	spin_unlock(&destroy_list_lock);
+
+	if (list_empty(&to_destroy))
+		return;
+
+	list_for_each_entry_safe(sem, sem2, &to_destroy, dtor_list) {
+		percpu_free_rwsem(sem);
+		if (sem->dtor)
+			sem->dtor(sem);
+		else
+			kfree(sem);
+	}
+}
+
+static DECLARE_WORK(destroy_list_work, destroy_list_workfn);
+
+void percpu_rwsem_destroy(struct percpu_rw_semaphore *sem,
+			  void (*dtor)(struct percpu_rw_semaphore *))
+{
+	if (in_atomic()) {
+		spin_lock(&destroy_list_lock);
+		sem->dtor = dtor;
+		list_add_tail(&sem->dtor_list, &destroy_list);
+		spin_unlock(&destroy_list_lock);
+		schedule_work(&destroy_list_work);
+	} else {
+		percpu_free_rwsem(sem);
+		if (dtor)
+			dtor(sem);
+		else
+			kfree(sem);
+	}
+}
