3e1034eb71d619fd23fab2835411eabd9f4d26f0
Stephen Dickey <quic_dickey@quicinc.com>
2021-11-04 15:25:35 -0700

    ANDROID: sched: add hook to set_cpus_allowed_ptr_locked
    It is necessary to update the valid mask to implement CPU Pause
    from a vendor module. Introduce a hook to allow this.
    
    Bug: 200103201
    Change-Id: Ic99b74d17f361453b044e115a54698e566db13b6
    Signed-off-by: Stephen Dickey <quic_dickey@quicinc.com>
    Signed-off-by: Ashay Jaiswal <quic_ashayj@quicinc.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -176,3 +176,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_post_init_entity_util_avg);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_uclamp_eff_get);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_util_est_update);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_setscheduler_uclamp);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_set_cpus_allowed_ptr_locked);
