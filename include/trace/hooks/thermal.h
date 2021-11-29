cbfadc6ed51357fce1ee618299d14fa6bb66c1f5
Jeson Gao <jeson.gao@unisoc.com>
2021-11-29 17:01:50 +0800

    ANDROID: thermal: Add vendor hooks for thermal
    Need to get the request frequency and target frequency
    use it to do frequency check and modify it according to
    the platform thermal requirements.
    
    Bug: 208166320
    
    Signed-off-by: Jeson Gao <jeson.gao@unisoc.com>
    Change-Id: I776b43c8f559b8a072abd8d3abcb3528348b2c5d
    (cherry picked from commit fc827b344f76709dc7f242c7db32e1828f510fdd)

---
+++ include/trace/hooks/thermal.h
@@ -0,0 +1,36 @@
+<<<<<<< HEAD   (405073 ANDROID: ABI: add fips140 symbol list)
+=======
+/* SPDX-License-Identifier: GPL-2.0 */
+#undef TRACE_SYSTEM
+#define TRACE_SYSTEM thermal
+
+#define TRACE_INCLUDE_PATH trace/hooks
+
+#if !defined(_TRACE_HOOK_THERMAL_H) || defined(TRACE_HEADER_MULTI_READ)
+#define _TRACE_HOOK_THERMAL_H
+
+#include <linux/tracepoint.h>
+#include <trace/hooks/vendor_hooks.h>
+
+DECLARE_HOOK(android_vh_enable_thermal_genl_check,
+	TP_PROTO(int event, int tz_id, int *enable_thermal_genl),
+	TP_ARGS(event, tz_id, enable_thermal_genl));
+
+struct thermal_zone_device;
+DECLARE_HOOK(android_vh_thermal_pm_notify_suspend,
+	     TP_PROTO(struct thermal_zone_device *tz, int *irq_wakeable),
+	     TP_ARGS(tz, irq_wakeable));
+
+DECLARE_HOOK(android_vh_modify_thermal_request_freq,
+	TP_PROTO(struct cpufreq_policy *policy, unsigned long *request_freq),
+	TP_ARGS(policy, request_freq));
+
+DECLARE_HOOK(android_vh_modify_thermal_target_freq,
+	TP_PROTO(struct cpufreq_policy *policy, unsigned int *target_freq),
+	TP_ARGS(policy, target_freq));
+
+#endif /* _TRACE_HOOK_THERMAL_H */
+/* This part must be outside protection */
+#include <trace/define_trace.h>
+
+>>>>>>> CHANGE (fc827b ANDROID: thermal: Add vendor hooks for thermal)
