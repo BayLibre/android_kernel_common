24584515b58df11ed7b8a0d8c59320cd63b24f65
Will Deacon <will@kernel.org>
2021-12-09 13:15:41 +0000

    KVM: arm64: Introduce KVM_CAP_ARM_PROTECTED_VM to set/query PVM firmware
    Expose a new capability, KVM_CAP_ARM_PROTECTED_VM, for protected VMs
    which allows the size of the PVM firmware region to be discovered from
    userspace and for the firmware load address to be specified if it is
    required.
    
    Change-Id: I9500f6462e20272038a35c582a4ea9610872372d
    Signed-off-by: Will Deacon <will@kernel.org>

--- include/uapi/linux/kvm.h
+++ include/uapi/linux/kvm.h
@@ -1201,6 +1201,8 @@ struct kvm_ppc_resize_hpt {
 
 #define KVM_CAP_ARM_PROTECTED_VM 0xffbadab1
 
+#define KVM_CAP_ARM_PROTECTED_VM 0xffbadab1
+
 #ifdef KVM_CAP_IRQ_ROUTING
 
 struct kvm_irq_routing_irqchip {
