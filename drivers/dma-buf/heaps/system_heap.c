8abcccab107a68a555b78709aed730a3f3b343d7
liuhailong <liuhailong@oppo.com>
2021-12-16 17:59:47 +0800

    ANDROID: dma-buf: heaps: limit a single allocation to half of total_ram
    android use allocator-svc to allocate dmabuf. If client allocates
    more than total_ram size dma_buf, the service is a native process
    and can not be killed by oom killer or lowmemory killer, it will
    take down to the whole system.
    
    Bug: 210970622
    Change-Id: I192713cd8668dd78bd52086a116afd7eb945f492
    Signed-off-by: liuhailong <liuhailong@oppo.com>

--- drivers/dma-buf/heaps/system_heap.c
+++ drivers/dma-buf/heaps/system_heap.c
@@ -412,6 +412,9 @@ static struct dma_buf *system_heap_do_allocate(struct dma_heap *heap,
 	struct page *page, *tmp_page;
 	int i, ret = -ENOMEM;
 
+	if (len / PAGE_SIZE > totalram_pages() / 2)
+		return ERR_PTR(-ENOMEM);
+
 	buffer = kzalloc(sizeof(*buffer), GFP_KERNEL);
 	if (!buffer)
 		return ERR_PTR(-ENOMEM);
