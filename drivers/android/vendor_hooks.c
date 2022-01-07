75222be2b1a6d5c4304b1b1b29e65a1c96f6e66f
Stephen Dickey <quic_dickey@quicinc.com>
2022-01-06 16:11:49 -0800

    ANDROID: sched: core: hook for get_nohz_timer_target
    Allow module to control behavior of get_nohz_timer_target.
    
    Bug: 200103201
    Change-Id: I38cb201ebf06db7bbce0d6cb68dbbe3729355be8
    Signed-off-by: Stephen Dickey <quic_dickey@quicinc.com>
    Signed-off-by: Ashay Jaiswal <quic_ashayj@quicinc.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -180,3 +180,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_setscheduler_uclamp);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_set_cpus_allowed_ptr_locked);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_rto_next_cpu);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_is_cpu_allowed);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_get_nohz_timer_target);
