a304e5a25dba862f3153ededbbe30a928ab4b202
Arunachalam Ganapathy <arunachalam.ganapathy@arm.com>
2022-01-14 17:52:33 +0000

    ANDROID: trusty: Add transport descriptor
    Use transport descriptor to hold transport specific operations. This
    helps to add new transport to Trusty core.
    
    Bug: 283822781
    Signed-off-by: Arunachalam Ganapathy <arunachalam.ganapathy@arm.com>
    Change-Id: Ibbde50de0302f6d259a7d572f0910067ce712b37

--- drivers/trusty/trusty-private.h
+++ drivers/trusty/trusty-private.h
@@ -17,12 +17,14 @@ struct trusty_work {
 };
 
 struct trusty_msg_ops {
+	const struct trusty_transport_desc *desc;
 	u32 (*send_direct_msg)(struct device *dev, unsigned long fid,
 			       unsigned long a0, unsigned long a1,
 			       unsigned long a2);
 };
 
 struct trusty_mem_ops {
+	const struct trusty_transport_desc *desc;
 	int (*trusty_share_memory)(struct device *dev, u64 *id,
 				   struct scatterlist *sglist,
 				   unsigned int nents, pgprot_t pgprot, u64 tag);
@@ -49,6 +51,19 @@ struct trusty_state {
 	struct device_dma_parameters dma_parms;
 	const struct trusty_msg_ops *msg_ops;
 	const struct trusty_mem_ops *mem_ops;
+	struct trusty_ffa_state *ffa;
+};
+
+struct trusty_ffa_state {
+	struct device *dev; /* ffa device */
+	const struct ffa_dev_ops *ops;
+	struct mutex share_memory_msg_lock; /* protects share_memory_msg */
+};
+
+struct trusty_transport_desc {
+	const char *name;
+	int (*setup)(struct device *dev);
+	void (*cleanup)(struct device *dev);
 };
 
 int trusty_init_api_version(struct trusty_state *s, struct device *dev,
@@ -58,7 +73,6 @@ int trusty_init_api_version(struct trusty_state *s, struct device *dev,
 						   unsigned long a1,
 						   unsigned long a2));
 
-int trusty_smc_transport_setup(struct device *dev);
-void trusty_smc_transport_cleanup(struct device *dev);
+extern const struct trusty_transport_desc trusty_smc_transport;
 
 #endif /* _TRUSTY_PRIVATE_H */
