0a2dc514e23bd70635f119b12a16e221d7a43bcb
Lina Wang <lina.wang@mediatek.com>
2022-02-07 13:54:08 +0800

    FROMLIST: net: fix wrong network header length
    When clatd starts with ebpf offloaing, and NETIF_F_GRO_FRAGLIST is enable,
    several skbs are gathered in skb_shinfo(skb)->frag_list. The first skb's
    ipv6 header will be changed to ipv4 after bpf_skb_proto_6_to_4,
    network_header\transport_header\mac_header have been updated as ipv4 acts,
    but other skbs in frag_list didnot update anything, just ipv6 packets.
    
    udp_queue_rcv_skb will call skb_segment_list to traverse other skbs in
    frag_list and make sure right udp payload is delivered to user space.
    Unfortunately, other skbs in frag_list who are still ipv6 packets are
    updated like the first skb and will have wrong transport header length.
    
    e.g.before bpf_skb_proto_6_to_4,the first skb and other skbs in frag_list
    has the same network_header(24)& transport_header(64), after
    bpf_skb_proto_6_to_4, ipv6 protocol has been changed to ipv4, the first
    skb's network_header is 44,transport_header is 64, other skbs in frag_list
    didnot change.After skb_segment_list, the other skbs in frag_list has
    different network_header(24) and transport_header(44), so there will be 20
    bytes difference,that is difference between ipv6 header and ipv4 header.
    
    Actually, there are two solutions to fix it, one is traversing all skbs
    and changing every skb header in bpf_skb_proto_6_to_4, the other is
    modifying frag_list skb's header in skb_segment_list. Considering
    efficiency, adopt the second one--- when the first skb and other skbs in
    frag_list has different network_header length, restore them to make sure
    right udp payload is delivered to user space.
    
    Bug: 218157620
    
    Signed-off-by: Lina Wang <lina.wang@mediatek.com>
    Change-Id: I0d1ad8c914476e3d44f6bd9aedcd0e66c4c7455b

--- net/core/skbuff.c
+++ net/core/skbuff.c
@@ -3682,6 +3682,7 @@ struct sk_buff *skb_segment_list(struct sk_buff *skb,
 	struct sk_buff *tail = NULL;
 	struct sk_buff *nskb, *tmp;
 	int err;
+	unsigned int len_diff = 0;
 
 	skb_push(skb, -skb_network_offset(skb) + offset);
 
@@ -3721,9 +3722,11 @@ struct sk_buff *skb_segment_list(struct sk_buff *skb,
 		skb_push(nskb, -skb_network_offset(nskb) + offset);
 
 		skb_release_head_state(nskb);
+		len_diff = skb_network_header_len(nskb) - skb_network_header_len(skb);
 		 __copy_skb_header(nskb, skb);
 
 		skb_headers_offset_update(nskb, skb_headroom(nskb) - skb_headroom(skb));
+		nskb->transport_header += len_diff;
 		skb_copy_from_linear_data_offset(skb, -tnl_hlen,
 						 nskb->data - tnl_hlen,
 						 offset + tnl_hlen);
