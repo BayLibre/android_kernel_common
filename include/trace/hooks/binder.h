9a40048c2e20c0ce55c622d5680541cb7d86eabb
Liujie Xie <xieliujie@oppo.com>
2022-02-10 12:11:10 +0800

    ANDROID: vendor_hooks: Add hooks for binder proc transaction
    We need pointers to proc and t, the current hooks 
    in binder_proc_transaction are unable to use.
    
    Bug: 218771978
    Change-Id: I730964f965a015e5f5a3e237d9b3bd084b5bd0d0
    Signed-off-by: Liujie Xie <xieliujie@oppo.com>

--- include/trace/hooks/binder.h
+++ include/trace/hooks/binder.h
@@ -98,6 +98,11 @@ DECLARE_HOOK(android_vh_binder_read_done,
 DECLARE_HOOK(android_vh_binder_has_work_ilocked,
 	TP_PROTO(struct binder_thread *thread, bool do_proc_work, int *ret),
 	TP_ARGS(thread, do_proc_work, ret));
+DECLARE_HOOK(android_vh_binder_proc_transaction_finish,
+	TP_PROTO(struct binder_proc *proc, struct task_struct *binder_th_task,
+	struct binder_transaction *t, bool pending_async, bool sync),
+	TP_ARGS(proc, binder_th_task, t, pending_async, sync));
+
 /* macro versions of hooks are no longer required */
 
 #endif /* _TRACE_HOOK_BINDER_H */
