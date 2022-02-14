1e0cc70c31131c917fc7a46877465fe41462e3b1
David Brazdil <dbrazdil@google.com>
2022-02-14 22:30:37 +0000

    ANDROID: KVM: arm64: iommu: Host stage-2 idmap callbacks
    Add IOMMU callbacks for host stage-2 idmap changes.
    'host_stage2_idmap_prepare' is called first and is expected to apply
    the changes on the driver level, eg. update driver-specific page table
    information. If successful, the generic code invokes
    'host_stage2_idmap_apply' on each currently powered IOMMU device
    associated with the driver to apply the changes.
    [@smostafa]
    Port to new IOMMU layer.
    
    Bug: 190463801
    Bug: 277989609
    Bug: 278749606
    Change-Id: Ifcc063896f6e8967c332dbaa5b7e7f2ba138abbf
    Signed-off-by: David Brazdil <dbrazdil@google.com>
    (cherry picked from commit 4395ddff4bc5ba21935e53861b86e7063447bf85)
    Signed-off-by: Mostafa Saleh <smostafa@google.com>
    Signed-off-by: Quentin Perret <qperret@google.com>

--- arch/arm64/kvm/hyp/nvhe/mem_protect.c
+++ arch/arm64/kvm/hyp/nvhe/mem_protect.c
@@ -16,6 +16,7 @@
 #include <hyp/fault.h>
 
 #include <nvhe/gfp.h>
+#include <nvhe/iommu.h>
 #include <nvhe/memory.h>
 #include <nvhe/mem_protect.h>
 #include <nvhe/mm.h>
@@ -557,8 +558,15 @@ static bool range_is_memory(u64 start, u64 end)
 static inline int __host_stage2_idmap(u64 start, u64 end,
 				      enum kvm_pgtable_prot prot)
 {
-	return kvm_pgtable_stage2_map(&host_mmu.pgt, start, end - start, start,
-				      prot, &host_s2_pool, 0);
+	int ret;
+
+	ret = kvm_pgtable_stage2_map(&host_mmu.pgt, start, end - start, start,
+				     prot, &host_s2_pool, 0);
+	if (ret)
+		return ret;
+
+	kvm_iommu_host_stage2_idmap(start, end, prot);
+	return 0;
 }
 
 /*
@@ -634,14 +642,21 @@ static kvm_pte_t kvm_init_invalid_leaf_owner(u8 owner_id)
 int host_stage2_set_owner_locked(phys_addr_t addr, u64 size, u8 owner_id)
 {
 	kvm_pte_t annotation;
+	enum kvm_pgtable_prot prot;
+	int ret;
 
 	if (owner_id > KVM_MAX_OWNER_ID)
 		return -EINVAL;
 
 	annotation = kvm_init_invalid_leaf_owner(owner_id);
 
-	return host_stage2_try(kvm_pgtable_stage2_annotate, &host_mmu.pgt,
-			       addr, size, &host_s2_pool, annotation);
+	ret = host_stage2_try(kvm_pgtable_stage2_annotate, &host_mmu.pgt,
+			      addr, size, &host_s2_pool, annotation);
+	if (ret)
+		return ret;
+	prot = owner_id == PKVM_ID_HOST ? PKVM_HOST_MEM_PROT : 0;
+	kvm_iommu_host_stage2_idmap(addr, addr + size, prot);
+	return 0;
 }
 
 static bool host_stage2_force_pte(u64 addr, u64 end, enum kvm_pgtable_prot prot)
