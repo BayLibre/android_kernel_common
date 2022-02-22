633720b0faf0795802dc13b24e64e10d516dbbd7
Liujie Xie <xieliujie@oppo.com>
2022-02-22 19:29:53 +0800

    ANDROID: vendor_hooks: Add hooks for __alloc_pages_direct_reclaim
    Provide a vendor hook to allow drain_all_pages to be skipped
    during direct reclaim in some cases to avoid delays caused by
    it in cases when the benefits of draining pcp lists are known
    to be small.
    
    Bug: 220811627
    Bug: 280918869
    Signed-off-by: Liujie Xie <xieliujie@oppo.com>
    Change-Id: I0805241f81e0a94afcf62c98e97cff125d4061e2

--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -24,6 +24,11 @@ DECLARE_RESTRICTED_HOOK(android_rvh_set_readahead_gfp_mask,
 			TP_PROTO(unsigned int *flags),	/* gfp_t *flags */
 			TP_ARGS(flags), 1);
 
+DECLARE_HOOK(android_vh_drain_all_pages_bypass,
+	TP_PROTO(gfp_t gfp_mask, unsigned int order, unsigned long alloc_flags,
+		int migratetype, unsigned long did_some_progress,
+		bool *bypass),
+	TP_ARGS(gfp_mask, order, alloc_flags, migratetype, did_some_progress, bypass));
 #endif /* _TRACE_HOOK_MM_H */
 
 /* This part must be outside protection */
