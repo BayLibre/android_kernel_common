5090b7a5e94060bba983391bdf68011f1ddbc563
David Brazdil <dbrazdil@google.com>
2022-02-23 19:54:47 +0000

    ANDROID: KVM: arm64: Remove unused IOMMU hooks, kvm_iommu_ops
    With new generic IOMMU code in place, and with all S2MPU code
    having been migrated to the new pkvm_iommu_ops callbacks, remove
    all the now unused code.
    
    Bug: 190463801
    Change-Id: I83a1ac7c2e30ee76712079727da4607bda5de68a
    Signed-off-by: David Brazdil <dbrazdil@google.com>
    (cherry picked from commit e900628cfca812f9f4c4c93754d91be16be4e466)
    Signed-off-by: Mostafa Saleh <smostafa@google.com>

--- arch/arm64/kvm/hyp/nvhe/hyp-main.c
+++ arch/arm64/kvm/hyp/nvhe/hyp-main.c
@@ -36,8 +36,6 @@ static DEFINE_PER_CPU(struct user_fpsimd_state, loaded_host_fpsimd_state);
 
 DEFINE_PER_CPU(struct kvm_nvhe_init_params, kvm_init_params);
 
-struct kvm_iommu_ops kvm_iommu_ops;
-
 void __kvm_hyp_host_forward_smc(struct kvm_cpu_context *host_ctxt);
 
 static int pkvm_refill_memcache(struct pkvm_hyp_vcpu *hyp_vcpu)
@@ -1013,7 +1011,6 @@ static void handle___pkvm_init(struct kvm_cpu_context *host_ctxt)
 	DECLARE_REG(unsigned long, nr_cpus, host_ctxt, 3);
 	DECLARE_REG(unsigned long *, per_cpu_base, host_ctxt, 4);
 	DECLARE_REG(u32, hyp_va_bits, host_ctxt, 5);
-	DECLARE_REG(enum kvm_iommu_driver, iommu_driver, host_ctxt, 6);
 
 	/*
 	 * __pkvm_init() will return only if an error occurred, otherwise it
@@ -1021,7 +1018,7 @@ static void handle___pkvm_init(struct kvm_cpu_context *host_ctxt)
 	 * with the host context directly.
 	 */
 	cpu_reg(host_ctxt, 1) = __pkvm_init(phys, size, nr_cpus, per_cpu_base,
-					    hyp_va_bits, iommu_driver);
+					    hyp_va_bits);
 }
 
 static void handle___pkvm_cpu_set_vector(struct kvm_cpu_context *host_ctxt)
@@ -1225,8 +1222,6 @@ static void handle_host_smc(struct kvm_cpu_context *host_ctxt)
 	bool handled;
 
 	handled = kvm_host_psci_handler(host_ctxt);
-	if (!handled && kvm_iommu_ops.host_smc_handler)
-		handled = kvm_iommu_ops.host_smc_handler(host_ctxt);
 	if (!handled)
 		default_host_smc_handler(host_ctxt);
 
