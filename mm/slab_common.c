97154b66399ddbc30e61f1f205be103f4e600cea
Catalin Marinas <catalin.marinas@arm.com>
2022-03-26 21:00:17 +0000

    mm/slab: Simplify create_kmalloc_cache() args and make it static
    create_kmalloc_cache() is now only called from new_kmalloc_cache() in
    the same file, so make it static. In addition, the useroffset argument
    is always 0 while usersize is the same as size. Remove them.
    
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Change-Id: I854839ab8448f9491564ee15e82f4ca55ed7f3fb
    Signed-off-by: Isaac J. Manjarres <isaacmanjarres@google.com>

--- mm/slab_common.c
+++ mm/slab_common.c
@@ -1096,16 +1096,15 @@ void __init create_boot_cache(struct kmem_cache *s, const char *name,
 	s->refcount = -1;	/* Exempt from merging for now */
 }
 
-struct kmem_cache *__init create_kmalloc_cache(const char *name,
-		unsigned int size, slab_flags_t flags,
-		unsigned int useroffset, unsigned int usersize)
+static struct kmem_cache *__init create_kmalloc_cache(const char *name,
+		unsigned int size, slab_flags_t flags)
 {
 	struct kmem_cache *s = kmem_cache_zalloc(kmem_cache, GFP_NOWAIT);
 
 	if (!s)
 		panic("Out of memory when creating slab %s\n", name);
 
-	create_boot_cache(s, name, size, flags, useroffset, usersize);
+	create_boot_cache(s, name, size, flags, 0, size);
 	list_add(&s->list, &slab_caches);
 	memcg_link_cache(s, NULL);
 	s->refcount = 1;
@@ -1292,8 +1291,7 @@ new_kmalloc_cache(int idx, int type, slab_flags_t flags)
 
 	if (!kmalloc_caches[type][aligned_idx])
 		kmalloc_caches[type][aligned_idx] = create_kmalloc_cache(
-					name, aligned_size, flags, 0,
-					aligned_size);
+					name, aligned_size, flags);
 	if (idx != aligned_idx)
 		kmalloc_caches[type][idx] = kmalloc_caches[type][aligned_idx];
 }
