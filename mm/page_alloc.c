408d8c20833288bf35150c475e7c30c0fcc91e52
Peifeng Li <lipeifeng@oppo.com>
2022-03-26 20:43:40 +0800

    ANDROID: vendor_hooks: Add hooks to for free_unref_page_commit
    Provide a vendor hook to skip cma-pages to add in pcplist when
    free_unref_page_commit.
    
    The patch is revelant to skip drain_all_pages in alloc_contig_range,
    the revelant hooks is android_vh_cma_drain_all_pages_bypass
    which is to avoid to delay in drain pcppages when drain_all_pages.
    
    In most case, pcp->high is small so that free-pages with other mt_types
    can also fill with pcplist full.
    
    Bug: 224732340
    Bug: 280918869
    Signed-off-by: Peifeng Li <lipeifeng@oppo.com>
    Change-Id: Ifdeeed9f8934d87671ec3fa6787a02675b993082

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -3559,6 +3559,7 @@ void free_unref_page(struct page *page, unsigned int order)
 	struct zone *zone;
 	unsigned long pfn = page_to_pfn(page);
 	int migratetype;
+	bool pcp_skip_cma_pages = false;
 
 	if (!free_unref_page_prepare(page, pfn, order))
 		return;
@@ -3572,7 +3573,10 @@ void free_unref_page(struct page *page, unsigned int order)
 	 */
 	migratetype = get_pcppage_migratetype(page);
 	if (unlikely(migratetype >= MIGRATE_PCPTYPES)) {
-		if (unlikely(is_migrate_isolate(migratetype))) {
+		trace_android_vh_pcplist_add_cma_pages_bypass(migratetype,
+			&pcp_skip_cma_pages);
+		if (unlikely(is_migrate_isolate(migratetype)) ||
+			pcp_skip_cma_pages) {
 			free_one_page(page_zone(page), page, pfn, order, migratetype, FPI_NONE);
 			return;
 		}
@@ -3602,6 +3606,7 @@ void free_unref_page_list(struct list_head *list)
 	unsigned long flags;
 	int batch_count = 0;
 	int migratetype;
+	bool pcp_skip_cma_pages = false;
 
 	/* Prepare pages for freeing */
 	list_for_each_entry_safe(page, next, list, lru) {
@@ -3616,7 +3621,10 @@ void free_unref_page_list(struct list_head *list)
 		 * comment in free_unref_page.
 		 */
 		migratetype = get_pcppage_migratetype(page);
-		if (unlikely(is_migrate_isolate(migratetype))) {
+		trace_android_vh_pcplist_add_cma_pages_bypass(migratetype,
+			&pcp_skip_cma_pages);
+		if (unlikely(is_migrate_isolate(migratetype)) ||
+			pcp_skip_cma_pages) {
 			list_del(&page->lru);
 			free_one_page(page_zone(page), page, pfn, 0, migratetype, FPI_NONE);
 			continue;
