e34dcb869164aa4c81942d450d6a794b25eae5bb
Quentin Perret <qperret@google.com>
2022-03-28 13:37:28 +0100

    ANDROID: KVM: arm64: Do not allow memslot modifications in protected mode
    Currently trying to move or delete a memslot results in a warning
    and a failure. Userspace shouldn't be able to trigger kernel
    warnings.
    
    Bug: 226890762
    Signed-off-by: Quentin Perret <qperret@google.com>
    Signed-off-by: Fuad Tabba <tabba@google.com>
    Change-Id: I75c03e1d57c06c6e357bca33e9784233d61203f3

--- virt/kvm/kvm_main.c
+++ virt/kvm/kvm_main.c
@@ -1521,6 +1521,14 @@ static struct kvm_memslots *kvm_dup_memslots(struct kvm_memslots *old,
 	return slots;
 }
 
+int __weak kvm_arch_check_set_memslot(struct kvm *kvm,
+				      struct kvm_memory_slot *old,
+				      struct kvm_memory_slot *new,
+				      enum kvm_mr_change change)
+{
+	return 0;
+}
+
 static int kvm_set_memslot(struct kvm *kvm,
 			   const struct kvm_userspace_memory_region *mem,
 			   struct kvm_memory_slot *new, int as_id,
@@ -1545,6 +1553,13 @@ static int kvm_set_memslot(struct kvm *kvm,
 	 * changes made to the previous memslots.
 	 */
 	mutex_lock(&kvm->slots_arch_lock);
+
+	/* Give architecture-specific code a chance to reject the change. */
+	r = kvm_arch_check_set_memslot(kvm, old, new, change);
+	if (r) {
+		mutex_unlock(&kvm->slots_arch_lock);
+		return r;
+	}
 
 	slots = kvm_dup_memslots(__kvm_memslots(kvm, as_id), change);
 	if (!slots) {
