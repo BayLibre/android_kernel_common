14f4fbd32cf7c83d351a7ee5c370073012db5be2
Greg Kroah-Hartman <gregkh@google.com>
2022-03-31 19:11:31 +0200

    Revert "ANDROID: vendor_hooks: add waiting information for blocked tasks"
    This reverts commit 9ad8ff902e39f37baf15904c6627ca03e8f60c27.  It causes
    merge issues with 5.18-rc1 and has to be reverted for now.  If it is
    still needed, it can be added back after 5.18-rc1.
    
    Bug: 162776704
    Cc: Sangmoon Kim <sangmoon.kim@samsung.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I2b2a575b0d4e751bd4bbe969bdfc77c01bb8dac5

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -34,7 +34,6 @@
 #include <linux/rseq.h>
 #include <linux/seqlock.h>
 #include <linux/kcsan.h>
-#include <linux/android_vendor.h>
 #include <asm/kmap_size.h>
 
 /* task_struct member predeclarations (sorted alphabetically): */
@@ -1489,8 +1488,6 @@ struct task_struct {
 	struct callback_head		mce_kill_me;
 	int				mce_count;
 #endif
-	ANDROID_VENDOR_DATA_ARRAY(1, 64);
-	ANDROID_OEM_DATA_ARRAY(1, 6);
 
 #ifdef CONFIG_KRETPROBES
 	struct llist_head               kretprobe_instances;
