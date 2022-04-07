d6de34d8f4d12aec3baf8d512411372b98e1a868
Venkata Rao Kakani <vkakani@codeaurora.org>
2022-04-07 12:38:38 +0530

    Enable console on ACK kernel to debug early kernel boot issue
    Change-Id: I97ded296f02d4c065c048178a35b8a65a63f17dd
    Signed-off-by: Venkata Rao Kakani <vkakani@codeaurora.org>

--- include/linux/dma-mapping.h
+++ include/linux/dma-mapping.h
@@ -141,6 +141,7 @@ struct dma_map_ops {
 };
 
 #define DMA_MAPPING_ERROR		(~(dma_addr_t)0)
+#define DMA_ERROR_CODE       (~(dma_addr_t)0)
 
 extern const struct dma_map_ops dma_virt_ops;
 extern const struct dma_map_ops dma_dummy_ops;
