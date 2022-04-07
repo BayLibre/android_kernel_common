45affc717651a23fbd844084f9ead68c90848c01
Di Shen <di.shen@unisoc.com>
2022-04-07 19:46:18 +0800

    ANDROID: thermal: Add vendor hook to get thermal zone device
    Need to get temperature data and config info from thermal zone device.
    
    Bug: 208946028
    
    Signed-off-by: Di Shen <di.shen@unisoc.com>
    Signed-off-by: Jeson Gao <jeson.gao@unisoc.com>
    Change-Id: I5945df5258181b4a441b6bbe09327099491418b3
    (cherry picked from commit c53f0e3530641d6b696bae12eed55c58599ab516)
    (cherry picked from commit 12b8ef18b20dde557e696b6898c19d7353dcc90c)

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -255,3 +255,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_modify_thermal_request_freq);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_modify_thermal_target_freq);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_thermal_register);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_thermal_unregister);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_get_thermal_zone_device);
