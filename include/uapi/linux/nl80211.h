a1dd60fec852f026329e96db18202825c1a30f36
Johannes Berg <johannes.berg@intel.com>
2022-04-14 16:50:57 +0200

    BACKPORT: FROMLIST: cfg80211: do some rework towards MLO link APIs
    In order to support multi-link operation with multiple links,
    start adding some APIs. The notable addition here is to have
    the link ID in a new nl80211 attribute, that will be used to
    differentiate the links in many nl80211 operations.
    
    So far, this patch adds the netlink NL80211_ATTR_MLO_LINK_ID
    attribute (as well as the NL80211_ATTR_MLO_LINKS attribute)
    and plugs it through the system in some places, checking the
    validity etc. along with other infrastructure needed for it.
    
    For now, I've decided to include only the over-the-air link
    ID in the API. I know we discussed that we eventually need to
    have to have other ways of identifying a link, but for local
    AP mode and auth/assoc commands as well as set_key etc. we'll
    use the OTA ID.
    
    Also included in this patch is some refactoring of the data
    structures in struct wireless_dev, splitting for the first
    time the data into type dependent pieces, to make reasoning
    about these things easier.
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    
    Bug: 233387627
    Change-Id: I023f35d382282691d7e2cbc607fb11691355cd63
    Link: https://patchwork.kernel.org/project/linux-wireless/patch/20220520132938.d45ac277fe08.I023f35d382282691d7e2cbc607fb11691355cd63@changeid/
    Signed-off-by: Veerendranath Jakkam <quic_vjakkam@quicinc.com>

--- include/uapi/linux/nl80211.h
+++ include/uapi/linux/nl80211.h
@@ -324,6 +324,17 @@
  */
 
 /**
+ * DOC: Multi-Link Operation
+ *
+ * In Multi-Link Operation, a connection between to MLDs utilizes multiple
+ * links. To use this in nl80211, various commands and responses now need
+ * to or will include the new %NL80211_ATTR_MLO_LINKS attribute.
+ * Additionally, various commands that need to operate on a specific link
+ * now need to be given the %NL80211_ATTR_MLO_LINK_ID attribute, e.g. to
+ * use %NL80211_CMD_START_AP or similar functions.
+ */
+
+/**
  * enum nl80211_commands - supported nl80211 commands
  *
  * @NL80211_CMD_UNSPEC: unspecified command to catch errors
@@ -1237,6 +1248,12 @@
  *      to describe the BSSID address of the AP and %NL80211_ATTR_TIMEOUT to
  *      specify the timeout value.
  *
+ * @NL80211_CMD_ADD_LINK: Add a new link to an interface. The
+ *	%NL80211_ATTR_MLO_LINK_ID attribute is used for the new link.
+ * @NL80211_CMD_REMOVE_LINK: Remove a link from an interface. This may come
+ *	without %NL80211_ATTR_MLO_LINK_ID as an easy way to remove all links
+ *	in preparation for e.g. roaming to a regular (non-MLO) AP.
+ *
  * @NL80211_CMD_MAX: highest used command number
  * @__NL80211_CMD_AFTER_LAST: internal use
  */
@@ -1498,6 +1515,9 @@ enum nl80211_commands {
 	__NL80211_CMD_AFTER_LAST,
 	NL80211_CMD_MAX = __NL80211_CMD_AFTER_LAST - 1
 };
+
+#define NL80211_CMD_ADD_LINK NL80211_CMD_RESERVED_DO_NOT_USE_1
+#define NL80211_CMD_REMOVE_LINK NL80211_CMD_RESERVED_DO_NOT_USE_2
 
 /*
  * Allow user space programs to use #ifdef on new commands by defining them
@@ -2681,6 +2701,11 @@ enum nl80211_commands {
  *	suites allowed as %NL80211_MAX_NR_AKM_SUITES which is the legacy maximum
  *	number prior to the introduction of this attribute.
  *
+ * @NL80211_ATTR_MLO_LINK_ID: A (u8) link ID for use with MLO, to be used with
+ *	various commands that need a link ID to operate.
+ * @NL80211_ATTR_MLO_LINKS: A nested array of links, each containing some
+ *	per-link information and a link ID.
+ *
  * @NUM_NL80211_ATTR: total number of nl80211_attrs available
  * @NL80211_ATTR_MAX: highest attribute number currently defined
  * @__NL80211_ATTR_AFTER_LAST: internal use
@@ -3224,6 +3249,8 @@ enum nl80211_attrs {
 };
 
 #define NL80211_ATTR_MAX_NUM_AKM_SUITES NL80211_ATTR_RESERVED_DO_NOT_USE_1
+#define NL80211_ATTR_MLO_LINKS NL80211_ATTR_RESERVED_DO_NOT_USE_2
+#define NL80211_ATTR_MLO_LINK_ID NL80211_ATTR_RESERVED_DO_NOT_USE_3
 
 /* source-level API compatibility */
 #define NL80211_ATTR_SCAN_GENERATION NL80211_ATTR_GENERATION
