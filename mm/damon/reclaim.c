467c8dd4275f184cc267d4b55a7e1448876f8639
Hailong Tu <tuhailong@oppo.com>
2022-04-20 16:37:15 +0800

    FROMLIST: mm/damon: Fix the timer always stays active
    The timer stays active even if the reclaim mechanism is never enabled.
    It is unnecessary overhead can be completely avoided by using module_param_call() for enabled flag.
    
    Signed-off-by: Hailong Tu <tuhailong@gmail.com>
    
    Link: https://patchwork.kernel.org/project/linux-mm/patch/20220421010640.383365-1-tuhailong@gmail.com/
    
    Bug: 228223814
    Signed-off-by: Hailong Tu <tuhailong@oppo.com>
    Change-Id: I77591e41ce424ce16a4a5c70e7a86cdae996a354

--- mm/damon/reclaim.c
+++ mm/damon/reclaim.c
@@ -28,7 +28,6 @@
  * this.
  */
 static bool enabled __read_mostly;
-module_param(enabled, bool, 0600);
 
 /*
  * Time threshold for cold memory regions identification in microseconds.
@@ -358,10 +357,31 @@ static void damon_reclaim_timer_fn(struct work_struct *work)
 			enabled = last_enabled;
 	}
 
-	schedule_delayed_work(&damon_reclaim_timer,
+	if (enabled)
+		schedule_delayed_work(&damon_reclaim_timer,
 			msecs_to_jiffies(ENABLE_CHECK_INTERVAL_MS));
 }
 static DECLARE_DELAYED_WORK(damon_reclaim_timer, damon_reclaim_timer_fn);
+
+static int enabled_show(char *buffer, const struct kernel_param *kp)
+{
+	return sprintf(buffer, "%c\n", enabled ? 'Y' : 'N');
+}
+
+static int enabled_store(const char *val,
+		const struct kernel_param *kp)
+{
+	int rc = param_set_bool(val, kp);
+
+	if (rc < 0)
+		return rc;
+
+	if (enabled)
+		schedule_delayed_work(&damon_reclaim_timer, 0);
+
+	return 0;
+}
+module_param_call(enabled, enabled_store, enabled_show, &enabled, 0600);
 
 static int damon_reclaim_after_aggregation(struct damon_ctx *c)
 {
