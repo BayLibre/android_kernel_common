b2c441c6bc1f697fdf60eccb2e8ba22e3a61c9fb
Robin Hsu <robinhsu@google.com>
2022-05-13 10:19:50 +0800

    ANDROID: mm: vh for compaction begin/end
    Add vendor hook for compaction begin/end.  The first use would be
    to measure compaction durations.
    
    Bug: 229927848
    Test: echo 1 > /proc/sys/vm/compact_memory and observe output change in
    /sys/kernel/pixel_stat/mm/compaction/mm_compaction_duration
    Signed-off-by: Robin Hsu <robinhsu@google.com>
    Change-Id: I3d95434bf49b37199056dc9ddfc36a59a7de17b7
    Signed-off-by: Richard Chang <richardycc@google.com>
    (cherry picked from commit 13b6bd38bb1f43bfffdb08c8f3a4a20d36ccd670)
    Signed-off-by: liangjlee <liangjlee@google.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -613,3 +613,5 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_dma_buf_release);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_send_signal_locked);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rmqueue);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_filemap_get_folio);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mm_compaction_begin);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mm_compaction_end);
