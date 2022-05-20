cf42b68e50139d97a483550f118a8b1a44211315
Daniel Rosenberg <drosen@google.com>
2022-05-19 17:09:08 -0700

    ANDROID: fuse-bpf: Add BPF supporting functions
    This adds support for verifying fuse-bpf programs. These programs are
    not permitted to make any changes to their contexts unless they request
    the access via fuse_get_writeable_in/fuse_get_writeable_out. These
    return a buffer, either to the preexisting buffer, or a newly allocated
    one which will replace the preexisting buffer. The caller of the bpf
    program is responsible for cleaning up these allocations, and is
    notified via the flags set by the helper.
    
    Bug: 217569456
    Change-Id: I467fb1f2e82f695c13213ca3fa678b700ad961f6
    Signed-off-by: Daniel Rosenberg <drosen@google.com>

--- include/uapi/linux/bpf.h
+++ include/uapi/linux/bpf.h
@@ -5456,6 +5456,8 @@ union bpf_attr {
 	FN(dynptr_read),		\
 	FN(dynptr_write),		\
 	FN(dynptr_data),		\
+	FN(fuse_get_writeable_in),	\
+	FN(fuse_get_writeable_out),	\
 	/* */
 
 /* integer value in 'imm' field of BPF_CALL instruction selects which helper
