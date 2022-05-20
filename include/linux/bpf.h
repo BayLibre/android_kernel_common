91e639d83cde289112edc2c6089d260a64683ad6
Daniel Rosenberg <drosen@google.com>
2022-05-19 17:09:08 -0700

    ANDROID: fuse-bpf: Add BPF supporting functions
    This adds support for verifying fuse-bpf programs. These programs are
    not permitted to make any changes to their contexts unless they request
    the access via fuse_get_writeable_in/fuse_get_writeable_out. These
    return a buffer, either to the preexisting buffer, or a newly allocated
    one which will replace the preexisting buffer. The caller of the bpf
    program is responsible for cleaning up these allocations, and is
    notified via the flags set by the helper.
    
    Bug: 217569456
    Change-Id: I467fb1f2e82f695c13213ca3fa678b700ad961f6
    Signed-off-by: Daniel Rosenberg <drosen@google.com>

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -2269,6 +2269,8 @@ extern const struct bpf_func_proto bpf_loop_proto;
 extern const struct bpf_func_proto bpf_strncmp_proto;
 extern const struct bpf_func_proto bpf_copy_from_user_task_proto;
 extern const struct bpf_func_proto bpf_kptr_xchg_proto;
+extern const struct bpf_func_proto bpf_fuse_get_writeable_in_proto;
+extern const struct bpf_func_proto bpf_fuse_get_writeable_out_proto;
 
 const struct bpf_func_proto *tracing_prog_func_proto(
   enum bpf_func_id func_id, const struct bpf_prog *prog);
