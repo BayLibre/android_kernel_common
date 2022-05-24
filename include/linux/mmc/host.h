927bada2be7050e35cfc07beb9e96ef48688edbe
Aking Chen <akingchen@vivo.corp-partner.google.com>
2022-05-24 10:51:11 +0800

    ANDROID: mmc: Add vendor hooks
    Add vendor hooks to support vendor-specific exception handling features.
    
    Bug: 185083720
    
    Change-Id: I30381ed9835338308f2b03856f510a2982db6e6a
    Signed-off-by: Mingli Feng <mingli.feng@vivo.com>
    Signed-off-by: Aking Chen <akingchen@vivo.com>
    (cherry picked from commit e80bcd46271b80971283b64b4c8298100d1a8737)
    Signed-off-by: Aking Chen <akingchen@vivo.corp-partner.google.com>

--- include/linux/mmc/host.h
+++ include/linux/mmc/host.h
@@ -18,6 +18,8 @@
 #include <linux/keyslot-manager.h>
 #include <linux/android_kabi.h>
 
+#include <linux/android_vendor.h>
+
 struct mmc_ios {
 	unsigned int	clock;			/* clock rate */
 	unsigned short	vdd;
@@ -290,6 +292,7 @@ struct mmc_slot {
 	int cd_irq;
 	bool cd_wake_enabled;
 	void *handler_priv;
+	ANDROID_OEM_DATA(1);
 };
 
 /**
@@ -529,7 +532,8 @@ struct mmc_host {
 
 	ANDROID_KABI_RESERVE(1);
 	ANDROID_KABI_RESERVE(2);
-	u32			err_stats[MMC_ERR_MAX];
+	ANDROID_OEM_DATA(1);
+
 	unsigned long		private[] ____cacheline_aligned;
 };
 
