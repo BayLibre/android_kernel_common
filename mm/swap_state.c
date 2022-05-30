6c00465e6020b6ec33650e973353e4f1a4378494
Bing Han <bing.han@transsion.com>
2022-05-30 15:25:26 +0800

    ANDROID: vendor_hook: Add hook to swap_state
    Add hook function to specify where to store the reclaimed page:
    1) trace_android_rvh_add_to_swap: Replace the function add_to_swap,
     according to the reclaimed location information of the page, add
     the page to the swap of the specified location
    
    Bug: 234214858
    
    Signed-off-by: Bing Han <bing.han@transsion.com>
    Change-Id: I764a74e5b7ca4a8918b8258215de385d511a09da

--- mm/swap_state.c
+++ mm/swap_state.c
@@ -23,6 +23,7 @@
 #include <linux/huge_mm.h>
 #include <linux/shmem_fs.h>
 #include "internal.h"
+#include <trace/hooks/mm.h>
 
 /*
  * swapper_space is a fiction, retained to simplify the path through
@@ -217,7 +218,12 @@ void __delete_from_swap_cache(struct page *page,
 int add_to_swap(struct page *page)
 {
 	swp_entry_t entry;
-	int err;
+	int ret = 0;
+	bool skip = false;
+
+	trace_android_vh_add_to_swap(&ret, &skip);
+	if (skip)
+		return ret;
 
 	VM_BUG_ON_PAGE(!PageLocked(page), page);
 	VM_BUG_ON_PAGE(!PageUptodate(page), page);
