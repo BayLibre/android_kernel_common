71d64265408ec1ef15e0a33939d9e4ec87f1fbc6
Bing Han <bing.han@transsion.com>
2022-05-30 14:42:44 +0800

    ANDROID: vendor_hooks: Add hooks to process the status of reclaiming location
    android_vh_cow_user_page: when copy a page to a new page, set the
    status that whether the new page should be reclaimed to a specified
    swap location, according to the information of vm_fault.
    android_vh_add_anon_rmap: after add pte mapping to an anonymous page
    durning do_swap_page, update the status that whether this page need
    to be reclaimed to a swap location, according to the information
    of vm_fault.
    android_vh_handle_pte_fault_end: after handle_pte_fault, update the
    information that whether this page need to be reclaimed to a swap
    location.
    
    Bug: 234214858
    Signed-off-by: Bing Han <bing.han@transsion.com>
    Change-Id: Ie445c7b034ca176ec1e8fd1cd67c88581bf9ddf4

--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -149,6 +149,15 @@ DECLARE_HOOK(android_vh_subpage_dma_contig_alloc,
 DECLARE_HOOK(android_vh_ra_tuning_max_page,
 	TP_PROTO(struct readahead_control *ractl, unsigned long *max_page),
 	TP_ARGS(ractl, max_page));
+DECLARE_HOOK(android_vh_handle_pte_fault_end,
+	TP_PROTO(struct vm_fault *vmf, unsigned long highest_memmap_pfn),
+	TP_ARGS(vmf, highest_memmap_pfn));
+DECLARE_HOOK(android_vh_cow_user_page,
+	TP_PROTO(struct vm_fault *vmf, struct page *page),
+	TP_ARGS(vmf, page));
+DECLARE_HOOK(android_vh_add_anon_rmap,
+	TP_PROTO(struct vm_fault *vmf, struct page *page),
+	TP_ARGS(vmf, page));
 DECLARE_HOOK(android_vh_page_isolated_for_reclaim,
 	TP_PROTO(struct mm_struct *mm, struct page *page),
 	TP_ARGS(mm, page));
