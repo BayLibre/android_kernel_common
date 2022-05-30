d3523b3c90efd642800cd2049156a3822a9cd011
Bing Han <bing.han@transsion.com>
2022-05-30 14:42:44 +0800

    ANDROID: vendor_hooks: Add hooks to process the status of reclaiming location
    android_vh_cow_user_page, when copy a page to a new page, set the
     status that whether the new page should be reclaimed to a specified
     swap location, according to the information of vm_fault
    android_vh_swap_page_present, record whether the page is present
     in struct vm_fault, this information will be used in
     android_vh_cow_user_page to determine whether the new page should
     be reclaimed to a specified swap location
    
    Bug: 234214858
    Signed-off-by: Bing Han <bing.han@transsion.com>
    Change-Id: Ie445c7b034ca176ec1e8fd1cd67c88581bf9ddf4

--- mm/memory.c
+++ mm/memory.c
@@ -86,6 +86,7 @@
 
 #include "pgalloc-track.h"
 #include "internal.h"
+#include <trace/hooks/mm.h>
 
 #define CREATE_TRACE_POINTS
 #include <trace/events/pagefault.h>
@@ -3141,6 +3142,7 @@ static vm_fault_t wp_page_copy(struct vm_fault *vmf)
 				put_page(old_page);
 			return 0;
 		}
+		trace_android_vh_cow_user_page(vmf, new_page);
 	}
 
 	if (mem_cgroup_charge(new_page, mm, GFP_KERNEL))
@@ -3754,6 +3756,7 @@ vm_fault_t do_swap_page(struct vm_fault *vmf)
 	 * must be called after the swap_free(), or it will never succeed.
 	 */
 
+	trace_android_vh_swap_page_present(vmf, false);
 	inc_mm_counter_fast(vma->vm_mm, MM_ANONPAGES);
 	dec_mm_counter_fast(vma->vm_mm, MM_SWAPENTS);
 	pte = mk_pte(page, vmf->vma_page_prot);
