fa55c702a036707cc43d111ba830db7a4314023c
Bing Han <bing.han@transsion.com>
2022-05-30 16:00:22 +0800

    ANDROID: vendor_hook: Add hook to update nr_swap_pages and total_swap_pages
      The specified swap is is regarded as reserved extended memory.
      So nr_swap_pages and total_swap_pages should not be affected
      by the specified swap.
    
      Provide a endor hook android_vh_account_swap_pages to replace
      the updating process of nr_swap_pages and total_swap_pages.
      When the page is swapped to the specified swap location,
      nr_swap_pages and total_swap_pages should not be updated.
    
    Bug: 234214858
    Signed-off-by: Bing Han <bing.han@transsion.com>
    Change-Id: Ib8dfb355d190399a037b9d9eda478a81c436e224

--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -166,6 +166,10 @@ DECLARE_HOOK(android_vh_get_swap_page,
 DECLARE_HOOK(android_vh_set_page_reclaim_type,
 	TP_PROTO(struct mm_struct *mm, struct page *page),
 	TP_ARGS(mm, page));
+DECLARE_HOOK(android_vh_account_swap_pages,
+	TP_PROTO(struct swap_info_struct *si, unsigned int nr_entries,
+		bool update_total_swap_pages, bool *skip),
+	TP_ARGS(si, nr_entries, update_total_swap_pages, skip));
 /* macro versions of hooks are no longer required */
 
 #endif /* _TRACE_HOOK_MM_H */
