af373c0e1bcbb36b2cd9e0b605e05d242b534ddf
Liujie Xie <xieliujie@oppo.com>
2022-06-01 15:38:17 +0800

    ANDROID: vendor_hooks: Add hook in shrink_node_memcgs
    Add vendor hook in shrink_node_memcgs to adjust whether
    to skip memory reclamation of memcg.
    
    Bug: 226482420
    Signed-off-by: Liujie Xie <xieliujie@oppo.com>
    Change-Id: I925856353e63c5a821027de4f8476c833e21b982

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -543,3 +543,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_customize_thp_gfp_orders);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_init_adjust_zone_wmark);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_ksys_umount);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_do_group_exit);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_shrink_node_memcgs);
