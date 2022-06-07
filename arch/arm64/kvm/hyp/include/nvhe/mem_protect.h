47ac55ecb940aa8841b13b686cea6ae12e0be8d7
Quentin Perret <qperret@google.com>
2022-06-07 12:00:07 +0000

    ANDROID: KVM: arm64: Don't update IOMMUs unnecessarily
    When handling host stage-2 faults the hypervisor currently updates the
    CPU _and_ IOMMUs page-tables. However, since we currently proactively
    map accessible PA ranges into IOMMUs, updating them during stage-2
    faults is unnecessary -- it only needs to be done during ownership
    transitions. Optimize this by skipping the IOMMU updates from the host
    memory abort path, which also reduces contention on the host stage-2
    lock during boot and saves up to 1.1 sec of boot time on Pixel 6.
    
    Bug: 232879742
    Change-Id: Icb6f1b2d10de8cc2f976bbc94b9b79ad7aaa6135
    Signed-off-by: Quentin Perret <qperret@google.com>
    (cherry picked from commit 20c6e1ba55403fad0e7f039cc5d7e6f8c0fffa50)
    Signed-off-by: Mostafa Saleh <smostafa@google.com>

--- arch/arm64/kvm/hyp/include/nvhe/mem_protect.h
+++ arch/arm64/kvm/hyp/include/nvhe/mem_protect.h
@@ -79,7 +79,8 @@ int __pkvm_remove_ioguard_page(struct pkvm_hyp_vcpu *hyp_vcpu, u64 ipa);
 bool __pkvm_check_ioguard_page(struct pkvm_hyp_vcpu *hyp_vcpu);
 
 bool addr_is_memory(phys_addr_t phys);
-int host_stage2_idmap_locked(phys_addr_t addr, u64 size, enum kvm_pgtable_prot prot);
+int host_stage2_idmap_locked(phys_addr_t addr, u64 size, enum kvm_pgtable_prot prot,
+			     bool update_iommu);
 int host_stage2_set_owner_locked(phys_addr_t addr, u64 size, u8 owner_id);
 int host_stage2_unmap_dev_locked(phys_addr_t start, u64 size);
 int kvm_host_prepare_stage2(void *pgt_pool_base);
