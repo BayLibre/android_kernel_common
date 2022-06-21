MiCode/Xiaomi_Kernel_OpenSource

commit 7b26719a77c462ab8ee8bacfd9f1ede643415318

ANDROID: GKI: use internal type definitions in vendor hooks
This change is intended to reduce the chance of a missed ABI break
involving vendor hooks.

The following types are now fully defined in ABI XML.

* `struct binder_transaction`
* `struct binder_thread`
* `struct binder_proc`
* `struct printk_ringbuffer`
* `struct printk_record`

symbol '__alloc_pages' changed
  CRC changed from 0x6497cbd to 0x230fd8e5

symbol '__find_vma' changed
  CRC changed from 0xf11b619f to 0xb0069a61

symbol '__free_pages' changed
  CRC changed from 0xba5eeb68 to 0xdc394265

... 195 omitted; 198 symbols have only CRC changes

Bug: 233047575
Change-Id: I807227f6161f0a44ec3657dc8cdb8a3fdffc40b3
Signed-off-by: Giuliano Procida <gprocida@google.com>

include/trace/hooks/sched.h
@@ -9,6 +9,8 @@
  * Following tracepoints are not exported in tracefs and provide a
  * mechanism for vendor modules to hook and extend functionality
  */
+/* struct cgroup_taskset */
+#include <../kernel/cgroup/cgroup-internal.h>
 /* struct cgroup_subsys_state */
 #include <linux/cgroup-defs.h>
 /* struct em_perf_domain */
@@ -233,7 +235,6 @@ DECLARE_RESTRICTED_HOOK(android_rvh_update_misfit_status,
 	TP_PROTO(struct task_struct *p, struct rq *rq, bool *need_update),
 	TP_ARGS(p, rq, need_update), 1);
 
-struct cgroup_taskset;
 DECLARE_RESTRICTED_HOOK(android_rvh_cpu_cgroup_attach,
 	TP_PROTO(struct cgroup_taskset *tset),
 	TP_ARGS(tset), 1);

include/trace/hooks/audio_usboffload.h
@@ -9,9 +9,10 @@
 
 #include <trace/hooks/vendor_hooks.h>
 
+/* struct snd_usb_audio */
+#include <../sound/usb/usbaudio.h>
 /* struct usb_interface */
 #include <linux/usb.h>
-struct snd_usb_audio;
 
 DECLARE_HOOK(android_vh_audio_usb_offload_vendor_set,
 	TP_PROTO(void *arg),

include/trace/hooks/binder.h
@@ -10,12 +10,12 @@
  * Following tracepoints are not exported in tracefs and provide a
  * mechanism for vendor modules to hook and extend functionality
  */
+/* struct binder_proc, struct binder_thread, struct binder_transaction */
+#include <../drivers/android/binder_internal.h>
 /* struct task_struct */
 #include <linux/sched.h>
 /* struct binder_transaction_data */
 #include <uapi/linux/android/binder.h>
-#include <../drivers/android/binder_internal.h>
-struct seq_file;
 DECLARE_HOOK(android_vh_binder_transaction_init,
 	TP_PROTO(struct binder_transaction *t),
 	TP_ARGS(t));

include/trace/hooks/block.h
@@ -10,10 +10,12 @@
 #include <linux/tracepoint.h>
 #include <trace/hooks/vendor_hooks.h>
 
+/* struct blk_mq_tags */
+#include <../block/blk-mq-tag.h>
+/* struct blk_mq_alloc_data */
+#include <../block/blk-mq.h>
 /* struct blk_mq_tag_set */
 #include <linux/blk-mq.h>
-struct blk_mq_tags;
-struct blk_mq_alloc_data;
 
 DECLARE_HOOK(android_vh_blk_alloc_rqs,
 	TP_PROTO(size_t *rq_size, struct blk_mq_tag_set *set,

include/trace/hooks/cgroup.h
@@ -7,6 +7,8 @@
 #define _TRACE_HOOK_CGROUP_H
 #include <trace/hooks/vendor_hooks.h>
 
+/* struct cgroup_taskset */
+#include <../kernel/cgroup/cgroup-internal.h>
 /* struct cgroup_subsys */
 #include <linux/cgroup-defs.h>
 /* struct task_struct */
@@ -15,8 +17,6 @@ DECLARE_HOOK(android_vh_cgroup_set_task,
 	TP_PROTO(int ret, struct task_struct *task),
 	TP_ARGS(ret, task));
 
-struct cgroup_taskset;
-struct cgroup;
 DECLARE_HOOK(android_vh_cgroup_attach,
 	TP_PROTO(struct cgroup_subsys *ss, struct cgroup_taskset *tset),
 	TP_ARGS(ss, tset))

include/trace/hooks/debug.h
@@ -10,7 +10,8 @@
 #include <trace/hooks/vendor_hooks.h>
 
 #if defined(CONFIG_TRACEPOINTS) && defined(CONFIG_ANDROID_VENDOR_HOOKS)
-struct pt_regs;
+/* struct pt_regs */
+#include <asm/ptrace.h>
 
 DECLARE_HOOK(android_vh_ipi_stop,
 	TP_PROTO(struct pt_regs *regs),

include/trace/hooks/fault.h
@@ -7,7 +7,8 @@
 #define _TRACE_HOOK_FAULT_H
 #include <trace/hooks/vendor_hooks.h>
 
-struct pt_regs;
+/* struct pt_regs */
+#include <asm/ptrace.h>
 DECLARE_RESTRICTED_HOOK(android_rvh_die_kernel_fault,
 	TP_PROTO(const char *msg, unsigned long addr, unsigned int esr, struct pt_regs *regs),
 	TP_ARGS(msg, addr, esr, regs), 1);

include/trace/hooks/logbuf.h
@@ -9,8 +9,8 @@
 
 #include <trace/hooks/vendor_hooks.h>
 
-struct printk_ringbuffer;
-struct printk_record;
+/* struct printk_record, struct printk_ringbuffer */
+#include <../kernel/printk/printk_ringbuffer.h>
 
 DECLARE_HOOK(android_vh_logbuf,
 	TP_PROTO(struct printk_ringbuffer *rb, struct printk_record *r),

BayLibre/android_kernel_common

commit 4d01c9c0f34b5acdfe8ef915d4ca2b7fb99225bd

ANDROID: Fix build error with CONFIG_UCLAMP_TASK disabled
After 7b26719a77c4 ("ANDROID: GKI: use internal type definitions in
vendor hooks") we stopped providing a forward declaration of 'struct
uclamp_se' and instead relied on pulling its definition from
linux/sched.h. the 'uclamp_se' structure is conditionally defined based
upon CONFIG_UCLAMP_TASK therefore causing a build error to show up for
the android_rvh_uclamp_eff_get trace point.

Fix this by making the tracepoint definition conditional upon
CONFIG_UCLAMP_TASK.

Fixes: 7b26719a77c4 ("ANDROID: GKI: use internal type definitions in vendor hooks")
Signed-off-by: Florian Fainelli <florian.fainelli@broadcom.com>
Change-Id: Iaa803d149150c1703435f9cfa52ea1406a9521fd

include/trace/hooks/sched.h
@@ -327,10 +327,12 @@ DECLARE_HOOK(android_vh_irqtime_account_process_tick,
 	TP_PROTO(struct task_struct *p, struct rq *rq, int user_tick, int ticks),
 	TP_ARGS(p, rq, user_tick, ticks));
 
+#ifdef CONFIG_UCLAMP_TASK
 DECLARE_RESTRICTED_HOOK(android_rvh_uclamp_eff_get,
 	TP_PROTO(struct task_struct *p, enum uclamp_id clamp_id,
 		 struct uclamp_se *uclamp_max, struct uclamp_se *uclamp_eff, int *ret),
 	TP_ARGS(p, clamp_id, uclamp_max, uclamp_eff, ret), 1);
+#endif
 
 DECLARE_RESTRICTED_HOOK(android_rvh_after_enqueue_task,
 	TP_PROTO(struct rq *rq, struct task_struct *p, int flags),