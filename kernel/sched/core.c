6d4eeceac36c16ae0e2bf08a2408d926bafd72fd
muduo qin <muduo.qin@transsion.com>
2022-06-22 17:01:37 +0800

    ANDROID: GKI: Export uclamp_eff_value symbol for OEM modules
    Bug: 236096982
    Signed-off-by: muduo qin <muduo.qin@transsion.com>
    Change-Id: Ibc5b6f56ad6641ba6b382313ec2995ec17849893

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1034,6 +1034,7 @@ unsigned long uclamp_eff_value(struct task_struct *p, enum uclamp_id clamp_id)
 
 	return (unsigned long)uc_eff.value;
 }
+EXPORT_SYMBOL_GPL(uclamp_eff_value);
 
 /*
  * When a task is enqueued on a rq, the clamp bucket currently defined by the
