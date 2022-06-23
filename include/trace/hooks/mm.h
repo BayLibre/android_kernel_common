200bfbd7e35395f5ad7f3d89b8fba51e24c5b4a2
Peifeng Li <lipeifeng@oppo.com>
2022-06-23 14:15:35 +0800

    ANDROID: vendor_hooks: support mapped-protect
    The patchset supports mapped-protect to decrease page thrashing
    and the cpu-loading of the memory-reclaim-process.
    
    This patch(1/3):
    
    Support three hooks as follows to account
    the amount of multi-mapped pages in kernel:
    
    - android_vh_update_mapped_mul
    - android_vh_add_mapped_mul_op_lrulist
    - android_vh_dec_mapped_mul_op_lrulist
    
    Bug: 236578020
    Signed-off-by: Peifeng Li <lipeifeng@oppo.com>
    Change-Id: Ia2c7015aab442be7dbb496b8b630b9dff59ab935

--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -122,6 +122,15 @@ DECLARE_HOOK(android_vh_drain_all_pages_bypass,
 		int migratetype, unsigned long did_some_progress,
 		bool *bypass),
 	TP_ARGS(gfp_mask, order, alloc_flags, migratetype, did_some_progress, bypass));
+DECLARE_HOOK(android_vh_update_mapped_mul,
+	TP_PROTO(struct page *page, bool inc_size, bool *ret, bool *success),
+	TP_ARGS(page, inc_size, ret, success));
+DECLARE_HOOK(android_vh_add_mapped_mul_op_lrulist,
+	TP_PROTO(struct page *page, enum lru_list lru),
+	TP_ARGS(page, lru));
+DECLARE_HOOK(android_vh_dec_mapped_mul_op_lrulist,
+	TP_PROTO(struct page *page, enum lru_list lru),
+	TP_ARGS(page, lru));
 DECLARE_HOOK(android_vh_cma_drain_all_pages_bypass,
 	TP_PROTO(unsigned int migratetype, bool *bypass),
 	TP_ARGS(migratetype, bypass));
