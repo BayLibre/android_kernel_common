408cf2f0761b97e586c4514ee04fd5139a324d39
xieliujie <xieliujie@oppo.com>
2022-07-01 18:42:13 +0800

    ANDROID: vendor_hooks: Add hooks for signal
    Add hook to boost thread when process killed.
    
    Bug: 237749933
    Signed-off-by: xieliujie <xieliujie@oppo.com>
    Change-Id: I7cc6f248397021f3a8271433144a0e582ed27cfa
    (cherry picked from commit 709679142d583b0b7338d931fdd43b27b1bbf9e0)
    (cherry picked from commit 226f36edddc3ad523dbb901ca3d5683f5f4e1707)

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -61,6 +61,7 @@
 #include <trace/hooks/audio_usboffload.h>
 #include <trace/hooks/typec.h>
 #include <trace/hooks/user.h>
+#include <trace/hooks/signal.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -230,3 +231,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_preset);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_alloc_uid);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_free_user);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_cpuset_fork);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_exit_signal);
--- kernel/signal.c
+++ kernel/signal.c
@@ -56,6 +56,8 @@
 #include <asm/cacheflush.h>
 #include <asm/syscall.h>	/* for syscall_get_* */
 
+#undef CREATE_TRACE_POINTS
+#include <trace/hooks/signal.h>
 /*
  * SLAB caches for signal bits.
  */
@@ -1055,6 +1057,7 @@ static void complete_signal(int sig, struct task_struct *p, enum pid_type type)
 			signal->group_stop_count = 0;
 			t = p;
 			do {
+				trace_android_vh_exit_signal(t);
 				task_clear_jobctl_pending(t, JOBCTL_PENDING_MASK);
 				sigaddset(&t->pending.signal, SIGKILL);
 				signal_wake_up(t, 1);
