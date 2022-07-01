d19cc86c3d85971383b397d40c7c55a20c0f4597
xieliujie <xieliujie@oppo.com>
2022-07-01 18:42:13 +0800

    ANDROID: vendor_hooks: Add hooks for signal
    Add hook to boost thread when process killed.
    
    Bug: 237749933
    Signed-off-by: xieliujie <xieliujie@oppo.com>
    Change-Id: I7cc6f248397021f3a8271433144a0e582ed27cfa
    (cherry picked from commit 709679142d583b0b7338d931fdd43b27b1bbf9e0)
    (cherry picked from commit 226f36edddc3ad523dbb901ca3d5683f5f4e1707)

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -230,3 +230,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_preset);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_alloc_uid);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_free_user);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_cpuset_fork);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_exit_signal);
