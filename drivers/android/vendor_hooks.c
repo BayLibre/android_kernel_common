8b3690a3a13cf22ee84704a81992c44a1d19ec03
xiaofeng <xiaofeng5@xiaomi.com>
2022-07-12 20:43:49 +0800

    ANDROID: vendor_hooks:vendor hook for pidfd_open
    Add vendor hook when detecting process status through
    pidfd_open.
    
    Bug: 238725692
    Change-Id: I565988cb8bf6dd44ab4dc15c410c2dcf50703def
    Signed-off-by: xiaofeng <xiaofeng5@xiaomi.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -451,3 +451,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_alloc_uid);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_free_user);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_modify_thermal_cpu_get_power);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mmput);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_pidfd_open);
