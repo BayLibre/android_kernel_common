9261a47871427f7da0e13e78b2853b45abb396fc
Vincent Donnefort <vdonnefort@google.com>
2022-07-15 17:03:12 +0100

    ANDROID: KVM: arm64: Add support for dynamic static_keys in nVHE hyp
    Currently, only the host can flip before init, static keys used by the
    nVHE hypervisor at EL2. Add in hypervisor static_branch_enable() and
    static_branch_disable(). Register a separate jump_table and bring a minimal
    support for toggling static branches (static_branch_{enable,disable}).
    
    It is only possible for the hypervisor to flip !linked static keys, i.e.
    keys declared in the hypervisor and not referenced anywhere else.
    
    The arm64 insn libraries and patching can't be used in hypervisor due to
    too many missing dependencies. A very simple version is then also added.
    
    This intends to be used by the upcoming in-hyp trace events.
    
    Bug: 229972309
    Change-Id: I1dbd249d8845c50594dc5fbb3a41e032d8445d39
    Signed-off-by: Vincent Donnefort <vdonnefort@google.com>

--- arch/arm64/kvm/hyp/nvhe/setup.c
+++ arch/arm64/kvm/hyp/nvhe/setup.c
@@ -11,6 +11,7 @@
 #include <asm/kvm_pkvm.h>
 
 #include <nvhe/early_alloc.h>
+#include <nvhe/jump_label.h>
 #include <nvhe/gfp.h>
 #include <nvhe/memory.h>
 #include <nvhe/mem_protect.h>
@@ -168,6 +169,10 @@ static int recreate_hyp_mappings(phys_addr_t phys, unsigned long size,
 	end = start + pvmfw_size;
 	prot = pkvm_mkstate(PAGE_HYP_RO, PKVM_PAGE_OWNED);
 	ret = pkvm_create_mappings(start, end, prot);
+	if (ret)
+		return ret;
+
+	ret = create_hyp_jump_label_mappings();
 	if (ret)
 		return ret;
 
