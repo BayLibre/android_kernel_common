0e9aaa82eb0437094bcc6ca6edd1562c77495711
Isaac J. Manjarres <isaacmanjarres@google.com>
2022-07-20 18:42:54 -0700

    ANDROID: mm/slab: Correctly derive kmalloc cache index for constant sizes
    Allowing the kmalloc minimum alignment to change dynamically can lead
    to two scenarios:
    
    Scenario 1: the dynamically detected kmalloc minimum alignment is
    larger than the hardcoded value of ARCH_KMALLOC_MINALIGN
    
    Scenario 2: the dynamically detected kmalloc minimum alignment is
    smaller than the hardcoded value of ARCH_KMALLOC_MINALIGN
    
    Scenario 1 would not cause any functional issues, as the kmalloc cache
    table would be setup so that allocations between ARCH_KMALLOC_MINALIGN
    and the dynamically detected kmalloc value are satisfied by the
    kmalloc cache that satisfied allocations up to the dynamically detected
    kmalloc value in size.
    
    However, scenario 2 would result in situations where memory is
    overallocated. For example, consider the case where
    ARCH_KMALLOC_MINALIGN is 128, and the dynamically detected kmalloc
    minimum alignment is 64. Suppose a structure is to be allocated through
    kmalloc, and the structure's size--32 bytes--is known at compile time.
    In that case, the logic in kmalloc_index() will use the hardcoded value
    of ARCH_KMALLOC_MINALIGN, which causes the allocation to be satisfied by
    the kmalloc-128 cache, but it could've been satisfied by the kmalloc-64
    cache, leaving the kmalloc-64 cache underutilized.
    
    To avoid this scenario, introduce variables that can be updated and read
    at runtime to correctly determine which kmalloc cache to use to satisfy
    an allocation.
    
    Bug: XXXXXX
    Change-Id: I622dfb7ac21ebbe869ce5ed175e8a33168e100d3
    Signed-off-by: Isaac J. Manjarres <isaacmanjarres@google.com>

--- include/linux/slab.h
+++ include/linux/slab.h
@@ -343,6 +343,8 @@ enum kmalloc_cache_type {
 #ifndef CONFIG_SLOB
 extern struct kmem_cache *
 kmalloc_caches[NR_KMALLOC_TYPES][KMALLOC_SHIFT_HIGH + 1];
+extern int kmalloc_min_size;
+extern int kmalloc_shift_low;
 
 unsigned int arch_kmalloc_minalign(void);
 
@@ -397,12 +399,12 @@ static __always_inline unsigned int __kmalloc_index(size_t size,
 	if (!size)
 		return 0;
 
-	if (size <= KMALLOC_MIN_SIZE)
-		return KMALLOC_SHIFT_LOW;
+	if (size <= kmalloc_min_size)
+		return kmalloc_shift_low;
 
-	if (KMALLOC_MIN_SIZE <= 32 && size > 64 && size <= 96)
+	if (kmalloc_min_size <= 32 && size > 64 && size <= 96)
 		return 1;
-	if (KMALLOC_MIN_SIZE <= 64 && size > 128 && size <= 192)
+	if (kmalloc_min_size <= 64 && size > 128 && size <= 192)
 		return 2;
 	if (size <=          8) return 3;
 	if (size <=         16) return 4;
