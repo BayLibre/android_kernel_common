d2c3c9fdef4cfe987440af664f17159e62ded5eb
Daniel Rosenberg <drosen@google.com>
2022-07-27 13:11:07 -0700

    ANDROID: fuse-bpf: Don't support export_operations
    In the future, we may choose to support these, but it poses some
    challenges. In order to create a disconnected dentry/inode, we'll need
    to encode the mountpoint and bpf into the file_handle, which means we'd
    need a stable representation of them. This also won't hold up to cases
    where the bpf is not stateless. One possibility is registering bpf
    programs and mounts in a specific order, so they can be assigned
    consistent ids we can use in the file_handle. We can defer to the lower
    filesystem for the lower inode's representation in the file_handle.
    
    Bug: 202785178
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    Change-Id: Ib64b2693f5934bb30d67ddc0f51e3d04472f5e80

--- fs/fuse/inode.c
+++ fs/fuse/inode.c
@@ -1111,6 +1111,12 @@ static int fuse_encode_fh(struct inode *inode, u32 *fh, int *max_len,
 	nodeid = get_fuse_inode(inode)->nodeid;
 	generation = inode->i_generation;
 
+	/* TODO: Does it make sense to support this in some cases? */
+	if (!nodeid && get_fuse_inode(inode)->backing_inode) {
+		*max_len = 0;
+		return FILEID_INVALID;
+	}
+
 	fh[0] = (u32)(nodeid >> 32);
 	fh[1] = (u32)(nodeid & 0xffffffff);
 	fh[2] = generation;
