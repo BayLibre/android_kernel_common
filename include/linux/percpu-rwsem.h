63a5bfd6ab09b02c77d860dd9350cccfdd5a6f7f
Peifeng Li <lipeifeng@oppo.com>
2022-08-03 19:42:27 +0800

    ANDROID: vendor_hook: add hooks to protect locking-tsk in cpu scheduler
    Providing vendor hooks to record the start time of holding the lock, which
    protects rwsem/mutex locking-process from being preemptedfor a short time
    in some cases.
    
    - android_vh_record_mutex_lock_starttime
    - android_vh_record_rtmutex_lock_starttime
    - android_vh_record_rwsem_lock_starttime
    - android_vh_record_pcpu_rwsem_starttime
    
    This commit is a combination of the following three commits.
    https://android-review.googlesource.com/c/kernel/common/+/2579595
    https://android-review.googlesource.com/c/kernel/common/+/2617070
    https://android-review.googlesource.com/c/kernel/common/+/2694147
    
    Bug: 241191475
    Change-Id: I0e967a1e8b77c32a1ad588acd54028fae2f90c4e
    Signed-off-by: xieliujie <xieliujie@oppo.com>
    (cherry picked from commit f7294947672eb6b786f3c16b49e71e6a239101ad)

--- include/linux/percpu-rwsem.h
+++ include/linux/percpu-rwsem.h
@@ -9,6 +9,9 @@
 #include <linux/rcu_sync.h>
 #include <linux/lockdep.h>
 
+void _trace_android_vh_record_pcpu_rwsem_starttime(
+		u64 sem, unsigned long settime);
+
 struct percpu_rw_semaphore {
 	struct rcu_sync		rss;
 	unsigned int __percpu	*read_count;
@@ -50,6 +53,7 @@ static inline void percpu_down_read(struct percpu_rw_semaphore *sem)
 
 	rwsem_acquire_read(&sem->dep_map, 0, 0, _RET_IP_);
 
+	_trace_android_vh_record_pcpu_rwsem_starttime((u64)sem, jiffies);
 	preempt_disable();
 	/*
 	 * We are in an RCU-sched read-side critical section, so the writer
@@ -88,8 +92,10 @@ static inline bool percpu_down_read_trylock(struct percpu_rw_semaphore *sem)
 	 * bleeding the critical section out.
 	 */
 
-	if (ret)
+	if (ret) {
+		_trace_android_vh_record_pcpu_rwsem_starttime((u64)sem, jiffies);
 		rwsem_acquire_read(&sem->dep_map, 0, 1, _RET_IP_);
+	}
 
 	return ret;
 }
@@ -118,6 +124,7 @@ static inline void percpu_up_read(struct percpu_rw_semaphore *sem)
 		this_cpu_dec(*sem->read_count);
 		rcuwait_wake_up(&sem->writer);
 	}
+	_trace_android_vh_record_pcpu_rwsem_starttime((u64)sem, 0);
 	preempt_enable();
 }
 
