5633ff5aec1c4d2ed9101cc117b6333870bca81a
Paul Lawrence <paullawrence@google.com>
2022-08-15 10:12:59 -0700

    ANDROID: fuse-bpf: readddir postfilter fixes
    Test: fuse_test passes, with appropriate user code directories can be
    hidden
    Bug: 219958836
    Signed-off-by: Paul Lawrence <paullawrence@google.com>
    Change-Id: Ic9cb0e9009066000b01edd904168564c968a4802

--- fs/fuse/backing.c
+++ fs/fuse/backing.c
@@ -22,6 +22,9 @@ struct fuse_bpf_aio_req {
 
 static struct kmem_cache *fuse_bpf_aio_request_cachep;
 
+static void fuse_stat_to_attr(struct fuse_conn *fc, struct inode *inode,
+		struct kstat *stat, struct fuse_attr *attr);
+
 static void fuse_file_accessed(struct file *dst_file, struct file *src_file)
 {
 	struct inode *dst_inode;
@@ -207,8 +210,10 @@ int fuse_open_finalize(struct bpf_fuse_args *fa, int *out,
 	struct fuse_file *ff = file->private_data;
 	struct fuse_open_out *foo = fa->out_args[0].value;
 
-	if (ff)
+	if (ff) {
 		ff->fh = foo->fh;
+		ff->nodeid = get_fuse_inode(inode)->nodeid;
+	}
 	return 0;
 }
 
@@ -1330,6 +1335,9 @@ int fuse_lookup_backing(struct bpf_fuse_args *fa, struct dentry **out, struct in
 	struct dentry *backing_entry;
 	const char *name;
 	int len;
+	struct fuse_entry_out *feo = (void *)fa->out_args[0].value;
+	struct kstat stat;
+	int err;
 
 	/* TODO this will not handle lookups over mount points */
 	inode_lock_nested(dir_backing_inode, I_MUTEX_PARENT);
@@ -1348,10 +1356,21 @@ int fuse_lookup_backing(struct bpf_fuse_args *fa, struct dentry **out, struct in
 
 	fuse_entry->backing_path = (struct path) {
 		.dentry = backing_entry,
-		.mnt = dir_fuse_entry->backing_path.mnt,
+		.mnt = mntget(dir_fuse_entry->backing_path.mnt),
 	};
 
-	mntget(fuse_entry->backing_path.mnt);
+	if (d_is_negative(backing_entry))
+		return 0;
+
+	err = vfs_getattr(&fuse_entry->backing_path, &stat,
+				  STATX_BASIC_STATS, 0);
+	if (err) {
+		path_put_init(&fuse_entry->backing_path);
+		return err;
+	}
+
+	fuse_stat_to_attr(get_fuse_conn(dir),
+			  backing_entry->d_inode, &stat, &feo->attr);
 	return 0;
 }
 
