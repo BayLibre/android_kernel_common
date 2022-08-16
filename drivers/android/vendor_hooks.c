d61c3592bde46e6a54ed1859ebc530c301e591dc
Ziyi Cui <ziyic@google.com>
2022-08-16 20:57:48 +0000

    ANDROID: power: Add vendor hook for suspend
    The purpose of this vendor hook is to trace early resume latency.
    
    Bug: 241946090
    Signed-off-by: Ziyi Cui <ziyic@google.com>
    Change-Id: I508f5a34fd3210178af0cb142461baf814196b9f
    (cherry picked from commit 737a5314c9f35665fa61e2356f65501e0dc53b92)

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -293,4 +293,5 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_shrink_lruvec_blk_plug);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_reclaim_pages_plug);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_resume_begin);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_resume_end);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_early_resume_begin);
 
