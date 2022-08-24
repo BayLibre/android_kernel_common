1400c2c4e316a40f60e638aff81e300abbba1fbf
xiaofeng <xiaofeng5@xiaomi.com>
2022-08-24 19:34:06 +0800

    ANDROID: vendor_hooks:vendor hook for __alloc_pages_slowpath.
    add vendor hook in __alloc_pages_slowpath ahead of
    __alloc_pages_direct_reclaim and warn_alloc.
    
    Bug: 243629905
    Change-Id: Ieacc6cf79823c0bfacfdeec9afb55ed66f40d0b0
    Signed-off-by: xiaofeng <xiaofeng5@xiaomi.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -230,3 +230,5 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_preset);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_alloc_uid);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_free_user);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_cpuset_fork);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_alloc_pages_reclaim_bypass);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_alloc_pages_failure_bypass);
--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -24,6 +24,16 @@ DECLARE_RESTRICTED_HOOK(android_rvh_set_readahead_gfp_mask,
 			TP_PROTO(unsigned int *flags),	/* gfp_t *flags */
 			TP_ARGS(flags), 1);
 
+DECLARE_HOOK(android_vh_alloc_pages_reclaim_bypass,
+    TP_PROTO(gfp_t gfp_mask, int order, int alloc_flags,
+	int migratetype, struct page **page),
+	TP_ARGS(gfp_mask, order, alloc_flags, migratetype, page));
+
+DECLARE_HOOK(android_vh_alloc_pages_failure_bypass,
+	TP_PROTO(gfp_t gfp_mask, int order, int alloc_flags,
+	int migratetype, struct page **page),
+	TP_ARGS(gfp_mask, order, alloc_flags, migratetype, page));
+
 #endif /* _TRACE_HOOK_MM_H */
 
 /* This part must be outside protection */
