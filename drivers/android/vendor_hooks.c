b9e314cacfdc255c24d4bdfd6359697c16c5ac5b
xiaofeng <xiaofeng5@xiaomi.com>
2022-08-24 19:34:06 +0800

    ANDROID: vendor_hooks:vendor hook for __alloc_pages_slowpath.
    add vendor hook in __alloc_pages_slowpath ahead of
    __alloc_pages_direct_reclaim and warn_alloc.
    
    Bug: 243629905
    Bug: 395197442
    Change-Id: Ieacc6cf79823c0bfacfdeec9afb55ed66f40d0b0
    Signed-off-by: xiaofeng <xiaofeng5@xiaomi.com>
    Signed-off-by: huzhanyuan  <huzhanyuan@oppo.com>
    Signed-off-by: wengle <wengle@oppo.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -332,3 +332,5 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_tune_scan_control);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_adjust_kvmalloc_flags);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_exit_signal);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_vmscan_kswapd_done);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_alloc_pages_reclaim_bypass);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_alloc_pages_failure_bypass);
