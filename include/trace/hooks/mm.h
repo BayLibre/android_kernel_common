4e97d8faebd1d40d1a7a4e064215c9f4bc9290c1
Peifeng Li <lipeifeng@oppo.com>
2022-09-13 19:07:41 +0800

    ANDROID: vendor_hooks: Add hooks for lookaround
    Add hooks for support lookaround in memory reclamation.
    
    - android_vh_test_clear_look_around_ref
    - android_vh_check_page_look_around_ref
    - android_vh_look_around_migrate_page
    - android_vh_look_around
    Bug: 241079328
    
    Signed-off-by: Peifeng Li <lipeifeng@oppo.com>
    Change-Id: I9a606ae71d2f1303df3b02403b30bc8fdc9d06dd
    (cherry picked from commit f50f24e781738c8e5aa9f285d8726202f33107d6)

--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -11,6 +11,7 @@
 
 struct shmem_inode_info;
 struct folio;
+struct page_vma_mapped_walk;
 
 DECLARE_RESTRICTED_HOOK(android_rvh_shmem_get_folio,
 			TP_PROTO(struct shmem_inode_info *info, struct folio **folio),
@@ -129,6 +130,16 @@ DECLARE_HOOK(android_vh_free_one_page_bypass,
 	TP_PROTO(struct page *page, struct zone *zone, int order, int migratetype,
 		int fpi_flags, bool *bypass),
 	TP_ARGS(page, zone, order, migratetype, fpi_flags, bypass));
+DECLARE_HOOK(android_vh_test_clear_look_around_ref,
+	TP_PROTO(struct page *page),
+	TP_ARGS(page));
+DECLARE_HOOK(android_vh_look_around_migrate_page,
+	TP_PROTO(struct page *old_page, struct page *new_page),
+	TP_ARGS(old_page, new_page));
+DECLARE_HOOK(android_vh_look_around,
+	TP_PROTO(struct page_vma_mapped_walk *pvmw, struct page *page,
+		struct vm_area_struct *vma, int *referenced),
+	TP_ARGS(pvmw, page, vma, referenced));
 
 #endif /* _TRACE_HOOK_MM_H */
 
