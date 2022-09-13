913acfc58ae132a4591b3f2265581f435a22548a
Peifeng Li <lipeifeng@oppo.com>
2022-09-13 19:07:41 +0800

    ANDROID: vendor_hooks: Add hooks for lookaround
    Add hooks for support lookaround in memory reclamation.
    
    - android_vh_test_clear_look_around_ref
    - android_vh_check_page_look_around_ref
    - android_vh_look_around_migrate_page
    - android_vh_look_around
    Bug: 241079328
    
    Signed-off-by: Peifeng Li <lipeifeng@oppo.com>
    Change-Id: I9a606ae71d2f1303df3b02403b30bc8fdc9d06dd
    (cherry picked from commit f50f24e781738c8e5aa9f285d8726202f33107d6)

--- mm/rmap.c
+++ mm/rmap.c
@@ -812,12 +812,16 @@ static bool page_referenced_one(struct page *page, struct vm_area_struct *vma,
 		}
 
 		if (pvmw.pte) {
+<<<<<<< HEAD   (bbd71c ANDROID: GKI: Update symbol list for Amlogic)
 			if (lru_gen_enabled() && pte_young(*pvmw.pte) &&
 			    !(vma->vm_flags & (VM_SEQ_READ | VM_RAND_READ))) {
 				lru_gen_look_around(&pvmw);
 				referenced++;
 			}
 
+=======
+			trace_android_vh_look_around(&pvmw, page, vma, &referenced);
+>>>>>>> CHANGE (f50f24 ANDROID: vendor_hooks: Add hooks for lookaround)
 			if (ptep_clear_flush_young_notify(vma, address,
 						pvmw.pte)) {
 				/*
