8f2055974760b1546983c95b4053f96a5115d67a
Shreyas K K <quic_shrekk@quicinc.com>
2022-10-07 22:12:28 +0530

    ANDROID: vendor hooks: Add hooks to support bootloader based hibernation
    Add vendor hooks to disable randomization of swap slot allocation for
    swap partition used for saving hibernation image. Another level of
    randomization of swap slots takes place at the firmware level as well
    in order to address the wear leveling for UFS/MMC devices, so this
    vendor hook checks if a block device represents the swap partition being
    used for saving hibernation image, if yes, the swap slot allocation for
    such partition is serialized at kernel level.
    
    There is a performance advantage of reading contiguous pages of hibernation
    image, it makes the restore logic of hibernation image simpler and faster
    as there are no seeks involved in the secondary storage to read multiple
    contiguous pages of the image.
    
    Bug: 379617660
    Change-Id: Id41a25fd15c5b71f8064f1c7d9cac9560fcdde85
    Signed-off-by: Vivek Kumar <quic_vivekuma@quicinc.com>
    Signed-off-by: Shreyas K K <quic_shrekk@quicinc.com>
    Signed-off-by: Auditya Bhattaram <quic_audityab@quicinc.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -68,6 +68,7 @@
 #include <trace/hooks/dmabuf.h>
 #include <trace/hooks/softlockup.h>
 #include <trace/hooks/dwc3.h>
+#include <trace/hooks/bl_hib.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -512,3 +513,6 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_clear_rwsem_writer_owned);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_percpu_rwsem_down_read);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_percpu_rwsem_up_write);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_percpu_rwsem_wait_complete);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_check_hibernation_swap);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_save_cpu_resume);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_save_hib_resume_bdev);
