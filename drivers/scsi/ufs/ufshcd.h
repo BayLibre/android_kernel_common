cde1f61ec6b8ad8aeaa2c115fa79d38658f694e3
Bart Van Assche <bvanassche@acm.org>
2022-10-18 13:29:56 -0700

    UPSTREAM: scsi: ufs: Track system suspend / resume activity
    Add a new boolean variable that tracks whether the system is suspending,
    suspended or resuming. This information will be used in a later commit to
    fix a deadlock between the SCSI error handler and the suspend code.
    
    Reviewed-by: Adrian Hunter <adrian.hunter@intel.com>
    Change-Id: Ida8b4e67460db2546314361456c6b0aba1a7a7a0
    Signed-off-by: Bart Van Assche <bvanassche@acm.org>
    Link: https://lore.kernel.org/r/20221018202958.1902564-9-bvanassche@acm.org
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    (cherry picked from commit 1a547cbc6fdd07992f915a614a3f7ba3fccef8fb)
    Signed-off-by: Bart Van Assche <bvanassche@google.com>

--- drivers/scsi/ufs/ufshcd.h
+++ drivers/scsi/ufs/ufshcd.h
@@ -816,6 +816,8 @@ struct ufs_hba_monitor {
  * @max_pwr_info: keeps the device max valid pwm
  * @clk_scaling_lock: used to serialize device commands and clock scaling
  * @desc_size: descriptor sizes reported by device
+ * @system_suspending: system suspend has been started and system resume has
+ *	not yet finished.
  * @urgent_bkops_lvl: keeps track of urgent bkops level for device
  * @is_urgent_bkops_lvl_checked: keeps track if the urgent bkops level for
  *  device is known or not.
@@ -946,6 +948,7 @@ struct ufs_hba {
 
 	struct devfreq *devfreq;
 	struct ufs_clk_scaling clk_scaling;
+	bool system_suspending;
 	bool is_sys_suspended;
 
 	enum bkops_status urgent_bkops_lvl;
