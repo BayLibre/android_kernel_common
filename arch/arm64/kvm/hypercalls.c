a31308b3cb17e289372c6ca125f527aba59d6f04
Vincent Donnefort <vdonnefort@google.com>
2022-10-26 12:42:51 +0100

    ANDROID: KVM: arm64: Add protected_shared_mem statistic
    When using nVHE in protected mode, protected memory can be between
    host and a guest. Tracking this value is interesting from a debug
    perspective, to identify potential leaks.
    
    Keeping the count of memory sharing is easy, each share/unshare will return
    to the host where the accounting will take place.
    
    Bug: 222044477
    Change-Id: I43dcd258789f79dbfe489e5bf721e606c5e6e022
    Signed-off-by: Vincent Donnefort <vdonnefort@google.com>

--- arch/arm64/kvm/hypercalls.c
+++ arch/arm64/kvm/hypercalls.c
@@ -83,6 +83,8 @@ static bool kvm_hvc_call_default_allowed(u32 func_id)
 	 */
 	case ARM_SMCCC_VERSION_FUNC_ID:
 	case ARM_SMCCC_ARCH_FEATURES_FUNC_ID:
+	case ARM_SMCCC_VENDOR_HYP_KVM_MEM_SHARE_FUNC_ID:
+	case ARM_SMCCC_VENDOR_HYP_KVM_MEM_UNSHARE_FUNC_ID:
 		return true;
 	default:
 		/* PSCI 0.2 and up is in the 0:0x1f range */
@@ -223,6 +225,12 @@ int kvm_hvc_call_handler(struct kvm_vcpu *vcpu)
 		break;
 	case ARM_SMCCC_VENDOR_HYP_KVM_PTP_FUNC_ID:
 		kvm_ptp_get_time(vcpu, val);
+		break;
+	case ARM_SMCCC_VENDOR_HYP_KVM_MEM_SHARE_FUNC_ID:
+		atomic64_add(PAGE_SIZE, &vcpu->kvm->stat.protected_shared_mem2);
+		break;
+	case ARM_SMCCC_VENDOR_HYP_KVM_MEM_UNSHARE_FUNC_ID:
+		atomic64_sub(PAGE_SIZE, &vcpu->kvm->stat.protected_shared_mem2);
 		break;
 	case ARM_SMCCC_VENDOR_HYP_KVM_MEM_RELINQUISH_FUNC_ID:
 		pkvm_host_reclaim_page(vcpu->kvm, smccc_get_arg1(vcpu));
