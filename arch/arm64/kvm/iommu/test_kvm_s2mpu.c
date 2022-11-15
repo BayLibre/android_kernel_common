084c26e398773fcea5a1372bf93b909006d8f306
Mostafa Saleh <smostafa@google.com>
2022-11-15 14:35:05 +0000

    ANDROID: KVM: arm64: s2mpu: update self test to use page table ops
    Some functions and variables used by the test need to have extern
    linkage, otherwise keep them static
    Run the test one time for each version
    
    Bug: 255731794
    Change-Id: I4b5f910361fd83a9abc1052753c2a1126150d3ff
    Signed-off-by: Mostafa Saleh <smostafa@google.com>

--- arch/arm64/kvm/iommu/test_kvm_s2mpu.c
+++ arch/arm64/kvm/iommu/test_kvm_s2mpu.c
@@ -11,6 +11,8 @@
 #include <linux/slab.h>
 
 #include <asm/kvm_s2mpu.h>
+#include <asm/io-pgtable-s2mpu.h>
+
 
 KSTM_MODULE_GLOBALS();
 
@@ -23,12 +25,25 @@ KSTM_MODULE_GLOBALS();
 		}							\
 	} while (0)
 
+
+void __set_fmpt_range(struct fmpt *fmpt, size_t start_gb_byte,
+				    size_t end_gb_byte, enum mpt_prot prot);
+bool __is_smpt_uniform(u32 *smpt, enum mpt_prot prot);
+void __set_smpt_range(u32 *smpt, size_t start_gb_byte,
+				    size_t end_gb_byte, enum mpt_prot prot);
+void __set_smpt_range_slow(u32 *smpt, size_t start_gb_byte,
+					 size_t end_gb_byte, enum mpt_prot prot);
+
+extern const u64 *config_lut_prot;
+extern u32 config_prot_bits;
+static  const struct s2mpu_pgtable_ops *ops;
 static struct fmpt g_fmpt;
-static u32 g_smpt[SMPT_NUM_WORDS];
+//reserve memory for bigger SMPT
+static u32 g_smpt[SMPT_NUM_WORDS(V9_MPT_PROT_BITS)];
 
 static void __init init_smpt(enum mpt_prot prot)
 {
-	memset(g_smpt, (char)mpt_prot_doubleword[prot], SMPT_SIZE);
+	memset(g_smpt, (char)config_lut_prot[prot], SMPT_SIZE(config_prot_bits));
 }
 
 static void __init init_fmpt(enum mpt_prot prot, bool gran_1g)
@@ -44,8 +59,8 @@ static void __init init_fmpt(enum mpt_prot prot, bool gran_1g)
 static enum mpt_prot __init get_prot_at(size_t gb_byte_off)
 {
 	size_t page_idx = gb_byte_off / SMPT_GRAN;
-	size_t word_idx = page_idx / SMPT_ELEMS_PER_WORD;
-	size_t bit_shift = (page_idx % SMPT_ELEMS_PER_WORD) * MPT_PROT_BITS;
+	size_t word_idx = page_idx / SMPT_ELEMS_PER_WORD(config_prot_bits);
+	size_t bit_shift = (page_idx % SMPT_ELEMS_PER_WORD(config_prot_bits)) * config_prot_bits;
 
 	return (g_smpt[word_idx] >> bit_shift) & MPT_PROT_MASK;
 }
@@ -106,8 +121,8 @@ static int __init test_set_fmpt__fmpt_no_change_partial(void)
 /* Convert from 1G to PAGE_SIZE granule. */
 static int __init test_set_fmpt__fmpt_to_smpt(void)
 {
-	size_t start = 5 * SMPT_WORD_BYTE_RANGE / 2;
-	size_t end = 20 * SMPT_WORD_BYTE_RANGE;
+	size_t start = 5 * SMPT_WORD_BYTE_RANGE(config_prot_bits) / 2;
+	size_t end = 20 * SMPT_WORD_BYTE_RANGE(config_prot_bits);
 
 	init_fmpt(MPT_PROT_R, /*gran_1g*/ true);
 	__set_fmpt_range(&g_fmpt, start, end, MPT_PROT_RW);
@@ -130,9 +145,8 @@ static int __init test_set_fmpt__smpt_to_fmpt_whole(void)
 /* Convert from PAGE_SIZE to 1G granule by making the SMPT uniform. */
 static int __init test_set_fmpt__smpt_to_fmpt_partial(void)
 {
-	size_t start = 5 * SMPT_WORD_BYTE_RANGE / 2;
-	size_t end = 20 * SMPT_WORD_BYTE_RANGE;
-
+	size_t start = 5 * SMPT_WORD_BYTE_RANGE(config_prot_bits) / 2;
+	size_t end = 20 * SMPT_WORD_BYTE_RANGE(config_prot_bits);
 	/* Create SMPT with all PROT_W except a small subrange. */
 	init_fmpt(MPT_PROT_W, /*gran_1g*/ false);
 	__set_smpt_range(g_smpt, start, end, MPT_PROT_RW);
@@ -172,42 +186,42 @@ static int __init __test_set_smpt(size_t start_byte, size_t end_byte)
 /* Range within one SMPT word, force a fallback to __set_smpt_range_slow. */
 static int __init test_set_smpt__within_one_word(void)
 {
-	return __test_set_smpt(3 * SMPT_WORD_BYTE_RANGE + 5 * PAGE_SIZE,
-			       3 * SMPT_WORD_BYTE_RANGE + 6 * PAGE_SIZE);
+	return __test_set_smpt(3 * SMPT_WORD_BYTE_RANGE(config_prot_bits) + 5 * PAGE_SIZE,
+			       3 * SMPT_WORD_BYTE_RANGE(config_prot_bits) + 6 * PAGE_SIZE);
 }
 
 /* No whole SMPT word, force a fallback to __set_smpt_range_slow. */
 static int __init test_set_smpt__no_whole_word(void)
 {
-	return __test_set_smpt(3 * SMPT_WORD_BYTE_RANGE + 5 * PAGE_SIZE,
-			       4 * SMPT_WORD_BYTE_RANGE + 2 * PAGE_SIZE);
+	return __test_set_smpt(3 * SMPT_WORD_BYTE_RANGE(config_prot_bits) + 5 * PAGE_SIZE,
+			       4 * SMPT_WORD_BYTE_RANGE(config_prot_bits) + 2 * PAGE_SIZE);
 }
 
 /* Both start and end aligned to SMPT word. */
 static int __init test_set_smpt__no_prologue_or_epilogue(void)
 {
-	return __test_set_smpt(10 * SMPT_WORD_BYTE_RANGE,
-			       20 * SMPT_WORD_BYTE_RANGE);
+	return __test_set_smpt(10 * SMPT_WORD_BYTE_RANGE(config_prot_bits),
+			       20 * SMPT_WORD_BYTE_RANGE(config_prot_bits));
 }
 
 /* Start not aligned to SMPT word. */
 static int __init test_set_smpt__prologue(void)
 {
-	return __test_set_smpt(17 * SMPT_WORD_BYTE_RANGE / 2,
-			       20 * SMPT_WORD_BYTE_RANGE);
+	return __test_set_smpt(17 * SMPT_WORD_BYTE_RANGE(config_prot_bits) / 2,
+			       20 * SMPT_WORD_BYTE_RANGE(config_prot_bits));
 }
 
 /* End not aligned to SMPT word. */
 static int __init test_set_smpt__epilogue(void)
 {
-	return __test_set_smpt(0, 17 * SMPT_WORD_BYTE_RANGE / 2);
+	return __test_set_smpt(0, 17 * SMPT_WORD_BYTE_RANGE(config_prot_bits) / 2);
 }
 
 /* Neither start nor end aligned to SMPT word. */
 static int __init test_set_smpt__prologue_and_epilogue(void)
 {
-	return __test_set_smpt(17 * SMPT_WORD_BYTE_RANGE / 2,
-			       31 * SMPT_WORD_BYTE_RANGE / 2);
+	return __test_set_smpt(17 * SMPT_WORD_BYTE_RANGE(config_prot_bits) / 2,
+			       31 * SMPT_WORD_BYTE_RANGE(config_prot_bits) / 2);
 }
 
 static int __init __test_set_smpt_slow(size_t start_byte, size_t end_byte)
@@ -219,41 +233,40 @@ static int __init __test_set_smpt_slow(size_t start_byte, size_t end_byte)
 
 static int __init test_set_smpt_slow__empty_word_align(void)
 {
-	return __test_set_smpt_slow(3 * SMPT_WORD_BYTE_RANGE,
-				    3 * SMPT_WORD_BYTE_RANGE);
+	return __test_set_smpt_slow(3 * SMPT_WORD_BYTE_RANGE(config_prot_bits),
+				    3 * SMPT_WORD_BYTE_RANGE(config_prot_bits));
 }
 
 static int __init test_set_smpt_slow__empty_page_align(void)
 {
-	return __test_set_smpt_slow(3 * SMPT_WORD_BYTE_RANGE + PAGE_SIZE,
-				    3 * SMPT_WORD_BYTE_RANGE + PAGE_SIZE);
+	return __test_set_smpt_slow(3 * SMPT_WORD_BYTE_RANGE(config_prot_bits) + PAGE_SIZE,
+				    3 * SMPT_WORD_BYTE_RANGE(config_prot_bits) + PAGE_SIZE);
 }
 
 static int __init test_set_smpt_slow__one_whole_word(void)
 {
-	return __test_set_smpt_slow(3 * SMPT_WORD_BYTE_RANGE,
-				    4 * SMPT_WORD_BYTE_RANGE);
+	return __test_set_smpt_slow(3 * SMPT_WORD_BYTE_RANGE(config_prot_bits),
+				    4 * SMPT_WORD_BYTE_RANGE(config_prot_bits));
 }
 
 static int __init test_set_smpt_slow__one_partial_word(void)
 {
-	return __test_set_smpt_slow(3 * SMPT_WORD_BYTE_RANGE + PAGE_SIZE,
-				    4 * SMPT_WORD_BYTE_RANGE - PAGE_SIZE);
+	return __test_set_smpt_slow(3 * SMPT_WORD_BYTE_RANGE(config_prot_bits) + PAGE_SIZE,
+				    4 * SMPT_WORD_BYTE_RANGE(config_prot_bits) - PAGE_SIZE);
 }
 
 static int __init test_set_smpt_slow__multiple_whole_words(void)
 {
-	return __test_set_smpt_slow(13 * SMPT_WORD_BYTE_RANGE,
-				    17 * SMPT_WORD_BYTE_RANGE);
+	return __test_set_smpt_slow(13 * SMPT_WORD_BYTE_RANGE(config_prot_bits),
+				    17 * SMPT_WORD_BYTE_RANGE(config_prot_bits));
 }
 
 static int __init test_set_smpt_slow__multiple_partial_words(void)
 {
-	return __test_set_smpt_slow((13 * 2 + 1) * SMPT_WORD_BYTE_RANGE / 2,
-				    (17 * 4 + 1) * SMPT_WORD_BYTE_RANGE / 4);
+	return __test_set_smpt_slow((13 * 2 + 1) * SMPT_WORD_BYTE_RANGE(config_prot_bits) / 2,
+				    (17 * 4 + 1) * SMPT_WORD_BYTE_RANGE(config_prot_bits) / 4);
 }
-
-static void __init selftest(void)
+void run_tests(void)
 {
 	KSTM_CHECK_ZERO(test_set_fmpt__fmpt_to_fmpt_whole());
 	KSTM_CHECK_ZERO(test_set_fmpt__fmpt_no_change_whole());
@@ -276,6 +289,20 @@ static void __init selftest(void)
 	KSTM_CHECK_ZERO(test_set_smpt_slow__one_partial_word());
 	KSTM_CHECK_ZERO(test_set_smpt_slow__multiple_whole_words());
 	KSTM_CHECK_ZERO(test_set_smpt_slow__multiple_partial_words());
+}
+
+static void __init selftest(void)
+{
+	struct s2mpu_pgtable_cfg cfg;
+	pr_info("Testing V9");
+	cfg.version = 0x90000000;
+	ops = s2mpu_alloc_pgtable_ops(cfg);
+	run_tests();
+	pr_info("Testing V1-V2");
+	cfg.version = 0x20000000;
+	ops = s2mpu_alloc_pgtable_ops(cfg);
+	run_tests();
+	pr_info("Done");
 }
 
 KSTM_MODULE_LOADERS(test_kvm_s2mpu);
