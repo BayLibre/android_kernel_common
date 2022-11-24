3f2478010361a343ff12427c7bedecd35f46b03e
Vincent Donnefort <vdonnefort@google.com>
2022-11-24 15:23:26 +0000

    ANDROID: KVM: arm64: Block module loading based on cmdline or HVC
    Disable module loading, based on the cmdline option
    kvm-arm.protected_modules. If enabled, the feature can later be
    irreversibly disabled with the HVC __pkvm_close_module_registration().
    
    Bug: 244373730
    Change-Id: I693a1db350ae15bcaab59103a68b60f087dcc6b4
    Signed-off-by: Vincent Donnefort <vdonnefort@google.com>

--- arch/arm64/kvm/hyp/include/nvhe/modules.h
+++ arch/arm64/kvm/hyp/include/nvhe/modules.h
@@ -7,6 +7,10 @@
 int __pkvm_init_module(void *module_init);
 int __pkvm_register_hcall(unsigned long hfn_hyp_va);
 int handle_host_dynamic_hcall(struct kvm_cpu_context *host_ctxt);
+void pkvm_modules_lock(void);
+void pkvm_modules_unlock(void);
+bool pkvm_modules_enabled(void);
+int __pkvm_close_module_registration(void);
 #else
 static inline int __pkvm_init_module(void *module_init); { return -EOPNOTSUPP; }
 static inline int
@@ -15,4 +19,8 @@ static inline int handle_host_dynamic_hcall(struct kvm_cpu_context *host_ctxt)
 {
 	return HCALL_UNHANDLED;
 }
+void pkvm_modules_lock(void) { }
+void pkvm_modules_unlock(void) { }
+bool pkvm_modules_enabled(void) { return false; }
+int __pkvm_close_module_registration(void) { return -EOPNOTSUPP; }
 #endif
