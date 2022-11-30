cb3e04ef9938833ff7abc603a7503928c0ff6e65
Asutosh Das <quic_asutoshd@quicinc.com>
2022-11-30 15:50:36 -0800

    UPSTREAM: ufs: core: Defer adding host to scsi if mcq is supported
    If MCQ support is present, enabling it after MCQ support
    has been configured would require reallocating tags and memory.
    It would also free up the already allocated memory in
    Single Doorbell Mode. So defer invoking scsi_add_host() until
    MCQ is configured.
    
    Co-developed-by: Can Guo <quic_cang@quicinc.com>
    Change-Id: Ie5e8e122c0aad81ac6f595c79d0c7afb3bd47fbf
    Signed-off-by: Can Guo <quic_cang@quicinc.com>
    Signed-off-by: Asutosh Das <quic_asutoshd@quicinc.com>
    Reviewed-by: Bart Van Assche <bvanassche@acm.org>
    Reviewed-by: Manivannan Sadhasivam <mani@kernel.org>
    Link: https://lore.kernel.org/linux-scsi/1e64cece4de5f01c05a3d157f7afcdffa5044e04.1670541364.git.quic_asutoshd@quicinc.com/
    Signed-off-by: SEO HOYOUNG <hy50.seo@samsung.com>

--- drivers/ufs/core/ufshcd.c
+++ drivers/ufs/core/ufshcd.c
@@ -95,6 +95,11 @@
 /* UFSHC 4.0 compliant HC support this mode, refer param_set_mcq_mode() */
 static bool use_mcq_mode = true;
 
+static bool is_mcq_supported(struct ufs_hba *hba)
+{
+        return hba->mcq_sup && use_mcq_mode;
+}
+
 static int param_set_mcq_mode(const char *val, const struct kernel_param *kp)
 {
 	int ret;
@@ -8299,6 +8304,7 @@ static int ufshcd_probe_hba(struct ufs_hba *hba, bool init_dev_params)
 	int ret;
 	unsigned long flags;
 	ktime_t start = ktime_get();
+	struct Scsi_Host *host = hba->host;
 
 	hba->ufshcd_state = UFSHCD_STATE_RESET;
 
@@ -8333,6 +8339,14 @@ static int ufshcd_probe_hba(struct ufs_hba *hba, bool init_dev_params)
 		ret = ufshcd_device_params_init(hba);
 		if (ret)
 			goto out;
+
+		if (is_mcq_supported(hba)) {
+			ret = scsi_add_host(host, hba->dev);
+			if (ret) {
+				dev_err(hba->dev, "scsi_add_host failed\n");
+				goto out;
+			}
+		}
 	}
 
 	ufshcd_tune_unipro_params(hba);
@@ -9897,10 +9911,12 @@ int ufshcd_init(struct ufs_hba *hba, void __iomem *mmio_base, unsigned int irq)
 		hba->is_irq_enabled = true;
 	}
 
-	err = scsi_add_host(host, hba->dev);
-	if (err) {
-		dev_err(hba->dev, "scsi_add_host failed\n");
-		goto out_disable;
+	if (!is_mcq_supported(hba)) {
+		err = scsi_add_host(host, hba->dev);
+		if (err) {
+			dev_err(hba->dev, "scsi_add_host failed\n");
+			goto out_disable;
+		}
 	}
 
 	hba->tmf_tag_set = (struct blk_mq_tag_set) {
