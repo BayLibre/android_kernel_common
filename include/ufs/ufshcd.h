ca3b4eba8f1fc100c70f614f1eb52ebff266f560
Bart Van Assche <bvanassche@google.com>
2022-12-02 12:46:59 -0800

    ANDROID: scsi: ufs: exynos: Enable BLK_SUB_PAGE_SEGMENTS for large page sizes
    Bug: 261097222
    Change-Id: I7166be4017293d328f001b3818fed64a0a4a4cfe
    Signed-off-by: Bart Van Assche <bvanassche@google.com>

--- include/ufs/ufshcd.h
+++ include/ufs/ufshcd.h
@@ -629,6 +629,15 @@ enum ufshcd_quirks {
 	 * thus need this quirk to skip related flow.
 	 */
 	UFSHCD_QUIRK_MCQ_BROKEN_RTC			= 1 << 21,
+
+	/*
+	 * The host page size exceeds the maximum DMA segment size.
+	 * Since the UFSHCI standard requires that host controllers support
+	 * values up to 256 KiB in the Data Byte Count (DBC) and Common Data
+	 * Size (CDS) fields, this quirk only needs to be set for non-compliant
+	 * host controllers.
+	 */
+	UFSHCD_QUIRK_SUB_PAGE_SEGMENTS			= 1 << 23,
 };
 
 enum ufshcd_android_quirks {
@@ -661,6 +670,7 @@ enum ufshcd_android_quirks {
 	 * keys were stored in it.
 	 */
 	UFSHCD_ANDROID_QUIRK_KEYS_IN_PRDT		= 1 << 2,
+
 };
 
 enum ufshcd_caps {
