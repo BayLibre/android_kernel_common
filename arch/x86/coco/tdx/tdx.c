6093dcc230c0ce134ca85f05073b795a0228f039
Shaoqin Huang <shaoqin.huang@intel.com>
2022-12-07 00:36:12 -0800

    ANDROID: x86/tdx: Add ve_x86_ops to wrap different operation for virt exception
    Define a ve_x86_ops to provide different handler for virt exception
    related function. Later will add virt exception support for pkvm guest,
    which will use this ve_x86_ops.
    
    Bug: 395299836
    Test: Boot, verify cpus are de-privileged and run a minimal protected vm.
    
    Change-Id: Iaacae5e4c5d9a512045427d8bfb22bfad52a9422
    Signed-off-by: Shaoqin Huang <shaoqin.huang@intel.com>
    Reviewed-by: Jason Chen CJ <jason.cj.chen@intel.com>
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>
    
     Conflicts:
    	arch/x86/include/asm/tdx.h
    
    Change-Id: I7d76bbcebec409fb89bec6cbf89f26bac98ddd2e
    Signed-off-by: Grzegorz Jaszczyk <jaszczyk@google.com>

--- arch/x86/coco/tdx/tdx.c
+++ arch/x86/coco/tdx/tdx.c
@@ -512,7 +512,7 @@ static int handle_cpuid(struct pt_regs *regs, struct ve_info *ve)
 	return ve_instr_len(ve);
 }
 
-bool mmio_read(int size, unsigned long addr, unsigned long *val)
+static bool mmio_read(int size, unsigned long addr, unsigned long *val)
 {
 	struct tdx_module_args args = {
 		.r10 = TDX_HYPERCALL_STANDARD,
@@ -529,7 +529,7 @@ bool mmio_read(int size, unsigned long addr, unsigned long *val)
 	return true;
 }
 
-bool mmio_write(int size, unsigned long addr, unsigned long val)
+static bool mmio_write(int size, unsigned long addr, unsigned long val)
 {
 	return !_tdx_hypercall(hcall_func(EXIT_REASON_EPT_VIOLATION), size,
 			       EPT_WRITE, addr, val);
@@ -710,7 +710,7 @@ static int virt_exception_kernel(struct pt_regs *regs, struct ve_info *ve)
 	}
 }
 
-bool tdx_handle_virt_exception(struct pt_regs *regs, struct ve_info *ve)
+static bool tdx_handle_virt_exception(struct pt_regs *regs, struct ve_info *ve)
 {
 	int insn_len;
 
@@ -1043,5 +1043,13 @@ void __init tdx_early_init(void)
 	 */
 	x86_cpuinit.parallel_bringup = false;
 
+	//JAZ: TODO: it was not here in 6.12
 	tdx_announce();
+
+	ve_x86_ops.mmio_read = mmio_read;
+	ve_x86_ops.mmio_write = mmio_write;
+	ve_x86_ops.handle_virt_exception = tdx_handle_virt_exception;
+	ve_x86_ops.get_ve_info = tdx_get_ve_info;
+
+	pr_info("Guest detected\n");
 }
