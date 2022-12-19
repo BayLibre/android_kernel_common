93cb1328dc23d02604bee82f73a1f02761f6a32d
Rick Yiu <rickyiu@google.com>
2022-12-19 07:02:04 +0000

    ANDROID: topology: Add vendor hook for use_amu_fie
    Vendors could decide to use amu fie feature or not, such as if they
    have some feature that needs amu fie to be disabled.
    
    Bug: 344826816
    Bug: 297343949
    Bug: 261753861
    Change-Id: I6b44ee899c58e7d5a8b2db03a30e3ab14b08f101
    Signed-off-by: Rick Yiu <rickyiu@google.com>
    (cherry picked from commit 2b25d535d0703dbee0f9860dfd2c487e96a89fd8)
    [Fix trivial conflict in vendor_hooks.c due to different ordering]
    Signed-off-by: Qais Yousef <qyousef@google.com>
    (cherry picked from commit 2ab1628371a2ba59db19cbec7f05146dded9a815)

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -287,3 +287,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_exit_signal_whether_wake);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_exit_check);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_freeze_whether_wake);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_regmap_update);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_use_amu_fie);
