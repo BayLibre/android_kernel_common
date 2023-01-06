75758a118fc3216021cd94b49f8d5fdc5cae0a1f
Quentin Perret <qperret@google.com>
2023-01-06 17:40:37 +0000

    ANDROID: KVM: arm64: Allow skipping module page donation
    __pkvm_map_module_page() currently goes through a full host-to-hyp
    donation for every module page being mapped. However, in order to allow
    mapping protected pages as modules, let's make this behaviour optional.
    
    Bug: 264070847
    Change-Id: Ic3b1b68f11283c215bc8c6e1352f91bcf52a3935
    Signed-off-by: Quentin Perret <qperret@google.com>

--- arch/arm64/kvm/hyp/nvhe/mm.c
+++ arch/arm64/kvm/hyp/nvhe/mm.c
@@ -118,7 +118,7 @@ void *__pkvm_alloc_module_va(u64 nr_pages)
 	return (void *)addr;
 }
 
-int __pkvm_map_module_page(u64 pfn, void *va, enum kvm_pgtable_prot prot)
+int __pkvm_map_module_page(u64 pfn, void *va, enum kvm_pgtable_prot prot, bool is_protected)
 {
 	unsigned long addr = (unsigned long)va;
 	int ret;
@@ -130,9 +130,11 @@ int __pkvm_map_module_page(u64 pfn, void *va, enum kvm_pgtable_prot prot)
 	 */
 	WARN_ON(addr < mod_range_start || mod_range_end <= addr);
 
-	ret = __pkvm_host_donate_hyp(pfn, 1);
-	if (ret)
-		return ret;
+	if (!is_protected) {
+		ret = __pkvm_host_donate_hyp(pfn, 1);
+		if (ret)
+			return ret;
+	}
 
 	ret = __pkvm_create_mappings(addr, PAGE_SIZE, hyp_pfn_to_phys(pfn), prot);
 	if (ret) {
