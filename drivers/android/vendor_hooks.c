e9d9c895130e333c7cdf714727a11d76a9eee8ff
xiaofeng <xiaofeng5@xiaomi.com>
2023-01-09 11:16:22 +0800

    ANDROID: vendor_hooks:vendor hook for __alloc_pages_slowpath()
    add vendor hook in __alloc_pages_slowpath() to defer some unimportant processes from directly memory reclaiming.
    
    Bug: 264627599
    Change-Id: I6d49dd373c07ce66eea50f6e007fefe7d4f07c29
    Signed-off-by: xiaofeng <xiaofeng5@xiaomi.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -413,6 +413,8 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_skip_swap_map_write);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_post_image_save);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_dm_update_clone_bio);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_ctl_dirty_rate);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_alloc_pages_reclaim_defer);
+
 /*
  * For type visibility
  */
