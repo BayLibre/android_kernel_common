ee20d1ee2459a271a2006baef341e5f3cf355d42
Huang Yiwei <quic_hyiwei@quicinc.com>
2023-01-10 17:29:51 +0800

    ANDROID: hung_task: Add vendor hook for hung task detect
    Add vendor hook for hung task detect, so we can decide which
    threads need to check, avoiding false alarms.
    
    Bug: 188684133
    Change-Id: I5d7dfeb071cbfda8121134c38a458202aaa3a8c6
    Signed-off-by: Huang Yiwei <quic_hyiwei@quicinc.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -42,6 +42,7 @@
 #include <trace/hooks/futex.h>
 #include <trace/hooks/fips140.h>
 #include <trace/hooks/timer.h>
+#include <trace/hooks/hung_task.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -126,3 +127,5 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_aes_expandkey);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_aes_encrypt);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_aes_decrypt);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_timer_calc_index);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_check_uninterruptible_tasks);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_check_uninterruptible_tasks_dn);
