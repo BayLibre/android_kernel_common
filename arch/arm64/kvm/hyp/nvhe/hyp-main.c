maltesermailo/android_kernel_pantah

ANDROID: KVM: arm64: Add EL2 wakeup cause hyp events
To bring some visibility to what is causing the hypervisor to wakeup, bring
3 new hypervisor events:

  * host issued HVC64 (host_hcall)
  * host issued SMC (host_smc)
  * host mem abort (host_mem_abort)

Bug: 229972309
Change-Id: I920ff7efa59b12b63bea4637ddd91b7cba614854
Signed-off-by: Vincent Donnefort <vdonnefort@google.com>

arch/arm64/kvm/hyp/nvhe/hyp-main.c
@@ -1156,8 +1156,11 @@ static void handle_host_hcall(struct kvm_cpu_context *host_ctxt)
 	cpu_reg(host_ctxt, 0) = SMCCC_RET_SUCCESS;
 	hfn(host_ctxt);
 
+	trace_hyp_host_hcall(id, 0);
+
 	return;
 inval:
+	trace_hyp_host_hcall(id, 1);
 	cpu_reg(host_ctxt, 0) = SMCCC_RET_NOT_SUPPORTED;
 }
 
@@ -1168,11 +1171,14 @@ static void default_host_smc_handler(struct kvm_cpu_context *host_ctxt)
 
 static void handle_host_smc(struct kvm_cpu_context *host_ctxt)
 {
+	DECLARE_REG(u64, func_id, host_ctxt, 0);
 	bool handled;
 
 	handled = kvm_host_psci_handler(host_ctxt);
 	if (!handled)
 		default_host_smc_handler(host_ctxt);
+
+	trace_hyp_host_smc(func_id, !handled);
 
 	/* SMC was trapped, move ELR past the current PC. */
 	kvm_skip_host_instr();

arch/arm64/kvm/hyp/nvhe/mem_protect.c
@@ -7,6 +7,7 @@
 #include <linux/kvm_host.h>
 #include <asm/kvm_emulate.h>
 #include <asm/kvm_hyp.h>
+#include <asm/kvm_hypevents.h>
 #include <asm/kvm_mmu.h>
 #include <asm/kvm_pgtable.h>
 #include <asm/kvm_pkvm.h>
@@ -608,6 +609,8 @@ void handle_host_mem_abort(struct kvm_cpu_context *host_ctxt)
 		host_inject_abort(host_ctxt);
 	else
 		BUG_ON(ret && ret != -EAGAIN);
+
+	trace_hyp_host_mem_abort(esr, addr);
 }
 
 struct pkvm_mem_transition {

BayLibre/android_kernel_common

arch/arm64/kvm/hyp/nvhe/hyp-main.c
@@ -1348,13 +1348,17 @@ static void handle_host_hcall(struct kvm_cpu_context *host_ctxt)
 	cpu_reg(host_ctxt, 0) = SMCCC_RET_SUCCESS;
 	hfn(host_ctxt);
 
+	trace_host_hcall(id, 0);
+
 	return;
 inval:
+	trace_host_hcall(id, 1);
 	cpu_reg(host_ctxt, 0) = SMCCC_RET_NOT_SUPPORTED;
 }
 
 static void handle_host_smc(struct kvm_cpu_context *host_ctxt)
 {
+	DECLARE_REG(u64, func_id, host_ctxt, 0);
 	bool handled;
 
 	handled = kvm_host_psci_handler(host_ctxt);
@@ -1364,6 +1368,8 @@ static void handle_host_smc(struct kvm_cpu_context *host_ctxt)
 		handled = default_host_smc_handler(host_ctxt);
 	if (!handled)
 		__kvm_hyp_host_forward_smc(host_ctxt);
+
+	trace_host_smc(func_id, !handled);
 
 	/* SMC was trapped, move ELR past the current PC. */
 	kvm_skip_host_instr();

arch/arm64/kvm/hyp/nvhe/mem_protect.c
@@ -7,6 +7,7 @@
 #include <linux/kvm_host.h>
 #include <asm/kvm_emulate.h>
 #include <asm/kvm_hyp.h>
+#include <asm/kvm_hypevents.h>
 #include <asm/kvm_mmu.h>
 #include <asm/kvm_pgtable.h>
 #include <asm/kvm_pkvm.h>
@@ -822,6 +823,8 @@ void handle_host_mem_abort(struct kvm_cpu_context *host_ctxt)
 		host_inject_abort(host_ctxt);
 	else
 		BUG_ON(ret && ret != -EAGAIN);
+
+	trace_host_mem_abort(esr, addr);
 }
 
 struct pkvm_mem_transition {