653900c643692bf0ebe62eaead1ff24928f89da2
Quentin Perret <qperret@google.com>
2023-01-23 16:16:18 +0000

    ANDROID: KVM: arm64: Provide overide registers at EL2
    We will soon need to access the *_overide variants of certain system
    registers from the nVHE EL2 code, so let's copy them as we do for their
    sanitized counterparts.
    
    Bug: 266405428
    Bug: 265901597
    Change-Id: Ief25545c8d79bf2913eb2a6f8302bbbc35bef8b0
    Signed-off-by: Quentin Perret <qperret@google.com>

--- arch/arm64/kvm/hyp/nvhe/sys_regs.c
+++ arch/arm64/kvm/hyp/nvhe/sys_regs.c
@@ -27,6 +27,10 @@ u64 id_aa64mmfr0_el1_sys_val;
 u64 id_aa64mmfr1_el1_sys_val;
 u64 id_aa64mmfr2_el1_sys_val;
 
+struct arm64_ftr_override id_aa64pfr0_override;
+struct arm64_ftr_override id_aa64pfr1_override;
+struct arm64_ftr_override id_aa64smfr0_override;
+
 /*
  * Inject an unknown/undefined exception to an AArch64 guest while most of its
  * sysregs are live.
