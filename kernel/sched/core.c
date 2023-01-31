d388db3ff1f5f807679757ffe45ff23444687eda
David Dai <davidai@google.com>
2023-01-30 16:28:52 -0800

    sched/fair: Add util_guest for tasks
    For virtualization usecases, task utilization signals currently tracked
    on the host aren't sufficient to accurately represent the workload on
    vCPU threads, which results in poor frequency selection and poor
    performance.
    
    Introduce a guest_util sched property for guest OSes to be able to
    provide utilization hints to the scheduler.
    
    Change-Id: I7b1d3c6c33a5bbe2675fd150d8f82785fa3d5cf2
    Signed-off-by: David Dai <davidai@google.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1974,6 +1974,15 @@ static void __setscheduler_uclamp(struct task_struct *p,
 	}
 }
 
+static void __setscheduler_task_util(struct task_struct *p,
+				  const struct sched_attr *attr)
+{
+
+	if (likely(!(attr->sched_flags & SCHED_FLAG_UTIL_GUEST)))
+		return;
+
+	p->se.avg.util_guest = attr->sched_util_min;
+}
 static void uclamp_fork(struct task_struct *p)
 {
 	enum uclamp_id clamp_id;
@@ -7652,7 +7661,7 @@ recheck:
 			return -EINVAL;
 	}
 
-	if (attr->sched_flags & ~(SCHED_FLAG_ALL | SCHED_FLAG_SUGOV))
+	if (attr->sched_flags & ~(SCHED_FLAG_ALL | SCHED_FLAG_SUGOV | SCHED_FLAG_UTIL_GUEST))
 		return -EINVAL;
 
 	/*
@@ -7672,6 +7681,9 @@ recheck:
 			return retval;
 
 		if (attr->sched_flags & SCHED_FLAG_SUGOV)
+			return -EINVAL;
+
+		if (attr->sched_flags & SCHED_FLAG_UTIL_GUEST)
 			return -EINVAL;
 
 		retval = security_task_setscheduler(p);
@@ -7716,6 +7728,8 @@ recheck:
 		if (dl_policy(policy) && dl_param_changed(p, attr))
 			goto change;
 		if (attr->sched_flags & SCHED_FLAG_UTIL_CLAMP)
+			goto change;
+		if (attr->sched_flags & SCHED_FLAG_UTIL_GUEST)
 			goto change;
 
 		p->sched_reset_on_fork = reset_on_fork;
@@ -7804,6 +7818,7 @@ change:
 		__setscheduler_prio(p, newprio);
 	}
 	__setscheduler_uclamp(p, attr);
+	__setscheduler_task_util(p, attr);
 
 	if (queued) {
 		/*
