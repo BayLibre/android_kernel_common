e314bc7cce30a31e49f461ed8fed583288cc9f96
Vincent Donnefort <vdonnefort@google.com>
2023-02-10 10:21:49 +0000

    ANDROID: KVM: arm64: Header file for hyp event definition in nVHE
    Currently, hyp events are only generated from the host/hyp common file
    kvm_hypevents.h. In preparation to support in-module declared hyp
    events, move the definition macros into a dedicated file:
    define_events.h.
    
    That new header file is for nVHE side hyp event definitions, using the
    event list from HYP_EVENT_FILE.
    
      e.g. #define HYP_EVENT_FILE my_events.h
           #include <nvhe/define_events.h>
    
    This change has been heavily inspired by tracefs definitions and is a
    preparation to support in-module declared events. In-module declared
    events are events that are declared in the module that defines the
    event. This is in contrast to the current approach, where events are
    declared in a separate header file.
    
    Bug: 268495982
    Change-Id: I0c6b1d30a0a1ec680e8a6909b67ca677ca865421
    Signed-off-by: Vincent Donnefort <vdonnefort@google.com>

--- arch/arm64/kvm/hyp/nvhe/events.c
+++ arch/arm64/kvm/hyp/nvhe/events.c
@@ -3,20 +3,13 @@
  * Copyright (C) 2023 Google LLC
  */
 
-#include <nvhe/trace.h>
 #include <nvhe/mm.h>
+#include <nvhe/trace.h>
+
+#include <nvhe/define_events.h>
 
 extern struct hyp_event_id __hyp_event_ids_start[];
 extern struct hyp_event_id __hyp_event_ids_end[];
-
-#undef HYP_EVENT
-#define HYP_EVENT(__name, __proto, __struct, __assign, __printk)	\
-	atomic_t __ro_after_init __name##_enabled = ATOMIC_INIT(0);	\
-	struct hyp_event_id hyp_event_id_##__name __section(".hyp.event_ids") = {	\
-		.data = (void *)&__name##_enabled,			\
-	}
-
-#include <asm/kvm_hypevents.h>
 
 int __pkvm_enable_event(unsigned short id, bool enable)
 {
