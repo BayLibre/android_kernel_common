dc99cd80a3f4164d1d4bc89f4c035e2a14591cbc
Vincent Donnefort <vdonnefort@google.com>
2023-02-10 10:28:11 +0000

    ANDROID: KVM: arm64: Header file for hyp event definition in host
    Currently, hyp events are only generated from the host/hyp common file
    kvm_hypevents.h. In preparation to support in-module declared hyp
    events, move the definition macros into a dedicated file:
    kvm_define_hypevents.h.
    
    That new header file is for the host side hyp event definitions, using
    the event list from HYP_EVENT_FILE.
    
          e.g. #define HYP_EVENT_FILE my_events.h
    	   #include <asm/kvm_define_hypevents.h>
    
    This change has been heavily inspired by tracefs definitions and is a
    preparation to support in-module declared events. This will give tracing
    flexibility to pKVM module developers.
    
    Bug: 268495982
    Change-Id: I858125f4140227ec11cbb3042dfd2ebe5fe2c13b
    Signed-off-by: Vincent Donnefort <vdonnefort@google.com>

--- arch/arm64/kvm/hyp_events.c
+++ arch/arm64/kvm/hyp_events.c
@@ -4,83 +4,10 @@
  */
 
 #include <linux/tracefs.h>
-#include <linux/trace_events.h>
 
 #include <asm/kvm_host.h>
-#include <asm/kvm_hypevents_defs.h>
+#include <asm/kvm_define_hypevents.h>
 #include <asm/setup.h>
-
-#include "hyp_trace.h"
-
-#define HYP_EVENT_NAME_MAX 32
-
-struct hyp_event {
-	struct trace_event_call *call;
-	char name[HYP_EVENT_NAME_MAX];
-	bool *enabled;
-};
-
-#define HYP_EVENT(__name, __proto, __struct, __assign, __printk)		\
-	HYP_EVENT_FORMAT(__name, __struct);					\
-	enum print_line_t hyp_event_trace_##__name(struct trace_iterator *iter,	\
-					  int flags, struct trace_event *event) \
-	{									\
-		struct ht_iterator *ht_iter = (struct ht_iterator *)iter;	\
-		struct trace_hyp_format_##__name __maybe_unused *__entry =	\
-			(struct trace_hyp_format_##__name *)ht_iter->ent;	\
-		trace_seq_puts(&ht_iter->seq, #__name);				\
-		trace_seq_putc(&ht_iter->seq, ' ');				\
-		trace_seq_printf(&ht_iter->seq, __printk);			\
-		trace_seq_putc(&ht_iter->seq, '\n');				\
-		return TRACE_TYPE_HANDLED;					\
-	}
-#include <asm/kvm_hypevents.h>
-
-#undef he_field
-#define he_field(_type, _item)						\
-	{								\
-		.type = #_type, .name = #_item,				\
-		.size = sizeof(_type), .align = __alignof__(_type),	\
-		.is_signed = is_signed_type(_type),			\
-	},
-#undef HYP_EVENT
-#define HYP_EVENT(__name, __proto, __struct, __assign, __printk)		\
-	static struct trace_event_fields hyp_event_fields_##__name[] = {	\
-		__struct							\
-		{}								\
-	};									\
-
-#undef __ARM64_KVM_HYPEVENTS_H_
-#include <asm/kvm_hypevents.h>
-
-#undef HYP_EVENT
-#undef HE_PRINTK
-#define __entry REC
-#define HE_PRINTK(fmt, args...) "\"" fmt "\", " __stringify(args)
-#define HYP_EVENT(__name, __proto, __struct, __assign, __printk)		\
-	static char hyp_event_print_fmt_##__name[] = __printk;			\
-	static struct trace_event_functions hyp_event_funcs_##__name = {	\
-		.trace = &hyp_event_trace_##__name,				\
-	};									\
-	static struct trace_event_class hyp_event_class_##__name = {		\
-		.system		= "nvhe-hypervisor",				\
-		.fields_array	= hyp_event_fields_##__name,			\
-		.fields		= LIST_HEAD_INIT(hyp_event_class_##__name.fields),\
-	};									\
-	static struct trace_event_call hyp_event_call_##__name = {		\
-		.class = &hyp_event_class_##__name,				\
-		.event.funcs = &hyp_event_funcs_##__name,			\
-		.print_fmt = hyp_event_print_fmt_##__name,			\
-	};									\
-	static bool hyp_event_enabled_##__name;					\
-	struct hyp_event __section("_hyp_events") hyp_event_##__name = {	\
-		.name = #__name,						\
-		.call = &hyp_event_call_##__name,				\
-		.enabled = &hyp_event_enabled_##__name,				\
-	}
-
-#undef __ARM64_KVM_HYPEVENTS_H_
-#include <asm/kvm_hypevents.h>
 
 extern struct hyp_event __hyp_events_start[];
 extern struct hyp_event __hyp_events_end[];
