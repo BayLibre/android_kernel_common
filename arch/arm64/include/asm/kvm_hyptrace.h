903634dfe0fb3760b5984cdbda5c3f2ea0beb1dd
Vincent Donnefort <vdonnefort@google.com>
2023-02-10 10:28:11 +0000

    ANDROID: KVM: arm64: Header file for hyp event definition in host
    Currently, hyp events are only generated from the host/hyp common file
    kvm_hypevents.h. In preparation to support in-module declared hyp
    events, move the definition macros into a dedicated file:
    kvm_define_hypevents.h.
    
    That new header file is for the host side hyp event definitions, using
    the event list from HYP_EVENT_FILE.
    
          e.g. #define HYP_EVENT_FILE my_events.h
    	   #include <asm/kvm_define_hypevents.h>
    
    This change has been heavily inspired by tracefs definitions and is a
    preparation to support in-module declared events. This will give tracing
    flexibility to pKVM module developers.
    
    Bug: 268495982
    Change-Id: I858125f4140227ec11cbb3042dfd2ebe5fe2c13b
    Signed-off-by: Vincent Donnefort <vdonnefort@google.com>

--- arch/arm64/include/asm/kvm_hyptrace.h
+++ arch/arm64/include/asm/kvm_hyptrace.h
@@ -3,6 +3,24 @@
 #include <asm/kvm_hyp.h>
 
 #include <linux/ring_buffer_ext.h>
+#include <linux/trace_seq.h>
+#include <linux/workqueue.h>
+
+struct ht_iterator {
+	struct ring_buffer_iter **buf_iter;
+	struct hyp_entry_hdr *ent;
+	struct trace_seq seq;
+	struct list_head list;
+	u64 ts;
+	void *spare;
+	size_t copy_leftover;
+	size_t ent_size;
+	struct delayed_work poke_work;
+	unsigned long lost_events;
+	cpumask_var_t cpus;
+	int ent_cpu;
+	int cpu;
+};
 
 /*
  * Host donations to the hypervisor to store the struct hyp_buffer_page.
