3f459462fdd6e95f80954f72ae84d70888052edb
Sebastian Ene <sebastianene@google.com>
2023-02-14 16:43:59 +0000

    ANDROID: KVM: arm64: Use the hypervisor lock for FF-A transactions
    Synchronize the access to the hypervisor FF-A buffers using the
    hypervisor buffers lock instead of the host lock. This is required to
    prevent concurrent access to the hypervisor pair of buffers when
    multipple VMs attempt to use them as part of an FF-A transaction.
    
    The locks from step 3,6 where replaced with a global ffa lock:
    
    1. Host driver copies data into the
    Host(RX/TX buffers mapped inside the Hypervisor)
    2. Host driver issues smc(..) to the Hypervisor
    3. Hypervisor locks the host buffers    <- LOCK ACQUIRED
    4. Hypervisor validates and copies the content from the
    Host(RX/TX buffers mapped inside the Hypervisor) to the Hyp(RX/TX buffers
    mapped inside EL3/SPMC)
    5. The call is forwarded to EL3
    6. Hypervisor unlocks the host buffers. <- LOCK RELEASED
    
    Bug: 269285339
    Bug: 278749606
    Change-Id: Id7f79ffc8d2bc20be591251991cd66e307746102
    Signed-off-by: Sebastian Ene <sebastianene@google.com>

--- arch/arm64/kvm/hyp/nvhe/ffa.c
+++ arch/arm64/kvm/hyp/nvhe/ffa.c
@@ -55,21 +55,20 @@ struct kvm_ffa_descriptor_buffer {
 static struct kvm_ffa_descriptor_buffer ffa_desc_buf;
 
 struct kvm_ffa_buffers {
-	hyp_spinlock_t lock;
 	void *tx;
 	void *rx;
 };
 
 /*
  * Note that we don't currently lock these buffers explicitly, instead
- * relying on the locking of the host FFA buffers as we only have one
- * client.
+ * relying on the locking of the hyp FFA buffers.
  */
 static struct kvm_ffa_buffers hyp_buffers;
 static struct kvm_ffa_buffers host_buffers;
 static u32 hyp_ffa_version;
 static bool has_version_negotiated;
 static hyp_spinlock_t version_lock;
+static hyp_spinlock_t kvm_ffa_hyp_lock;
 
 static void ffa_to_smccc_error(struct arm_smccc_res *res, u64 ffa_errno)
 {
@@ -196,7 +195,7 @@ static void do_ffa_rxtx_map(struct arm_smccc_res *res,
 		goto out;
 	}
 
-	hyp_spin_lock(&host_buffers.lock);
+	hyp_spin_lock(&kvm_ffa_hyp_lock);
 	if (host_buffers.tx) {
 		ret = FFA_RET_DENIED;
 		goto out_unlock;
@@ -232,7 +231,7 @@ static void do_ffa_rxtx_map(struct arm_smccc_res *res,
 	host_buffers.rx = rx_virt;
 
 out_unlock:
-	hyp_spin_unlock(&host_buffers.lock);
+	hyp_spin_unlock(&kvm_ffa_hyp_lock);
 out:
 	ffa_to_smccc_res(res, ret);
 	return;
@@ -258,7 +257,7 @@ static void do_ffa_rxtx_unmap(struct arm_smccc_res *res,
 		goto out;
 	}
 
-	hyp_spin_lock(&host_buffers.lock);
+	hyp_spin_lock(&kvm_ffa_hyp_lock);
 	if (!host_buffers.tx) {
 		ret = FFA_RET_INVALID_PARAMETERS;
 		goto out_unlock;
@@ -272,7 +271,7 @@ static void do_ffa_rxtx_unmap(struct arm_smccc_res *res,
 	WARN_ON(__pkvm_host_unshare_hyp(hyp_virt_to_pfn(host_buffers.rx)));
 	host_buffers.rx = NULL;
 out_unlock:
-	hyp_spin_unlock(&host_buffers.lock);
+	hyp_spin_unlock(&kvm_ffa_hyp_lock);
 out:
 	ffa_to_smccc_res(res, ret);
 }
@@ -363,7 +362,7 @@ static void do_ffa_mem_frag_tx(struct arm_smccc_res *res,
 	if (fraglen % sizeof(*buf))
 		goto out;
 
-	hyp_spin_lock(&host_buffers.lock);
+	hyp_spin_lock(&kvm_ffa_hyp_lock);
 	if (!host_buffers.tx)
 		goto out_unlock;
 
@@ -388,7 +387,7 @@ static void do_ffa_mem_frag_tx(struct arm_smccc_res *res,
 		WARN_ON(ffa_host_unshare_ranges(buf, nr_ranges));
 
 out_unlock:
-	hyp_spin_unlock(&host_buffers.lock);
+	hyp_spin_unlock(&kvm_ffa_hyp_lock);
 out:
 	if (ret)
 		ffa_to_smccc_res(res, ret);
@@ -431,7 +430,7 @@ static void __do_ffa_mem_xfer(const u64 func_id,
 		goto out;
 	}
 
-	hyp_spin_lock(&host_buffers.lock);
+	hyp_spin_lock(&kvm_ffa_hyp_lock);
 	if (!host_buffers.tx) {
 		ret = FFA_RET_INVALID_PARAMETERS;
 		goto out_unlock;
@@ -482,7 +481,7 @@ static void __do_ffa_mem_xfer(const u64 func_id,
 	}
 
 out_unlock:
-	hyp_spin_unlock(&host_buffers.lock);
+	hyp_spin_unlock(&kvm_ffa_hyp_lock);
 out:
 	if (ret)
 		ffa_to_smccc_res(res, ret);
@@ -516,7 +515,7 @@ static void do_ffa_mem_reclaim(struct arm_smccc_res *res,
 
 	handle = PACK_HANDLE(handle_lo, handle_hi);
 
-	hyp_spin_lock(&host_buffers.lock);
+	hyp_spin_lock(&kvm_ffa_hyp_lock);
 
 	buf = hyp_buffers.tx;
 	*buf = (struct ffa_mem_region) {
@@ -578,7 +577,7 @@ static void do_ffa_mem_reclaim(struct arm_smccc_res *res,
 	WARN_ON(ffa_host_unshare_ranges(reg->constituents,
 					reg->addr_range_cnt));
 out_unlock:
-	hyp_spin_unlock(&host_buffers.lock);
+	hyp_spin_unlock(&kvm_ffa_hyp_lock);
 
 	if (ret)
 		ffa_to_smccc_res(res, ret);
@@ -731,7 +730,7 @@ static void do_ffa_part_get(struct arm_smccc_res *res,
 	DECLARE_REG(u32, flags, ctxt, 5);
 	u32 count, partition_sz, copy_sz;
 
-	hyp_spin_lock(&host_buffers.lock);
+	hyp_spin_lock(&kvm_ffa_hyp_lock);
 	if (!host_buffers.rx) {
 		ffa_to_smccc_res(res, FFA_RET_BUSY);
 		goto out_unlock;
@@ -767,7 +766,7 @@ static void do_ffa_part_get(struct arm_smccc_res *res,
 
 	memcpy(host_buffers.rx, hyp_buffers.rx, copy_sz);
 out_unlock:
-	hyp_spin_unlock(&host_buffers.lock);
+	hyp_spin_unlock(&kvm_ffa_hyp_lock);
 }
 
 bool kvm_host_ffa_handler(struct kvm_cpu_context *host_ctxt, u32 func_id)
@@ -884,13 +883,8 @@ int hyp_ffa_init(void *pages)
 	};
 
 	hyp_buffers = (struct kvm_ffa_buffers) {
-		.lock	= __HYP_SPIN_LOCK_UNLOCKED,
 		.tx	= tx,
 		.rx	= rx,
-	};
-
-	host_buffers = (struct kvm_ffa_buffers) {
-		.lock	= __HYP_SPIN_LOCK_UNLOCKED,
 	};
 
 	/* Map our hypervisor buffers into the SPMD */
@@ -898,6 +892,7 @@ int hyp_ffa_init(void *pages)
 	if (ret)
 		return ret;
 
+	kvm_ffa_hyp_lock = __HYP_SPIN_LOCK_UNLOCKED;
 	version_lock = __HYP_SPIN_LOCK_UNLOCKED;
 	return 0;
 }
