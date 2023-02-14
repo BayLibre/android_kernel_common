d6c243bb5dc73d4815587cad16695962c06185fc
Fuad Tabba <tabba@google.com>
2023-02-14 09:52:16 +0000

    ANDROID: KVM: arm64: Store the maximum sve vector length at hyp
    The code to determine the maximum sve vector length by the system
    isn't trivial. In subsequent patches hyp needs to know it for
    allocating the required amount of memory without relying on an
    untrusted host for it. Store it in hyp while the host is still
    trusted.
    
    Signed-off-by: Fuad Tabba <tabba@google.com>
    
    Bug: 267291591
    Change-Id: I2561af67722a99d8a989b26cb47d073eba3869ff

--- arch/arm64/include/asm/kvm_hyp.h
+++ arch/arm64/include/asm/kvm_hyp.h
@@ -114,7 +114,7 @@ void __noreturn __hyp_do_panic(struct kvm_cpu_context *host_ctxt, u64 spsr,
 void __pkvm_init_switch_pgd(phys_addr_t phys, unsigned long size,
 			    phys_addr_t pgd, void *sp, void *cont_fn);
 int __pkvm_init(phys_addr_t phys, unsigned long size, unsigned long nr_cpus,
-		unsigned long *per_cpu_base, u32 hyp_va_bits);
+		unsigned long *per_cpu_base, u32 hyp_va_bits, u32 sve_max_vl);
 void __noreturn __host_enter(struct kvm_cpu_context *host_ctxt);
 #endif
 
