8d3a9bf8bd2dc2220ec1984db28f0481cc223f88
Fuad Tabba <tabba@google.com>
2023-02-14 13:13:27 +0000

    ANDROID: KVM: arm64: Save and restore host sve state in pKVM
    Non-protected mode relies on the host to restore its SVE state if
    necessary. However, protected VMs shouldn't reveal any
    information to the host, including whether they have potentially
    dirtied the host's sve state. Therefore, save and restore the
    host's sve state at hyp in protected mode.
    
    Currently this behavior applies to protected and non-protected
    VMs in protected mode. It could be optimised for non-protected
    VMs by applying the same behavior as non-protected mode, which is
    to inform the host that it should restore its sve state. But for
    now it's kept this way to maintain the same behavior for all VMs
    in protected mode.
    
    Signed-off-by: Fuad Tabba <tabba@google.com>
    
    Bug: 267291591
    Change-Id: Ifbcc64b387c3f821a6c1047e8c843f6250a3f690

--- arch/arm64/include/asm/kvm_hyp.h
+++ arch/arm64/include/asm/kvm_hyp.h
@@ -134,6 +134,13 @@ extern bool kvm_nvhe_sym(smccc_trng_available);
 
 extern bool kvm_nvhe_sym(__pkvm_modules_enabled);
 
+extern u32 hyp_sve_max_vl;
+
+static inline u32 sve_vl_from_zcr(u64 zcr)
+{
+	return ((zcr & ZCR_ELx_LEN_MASK) + 1) * 8;
+}
+
 struct kvm_nvhe_clock_data {
 	u32 mult;
 	u32 shift;
