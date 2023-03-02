cde11916c7137e56906477632c03e16d126c061c
Yangtao Li <frank.li@vivo.com>
2023-03-02 17:55:09 +0800

    UPSTREAM: f2fs: fix to handle filemap_fdatawrite() error in f2fs_ioc_decompress_file/f2fs_ioc_compress_file
    It seems inappropriate that the current logic does not handle
    filemap_fdatawrite() errors, so let's fix it.
    
    Bug: 456400355
    Change-Id: I5e7d23e94eabaca90cb1da398e5b1831eaec8b60
    Signed-off-by: Yangtao Li <frank.li@vivo.com>
    Reviewed-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    (cherry picked from commit b822dc914917f9381e7a04ff6872c1b6883dd4ae)
    Signed-off-by: Sandeep Dhavale <dhavale@google.com>

--- fs/f2fs/file.c
+++ fs/f2fs/file.c
@@ -4120,8 +4120,11 @@ static int f2fs_ioc_decompress_file(struct file *filp, unsigned long arg)
 		if (ret < 0)
 			break;
 
-		if (get_dirty_pages(inode) >= blk_per_seg)
-			filemap_fdatawrite(inode->i_mapping);
+		if (get_dirty_pages(inode) >= blk_per_seg) {
+			ret = filemap_fdatawrite(inode->i_mapping);
+			if (ret < 0)
+				break;
+		}
 
 		count -= len;
 		page_idx += len;
@@ -4189,8 +4192,11 @@ static int f2fs_ioc_compress_file(struct file *filp, unsigned long arg)
 		if (ret < 0)
 			break;
 
-		if (get_dirty_pages(inode) >= blk_per_seg)
-			filemap_fdatawrite(inode->i_mapping);
+		if (get_dirty_pages(inode) >= blk_per_seg) {
+			ret = filemap_fdatawrite(inode->i_mapping);
+			if (ret < 0)
+				break;
+		}
 
 		count -= len;
 		page_idx += len;
