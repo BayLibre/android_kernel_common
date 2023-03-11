d148ee74089ce00ffb28ae65641d5bc2239f4133
Doug Berger <opendmb@gmail.com>
2023-03-10 16:38:52 -0800

    BACKPORT: FROMLIST: memblock: introduce MEMBLOCK_MOVABLE flag
    The MEMBLOCK_MOVABLE flag is introduced to designate a memblock
    as only supporting movable allocations by the page allocator.
    
    Link: https://lore.kernel.org/lkml/20230311003855.645684-7-opendmb@gmail.com/
    [doug: removed merge collision with MEMBLOCK_DRIVER_MANAGED ]
    Change-Id: Icdefccf83a6703ad9a44118d78a39920c9684251
    Signed-off-by: Doug Berger <opendmb@gmail.com>
    Signed-off-by: Florian Fainelli <florian.fainelli@broadcom.com>
    Signed-off-by: Danesh Petigara <danesh.petigara@broadcom.com>
    (cherry picked from commit a1f3345b0b5c14cfeccb4baf4e2a2063f6293bec)

--- include/linux/memblock.h
+++ include/linux/memblock.h
@@ -47,6 +47,7 @@ enum memblock_flags {
 	MEMBLOCK_MIRROR		= 0x2,	/* mirrored region */
 	MEMBLOCK_NOMAP		= 0x4,	/* don't add to kernel direct mapping */
 	MEMBLOCK_DRIVER_MANAGED = 0x8,	/* always detected via a driver */
+	MEMBLOCK_MOVABLE	= 0x10,	/* designated movable block */
 };
 
 /**
@@ -127,6 +128,8 @@ int memblock_clear_hotplug(phys_addr_t base, phys_addr_t size);
 int memblock_mark_mirror(phys_addr_t base, phys_addr_t size);
 int memblock_mark_nomap(phys_addr_t base, phys_addr_t size);
 int memblock_clear_nomap(phys_addr_t base, phys_addr_t size);
+int memblock_mark_movable(phys_addr_t base, phys_addr_t size);
+int memblock_clear_movable(phys_addr_t base, phys_addr_t size);
 
 void memblock_free_all(void);
 void memblock_free(void *ptr, size_t size);
@@ -265,6 +268,11 @@ static inline bool memblock_is_nomap(struct memblock_region *m)
 static inline bool memblock_is_driver_managed(struct memblock_region *m)
 {
 	return m->flags & MEMBLOCK_DRIVER_MANAGED;
+}
+
+static inline bool memblock_is_movable(struct memblock_region *m)
+{
+	return m->flags & MEMBLOCK_MOVABLE;
 }
 
 int memblock_search_pfn_nid(unsigned long pfn, unsigned long *start_pfn,
