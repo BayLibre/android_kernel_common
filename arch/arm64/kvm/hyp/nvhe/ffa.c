95d6ca0a8abb989d0771499a58c6321d5b8e40d2
Sebastian Ene <sebastianene@google.com>
2023-03-16 16:34:23 +0000

    ANDROID: KVM: arm64: Reclaim the guest memory with FFA_MEM_RECLAIM
    Implement the guest memory reclaim by forwarding the handle of the
    memory region to the Secure OS. The constituents are stored in the
    ffa_descriptor_buffer and they are upper bounded to
    
    Bug: 269285339
    Change-Id: Ie32f57ffc83c600cf6b961e0d18443d79423832b
    Signed-off-by: Sebastian Ene <sebastianene@google.com>

--- arch/arm64/kvm/hyp/nvhe/ffa.c
+++ arch/arm64/kvm/hyp/nvhe/ffa.c
@@ -933,6 +933,8 @@ static void do_ffa_mem_reclaim(struct arm_smccc_res *res,
 	hyp_spin_lock(&hyp_buffers.lock);
 
 	buf = hyp_buffers.tx;
+
+	/* TODO: Verify if it really matters passing the sender_id here. */
 	*buf = (struct ffa_mem_region) {
 		.sender_id	= HOST_FFA_ID,
 		.handle		= handle,
@@ -982,9 +984,16 @@ static void do_ffa_mem_reclaim(struct arm_smccc_res *res,
 		goto out_unlock;
 
 	reg = (void *)buf + offset;
-	/* If the SPMD was happy, then we should be too. */
-	WARN_ON(ffa_host_unshare_ranges(reg->constituents,
-					reg->addr_range_cnt));
+
+	if (vmid == 0) {
+		/* If the SPMD was happy, then we should be too. */
+		WARN_ON(ffa_host_unshare_ranges(reg->constituents,
+						reg->addr_range_cnt));
+	} else {
+		WARN_ON(ffa_guest_unshare_ranges(reg->constituents,
+						 reg->addr_range_cnt,
+						 ctxt, vmid));
+	}
 out_unlock:
 	hyp_spin_unlock(&hyp_buffers.lock);
 
@@ -1122,6 +1131,8 @@ int kvm_guest_ffa_handler(struct pkvm_hyp_vcpu *hyp_vcpu, u64 *exit_code)
 				      exit_code);
 		goto out_handled;
 	case FFA_MEM_RECLAIM:
+		do_ffa_mem_reclaim(&res, ctxt, vmid);
+		goto out_handled;
 	case FFA_MEM_LEND:
 	case FFA_FN64_MEM_LEND:
 	case FFA_MEM_FRAG_TX:
