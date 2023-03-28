0997c923814124a2cc0ff82038fc39c1d582ca3f
Alice Ryhl <aliceryhl@google.com>
2023-03-28 15:14:30 +0000

    ANDROID: rust: add io_buffer.rs and user_ptr.rs
    This is a placeholder commit containing the latest versions of
    io_buffer.rs and user_ptr.rs as needed by Rust binder. See the link
    below for the version that these files are based on. The commit message
    should be updated to include all authors before upstreaming.
    
    Link: https://github.com/Rust-for-Linux/linux/blob/575b1c015f5fdaac4b8b85ba3d579c324c3c2f41/rust/kernel/user_ptr.rs
    Bug: 278052745
    Change-Id: I418a05ed8e20066f38994fb4f900ef5b8170b74a
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>

--- rust/kernel/lib.rs
+++ rust/kernel/lib.rs
@@ -37,6 +37,7 @@ mod allocator;
 mod build_assert;
 pub mod error;
 pub mod init;
+pub mod io_buffer;
 pub mod prelude;
 pub mod print;
 mod static_assert;
@@ -46,6 +47,7 @@ pub mod str;
 pub mod sync;
 pub mod task;
 pub mod types;
+pub mod user_ptr;
 
 #[doc(hidden)]
 pub use bindings;
