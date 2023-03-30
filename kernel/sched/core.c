60ba8ca8f2ee46fd9cc8b60aaf6b2c0580f4fc86
David Dai <davidai@google.com>
2023-03-30 15:43:36 -0700

    sched/fair: Add util_guest for tasks
    For virtualization usecases, util_est and util_avg currently tracked
    on the host aren't sufficient to accurately represent the workload on
    vCPU threads, which results in poor frequency selection and performance.
    For example, when a large workload migrates from a busy vCPU thread to
    an idle vCPU thread, it incurs additional DVFS ramp-up latencies
    as util accumulates.
    
    Introduce a new "util_guest" member as an additional PELT signal that's
    independently updated by the guest. When used, it's max aggregated to
    provide a boost to both task_util and task_util_est.
    
    Updating task_util and task_util_est will ensure:
    -Better task placement decisions for vCPU threads on the host
    -Correctly updating util_est.ewma during dequeue
    -Additive util with other threads on the same runqueue for more
    accurate frequency responses
    
    Co-developed-by: Saravana Kannan <saravanak@google.com>
    Change-Id: I56eb913f793dd1bac81c5dad95a860ad79c8589c
    Signed-off-by: Saravana Kannan <saravanak@google.com>
    Signed-off-by: David Dai <davidai@google.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1660,6 +1660,16 @@ static inline void uclamp_post_fork(struct task_struct *p) { }
 static inline void init_uclamp(void) { }
 #endif /* CONFIG_UCLAMP_TASK */
 
+static void __setscheduler_task_util(struct task_struct *p,
+				  const struct sched_attr *attr)
+{
+
+	if (likely(!(attr->sched_flags & SCHED_FLAG_UTIL_GUEST)))
+		return;
+
+	p->se.avg.util_guest = attr->sched_util_min;
+}
+
 static inline void enqueue_task(struct rq *rq, struct task_struct *p, int flags)
 {
 	if (!(flags & ENQUEUE_NOCLOCK))
@@ -5495,7 +5505,7 @@ recheck:
 			return -EINVAL;
 	}
 
-	if (attr->sched_flags & ~(SCHED_FLAG_ALL | SCHED_FLAG_SUGOV))
+	if (attr->sched_flags & ~(SCHED_FLAG_ALL | SCHED_FLAG_SUGOV | SCHED_FLAG_UTIL_GUEST))
 		return -EINVAL;
 
 	/*
@@ -5569,6 +5579,9 @@ recheck:
 		if (attr->sched_flags & SCHED_FLAG_SUGOV)
 			return -EINVAL;
 
+		if (attr->sched_flags & SCHED_FLAG_UTIL_GUEST)
+			return -EINVAL;
+
 		retval = security_task_setscheduler(p);
 		if (retval)
 			return retval;
@@ -5611,6 +5624,8 @@ recheck:
 		if (dl_policy(policy) && dl_param_changed(p, attr))
 			goto change;
 		if (attr->sched_flags & SCHED_FLAG_UTIL_CLAMP)
+			goto change;
+		if (attr->sched_flags & SCHED_FLAG_UTIL_GUEST)
 			goto change;
 
 		p->sched_reset_on_fork = reset_on_fork;
@@ -5700,6 +5715,7 @@ change:
 		trace_android_rvh_setscheduler(p);
 	}
 	__setscheduler_uclamp(p, attr);
+	__setscheduler_task_util(p, attr);
 
 	if (queued) {
 		/*
