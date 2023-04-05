4f3dbe711571a5c0875463cef05a30bf7965b37a
Alexandru Elisei <alexandru.elisei@arm.com>
2023-04-05 13:44:15 +0100

    [DO NOT SUBMIT] mm: compaction: Do not use MIGRATE_METADATA for pages with metadata
    MIGRATE_METADATA pages are special because for the one architecture
    (arm64) that use them, it is not possible to have metadata associated
    with a page used to store metadata.
    
    To avoid a situation where a page with metadata is being migrated to a
    page which cannot have metadata, keep track of whether such pages have
    been isolated as the source for migration. When allocating a destination
    page for migration, deny allocations from MIGRATE_METADATA if that's the
    case.
    
    fast_isolate_freepages() takes pages only from the MIGRATE_MOVABLE list,
    which means it is not necessary to have a similar check, as
    MIGRATE_METADATA pages will never be considered.
    
    Bug: 234781130
    Change-Id: I882b58ea857efbf769d00d5b4942866d3dcaeb47
    Signed-off-by: Peter Collingbourne <pcc@google.com>

--- include/linux/memory_metadata.h
+++ include/linux/memory_metadata.h
@@ -3,6 +3,7 @@
 #define __LINUX_MEMORY_METADATA_H
 
 #include <linux/gfp.h>
+#include <linux/mm_types.h>
 
 extern unsigned long totalmetadata_pages;
 
@@ -13,12 +14,18 @@ extern unsigned long totalmetadata_pages;
 #define metadata_storage_enabled()		arch_metadata_storage_enabled()
 
 #define alloc_can_use_metadata_pages(gfp_mask)	arch_alloc_can_use_metadata_pages(gfp_mask)
+#define page_has_metadata(page)			arch_page_has_metadata(page)
+
 #else
 static inline bool metadata_storage_enabled(void)
 {
 	return false;
 }
 static inline bool alloc_can_use_metadata_pages(gfp_t gfp_mask)
+{
+	return false;
+}
+static inline bool page_has_metadata(struct page *page)
 {
 	return false;
 }
