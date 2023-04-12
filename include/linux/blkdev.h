199e1da6e034c00d2143c5ef156cf7fa90d6979d
Bart Van Assche <bvanassche@google.com>
2023-04-11 17:54:35 -0700

    ANDROID: block: Move the flush request information
    Move the members that are used for processing request queue flushing
    from struct request to past the request private data. This patch
    prepares for letting I/O schedulers process flush operations. This patch
    is a GKI-compliant version of the following patch:
    
        include/linux/blk-mq.h
        @ @ -173,23 +173,19 @ @ struct request {
    
         	/*
        -	 * Three pointers are available for the IO schedulers, if they need
        -	 * more they have to dynamically allocate it.  Flush requests are
        -	 * never put on the IO scheduler. So let the flush fields share
        -	 * space with the elevator data.
        +	 * Three pointers are available for IO schedulers. If they need
        +	 * more private data they have to allocate it dynamically.
         	 */
        -	union {
        -		struct {
        -			struct io_cq		*icq;
        -			void			*priv[2];
        -		} elv;
        -
        -		struct {
        -			unsigned int		seq;
        -			struct list_head	list;
        -			rq_end_io_fn		*saved_end_io;
        -		} flush;
        -	};
        +	struct {
        +		struct io_cq		*icq;
        +		void			*priv[2];
        +	} elv;
        +
        +	struct {
        +		unsigned int		seq;
        +		struct list_head	list;
        +		rq_end_io_fn		*saved_end_io;
        +	} flush;
    
         	union {
         		struct __call_single_data csd;
    
    Bug: 275581839
    Change-Id: Id1199ff450c7402264b8958b89c0bd5ee2e3b966
    Signed-off-by: Bart Van Assche <bvanassche@google.com>

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -177,7 +177,7 @@ struct request {
 			unsigned int		seq;
 			struct list_head	list;
 			rq_end_io_fn		*saved_end_io;
-		} flush;
+		} flush /* DEPRECATED - DO NOT USE */;
 	};
 
 	struct gendisk *rq_disk;
