5f6daa4b0302e8d40f9c2f04e9aa80daef86ed7c
Elliot Berman <quic_eberman@quicinc.com>
2023-04-14 16:41:21 -0700

    ANDROID: gunyah: Sync with latest "gunyah: vm_mgr: Add/remove user memory regions"
    Align Gunyah memory parcel to Gunyah v12 patches posted to kernel.org.
    
    We deviate from a perfect copy from kernel.org because:
     - in pages_are_mergeable, zone_device_pages_have_same_pgmap is not
       present in 6.1. Drop this check.
    
    https://lore.kernel.org/all/20230509204801.2824351-11-quic_eberman@quicinc.com/
    
    Bug: 279506910
    Change-Id: I90ec2ac416b24bcc65635f27cae7665ce879783f
    Signed-off-by: Elliot Berman <quic_eberman@quicinc.com>

--- include/linux/gunyah_rsc_mgr.h
+++ include/linux/gunyah_rsc_mgr.h
@@ -71,7 +71,7 @@ struct gh_rm_mem_acl_entry {
 } __packed;
 
 struct gh_rm_mem_entry {
-	__le64 ipa_base;
+	__le64 phys_addr;
 	__le64 size;
 } __packed;
 
@@ -81,16 +81,16 @@ enum gh_rm_mem_type {
 };
 
 /*
- * struct gh_rm_mem_parcel - Package info about memory to be lent/shared/donated/reclaimed
+ * struct gh_rm_mem_parcel - Info about memory to be lent/shared/donated/reclaimed
  * @mem_type: The type of memory: normal (DDR) or IO
  * @label: An client-specified identifier which can be used by the other VMs to identify the purpose
  *         of the memory parcel.
+ * @n_acl_entries: Count of the number of entries in the @acl_entries array.
  * @acl_entries: An array of access control entries. Each entry specifies a VM and what access
  *               is allowed for the memory parcel.
- * @n_acl_entries: Count of the number of entries in the `acl_entries` array.
- * @mem_entries: An list of regions to be associated with the memory parcel. Addresses should be
+ * @n_mem_entries: Count of the number of entries in the @mem_entries array.
+ * @mem_entries: An array of regions to be associated with the memory parcel. Addresses should be
  *               (intermediate) physical addresses from Linux's perspective.
- * @n_mem_entries: Count of the number of entries in the `mem_entries` array.
  * @mem_handle: On success, filled with memory handle that RM allocates for this memory parcel
  */
 struct gh_rm_mem_parcel {
