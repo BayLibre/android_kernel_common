a0267383e5ea7af4fbfcfd2bb1d66d71e77acf5b
Elliot Berman <quic_eberman@quicinc.com>
2023-04-17 12:07:52 -0700

    ANDROID: BACKPORT: gunyah-v12: Misc adjustments
    Apply remaining minor fixups from Gunyah v12 patches:
    
    https://lore.kernel.org/all/20230424231558.70911-1-quic_eberman@quicinc.com/
    
    Bug: 279506910
    Change-Id: I1a596b9df29d210c51b612845e4a1aafbea00441
    Signed-off-by: Elliot Berman <quic_eberman@quicinc.com>

--- drivers/virt/gunyah/rsc_mgr.c
+++ drivers/virt/gunyah/rsc_mgr.c
@@ -126,7 +126,8 @@ struct gh_rm_connection {
  * @dev: pointer to device
  * @tx_ghrsc: message queue resource to TX to RM
  * @rx_ghrsc: message queue resource to RX from RM
- * @msgq: mailbox instance of above
+ * @msgq: mailbox instance of TX/RX resources above
+ * @msgq_client: mailbox client of above msgq
  * @active_rx_connection: ongoing gh_rm_connection for which we're receiving fragments
  * @last_tx_ret: return value of last mailbox tx
  * @call_xarray: xarray to allocate & lookup sequence IDs for Request/Response flows
@@ -160,7 +161,7 @@ struct gh_rm {
 
 /**
  * gh_rm_remap_error() - Remap Gunyah resource manager errors into a Linux error code
- * @gh_error: "Standard" return value from Gunyah resource manager
+ * @rm_error: "Standard" return value from Gunyah resource manager
  */
 static inline int gh_rm_remap_error(enum gh_rm_error rm_error)
 {
--- drivers/virt/gunyah/rsc_mgr_rpc.c
+++ drivers/virt/gunyah/rsc_mgr_rpc.c
@@ -308,7 +308,7 @@ int gh_rm_mem_reclaim(struct gh_rm *rm, struct gh_rm_mem_parcel *parcel)
 	int ret;
 
 	ret = gh_rm_call(rm, GH_RM_RPC_MEM_RECLAIM, &req, sizeof(req), NULL, NULL);
-	/* Do not call platform mem reclaim hooks: the reclaim didn't happen*/
+	/* Only call the platform mem reclaim hooks if we reclaimed the memory */
 	if (ret)
 		return ret;
 
