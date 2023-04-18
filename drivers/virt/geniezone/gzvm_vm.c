fc1737d4945fb2d0c443d6b18b81213e557d506c
Yingshiuan Pan <yingshiuan.pan@mediatek.com>
2023-04-18 12:11:14 +0800

    ANDROID: virt: geniezone: Add ioeventfd support
    Ioeventfd leverages eventfd to provide asynchronous notification
    mechanism for VMM. VMM can register a mmio address and bind with an
    eventfd. Once a mmio trap occurs on this registered region, its
    corresponding eventfd will be notified.
    
    Signed-off-by: Yingshiuan Pan <yingshiuan.pan@mediatek.com>
    Signed-off-by: Yi-De Wu <yi-de.wu@mediatek.com>
    Signed-off-by: Liju Chen <liju-clr.chen@mediatek.com>
    Change-Id: Id4c599e4ab7ceac81e0c5ca5aace16ad235d7aef
    Bug: 472514996
    Link: https://lore.kernel.org/lkml/20241114100802.4116-12-liju-clr.chen@mediatek.com/
    (cherry picked from commit d28d039fd4a6f9d5477195dca1a715ead3aa035f)

--- drivers/virt/geniezone/gzvm_vm.c
+++ drivers/virt/geniezone/gzvm_vm.c
@@ -254,6 +254,16 @@ static long gzvm_vm_ioctl(struct file *filp, unsigned int ioctl,
 		ret = gzvm_irqfd(gzvm, &data);
 		break;
 	}
+	case GZVM_IOEVENTFD: {
+		struct gzvm_ioeventfd data;
+
+		if (copy_from_user(&data, argp, sizeof(data))) {
+			ret = -EFAULT;
+			goto out;
+		}
+		ret = gzvm_ioeventfd(gzvm, &data);
+		break;
+	}
 	case GZVM_ENABLE_CAP: {
 		struct gzvm_enable_cap cap;
 
@@ -278,6 +288,7 @@ static void gzvm_destroy_vm(struct gzvm *gzvm)
 	mutex_lock(&gzvm->lock);
 
 	gzvm_vm_irqfd_release(gzvm);
+	gzvm_vm_ioeventfd_release(gzvm);
 	gzvm_destroy_vcpus(gzvm);
 	gzvm_arch_destroy_vm(gzvm->vm_id);
 
@@ -346,6 +357,14 @@ static struct gzvm *gzvm_create_vm(struct gzvm_driver *drv, unsigned long vm_typ
 	ret = gzvm_vm_irqfd_init(gzvm);
 	if (ret) {
 		pr_err("Failed to initialize irqfd\n");
+		kfree(gzvm);
+		return ERR_PTR(ret);
+	}
+
+	ret = gzvm_init_ioeventfd(gzvm);
+	if (ret) {
+		pr_err("Failed to initialize ioeventfd\n");
+		gzvm_vm_irqfd_release(gzvm);
 		kfree(gzvm);
 		return ERR_PTR(ret);
 	}
