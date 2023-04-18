9e2fa62c94efac1a516df3a0438ae05e6b56d4f4
John Stultz <jstultz@google.com>
2023-04-18 14:13:48 +0000

    WIP: tick: Introduce a dyn_hz boot option
    Introduce dyn_hz= option, which allows the actual timer tick to
    be scaled down at boot time.
    
    This allows kerenls to be built with HZ=1000 but systems to
    effectively run as if HZ=100 if specified.
    
    The system will still run with the configured HZ value, but
    ticks will just arrive "late".
    
    The valid values are between 100 and the build time CONFIG_HZ
    value.
    
    Change-Id: Ie834508e1f64a4d39f13b426329e3c54ae9155a1
    Signed-off-by: John Stultz <jstultz@google.com>

--- kernel/sched/cputime.c
+++ kernel/sched/cputime.c
@@ -499,7 +499,7 @@ void account_process_tick(struct task_struct *p, int user_tick)
 		return;
 	}
 
-	cputime = TICK_NSEC;
+	cputime = DYN_TICK_NSEC;
 	steal = steal_account_process_time(ULONG_MAX);
 
 	if (steal >= cputime)
