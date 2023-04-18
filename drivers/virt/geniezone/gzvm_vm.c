9148f3a65ce5f14e408de17172ae4deb66387546
Yi-De Wu <yi-de.wu@mediatek.com>
2023-04-18 12:11:14 +0800

    FROMLIST: virt: geniezone: Add ioeventfd support
    Ioeventfd leverages eventfd to provide asynchronous notification
    mechanism for VMM. VMM can register a mmio address and bind with an
    eventfd. Once a mmio trap occurs on this registered region, its
    corresponding eventfd will be notified.
    
    Change-Id: Iff6bb7dd8ba42d08813e531ab40629492a1218bc
    Signed-off-by: Yingshiuan Pan <yingshiuan.pan@mediatek.com>
    Signed-off-by: Liju Chen <liju-clr.chen@mediatek.com>
    Signed-off-by: Yi-De Wu <yi-de.wu@mediatek.com>
    Bug: 280363874
    Link: https://lore.kernel.org/lkml/20230609085214.31071-7-yi-de.wu@mediatek.com/

--- drivers/virt/geniezone/gzvm_vm.c
+++ drivers/virt/geniezone/gzvm_vm.c
@@ -381,6 +381,16 @@ static long gzvm_vm_ioctl(struct file *filp, unsigned int ioctl,
 		ret = gzvm_irqfd(gzvm, &data);
 		break;
 	}
+	case GZVM_IOEVENTFD: {
+		struct gzvm_ioeventfd data;
+
+		if (copy_from_user(&data, argp, sizeof(data))) {
+			ret = -EFAULT;
+			goto out;
+		}
+		ret = gzvm_ioeventfd(gzvm, &data);
+		break;
+	}
 	case GZVM_ENABLE_CAP: {
 		struct gzvm_enable_cap cap;
 
@@ -455,6 +465,14 @@ static struct gzvm *gzvm_create_vm(unsigned long vm_type)
 	if (ret) {
 		dev_err(gzvm_dev.this_device,
 			"Failed to initialize irqfd\n");
+		kfree(gzvm);
+		return ERR_PTR(ret);
+	}
+
+	ret = gzvm_init_ioeventfd(gzvm);
+	if (ret) {
+		dev_err(gzvm_dev.this_device,
+			"Failed to initialize ioeventfd\n");
 		kfree(gzvm);
 		return ERR_PTR(ret);
 	}
