e31c8a10f709648f7959432f4b13017eac97783a
guoqingxin <guoqingxin@oppo.com>
2023-04-21 14:57:12 +0800

    ANDROID: vendor_hooks: Add hooks for signal
    Add hooks to support OPPO's customized freezer.
    when the process receives a specified signal,
    OPPO's freezer will unfreeze it.
    
    Bug: 278505222
    
    Change-Id: Ide689412775ca91e5bb792f1365a096b3be6afc2
    Signed-off-by: guoqingxin <guoqingxin@oppo.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -56,6 +56,7 @@
 #include <trace/hooks/thermal.h>
 #include <trace/hooks/audio_usboffload.h>
 #include <trace/hooks/typec.h>
+#include <trace/hooks/signal.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -180,3 +181,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_alloc_new_buf_locked);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_reply);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_trans);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_preset);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_do_send_sig_info);
