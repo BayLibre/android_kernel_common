3701ba4f040e77b1336444d742ced24bcbd9c888
xieliujie <xieliujie@oppo.com>
2023-04-22 21:34:56 +0800

    ANDROID: vendor_hooks: Porting oem futex hooks from android13
    Since futex.c has been split into multiple files in android14, hooks
    in the gerrits below need to be move into new files:
    1.https://android-review.googlesource.com/c/kernel/common/+/2195355
    This patch add hooks for oem futex optimization.
    If an important task is going to sleep through do_futex(),
    find out it's futex-owner by the pid comes from userspace,
    and boost the owner by some means to shorten the sleep time.
    How to boost? Depends on these hooks:
    commit 53e809978443 ("ANDROID: vendor_hooks: Add hooks for scheduler")
    2.https://android-review.googlesource.com/c/kernel/common/+/2125103
    This patch add hook for recording the sleeping time due to sys_futex.
    
    Bug: 279355016
    Bug: 243110112
    Bug: 210947226
    Change-Id: I0f2cfbab25d2864b2d3460a69eedaff9e9996b79
    Signed-off-by: xieliujie <xieliujie@oppo.com>

--- kernel/futex/syscalls.c
+++ kernel/futex/syscalls.c
@@ -3,6 +3,7 @@
 #include <linux/compat.h>
 #include <linux/syscalls.h>
 #include <linux/time_namespace.h>
+#include <trace/hooks/futex.h>
 
 #include "futex.h"
 
@@ -98,6 +99,7 @@ long do_futex(u32 __user *uaddr, int op, u32 val, ktime_t *timeout,
 			return -ENOSYS;
 	}
 
+	trace_android_vh_do_futex(cmd, &flags, uaddr2);
 	switch (cmd) {
 	case FUTEX_WAIT:
 		val3 = FUTEX_BITSET_MATCH_ANY;
