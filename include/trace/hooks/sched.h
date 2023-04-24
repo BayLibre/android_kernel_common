ec9e8f4cac64d0feb2e5927ee934da1f6c43cf85
xieliujie <xieliujie@oppo.com>
2023-04-24 11:20:20 +0800

    ANDROID: vendor_hooks: add hook account_process_tick_gran
    1) this hook will allow to account tick in every third of more ticks
    to save cpu time for accounting
    2) combine trace_android_vh_account_task_time in this hook, which help us to get information
    about the high load task and the cpu they running on
    
    Bug: 279549765
    Change-Id: I5d18e0167fdce076d13aecc653dcf6387bcb25f2
    Signed-off-by: Liujie Xie <xieliujie@oppo.com>
    Signed-off-by: xieliujie <xieliujie@oppo.com>
    Signed-off-by: John Stultz <jstultz@google.com>

--- include/trace/hooks/sched.h
+++ include/trace/hooks/sched.h
@@ -351,6 +351,9 @@ DECLARE_RESTRICTED_HOOK(android_rvh_find_energy_efficient_cpu,
 	TP_PROTO(struct task_struct *p, int prev_cpu, int sync, int *new_cpu),
 	TP_ARGS(p, prev_cpu, sync, new_cpu), 1);
 
+DECLARE_HOOK(android_vh_account_process_tick_gran,
+	TP_PROTO(struct task_struct *p, struct rq *rq, int user_tick, int *ticks),
+	TP_ARGS(p, rq, user_tick, ticks));
 /* macro versions of hooks are no longer required */
 
 #endif /* _TRACE_HOOK_SCHED_H */
