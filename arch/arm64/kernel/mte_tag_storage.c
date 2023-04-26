6be2ad3e6dceacfce94dab84ef09337e55c42241
Alexandru Elisei <alexandru.elisei@arm.com>
2023-04-26 15:04:55 +0100

    [DO NOT SUBMIT] arm64: mte: Disable dynamic tag storage management if HW KASAN is enabled
    Reserving the tag storage associated with a page when said page is
    allocated with tags requires the ability to migrate existing data if the
    tag storage is in use as data (non tagged) page.
    
    The kernel allocates pages, which are now tagged because of HW KASAN, in
    non-preemptible contexts, which can make reserving the associate tag
    storage impossible.
    
    Don't expose the tag storage pages to the memory allocator if HW KASAN is
    enabled.
    
    Bug: 234781130
    Change-Id: Id31791a7c63b4765dd9bcba2a3cea1d1d4c21f31
    Signed-off-by: Peter Collingbourne <pcc@google.com>

--- arch/arm64/kernel/mte_tag_storage.c
+++ arch/arm64/kernel/mte_tag_storage.c
@@ -345,6 +345,18 @@ static int __init mte_tag_storage_activate_regions(void)
 		return 0;
 	}
 
+	/*
+	 * The kernel allocates memory in non-preemptible contexts, which makes
+	 * migration impossible when reserving the associated tag storage.
+	 *
+	 * The check is safe to make because KASAN HW tags are enabled before
+	 * the rest of the init functions are called, in smp_prepare_boot_cpu().
+	 */
+	if (kasan_hw_tags_enabled()) {
+		pr_info("KASAN HW tags enabled, disabling tag storage");
+		return 0;
+	}
+
 	ret = mte_tag_storage_check_zone();
 	if (ret)
 		return ret;
