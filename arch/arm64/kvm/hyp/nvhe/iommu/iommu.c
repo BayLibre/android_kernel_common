07d1314a74ab47f7947290722d0c941f1ef15133
Mostafa Saleh <smostafa@google.com>
2023-05-02 16:29:52 +0000

    ANDROID: KVM: arm64: iommu: Add PASID to driver calls
    In preparation to add stage-1 support, support PASID in driver calls.
    
    For now they are not used in the SMMUv3 driver.
    
    As EL2 driver has not notion of iommu masters, we need to pass the
    pasid_bits in the attach_dev hypercall.
    
    Bug: 277989609
    Bug: 278749606
    Change-Id: I6648a59f9738e847ba13065211bb0cd8df027e70
    Signed-off-by: Mostafa Saleh <smostafa@google.com>

--- arch/arm64/kvm/hyp/nvhe/iommu/iommu.c
+++ arch/arm64/kvm/hyp/nvhe/iommu/iommu.c
@@ -253,7 +253,7 @@ out_unlock:
 }
 
 int kvm_iommu_attach_dev(pkvm_handle_t iommu_id, pkvm_handle_t domain_id,
-			 u32 endpoint_id)
+			 u32 endpoint_id, u32 pasid, u32 pasid_bits)
 {
 	int ret = -EINVAL;
 	struct kvm_hyp_iommu *iommu;
@@ -268,7 +268,8 @@ int kvm_iommu_attach_dev(pkvm_handle_t iommu_id, pkvm_handle_t domain_id,
 	if (!domain || domain_get(domain))
 		goto out_unlock;
 
-	ret = kvm_iommu_ops->attach_dev(iommu, domain_id, domain, endpoint_id);
+	ret = kvm_iommu_ops->attach_dev(iommu, domain_id, domain,
+					endpoint_id, pasid, pasid_bits);
 	if (ret)
 		goto err_put_domain;
 
@@ -290,7 +291,7 @@ err_put_domain:
 }
 
 int kvm_iommu_detach_dev(pkvm_handle_t iommu_id, pkvm_handle_t domain_id,
-			 u32 endpoint_id)
+			 u32 endpoint_id, u32 pasid)
 {
 	int ret = -EINVAL;
 	struct kvm_hyp_iommu *iommu;
@@ -305,7 +306,7 @@ int kvm_iommu_detach_dev(pkvm_handle_t iommu_id, pkvm_handle_t domain_id,
 	if (!domain || atomic_read(&domain->refs) <= 1)
 		goto out_unlock;
 
-	ret = kvm_iommu_ops->detach_dev(iommu, domain_id, domain, endpoint_id);
+	ret = kvm_iommu_ops->detach_dev(iommu, domain_id, domain, endpoint_id, pasid);
 	if (ret)
 		goto out_unlock;
 
