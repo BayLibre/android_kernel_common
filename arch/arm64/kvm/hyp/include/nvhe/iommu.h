530910ae4b906dcf49aca540b54d18c7f7cc613e
Mostafa Saleh <smostafa@google.com>
2023-05-02 16:29:52 +0000

    ANDROID: KVM: arm64: iommu: Add PASID to driver calls
    In preparation to add stage-1 support, support PASID in driver calls.
    
    For now they are not used in the SMMUv3 driver.
    
    As EL2 driver has not notion of iommu masters, we need to pass the
    pasid_bits in the attach_dev hypercall.
    
    Bug: 277989609
    Bug: 278749606
    Change-Id: I6648a59f9738e847ba13065211bb0cd8df027e70
    Signed-off-by: Mostafa Saleh <smostafa@google.com>

--- arch/arm64/kvm/hyp/include/nvhe/iommu.h
+++ arch/arm64/kvm/hyp/include/nvhe/iommu.h
@@ -32,9 +32,9 @@ void kvm_iommu_reclaim_pages_idmap(void *p, u8 order);
 int kvm_iommu_alloc_domain(pkvm_handle_t domain_id);
 int kvm_iommu_free_domain(pkvm_handle_t domain_id);
 int kvm_iommu_attach_dev(pkvm_handle_t iommu_id, pkvm_handle_t domain_id,
-			 u32 endpoint_id);
+			 u32 endpoint_id, u32 pasid, u32 pasid_bits);
 int kvm_iommu_detach_dev(pkvm_handle_t iommu_id, pkvm_handle_t domain_id,
-			 u32 endpoint_id);
+			 u32 endpoint_id, u32 pasid);
 size_t kvm_iommu_map_pages(pkvm_handle_t domain_id,
 			   unsigned long iova, phys_addr_t paddr, size_t pgsize,
 			   size_t pgcount, int prot);
@@ -51,9 +51,9 @@ struct kvm_iommu_ops {
 	int (*alloc_domain)(struct kvm_hyp_iommu_domain *domain);
 	void (*free_domain)(struct kvm_hyp_iommu_domain *domain);
 	int (*attach_dev)(struct kvm_hyp_iommu *iommu, struct kvm_hyp_iommu_domain *domain,
-			  u32 endpoint_id);
+			  u32 endpoint_id, u32 pasid, u32 pasid_bits);
 	int (*detach_dev)(struct kvm_hyp_iommu *iommu, struct kvm_hyp_iommu_domain *domain,
-			  u32 endpoint_id);
+			  u32 endpoint_id, u32 pasid);
 	bool (*dabt_handler)(struct kvm_cpu_context *host_ctxt, u64 esr, u64 addr);
 	int (*suspend)(struct kvm_hyp_iommu *iommu);
 	int (*resume)(struct kvm_hyp_iommu *iommu);
