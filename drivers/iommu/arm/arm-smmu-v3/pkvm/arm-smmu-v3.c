fe04ef9d6e7bac85e8ed1f8133dc91050b5b9f86
Mostafa Saleh <smostafa@google.com>
2023-05-07 23:49:31 +0000

    ANDROID: KVM: arm64: iommu: smmuv3: Stage-1 TLB ops
    Bug: 277989609
    Bug: 278749606
    Change-Id: I388cfde02695dc42da316c05eddfabfc5f4e842b
    Signed-off-by: Mostafa Saleh <smostafa@google.com>

--- drivers/iommu/arm/arm-smmu-v3/pkvm/arm-smmu-v3.c
+++ drivers/iommu/arm/arm-smmu-v3/pkvm/arm-smmu-v3.c
@@ -160,8 +160,21 @@ static int smmu_add_cmd(struct hyp_arm_smmu_v3_device *smmu,
 		cmd[0] |= FIELD_PREP(CMDQ_CFGI_0_SID, ent->cfgi.sid);
 		cmd[1] |= FIELD_PREP(CMDQ_CFGI_1_LEAF, ent->cfgi.leaf);
 		break;
+	case CMDQ_OP_TLBI_NH_VA:
+		cmd[0] |= FIELD_PREP(CMDQ_TLBI_0_VMID, ent->tlbi.vmid);
+		cmd[0] |= FIELD_PREP(CMDQ_TLBI_0_NUM, ent->tlbi.num);
+		cmd[0] |= FIELD_PREP(CMDQ_TLBI_0_SCALE, ent->tlbi.scale);
+		cmd[0] |= FIELD_PREP(CMDQ_TLBI_0_ASID, ent->tlbi.asid);
+		cmd[1] |= FIELD_PREP(CMDQ_TLBI_1_LEAF, ent->tlbi.leaf);
+		cmd[1] |= FIELD_PREP(CMDQ_TLBI_1_TTL, ent->tlbi.ttl);
+		cmd[1] |= FIELD_PREP(CMDQ_TLBI_1_TG, ent->tlbi.tg);
+		cmd[1] |= ent->tlbi.addr & CMDQ_TLBI_1_VA_MASK;
+		break;
 	case CMDQ_OP_TLBI_NSNH_ALL:
 		break;
+	case CMDQ_OP_TLBI_NH_ASID:
+		cmd[0] |= FIELD_PREP(CMDQ_TLBI_0_ASID, ent->tlbi.asid);
+		fallthrough;
 	case CMDQ_OP_TLBI_S12_VMALL:
 		cmd[0] |= FIELD_PREP(CMDQ_TLBI_0_VMID, ent->tlbi.vmid);
 		break;
@@ -457,10 +470,19 @@ static void smmu_tlb_flush_all(void *cookie)
 	struct hyp_arm_smmu_v3_domain *smmu_domain = domain->priv;
 	struct hyp_arm_smmu_v3_device *smmu;
 	struct domain_iommu_node *iommu_node;
-	struct arm_smmu_cmdq_ent cmd = {
-		.opcode = CMDQ_OP_TLBI_S12_VMALL,
-		.tlbi.vmid = data->domain_id,
-	};
+	struct arm_smmu_cmdq_ent cmd;
+	struct arm_lpae_io_pgtable *pgtable = container_of(domain->pgtable,
+							   struct arm_lpae_io_pgtable, iop);
+
+	if (pgtable->iop.cfg.fmt == ARM_64_LPAE_S2) {
+		cmd.opcode = CMDQ_OP_TLBI_S12_VMALL;
+		cmd.tlbi.vmid = data->domain_id;
+	} else {
+		cmd.opcode = CMDQ_OP_TLBI_NH_ASID;
+		cmd.tlbi.asid = data->domain_id;
+		/* Domain ID is unique across all VMs. */
+		cmd.tlbi.vmid = 0;
+	}
 
 	list_for_each_entry(iommu_node, &smmu_domain->iommu_list, list) {
 		smmu = to_smmu(iommu_node->iommu);
@@ -483,12 +505,19 @@ static void smmu_tlb_inv_range(struct kvm_iommu_tlb_cookie *data,
 	struct hyp_arm_smmu_v3_device *smmu;
 	struct domain_iommu_node *iommu_node;
 	unsigned long end = iova + size;
-	struct arm_smmu_cmdq_ent cmd = {
-		.opcode = CMDQ_OP_TLBI_S2_IPA,
-		.tlbi.vmid = data->domain_id,
-		.tlbi.leaf = leaf,
-	};
+	struct arm_smmu_cmdq_ent cmd;
+	struct arm_lpae_io_pgtable *pgtable = container_of(domain->pgtable,
+							   struct arm_lpae_io_pgtable, iop);
 
+	cmd.tlbi.leaf = leaf;
+	if (pgtable->iop.cfg.fmt == ARM_64_LPAE_S2) {
+		cmd.opcode = CMDQ_OP_TLBI_S2_IPA;
+		cmd.tlbi.vmid = data->domain_id;
+	} else {
+		cmd.opcode = CMDQ_OP_TLBI_NH_VA;
+		cmd.tlbi.asid = data->domain_id;
+		cmd.tlbi.vmid = 0;
+	}
 	/*
 	 * There are no mappings at high addresses since we don't use TTB1, so
 	 * no overflow possible.
