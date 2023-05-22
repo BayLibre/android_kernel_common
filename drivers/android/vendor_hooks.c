a95bebb42593d0e5ac68ece00a7ca17543847249
Sarthak Garg <quic_sartgarg@quicinc.com>
2023-05-22 12:43:31 +0530

    ANDROID: vendor_hooks: Define new hooks in _mmc_suspend/resume
    Define new hooks in _mmc_suspend/resume to control few things in card
    suspend/resume paths which further allows to enable some additional
    steps in mmc_suspend/resume paths as per host specific requirements.
    
    Bug: 283922495
    Link:
    https://patchwork.kernel.org/project/linux-mmc/patch/20230401165723.19762-2-quic_sartgarg@quicinc.com/
    
    Change-Id: Ief52d1dc6b01e9866f004b46687dffa4eb1e7bc1
    Signed-off-by: Sarthak Garg <quic_sartgarg@quicinc.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -29,6 +29,7 @@
 #include <trace/hooks/preemptirq.h>
 #include <trace/hooks/ftrace_dump.h>
 #include <trace/hooks/ufshcd.h>
+#include <trace/hooks/mmc.h>
 #include <trace/hooks/cgroup.h>
 #include <trace/hooks/sys.h>
 #include <trace/hooks/iommu.h>
@@ -245,3 +246,5 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_psi_event);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_psi_group);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_cpufreq_acct_update_power);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_exit_signal);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_mmc_resume);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_mmc_suspend);
