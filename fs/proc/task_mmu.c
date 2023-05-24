6a1f28ca4ab201efdf5c1aa7a958daa290bb5d8c
Sooyong Suk <s.suk@samsung.com>
2023-05-24 09:34:19 +0900

    ANDROID: task_mmu: add vendor hook for swap entry
    Add vendor hook in smaps_pte_entry for swap entry
    - android_vh_smaps_pte_entry
    
    This vendor hook is to show more information for
    swap entries of a process based on the
    characteristics, such as written-back, same-filled
    or huge (uncompressed).
    
    Bug: 284059805
    
    Change-Id: Ie4a48ae42212c056992d34a10b026b60439d0012
    Signed-off-by: Sooyong Suk <s.suk@samsung.com>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -403,6 +403,9 @@ struct mem_size_stats {
 	unsigned long shmem_thp;
 	unsigned long file_thp;
 	unsigned long swap;
+	unsigned long writeback;
+	unsigned long same;
+	unsigned long huge;
 	unsigned long shared_hugetlb;
 	unsigned long private_hugetlb;
 	u64 pss;
@@ -412,6 +415,12 @@ struct mem_size_stats {
 	u64 pss_dirty;
 	u64 pss_locked;
 	u64 swap_pss;
+};
+
+enum swap_entry_type {
+	SWP_WRITEBACK = 1,
+	SWP_SAME,
+	SWP_HUGE,
 };
 
 static void smaps_page_accumulate(struct mem_size_stats *mss,
@@ -541,6 +550,7 @@ static void smaps_pte_entry(pte_t *pte, unsigned long addr,
 
 		if (!non_swap_entry(swpent)) {
 			int mapcount;
+			int type = 0;
 
 			mss->swap += PAGE_SIZE;
 			mapcount = swp_swapcount(swpent);
@@ -552,6 +562,13 @@ static void smaps_pte_entry(pte_t *pte, unsigned long addr,
 			} else {
 				mss->swap_pss += (u64)PAGE_SIZE << PSS_SHIFT;
 			}
+			trace_android_vh_smaps_pte_entry(swpent, &type);
+			if (type == SWP_WRITEBACK)
+				mss->writeback += PAGE_SIZE;
+			else if (type == SWP_SAME)
+				mss->same += PAGE_SIZE;
+			else if (type == SWP_HUGE)
+				mss->huge += PAGE_SIZE;
 		} else if (is_pfn_swap_entry(swpent)) {
 			if (is_migration_entry(swpent))
 				migration = true;
@@ -841,6 +858,9 @@ static void __show_smap(struct seq_file *m, const struct mem_size_stats *mss,
 	SEQ_PUT_DEC(" kB\nSwap:           ", mss->swap);
 	SEQ_PUT_DEC(" kB\nSwapPss:        ",
 					mss->swap_pss >> PSS_SHIFT);
+	SEQ_PUT_DEC(" kB\nWriteback:      ", mss->writeback);
+	SEQ_PUT_DEC(" kB\nSame:           ", mss->same);
+	SEQ_PUT_DEC(" kB\nHuge:           ", mss->huge);
 	SEQ_PUT_DEC(" kB\nLocked:         ",
 					mss->pss_locked >> PSS_SHIFT);
 	seq_puts(m, " kB\n");
