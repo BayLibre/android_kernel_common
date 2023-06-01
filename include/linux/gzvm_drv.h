9f3065e0ee3e4327694a974767d5967fce802b5c
Yi-De Wu <yi-de.wu@mediatek.com>
2023-06-01 15:15:15 +0800

    FROMLIST: virt: geniezone: Add dtb config support
    Hypervisor might need to know the accurate address and size of dtb
    passed from userspace. And then hypervisor would parse the dtb and get
    vm information.
    
    Change-Id: I23194d45f5c60555ba7fde9dd8d393443fd41310
    Signed-off-by: Jerry Wang <ze-yu.wang@mediatek.com>
    Signed-off-by: Liju-clr Chen <liju-clr.chen@mediatek.com>
    Signed-off-by: Yi-De Wu <yi-de.wu@mediatek.com>
    Bug: 280363874
    Link: https://lore.kernel.org/lkml/20230609085214.31071-9-yi-de.wu@mediatek.com/

--- include/linux/gzvm_drv.h
+++ include/linux/gzvm_drv.h
@@ -146,6 +146,7 @@ extern struct platform_device *gzvm_debug_dev;
 
 int gzvm_arch_memregion_purpose(struct gzvm *gzvm,
 				struct gzvm_userspace_memory_region *mem);
+int gzvm_arch_set_dtb_config(struct gzvm *gzvm, struct gzvm_dtb_config *args);
 
 int gzvm_init_ioeventfd(struct gzvm *gzvm);
 int gzvm_ioeventfd(struct gzvm *gzvm, struct gzvm_ioeventfd *args);
