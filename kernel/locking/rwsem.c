e61f85d171cb33180687e08c05a327ef9c0f73fe
xieliujie <xieliujie@oppo.com>
2023-06-06 20:45:12 +0800

    ANDROID: vendor_hook: Avoid clearing protect-flag before waking waiters
    With hooks below, we can mark a lock-owned thread with an identifiable flag, which can protect it from being preempted by some other unimportant threads, and then waiter will be wakeup more quickly.
    https://android-review.googlesource.com/c/kernel/common/+/2183353
    
    but now we find an issue like this one:
    static inline void __up_write(struct rw_semaphore *sem)
    {
            ...
            // Step 1. we clear flag.
            trace_android_vh_record_rwsem_lock_starttime(current, 0);
            // Step 2. owner may be preempted by unimportant threads.
            rwsem_clear_owner(sem);
            ...
            // Step 3. wake up waiter, but it's too later.
            if (unlikely(tmp & RWSEM_FLAG_WAITERS))
                    rwsem_wake(sem);
    }
    
    This patch will clear protect-flag after waking up waiters.
    
    Bug: 286024926
    Change-Id: I71f8b6a7d8a01336fd36b8267c2cb5edab65bd11
    Signed-off-by: xieliujie <xieliujie@oppo.com>
    (cherry picked from commit be4040bed108d3557d28c324d99de88cfa3ddb6c)

--- kernel/locking/rwsem.c
+++ kernel/locking/rwsem.c
@@ -1368,7 +1368,11 @@ static inline void __up_read(struct rw_semaphore *sem)
 	DEBUG_RWSEMS_WARN_ON(sem->magic != sem, sem);
 	DEBUG_RWSEMS_WARN_ON(!is_rwsem_reader_owned(sem), sem);
 
+<<<<<<< HEAD   (63dc3a FROMGIT: pstore: Revert pmsg_lock back to a normal mutex)
 	trace_android_vh_record_rwsem_lock_starttime(current, 0);
+=======
+	preempt_disable();
+>>>>>>> CHANGE (be4040 ANDROID: vendor_hook: Avoid clearing protect-flag before wak)
 	rwsem_clear_reader_owned(sem);
 	tmp = atomic_long_add_return_release(-RWSEM_READER_BIAS, &sem->count);
 	DEBUG_RWSEMS_WARN_ON(tmp < 0, sem);
@@ -1377,6 +1381,11 @@ static inline void __up_read(struct rw_semaphore *sem)
 		clear_nonspinnable(sem);
 		rwsem_wake(sem);
 	}
+<<<<<<< HEAD   (63dc3a FROMGIT: pstore: Revert pmsg_lock back to a normal mutex)
+=======
+	trace_android_vh_record_rwsem_lock_starttime(current, 0);
+	preempt_enable();
+>>>>>>> CHANGE (be4040 ANDROID: vendor_hook: Avoid clearing protect-flag before wak)
 }
 
 /*
@@ -1394,13 +1403,13 @@ static inline void __up_write(struct rw_semaphore *sem)
 	DEBUG_RWSEMS_WARN_ON((rwsem_owner(sem) != current) &&
 			    !rwsem_test_oflags(sem, RWSEM_NONSPINNABLE), sem);
 
-	trace_android_vh_record_rwsem_lock_starttime(current, 0);
 	preempt_disable();
 	rwsem_clear_owner(sem);
 	tmp = atomic_long_fetch_add_release(-RWSEM_WRITER_LOCKED, &sem->count);
 	preempt_enable();
 	if (unlikely(tmp & RWSEM_FLAG_WAITERS))
 		rwsem_wake(sem);
+	trace_android_vh_record_rwsem_lock_starttime(current, 0);
 }
 
 /*
