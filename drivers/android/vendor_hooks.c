cfa3d7bb3b7713a599fd900f044e2909a90eadd3
Hajun Sung <hajun.sung@samsung.com>
2023-06-08 13:47:11 +0900

    ANDROID: arm64: add vendor hooks for bad_stack.
    - Bad stack action can cause other data abort or else.
      Add vendor hook for exceptional work process.
    
      Also, bad stack behavior can not be caused by SW action,
      then vendor can collect debug info before panic print and makes force reset
      to get HW status for futher analysis.
    
    Bug: 286198053
    Signed-off-by: Hajun Sung <hajun.sung@samsung.com>
    Change-Id: Ide53b2e7ec6a18f9f0bbc1cd5c3d0858de108939

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -227,6 +227,7 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_do_sp_pc_abort);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_do_undefinstr);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_do_el1_bti);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_do_el1_fpac);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_handle_bad_stack);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_panic_unhandled);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_arm64_serror_panic);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_disable_thermal_cooling_stats);
