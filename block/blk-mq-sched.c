e91d1e4f1273dd07a7e299ce5406d3fa104dd3ab
Randall Huang <huangrandall@google.com>
2023-06-08 05:17:32 +0000

    Revert "ANDROID: block: Simplify blk_mq_sched_bypass_insert()"
    This reverts commit 0545a08098470f8bd8e4035bacb6dc9ffdb04b04.
    
    Reason for revert: b/285769645
    
    Change-Id: Ib00fc6e71202eb562ad976339e2c54743d8292f9
    Signed-off-by: Randall Huang <huangrandall@google.com>

--- block/blk-mq-sched.c
+++ block/blk-mq-sched.c
@@ -425,7 +425,10 @@ static bool blk_mq_sched_bypass_insert(struct request *rq)
 	 * passthrough request is added to scheduler queue, there isn't any
 	 * chance to dispatch it given we prioritize requests in hctx->dispatch.
 	 */
-	return (rq->rq_flags & RQF_FLUSH_SEQ) || blk_rq_is_passthrough(rq);
+	if ((rq->rq_flags & RQF_FLUSH_SEQ) || blk_rq_is_passthrough(rq))
+		return true;
+
+	return false;
 }
 
 void blk_mq_sched_insert_request(struct request *rq, bool at_head,
