b8012e4431ea5a4dfb2257bfc5c2d4f2fb7f3f2d
Bart Van Assche <bvanassche@acm.org>
2023-06-12 13:33:11 -0700

    ANDROID: block: Support submitting passthrough requests with small segments
    If the segment size is smaller than the page size there may be multiple
    segments per bvec even if a bvec only contains a single page. Hence this
    patch.
    
    Bug: 308663717
    Bug: 319125789
    Change-Id: I446aab83a2c519cb3c42d5d8ffd814dcc34274d2
    Signed-off-by: Bart Van Assche <bvanassche@acm.org>

--- block/blk-map.c
+++ block/blk-map.c
@@ -266,17 +266,26 @@ static int bio_map_user_iov(struct request *rq, struct iov_iter *iter,
 		} else {
 			for (j = 0; j < npages; j++) {
 				struct page *page = pages[j];
-				unsigned int n = PAGE_SIZE - offs;
+				unsigned int n = PAGE_SIZE - offs, page_bytes;
 				bool same_page = false;
 
 				if (n > bytes)
 					n = bytes;
 
-				if (!bio_add_hw_page(rq->q, bio, page, n, offs,
-						     max_sectors, &same_page)) {
+				page_bytes = bio_add_hw_page(rq->q, bio, page, n,
+						offs, max_sectors, &same_page);
+				if (page_bytes == 0) {
 					if (same_page)
 						put_page(page);
 					break;
+				} else if (page_bytes != n) {
+					/*
+					 * The segment size is smaller than the
+					 * page size and an iov exceeds the
+					 * segment size. Give up.
+					 */
+					ret = -EREMOTEIO;
+					goto out_unmap;
 				}
 
 				added += n;
@@ -486,7 +495,7 @@ int blk_rq_append_bio(struct request *rq, struct bio *bio)
 	unsigned int nr_segs = 0;
 
 	bio_for_each_bvec(bv, bio, iter)
-		nr_segs++;
+		nr_segs += blk_segments(&rq->q->limits, bv.bv_len);
 
 	if (!rq->bio) {
 		blk_rq_bio_prep(rq, bio, nr_segs);
@@ -542,10 +551,18 @@ int blk_rq_map_user_iov(struct request_queue *q, struct request *rq,
 
 	i = *iter;
 	do {
-		if (copy)
+		if (copy) {
 			ret = bio_copy_user_iov(rq, map_data, &i, gfp_mask);
-		else
+		} else {
 			ret = bio_map_user_iov(rq, &i, gfp_mask);
+			/*
+			 * Fall back to copying the data if bio_map_user_iov()
+			 * returns -EREMOTEIO.
+			 */
+			if (ret == -EREMOTEIO)
+				ret = bio_copy_user_iov(rq, map_data, &i,
+							gfp_mask);
+		}
 		if (ret)
 			goto unmap_rq;
 		if (!bio)
