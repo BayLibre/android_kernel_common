1ef4ebc97428305a6f679aa7507f2de4515da4b3
Manish Pandey <quic_mapa@quicinc.com>
2023-06-15 20:11:56 +0530

    ANDROID: GKI: Fix inconsistent for ufshcd_wb_toggle.
    After commit (7e00979 Revert "scsi: ufs: core: Fix devfreq deadlocks)
    ufshcd_wb_toggle is called twice, which leads wb_enabled flag inconsistency .
    
    Fix this by removing ufshcd_wb_toggle while ufshcd_clock_scaling_unprepare.
    
    Change-Id: I12c0eac4c58ce823915cb5ba9bfd474687436435
    Signed-off-by: Manish Pandey <quic_mapa@quicinc.com>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -1226,15 +1226,10 @@ out:
 
 static void ufshcd_clock_scaling_unprepare(struct ufs_hba *hba, bool writelock)
 {
-	/* Enable Write Booster if we have scaled up else disable it */
-	if (ufshcd_enable_wb_if_scaling_up(hba)) {
-		if (writelock)
-			up_write(&hba->clk_scaling_lock);
-		else
-			up_read(&hba->clk_scaling_lock);
-
-		ufshcd_wb_toggle(hba, writelock);
-	}
+	if (writelock)
+		up_write(&hba->clk_scaling_lock);
+	else
+		up_read(&hba->clk_scaling_lock);
 
 	ufshcd_scsi_unblock_requests(hba);
 	ufshcd_release(hba);
@@ -1282,9 +1277,11 @@ static int ufshcd_devfreq_scale(struct ufs_hba *hba, bool scale_up)
 	}
 
 	/* Enable Write Booster if we have scaled up else disable it */
-	downgrade_write(&hba->clk_scaling_lock);
-	is_writelock = false;
-	ufshcd_wb_toggle(hba, scale_up);
+	if (ufshcd_enable_wb_if_scaling_up(hba)) {
+		downgrade_write(&hba->clk_scaling_lock);
+		is_writelock = false;
+		ufshcd_wb_toggle(hba, scale_up);
+	}
 
 out_unprepare:
 	ufshcd_clock_scaling_unprepare(hba, is_writelock);
