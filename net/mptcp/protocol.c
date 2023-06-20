caa7ecbbe09d90598d166ff808a8e8b01785a804
Paolo Abeni <pabeni@redhat.com>
2023-06-20 18:24:23 +0200

    BACKPORT: mptcp: ensure listener is unhashed before updating the sk status
    commit 57fc0f1ceaa4016354cf6f88533e20b56190e41a upstream.
    
    The MPTCP protocol access the listener subflow in a lockless
    manner in a couple of places (poll, diag). That works only if
    the msk itself leaves the listener status only after that the
    subflow itself has been closed/disconnected. Otherwise we risk
    deadlock in diag, as reported by Christoph.
    
    Address the issue ensuring that the first subflow (the listener
    one) is always disconnected before updating the msk socket status.
    
    Reported-by: Christoph Paasch <cpaasch@apple.com>
    Closes: https://github.com/multipath-tcp/mptcp_net-next/issues/407
    Fixes: b29fcfb54cd7 ("mptcp: full disconnect implementation")
    Cc: stable@vger.kernel.org
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    Reviewed-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    [surenb: skipped change in __mptcp_close_ssk(), code is not there]
    Change-Id: I65240daef63e57b2fa9a92a711ec610a32f1dcaf
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- net/mptcp/protocol.c
+++ net/mptcp/protocol.c
@@ -2918,6 +2918,24 @@ static __poll_t mptcp_check_readable(struct mptcp_sock *msk)
 	return EPOLLIN | EPOLLRDNORM;
 }
 
+static void mptcp_check_listen_stop(struct sock *sk)
+{
+	struct sock *ssk;
+
+	if (inet_sk_state_load(sk) != TCP_LISTEN)
+		return;
+
+	ssk = mptcp_sk(sk)->first;
+	if (WARN_ON_ONCE(!ssk || inet_sk_state_load(ssk) != TCP_LISTEN))
+		return;
+
+	lock_sock_nested(ssk, SINGLE_DEPTH_NESTING);
+	mptcp_subflow_queue_clean(sk, ssk);
+	inet_csk_listen_stop(ssk);
+	tcp_set_state(ssk, TCP_CLOSE);
+	release_sock(ssk);
+}
+
 bool __mptcp_close(struct sock *sk, long timeout)
 {
 	struct mptcp_subflow_context *subflow;
@@ -2928,6 +2946,7 @@ bool __mptcp_close(struct sock *sk, long timeout)
 	sk->sk_shutdown = SHUTDOWN_MASK;
 
 	if ((1 << sk->sk_state) & (TCPF_LISTEN | TCPF_CLOSE)) {
+		mptcp_check_listen_stop(sk);
 		inet_sk_state_store(sk, TCP_CLOSE);
 		goto cleanup;
 	}
@@ -3036,6 +3055,7 @@ static int mptcp_disconnect(struct sock *sk, int flags)
 	if (msk->fastopening)
 		return 0;
 
+	mptcp_check_listen_stop(sk);
 	inet_sk_state_store(sk, TCP_CLOSE);
 
 	mptcp_stop_timer(sk);
