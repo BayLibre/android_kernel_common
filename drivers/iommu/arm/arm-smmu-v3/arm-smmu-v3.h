863dcd29923643265988da2599ddb41a377bb0d0
Michael Shavit <mshavit@google.com>
2023-06-21 14:37:24 +0800

    FROMLIST: iommu/arm-smmu-v3-sva: Remove arm_smmu_bond
    There's a 1:1 relationship between arm_smmu_bond and the iommu_domain
    used in set_dev_pasid/remove_dev_pasid. arm_smmu_bond has become an
    unnecessary complication. It's more natural to store any needed
    information at the iommu_domain container level.
    
    Link: https://lore.kernel.org/r/20230621063825.268890-13-mshavit@google.com
    Change-Id: I227176e9c2b8b7c6d2681ac391939df2ac9fb52a
    Signed-off-by: Michael Shavit <mshavit@google.com>
    Bug: 244120707

--- drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.h
+++ drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.h
@@ -705,7 +705,7 @@ struct arm_smmu_master {
 	bool				stall_enabled;
 	bool				sva_enabled;
 	bool				iopf_enabled;
-	struct list_head		bonds;
+	unsigned int			nr_attached_sva_domains;
 	unsigned int			ssid_bits;
 	unsigned int			nr_attached_pasid_domains;
 };
