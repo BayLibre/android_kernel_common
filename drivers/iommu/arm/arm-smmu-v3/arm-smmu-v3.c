afea69c39d0917e1b4c5887c4862d031e1c185c5
Michael Shavit <mshavit@google.com>
2023-06-21 14:37:18 +0800

    FROMLIST: iommu/arm-smmu-v3: Simplify arm_smmu_enable_ats
    arm_smmu_enable_ats's call to inv_domain would trigger an invalidation
    for all masters that a domain is attached to everytime it's attached to
    another ATS-enabled master. It doesn't seem like those invalidations are
    necessary, and it's easier to reason about arm_smmu_enable_ats if it
    only issues invalidation commands for the current master.
    
    Link: https://lore.kernel.org/r/20230621063825.268890-7-mshavit@google.com
    Change-Id: I8185028500e29c26f8e5b1a2c4bc5057c777f728
    Signed-off-by: Michael Shavit <mshavit@google.com>
    Bug: 244120707

--- drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.c
+++ drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.c
@@ -2277,7 +2277,7 @@ static void arm_smmu_enable_ats(struct arm_smmu_master *master)
 	pdev = to_pci_dev(master->dev);
 
 	atomic_inc(&smmu_domain->nr_ats_masters);
-	arm_smmu_atc_inv_domain(smmu_domain, 0, 0, 0);
+	arm_smmu_atc_inv_master(master);
 	if (pci_enable_ats(pdev, stu))
 		dev_err(master->dev, "Failed to enable ATS (STU %zu)\n", stu);
 }
