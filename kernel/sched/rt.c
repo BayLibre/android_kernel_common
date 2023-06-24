c2a8e282acbafc543093f7d0571680fb91a0cbc9
John Stultz <jstultz@google.com>
2023-06-24 03:17:20 +0000

    sched: Add an initial sketch of the find_proxy_task() function
    Add a find_proxy_task() function which doesn't do much.
    
    When we select a blocked task to run, we will just deactivate it
    and pick again. The exception being if it has become unblocked
    after find_proxy_task() was called.
    
    Greatly simplified from patch by:
      Peter Zijlstra (Intel) <peterz@infradead.org>
      Juri Lelli <juri.lelli@redhat.com>
      Valentin Schneider <valentin.schneider@arm.com>
      Connor O'Brien <connoro@google.com>
    
    Cc: Joel Fernandes <joelaf@google.com>
    Cc: Qais Yousef <qyousef@google.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Juri Lelli <juri.lelli@redhat.com>
    Cc: Vincent Guittot <vincent.guittot@linaro.org>
    Cc: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Cc: Valentin Schneider <vschneid@redhat.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Ben Segall <bsegall@google.com>
    Cc: Zimuzo Ezeozue <zezeozue@google.com>
    Cc: Youssef Esmat <youssefesmat@google.com>
    Cc: Mel Gorman <mgorman@suse.de>
    Cc: Daniel Bristot de Oliveira <bristot@redhat.com>
    Cc: Will Deacon <will@kernel.org>
    Cc: Waiman Long <longman@redhat.com>
    Cc: Boqun Feng <boqun.feng@gmail.com>
    Cc: "Paul E. McKenney" <paulmck@kernel.org>
    Cc: Metin Kaya <Metin.Kaya@arm.com>
    Cc: Xuewen Yan <xuewen.yan94@gmail.com>
    Cc: K Prateek Nayak <kprateek.nayak@amd.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: kernel-team@android.com
    [jstultz: Split out from larger proxy patch and simplified
     for review and testing.]
    Signed-off-by: John Stultz <jstultz@google.com>
    Change-Id: I4961d35b6b680ca7233a75fc6c0e025d748f88e1
    ---
    v5:
    * Split out from larger proxy patch
    v7:
    * Fixed unused function arguments, spelling nits, and tweaks for
      clarity, pointed out by Metin Kaya
    v8:
    * Fixed case where we might return a blocked task from find_proxy_task()
    * More cleanups and typo fixes from Metin Kaya
    * Dropped BUG() in !PROXY case for WARN_ONCE

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -1568,8 +1568,19 @@ enqueue_task_rt(struct rq *rq, struct task_struct *p, int flags)
 
 	enqueue_rt_entity(rt_se, flags);
 
-	if (!task_current(rq, p) && p->nr_cpus_allowed > 1 &&
-	    !should_honor_rt_sync(rq, p, sync))
+	/*
+	 * Current can't be pushed away. Selected is tied to current,
+	 * so don't push it either.
+	 */
+	if (task_current(rq, p) || task_current_selected(rq, p))
+		return;
+	/*
+	 * Pinned tasks can't be pushed.
+	 */
+	if (p->nr_cpus_allowed == 1)
+		return;
+
+	if (!should_honor_rt_sync(rq, p, sync))
 		enqueue_pushable_task(rq, p);
 }
 
