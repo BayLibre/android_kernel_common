20b9b1bdb3a064cbdebab878107258b2c9fdd6e2
Alice Ryhl <aliceryhl@google.com>
2023-07-03 12:07:30 +0000

    FROMLIST: rust_binder: add oneway transactions
    Add support for sending oneway transactions using the binder driver.
    
    Oneway transactions have a special requirement that they need to be
    serialized. That is, the driver should not deliver an oneway transaction
    when another one is already being handled. This is to avoid the case
    where oneway transactions appear to arrive in the wrong order to
    userspace. However, this patch does not implement serialization of
    oneway transactions. Therefore, this patch does not actually make it
    possible to send oneway transactions - it just defines them and adds a
    TODO for making it possible to send them. It will be made possible to
    send them in a later patch that makes them serialized.
    
    To receive transactions, the process must first use mmap to create a
    memory region for holding the contents of incoming transactions. The
    driver will manage the resulting memory using two files: `allocation.rs`
    and `range_alloc.rs`.
    
    The `allocation.rs` file is responsible for actually managing the
    mmap'ed region of memory and has methods for writing to it.
    
    The `range_alloc.rs` file contains a data structure for tracking where
    in the mmap we are storing different things. It doesn't actually touch
    the mmap itself. Basically, it's a data structure that stores a set of
    non-overlapping intervals (the allocations) and it is able to find the
    smallest offset where the next X bytes are free and allocate that
    region.
    
    Other than that, this patch introduces a `Transaction` struct that
    stores the information related to a transaction, and adds the necessary
    infrastructure to send and receive them. This uses the work lists
    introduced by a previous patch to deliver incoming transactions.
    
    There are several different possible implementations of the range
    allocator, and we have implemented several of them. The simplest
    possible implementation is to use a linked list to store the allocations
    and free regions sorted by address. Another possibility is to store the
    same thing using a red-black tree. The red-black tree is preferable to
    the linked list because its accesses are logarithmic rather than linear.
    
    This RFC implements the range allocator using a red-black tree.
    
    We have also looked into replacing the red-black tree with an XArray.
    However, this is challenging because it doesn't have a good way to look
    up the smallest free region whose size is at least some lower bound. You
    can use `xa_find`, but there could be many free regions of the same
    size, which makes it a challenge to maintain this information correctly.
    We also run into issues with having to allocate while holding a lock.
    Finally, the XArray is not optimized for this use-case: all of the
    indices are going to have gaps between them.
    
    Co-developed-by: Wedson Almeida Filho <wedsonaf@gmail.com>
    Signed-off-by: Wedson Almeida Filho <wedsonaf@gmail.com>
    Co-developed-by: Matt Gilbride <mattgilbride@google.com>
    Signed-off-by: Matt Gilbride <mattgilbride@google.com>
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>
    Change-Id: Iefd57c94a55db88d88258115909dd503149f941f
    Link: https://lore.kernel.org/rust-for-linux/20231101-rust-binder-v1-6-08ba9197f637@google.com/
    (cherry picked from commit 3c9969b026cb0274d6a7086957f0e534ddf79016
     https://github.com/darksonn/linux rust-binder-rfc)
    [ Changed according to reviews on gerrit. ]
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>
    Bug: 278052745

--- drivers/android/process.rs
+++ drivers/android/process.rs
@@ -16,6 +16,7 @@ use kernel::{
     file::{self, File},
     list::{HasListLinks, List, ListArc, ListArcSafe, ListItem, ListLinks},
     mm,
+    page::Page,
     prelude::*,
     rbtree::{self, RBTree},
     sync::poll::PollTable,
@@ -27,15 +28,34 @@ use kernel::{
 };
 
 use crate::{
+    allocation::{Allocation, AllocationInfo},
     context::Context,
     defs::*,
-    error::BinderError,
+    error::{BinderError, BinderResult},
     node::{Node, NodeRef},
+    range_alloc::{self, RangeAllocator},
     thread::{PushWorkRes, Thread},
     DArc, DLArc, DTRWrap, DeliverToRead,
 };
 
 use core::mem::take;
+
+struct Mapping {
+    address: usize,
+    alloc: RangeAllocator<AllocationInfo>,
+    pages: Arc<Vec<Page>>,
+}
+
+impl Mapping {
+    fn new(address: usize, size: usize, pages: Arc<Vec<Page>>) -> Result<Self> {
+        let alloc = RangeAllocator::new(size)?;
+        Ok(Self {
+            address,
+            alloc,
+            pages,
+        })
+    }
+}
 
 const PROC_DEFER_FLUSH: u8 = 1;
 const PROC_DEFER_RELEASE: u8 = 2;
@@ -48,6 +68,7 @@ pub(crate) struct ProcessInner {
     /// INVARIANT: Threads pushed to this list must be owned by this process.
     ready_threads: List<Thread>,
     nodes: RBTree<u64, DArc<Node>>,
+    mapping: Option<Mapping>,
     work: List<DTRWrap<dyn DeliverToRead>>,
 
     /// The number of requested threads that haven't registered yet.
@@ -68,6 +89,7 @@ impl ProcessInner {
             is_dead: false,
             threads: RBTree::new(),
             ready_threads: List::new(),
+            mapping: None,
             nodes: RBTree::new(),
             work: List::new(),
             requested_thread_count: 0,
@@ -479,6 +501,15 @@ impl Process {
         Ok(target)
     }
 
+    pub(crate) fn get_transaction_node(&self, handle: u32) -> BinderResult<NodeRef> {
+        // When handle is zero, try to get the context manager.
+        if handle == 0 {
+            Ok(self.ctx.get_manager_node(true)?)
+        } else {
+            Ok(self.get_node_from_handle(handle, true)?)
+        }
+    }
+
     pub(crate) fn get_node_from_handle(&self, handle: u32, strong: bool) -> Result<NodeRef> {
         self.node_refs
             .lock()
@@ -526,6 +557,97 @@ impl Process {
                     // This only fails if the process is dead.
                     let _ = inner.push_work(node);
                 }
+            }
+        }
+        Ok(())
+    }
+
+    pub(crate) fn buffer_alloc(
+        self: &Arc<Self>,
+        size: usize,
+        is_oneway: bool,
+    ) -> BinderResult<Allocation> {
+        let alloc = range_alloc::ReserveNewBox::try_new()?;
+        let mut inner = self.inner.lock();
+        let mapping = inner.mapping.as_mut().ok_or_else(BinderError::new_dead)?;
+        let offset = mapping.alloc.reserve_new(size, is_oneway, alloc)?;
+        Ok(Allocation::new(
+            self.clone(),
+            offset,
+            size,
+            mapping.address + offset,
+            mapping.pages.clone(),
+        ))
+    }
+
+    pub(crate) fn buffer_get(self: &Arc<Self>, ptr: usize) -> Option<Allocation> {
+        let mut inner = self.inner.lock();
+        let mapping = inner.mapping.as_mut()?;
+        let offset = ptr.checked_sub(mapping.address)?;
+        let (size, odata) = mapping.alloc.reserve_existing(offset).ok()?;
+        let mut alloc = Allocation::new(self.clone(), offset, size, ptr, mapping.pages.clone());
+        if let Some(data) = odata {
+            alloc.set_info(data);
+        }
+        Some(alloc)
+    }
+
+    pub(crate) fn buffer_raw_free(&self, ptr: usize) {
+        let mut inner = self.inner.lock();
+        if let Some(ref mut mapping) = &mut inner.mapping {
+            if ptr < mapping.address
+                || mapping
+                    .alloc
+                    .reservation_abort(ptr - mapping.address)
+                    .is_err()
+            {
+                pr_warn!(
+                    "Pointer {:x} failed to free, base = {:x}\n",
+                    ptr,
+                    mapping.address
+                );
+            }
+        }
+    }
+
+    pub(crate) fn buffer_make_freeable(&self, offset: usize, data: Option<AllocationInfo>) {
+        let mut inner = self.inner.lock();
+        if let Some(ref mut mapping) = &mut inner.mapping {
+            if mapping.alloc.reservation_commit(offset, data).is_err() {
+                pr_warn!("Offset {} failed to be marked freeable\n", offset);
+            }
+        }
+    }
+
+    fn create_mapping(&self, vma: &mut mm::virt::Area) -> Result {
+        use kernel::page::PAGE_SIZE;
+        let size = core::cmp::min(vma.end() - vma.start(), bindings::SZ_4M as usize);
+        let page_count = size / PAGE_SIZE;
+
+        // Allocate and map all pages.
+        //
+        // N.B. If we fail halfway through mapping these pages, the kernel will unmap them.
+        let mut pages = Vec::new();
+        pages.try_reserve_exact(page_count)?;
+        let mut address = vma.start();
+        for _ in 0..page_count {
+            let page = Page::new()?;
+            vma.insert_page(address, &page)?;
+            pages.try_push(page)?;
+            address += PAGE_SIZE;
+        }
+
+        let ref_pages = Arc::try_new(pages)?;
+        let mapping = Mapping::new(vma.start(), size, ref_pages)?;
+
+        // Save pages for later.
+        let mut inner = self.inner.lock();
+        match &inner.mapping {
+            None => inner.mapping = Some(mapping),
+            Some(_) => {
+                drop(inner);
+                drop(mapping);
+                return Err(EBUSY);
             }
         }
         Ok(())
@@ -630,18 +752,6 @@ impl Process {
 
         self.ctx.deregister_process(&self);
 
-        // Cancel all pending work items.
-        while let Some(work) = self.get_work() {
-            work.into_arc().cancel();
-        }
-
-        // Drop all references. We do this dance with `swap` to avoid destroying the references
-        // while holding the lock.
-        let mut refs = self.node_refs.lock();
-        let mut node_refs = take(&mut refs.by_handle);
-        drop(refs);
-        drop(node_refs);
-
         // Move the threads out of `inner` so that we can iterate over them without holding the
         // lock.
         let mut inner = self.inner.lock();
@@ -652,6 +762,33 @@ impl Process {
         for thread in threads.values() {
             thread.release();
         }
+
+        // Cancel all pending work items.
+        while let Some(work) = self.get_work() {
+            work.into_arc().cancel();
+        }
+
+        // Free any resources kept alive by allocated buffers.
+        let omapping = self.inner.lock().mapping.take();
+        if let Some(mut mapping) = omapping {
+            let address = mapping.address;
+            let pages = mapping.pages.clone();
+            mapping.alloc.take_for_each(|offset, size, odata| {
+                let ptr = offset + address;
+                let mut alloc = Allocation::new(self.clone(), offset, size, ptr, pages.clone());
+                if let Some(data) = odata {
+                    alloc.set_info(data);
+                }
+                drop(alloc)
+            });
+        }
+
+        // Drop all references. We do this dance with `swap` to avoid destroying the references
+        // while holding the lock.
+        let mut refs = self.node_refs.lock();
+        let mut node_refs = take(&mut refs.by_handle);
+        drop(refs);
+        drop(node_refs);
     }
 
     pub(crate) fn flush(this: ArcBorrow<'_, Process>) -> Result {
@@ -769,11 +906,27 @@ impl Process {
     }
 
     pub(crate) fn mmap(
-        _this: ArcBorrow<'_, Process>,
+        this: ArcBorrow<'_, Process>,
         _file: &File,
-        _vma: &mut mm::virt::Area,
+        vma: &mut mm::virt::Area,
     ) -> Result {
-        Err(EINVAL)
+        // We don't allow mmap to be used in a different process.
+        if !core::ptr::eq(kernel::current!().group_leader(), &*this.task) {
+            return Err(EINVAL);
+        }
+        if vma.start() == 0 {
+            return Err(EINVAL);
+        }
+        let mut flags = vma.flags();
+        use mm::virt::flags::*;
+        if flags & WRITE != 0 {
+            return Err(EPERM);
+        }
+        flags |= DONTCOPY | MIXEDMAP;
+        flags &= !MAYWRITE;
+        vma.set_flags(flags);
+        // TODO: Set ops. We need to learn when the user unmaps so that we can stop using it.
+        this.create_mapping(vma)
     }
 
     pub(crate) fn poll(
