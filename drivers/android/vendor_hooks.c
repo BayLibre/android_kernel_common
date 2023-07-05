cf082abb643f3b3a81c9ad5482328562e801d9b9
shenjiangjiang <shenjiangjiang@oppo.com>
2023-07-05 17:50:26 +0800

    ANDROID: vendor_hook: Add hook to abort reclaim and compaction
    We need to abort the reclaim/compaction by sending
    signal(such as SIGUSR2) to the reclaim thread, or
    just abort when cpu-usage is too-high or free-mem is enough.
    
    Bug: 289987875
    Change-Id: I4b637cbd2b37235eec27a985a9b5b95598247c59
    Signed-off-by: shenjiangjiang <shenjiangjiang@oppo.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -426,6 +426,7 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_alloc_highpage_movable_gfp_adjust);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_anon_gfp_adjust);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_dma_buf_stats_teardown);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_madvise_cold_or_pageout);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_madvise_cold_or_pageout_abort);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_cma_alloc_retry);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_encrypt_page);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_init_aes_encrypt);
@@ -440,6 +441,7 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_psi_group);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rmqueue_smallest_bypass);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_free_one_page_bypass);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_use_cma_first_check);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_compact_finished);
 /*
  * For type visibility
  */
