fd0fbd60a88eb86e6b8a9eb5451ce7ee72109736
Vatsal Parasrampuria <vp9924@zebra.com>
2023-07-19 20:04:28 +0000

    ANDROID: wakeupbypass: Add vendor hook for batteryswap
    Implemented a hook to check if battery swap is enabled in
    alarm timer suspend routine. During a battery swap, it is
    crucial to ensure that the device remains in a suspended
    state, relying on a limited backup power source. It is
    essential to prevent any unintended awakenings in this
    state, as they could potentially lead to sudden surges
    in the power consumption, ultimately resulting in a
    device shutdown. Hence, we disable alarmtimer IRQs when
    in batteryswap mode.
    
    Bug: 290881352
    Change-Id: I31dc30d9a3168bb1356cccba49f0a70fd6b73782
    Signed-off-by: Vatsal Parasrampuria <vp9924@zebra.com>
    (cherry picked from commit b4b7d228301ff12eeef059f28d20b19ebd7c29b4)

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -76,6 +76,7 @@
 #include <trace/hooks/ipv4.h>
 #include <trace/hooks/pci.h>
 #include <trace/hooks/dmabuf.h>
+#include <trace/hooks/wakeupbypass.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -479,6 +480,12 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_look_around);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_look_around_migrate_page);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_test_clear_look_around_ref);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_dma_buf_stats_teardown);
+<<<<<<< HEAD   (81e741 FROMGIT: pstore: Revert pmsg_lock back to a normal mutex)
+=======
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_madvise_cold_or_pageout_abort);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_compact_finished);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_wakeup_bypass);
+>>>>>>> CHANGE (b4b7d2 ANDROID: wakeupbypass: Add vendor hook for batteryswap)
 /*
  * For type visibility
  */
