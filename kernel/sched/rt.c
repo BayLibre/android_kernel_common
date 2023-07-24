b7f1811f28f72bbe03df37a73ae6d77b94bbaf6c
John Stultz <jstultz@google.com>
2023-07-24 23:05:20 +0000

    sched: Handle blocked-waiter migration (and return migration)
    Add logic to handle migrating a blocked waiter to a remote
    cpu where the lock owner is runnable.
    
    Additionally, as the blocked task may not be able to run
    on the remote cpu, add logic to handle return migration once
    the waiting task is given the mutex.
    
    Because tasks may get migrated to where they cannot run, also
    modify the scheduling classes to avoid sched class migrations on
    mutex blocked tasks, leaving find_proxy_task() and related logic
    to do the migrations and return migrations.
    
    This was split out from the larger proxy patch, and
    significantly reworked.
    
    Credits for the original patch go to:
      Peter Zijlstra (Intel) <peterz@infradead.org>
      Juri Lelli <juri.lelli@redhat.com>
      Valentin Schneider <valentin.schneider@arm.com>
      Connor O'Brien <connoro@google.com>
    
    Cc: Joel Fernandes <joelaf@google.com>
    Cc: Qais Yousef <qyousef@google.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Juri Lelli <juri.lelli@redhat.com>
    Cc: Vincent Guittot <vincent.guittot@linaro.org>
    Cc: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Cc: Valentin Schneider <vschneid@redhat.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Ben Segall <bsegall@google.com>
    Cc: Zimuzo Ezeozue <zezeozue@google.com>
    Cc: Youssef Esmat <youssefesmat@google.com>
    Cc: Mel Gorman <mgorman@suse.de>
    Cc: Daniel Bristot de Oliveira <bristot@redhat.com>
    Cc: Will Deacon <will@kernel.org>
    Cc: Waiman Long <longman@redhat.com>
    Cc: Boqun Feng <boqun.feng@gmail.com>
    Cc: "Paul E. McKenney" <paulmck@kernel.org>
    Cc: Metin Kaya <Metin.Kaya@arm.com>
    Cc: Xuewen Yan <xuewen.yan94@gmail.com>
    Cc: K Prateek Nayak <kprateek.nayak@amd.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: kernel-team@android.com
    Signed-off-by: John Stultz <jstultz@google.com>
    Change-Id: I57bbfed09db2a10d3f8ec0beb23b9f080e7bc991
    ---
    v6:
    * Integrated sched_proxy_exec() check in proxy_return_migration()
    * Minor cleanups to diff
    * Unpin the rq before calling __balance_callbacks()
    * Tweak proxy migrate to migrate deeper task in chain, to avoid
      tasks pingponging between rqs
    v7:
    * Fixup for unused function arguments
    * Switch from that_rq -> target_rq, other minor tweaks, and typo
      fixes suggested by Metin Kaya
    * Switch back to doing return migration in the ttwu path, which
      avoids nasty lock juggling and performance issues
    * Fixes for UP builds
    v8:
    * More simplifications from Metin Kaya
    * Fixes for null owner case, including doing return migration

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -1558,6 +1558,9 @@ enqueue_task_rt(struct rq *rq, struct task_struct *p, int flags)
 	if (p->nr_cpus_allowed == 1)
 		return;
 
+	if (task_is_blocked(p))
+		return;
+
 	enqueue_pushable_task(rq, p);
 }
 
@@ -1855,6 +1858,8 @@ static void put_prev_task_rt(struct rq *rq, struct task_struct *p)
 
 	update_rt_rq_load_avg(rq_clock_pelt(rq), rq, 1);
 
+	if (task_is_blocked(p))
+		return;
 	/*
 	 * The previous task needs to be made eligible for pushing
 	 * if it is still active
