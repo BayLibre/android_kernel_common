918429659a11fc22c849c12a9900cdf2f11313aa
Robin Peng <robinpeng@google.com>
2023-07-26 14:17:12 +0000

    Revert "UPSTREAM: mm/page_alloc: remove mistaken page == NULL check in rmqueue"
    This reverts commit 0552a3b18afd2c002b35729eb4e3d98aea65f7c6.
    
    Change-Id: I6e279f19cee6beb6b6da87e469dca8345247cea9
    Signed-off-by: Robin Peng <robinpeng@google.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -3879,12 +3879,14 @@ struct page *rmqueue(struct zone *preferred_zone,
 	WARN_ON_ONCE((gfp_flags & __GFP_NOFAIL) && (order > 1));
 	page = rmqueue_buddy(preferred_zone, zone, order, alloc_flags,
 							migratetype);
+	if (unlikely(!page))
+		return NULL;
 	trace_android_vh_rmqueue(preferred_zone, zone, order,
 			gfp_flags, alloc_flags, migratetype);
 
 out:
 	/* Separate test+clear to avoid unnecessary atomics */
-	if (unlikely(test_bit(ZONE_BOOSTED_WATERMARK, &zone->flags))) {
+	if (test_bit(ZONE_BOOSTED_WATERMARK, &zone->flags)) {
 		clear_bit(ZONE_BOOSTED_WATERMARK, &zone->flags);
 		wakeup_kswapd(zone, 0, 0, zone_idx(zone));
 	}
