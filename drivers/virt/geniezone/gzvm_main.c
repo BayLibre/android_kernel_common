6de3e676549e3defdbe21408dfb5f2a65aa8231c
Yingshiuan Pan <yingshiuan.pan@mediatek.com>
2023-08-02 17:01:54 +0800

    FROMLIST: virt: geniezone: Add vm support
    The VM component is responsible for setting up the capability and memory
    management for the protected VMs. The capability is mainly about the
    lifecycle control and boot context initialization.
    
    Signed-off-by: Yingshiuan Pan <yingshiuan.pan@mediatek.com>
    Co-developed-by: Jerry Wang <ze-yu.wang@mediatek.com>
    Signed-off-by: Jerry Wang <ze-yu.wang@mediatek.com>
    Signed-off-by: Yi-De Wu <yi-de.wu@mediatek.com>
    Signed-off-by: Liju Chen <liju-clr.chen@mediatek.com>
    Change-Id: I7b46874fadddb559a247d79f09dcc64585a55438
    Link: https://lore.kernel.org/lkml/20240730082436.9151-6-liju-clr.chen@mediatek.com/
    Bug: 375986658

--- drivers/virt/geniezone/gzvm_main.c
+++ drivers/virt/geniezone/gzvm_main.c
@@ -4,6 +4,7 @@
  */
 
 #include <linux/device.h>
+#include <linux/file.h>
 #include <linux/kdev_t.h>
 #include <linux/miscdevice.h>
 #include <linux/module.h>
@@ -40,7 +41,21 @@ int gzvm_err_to_errno(unsigned long err)
 	return -EINVAL;
 }
 
+static long gzvm_dev_ioctl(struct file *filp, unsigned int cmd,
+			   unsigned long user_args)
+{
+	switch (cmd) {
+	case GZVM_CREATE_VM:
+		return gzvm_dev_ioctl_create_vm(user_args);
+	default:
+		break;
+	}
+
+	return -ENOTTY;
+}
+
 static const struct file_operations gzvm_chardev_ops = {
+	.unlocked_ioctl = gzvm_dev_ioctl,
 	.llseek		= noop_llseek,
 };
 
@@ -62,6 +77,7 @@ static int gzvm_drv_probe(struct platform_device *pdev)
 
 static void gzvm_drv_remove(struct platform_device *pdev)
 {
+	gzvm_destroy_all_vms();
 	misc_deregister(&gzvm_dev);
 }
 
--- include/linux/soc/mediatek/gzvm_drv.h
+++ include/linux/soc/mediatek/gzvm_drv.h
@@ -6,6 +6,12 @@
 #ifndef __GZVM_DRV_H__
 #define __GZVM_DRV_H__
 
+#include <linux/list.h>
+#include <linux/mutex.h>
+#include <linux/gzvm.h>
+
+#define INVALID_VM_ID   0xffff
+
 /*
  * These are the definitions of APIs between GenieZone hypervisor and driver,
  * there's no need to be visible to uapi. Furthermore, we need GenieZone
@@ -17,9 +23,30 @@
 #define ERR_NOT_IMPLEMENTED     (-27)
 #define ERR_FAULT               (-40)
 
+/**
+ * struct gzvm: the following data structures are for data transferring between
+ * driver and hypervisor, and they're aligned with hypervisor definitions.
+ * @mm: userspace tied to this vm
+ * @lock: lock for list_add
+ * @vm_list: list head for vm list
+ * @vm_id: vm id
+ */
+struct gzvm {
+	struct mm_struct *mm;
+	struct mutex lock;
+	struct list_head vm_list;
+	u16 vm_id;
+};
+
+int gzvm_dev_ioctl_create_vm(unsigned long vm_type);
+
 int gzvm_err_to_errno(unsigned long err);
+
+void gzvm_destroy_all_vms(void);
 
 /* arch-dependant functions */
 int gzvm_arch_probe(void);
+int gzvm_arch_create_vm(unsigned long vm_type);
+int gzvm_arch_destroy_vm(u16 vm_id);
 
 #endif /* __GZVM_DRV_H__ */
