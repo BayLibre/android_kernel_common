2a0702d1f611b96233cd70b3ef93d2e6331bd769
Juan Yescas <jyescas@google.com>
2023-08-15 15:20:37 -0700

    16k: Set PRDT_SET_SIZE for 16k page size configuration
    Bug: 295380395
    Change-Id: I57efcf0bc1e582aaceb8dbde059008a3ef1d0a66

--- drivers/scsi/ufs/ufs-exynos.c
+++ drivers/scsi/ufs/ufs-exynos.c
@@ -1020,8 +1020,15 @@ static int exynos_ufs_post_link(struct ufs_hba *hba)
 	exynos_ufs_fit_aggr_timeout(ufs);
 
 	hci_writel(ufs, 0xa, HCI_DATA_REORDER);
+#ifdef CONFIG_ARM64_16K_PAGES
+	pr_info("----------------- gs-google PRDT_SET_SIZE 14 ------------------\n");
+	hci_writel(ufs, PRDT_SET_SIZE(14), HCI_TXPRDT_ENTRY_SIZE);
+	hci_writel(ufs, PRDT_SET_SIZE(14), HCI_RXPRDT_ENTRY_SIZE);
+#else
 	hci_writel(ufs, PRDT_SET_SIZE(12), HCI_TXPRDT_ENTRY_SIZE);
 	hci_writel(ufs, PRDT_SET_SIZE(12), HCI_RXPRDT_ENTRY_SIZE);
+#endif
+
 	hci_writel(ufs, (1 << hba->nutrs) - 1, HCI_UTRL_NEXUS_TYPE);
 	hci_writel(ufs, (1 << hba->nutmrs) - 1, HCI_UTMRL_NEXUS_TYPE);
 	hci_writel(ufs, 0xf, HCI_AXIDMA_RWDATA_BURST_LEN);
