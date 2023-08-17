85b0394290322fdd32ceb04db3b6c72e1a064e4f
Mostafa Saleh <smostafa@google.com>
2023-08-17 17:01:17 +0000

    ANDROID: iommu/arm-smmu-v3-kvm: Support EL2 compiling as a module
    Support compiling the EL2 driver as a module this includes:
    - Using module ops at EL2 instead of direct function calls.
    - Add module init function smmu_init_hyp_module.
    - Add Kbuild file for EL2 module.
    
    Built-in build is still supported.
    
    Bug: 277989609
    Bug: 278749606
    Change-Id: I722271e896e1556a2a0cbcac0b2b0b5c20748039
    Signed-off-by: Mostafa Saleh <smostafa@google.com>

---
+++ drivers/iommu/arm/arm-smmu-v3/pkvm/arm-smmu-v3-module.h
@@ -0,0 +1,40 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+#ifndef __ARM_SMMU_V3_MODULE__
+#define __ARM_SMMU_V3_MODULE__
+
+#if defined(__KVM_NVHE_HYPERVISOR__) && defined(MODULE)
+
+#include <asm/kvm_pkvm_module.h>
+
+extern const struct pkvm_module_ops		*mod_ops;
+
+#define CALL_FROM_OPS(fn, ...)			mod_ops->fn(__VA_ARGS__)
+
+#undef memset
+#undef memcpy
+#undef kvm_flush_dcache_to_poc
+
+/* Needs alternatives which is not supported at the moment. */
+#undef cmpxchg64_relaxed
+#define cmpxchg64_relaxed			__ll_sc__cmpxchg_case_64
+
+#define hyp_free(x)				CALL_FROM_OPS(hyp_free, x)
+#define hyp_alloc_errno()			CALL_FROM_OPS(hyp_alloc_errno)
+#define hyp_alloc(x)				CALL_FROM_OPS(hyp_alloc, x)
+#define kvm_iommu_donate_pages(x, y)		CALL_FROM_OPS(iommu_donate_pages, x, y)
+#define kvm_iommu_donate_pages_iopt(x, y, z)	CALL_FROM_OPS(iommu_donate_pages_iopt, x, y, z)
+#define kvm_iommu_reclaim_pages(x, y)		CALL_FROM_OPS(iommu_reclaim_pages, x, y)
+#define kvm_iommu_reclaim_pages_iopt(x, y, z)	CALL_FROM_OPS(iommu_reclaim_pages_iopt, x, y, z)
+#define kvm_iommu_request(x)			CALL_FROM_OPS(iommu_request, x)
+#define hyp_virt_to_phys(x)			CALL_FROM_OPS(hyp_pa, x)
+#define hyp_phys_to_virt(x)			CALL_FROM_OPS(hyp_va, x)
+#define pkvm_create_mappings(x, y, z)		CALL_FROM_OPS(create_mappings, x, y, z)
+#define pkvm_create_hyp_device_mapping(x, y, z) CALL_FROM_OPS(create_hyp_device_mapping, x, y, z)
+#define memcpy(x, y, z)				CALL_FROM_OPS(memcpy, x, y, z)
+#define kvm_iommu_init_device(x)		CALL_FROM_OPS(iommu_init_device, x)
+#define pkvm_udelay(x)				CALL_FROM_OPS(udelay, x)
+#define kvm_flush_dcache_to_poc(x, y)		CALL_FROM_OPS(flush_dcache_to_poc, x, y)
+#define hyp_alloc_missing_donations()		CALL_FROM_OPS(hyp_alloc_missing_donations)
+#endif
+
+#endif /* __ARM_SMMU_V3_MODULE__ */
