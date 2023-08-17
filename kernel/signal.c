3325961a7c1a3f6834672df0085f751a1ff3e6c9
xieliujie <xieliujie@oppo.com>
2023-08-17 17:06:46 +0800

    ANDROID: vendor_hooks: Add hooks for waking up and exiting control
    Add hooks at process waking up and exiting routines so that oems
    can control these procedures. One possible benifit is the peak of
    system load can be shaved and load can be more smooth when a large
    number of threads is killed once upon a time, while a sudden peak
    of system load can probably lead to user junk issues.
    
    Bug: 296493318
    Change-Id: Ide5f9e63a4f50d6a9e3ffbc9516de9ce48ededef
    Signed-off-by: xieliujie <xieliujie@oppo.com>

--- kernel/signal.c
+++ kernel/signal.c
@@ -58,6 +58,8 @@
 #include <asm/cacheflush.h>
 #include <asm/syscall.h>	/* for syscall_get_* */
 
+#undef CREATE_TRACE_POINTS
+#include <trace/hooks/dtask.h>
 /*
  * SLAB caches for signal bits.
  */
@@ -1005,6 +1007,7 @@ static void complete_signal(int sig, struct task_struct *p, enum pid_type type)
 {
 	struct signal_struct *signal = p->signal;
 	struct task_struct *t;
+	bool wake;
 
 	/*
 	 * Now find a thread we can wake up to take the signal off the queue.
@@ -1062,7 +1065,10 @@ static void complete_signal(int sig, struct task_struct *p, enum pid_type type)
 			do {
 				task_clear_jobctl_pending(t, JOBCTL_PENDING_MASK);
 				sigaddset(&t->pending.signal, SIGKILL);
-				signal_wake_up(t, 1);
+				wake = true;
+				trace_android_vh_exit_signal_whether_wake(t, &wake);
+				if (wake)
+					signal_wake_up(t, 1);
 			} while_each_thread(p, t);
 			return;
 		}
