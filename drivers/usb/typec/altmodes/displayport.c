fd22848cb6bc4ac0c98d7f6afdf2f8b774120cc5
RD Babiera <rdbabiera@google.com>
2023-09-05 16:47:55 +0000

    [DO NOT SUBMIT] usb: typec: altmodes/displayport: verify compatible source/sink role combination in probe
    DisplayPort Alt Mode CTS test 10.3.8 states that both sides of the
    connection between the port and port partner shall be compatible with one
    another such that they resolve into a Source and Sink.
    
    Although the DisplayPort driver checks for compatible pin configurations
    that would resolve a Source and Sink combination, the CTS test is designed
    to send a Status message to the device that has compatible pin
    configurations but advertises the same Port Connected value as the device,
    failing the test.
    
    Explicitly verify that the port and port partner can resolve into a
    compatible Source/Sink combination.
    
    Bug:
    Change-Id: I1fce674ad147326ac9768b25592affa3a6432a3a
    Signed-off-by: RD Babiera <rdbabiera@google.com>

--- drivers/usb/typec/altmodes/displayport.c
+++ drivers/usb/typec/altmodes/displayport.c
@@ -559,10 +559,19 @@ int dp_altmode_probe(struct typec_altmode *alt)
 	const struct typec_altmode *port = typec_altmode_get_partner(alt);
 	struct dp_altmode *dp;
 	int ret;
+	int port_cap, partner_cap;
 
 	/* FIXME: Port can only be DFP_U. */
 
-	/* Make sure we have compatiple pin configurations */
+	/* Make sure that we port and partner can resolve into source and sink */
+	port_cap = DP_CAP_CAPABILITY(port->vdo);
+	partner_cap = DP_CAP_CAPABILITY(alt->vdo);
+	if (!((port_cap & DP_CAP_DFP_D) && (partner_cap & DP_CAP_UFP_D)) &&
+	    !((port_cap & DP_CAP_UFP_D) && (partner_cap & DP_CAP_DFP_D))) {
+		return -ENODEV;
+	}
+
+	/* Make sure we have compatible pin configurations */
 	if (!(DP_CAP_PIN_ASSIGN_DFP_D(port->vdo) &
 	      DP_CAP_PIN_ASSIGN_UFP_D(alt->vdo)) &&
 	    !(DP_CAP_PIN_ASSIGN_UFP_D(port->vdo) &
