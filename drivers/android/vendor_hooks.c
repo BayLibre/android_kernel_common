8df9cdf1fb9cdb52f0f5b2efa3ac3cdbb68684b9
Tangquan Zheng <zhengtangquan@oppo.com>
2023-09-06 10:39:36 +0800

    ANDROID: signal: Add vendor hook for memory reap
    Add a vendor hook to determine if the memory of a process that received
    a SIGKILL can be reaped. This is a partial cherry-pick of aosp/1724512
    and aosp/2093626.
    
    Bug: 232062955
    Change-Id: I75072bd264df33caff67d083821ee6f33ca83af9
    Signed-off-by: Tangquan Zheng <zhengtangquan@oppo.com>
    [cmllamas: minor cleanups and refactoring]
    Signed-off-by: Carlos Llamas <cmllamas@google.com>
    (cherry picked from commit e874bfd349bb9736126900e50cb770f54ca066a9)

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -54,6 +54,7 @@
  * associated with them) to allow external modules to probe them.
  */
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_do_send_sig_info);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_killed_process);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_arch_set_freq_scale);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mutex_wait_start);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mutex_wait_finish);
