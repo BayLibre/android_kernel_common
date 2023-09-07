d9f1ea80638d55ca2901988c900cd88a62bf63a8
Vincent Donnefort <vdonnefort@google.com>
2023-09-07 16:44:46 +0100

    ANDROID: KVM: arm64: per-CPU shared page for pKVM hyp requests
    Currently, when the hypervisor needs something from the host, the only
    choice is to return an error such as -ENOMEM (toping-up the
    vcpu memcache) or to fake a mem_abort for a fault in the case of the
    guest memsharing HVC.
    
    This doesn't really scale and prevent from using more detailed
    arguments. So let's create a share page which holds a list of
    "requests". When the hypervisor returns to the host, the latter just
    need to check for the exception ARM_EXCEPTION_HYP_REQ and read the
    shared page.
    
    The shared page being still writable by the host, we can't trust it. The
    hypervisor is then solely writing it.
    
    Bug: 278749606
    Bug: 243642516
    Change-Id: I4a7799982e4f1a75d81b26eb3b0eec4c7feb41b0
    Signed-off-by: Vincent Donnefort <vdonnefort@google.com>

--- arch/arm64/kvm/hyp/nvhe/hyp-main.c
+++ arch/arm64/kvm/hyp/nvhe/hyp-main.c
@@ -116,11 +116,10 @@ void __hyp_exit(void)
 
 static int pkvm_refill_memcache(struct pkvm_hyp_vcpu *hyp_vcpu)
 {
-	struct pkvm_hyp_vm *hyp_vm = pkvm_hyp_vcpu_to_hyp_vm(hyp_vcpu);
-	u64 nr_pages = VTCR_EL2_LVLS(hyp_vm->kvm.arch.vtcr) - 1;
 	struct kvm_vcpu *host_vcpu = hyp_vcpu->host_vcpu;
 
-	return refill_memcache(&hyp_vcpu->vcpu.arch.pkvm_memcache, nr_pages,
+	return refill_memcache(&hyp_vcpu->vcpu.arch.pkvm_memcache,
+			       host_vcpu->arch.pkvm_memcache.nr_pages,
 			       &host_vcpu->arch.pkvm_memcache);
 }
 
@@ -632,6 +631,16 @@ static void sync_debug_state(struct pkvm_hyp_vcpu *hyp_vcpu)
 	vcpu->arch.debug_ptr = &host_vcpu->arch.vcpu_debug_state;
 }
 
+static void __flush_hyp_reqs(struct pkvm_hyp_vcpu *hyp_vcpu)
+{
+	struct kvm_hyp_req *hyp_req = hyp_vcpu->vcpu.arch.hyp_reqs;
+
+	hyp_req->type = KVM_HYP_REQ_END;
+
+	/* We potentially requested to the host a memcache refill */
+	pkvm_refill_memcache(hyp_vcpu);
+}
+
 static void flush_hyp_vcpu(struct pkvm_hyp_vcpu *hyp_vcpu)
 {
 	struct kvm_vcpu *host_vcpu = hyp_vcpu->host_vcpu;
@@ -678,6 +687,9 @@ static void flush_hyp_vcpu(struct pkvm_hyp_vcpu *hyp_vcpu)
 		if (ec_handler)
 			ec_handler(hyp_vcpu);
 		break;
+	case ARM_EXCEPTION_HYP_REQ:
+		__flush_hyp_reqs(hyp_vcpu);
+		break;
 	default:
 		BUG();
 	}
@@ -703,6 +715,9 @@ static void sync_hyp_vcpu(struct pkvm_hyp_vcpu *hyp_vcpu, u32 exit_reason)
 
 	switch (ARM_EXCEPTION_CODE(exit_reason)) {
 	case ARM_EXCEPTION_IRQ:
+	case ARM_EXCEPTION_EL1_SERROR:
+	case ARM_EXCEPTION_IL:
+	case ARM_EXCEPTION_HYP_REQ:
 		break;
 	case ARM_EXCEPTION_TRAP:
 		esr_ec = ESR_ELx_EC(kvm_vcpu_get_esr(&hyp_vcpu->vcpu));
@@ -714,9 +729,6 @@ static void sync_hyp_vcpu(struct pkvm_hyp_vcpu *hyp_vcpu, u32 exit_reason)
 
 		if (ec_handler)
 			ec_handler(hyp_vcpu);
-		break;
-	case ARM_EXCEPTION_EL1_SERROR:
-	case ARM_EXCEPTION_IL:
 		break;
 	default:
 		BUG();
