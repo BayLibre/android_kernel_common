2f0406136ae058e6dcc3d0591bedef7763bb793a
Yingshiuan Pan <yingshiuan.pan@mediatek.com>
2023-09-12 15:00:40 -0600

    FROMLIST: virt: geniezone: Add block-based demand paging support
    To balance memory usage and performance, GenieZone supports larger
    granularity demand paging, called block-based demand paging.
    Gzvm driver uses enable_cap to query the hypervisor if it supports
    block-based demand paging and the given granularity or not. Meanwhile,
    the gzvm driver allocates a shared buffer for storing the physical
    pages later.
    
    If the hypervisor supports, every time the gzvm driver handles guest
    page faults, it allocates more memory in advance (default: 2MB) for
    demand paging. And fills those physical pages into the allocated shared
    memory, then calls the hypervisor to map to guest's memory.
    
    The physical pages allocated for block-based demand paging is not
    necessary to be contiguous because in many cases, 2MB block is not
    followed. 1st, the memory is allocated because of VMM's page fault
    (VMM loads kernel image to guest memory before running). In this case,
    the page is allocated by the host kernel and using PAGE_SIZE. 2nd is
    that guest may return memory to host via ballooning and that is still
    4KB (or PAGE_SIZE) granularity. Therefore, we do not have to allocate
    physically contiguous 2MB pages.
    
    Signed-off-by: Yingshiuan Pan <yingshiuan.pan@mediatek.com>
    Signed-off-by: Yi-De Wu <yi-de.wu@mediatek.com>
    Signed-off-by: Liju Chen <liju-clr.chen@mediatek.com>
    Change-Id: I3af15b0e8ad4c8aef5bd5023d309e848caddfb9a
    Link: https://lore.kernel.org/lkml/20240730082436.9151-18-liju-clr.chen@mediatek.com/
    Bug: 375986658

--- drivers/virt/geniezone/gzvm_mmu.c
+++ drivers/virt/geniezone/gzvm_mmu.c
@@ -114,6 +114,53 @@ static int handle_single_demand_page(struct gzvm *vm, int memslot_id, u64 gfn)
 	ret = gzvm_arch_map_guest(vm->vm_id, memslot_id, pfn, gfn, 1);
 	if (unlikely(ret))
 		return -EFAULT;
+
+	return ret;
+}
+
+static int handle_block_demand_page(struct gzvm *vm, int memslot_id, u64 gfn)
+{
+	u64 pfn, __gfn;
+	int ret, i;
+
+	u32 nr_entries = GZVM_BLOCK_BASED_DEMAND_PAGE_SIZE / PAGE_SIZE;
+	struct gzvm_memslot *memslot = &vm->memslot[memslot_id];
+	u64 start_gfn = ALIGN_DOWN(gfn, nr_entries);
+	u32 total_pages = memslot->npages;
+	u64 base_gfn = memslot->base_gfn;
+
+	/*
+	 * If the start/end gfn of this demand paging block is outside the
+	 * memory region of memslot, adjust the start_gfn/nr_entries.
+	 */
+	if (start_gfn < base_gfn)
+		start_gfn = base_gfn;
+
+	if (start_gfn + nr_entries > base_gfn + total_pages)
+		nr_entries = base_gfn + total_pages - start_gfn;
+
+	mutex_lock(&vm->demand_paging_lock);
+	for (i = 0, __gfn = start_gfn; i < nr_entries; i++, __gfn++) {
+		ret = gzvm_vm_allocate_guest_page(vm, memslot, __gfn, &pfn);
+		if (unlikely(ret)) {
+			pr_notice("VM-%u failed to allocate page for GFN 0x%llx (%d)\n",
+				  vm->vm_id, __gfn, ret);
+			ret = -ERR_FAULT;
+			goto err_unlock;
+		}
+		vm->demand_page_buffer[i] = pfn;
+	}
+
+	ret = gzvm_arch_map_guest_block(vm->vm_id, memslot_id, start_gfn,
+					nr_entries);
+	if (unlikely(ret)) {
+		ret = -EFAULT;
+		goto err_unlock;
+	}
+
+err_unlock:
+	mutex_unlock(&vm->demand_paging_lock);
+
 	return ret;
 }
 
@@ -140,5 +187,8 @@ int gzvm_handle_page_fault(struct gzvm_vcpu *vcpu)
 	if (unlikely(vm->mem_alloc_mode == GZVM_FULLY_POPULATED))
 		return -EFAULT;
 
-	return handle_single_demand_page(vm, memslot_id, gfn);
+	if (vm->demand_page_gran == PAGE_SIZE)
+		return handle_single_demand_page(vm, memslot_id, gfn);
+	else
+		return handle_block_demand_page(vm, memslot_id, gfn);
 }
