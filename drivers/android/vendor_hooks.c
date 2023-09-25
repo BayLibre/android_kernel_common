248d6f31940ffef4dfb0d0da382ad5abbeca19d5
JohnHsu <john.hsu@mediatek.com>
2023-09-25 14:08:53 +0800

    ANDROID: mm: Add vendor hook for compact pages work.
    To monitor the efficiency of each action about compaction.
    Add the vendor_hook function and call it in kcompactd_do_work() and
    try_to_compact_pages()
    
    ANDROID vendor hook
    android_vh_compaction_exit(int, int, const int)
    android_vh_compaction_try_to_compact_exit(enum *compact result)
    
    Bug: 301044280
    Bug: 330661608
    
    Change-Id: I4c3f94e77eb2b16ba154ba88a9f75095536de916
    Signed-off-by: John Hsu <john.hsu@mediatek.com>
    Signed-off-by: Chinwen Chang <chinwen.chang@mediatek.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -459,3 +459,5 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_alloc_pages_reclaim_cycle_end);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_bd_link_disk_holder);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_new_ref);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_del_ref);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_compaction_exit);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_compaction_try_to_compact_exit);
