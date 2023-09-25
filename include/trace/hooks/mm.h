3625baaa466f03d6702752b309c9ccaa83171eed
JohnHsu <john.hsu@mediatek.com>
2023-09-25 16:57:06 +0800

    ANDROID: mm: Add vendor hooks for __alloc_pages_slowpath
    To monitor the efficiency of memory relciaming in __alloc_pages_slowpath, add vendor hooks in each stages of __alloc_pages_slowpath including __alloc_pages_may_oom and __alloc_pages_direct_reclaim.
    
    android_vh_mm_direct_reclaim_enter()
    android_vh_mm_direct_reclaim_exit(unsigned long, int)
    android_vh_mm_may_oom_exit(struct oom_control *, unsigned long)
    
    Bug: 301044280
    Bug: 330661608
    
    Change-Id: Ic5b5f1c2ad31b16e7339f539fcf54659e9acaba7
    Signed-off-by: John Hsu <john.hsu@mediatek.com>

--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -59,7 +59,6 @@ DECLARE_HOOK(android_vh_look_around,
 	TP_PROTO(struct page_vma_mapped_walk *pvmw, struct folio *folio,
 		struct vm_area_struct *vma, int *referenced),
 	TP_ARGS(pvmw, folio, vma, referenced));
-
 struct mem_cgroup;
 DECLARE_HOOK(android_vh_mem_cgroup_alloc,
 	TP_PROTO(struct mem_cgroup *memcg),
@@ -89,6 +88,16 @@ DECLARE_HOOK(android_vh_adjust_kvmalloc_flags,
 DECLARE_HOOK(android_vh_alloc_pages_slowpath,
 	TP_PROTO(gfp_t gfp_mask, unsigned int order, unsigned long delta),
 	TP_ARGS(gfp_mask, order, delta));
+DECLARE_HOOK(android_vh_mm_direct_reclaim_enter,
+	TP_PROTO(unsigned int order),
+	TP_ARGS(order));
+DECLARE_HOOK(android_vh_mm_direct_reclaim_exit,
+	TP_PROTO(unsigned long did_some_progress, int retry_times),
+	TP_ARGS(did_some_progress, retry_times));
+struct oom_control;
+DECLARE_HOOK(android_vh_mm_may_oom_exit,
+	TP_PROTO(struct oom_control *oc, unsigned long did_some_progress),
+	TP_ARGS(oc, did_some_progress));
 #endif /* _TRACE_HOOK_MM_H */
 
 /* This part must be outside protection */
