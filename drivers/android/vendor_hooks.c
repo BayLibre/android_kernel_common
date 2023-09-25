3d0525ef4c36fd8c47aace689213208389ecb2ed
JohnHsu <john.hsu@mediatek.com>
2023-09-25 17:28:48 +0800

    ANDROID: mm: Add vendor hooks for recording when kswapd finishing the reclaim job
    To monitor the reclaiming ability of kswapd, add vendor hook recording when the kswapd finish the reclaiming job and the reclaim progress.
    
    android_vh_vmscan_kswpad_done(int, unsigned int, unsigned int, unsigned int)
    
    Bug: 301044280
    Bug: 330661608
    Bug: 395197442
    Bug: 474393324
    
    Change-Id: Id6e0a97003f0a156cff4d0996bc38bcd89b1dc69
    Signed-off-by: John Hsu <john.hsu@mediatek.com>
    Signed-off-by: wengle <wengle@oppo.com>
    (cherry picked from commit 21c64f5a218b059aa53c4d6c35a50889e95bd4dd)

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -307,3 +307,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_regmap_update);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_preset);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_reply);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_trans);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_vmscan_kswapd_done);
