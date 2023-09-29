70d442a3a5721f3972d80a93e869d220a7ad3ffe
Keita Aihara <keita.aihara@sony.com>
2023-09-29 10:57:49 +0900

    ANDROID: mmc: Add vendor hooks for sdcard failure diagnostics
    For sdcard failure diagnostics, add vendor hooks to monitor the sdcard
    bus speed mode change and the sdcard reset failure in the request
    recovery handling.
    
    Bug: 303549480
    Change-Id: Id40337ffb09a31199335cdb8a273d76618415649
    Signed-off-by: Keita Aihara <keita.aihara@sony.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -345,3 +345,5 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mm_compaction_begin);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mm_compaction_end);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_bus_iommu_probe);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rmqueue);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mmc_blk_mq_rw_recovery);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_sd_update_bus_speed_mode);
