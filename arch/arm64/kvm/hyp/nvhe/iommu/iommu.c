fd774824d8a20da738b955504c3282d33fddeb56
Mostafa Saleh <smostafa@google.com>
2023-10-04 11:32:55 +0000

    ANDROID: KVM: arm64: iommu: Implement reclaim for IOMMU pool
    Implement reclaim operations for IOMMU allocator.
    
    Bug: 277989609
    Bug: 278749606
    Change-Id: Id8bdd1319c908f12658bd4015133d20ac8ed8ec2
    Signed-off-by: Mostafa Saleh <smostafa@google.com>

--- arch/arm64/kvm/hyp/nvhe/iommu/iommu.c
+++ arch/arm64/kvm/hyp/nvhe/iommu/iommu.c
@@ -63,13 +63,23 @@ int kvm_iommu_refill(struct kvm_hyp_memcache *host_mc)
 
 void kvm_iommu_reclaim(struct kvm_hyp_memcache *host_mc, int target)
 {
-	/* TODO */
+	void *p;
+	struct hyp_page *page;
+
+	while (target--) {
+		p = hyp_alloc_pages(&iommu_host_pool, 0);
+		if (!p)
+			return;
+		page = hyp_virt_to_page(p);
+		push_hyp_memcache(host_mc, p, hyp_virt_to_phys, page->order);
+		WARN_ON(__pkvm_hyp_donate_host(hyp_virt_to_pfn(p), 1 << page->order));
+		memset(page, 0, sizeof(struct hyp_page));
+	}
 }
 
 int kvm_iommu_reclaimable(void)
 {
-	/* TODO */
-	return 0;
+	return hyp_pool_free_pages(&iommu_host_pool);
 }
 
 struct hyp_mgt_allocator_ops kvm_iommu_allocator_ops = {
