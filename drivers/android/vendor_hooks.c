c6a3204a239edfc80b9ed93bfd11fc1a62743336
qinglin.li <qinglin.li@amlogic.com>
2023-10-08 15:18:14 +0800

    ANDROID: vendor_hooks: add vendor hook in free_unref_page_prepare() and __free_pages_ok()
    By this vh, so that we can get page info when free
    
    Bug: 304066882
    Change-Id: Ide5ab37a59f54cc473a5d222bd30e89b86ee0bf4
    Signed-off-by: Qinglin Li <qinglin.li@amlogic.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -359,3 +359,5 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_alloc_uid);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_free_user);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_cma_alloc_bypass);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_xhci_urb_suitable_bypass);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_free_unref_page_prepare);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_free_pages_ok);
