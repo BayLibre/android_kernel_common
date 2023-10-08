2da49e2f84a7c13999ae9853bdbe746e32229898
qinglin.li <qinglin.li@amlogic.com>
2023-10-08 15:18:14 +0800

    ANDROID: vendor_hooks: add vendor hook in free_unref_page_prepare() and __free_pages_ok()
    By this vh, so that we can get page info when free
    
    Bug: 304066882
    Change-Id: Ide5ab37a59f54cc473a5d222bd30e89b86ee0bf4
    Signed-off-by: Qinglin Li <qinglin.li@amlogic.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -460,6 +460,8 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_clear_page_migrating);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_cma_alloc_bypass);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_enable_thermal_genl_check);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_xhci_urb_suitable_bypass);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_free_unref_page_prepare);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_free_pages_ok);
 /*
  * For type visibility
  */
--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -79,6 +79,7 @@
 #include <asm/sections.h>
 #include <asm/tlbflush.h>
 #include <asm/div64.h>
+#include <trace/hooks/mm.h>
 #include "internal.h"
 #include "shuffle.h"
 #include "page_reporting.h"
@@ -1783,6 +1784,7 @@ static void __free_pages_ok(struct page *page, unsigned int order,
 	spin_unlock_irqrestore(&zone->lock, flags);
 
 	__count_vm_events(PGFREE, 1 << order);
+	trace_android_vh_free_pages_ok(page, order);
 }
 
 void __free_pages_core(struct page *page, unsigned int order)
@@ -3533,6 +3535,7 @@ static bool free_unref_page_prepare(struct page *page, unsigned long pfn,
 
 	migratetype = get_pfnblock_migratetype(page, pfn);
 	set_pcppage_migratetype(page, migratetype);
+	trace_android_vh_free_unref_page_prepare(page, order);
 	return true;
 }
 
