49664dc57cc87ec91570776d0d90c113b4acfaa5
Fuad Tabba <tabba@google.com>
2023-10-11 13:55:44 +0100

    ANDROID: KVM: arm64: Store hyp address in the host fp state array
    Store the hyp address in kvm_arm_hyp_host_fp_state[], to avoid having to
    calculate it with kern_hyp_va() on every access.
    
    Bug: 303684934
    Signed-off-by: Fuad Tabba <tabba@google.com>
    Change-Id: I52902c297f9b957a8d035be942e3cbeb32fed0a2

--- arch/arm64/kvm/hyp/nvhe/pkvm.c
+++ arch/arm64/kvm/hyp/nvhe/pkvm.c
@@ -45,7 +45,7 @@ unsigned long __ro_after_init kvm_arm_hyp_host_fp_state[NR_CPUS];
 
 static void *__get_host_fpsimd_bytes(void)
 {
-	return kern_hyp_va((void *) kvm_arm_hyp_host_fp_state[hyp_smp_processor_id()]);
+	return (void *)kvm_arm_hyp_host_fp_state[hyp_smp_processor_id()];
 }
 
 struct user_fpsimd_state *get_host_fpsimd_state(struct kvm_vcpu *vcpu)
