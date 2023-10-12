maltesermailo/android_kernel_pantah

ANDROID: export symbols to use DAMON as a external driver
Bug: 300502883

Change-Id: I7f7ba0d859103476089635d602b7f0486f52a9d9
Signed-off-by: cuiyangpei <cuiyangpei@xiaomi.com>

kernel/resource.c
@@ -418,6 +418,7 @@ int walk_system_ram_res(u64 start, u64 end, void *arg,
 	return __walk_iomem_res_desc(start, end, flags, IORES_DESC_NONE, arg,
 				     func);
 }
+EXPORT_SYMBOL(walk_system_ram_res);
 
 /*
  * This function calls the @func callback against all memory ranges, which

mm/madvise.c
@@ -1394,6 +1394,7 @@ int do_madvise(struct mm_struct *mm, unsigned long start, size_t len_in, int beh
 
 	return error;
 }
+EXPORT_SYMBOL(do_madvise);
 
 SYSCALL_DEFINE3(madvise, unsigned long, start, size_t, len_in, int, behavior)
 {

mm/memcontrol.c
@@ -1131,6 +1131,7 @@ out_unlock:
 
 	return memcg;
 }
+EXPORT_SYMBOL(mem_cgroup_iter);
 
 /**
  * mem_cgroup_iter_break - abort a hierarchy walk prematurely

mm/page_vma_mapped.c
@@ -294,6 +294,7 @@ next_pte:
 
 	return false;
 }
+EXPORT_SYMBOL(page_vma_mapped_walk);
 
 /**
  * page_mapped_in_vma - check whether a page is really mapped in a VMA

mm/pagewalk.c
@@ -481,6 +481,7 @@ int walk_page_range(struct mm_struct *mm, unsigned long start,
 	} while (start = next, start < end);
 	return err;
 }
+EXPORT_SYMBOL(walk_page_range);
 
 /*
  * Similar to walk_page_range() but can walk any page tables even if they are

mm/rmap.c
@@ -609,6 +609,7 @@ out:
 	rcu_read_unlock();
 	return anon_vma;
 }
+EXPORT_SYMBOL(page_lock_anon_vma_read);
 
 void page_unlock_anon_vma_read(struct anon_vma *anon_vma)
 {
@@ -2511,6 +2512,7 @@ void rmap_walk(struct page *page, struct rmap_walk_control *rwc)
 	else
 		rmap_walk_file(page, rwc, false);
 }
+EXPORT_SYMBOL(rmap_walk);
 
 /* Like rmap_walk, but caller holds relevant rmap lock */
 void rmap_walk_locked(struct page *page, struct rmap_walk_control *rwc)

mm/util.c
@@ -764,6 +764,7 @@ void *page_rmapping(struct page *page)
 	page = compound_head(page);
 	return __page_rmapping(page);
 }
+EXPORT_SYMBOL(page_rmapping);
 
 /*
  * Return true if this page is mapped into pagetables.

mm/vmscan.c
@@ -1245,6 +1245,7 @@ void putback_lru_page(struct page *page)
 	lru_cache_add(page);
 	put_page(page);		/* drop ref from isolate */
 }
+EXPORT_SYMBOL(putback_lru_page);
 
 enum page_references {
 	PAGEREF_RECLAIM,
@@ -2130,6 +2131,7 @@ int isolate_lru_page(struct page *page)
 
 	return ret;
 }
+EXPORT_SYMBOL(isolate_lru_page);
 
 /*
  * A direct reclaimer may isolate SWAP_CLUSTER_MAX pages from the LRU list and

BayLibre/android_kernel_common

kernel/resource.c
@@ -418,6 +418,7 @@ int walk_system_ram_res(u64 start, u64 end, void *arg,
 	return __walk_iomem_res_desc(start, end, flags, IORES_DESC_NONE, arg,
 				     func);
 }
+EXPORT_SYMBOL_GPL(walk_system_ram_res);
 
 /*
  * This function calls the @func callback against all memory ranges, which

mm/madvise.c
@@ -1394,6 +1394,7 @@ int do_madvise(struct mm_struct *mm, unsigned long start, size_t len_in, int beh
 
 	return error;
 }
+EXPORT_SYMBOL_GPL(do_madvise);
 
 SYSCALL_DEFINE3(madvise, unsigned long, start, size_t, len_in, int, behavior)
 {

mm/memcontrol.c
@@ -1131,6 +1131,7 @@ out_unlock:
 
 	return memcg;
 }
+EXPORT_SYMBOL_GPL(mem_cgroup_iter);
 
 /**
  * mem_cgroup_iter_break - abort a hierarchy walk prematurely

mm/page_vma_mapped.c
@@ -294,6 +294,7 @@ next_pte:
 
 	return false;
 }
+EXPORT_SYMBOL_GPL(page_vma_mapped_walk);
 
 /**
  * page_mapped_in_vma - check whether a page is really mapped in a VMA

mm/pagewalk.c
@@ -481,6 +481,7 @@ int walk_page_range(struct mm_struct *mm, unsigned long start,
 	} while (start = next, start < end);
 	return err;
 }
+EXPORT_SYMBOL_GPL(walk_page_range);
 
 /*
  * Similar to walk_page_range() but can walk any page tables even if they are

mm/rmap.c
@@ -609,6 +609,7 @@ out:
 	rcu_read_unlock();
 	return anon_vma;
 }
+EXPORT_SYMBOL_GPL(page_lock_anon_vma_read);
 
 void page_unlock_anon_vma_read(struct anon_vma *anon_vma)
 {
@@ -2511,6 +2512,7 @@ void rmap_walk(struct page *page, struct rmap_walk_control *rwc)
 	else
 		rmap_walk_file(page, rwc, false);
 }
+EXPORT_SYMBOL_GPL(rmap_walk);
 
 /* Like rmap_walk, but caller holds relevant rmap lock */
 void rmap_walk_locked(struct page *page, struct rmap_walk_control *rwc)

mm/util.c
@@ -764,6 +764,7 @@ void *page_rmapping(struct page *page)
 	page = compound_head(page);
 	return __page_rmapping(page);
 }
+EXPORT_SYMBOL_GPL(page_rmapping);
 
 /*
  * Return true if this page is mapped into pagetables.

mm/vmscan.c
@@ -1245,6 +1245,7 @@ void putback_lru_page(struct page *page)
 	lru_cache_add(page);
 	put_page(page);		/* drop ref from isolate */
 }
+EXPORT_SYMBOL_GPL(putback_lru_page);
 
 enum page_references {
 	PAGEREF_RECLAIM,
@@ -2130,6 +2131,7 @@ int isolate_lru_page(struct page *page)
 
 	return ret;
 }
+EXPORT_SYMBOL_GPL(isolate_lru_page);
 
 /*
  * A direct reclaimer may isolate SWAP_CLUSTER_MAX pages from the LRU list and