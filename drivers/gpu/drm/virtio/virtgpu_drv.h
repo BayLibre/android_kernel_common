f42fdd229b8c64df31175ed3f475bc52cdb48268
Gurchetan Singh <gurchetansingh@chromium.org>
2023-10-12 15:47:25 -0700

    ANDROID: drm/virtio: implement debug name via CONTEXT_INIT
    This allows setting the debug name during CONTEXT_INIT.
    
    Signed-off-by: Gurchetan Singh <gurchetansingh@chromium.org>
    
    BUG=304997376
    TEST=see process names host side
    
    Change-Id: I5ada9f856d4d5729648c9f188d9901ff9fdf6b46
    Signed-off-by: Gurchetan Singh <gurchetansingh@chromium.org>

--- drivers/gpu/drm/virtio/virtgpu_drv.h
+++ drivers/gpu/drm/virtio/virtgpu_drv.h
@@ -59,6 +59,8 @@
 #define MAX_CAPSET_ID 63
 #define MAX_RINGS 64
 
+#define DEBUG_NAME_MAX_LEN 64
+
 struct virtio_gpu_object_params {
 	unsigned long size;
 	bool dumb;
@@ -272,6 +274,8 @@ struct virtio_gpu_fpriv {
 	uint64_t base_fence_ctx;
 	uint64_t ring_idx_mask;
 	struct mutex context_lock;
+	char debug_name[DEBUG_NAME_MAX_LEN];
+	char explicit_debug_name;
 };
 
 /* virtgpu_ioctl.c */
