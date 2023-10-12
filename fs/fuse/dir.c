2c75628fee27bf79c18b5e48bfe157339246c549
liujinbao1 <liujinbao1@xiaomi.corp-partner.google.com>
2023-10-12 12:28:06 +0800

    ANDROID: fix fuse panic in fuse_dentry_revalidate | backing_inode NULL
    [27030.109330][T20342] Unable to handle kernel NULL pointer dereference at virtual address 0000000000000160
    
    [27030.761152][T20342] pc : ihold+0xc/0x50
    [27030.761162][T20342] lr : backing_data_changed+0x68/0x150
    
    [27030.761188][T20342] Call trace:
    [27030.761189][T20342]  ihold+0xc/0x50
    [27030.761191][T20342]  fuse_dentry_revalidate+0x25c/0x3f0
    [27030.761203][T20342]  lookup_fast+0x6c/0x160
    [27030.761213][T20342]  link_path_walk+0x244/0x3c8
    [27030.761215][T20342]  filename_parentat+0xe0/0x20c
    [27030.761219][T20342]  filename_create+0x50/0x17c
    [27030.761221][T20342]  do_mkdirat+0x48/0x1f0
    [27030.761223][T20342]  __arm64_sys_mkdirat+0x3c/0x54
    [27030.761225][T20342]  invoke_syscall+0x58/0x114
    [27030.761236][T20342]  el0_svc_common+0x88/0xfc
    [27030.761238][T20342]  do_el0_svc+0x2c/0xb8
    [27030.761241][T20342]  el0_svc+0x2c/0xa4
    [27030.761248][T20342]  el0t_64_sync_handler+0x68/0xb4
    [27030.761250][T20342]  el0t_64_sync+0x1a0/0x1a4
    [27030.761254][T20342] Code: df7c3633 d503201f 91058008 52800029 (b8e90108)
    [27030.761255][T20342] ---[ end trace 0000000000000000 ]---
    [27030.761257][T20342] Kernel panic - not syncing: Oops: Fatal exception
    
    Bug: 296013218
    Test:  setenforce 0
           mkdir /storage/emulated/Android/data/Fusedump -p ,no panic on Android
    Change-Id: Id8a67c43d1edfa010403d5f17e31109b796998cf
    Signed-off-by: liujinbao1 <liujinbao1@xiaomi.corp-partner.google.com>

--- fs/fuse/dir.c
+++ fs/fuse/dir.c
@@ -192,7 +192,7 @@ static bool backing_data_changed(struct fuse_inode *fi, struct dentry *entry,
 	int err;
 	bool ret = true;
 
-	if (!entry) {
+	if (!entry || !fi->backing_inode) {
 		ret = false;
 		goto put_backing_file;
 	}
@@ -200,7 +200,6 @@ static bool backing_data_changed(struct fuse_inode *fi, struct dentry *entry,
 	get_fuse_backing_path(entry, &new_backing_path);
 	new_backing_inode = fi->backing_inode;
 	ihold(new_backing_inode);
-
 	err = fuse_handle_backing(bpf_arg, &new_backing_inode, &new_backing_path);
 
 	if (err)
