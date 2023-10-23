9f3c1ac4061d485de38fe08b2c68ca98bf7d0874
Chungkai Mei <chungkai@google.com>
2023-10-23 09:01:27 +0000

    ANDROID: sched: Add vendor hook for util_fits_cpu
    vendor may have the need to implement their own util_fits_cpu
    function
    
    Bug: 261704404
    Change-Id: I0318f6cadd5c45a37f1acafdded479b25e520e6b
    Signed-off-by: Chungkai Mei <chungkai@google.com>
    (cherry picked from commit 3525a7292bfbf5d9e317660161252b322b6042b9)

--- kernel/sched/vendor_hooks.c
+++ kernel/sched/vendor_hooks.c
@@ -71,6 +71,7 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_place_entity);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_build_perf_domains);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_update_cpu_capacity);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_update_misfit_status);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_util_fits_cpu);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_rto_next_cpu);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_is_cpu_allowed);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_get_nohz_timer_target);
