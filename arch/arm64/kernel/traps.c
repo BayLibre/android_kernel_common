571990ba086fb63dd23289628ffe5483f3e21da2
Greg Kroah-Hartman <gregkh@google.com>
2023-10-26 09:55:27 +0000

    Revert "arm64: split EL0/EL1 UNDEF handlers"
    This reverts commit 7a76df1ae1b3a6d1cc806066d48c6b95dc4335fa.
    
    It causes merge errors with the current android12-5.10-lts branch
    changes, so must be dropped for now.  It can come back later if done so
    in an abi-safe way.
    
    Change-Id: Ic3b05d72df71605b1325488cad611a54c073b1e0
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- arch/arm64/kernel/traps.c
+++ arch/arm64/kernel/traps.c
@@ -395,7 +395,7 @@ void arm64_notify_segfault(unsigned long addr)
 	force_signal_inject(SIGSEGV, code, addr, 0);
 }
 
-void do_el0_undef(struct pt_regs *regs, unsigned long esr)
+void do_undefinstr(struct pt_regs *regs, unsigned long esr)
 {
 	/* check for AArch32 breakpoint instructions */
 	if (!aarch32_break_handler(regs))
@@ -404,16 +404,12 @@ void do_el0_undef(struct pt_regs *regs, unsigned long esr)
 	if (call_undef_hook(regs) == 0)
 		return;
 
+	if (!user_mode(regs))
+		die("Oops - Undefined instruction", regs, esr);
+
 	force_signal_inject(SIGILL, ILL_ILLOPC, regs->pc, 0);
 }
-
-void do_el1_undef(struct pt_regs *regs, unsigned long esr)
-{
-	if (call_undef_hook(regs) == 0)
-		return;
-
-	die("Oops - Undefined instruction", regs, esr);
-}
+NOKPROBE_SYMBOL(do_undefinstr);
 
 void do_el0_bti(struct pt_regs *regs)
 {
@@ -424,6 +420,7 @@ void do_el1_bti(struct pt_regs *regs, unsigned long esr)
 {
 	die("Oops - BTI", regs, esr);
 }
+NOKPROBE_SYMBOL(do_el1_bti);
 
 void do_el0_fpac(struct pt_regs *regs, unsigned long esr)
 {
@@ -438,6 +435,7 @@ void do_el1_fpac(struct pt_regs *regs, unsigned long esr)
 	 */
 	die("Oops - FPAC", regs, esr);
 }
+NOKPROBE_SYMBOL(do_el1_fpac);
 
 #define __user_cache_maint(insn, address, res)			\
 	if (address >= user_addr_max()) {			\
@@ -673,7 +671,7 @@ void do_el0_cp15(unsigned long esr, struct pt_regs *regs)
 		hook_base = cp15_64_hooks;
 		break;
 	default:
-		do_el0_undef(regs, esr);
+		do_undefinstr(regs, esr);
 		return;
 	}
 
@@ -688,7 +686,7 @@ void do_el0_cp15(unsigned long esr, struct pt_regs *regs)
 	 * EL0. Fall back to our usual undefined instruction handler
 	 * so that we handle these consistently.
 	 */
-	do_el0_undef(regs, esr);
+	do_undefinstr(regs, esr);
 }
 #endif
 
@@ -707,7 +705,7 @@ void do_el0_sys(unsigned long esr, struct pt_regs *regs)
 	 * back to our usual undefined instruction handler so that we handle
 	 * these consistently.
 	 */
-	do_el0_undef(regs, esr);
+	do_undefinstr(regs, esr);
 }
 
 static const char *esr_class_str[] = {
