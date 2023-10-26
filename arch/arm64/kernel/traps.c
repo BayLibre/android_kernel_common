22fe7699cef2443ba60b22cc0335356773a6034d
Greg Kroah-Hartman <gregkh@google.com>
2023-10-26 09:55:25 +0000

    Revert "arm64: factor insn read out of call_undef_hook()"
    This reverts commit 15e964971ff701c661936aa6103d51a895f32e24.
    
    It causes merge errors with the current android12-5.10-lts branch
    changes, so must be dropped for now.  It can come back later if done so
    in an abi-safe way.
    
    Change-Id: Ib8e2c0ddbd6412fec12e28e72a210b68b46b23a4
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- arch/arm64/kernel/traps.c
+++ arch/arm64/kernel/traps.c
@@ -303,22 +303,25 @@ void unregister_undef_hook(struct undef_hook *hook)
 	raw_spin_unlock_irqrestore(&undef_lock, flags);
 }
 
-static int user_insn_read(struct pt_regs *regs, u32 *insnp)
+static int call_undef_hook(struct pt_regs *regs)
 {
+	struct undef_hook *hook;
+	unsigned long flags;
 	u32 instr;
+	int (*fn)(struct pt_regs *regs, u32 instr) = NULL;
 	void __user *pc = (void __user *)instruction_pointer(regs);
 
 	if (compat_thumb_mode(regs)) {
 		/* 16-bit Thumb instruction */
 		__le16 instr_le;
 		if (get_user(instr_le, (__le16 __user *)pc))
-			return -EFAULT;
+			goto exit;
 		instr = le16_to_cpu(instr_le);
 		if (aarch32_insn_is_wide(instr)) {
 			u32 instr2;
 
 			if (get_user(instr_le, (__le16 __user *)(pc + 2)))
-				return -EFAULT;
+				goto exit;
 			instr2 = le16_to_cpu(instr_le);
 			instr = (instr << 16) | instr2;
 		}
@@ -326,19 +329,9 @@ static int user_insn_read(struct pt_regs *regs, u32 *insnp)
 		/* 32-bit ARM instruction */
 		__le32 instr_le;
 		if (get_user(instr_le, (__le32 __user *)pc))
-			return -EFAULT;
+			goto exit;
 		instr = le32_to_cpu(instr_le);
 	}
-
-	*insnp = instr;
-	return 0;
-}
-
-static int call_undef_hook(struct pt_regs *regs, u32 instr)
-{
-	struct undef_hook *hook;
-	unsigned long flags;
-	int (*fn)(struct pt_regs *regs, u32 instr) = NULL;
 
 	raw_spin_lock_irqsave(&undef_lock, flags);
 	list_for_each_entry(hook, &undef_hook, node)
@@ -347,7 +340,7 @@ static int call_undef_hook(struct pt_regs *regs, u32 instr)
 			fn = hook->fn;
 
 	raw_spin_unlock_irqrestore(&undef_lock, flags);
-
+exit:
 	return fn ? fn(regs, instr) : 1;
 }
 
@@ -399,19 +392,13 @@ void arm64_notify_segfault(unsigned long addr)
 
 void do_el0_undef(struct pt_regs *regs, unsigned long esr)
 {
-	u32 insn;
-
 	/* check for AArch32 breakpoint instructions */
 	if (!aarch32_break_handler(regs))
 		return;
 
-	if (user_insn_read(regs, &insn))
-		goto out_err;
-
-	if (call_undef_hook(regs, insn) == 0)
+	if (call_undef_hook(regs) == 0)
 		return;
 
-out_err:
 	force_signal_inject(SIGILL, ILL_ILLOPC, regs->pc, 0);
 }
 
