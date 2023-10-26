73225e661b895568e572ffa3222a9a62ebc29daa
Greg Kroah-Hartman <gregkh@google.com>
2023-10-26 09:55:29 +0000

    Revert "arm64: rework BTI exception handling"
    This reverts commit 793ed958b62ad4831e03de51b3d23ad6eb23eea7.
    
    It causes merge errors with the current android12-5.10-lts branch
    changes, so must be dropped for now.  It can come back later if done so
    in an abi-safe way.
    
    Change-Id: Ideeb8aebca06cdda3074108255c15153e2d8896c
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- arch/arm64/kernel/traps.c
+++ arch/arm64/kernel/traps.c
@@ -411,16 +411,12 @@ void do_undefinstr(struct pt_regs *regs, unsigned long esr)
 }
 NOKPROBE_SYMBOL(do_undefinstr);
 
-void do_el0_bti(struct pt_regs *regs)
+void do_bti(struct pt_regs *regs)
 {
+	BUG_ON(!user_mode(regs));
 	force_signal_inject(SIGILL, ILL_ILLOPC, regs->pc, 0);
 }
-
-void do_el1_bti(struct pt_regs *regs, unsigned long esr)
-{
-	die("Oops - BTI", regs, esr);
-}
-NOKPROBE_SYMBOL(do_el1_bti);
+NOKPROBE_SYMBOL(do_bti);
 
 void do_el0_fpac(struct pt_regs *regs, unsigned long esr)
 {
