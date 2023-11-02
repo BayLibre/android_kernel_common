2a3ac34d1c0143bdc4a22ed564412e253e35277b
Chungkai Mei <chungkai@google.com>
2023-11-02 05:31:05 +0000

    ANDROID: sched: Add vendor hook for update_load_sum
    vendor may have the need to modify update_load_sum function
    
    Bug: 181105055
    Change-Id: I35964977a4b8917c62773d48a37340f880774e38
    Signed-off-by: Chungkai Mei <chungkai@google.com>

--- kernel/sched/pelt.c
+++ kernel/sched/pelt.c
@@ -25,6 +25,7 @@
  */
 
 #include <linux/sched.h>
+#include <trace/hooks/sched.h>
 #include "sched.h"
 #include "pelt.h"
 
@@ -186,6 +187,7 @@ int ___update_load_sum(u64 now, struct sched_avg *sa,
 	u64 delta;
 
 	delta = now - sa->last_update_time;
+
 	/*
 	 * This should only happen when time goes backwards, which it
 	 * unfortunately does during sched clock init when we swap over to TSC.
@@ -204,6 +206,8 @@ int ___update_load_sum(u64 now, struct sched_avg *sa,
 		return 0;
 
 	sa->last_update_time += delta << 10;
+
+	trace_android_rvh_update_load_sum(sa, &delta, &sched_pelt_lshift);
 
 	/*
 	 * running is a subset of runnable (weight) so running can't be set if
