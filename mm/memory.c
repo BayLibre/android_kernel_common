a1ce1955ac5f2ba013e5e925396aa3fdb541b994
Alexandru Elisei <alexandru.elisei@arm.com>
2023-11-07 14:13:37 +0000

    [DO NOT SUBMIT] mm: Call arch_swap_prepare_to_restore() before arch_swap_restore()
    arm64 uses arch_swap_restore() to restore saved tags before the page is
    swapped in and it's called in atomic context (with the ptl lock held).
    
    Introduce arch_swap_prepare_to_restore() that will allow an architecture to
    perform extra work during swap in and outside of a critical section.
    
    Bug: 234781130
    Change-Id: Iac3d3362b3f4f6b12021b2fbae152bdb27f597d1
    Signed-off-by: Peter Collingbourne <pcc@google.com>

--- mm/memory.c
+++ mm/memory.c
@@ -3950,6 +3950,10 @@ vm_fault_t do_swap_page(struct vm_fault *vmf)
 
 	cgroup_throttle_swaprate(page, GFP_KERNEL);
 
+	ret = arch_swap_prepare_to_restore(entry, folio);
+	if (ret)
+		goto out_page;
+
 	/*
 	 * Back out if somebody else already faulted in this pte.
 	 */
