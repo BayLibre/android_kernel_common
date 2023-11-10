5d26ba2ce3ff2b6b226b3d4e383b17485b750511
Alexandru Elisei <alexandru.elisei@arm.com>
2023-11-10 10:30:40 +0000

    [DO NOT SUBMIT] arm64: mte: Fast track reserving tag storage when the block is free
    A double digit performance decrease for Chrome startup time has been
    reported with the dynamic tag storage management enabled. A large part of
    the regression is due to lru_cache_disable(), called from
    __alloc_contig_migrate_range(), which IPIs all CPUs in the system.
    
    Improve the performance by taking the storage block directly from the
    freelist if it's free, thus sidestepping the costly function call.
    
    Note that at the moment this is implemented only when the block size is
    1 (the block is one page); larger block sizes could be added later if
    necessary.
    
    Bug: 234781130
    Change-Id: I0bbb31aa03f1a13ff1dc020c593e25a1366c54a4
    Signed-off-by: Peter Collingbourne <pcc@google.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -9825,7 +9825,7 @@ bool is_free_buddy_page(struct page *page)
 }
 EXPORT_SYMBOL(is_free_buddy_page);
 
-#ifdef CONFIG_MEMORY_FAILURE
+#ifdef CONFIG_WANTS_TAKE_PAGE_OFF_BUDDY
 /*
  * Break down a higher-order page in sub-pages, and keep our target out of
  * buddy allocator.
@@ -9859,11 +9859,10 @@ static void break_down_buddy_pages(struct zone *zone, struct page *page,
 		}
 	}
 }
-
 /*
- * Take a page that will be marked as poisoned off the buddy allocator.
+ * Take a page off the buddy allocator, and optionally mark it as poisoned.
  */
-bool take_page_off_buddy(struct page *page)
+bool take_page_off_buddy(struct page *page, bool poison)
 {
 	struct zone *zone = page_zone(page);
 	unsigned long pfn = page_to_pfn(page);
@@ -9884,7 +9883,8 @@ bool take_page_off_buddy(struct page *page)
 			del_page_from_free_list(page_head, zone, page_order);
 			break_down_buddy_pages(zone, page_head, page, 0,
 						page_order, migratetype);
-			SetPageHWPoisonTakenOff(page);
+			if (poison)
+				SetPageHWPoisonTakenOff(page);
 			if (!is_migrate_isolate(migratetype))
 				__mod_zone_freepage_state(zone, -1, migratetype);
 			ret = true;
@@ -9898,9 +9898,10 @@ bool take_page_off_buddy(struct page *page)
 }
 
 /*
- * Cancel takeoff done by take_page_off_buddy().
+ * Cancel takeoff done by take_page_off_buddy(), and optionally unpoison the
+ * page.
  */
-bool put_page_back_buddy(struct page *page)
+bool put_page_back_buddy(struct page *page, bool unpoison)
 {
 	struct zone *zone = page_zone(page);
 	unsigned long pfn = page_to_pfn(page);
@@ -9910,9 +9911,11 @@ bool put_page_back_buddy(struct page *page)
 
 	spin_lock_irqsave(&zone->lock, flags);
 	if (put_page_testzero(page)) {
-		ClearPageHWPoisonTakenOff(page);
+		VM_WARN_ON_ONCE(PageHWPoisonTakenOff(page) && !unpoison);
+		if (unpoison)
+			ClearPageHWPoisonTakenOff(page);
 		__free_one_page(page, pfn, zone, 0, migratetype, FPI_NONE);
-		if (TestClearPageHWPoison(page)) {
+		if (!unpoison || (unpoison && TestClearPageHWPoison(page))) {
 			ret = true;
 		}
 	}
