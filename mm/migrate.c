54f53e7ca8d40a8cf5fe459e5c7ace0b0dbe7d47
Alexandru Elisei <alexandru.elisei@arm.com>
2023-11-10 12:08:19 +0000

    [DO NOT SUBMIT] mm: migrate/mempolicy: Add hook to modify migration target gfp
    It might be desirable for an architecture to modify the gfp flags used to
    allocate the destination page for migration based on the page that it is
    being replaced. For example, if an architectures has metadata associated
    with a page (like arm64, when the memory tagging extension is implemented),
    it can request that the destination page similarly has storage for tags
    already allocated.
    
    No functional change.
    
    Bug: 234781130
    Change-Id: I738e981e00fc40cb31574f5193fc57cdb55edf89
    Signed-off-by: Peter Collingbourne <pcc@google.com>

--- mm/migrate.c
+++ mm/migrate.c
@@ -1633,6 +1633,7 @@ struct page *alloc_migration_target(struct page *page, unsigned long private)
 		struct hstate *h = page_hstate(&folio->page);
 
 		gfp_mask = htlb_modify_alloc_mask(h, gfp_mask);
+		gfp_mask |= arch_migration_target_gfp(src, gfp);
 		return alloc_huge_page_nodemask(h, nid, mtc->nmask, gfp_mask);
 	}
 
@@ -1648,6 +1649,7 @@ struct page *alloc_migration_target(struct page *page, unsigned long private)
 	zidx = zone_idx(folio_zone(folio));
 	if (is_highmem_idx(zidx) || zidx == ZONE_MOVABLE)
 		gfp_mask |= __GFP_HIGHMEM;
+	gfp_mask |= arch_migration_target_gfp(src, gfp);
 
 	new_folio = __folio_alloc(gfp_mask, order, nid, mtc->nmask);
 
@@ -2120,6 +2122,7 @@ static struct page *alloc_misplaced_dst_page(struct page *page,
 			__GFP_NOWARN;
 		gfp &= ~__GFP_RECLAIM;
 	}
+	gfp |= arch_migration_target_gfp(src, gfp);
 	new = __folio_alloc_node(gfp, order, nid);
 
 	return &new->page;
