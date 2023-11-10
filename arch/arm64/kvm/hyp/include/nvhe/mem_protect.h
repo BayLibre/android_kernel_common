d076265e4ef1a3c9d8e624410a3603b97f07cce6
Mostafa Saleh <smostafa@google.com>
2023-11-10 13:45:51 +0000

    ANDROID: KVM: arm64: devices: Add request_mmio guest HVC
    request_mmio is a guest HVC that is used to authenticate a device
    MMIO resource, it returns a unique token, that can be used by the
    guest.
    
    Bug: 357781595
    Bug: 348382247
    Change-Id: I87d67ce9c14578702dda1b157411fdc622327286
    Signed-off-by: Mostafa Saleh <smostafa@google.com>

--- arch/arm64/kvm/hyp/include/nvhe/mem_protect.h
+++ arch/arm64/kvm/hyp/include/nvhe/mem_protect.h
@@ -75,6 +75,8 @@ u64 __pkvm_ptdump_walk_range(pkvm_handle_t handle, struct pkvm_ptdump_log_hdr *l
 int pkvm_hyp_donate_guest(struct pkvm_hyp_vcpu *vcpu, u64 pfn, u64 gfn);
 int hyp_check_range_owned(u64 addr, u64 size);
 
+int __pkvm_guest_get_valid_phys_page(struct pkvm_hyp_vm *vm, u64 *phys, u64 ipa);
+
 bool addr_is_memory(phys_addr_t phys);
 int host_stage2_idmap_locked(phys_addr_t addr, u64 size,
 			     enum kvm_pgtable_prot prot,
