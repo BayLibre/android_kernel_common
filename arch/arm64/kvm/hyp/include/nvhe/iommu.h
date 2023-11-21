9bba83e02f24087903c643683da0f56f5237e7cf
Mostafa Saleh <smostafa@google.com>
2023-11-21 16:41:00 +0000

    ANDROID: KVM: arm64: Add request_dma guest HVC
    Add request_dma HVC which returns 128 bit token to authenticate a
    pvIOMMU and vsid pair.
    
    Bug: 357781595
    Bug: 348382247
    Bug: 236685427
    Change-Id: If032cf3e14653c0114aec3595902d09d6b3fded2
    Signed-off-by: Mostafa Saleh <smostafa@google.com>

--- arch/arm64/kvm/hyp/include/nvhe/iommu.h
+++ arch/arm64/kvm/hyp/include/nvhe/iommu.h
@@ -25,6 +25,7 @@ struct kvm_iommu_private {
 int kvm_iommu_dev_block_dma(pkvm_handle_t iommu_id, u32 endpoint_id, bool host_to_guest);
 
 int kvm_iommu_force_free_domain(pkvm_handle_t domain_id, struct pkvm_hyp_vm *vm);
+int kvm_iommu_id_to_token(pkvm_handle_t smmu_id, u64 *out_token);
 
 struct kvm_iommu_ops {
 	int (*init)(pkvm_handle_t drv_id);
@@ -51,6 +52,7 @@ struct kvm_iommu_ops {
 			      unsigned long iova, size_t size);
 	int (*dev_block_dma)(pkvm_handle_t iommu, u32 endpoint_id,
 			     bool is_host_to_guest);
+	int (*get_iommu_token_by_id)(pkvm_handle_t smmu_id, u64 *out_token);
 	/* Core private data. */
 	struct kvm_iommu_private priv;
 };
