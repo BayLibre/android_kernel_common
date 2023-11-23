c13be9669838a95a34be7ee9a371606cb34f865c
Yunlong.Xing <Yunlong.Xing@unisoc.com>
2023-11-23 09:35:55 +0800

    ANDROID: dm verity: Increase the io priority of the kworker-kverity process
    In the monkey test scenario (randomly executing specified events
    within a specified time interval, such as switching applications,
    clicking, sliding, rotating, etc.), it was found that a large
    number of kworker-kverity processes were blocked for more than
    500ms after 20 minutes of testing, accounting for approximately
    70% of the total number of processes. In the same test scenario,
    after we increase the IO priority of the kworker-kverity process,
    the overall IO situation of the system has been optimized to a
    certain extent.
    
    Bug: 312709395
    Change-Id: I149a9252d10c87c00b5641e6f5e9d05b5f0c0ddb
    Signed-off-by: Yunlong.Xing <Yunlong.Xing@unisoc.com>

--- drivers/md/dm-verity-target.c
+++ drivers/md/dm-verity-target.c
@@ -18,6 +18,7 @@
 #include "dm-verity-verify-sig.h"
 #include <linux/module.h>
 #include <linux/reboot.h>
+#include <linux/ioprio.h>
 
 #define DM_MSG_PREFIX			"verity"
 
@@ -574,6 +575,9 @@ static void verity_finish_io(struct dm_verity_io *io, blk_status_t status)
 static void verity_work(struct work_struct *w)
 {
 	struct dm_verity_io *io = container_of(w, struct dm_verity_io, work);
+
+	struct bio *bio = dm_bio_from_per_bio_data(io, io->v->ti->per_io_data_size);
+	set_task_ioprio(current, bio->bi_ioprio);
 
 	verity_finish_io(io, errno_to_blk_status(verity_verify_io(io)));
 }
