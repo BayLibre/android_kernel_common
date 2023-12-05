246e42f95b0781aaf85f039139f5d37963aa2b44
Browse Zhang <browse.zhang@mediatek.com>
2023-12-05 10:48:20 +0800

    ANDROID: scsi: ufs: vendor check response and recovery addition
    As per Vendor HW design consideration,
    added Vendor specific error handling flow
    
    Under the error checking flow - Vendor can check error and fill results acordingly,
    If cmd->results filled by vendor, skip further error checking.
    
    For error handling part - Added vendor specific VH
    trace_android_vh_ufs_err_handler to handle the error
    If Vendor handled the error, skip err processing in normal flow
    
    BUG: 312806167
    Change-Id: I614737218537f197c68780f5b528b1948d5e29ea
    Signed-off-by: Sanjeev Yadav <sanjeev.y@mediatek.com>
    Signed-off-by: Browse Zhang <browse.zhang@mediatek.com>

--- drivers/scsi/ufs/ufshcd.h
+++ drivers/scsi/ufs/ufshcd.h
@@ -81,6 +81,19 @@ enum ufs_event_type {
 	UFS_EVT_CNT,
 };
 
+/* UFSHCD error handling flags */
+enum {
+	UFSHCD_EH_IN_PROGRESS = (1 << 0),
+};
+
+#define ufshcd_set_eh_in_progress(h) \
+	((h)->eh_flags |= UFSHCD_EH_IN_PROGRESS)
+#define ufshcd_eh_in_progress(h) \
+	((h)->eh_flags & UFSHCD_EH_IN_PROGRESS)
+#define ufshcd_clear_eh_in_progress(h) \
+	((h)->eh_flags &= ~UFSHCD_EH_IN_PROGRESS)
+
+
 /**
  * struct uic_command - UIC command structure
  * @command: UIC command
@@ -1047,6 +1060,11 @@ int ufshcd_wait_for_register(struct ufs_hba *hba, u32 reg, u32 mask,
 void ufshcd_parse_dev_ref_clk_freq(struct ufs_hba *hba, struct clk *refclk);
 void ufshcd_update_evt_hist(struct ufs_hba *hba, u32 id, u32 val);
 void ufshcd_hba_stop(struct ufs_hba *hba);
+void ufshcd_complete_requests(struct ufs_hba *hba);
+void ufshcd_scsi_unblock_requests(struct ufs_hba *hba);
+void ufshcd_release_scsi_cmd(struct ufs_hba *hba,
+				    struct ufshcd_lrb *lrbp);
+
 
 static inline void check_upiu_size(void)
 {
