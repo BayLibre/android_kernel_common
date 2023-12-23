e692cbf73925765a567fb767e6329a9937795596
Haonan Wang <haonan.wang@unisoc.com>
2023-12-23 18:18:27 +0800

    ANDROID: vendor_hooks: mm: add show_mem hook for memory when debug
    When oom occurs, we need to know not only the basic memory information,
    but also the memory information of each driver.
    
    Bug: 317660256
    Change-Id: Ib0a0f1443dd69a861bf1966c83dde83c93ccaaf7
    Signed-off-by: Haonan Wang <haonan.wang@unisoc.com>

--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -21,6 +21,10 @@ DECLARE_RESTRICTED_HOOK(android_rvh_set_readahead_gfp_mask,
 			TP_ARGS(flags), 1);
 
 */
+DECLARE_HOOK(android_vh_show_mem,
+        TP_PROTO(unsigned int filter, nodemask_t *nodemask),
+        TP_ARGS(filter, nodemask));
+
 #endif /* _TRACE_HOOK_MM_H */
 
 /* This part must be outside protection */
