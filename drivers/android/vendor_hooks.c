e794e66685d661233dff616da2c5f0cacce97672
YOUNGJIN JOO <youngjin79.joo@samsung.com>
2024-01-09 14:19:54 +0900

    ANDROID: Revert "ANDROID: arm64: add vendor hooks for unusal abort cases"
    This reverts commit 04b946846bd71e2ecd1fab1ef71d5bcd352fb965.
    
    Some changes are ommited and wrong parameter types are used.
    
    Bug: 319191172
    Change-Id: I65ffe718e99d58400ce3f20b267b8692a745476a
    Signed-off-by: YOUNGJIN JOO <youngjin79.joo@samsung.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -37,7 +37,6 @@
 #include <trace/hooks/mm.h>
 #include <trace/hooks/dtask.h>
 #include <trace/hooks/ftrace_dump.h>
-#include <trace/hooks/traps.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -108,4 +107,3 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_kmalloc_large_alloced);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_tune_swappiness);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_sched_show_task);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ftrace_format_check);
-EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_arm64_serror_panic);
