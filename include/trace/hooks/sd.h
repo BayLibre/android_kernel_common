275605c4c24254b6f9dff0a78854bf58a2d493ff
hao lv <hao.lv5@transsion.com>
2024-01-10 20:55:56 +0800

    ANDROID: add hooks into sd.c for customized I/O scheduler
    android_vh_sd_probe: Use customized sd_fops to replace the original
    solution
    
    Bug: 319582497
    
    Change-Id: I010980db22e145a49bb82cd99854f302f3315b13
    Signed-off-by: hao lv <hao.lv5@transsion.com>

---
+++ include/trace/hooks/sd.h
@@ -0,0 +1,23 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#undef TRACE_SYSTEM
+#define TRACE_SYSTEM sd
+
+#define TRACE_INCLUDE_PATH trace/hooks
+
+#if !defined(_TRACE_HOOK_SD_H) || defined(TRACE_HEADER_MULTI_READ)
+#define _TRACE_HOOK_SD_H
+
+#include <linux/tracepoint.h>
+#include <trace/hooks/vendor_hooks.h>
+
+struct gendisk;
+struct scsi_device;
+
+DECLARE_HOOK(android_vh_sd_probe,
+	TP_PROTO(struct gendisk *gd, struct scsi_device *sdp),
+	TP_ARGS(gd, sdp));
+
+#endif /* _TRACE_HOOK_SD_H */
+
+/* This part must be outside protection */
+#include <trace/define_trace.h>
