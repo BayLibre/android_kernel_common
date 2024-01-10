ffd93665e3017a56b5e5b20d8db11f499b697b2a
hao lv <hao.lv5@transsion.com>
2024-01-10 20:30:15 +0800

    ANDROID: add hooks into blk-merge.c
    android_vh_blk_queue_split: Get the number of segments corresponding to
    bio
    
    Bug: 319582497
    
    Change-Id: Id080db83a7cbdbd2b3f5f2a23fa1f322dbb0e7cb
    Signed-off-by: hao lv <hao.lv5@transsion.com>

--- include/trace/hooks/block.h
+++ include/trace/hooks/block.h
@@ -206,6 +206,11 @@ DECLARE_HOOK(android_vh_blk_mq_queue_tag_busy_iter,
 		 void *priv),
 	TP_ARGS(skip, hctx, fn, priv));
 
+DECLARE_HOOK(android_vh_blk_queue_split,
+	TP_PROTO(struct request_queue *q, struct bio **bio,
+		 struct bio *split, unsigned int *nr_segs),
+	TP_ARGS(q, bio, split, nr_segs));
+
 #endif /* _TRACE_HOOK_BLOCK_H */
 
 /* This part must be outside protection */
