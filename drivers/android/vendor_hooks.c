9f0e1c08a848060aa7d7249dac36ae8681c671eb
hao lv <hao.lv5@transsion.com>
2024-01-10 20:55:56 +0800

    ANDROID: add hooks into sd.c for customized I/O scheduler
    android_vh_sd_probe: Use customized sd_fops to replace the original
    solution
    
    Bug: 319582497
    
    Change-Id: I010980db22e145a49bb82cd99854f302f3315b13
    Signed-off-by: hao lv <hao.lv5@transsion.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -45,6 +45,7 @@
 #include <trace/hooks/thermal.h>
 #include <trace/hooks/ufshcd.h>
 #include <trace/hooks/block.h>
+#include <trace/hooks/sd.h>
 #include <trace/hooks/cgroup.h>
 #include <trace/hooks/sys.h>
 #include <trace/hooks/traps.h>
@@ -555,6 +556,7 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_elv_iosched_show);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_blk_mq_sched_insert_request);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_blk_register_queue);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_blk_needs_flush_plug);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_sd_probe);
 
 /*
  * For type visibility
