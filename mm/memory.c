3e4d87f6e829920539c9d6b1968ee3f579e2fd89
Lokesh Gidra <lokeshgidra@google.com>
2024-01-16 20:20:25 +0000

    ANDROID: userfaultfd: Let SPF bailout be decided in handle_userfault()
    Currently we bail out of speculative page fault when we detect that the
    fault address is in a userfaultfd registered vma. However, if userfaultfd
    is being used with UFFD_FEATURE_SIGBUS feature, then handle_userfault()
    doesn't do much and is easiest to handle with SPF. This patch lets
    MISSING userfaultfs on private anonymous mappings be allowed with SPF if
    UFFD_FEATURE_SIGBUS is used.
    
    With this patch we get 99% success rate for userfaults caused during
    userfaultfd GC's compaction phase. This translates into eliminating
    uninterruptible sleep time in do_page_fault() due to userfaults.
    
    Bug: 320478828
    Signed-off-by: Lokesh Gidra <lokeshgidra@google.com>
    Change-Id: Ic7fde0fde03602b35179bc0cf891ddbbc434190f

--- mm/memory.c
+++ mm/memory.c
@@ -5030,19 +5030,30 @@ static vm_fault_t ___handle_speculative_fault(struct mm_struct *mm,
 		return VM_FAULT_RETRY;
 	}
 
-	if (!vmf_allows_speculation(&vmf))
-		return VM_FAULT_RETRY;
-
 	vmf.vma_flags = READ_ONCE(vmf.vma->vm_flags);
 	vmf.vma_page_prot = READ_ONCE(vmf.vma->vm_page_prot);
 
 #ifdef CONFIG_USERFAULTFD
-	/* Can't call userland page fault handler in the speculative path */
-	if (unlikely(vmf.vma_flags & __VM_UFFD_FLAGS)) {
+	 /*
+	  * Only support SPF for SIGBUS+MISSING userfaults in private anonymous
+	  * VMAs. Rest all should be retried with mmap_lock.
+	  */
+        if (unlikely((vmf.vma_flags & __VM_UFFD_FLAGS) &&
+		     (!vma_is_anonymous(vmf.vma) || !(vmf.vma_flags & VM_UFFD_MISSING)))) {
 		trace_spf_vma_notsup(_RET_IP_, vmf.vma, address);
 		return VM_FAULT_RETRY;
 	}
 #endif
+
+	if (!vmf_allows_speculation(&vmf)) {
+		/*
+		 * It doesn't matter if anon_vma isn't there for SIGBUS+MISSING
+		 * userfaults in private anonymous VMAs
+		 */
+		if (!vmf.vma->anon_vma)
+			goto out_uffd;
+		return VM_FAULT_RETRY;
+	}
 
 	if (vmf.vma_flags & VM_GROWSDOWN || vmf.vma_flags & VM_GROWSUP) {
 		/*
@@ -5161,6 +5172,10 @@ static vm_fault_t ___handle_speculative_fault(struct mm_struct *mm,
 
 	local_irq_enable();
 
+	if (!vmf.pte && (vmf.vma_flags & VM_UFFD_MISSING) &&
+	    vma_is_anonymous(vmf.vma) && userfaultfd_using_sigbus(vmf.vma))
+		return VM_FAULT_SIGBUS;
+
 	/*
 	 * We need to re-validate the VMA after checking the bounds, otherwise
 	 * we might have a false positive on the bounds.
@@ -5196,6 +5211,15 @@ static vm_fault_t ___handle_speculative_fault(struct mm_struct *mm,
 out_walk:
 	trace_spf_vma_notsup(_RET_IP_, vmf.vma, address);
 	local_irq_enable();
+	/*
+	 * Failing page-table walk is similar to page-missing so give an
+	 * opportunity to SIGBUS+MISSING userfault to handle it before retrying
+	 * with mmap_lock
+	 */
+out_uffd:
+	if ((vmf.vma_flags & VM_UFFD_MISSING) && vma_is_anonymous(vmf.vma) &&
+	    userfaultfd_using_sigbus(vmf.vma))
+		return VM_FAULT_SIGBUS;
 	return VM_FAULT_RETRY;
 
 out_segv:
