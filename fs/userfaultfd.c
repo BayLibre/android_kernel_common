c72769b8f395192509749931d06d79a009c2c8db
Lokesh Gidra <lokeshgidra@google.com>
2024-01-16 20:20:25 +0000

    ANDROID: userfaultfd: Let SPF bailout be decided in handle_userfault()
    Currently we bail out of speculative page fault when we detech that the
    fault address is in a userfaultfd registered vma. However, if  userfaultfd
    is being used with UFFD_FEATURE_SIGBUS feature, then handle_userfault()
    doesn't do much.
    
    This patch lets handle_userfault() decide if we should bail out on SPF.
    
    Bug: 320478828
    Signed-off-by: Lokesh Gidra <lokeshgidra@google.com>
    Change-Id: Ic7fde0fde03602b35179bc0cf891ddbbc434190f

--- fs/userfaultfd.c
+++ fs/userfaultfd.c
@@ -476,6 +476,9 @@ vm_fault_t handle_userfault(struct vm_fault *vmf, unsigned long reason)
 	ret = VM_FAULT_RETRY;
 	if (vmf->flags & FAULT_FLAG_RETRY_NOWAIT)
 		goto out;
+	// Bail out if this fault is speculative.
+	if (vmf->flags & FAULT_FLAG_SPECULATIVE)
+		goto out;
 
 	/* take the reference before dropping the mmap_lock */
 	userfaultfd_ctx_get(ctx);
