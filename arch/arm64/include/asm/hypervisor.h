1092ef4f524c1b0e30b9381f594069dccd9b3a1e
Prakruthi Deepak Heragu <quic_pheragu@quicinc.com>
2024-01-22 12:10:04 -0800

    ANDROID: arm64: virt: Invalidate tlb once the balloon before reporting/inflating
    Instead of invalidating the tlb of the guest every page it relinquishes,
    invalidate once before the host is informed about the free pages.
    
    Bug: 315173520
    Change-Id: I8450fd552a27dfc791efa15e884982aeedaa3234
    Signed-off-by: Prakruthi Deepak Heragu <quic_pheragu@quicinc.com>
    (cherry picked from commit 886c9d1fc2b3a6c55c96e23e8ec0f5102a7e511d)

--- arch/arm64/include/asm/hypervisor.h
+++ arch/arm64/include/asm/hypervisor.h
@@ -10,6 +10,18 @@ void kvm_arm_init_hyp_services(void);
 void kvm_init_memshare_services(void);
 void kvm_init_ioremap_services(void);
 
+<<<<<<< HEAD   (df4996 UPSTREAM: erofs: fix infinite loop due to a race of filling )
+=======
+struct hypervisor_ops {
+#ifdef CONFIG_MEMORY_RELINQUISH
+	void (*page_relinquish)(struct page *page);
+	void (*post_page_relinquish_tlb_inv)(void);
+#endif
+};
+
+extern struct hypervisor_ops hyp_ops;
+
+>>>>>>> CHANGE (886c9d ANDROID: arm64: virt: Invalidate tlb once the balloon before)
 #ifdef CONFIG_MEMORY_RELINQUISH
 void kvm_init_memrelinquish_services(void);
 #else
