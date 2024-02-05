a3b31d86c08010a3a279dfcce9360d7ebba4c7b3
Kalesh Singh <kaleshsingh@google.com>
2024-02-05 11:12:46 -0800

    ANDROID: 16K: Add page-compat helper macros
    Add page-compat macros are namespace-prefixed by __ and otherwise
    named the same as their not-page-compat counter parts.
    
    Page-compat mode is disabled by default; it can be enabled
    by setting kernel boot parmeter, page_shift_compat=<shift>
    
    Valid values of page_shift_compat start at 1 larger than PAGE_SHIFT
    and end at 16 (64KB). For instance, on a 4KB kernel the valid range
    is [13, 16].
    
    Bug: 315325080
    Bug: 302403436
    Change-Id: I2f7fca417313652139ef21ac25c2b31080988c5b
    Signed-off-by: Kalesh Singh <kaleshsingh@google.com>

---
+++ mm/page_size_compat.c
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Page Size Emulation
+ *
+ * Copyright (c) 2024, Google LLC.
+ * Author: Kalesh Singh <kaleshsingh@goole.com>
+ */
+
+#include <linux/init.h>
+#include <linux/jump_label.h>
+#include <linux/kstrtox.h>
+#include <linux/page_size_compat.h>
+
+DEFINE_STATIC_KEY_FALSE(page_shift_compat_enabled);
+static int page_shift_compat = PAGE_SHIFT;
+static int max_page_shift_compat = 16;  /* Max of 64KB */
+
+static int __init early_page_shift_compat(char *buf)
+{
+	int ret;
+
+	ret = kstrtoint(buf, 10, &page_shift_compat);
+	if (ret)
+		return ret;
+
+	if (page_shift_compat > PAGE_SHIFT &&
+		page_shift_compat <= max_page_shift_compat)
+		static_branch_enable(&page_shift_compat_enabled);
+
+	return 0;
+}
+early_param("page_shift_compat", early_page_shift_compat);
+
+unsigned __page_shift(void)
+{
+	if (static_branch_unlikely(&page_shift_compat_enabled))
+		return page_shift_compat;
+	else
+		return PAGE_SHIFT;
+}
