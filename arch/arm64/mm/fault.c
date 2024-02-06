22f80ca14bac59f0b96f70d940784064792e905d
Chris Goldsworthy <quic_cgoldswo@quicinc.com>
2024-02-05 20:55:12 -0800

    ANDROID: arm64: Call fixup_exception() within do_sea()
    path_lookupat() is capable of safely reading unampped VAs. If an
    unmapped VA is read whilst the function is being called, that
    resulting page fault will get re-directed to __do_page_fault(), which
    will call fixup_exception() to handle the aforementioned
    unmapped-VA-read.
    
    Now, for an OS running in a VM, let's say that memory was still mapped
    at S1 but lent to another VM (i.e. unmapped at S2 for the given VM).
    The reading of an unmapped VA in path_lookupat() still needs to be
    handled. For hypervisors that inject an abort leading to a do_sea()
    call, call fixup_exception() if trace_android_rvh_try_fixup_sea was
    registered as a trace hook.
    
    Bug: 320358381
    Change-Id: I0aedcd954f08e3011b27524f9a7b038debbb246d
    Signed-off-by: Chris Goldsworthy <quic_cgoldswo@quicinc.com>

--- arch/arm64/mm/fault.c
+++ arch/arm64/mm/fault.c
@@ -749,6 +749,10 @@ static int do_sea(unsigned long far, unsigned long esr, struct pt_regs *regs)
 
 	inf = esr_to_fault_info(esr);
 
+	if (trace_android_rvh_try_fixup_sea_enabled() && !user_mode(regs) &&
+	    fixup_exception(regs))
+		return 0;
+
 	if (user_mode(regs) && apei_claim_sea(regs) == 0) {
 		/*
 		 * APEI claimed this as a firmware-first notification.
