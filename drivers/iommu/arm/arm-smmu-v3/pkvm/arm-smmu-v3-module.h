8e60167271afb5e02e76c3efdea6de31e87a477b
Mostafa Saleh <smostafa@google.com>
2024-02-14 16:25:00 +0000

    ANDROID: drivers/arm-smmu-v3-kvm: Allow registering SMMU beyond de-privilege
    Relax the initialising to allow devices to be probed beyond de-privilege.
    
    Registering is done by passing the ID of the IOMMU and the hyp struct which
    the kernel copies and uses.
    Later patches does some validation and limit kernel powers.
    
    Bug: 277989609
    Bug: 278749606
    
    Change-Id: Ie02d72e6f08badfabc1647a2352661238f1cd1ff
    Signed-off-by: Mostafa Saleh <smostafa@google.com>

--- drivers/iommu/arm/arm-smmu-v3/pkvm/arm-smmu-v3-module.h
+++ drivers/iommu/arm/arm-smmu-v3/pkvm/arm-smmu-v3-module.h
@@ -13,6 +13,7 @@ extern const struct pkvm_module_ops		*mod_ops;
 #undef memset
 #undef memcpy
 #undef kvm_flush_dcache_to_poc
+#undef kern_hyp_va
 
 /* Needs alternatives which is not supported at the moment. */
 #undef cmpxchg64_relaxed
@@ -39,6 +40,8 @@ extern const struct pkvm_module_ops		*mod_ops;
 #define kvm_iommu_iotlb_gather_add_page(x, y, z, w) \
 						CALL_FROM_OPS(iommu_iotlb_gather_add_page, x, y, z, w)
 #define __pkvm_host_donate_hyp(x, y)		___pkvm_host_donate_hyp(x, y, false)
+#define hyp_fixmap_map(x)			CALL_FROM_OPS(fixmap_map, x)
+#define hyp_fixmap_unmap()			CALL_FROM_OPS(fixmap_unmap)
 #endif
 
 #endif /* __ARM_SMMU_V3_MODULE__ */
