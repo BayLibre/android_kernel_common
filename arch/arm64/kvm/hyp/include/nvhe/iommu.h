d1fdd577a7b50c74db21090d6078e9537f1252cd
Mostafa Saleh <smostafa@google.com>
2024-02-14 17:51:02 +0000

    ANDROID: KVM: arm64: iommu: Add pkvm_iommu_finalise
    Add a function to prevent any more registration of IOMMUs, for now
    this is called after all the nodes probe, However this can be hardened
    to have a hard timeout in case that this was not called for any HW
    failure or misconfiguration.
    
    Bug: 277989609
    Bug: 278749606
    
    Change-Id: I51cb9342fdc5ae2b302be9cfeb623289f79c8312
    Signed-off-by: Mostafa Saleh <smostafa@google.com>

--- arch/arm64/kvm/hyp/include/nvhe/iommu.h
+++ arch/arm64/kvm/hyp/include/nvhe/iommu.h
@@ -23,6 +23,7 @@ int kvm_iommu_init_device(struct kvm_hyp_iommu *iommu);
 void *kvm_iommu_donate_pages(u8 order, bool request);
 void kvm_iommu_reclaim_pages(void *p, u8 order);
 int kvm_iommu_request(struct kvm_hyp_req *req);
+int kvm_iommu_finalise(void);
 
 /* alloc/free from idmap pool. */
 void *kvm_iommu_donate_pages_idmap(u8 order);
