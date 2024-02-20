15e1f718c70f7283087af6b17759372cf84a2d2d
Zhiguo Niu <zhiguo.niu@unisoc.com>
2024-02-20 14:11:24 +0800

    UPSTREAM: f2fs: stop checkpoint when get a out-of-bounds segment
    There is low probability that an out-of-bounds segment will be got
    on a small-capacity device. In order to prevent subsequent write requests
    allocating block address from this invalid segment, which may cause
    unexpected issue, stop checkpoint should be performed.
    
    Also introduce a new stop cp reason: STOP_CP_REASON_NO_SEGMENT.
    
    Note, f2fs_stop_checkpoint(, false) is complex and it may sleep, so we should
    move it outside segmap_lock spinlock coverage in get_new_segment().
    
    Signed-off-by: Zhiguo Niu <zhiguo.niu@unisoc.com>
    Reviewed-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    (cherry picked from commit 22d4368bd23c93de6b1bc3ef91967c34be160141)
    Change-Id: Ib7b2b74ec826d9ece0e1cb9ff2edd99399af8264
    Signed-off-by: Daeho Jeong <daehojeong@google.com>

--- fs/f2fs/segment.c
+++ fs/f2fs/segment.c
@@ -2563,6 +2563,7 @@ static void get_new_segment(struct f2fs_sb_info *sbi, unsigned int *newseg,
 	bool init = true;
 	int go_left = 0;
 	int i;
+	int ret = 0;
 
 	spin_lock(&free_i->segmap_lock);
 
@@ -2588,7 +2589,10 @@ find_other_zone:
 		if (dir == ALLOC_RIGHT) {
 			secno = find_first_zero_bit(free_i->free_secmap,
 							MAIN_SECS(sbi));
-			f2fs_bug_on(sbi, secno >= MAIN_SECS(sbi));
+			if (secno >= MAIN_SECS(sbi)) {
+				ret = -ENOSPC;
+				goto out_unlock;
+			}
 		} else {
 			go_left = 1;
 			left_start = hint - 1;
@@ -2645,7 +2649,13 @@ got_it:
 	f2fs_bug_on(sbi, test_bit(segno, free_i->free_segmap));
 	__set_inuse(sbi, segno);
 	*newseg = segno;
+out_unlock:
 	spin_unlock(&free_i->segmap_lock);
+
+	if (ret) {
+		f2fs_stop_checkpoint(sbi, false, STOP_CP_REASON_NO_SEGMENT);
+		f2fs_bug_on(sbi, 1);
+	}
 }
 
 static void reset_curseg(struct f2fs_sb_info *sbi, int type, int modified)
