14218b263fa96aa5156e44959258b6aed5fb10a1
Sebastian Ene <sebastianene@google.com>
2024-02-21 12:50:06 +0000

    ANDROID: KVM: arm64: Add hypervisor call to snapshot the guest stage-2
    The guest stage-2 pagetables are managed by the hypervisor under
    kvm.arm_mode="protected". When a VM is started in protected mode, the
    page-table-pages are isolated from the host. Introduce a call that
    extercises the snapshot walker to allow copying the guest stage-2
    pagetables under debugging configuration.
    
    Bug: 260238318
    Change-Id: Ia5ceb21b64ea989b812dee910262be813fd402f6
    Signed-off-by: Sebastian Ene <sebastianene@google.com>

--- arch/arm64/kvm/hyp/include/nvhe/mem_protect.h
+++ arch/arm64/kvm/hyp/include/nvhe/mem_protect.h
@@ -86,6 +86,8 @@ int __pkvm_remove_ioguard_page(struct pkvm_hyp_vcpu *hyp_vcpu, u64 ipa);
 bool __pkvm_check_ioguard_page(struct pkvm_hyp_vcpu *hyp_vcpu);
 int __pkvm_guest_relinquish_to_host(struct pkvm_hyp_vcpu *vcpu,
 				    u64 ipa, u64 *ppa);
+int __pkvm_stage2_snapshot(struct kvm_pgtable_snapshot *snapshot, pkvm_handle_t handle);
+int __pkvm_guest_stage2_snapshot(struct kvm_pgtable_snapshot *snap, struct pkvm_hyp_vm *vm);
 
 bool addr_is_memory(phys_addr_t phys);
 int host_stage2_idmap_locked(phys_addr_t addr, u64 size, enum kvm_pgtable_prot prot);
