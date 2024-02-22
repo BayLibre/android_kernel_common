3453479613dc67660ce48048ac07715b6d89798e
Chao Yu <chao@kernel.org>
2024-02-22 20:18:50 +0800

    UPSTREAM: f2fs: fix to handle segment allocation failure correctly
    If CONFIG_F2FS_CHECK_FS is off, and for very rare corner case that
    we run out of free segment, we should not panic kernel, instead,
    let's handle such error correctly in its caller.
    
    Signed-off-by: Chao Yu <chao@kernel.org>
    Tested-by: Zhiguo Niu <zhiguo.niu@unisoc.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    (cherry picked from commit ee8bc5f3636883349db5516a3a723933a9752cb8)
    Change-Id: I252f6185737129333aa672e5603939ad15dab06a
    Signed-off-by: xiuhong.wang <xiuhong.wang@unisoc.com>

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -1393,6 +1393,7 @@ static int __allocate_data_block(struct dnode_of_data *dn, int seg_type)
 alloc:
 	set_summary(&sum, dn->nid, dn->ofs_in_node, ni.version);
 	old_blkaddr = dn->data_blkaddr;
+<<<<<<< HEAD
 	f2fs_allocate_data_block(sbi, NULL, old_blkaddr, &dn->data_blkaddr,
 				&sum, seg_type, NULL);
 	if (GET_SEGNO(sbi, old_blkaddr) != NULL_SEGNO) {
@@ -1400,6 +1401,16 @@ alloc:
 					old_blkaddr, old_blkaddr);
 		f2fs_invalidate_compress_page(sbi, old_blkaddr);
 	}
+=======
+	err = f2fs_allocate_data_block(sbi, NULL, old_blkaddr,
+				&dn->data_blkaddr, &sum, seg_type, NULL);
+	if (err)
+		return err;
+
+	if (GET_SEGNO(sbi, old_blkaddr) != NULL_SEGNO)
+		f2fs_invalidate_internal_cache(sbi, old_blkaddr);
+
+>>>>>>> c93de673fa84 (UPSTREAM: f2fs: fix to handle segment allocation failure correctly)
 	f2fs_update_data_blkaddr(dn, dn->data_blkaddr);
 	return 0;
 }
