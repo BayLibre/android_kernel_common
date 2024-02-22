23f6c53429b50cfc34ce642c4f0acb3a8c4bdc55
Sebastian Ene <sebastianene@google.com>
2024-02-22 15:53:01 +0000

    ANDROID: KVM: arm64: Read the stage-2 pagetable config with the snapshot hvc
    When no memory is specified for the copy of the pagetable that we are
    about to create, just copy the configuration of the pagetable such as
    the start level and the number of IPA bits. This removes the need for
    adding a new interface for the same thing.
    
    Bug: 260238318
    Change-Id: Iddde8b5d65597a3acf2fcddcb272df666622a78e
    Signed-off-by: Sebastian Ene <sebastianene@google.com>

--- arch/arm64/kvm/hyp/nvhe/mem_protect.c
+++ arch/arm64/kvm/hyp/nvhe/mem_protect.c
@@ -2818,6 +2818,7 @@ static void pkvm_stage2_initialize_snapshot(const struct kvm_pgtable *from_pgt,
 	dest_pgt->start_level	= from_pgt->start_level;
 	dest_pgt->flags		= from_pgt->flags;
 	dest_pgt->pte_ops	= from_pgt->pte_ops;
+	dest_pgt->pgd		= NULL;
 }
 
 int __pkvm_guest_stage2_snapshot(struct kvm_pgtable_snapshot *snap,
@@ -2831,14 +2832,20 @@ int __pkvm_guest_stage2_snapshot(struct kvm_pgtable_snapshot *snap,
 	if (snap->used_pages_idx != 0)
 		return -EINVAL;
 
+	from_pgt = &vm->pgt;
+	to_pgt = &snap->pgtable;
+
+	pkvm_stage2_initialize_snapshot(from_pgt, to_pgt, &mm_ops);
+
+	if (snap->pgd_pages == 0 || snap->num_used_pages == 0)
+		return 0;
+
 	required_pgd_len = kvm_pgtable_stage2_pgd_size(vm->kvm.arch.vtcr);
 	if (snap->pgd_pages != (required_pgd_len >> PAGE_SHIFT))
 		return -EINVAL;
 
 	from_pgt = &vm->pgt;
 	to_pgt = &snap->pgtable;
-
-	pkvm_stage2_initialize_snapshot(from_pgt, to_pgt, &mm_ops);
 
 	guest_lock_component(vm);
 	to_pgt->pgd = kern_hyp_va(snap->pgd_hva);
