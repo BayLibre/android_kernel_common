a274b0ddb027de5b405f79fe40820a28179aa887
Elliot Berman <quic_eberman@quicinc.com>
2024-02-22 15:16:42 -0800

    FROMLIST: arch/mm: Export direct {un,}map functions
    Firmware and hypervisor drivers can donate system heap memory to their
    respective firmware/hypervisor entities. Those drivers should unmap the
    pages from the kernel's logical map before doing so.
    
    Export can_set_direct_map, set_direct_map_invalid_noflush, and
    set_direct_map_default_noflush.
    
    Bug: 338347082
    Link: https://lore.kernel.org/all/20240222-gunyah-v17-19-1e9da6763d38@quicinc.com/
    Change-Id: Ib94aa30156db3a6b9a755228ed7c20c910caaf65
    Signed-off-by: Elliot Berman <quic_eberman@quicinc.com>

--- arch/arm64/mm/pageattr.c
+++ arch/arm64/mm/pageattr.c
@@ -32,6 +32,7 @@ bool can_set_direct_map(void)
 	return rodata_full || debug_pagealloc_enabled() ||
 	       arm64_kfence_can_set_direct_map();
 }
+EXPORT_SYMBOL_GPL(can_set_direct_map);
 
 static int change_page_range(pte_t *ptep, unsigned long addr, void *data)
 {
@@ -176,6 +177,7 @@ int set_direct_map_invalid_noflush(struct page *page)
 				   (unsigned long)page_address(page),
 				   PAGE_SIZE, change_page_range, &data);
 }
+EXPORT_SYMBOL_GPL(set_direct_map_invalid_noflush);
 
 int set_direct_map_default_noflush(struct page *page)
 {
@@ -191,6 +193,7 @@ int set_direct_map_default_noflush(struct page *page)
 				   (unsigned long)page_address(page),
 				   PAGE_SIZE, change_page_range, &data);
 }
+EXPORT_SYMBOL_GPL(set_direct_map_default_noflush);
 
 #ifdef CONFIG_DEBUG_PAGEALLOC
 void __kernel_map_pages(struct page *page, int numpages, int enable)
