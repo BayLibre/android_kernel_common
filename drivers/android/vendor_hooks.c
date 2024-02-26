bfa571fa246a444448ab3f4f66030b70c7fa49c1
zhaoyang.huang <zhaoyang.huang@unisoc.com>
2024-02-26 11:15:16 +0800

    ANDROID: Add vendor hook for setting ioprio
    Introduce vhs for Implement a set of ioprio setting policy
    
    Bug: 326852045
    Test: build pass
    Change-Id: I60476f8d2a19d4246f498a1f5b4ae18d8e0f45e3
    Signed-off-by: zhaoyang.huang <zhaoyang.huang@unisoc.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -51,6 +51,7 @@
 #include <trace/hooks/futex.h>
 #include <trace/hooks/topology.h>
 #include <trace/hooks/thermal.h>
+#include <trace/hooks/ioprio.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -68,6 +69,9 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_arch_set_freq_scale);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_transaction_init);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_set_priority);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_restore_priority);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_bio_set_ioprio);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_bio_set_ioprio_ipu);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_bio_set_ioprio_iter);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_cpu_idle_enter);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_cpu_idle_exit);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mpam_set);
