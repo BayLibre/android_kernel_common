eb14451148db7a2c1ae9f41b86b8abc6fa6f7496
Greg Kroah-Hartman <gregkh@google.com>
2024-02-29 10:29:40 +0000

    ANDROID: GKI: the "reusachtig" padding sync with android15-6.1
    Add the initial set of ABI padding fields in android15-6.6 based on what
    is in the android15-6.1 branch.
    
    Bug: 151154716
    Change-Id: Icdb394863b2911389bfdced0fd1ea20236ca4ce1
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- include/linux/device/bus.h
+++ include/linux/device/bus.h
@@ -107,6 +107,11 @@ struct bus_type {
 	const struct iommu_ops *iommu_ops;
 
 	bool need_parent_lock;
+
+	ANDROID_KABI_RESERVE(1);
+	ANDROID_KABI_RESERVE(2);
+	ANDROID_KABI_RESERVE(3);
+	ANDROID_KABI_RESERVE(4);
 };
 
 int __must_check bus_register(const struct bus_type *bus);
