maltesermailo/android_kernel_pantah

ANDROID: 16K: x86_64: __PAGE_ALIGN vdso_addr()
In page-compat mode, the vdso address must be a multiple of the emulated
page-size.

Bug: 315325080
Bug: 302403436
Change-Id: I5453e13ab6ee360babde002626a3a9d505cd9627
Signed-off-by: Kalesh Singh <kaleshsingh@google.com>

arch/x86/entry/vdso/vma.c
@@ -15,6 +15,7 @@
 #include <linux/cpu.h>
 #include <linux/ptrace.h>
 #include <linux/time_namespace.h>
+#include <linux/page_size_compat.h>
 
 #include <asm/pvclock.h>
 #include <asm/vgtod.h>
@@ -318,7 +319,7 @@ static unsigned long vdso_addr(unsigned long start, unsigned len)
 	 * Round up the start address.  It can start out unaligned as a result
 	 * of stack start randomization.
 	 */
-	start = PAGE_ALIGN(start);
+	start = __PAGE_ALIGN(start);
 
 	/* Round the lowest possible end address up to a PMD boundary. */
 	end = (start + len + PMD_SIZE - 1) & PMD_MASK;
@@ -327,8 +328,8 @@ static unsigned long vdso_addr(unsigned long start, unsigned len)
 	end -= len;
 
 	if (end > start) {
-		offset = prandom_u32_max(((end - start) >> PAGE_SHIFT) + 1);
-		addr = start + (offset << PAGE_SHIFT);
+		offset = prandom_u32_max(((end - start) >> __PAGE_SHIFT) + 1);
+		addr = start + (offset << __PAGE_SHIFT);
 	} else {
 		addr = start;
 	}

BayLibre/android_kernel_common

Change-Id: I9cc83d37a77d9446672307dd642288b695f9ceae

arch/x86/entry/vdso/vma.c
@@ -15,6 +15,7 @@
 #include <linux/cpu.h>
 #include <linux/ptrace.h>
 #include <linux/time_namespace.h>
+#include <linux/page_size_compat.h>
 
 #include <asm/pvclock.h>
 #include <asm/vgtod.h>
@@ -295,7 +296,7 @@ static unsigned long vdso_addr(unsigned long start, unsigned len)
 	 * Round up the start address.  It can start out unaligned as a result
 	 * of stack start randomization.
 	 */
-	start = PAGE_ALIGN(start);
+	start = __PAGE_ALIGN(start);
 
 	/* Round the lowest possible end address up to a PMD boundary. */
 	end = (start + len + PMD_SIZE - 1) & PMD_MASK;
@@ -304,8 +305,8 @@ static unsigned long vdso_addr(unsigned long start, unsigned len)
 	end -= len;
 
 	if (end > start) {
-		offset = get_random_u32_below(((end - start) >> PAGE_SHIFT) + 1);
-		addr = start + (offset << PAGE_SHIFT);
+		offset = get_random_u32_below(((end - start) >> __PAGE_SHIFT) + 1);
+		addr = start + (offset << __PAGE_SHIFT);
 	} else {
 		addr = start;
 	}