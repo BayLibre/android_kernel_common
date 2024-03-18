e4b611d08736d9a4a4f37277b74d949a7716da34
Chris Goldsworthy <cgoldswo@codeaurora.org>
2024-03-17 23:52:20 -0700

    ANDROID: mm: Create hooks for ZONE_MOVABLE allocs
    Create a vendor hook inside of gfp_zone() to modify which allocations
    get to enter ZONE_MOVABLE, by zeroing out __GFP_HIGHMEM inside of the
    trace hook based on certain conditions.
    
    Separately, create a trace hook in the readahead path to affect the
    behavior of the tracehook in gfp_zone().
    
    In 5.15, we had set_skip_swapcache_flags trace-hook in do_swap_page()
    but commit ac26e9c7b809 ("ANDROID: cma: allow to use CMA in swap-in path")
    added __GFP_CMA explicitly, so the set_skip_swapcache_flags trace hook
    is no longer needed.
    
    Note:	To comply with vendor hook guidlines, avoid including types.h in
    	trace/hooks/mm.h and use unsigned int for gfp_t.
    
    Bug: 330201547
    Change-Id: Idfa6b0b06b1b819d706c847e702bc94ddf7aa55a
    Signed-off-by: Chris Goldsworthy <cgoldswo@codeaurora.org>
    Signed-off-by: Sukadev Bhattiprolu <quic_sukadev@quicinc.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -272,3 +272,5 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_new_ref);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_del_ref);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_logbuf_pr_cont);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_alloc_pages_slowpath);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_set_gfp_zone_flags);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_set_readahead_gfp_mask);
