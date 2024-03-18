c422b4357bfcbbfd9280cd022073afd0e2afb677
Chris Goldsworthy <cgoldswo@codeaurora.org>
2024-03-17 23:52:20 -0700

    ANDROID: mm: Create hooks for ZONE_MOVABLE allocs
    Create a vendor hook inside of gfp_zone() to modify which allocations
    get to enter ZONE_MOVABLE, by zeroing out __GFP_HIGHMEM inside of the
    trace hook based on certain conditions.
    
    Separately, create a trace hook in the readahead path to affect the
    behavior of the tracehook in gfp_zone().
    
    In 5.15, we had set_skip_swapcache_flags trace-hook in do_swap_page()
    but commit ac26e9c7b809 ("ANDROID: cma: allow to use CMA in swap-in path")
    added __GFP_CMA explicitly, so the set_skip_swapcache_flags trace hook
    is no longer needed.
    
    Note:	To comply with vendor hook guidlines, avoid including types.h in
    	trace/hooks/mm.h and use unsigned int for gfp_t.
    
    Bug: 330201547
    Change-Id: Idfa6b0b06b1b819d706c847e702bc94ddf7aa55a
    Signed-off-by: Chris Goldsworthy <cgoldswo@codeaurora.org>
    Signed-off-by: Sukadev Bhattiprolu <quic_sukadev@quicinc.com>

--- mm/memory.c
+++ mm/memory.c
@@ -3979,8 +3979,8 @@ vm_fault_t do_swap_page(struct vm_fault *vmf)
 			need_clear_cache = true;
 
 			/* skip swapcache */
-			folio = vma_alloc_folio(GFP_HIGHUSER_MOVABLE, 0,
-						vma, vmf->address, false);
+			folio = vma_alloc_folio(GFP_HIGHUSER_MOVABLE|__GFP_CMA,
+						0, vma, vmf->address, false);
 			page = &folio->page;
 			if (folio) {
 				__folio_set_locked(folio);
@@ -4006,7 +4006,8 @@ vm_fault_t do_swap_page(struct vm_fault *vmf)
 				folio->private = NULL;
 			}
 		} else {
-			page = swapin_readahead(entry, GFP_HIGHUSER_MOVABLE,
+			page = swapin_readahead(entry,
+						GFP_HIGHUSER_MOVABLE|__GFP_CMA,
 						vmf);
 			if (page)
 				folio = page_folio(page);
