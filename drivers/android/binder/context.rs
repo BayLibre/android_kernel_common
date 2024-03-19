f92cc05d20def40533ba60c6b024c292524d7532
Alice Ryhl <aliceryhl@google.com>
2024-03-19 16:32:24 +0000

    ANDROID: rust_binder: track processes part of a context
    Bug: 278052745
    Change-Id: I9f9897262feb24955d1ad7897e01be7432d83724
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>

--- drivers/android/binder/context.rs
+++ drivers/android/binder/context.rs
@@ -6,8 +6,10 @@ use kernel::{
     list::{HasListLinks, List, ListArc, ListArcSafe, ListItem, ListLinks},
     prelude::*,
     str::{CStr, CString},
-    sync::Arc,
+    sync::{Arc, Mutex},
 };
+
+use crate::process::Process;
 
 // This module defines the global variable containing the list of contexts. Since the
 // `kernel::sync` bindings currently don't support mutexes in globals, we use a temporary
@@ -83,9 +85,17 @@ pub(crate) struct ContextList {
     list: List<Context>,
 }
 
+/// This struct keeps track of the processes using this context, and which process is the context
+/// manager.
+struct Manager {
+    all_procs: List<Process>,
+}
+
 /// There is one context per binder file (/dev/binder, /dev/hwbinder, etc)
 #[pin_data]
 pub(crate) struct Context {
+    #[pin]
+    manager: Mutex<Manager>,
     pub(crate) name: CString,
     #[pin]
     links: ListLinks,
@@ -109,6 +119,9 @@ impl Context {
         let list_ctx = ListArc::pin_init(pin_init!(Context {
             name,
             links <- ListLinks::new(),
+            manager <- kernel::new_mutex!(Manager {
+                all_procs: List::new(),
+            }, "Context::manager"),
         }))?;
 
         let ctx = list_ctx.clone_arc();
@@ -124,5 +137,22 @@ impl Context {
         // SAFETY: We never add the context to any other linked list than this one, so it is either
         // in this list, or not in any list.
         unsafe { CONTEXTS.lock().list.remove(self) };
+    }
+
+    pub(crate) fn register_process(self: &Arc<Self>, proc: ListArc<Process>) {
+        if !Arc::ptr_eq(self, &proc.ctx) {
+            pr_err!("Context::register_process called on the wrong context.");
+            return;
+        }
+        self.manager.lock().all_procs.push_back(proc);
+    }
+
+    pub(crate) fn deregister_process(self: &Arc<Self>, proc: &Process) {
+        if !Arc::ptr_eq(self, &proc.ctx) {
+            pr_err!("Context::deregister_process called on the wrong context.");
+            return;
+        }
+        // SAFETY: We just checked that this is the right list.
+        unsafe { self.manager.lock().all_procs.remove(proc) };
     }
 }
--- drivers/android/binder/process.rs
+++ drivers/android/binder/process.rs
@@ -15,6 +15,7 @@
 use kernel::{
     bindings,
     file::File,
+    list::{HasListLinks, ListArc, ListArcSafe, ListItem, ListLinks},
     mm,
     prelude::*,
     sync::poll::PollTable,
@@ -32,11 +33,33 @@ use crate::{context::Context, defs::*};
 #[pin_data]
 pub(crate) struct Process {
     pub(crate) ctx: Arc<Context>,
+
+    // Links for process list in Context.
+    #[pin]
+    links: ListLinks,
+}
+
+kernel::list::impl_has_list_links! {
+    impl HasListLinks<0> for Process { self.links }
+}
+kernel::list::impl_list_arc_safe! {
+    impl ListArcSafe<0> for Process { untracked; }
+}
+kernel::list::impl_list_item! {
+    impl ListItem<0> for Process {
+        using ListLinks;
+    }
 }
 
 impl Process {
     fn new(ctx: Arc<Context>) -> Result<Arc<Self>> {
-        let process = Arc::try_new(Process { ctx })?;
+        let list_process = ListArc::pin_init(pin_init!(Process {
+            ctx,
+            links <- ListLinks::new(),
+        }))?;
+
+        let process = list_process.clone_arc();
+        process.ctx.register_process(list_process);
 
         Ok(process)
     }
@@ -83,7 +106,9 @@ impl Process {
         Self::new(ctx.into())
     }
 
-    pub(crate) fn release(_this: Arc<Process>, _file: &File) {}
+    pub(crate) fn release(this: Arc<Process>, _file: &File) {
+        this.ctx.deregister_process(&this);
+    }
 
     pub(crate) fn flush(_this: ArcBorrow<'_, Process>) -> Result {
         Ok(())
