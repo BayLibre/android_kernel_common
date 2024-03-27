706934be90a365e6d1a175107d3302afd257ca37
Sebastian Ene <sebastianene@google.com>
2024-03-27 13:38:30 +0000

    ANDROID: KVM: arm64: Add FF-A trace event
    Define a hypervisor trace event for FF-A smc calls. Log the function
    number and the associated arguments when the hypervisor traps the FF-A smc
    request coming from the host. Refactor the handling of the FF-A host smc
    calls without chaning the intended behaviour.
    
    Bug: 325404073
    Change-Id: I2d448a69e4250debfeeba07beae59682bc958e02
    Signed-off-by: Sebastian Ene <sebastianene@google.com>

--- arch/arm64/include/asm/kvm_hypevents.h
+++ arch/arm64/include/asm/kvm_hypevents.h
@@ -91,4 +91,23 @@ HYP_EVENT(__hyp_printk,
 	HE_PRINTK_UNKNOWN_FMT(hyp_printk_fmt_from_id(__entry->fmt_id),
 		__entry->a, __entry->b, __entry->c, __entry->d)
 );
+
+HYP_EVENT(ffa_call,
+	HE_PROTO(u64 func_id, u64 res_a0, u64 res_a1, u64 res_a2, u64 res_a3, int handled, int err),
+	HE_STRUCT(
+		he_field(u64, func_id)
+		he_field(u64, res_a0)
+		he_field(u64, res_a1)
+		he_field(u64, res_a2)
+		he_field(u64, res_a3)
+		he_field(int, handled)
+		he_field(int, err)
+	),
+	HE_ASSIGN(
+		__entry->func_id = func_id;
+	),
+	HE_PRINTK("ffa_func=0x%llx a0=0x%llx a1=0x%llx a2=0x%llx a3=%llx handled=%d err=%d",
+		  __entry->func_id, __entry->res_a0, __entry->res_a1,
+		  __entry->res_a2, __entry->res_a3, __entry->handled, __entry->err)
+);
 #endif
