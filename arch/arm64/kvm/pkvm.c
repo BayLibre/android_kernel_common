81c5fb1be3626b81ad9d26aacd5c421353520db6
Vincent Donnefort <vdonnefort@google.com>
2024-04-04 09:51:05 +0100

    ANDROID: KVM: arm64: wait_for_initramfs for pKVM module loading procfs
    Of course, the initramfs needs to be ready before procfs can be
    mounted... in the initramfs. While at it, only mount if a pKVM module
    must be loaded and only print a warning in case of failure.
    
    Bug: 357781595
    Bug: 301483379
    Bug: 331152809
    Change-Id: Ie56bd26d4575f69cb1f06ba6317a098649f6da44
    Reported-by: Mankyum Kim <mankyum.kim@samsung-slsi.corp-partner.google.com>
    Signed-off-by: Vincent Donnefort <vdonnefort@google.com>

--- arch/arm64/kvm/pkvm.c
+++ arch/arm64/kvm/pkvm.c
@@ -6,6 +6,7 @@
 
 #include <linux/init.h>
 #include <linux/interval_tree_generic.h>
+#include <linux/initrd.h>
 #include <linux/io.h>
 #include <linux/iommu.h>
 #include <linux/kmemleak.h>
@@ -870,6 +871,7 @@ static int __init __pkvm_request_early_module(char *module_name,
 		"PATH=/sbin:/usr/sbin:/bin:/usr/bin",
 		NULL
 	};
+	static bool proc;
 	char **argv;
 	int idx = 0;
 
@@ -900,6 +902,15 @@ static int __init __pkvm_request_early_module(char *module_name,
 
 	/* Even with CONFIG_STATIC_USERMODEHELPER we really want this path */
 	info->path = modprobe_path;
+
+	if (!proc) {
+		wait_for_initramfs();
+		if (init_mount("proc", "/proc", "proc",
+			       MS_SILENT | MS_NOEXEC | MS_NOSUID, NULL))
+			pr_warn("Couldn't mount /proc, pKVM module parameters will be ignored\n");
+
+		proc = true;
+	}
 
 	return call_usermodehelper_exec(info, UMH_WAIT_PROC | UMH_KILLABLE);
 err:
