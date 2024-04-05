d566f8a952d5e12a84d9ecad306d36038dc3af4d
Kalesh Singh <kaleshsingh@google.com>
2024-04-04 22:21:32 -0700

    ANDROID: 16K: Introduce ELF padding representation for VMAs
    The dynamic linker may extend ELF LOAD segment mappings to be contiguous
    in memory when loading a 16kB compatible ELF on a 4kB page-size system.
    This is done to reduce the use of unreclaimable VMA slab memory for the
    otherwise necessary "gap" VMAs. The extended portion of the mapping
    (VMA) can be viewed as "padding", meaning that the mapping in that range
    corresponds to an area of the file that does not contain contents of the
    respective segments (maybe zero's depending on how the ELF is built).
    
    For some compatibility mitigations, the regoin of a VMA corresponding to
    these paddign sections need to be known.
    
    In order to represent such regions without adding addtional overhead or
    breaking ABI, some upper bits of vm_flags are used.
    
    Add the VMA padding pages representation and the necessary APIs to
    manipulate it.
    
    Bug: 330117029
    Bug: 327600007
    Bug: 330767927
    Bug: 328266487
    Bug: 329803029
    Change-Id: Ieb9fa98e30ec9b0bec62256624f14e3ed6062a75
    Signed-off-by: Kalesh Singh <kaleshsingh@google.com>

--- mm/pgsize_migration.c
+++ mm/pgsize_migration.c
@@ -10,17 +10,22 @@
  * Author: Kalesh Singh <kaleshsingh@goole.com>
  */
 
+#include <linux/pgsize_migration.h>
+
 #include <linux/init.h>
 #include <linux/jump_label.h>
 #include <linux/kobject.h>
 #include <linux/kstrtox.h>
-#include <linux/mm.h>
 #include <linux/sysfs.h>
 
 #if PAGE_SIZE == SZ_4K
 DEFINE_STATIC_KEY_TRUE(pgsize_migration_enabled);
+
+#define is_pgsize_migration_enabled() 	(static_branch_likely(&pgsize_migration_enabled))
 #else /* PAGE_SIZE != SZ_4K */
 DEFINE_STATIC_KEY_FALSE(pgsize_migration_enabled);
+
+#define is_pgsize_migration_enabled() 	(static_branch_unlikely(&pgsize_migration_enabled))
 #endif /* PAGE_SIZE == SZ_4K */
 
 static ssize_t show_pgsize_migration_enabled(struct kobject *kobj,
@@ -79,3 +84,44 @@ static int __init init_pgsize_migration(void)
 	return 0;
 };
 late_initcall(init_pgsize_migration);
+
+#if PAGE_SIZE == SZ_4K
+void vma_set_pad_pages(struct vm_area_struct *vma,
+		       unsigned long nr_pages)
+{
+	vm_flags_t flags = 0;
+
+	if (!is_pgsize_migration_enabled())
+		return;
+
+	if (nr_pages & 1UL)
+		flags |=  VM_PAD_4KB_BIT1;
+	if (nr_pages & 2UL)
+		flags |=  VM_PAD_4KB_BIT2;
+	if (nr_pages & 4UL)
+		flags |=  VM_PAD_16KB_BIT1;
+	if (nr_pages & 8UL)
+		flags |=  VM_PAD_16KB_BIT2;
+
+	vma->vm_flags |= flags;
+}
+
+unsigned long vma_pad_pages(struct vm_area_struct *vma)
+{
+	unsigned long nr_pages = 0;
+
+	if (!is_pgsize_migration_enabled())
+		return nr_pages;
+
+	if (vma->vm_flags & VM_PAD_4KB_BIT1)
+		nr_pages |= 1UL;
+	if (vma->vm_flags & VM_PAD_4KB_BIT2)
+		nr_pages |= 2UL;
+	if (vma->vm_flags & VM_PAD_16KB_BIT1)
+		nr_pages |= 4UL;
+	if (vma->vm_flags & VM_PAD_16KB_BIT2)
+		nr_pages |= 8UL;
+
+	return nr_pages;
+}
+#endif /* PAGE_SIZE == SZ_4K */
