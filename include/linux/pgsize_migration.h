49b8cbab087290040e3e3f94a4be933f9d1e3309
Kalesh Singh <kaleshsingh@google.com>
2024-04-04 22:21:32 -0700

    ANDROID: 16K: Introduce ELF padding representation for VMAs
    The dynamic linker may extend ELF LOAD segment mappings to be contiguous
    in memory when loading a 16kB compatible ELF on a 4kB page-size system.
    This is done to reduce the use of unreclaimable VMA slab memory for the
    otherwise necessary "gap" VMAs. The extended portion of the mapping
    (VMA) can be viewed as "padding", meaning that the mapping in that range
    corresponds to an area of the file that does not contain contents of the
    respective segments (maybe zero's depending on how the ELF is built).
    
    For some compatibility mitigations, the regoin of a VMA corresponding to
    these paddign sections need to be known.
    
    In order to represent such regions without adding addtional overhead or
    breaking ABI, some upper bits of vm_flags are used.
    
    Add the VMA padding pages representation and the necessary APIs to
    manipulate it.
    
    Change-Id: Ieb9fa98e30ec9b0bec62256624f14e3ed6062a75
    Signed-off-by: Kalesh Singh <kaleshsingh@google.com>

---
+++ include/linux/pgsize_migration.h
@@ -0,0 +1,80 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _LINUX_PAGE_SIZE_MIGRATION_H
+#define _LINUX_PAGE_SIZE_MIGRATION_H
+
+/*
+ * include/linux/pgsize_migration.h
+ *
+ * Page Size Migration
+ *
+ * Copyright (c) 2024, Google LLC.
+ * Author: Kalesh Singh <kaleshsingh@goole.com>
+ *
+ * This file contains the APIs for mitigations to ensure
+ * app compatibility during the transition from 4kB to 16kB
+ * page size in Android.
+ */
+
+#include <linux/mm.h>
+
+/*
+ * 4 high bits of vm_flags [62,59] (63 is used for page size emulation in x86_64)
+ * are used to represent ELF segment padding up to 60kB, which is sufficient for
+ * ELFs of both 16kB and 64kB segment alignment (p_align).
+ *
+ * The representation is illustrated below.
+ *
+ *                    62        61        60        59
+ *                _________ _________ _________ _________
+ *               |  Bit 2  |  Bit 1  |  Bit 2  |  Bit 1  |
+ *               | of 16kB | of 16kB | of  4kB | of  4kB |
+ *               |  chunks |  chunks |  chunks |  chunks |
+ *               |_________|_________|_________|_________|
+ */
+
+#define VM_PAD_16KB_BIT2	(_AC(1,ULL) << 62)
+#define VM_PAD_16KB_BIT1	(_AC(1,ULL) << 61)
+#define VM_PAD_4KB_BIT2		(_AC(1,ULL) << 60)
+#define VM_PAD_4KB_BIT1		(_AC(1,ULL) << 59)
+#define VM_PAD_BITS		(VM_PAD_16KB_BIT2|VM_PAD_16KB_BIT1|VM_PAD_4KB_BIT2|VM_PAD_4KB_BIT1)
+#define VM_TOTAL_PAD_PAGES 	15
+
+static inline void vma_set_pad_pages(struct vm_area_struct *vma,
+				     unsigned long nr_pages)
+{
+	vm_flags_t flags = 0;
+
+	if (nr_pages & 1UL)
+		flags |=  VM_PAD_4KB_BIT1;
+	if (nr_pages & 2UL)
+		flags |=  VM_PAD_4KB_BIT2;
+	if (nr_pages & 4UL)
+		flags |=  VM_PAD_16KB_BIT1;
+	if (nr_pages & 8UL)
+		flags |=  VM_PAD_16KB_BIT2;
+
+	vma->vm_flags |= flags;
+}
+
+static inline unsigned long vma_pad_pages(struct vm_area_struct *vma)
+{
+	unsigned long nr_pages = 0;
+
+	if (vma->vm_flags & VM_PAD_4KB_BIT1)
+		nr_pages |= 1UL;
+	if (vma->vm_flags & VM_PAD_4KB_BIT2)
+		nr_pages |= 2UL;
+	if (vma->vm_flags & VM_PAD_16KB_BIT1)
+		nr_pages |= 4UL;
+	if (vma->vm_flags & VM_PAD_16KB_BIT2)
+		nr_pages |= 8UL;
+
+	return nr_pages;
+}
+
+static inline unsigned long vma_data_pages(struct vm_area_struct *vma)
+{
+	return vma_pages(vma) - vma_pad_pages(vma);
+}
+
+#endif /* _LINUX_PAGE_SIZE_MIGRATION_H */
