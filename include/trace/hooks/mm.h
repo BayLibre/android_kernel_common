de0e527edd1ef575de186fbf310f34dcb4b83dd1
Huan Yang <link@vivo.corp-partner.google.com>
2024-04-08 17:35:49 +0800

    ANDROID: vendor_hooks: add hooks in free page
    Add a hook when page going to free to pcp or buddy.
    This hook can giving a chance to trace or hold this
    page.
    
    BUGID: 333476585
    
    Change-Id: Ideed4f3a8d35b41ab485de35a7e8c49d566bcac7
    Signed-off-by: Huan Yang <link@vivo.corp-partner.google.com>

--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -86,6 +86,10 @@ DECLARE_HOOK(android_vh_kmalloc_slab,
 DECLARE_HOOK(android_vh_adjust_kvmalloc_flags,
 	TP_PROTO(unsigned int order, gfp_t *alloc_flags),
 	TP_ARGS(order, alloc_flags));
+DECLARE_HOOK(android_vh_pcp_free_one_page_bypass,
+	TP_PROTO(struct page *page, struct zone *zone, int order, int migratetype,
+		bool *bypass),
+	TP_ARGS(page, zone, order, migratetype, bypass));
 #endif /* _TRACE_HOOK_MM_H */
 
 /* This part must be outside protection */
