1e1022dc4960e07fb1e03f9f067ef0d2c46e09e4
qiusanming <qiusanming@xiaomi.com>
2024-04-13 13:16:47 +0800

    ANDROID: vendor_hooks: vendor hook for irq
    Add irq hooks to record enter and exit hwirq number to memory.
    When system panic, we can dump the memory to analyze runtime irq info.
    
    Bug: 335569355
    Change-Id: I605c3ae592738b6c90b9478ed577f7bbd1e04e82
    Signed-off-by: qiusanming <qiusanming@xiaomi.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -272,3 +272,5 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_new_ref);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_del_ref);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_logbuf_pr_cont);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_alloc_pages_slowpath);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_irq_enter);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_irq_exit);
