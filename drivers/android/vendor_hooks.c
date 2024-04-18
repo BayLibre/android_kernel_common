3db81bca29aad539382cce3195e4c403cd9f213b
hulianqin <hulianqin@vivo.corp-partner.google.com>
2024-04-18 15:51:37 +0800

    ANDROID: usb: Add vendor hooks
    We just need to be able to get the enumeration status of USB in the
    kernel, because vivo charging needs the enumeration status information
    of USB to decide whether to switch to dp and dm signal to the charger.
    
    Obtain some status information during the USB enumeration process to
    facilitate analysis and location of port abnormalities in the project
    development process and user scenarios (such as the upper card owner
    during the bringup process, etc.)
    
    Bug: 333350371
    
    Change-Id: Id6d54559d59f9b5cfe97c646f1c0571d0635cf75
    Signed-off-by: hulianqin <hulianqin@vivo.corp-partner.google.com>
    Signed-off-by: Lianqin Hu <hulianqin@vivo.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -61,6 +61,7 @@
 #include <trace/hooks/signal.h>
 #include <trace/hooks/logbuf.h>
 #include <trace/hooks/dmabuf.h>
+#include <trace/hooks/usb.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -272,3 +273,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_new_ref);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_del_ref);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_logbuf_pr_cont);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_alloc_pages_slowpath);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_configfs_uevent_work);
