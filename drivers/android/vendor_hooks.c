a7c054744652463aa84a0ad78398aa198e899c4f
zhujingpeng <zhujingpeng@vivo.com>
2024-04-23 21:50:15 +0800

    ANDROID: vendor_hooks: add hooks in rwsem read trylock
    When the lock is owned by readers and there is no RWSEM_FLAG_HANDOFF set,
    we allow some specific new readers to acquire the lock immediately
    in this hook, event if there are some writer tasks in the wait_list.
    This features can optimize the priority inversion problem caused by
    rwsem and improve system responsiveness and
    performance.
    
    Bug: 336506800
    
    Change-Id: I7e8fded73579933d1f61faa9fb6e5f300ffd71bf
    Signed-off-by: zhujingpeng <zhujingpeng@vivo.com>
    [jstultz: rebased, resolved collision]
    Signed-off-by: John Stultz <jstultz@google.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -306,3 +306,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mm_direct_reclaim_exit);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mm_may_oom_exit);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_vmscan_kswapd_done);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_cpu_capacity_show);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rwsem_read_trylock_failed);
