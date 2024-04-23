c38bea02e657e38302b3eea294f922252c2bb52b
hulianqin <hulianqin@vivo.corp-partner.google.com>
2024-04-23 14:11:24 +0800

    ANDROID: usb: Add vendor hooks for USB enumeration.
    Get USB enumeration status (such as bringup time or upper card owner
    time).
    
    We just need to be able to get the enumeration status of USB in the
    kernel, because vivo charging needs the enumeration status information
    of USB to decide whether to switch to dp and dm signal to the charger.
    
    Bug: 333350371
    
    Change-Id: I04be36f82ec0295d99525f43da2217ac16ba6be8
    Signed-off-by: hulianqin <hulianqin@vivo.corp-partner.google.com>
    Signed-off-by: Lianqin Hu <hulianqin@vivo.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -61,6 +61,7 @@
 #include <trace/hooks/signal.h>
 #include <trace/hooks/logbuf.h>
 #include <trace/hooks/dmabuf.h>
+#include <trace/hooks/usb.h>
 #include <trace/hooks/compaction.h>
 
 /*
@@ -296,6 +297,7 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_record_rwsem_reader_owned);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_clear_rwsem_reader_owned);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_record_rwsem_writer_owned);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_clear_rwsem_writer_owned);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_configfs_uevent_work);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_compaction_exit);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_compaction_try_to_compact_exit);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mm_direct_reclaim_enter);
