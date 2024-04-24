8cbbcc8efd1cf56a8cdabba4ad6e974823fb5c14
Mostafa Saleh <smostafa@google.com>
2024-04-24 17:05:30 +0000

    ANDROID: KVM: arm64: iommu: Track MMIO correctly
    Although at the moment pKVM doesn’t support device assignment, the
    hypervisor donates the MMIO of the IOMMU to itself (and in some
    other cases also).
    So, we shouldn’t allow the host to map those pages in the IOMMU page
    tables, for that we check the leaf pte and it’s mapped in host (or
    default) mapping fails.
    We should also prevent the other cases where a page was already mapped
    and the hypervisor attempts to donate it, as MMIO pages are not
    currently refcounted, we taint them by marking them as shared, this is
    too restrictive at the moment which would be improved with more
    infrastructure to track MMIO pages that ideally comes with device
    assignment support.
    
    Bug: 277989609
    Bug: 278749606
    
    Change-Id: Ib1b7af57f0a858f6e5f0641664fe1f7fa670f908
    Signed-off-by: Mostafa Saleh <smostafa@google.com>

--- arch/arm64/kvm/hyp/nvhe/mem_protect.c
+++ arch/arm64/kvm/hyp/nvhe/mem_protect.c
@@ -2262,6 +2262,8 @@ static int __pkvm_host_use_dma_page(phys_addr_t phys_addr)
 	int ret;
 	struct hyp_page *p = hyp_phys_to_page(phys_addr);
 	enum pkvm_page_state state;
+	kvm_pte_t pte;
+	enum kvm_pgtable_prot prot;
 
 	hyp_assert_lock_held(&host_mmu.lock);
 
@@ -2269,14 +2271,21 @@ static int __pkvm_host_use_dma_page(phys_addr_t phys_addr)
 	 * Some differences between handling of RAM and device memory:
 	 * - The hyp vmemmap area for device memory is not backed by physical
 	 *   pages in the hyp page tables.
-	 * - Device memory is unmapped automatically under memory pressure
-	 *   (host_stage2_try()) and the ownership information would be
-	 *   discarded.
-	 * We don't need to deal with that at the moment, because the host
-	 * cannot share or donate device memory, only RAM.
+	 * - However, in some cases modules can donate MMIO, as they can't be
+	 *   refcounted, taint them by marking them as shared, and that will
+	 *   prevent any future donation, this is too restrictive at the moment,
+	 *   which can be improved with more infrastructure to track MMIO pages
+	 *   that ideally comes with device assignment support.
 	 */
-	if (!addr_is_memory(phys_addr))
-		return 0;
+	if (!addr_is_memory(phys_addr)) {
+		ret = kvm_pgtable_get_leaf(&host_mmu.pgt, phys_addr, &pte, NULL);
+		if (ret)
+			return ret;
+		if (pte && !kvm_pte_valid(pte))
+			return -EPERM;
+		prot = pkvm_mkstate(KVM_HOST_S2_DEFAULT_MMIO_PTE, PKVM_PAGE_SHARED_OWNED);
+		return host_stage2_idmap_locked(phys_addr, PAGE_SIZE, prot, false);
+	}
 
 	state = hyp_phys_to_page(phys_addr)->host_state;
 
