a0aaf363c6c7a3646279dc985757ecd955d14bb3
Carlos Llamas <cmllamas@google.com>
2024-04-25 14:32:06 +0000

    ANDROID: test reverts from mTHP mm-unstable
    *** DO-NOT-MERGE ***
    
    For testing purposes only
    
    Change-Id: I059a0931a66a1437e892b8f7e32a26bafa42d08e
    Signed-off-by: Carlos Llamas <cmllamas@google.com>

--- mm/huge_memory.c
+++ mm/huge_memory.c
@@ -524,52 +524,6 @@ static const struct kobj_type thpsize_ktype = {
 	.sysfs_ops = &kobj_sysfs_ops,
 };
 
-DEFINE_PER_CPU(struct mthp_stat, mthp_stats) = {{{0}}};
-
-static unsigned long sum_mthp_stat(int order, enum mthp_stat_item item)
-{
-	unsigned long sum = 0;
-	int cpu;
-
-	for_each_possible_cpu(cpu) {
-		struct mthp_stat *this = &per_cpu(mthp_stats, cpu);
-
-		sum += this->stats[order][item];
-	}
-
-	return sum;
-}
-
-#define DEFINE_MTHP_STAT_ATTR(_name, _index)				\
-static ssize_t _name##_show(struct kobject *kobj,			\
-			struct kobj_attribute *attr, char *buf)		\
-{									\
-	int order = to_thpsize(kobj)->order;				\
-									\
-	return sysfs_emit(buf, "%lu\n", sum_mthp_stat(order, _index));	\
-}									\
-static struct kobj_attribute _name##_attr = __ATTR_RO(_name)
-
-DEFINE_MTHP_STAT_ATTR(anon_fault_alloc, MTHP_STAT_ANON_FAULT_ALLOC);
-DEFINE_MTHP_STAT_ATTR(anon_fault_fallback, MTHP_STAT_ANON_FAULT_FALLBACK);
-DEFINE_MTHP_STAT_ATTR(anon_fault_fallback_charge, MTHP_STAT_ANON_FAULT_FALLBACK_CHARGE);
-DEFINE_MTHP_STAT_ATTR(anon_swpout, MTHP_STAT_ANON_SWPOUT);
-DEFINE_MTHP_STAT_ATTR(anon_swpout_fallback, MTHP_STAT_ANON_SWPOUT_FALLBACK);
-
-static struct attribute *stats_attrs[] = {
-	&anon_fault_alloc_attr.attr,
-	&anon_fault_fallback_attr.attr,
-	&anon_fault_fallback_charge_attr.attr,
-	&anon_swpout_attr.attr,
-	&anon_swpout_fallback_attr.attr,
-	NULL,
-};
-
-static struct attribute_group stats_attr_group = {
-	.name = "stats",
-	.attrs = stats_attrs,
-};
-
 static struct thpsize *thpsize_create(int order, struct kobject *parent)
 {
 	unsigned long size = (PAGE_SIZE << order) / SZ_1K;
@@ -588,12 +542,6 @@ static struct thpsize *thpsize_create(int order, struct kobject *parent)
 	}
 
 	ret = sysfs_create_group(&thpsize->kobj, &thpsize_attr_group);
-	if (ret) {
-		kobject_put(&thpsize->kobj);
-		return ERR_PTR(ret);
-	}
-
-	ret = sysfs_create_group(&thpsize->kobj, &stats_attr_group);
 	if (ret) {
 		kobject_put(&thpsize->kobj);
 		return ERR_PTR(ret);
@@ -893,8 +841,6 @@ static vm_fault_t __do_huge_pmd_anonymous_page(struct vm_fault *vmf,
 		folio_put(folio);
 		count_vm_event(THP_FAULT_FALLBACK);
 		count_vm_event(THP_FAULT_FALLBACK_CHARGE);
-		count_mthp_stat(HPAGE_PMD_ORDER, MTHP_STAT_ANON_FAULT_FALLBACK);
-		count_mthp_stat(HPAGE_PMD_ORDER, MTHP_STAT_ANON_FAULT_FALLBACK_CHARGE);
 		return VM_FAULT_FALLBACK;
 	}
 	folio_throttle_swaprate(folio, gfp);
@@ -944,7 +890,6 @@ static vm_fault_t __do_huge_pmd_anonymous_page(struct vm_fault *vmf,
 		mm_inc_nr_ptes(vma->vm_mm);
 		spin_unlock(vmf->ptl);
 		count_vm_event(THP_FAULT_ALLOC);
-		count_mthp_stat(HPAGE_PMD_ORDER, MTHP_STAT_ANON_FAULT_ALLOC);
 		count_memcg_event_mm(vma->vm_mm, THP_FAULT_ALLOC);
 	}
 
@@ -1065,7 +1010,6 @@ vm_fault_t do_huge_pmd_anonymous_page(struct vm_fault *vmf)
 	folio = vma_alloc_folio(gfp, HPAGE_PMD_ORDER, vma, haddr, true);
 	if (unlikely(!folio)) {
 		count_vm_event(THP_FAULT_FALLBACK);
-		count_mthp_stat(HPAGE_PMD_ORDER, MTHP_STAT_ANON_FAULT_FALLBACK);
 		return VM_FAULT_FALLBACK;
 	}
 	return __do_huge_pmd_anonymous_page(vmf, &folio->page, gfp);
@@ -1846,7 +1790,7 @@ bool madvise_free_huge_pmd(struct mmu_gather *tlb, struct vm_area_struct *vma,
 	 * If other processes are mapping this folio, we couldn't discard
 	 * the folio unless they all do MADV_FREE so let's skip the folio.
 	 */
-	if (folio_likely_mapped_shared(folio))
+	if (folio_estimated_sharers(folio) != 1)
 		goto out;
 
 	if (!folio_trylock(folio))
@@ -2964,6 +2908,9 @@ static void __split_huge_page(struct page *page, struct list_head *list,
 	if (nr_dropped)
 		shmem_uncharge(head->mapping->host, nr_dropped);
 	remap_page(folio, nr);
+
+	if (folio_test_swapcache(folio))
+		split_swap_cluster(folio->swap);
 
 	for (i = 0; i < nr; i++) {
 		struct page *subpage = head + i;
