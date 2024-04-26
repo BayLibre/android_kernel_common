12579c691ae31a5d8b6ff09edb624a441086c384
Sooyong Suk <s.suk@samsung.com>
2024-04-26 15:00:40 +0900

    ANDROID: madvise: add vendor hooks for swap_entry
    Change-Id: Idfce0e2bd13814eab2542aa9afd5a25ced011d12
    Signed-off-by: Sooyong Suk <s.suk@samsung.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -306,3 +306,5 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mm_direct_reclaim_exit);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mm_may_oom_exit);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_vmscan_kswapd_done);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_cpu_capacity_show);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_madvise_swapin_walk_pmd_entry);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_process_madvise);
