f44c8de5400af1497a67d12be9ee367daced224d
Zhaoyang Huang <zhaoyang.huang@unisoc.com>
2024-04-28 17:00:11 +0800

    ANDROID: Add vendor hook for accounting ioprio
    Introduce vh for accounting bio's ioprio in different blkcg
    
    Bug: 336170140
    Test: build pass
    
    Change-Id: Ie68bbc2b8a49375bda16cf4206798f8caaccde98
    Signed-off-by: Zhaoyang Huang <zhaoyang.huang@unisoc.com>

--- block/blk-core.c
+++ block/blk-core.c
@@ -832,6 +832,7 @@ static void bio_set_ioprio(struct bio *bio)
 	if (IOPRIO_PRIO_CLASS(bio->bi_ioprio) == IOPRIO_CLASS_NONE)
 		bio->bi_ioprio = get_current_ioprio();
 	blkcg_set_ioprio(bio);
+	trace_android_vh_bio_ioprio_acct(bio);
 }
 
 /**
