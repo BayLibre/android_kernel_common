b0b2e44b54163deba05665e85239ff245eed723b
Dezhi Huang <huangdezhi@hihonor.com>
2024-04-28 17:04:10 +0800

    ANDROID: vendor_hooks: add hook to optimize the madvise processing flow
    Add vendor_hook trace_android_vh_process_madvise_begin, allow vendor modules
    to mark the start of the madvise processing.
    Add vendor_hook trace_android_vh_process_madvise_iter, allow vendor modules
    to document the state during the iteration of madvise processing and allow us
    to intervene in the loop based on the state of the process.
    
    Bug: 337591184
    Change-Id: I8cca4818de19c5823e2821ce9b5b0903cbee3281
    Signed-off-by: Dezhi Huang <huangdezhi@hihonor.com>

--- mm/madvise.c
+++ mm/madvise.c
@@ -221,8 +221,8 @@ static int swapin_walk_pmd_entry(pmd_t *pmd, unsigned long start,
 
 		pte_unmap_unlock(ptep, ptl);
 		ptep = NULL;
-		trace_android_vh_madvise_swapin_walk_pmd_entry(entry);
 
+		trace_android_vh_madvise_swapin_walk_pmd_entry(entry);
 		page = read_swap_cache_async(entry, GFP_HIGHUSER_MOVABLE,
 					     vma, addr, &splug);
 		if (page)
@@ -1577,14 +1577,19 @@ SYSCALL_DEFINE5(process_madvise, int, pidfd, const struct iovec __user *, vec,
 	}
 
 	total_len = iov_iter_count(&iter);
+	trace_android_vh_process_madvise_begin(task, behavior);
 
 	while (iov_iter_count(&iter)) {
+		trace_android_vh_process_madvise_iter(task, behavior, &ret);
+		if (ret < 0)
+			break;
 		ret = do_madvise(mm, (unsigned long)iter_iov_addr(&iter),
 					iter_iov_len(&iter), behavior);
 		if (ret < 0)
 			break;
 		iov_iter_advance(&iter, iter_iov_len(&iter));
 	}
+	
 	trace_android_vh_process_madvise_return_error(behavior, ret, &return_error);
 	if (return_error)
 		goto release_mm;
