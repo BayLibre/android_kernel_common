2e20ca4632f6e4922ea892853fee96095e1bdd9c
Alice Ryhl <aliceryhl@google.com>
2024-04-29 12:55:47 +0000

    ANDROID: rust_binder: wrap RangeAllocator in enum
    To make it possible to have multiple range allocator implementations, we
    provide a wrapper type that will delegate to the appropriate
    implementation. For now, we do not provide more than one implementation.
    
    Bug: 337803115
    Change-Id: Ie9412e1eb188d20cc83476836b66045856ad0b17
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>

--- drivers/android/binder/range_alloc/mod.rs
+++ drivers/android/binder/range_alloc/mod.rs
@@ -2,6 +2,96 @@
 
 // Copyright (C) 2024 Google LLC.
 
-mod tree;
+use kernel::{page::PAGE_SIZE, prelude::*, seq_file::SeqFile, task::Pid};
 
-pub use self::tree::*;
+mod tree;
+use self::tree::TreeRangeAllocator;
+
+pub(crate) use self::tree::ReserveNewBox;
+
+/// Represents a range of pages that have just become completely free.
+#[derive(Copy, Clone)]
+pub(crate) struct FreedRange {
+    pub(crate) start_page_idx: usize,
+    pub(crate) end_page_idx: usize,
+}
+
+impl FreedRange {
+    fn interior_pages(offset: usize, size: usize) -> FreedRange {
+        FreedRange {
+            // Divide round up
+            start_page_idx: (offset + (PAGE_SIZE - 1)) / PAGE_SIZE,
+            // Divide round down
+            end_page_idx: (offset + size) / PAGE_SIZE,
+        }
+    }
+}
+
+pub(crate) struct RangeAllocator<T> {
+    inner: Impl<T>,
+}
+
+enum Impl<T> {
+    Tree(TreeRangeAllocator<T>),
+}
+
+impl<T> RangeAllocator<T> {
+    pub(crate) fn new(size: usize) -> Result<Self> {
+        Ok(Self {
+            inner: Impl::Tree(TreeRangeAllocator::new(size)?),
+        })
+    }
+
+    pub(crate) fn debug_print(&self, m: &mut SeqFile) -> Result<()> {
+        match &self.inner {
+            Impl::Tree(tree) => tree.debug_print(m),
+        }
+    }
+
+    /// Try to reserve a new buffer, using the provided allocation if necessary.
+    ///
+    /// Returns the offset of the new allocation, and whether oneway spam has been detected.
+    pub(crate) fn reserve_new(
+        &mut self,
+        size: usize,
+        is_oneway: bool,
+        pid: Pid,
+        alloc: ReserveNewBox<T>,
+    ) -> Result<(usize, bool)> {
+        match &mut self.inner {
+            Impl::Tree(tree) => tree.reserve_new(size, is_oneway, pid, alloc),
+        }
+    }
+
+    /// Deletes the allocations at `offset`.
+    pub(crate) fn reservation_abort(&mut self, offset: usize) -> Result<FreedRange> {
+        match &mut self.inner {
+            Impl::Tree(tree) => tree.reservation_abort(offset),
+        }
+    }
+
+    /// Called when an allocation is no longer in use by the kernel.
+    pub(crate) fn reservation_commit(&mut self, offset: usize, data: Option<T>) -> Result {
+        match &mut self.inner {
+            Impl::Tree(tree) => tree.reservation_commit(offset, data),
+        }
+    }
+
+    /// Called when the kernel starts using an allocation.
+    ///
+    /// Returns the size of the existing entry and the data associated with it.
+    pub(crate) fn reserve_existing(&mut self, offset: usize) -> Result<(usize, Option<T>)> {
+        match &mut self.inner {
+            Impl::Tree(tree) => tree.reserve_existing(offset),
+        }
+    }
+
+    /// Call the provided callback at every allocated region.
+    ///
+    /// This destroys the range allocator. Used only during shutdown.
+    pub(crate) fn take_for_each<F: Fn(usize, usize, Option<T>)>(&mut self, callback: F) {
+        match &mut self.inner {
+            Impl::Tree(tree) => tree.take_for_each(callback),
+        }
+    }
+}
