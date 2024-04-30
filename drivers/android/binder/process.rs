26fbb83e384ce3841439bc57f450d2e9bbf37827
Alice Ryhl <aliceryhl@google.com>
2024-04-30 12:58:53 +0000

    ANDROID: rust_binder: refactor reserve_new allocations
    Once we add multiple implementations of `reserve_new`, what allocations
    we need will differ depending on the situation:
    
    1. With the fast path implementation, we won't need any allocations.
    2. With the tree implementation, we will need a `ReserveNewBox`.
    3. When we get too many ranges to use the fast path, we must allocate
       several `ReserveNewBox` values to convert it into a the tree
       implementation.
    
    The API provided by this commit provides the infrastructure for that.
    The idea is that `reserve_new` can fail with a request to allocate
    memory, and the caller will then release the spinlock, make the
    allocations, and call `reserve_new` again. The API is designed so that
    callers of `reserve_new` do not need to know about the details of what
    allocations are needed. The caller just calls `make_alloc`, and it takes
    care of making the right allocations.
    
    We do not make any allocations before making the first call to
    `reserve_new`. That is suboptimal right now, since we only have the tree
    implementation. But once we add the fast path, this will stop being
    suboptimal.
    
    Bug: 337803115
    Change-Id: I98f56339aa5ab8ab722be3b3119ae860884b7b82
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>

--- drivers/android/binder/process.rs
+++ drivers/android/binder/process.rs
@@ -40,7 +40,7 @@ use crate::{
     error::{BinderError, BinderResult},
     node::{CouldNotDeliverCriticalIncrement, CritIncrWrapper, Node, NodeDeath, NodeRef},
     prio::{self, BinderPriority},
-    range_alloc::{self, RangeAllocator},
+    range_alloc::{RangeAllocator, ReserveNew, ReserveNewArgs},
     thread::{PushWorkRes, Thread},
     DArc, DLArc, DTRWrap, DeliverToRead,
 };
@@ -873,17 +873,33 @@ impl Process {
     ) -> BinderResult<Allocation> {
         use kernel::page::PAGE_SIZE;
 
-        let alloc = range_alloc::ReserveNewBox::try_new()?;
-        let mut inner = self.inner.lock();
-        let mapping = inner.mapping.as_mut().ok_or_else(BinderError::new_dead)?;
-        let (offset, oneway_spam_detected) = mapping
-            .alloc
-            .reserve_new(size, is_oneway, from_pid, alloc)?;
+        let mut reserve_new_args = ReserveNewArgs {
+            size,
+            is_oneway,
+            pid: from_pid,
+            ..ReserveNewArgs::default()
+        };
 
-        let addr = mapping.address + offset;
-        drop(inner);
+        let (new_alloc, addr) = loop {
+            let mut inner = self.inner.lock();
+            let mapping = inner.mapping.as_mut().ok_or_else(BinderError::new_dead)?;
+            let alloc_request = match mapping.alloc.reserve_new(reserve_new_args)? {
+                ReserveNew::Success(new_alloc) => break (new_alloc, mapping.address),
+                ReserveNew::NeedAlloc(request) => request,
+            };
+            drop(inner);
 
-        let res = Allocation::new(self.clone(), offset, size, addr, oneway_spam_detected);
+            // We need to allocate memory and then call `reserve_new` again.
+            reserve_new_args = alloc_request.make_alloc()?;
+        };
+
+        let res = Allocation::new(
+            self.clone(),
+            new_alloc.offset,
+            size,
+            addr + new_alloc.offset,
+            new_alloc.oneway_spam_detected,
+        );
 
         // This allocation will be marked as in use until the `Allocation` is used to free it.
         //
@@ -896,8 +912,8 @@ impl Process {
         // allocation can only be removed via the destructor of the `Allocation` object that we
         // currently own.
         match self.pages.use_range(
-            offset / PAGE_SIZE,
-            (offset + size + (PAGE_SIZE - 1)) / PAGE_SIZE,
+            new_alloc.offset / PAGE_SIZE,
+            (new_alloc.offset + size + (PAGE_SIZE - 1)) / PAGE_SIZE,
         ) {
             Ok(()) => {}
             Err(err) => {
