3ede1ef2e1c2d0b322a00cbffa6f1dbc9b7667f5
Keir Fraser <keirf@google.com>
2024-05-08 09:00:34 +0000

    ANDROID: KVM: arm64: Fix clearing pvm firmware on init failure
    When pKVM fails to initialise (for example, due to lack of IOMMU)
    then the pvm firmware should be cleared. This was not happening:
    Fix it.
    
    Bug: 278749606
    Bug: 328362894
    Change-Id: If768806d442337c83cac7f7681232a4476778d82
    Signed-off-by: Keir Fraser <keirf@google.com>

--- arch/arm64/kvm/pkvm.c
+++ arch/arm64/kvm/pkvm.c
@@ -423,8 +423,10 @@ static int __init finalize_pkvm(void)
 {
 	int ret;
 
-	if (!is_protected_kvm_enabled() || !is_kvm_arm_initialised())
+	if (!is_protected_kvm_enabled() || !is_kvm_arm_initialised()) {
+		pkvm_firmware_rmem_clear();
 		return 0;
+	}
 
 	/*
 	 * Modules can play an essential part in the pKVM protection. All of
@@ -532,10 +534,10 @@ static int __init pkvm_firmware_rmem_clear(void)
 	void *addr;
 	phys_addr_t size;
 
-	if (likely(!pkvm_firmware_mem) || is_protected_kvm_enabled())
+	if (likely(!pkvm_firmware_mem))
 		return 0;
 
-	kvm_info("Clearing unused pKVM firmware memory\n");
+	kvm_info("Clearing pKVM firmware memory\n");
 	size = pkvm_firmware_mem->size;
 	addr = memremap(pkvm_firmware_mem->base, size, MEMREMAP_WB);
 	if (!addr)
@@ -546,7 +548,6 @@ static int __init pkvm_firmware_rmem_clear(void)
 	memunmap(addr);
 	return 0;
 }
-device_initcall_sync(pkvm_firmware_rmem_clear);
 
 static int pkvm_vm_ioctl_set_fw_ipa(struct kvm *kvm, u64 ipa)
 {
