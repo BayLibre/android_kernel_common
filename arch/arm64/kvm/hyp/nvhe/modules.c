f2b6ac01bb192f0053d55ea9bf64b45f3573e79d
Mostafa Saleh <smostafa@google.com>
2024-05-10 22:36:28 +0000

    ANDROID: KVM: arm64: Add hyp_smp_processor_id to module ops
    Soon, SMMUv3 driver will implement a synchronization mechanism that
    relies on the current CPU number, so export it to modules.
    
    Bug: 357781595
    Bug: 384432312
    Change-Id: I4a0f41148290fe96cdbbbf211f2338d877c236a6
    Signed-off-by: Mostafa Saleh <smostafa@google.com>

--- arch/arm64/kvm/hyp/nvhe/modules.c
+++ arch/arm64/kvm/hyp/nvhe/modules.c
@@ -113,6 +113,11 @@ static void tracing_mod_hyp_printk(u8 fmt_id, u64 a, u64 b, u64 c, u64 d)
 #endif
 }
 
+static int __hyp_smp_processor_id(void)
+{
+	return hyp_smp_processor_id();
+}
+
 const struct pkvm_module_ops module_ops = {
 	.create_private_mapping = __pkvm_create_private_mapping,
 	.alloc_module_va = __pkvm_alloc_module_va,
@@ -168,6 +173,7 @@ const struct pkvm_module_ops module_ops = {
 	.iommu_snapshot_host_stage2 = kvm_iommu_snapshot_host_stage2,
 	.iommu_donate_pages_atomic = kvm_iommu_donate_pages_atomic,
 	.iommu_reclaim_pages_atomic = kvm_iommu_reclaim_pages_atomic,
+	.hyp_smp_processor_id = __hyp_smp_processor_id,
 };
 
 int __pkvm_init_module(void *module_init)
