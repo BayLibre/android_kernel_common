e7a04d46fb63b942cbf0d5c6c147829602f2b001
T.J. Mercier <tjmercier@google.com>
2024-05-17 10:05:23 -0700

    FROMLIST: cgroup: Fix /proc/cgroups count for v2
    The /proc/cgroups documentation says that the num_cgroups value is,
    "the number of control groups in this hierarchy using this controller."
    
    The value printed is simply the total number of cgroups in the hierarchy
    which is correct for v1, but not for the shared v2 hierarchy.
    
    Consider:
    controllers="cpuset cpu io memory hugetlb pids rdma misc"
    for c in $controllers
    do
      echo +$c > /sys/fs/cgroup/cgroup.subtree_control
      mkdir /sys/fs/cgroup/$c
      echo +$c > /sys/fs/cgroup/$c/cgroup.subtree_control
      for i in `seq 100`; do mkdir /sys/fs/cgroup/$c/$i; done
    done
    cat /proc/cgroups
    
    #subsys_name	hierarchy	num_cgroups	enabled
    cpuset	0	809	1
    cpu	0	809	1
    cpuacct	0	809	1
    blkio	0	809	1
    memory	0	809	1
    devices	0	809	1
    freezer	0	809	1
    net_cls	0	809	1
    perf_event	0	809	1
    net_prio	0	809	1
    hugetlb	0	809	1
    pids	0	809	1
    rdma	0	809	1
    misc	0	809	1
    debug	0	809	1
    
    A count of 809 is reported for each controller, but only 109 should be
    reported for most of them since each controller is enabled in only part
    of the hierarchy. (Note that io depends on memcg, so its count should be
    209.)
    
    The number of cgroups using a controller is an important metric since
    kernel memory is used for each cgroup, and some kernel operations scale
    with the number of cgroups for some controllers (memory, io). So users
    have an interest in minimizing/tracking the number of them.
    
    Link: https://lore.kernel.org/all/20240528163713.2024887-1-tjmercier@google.com/
    Bug: 293511579
    Change-Id: I1e6848fb501206cb5bc50e0aa6d82a65b2ce7bc1
    Signed-off-by: T.J. Mercier <tjmercier@google.com>

--- include/linux/cgroup-defs.h
+++ include/linux/cgroup-defs.h
@@ -556,6 +556,12 @@ struct cgroup_root {
 	/* Number of cgroups in the hierarchy, used only for /proc/cgroups */
 	atomic_t nr_cgrps;
 
+	/*
+	 * Number of cgroups using each controller. Includes online and zombies.
+	 * Used only for /proc/cgroups.
+	 */
+	atomic_t nr_css[CGROUP_SUBSYS_COUNT];
+
 	/* A list running through the active hierarchies */
 	struct list_head root_list;
 
