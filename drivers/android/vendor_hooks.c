da24a5898407f98f744c2214fb989128d2fe9332
Wang Jianzheng <11134417@vivo.corp-partner.google.com>
2024-05-23 20:44:35 +0800

    ANDROID: vendor_hooks: add vendor hooks for readahead bio
    Add vendor hook to f2fs_read_single_page, so we can modify some bio flags according to the readahead_control.
    
    Bug: 342340767
    
    Change-Id: I0665c4bdec945808860b73217cc0d128f49c2eb7
    Signed-off-by: Wang Jianzheng <11134417@vivo.corp-partner.google.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -66,6 +66,7 @@
 #include <trace/hooks/sd.h>
 #include <trace/hooks/user.h>
 #include <trace/hooks/fuse.h>
+#include <trace/hooks/blk.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -376,3 +377,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_alloc_uid);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_free_user);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_save_vmalloc_stack);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_show_stack_hash);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_f2fs_read_single_page);
