af64c7e3d84d913c967c48a70a05855db9e46ee0
Vincent Donnefort <vdonnefort@google.com>
2024-05-23 16:41:50 +0100

    ANDROID: KVM: arm64: PSCI mem_protect sve_state and hyp_vcpu
    hyp_vcpu and sve_state for protected VMs can contain sensitive data we
    want to protect against cold-boot type attacks. Protect them with PSCI
    mem_protect.
    
    Bug: 340244758
    Bug: 273748186
    Bug: 278749606
    Change-Id: Ieb64a57715af2b2db18481b4e5f2a779552cac8b
    Signed-off-by: Vincent Donnefort <vdonnefort@google.com>

--- arch/arm64/kvm/hyp/nvhe/pkvm.c
+++ arch/arm64/kvm/hyp/nvhe/pkvm.c
@@ -671,6 +671,7 @@ static int init_pkvm_hyp_vcpu_sve(struct pkvm_hyp_vcpu *hyp_vcpu, struct kvm_vcp
 			ret = hyp_alloc_errno();
 			goto err;
 		}
+		hyp_alloc_protect(sve_state);
 	} else {
 		ret = hyp_pin_shared_mem(sve_state, sve_state + sve_state_size);
 		if (ret)
@@ -938,6 +939,9 @@ int __pkvm_init_vcpu(pkvm_handle_t handle, struct kvm_vcpu *host_vcpu)
 
 	hyp_vm->vcpus[idx] = hyp_vcpu;
 	hyp_vm->nr_vcpus++;
+
+	if (pkvm_hyp_vcpu_is_protected(hyp_vcpu))
+		hyp_alloc_protect(hyp_vcpu);
 
 unlock_vcpus:
 	hyp_spin_unlock(&hyp_vm->vcpus_lock);
