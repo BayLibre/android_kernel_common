0a3627b5f9c4fc43e06f6d56faa2d5ee0a65b736
xiaofeng <xiaofeng5@xiaomi.com>
2024-05-29 20:26:03 +0800

    ANDROID: psi: Add vendor hooks for PSI tracing
    Add hooks to capture various per-zone memory stats when
    a trigger threshold is hit.
    In the stability Monkey4SystemLongTime test, the camera startup will
    request a large amount of memory in a short period of time,
    resulting in a temporary shortage of system memory. In addition,
    other processes also need to continuously request memory.In this
    case, it is easy for the system to have lmkd blocked and unable to
    kill the process in time, resulting in OOM.
    
    This hook can be used to detect when lmkd is in D state and the
    memory pressure meets the set conditions, then the kill process
    mechanism is triggered to avoid the occurrence of OOM. In the
    previous test, 1/20 machines had a problem at 74.49 hours,but after
    the modification, 48 machines were tested for 120 hours without
    any problems.
    
    Bug: 268290366
    Change-Id: Ibe39263ddb05ffc3fa63b5225497a90c6480c8d7
    Signed-off-by: Georgi Djakov <quic_c_gdjako@quicinc.com>
    Signed-off-by: xiaofeng <xiaofeng5@xiaomi.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -34,6 +34,7 @@
 #include <trace/hooks/remoteproc.h>
 #include <trace/hooks/timer.h>
 #include <trace/hooks/fpsimd.h>
+#include <trace/hooks/psi.h>
 #include <trace/hooks/signal.h>
 
 /*
@@ -88,3 +89,5 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rproc_recovery);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rproc_recovery_set);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_timer_calc_index);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_is_fpsimd_save);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_psi_event);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_psi_group);
