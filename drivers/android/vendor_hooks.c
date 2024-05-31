maltesermailo/android_kernel_pantah

ANDROID: vendor_hooks: add hook to set vma flags with setting anon-name
Add hook to recognize the anon-name and set flags for vma, so that we can distinguish different types of memory in the kernel.

Bug: 343124472
Change-Id: I548e6dd1b7eb49eebfa26436452bbfba8e99ea17
Signed-off-by: yan chang <changyan1@xiaomi.com>

drivers/android/vendor_hooks.c
@@ -28,6 +28,7 @@
 #include <trace/hooks/mmc.h>
 #include <trace/hooks/cgroup.h>
 #include <trace/hooks/sys.h>
+#include <trace/hooks/madvise.h>
 #include <trace/hooks/iommu.h>
 #include <trace/hooks/net.h>
 #include <trace/hooks/pm_domain.h>
@@ -143,6 +144,9 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ufs_compl_command);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_cgroup_set_task);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_cgroup_force_kthread_migration);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_syscall_prctl_finished);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_anon_vma_name_recog);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_restore_mm);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_update_vma_flags);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ufs_send_uic_command);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ufs_send_tm_command);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ufs_check_int_errors);

include/trace/hooks/sys.h
@@ -11,6 +11,15 @@ struct task_struct;
 DECLARE_HOOK(android_vh_syscall_prctl_finished,
 	TP_PROTO(int option, struct task_struct *task),
 	TP_ARGS(option, task));
+
+struct mm_struct;
+struct anon_vma_name;
+DECLARE_HOOK(android_vh_anon_vma_name_recog,
+	TP_PROTO(struct mm_struct *mm, struct anon_vma_name *anon_name),
+	TP_ARGS(mm, anon_name));
+DECLARE_HOOK(android_vh_restore_mm,
+	TP_PROTO(struct mm_struct *mm),
+	TP_ARGS(mm));
 #endif
 
 #include <trace/define_trace.h>

kernel/sys.c
@@ -2351,7 +2351,9 @@ static int prctl_set_vma(unsigned long opt, unsigned long addr,
 		}
 
 		mmap_write_lock(mm);
+		trace_android_vh_anon_vma_name_recog(mm, anon_name);
 		error = madvise_set_anon_name(mm, addr, size, anon_name);
+		trace_android_vh_restore_mm(mm);
 		mmap_write_unlock(mm);
 		anon_vma_name_put(anon_name);
 		break;

BayLibre/android_kernel_common

drivers/android/vendor_hooks.c
@@ -28,6 +28,7 @@
 #include <trace/hooks/mmc.h>
 #include <trace/hooks/cgroup.h>
 #include <trace/hooks/sys.h>
+#include <trace/hooks/madvise.h>
 #include <trace/hooks/iommu.h>
 #include <trace/hooks/net.h>
 #include <trace/hooks/pm_domain.h>
@@ -145,6 +146,9 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ufs_compl_command);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_cgroup_set_task);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_cgroup_force_kthread_migration);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_syscall_prctl_finished);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_anon_vma_name_recog);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_restore_mm_flags);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_update_vma_flags);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ufs_send_uic_command);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ufs_send_tm_command);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ufs_check_int_errors);

include/trace/hooks/sys.h
@@ -11,6 +11,15 @@ struct task_struct;
 DECLARE_HOOK(android_vh_syscall_prctl_finished,
 	TP_PROTO(int option, struct task_struct *task),
 	TP_ARGS(option, task));
+
+struct mm_struct;
+struct anon_vma_name;
+DECLARE_HOOK(android_vh_anon_vma_name_recog,
+	TP_PROTO(struct mm_struct *mm, struct anon_vma_name *anon_name),
+	TP_ARGS(mm, anon_name));
+DECLARE_HOOK(android_vh_restore_mm_flags,
+	TP_PROTO(struct mm_struct *mm),
+	TP_ARGS(mm));
 #endif
 
 #include <trace/define_trace.h>

kernel/sys.c
@@ -2351,7 +2351,9 @@ static int prctl_set_vma(unsigned long opt, unsigned long addr,
 		}
 
 		mmap_write_lock(mm);
+		trace_android_vh_anon_vma_name_recog(mm, anon_name);
 		error = madvise_set_anon_name(mm, addr, size, anon_name);
+		trace_android_vh_restore_mm_flags(mm);
 		mmap_write_unlock(mm);
 		anon_vma_name_put(anon_name);
 		break;