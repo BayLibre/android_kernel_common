609f415d47d0ce3fadf2afce3a4c05bc104a00e5
Kalesh Singh <kaleshsingh@google.com>
2024-06-04 22:06:30 -0700

    Revert "UPSTREAM: mm/rmap: introduce and use hugetlb_add_file_rmap()"
    This reverts commit 06084fb024638b4f15bb99441ef80e3f5d540cb5.
    
    Change-Id: Ibc1a3b442082aa174c1f37af9a0ac0ea528d2cc5
    Signed-off-by: Kalesh Singh <kaleshsingh@google.com>

--- mm/hugetlb.c
+++ mm/hugetlb.c
@@ -5134,7 +5134,7 @@ again:
 			 * sleep during the process.
 			 */
 			if (!folio_test_anon(pte_folio)) {
-				hugetlb_add_file_rmap(pte_folio);
+				page_dup_file_rmap(&pte_folio->page, true);
 			} else if (page_try_dup_anon_rmap(&pte_folio->page,
 							  true, src_vma)) {
 				pte_t src_pte_old = entry;
@@ -6004,7 +6004,7 @@ static vm_fault_t hugetlb_no_page(struct mm_struct *mm,
 	if (anon_rmap)
 		hugetlb_add_new_anon_rmap(folio, vma, haddr);
 	else
-		hugetlb_add_file_rmap(folio);
+		page_dup_file_rmap(&folio->page, true);
 	new_pte = make_huge_pte(vma, &folio->page, ((vma->vm_flags & VM_WRITE)
 				&& (vma->vm_flags & VM_SHARED)));
 	/*
@@ -6428,7 +6428,7 @@ int hugetlb_mfill_atomic_pte(pte_t *dst_pte,
 		goto out_release_unlock;
 
 	if (folio_in_pagecache)
-		hugetlb_add_file_rmap(folio);
+		page_dup_file_rmap(&folio->page, true);
 	else
 		hugetlb_add_new_anon_rmap(folio, dst_vma, dst_addr);
 
