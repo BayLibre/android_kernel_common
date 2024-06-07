dab8cd77fcfd0edb990a7b0e257638a8379c4553
Robin Hsu <robinhsu@google.com>
2024-06-07 15:38:00 +0000

    ANDROID: Pixel: Add _GPL suffix for 'EXPORT_SYMBOL'
    Pixel MM Metrics:
        Fix EXPORT_SYMBOL to EXPORT_SYMBOL_GPL for all used symbols
    
    Bug: 299190787
    Test: local build
    Change-Id: I005ccfa15cee8252bc51242460bbab9b7d0eb2ab
    Signed-off-by: Robin Hsu <robinhsu@google.com>

--- fs/seq_file.c
+++ fs/seq_file.c
@@ -667,7 +667,7 @@ void seq_putc(struct seq_file *m, char c)
 
 	m->buf[m->count++] = c;
 }
-EXPORT_SYMBOL(seq_putc);
+EXPORT_SYMBOL_GPL(seq_putc);
 
 void seq_puts(struct seq_file *m, const char *s)
 {
@@ -680,7 +680,7 @@ void seq_puts(struct seq_file *m, const char *s)
 	memcpy(m->buf + m->count, s, len);
 	m->count += len;
 }
-EXPORT_SYMBOL(seq_puts);
+EXPORT_SYMBOL_GPL(seq_puts);
 
 /**
  * seq_put_decimal_ull_width - A helper routine for putting decimal numbers
@@ -726,7 +726,7 @@ void seq_put_decimal_ull_width(struct seq_file *m, const char *delimiter,
 overflow:
 	seq_set_overflow(m);
 }
-EXPORT_SYMBOL(seq_put_decimal_ull_width);
+EXPORT_SYMBOL_GPL(seq_put_decimal_ull_width);
 
 void seq_put_decimal_ull(struct seq_file *m, const char *delimiter,
 			 unsigned long long num)
@@ -818,7 +818,7 @@ void seq_put_decimal_ll(struct seq_file *m, const char *delimiter, long long num
 overflow:
 	seq_set_overflow(m);
 }
-EXPORT_SYMBOL(seq_put_decimal_ll);
+EXPORT_SYMBOL_GPL(seq_put_decimal_ll);
 
 /**
  * seq_write - write arbitrary data to buffer
