6aef004ce33e7b4b8e78217de9c25131f3d0b864
Kalesh Singh <kaleshsingh@google.com>
2024-06-12 23:43:59 -0700

    ANDROID: ABI: THP shattering: the reverse of collapsing
    Change-Id: I7637124bb1ede775dba7b1d363d53256f337851f
    Signed-off-by: Kalesh Singh <kaleshsingh@google.com>

--- include/linux/mm_types.h
+++ include/linux/mm_types.h
@@ -358,8 +358,14 @@ struct folio {
 			atomic_t _nr_pages_mapped;
 			atomic_t _pincount;
 #ifdef CONFIG_64BIT
+			unsigned int __padding;
 			unsigned int _folio_nr_pages;
 #endif
+			union {
+				unsigned long _private_1;
+				unsigned long *_dst_ul;
+				struct page **_dst_pp;
+			};
 	/* private: the union with struct page is transitional */
 		};
 		struct page __page_1;
