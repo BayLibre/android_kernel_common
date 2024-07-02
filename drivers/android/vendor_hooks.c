135df80e61a4bc1d9bcaec4e299a94de309f778d
huzhanyuan <huzhanyuan@oppo.com>
2024-07-02 17:19:21 +0800

    ANDROID: vendor_hooks: Add hook for fsnotify
    Add hook for fsnotify when file open with write mode.
    
    - android_vh_fsnotify_open
    
    Bug: 350651631
    
    Change-Id: Ibeada7ce1d5a4a78c49b9a255b1e3a436029b631
    Signed-off-by: huzhanyuan  <huzhanyuan@oppo.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -77,6 +77,7 @@
 #include <trace/hooks/psi.h>
 #include <trace/hooks/blk.h>
 #include <trace/hooks/suspend.h>
+#include <trace/hooks/fsnotify.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -498,3 +499,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ogki_udp6_unicast_rcv_skb);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ogki_kmem_cache_create_usercopy);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_init_adjust_zone_wmark);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_do_group_exit);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_fsnotify_open);
