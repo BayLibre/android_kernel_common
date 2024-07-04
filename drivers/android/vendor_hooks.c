005f45905a321ce2e4d087ff6a1a48c7ad2899b5
Zhifeng Zhu <zhifeng.zhu@vivo.com>
2024-07-04 14:47:17 +0800

    ANDROID: vendor_hooks: add hooks in prctl_set_vma
    In some special scenarios, some threads are allowed to skip waiting,
    write the set_anon_name task into the queue and return directly, thus
    improving the system's response speed and performance.
    
    Bug: 349739224
    
    Change-Id: Id3686309da108e242a30cfaf68f859b10215f402
    Signed-off-by: Zhifeng Zhu <zhifeng.zhu@vivo.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -559,4 +559,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_resume_begin);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_resume_end);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_early_resume_begin);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_use_amu_fie);
-
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_pr_set_vma_name_bypass);
