1f8d306496e23b5e1533411753693a846096892b
Mostafa Saleh <smostafa@google.com>
2024-07-08 18:25:03 +0000

    ANDROID: KVM: arm64: devices: Add reset handler for devices
    Many devices must be reset before and after assignment, and as the
    host can't be trusted to do this, we have to do it from the
    hypervisor, and as we only deal with MMIO(platform) devices, which
    has not standard interface, we provide a function to register
    reset handler to modules, that can be registered with the MMIO base
    address of the device with pkvm_device_register_reset.
    
    Bug: 357781595
    Bug: 348382247
    Change-Id: Ie700aacf440caa6f1ffee85086a1edecdc104a5a
    Signed-off-by: Mostafa Saleh <smostafa@google.com>

--- arch/arm64/include/asm/kvm_pkvm_module.h
+++ arch/arm64/include/asm/kvm_pkvm_module.h
@@ -11,6 +11,7 @@ typedef void (*dyn_hcall_t)(struct user_pt_regs *);
 struct kvm_hyp_iommu;
 struct iommu_iotlb_gather;
 struct kvm_hyp_iommu_domain;
+struct pkvm_device;
 
 #ifdef CONFIG_MODULES
 enum pkvm_psci_notification {
@@ -169,6 +170,15 @@ enum pkvm_psci_notification {
  * @iommu_donate_pages_atomic:	Allocate memory from IOMMU identity pool.
  * @iommu_reclaim_pages_atomic:	Reclaim memory from iommu_donate_pages_atomic()
  * @hyp_smp_processor_id:	Current CPU id
+ * @device_register_reset:	Register a reset callback for devices that is called
+ * 				before/after devices are assigned where direction is
+ * 				passed in the callback host_to_guest. Only one callback
+ * 				can be registered per device.
+ * 				Devices are identified by the base address of the MMIO
+ * 				as defined in the device tree.
+ * 				Reset is expected to clear any state/secretes on the
+ * 				device and put it in quiescent state, where it can't
+ * 				trigger any DMA.
  */
 struct pkvm_module_ops {
 	int (*create_private_mapping)(phys_addr_t phys, size_t size,
@@ -235,6 +245,8 @@ struct pkvm_module_ops {
 	void * (*iommu_donate_pages_atomic)(u8 order);
 	void (*iommu_reclaim_pages_atomic)(void *p, u8 order);
 	int (*hyp_smp_processor_id)(void);
+	int (*device_register_reset)(u64 phys, int (*cb)(struct pkvm_device *dev,
+							 bool host_to_guest));
 	ANDROID_KABI_RESERVE(1);
 	ANDROID_KABI_RESERVE(2);
 	ANDROID_KABI_RESERVE(3);
--- arch/arm64/kvm/hyp/nvhe/device/device.c
+++ arch/arm64/kvm/hyp/nvhe/device/device.c
@@ -123,6 +123,17 @@ out_unlock:
 	return ret;
 }
 
+static int pkvm_device_reset(struct pkvm_device *dev, bool host_to_guest)
+{
+	hyp_assert_lock_held(&device_spinlock);
+
+	/* Reset in mandatory. */
+	if (!dev->reset_handler)
+		return -ENODEV;
+
+	return dev->reset_handler(dev, host_to_guest);
+}
+
 static int __pkvm_device_assign(struct pkvm_device *dev, struct pkvm_hyp_vm *vm)
 {
 	int i;
@@ -137,6 +148,10 @@ static int __pkvm_device_assign(struct pkvm_device *dev, struct pkvm_hyp_vm *vm)
 		if (ret)
 			return ret;
 	}
+
+	ret = pkvm_device_reset(dev, true);
+	if (ret)
+		return ret;
 
 	dev->ctxt = vm;
 	return 0;
@@ -306,6 +321,7 @@ void pkvm_devices_teardown(struct pkvm_hyp_vm *vm)
 
 		if (dev->ctxt != vm)
 			continue;
+		WARN_ON(pkvm_device_reset(dev, false));
 		dev->ctxt = NULL;
 		pkvm_devices_reclaim_device(dev);
 	}
@@ -359,4 +375,23 @@ void pkvm_devices_put_context(u64 iommu_id, u64 endpoint)
 	hyp_refcount_dec(dev->refcount);
 
 	hyp_spin_unlock(&device_spinlock);
+}
+
+int pkvm_device_register_reset(u64 phys, int (*cb)(struct pkvm_device *, bool))
+{
+	struct pkvm_device *dev;
+	int ret = 0;
+
+	dev = pkvm_get_device(phys);
+	if (!dev)
+		return -ENODEV;
+
+	hyp_spin_lock(&device_spinlock);
+	if (!dev->reset_handler)
+		dev->reset_handler = cb;
+	else
+		ret = -EBUSY;
+	hyp_spin_unlock(&device_spinlock);
+
+	return ret;
 }
