e02166fccdacd39430bd8c22367617c203ec7317
Mostafa Saleh <smostafa@google.com>
2024-07-08 18:25:03 +0000

    ANDROID: KVM: arm64: devices: Add reset handler for devices
    Many devices must be reset before and after assignment, and as the
    host can't be trusted to do this, we have to do it from the
    hypervisor, and as we only deal with MMIO(platform) devices, which
    has not standard interface, we provide a function to register
    reset handler to modules, that can be registered with the MMIO base
    address of the device with pkvm_device_register_reset.
    
    Bug: 357781595
    Bug: 348382247
    Change-Id: Ie700aacf440caa6f1ffee85086a1edecdc104a5a
    Signed-off-by: Mostafa Saleh <smostafa@google.com>

--- arch/arm64/kvm/hyp/include/nvhe/pkvm.h
+++ arch/arm64/kvm/hyp/include/nvhe/pkvm.h
@@ -190,5 +190,6 @@ int pkvm_init_devices(void);
 int pkvm_device_hyp_assign_mmio(u64 pfn, u64 nr_pages);
 int pkvm_device_reclaim_mmio(u64 pfn, u64 nr_pages);
 int pkvm_host_map_guest_mmio(struct pkvm_hyp_vcpu *hyp_vcpu, u64 pfn, u64 gfn);
+int pkvm_device_register_reset(u64 phys, int (*cb)(struct pkvm_device *dev));
 
 #endif /* __ARM64_KVM_NVHE_PKVM_H__ */
