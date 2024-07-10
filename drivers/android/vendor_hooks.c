e7ea7ff5d43edef37ee8dbe88b6500d64db8c1d3
Sooyong Suk <s.suk@samsung.corp-partner.google.com>
2024-07-10 11:20:55 +0900

    ANDROID: mm: add vendor hook in vmscan.c
    Add vendor hook to skip rebalance anon lru list.
    
    Bug: 351175506
    Change-Id: Ib4e56976d892b96f6c319a1b69d23d71ac7ce993
    Signed-off-by: Sooyong Suk <s.suk@samsung.corp-partner.google.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -237,3 +237,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_vfree_bypass);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_async_psi_bypass);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_smaps_pte_entry);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_show_smap);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rebalance_anon_lru_bypass);
