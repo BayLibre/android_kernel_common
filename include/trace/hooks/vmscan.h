bce7454aae54bfd537112f5e2d880107885e6e97
Sooyong Suk <s.suk@samsung.corp-partner.google.com>
2024-07-10 11:20:55 +0900

    ANDROID: mm: add vendor hook in vmscan.c
    Add vendor hook to skip rebalance anon lru list.
    
    Bug: 351175506
    Change-Id: Ib4e56976d892b96f6c319a1b69d23d71ac7ce993
    Signed-off-by: Sooyong Suk <s.suk@samsung.corp-partner.google.com>

--- include/trace/hooks/vmscan.h
+++ include/trace/hooks/vmscan.h
@@ -51,6 +51,9 @@ DECLARE_HOOK(android_vh_vmscan_kswapd_done,
 	TP_PROTO(int node_id, unsigned int highest_zoneidx, unsigned int alloc_order,
 		unsigned int reclaim_order),
 	TP_ARGS(node_id, highest_zoneidx, alloc_order, reclaim_order));
+DECLARE_HOOK(android_vh_rebalance_anon_lru_bypass,
+	TP_PROTO(bool *bypass),
+	TP_ARGS(bypass));
 #endif /* _TRACE_HOOK_VMSCAN_H */
 /* This part must be outside protection */
 #include <trace/define_trace.h>
