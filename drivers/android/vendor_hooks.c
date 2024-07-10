79d1c0d0a4e00d97393956ccb347f7b2199c62d0
Sooyong Suk <s.suk@samsung.corp-partner.google.com>
2024-07-10 14:31:36 +0900

    ANDROID: mm: add vendor hook for __alloc_pages_slowpath()
    Add vendor hooks in __alloc_pages_slowpath() to print debug logs on
    allocation delay.
    trace_android_vh_alloc_pages_slowpath() already exists but new vendor hooks
    are needed for more information.
    
    Bug: 351175506
    Change-Id: If04c64dc436b0874a06f1a0a96cde918e96c1bb2
    Signed-off-by: Sooyong Suk <s.suk@samsung.corp-partner.google.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -239,3 +239,5 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_smaps_pte_entry);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_show_smap);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_psi_update_triggers);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rebalance_anon_lru_bypass);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_alloc_pages_slowpath_start);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_alloc_pages_slowpath_end);
