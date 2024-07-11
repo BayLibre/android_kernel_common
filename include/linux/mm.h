f6d66e24e9fde16294afb648fc92cf4e8540f23e
Sooyong Suk <s.suk@samsung.corp-partner.google.com>
2024-07-11 17:33:35 +0900

    ANDROID: page_alloc: export symbol for vendor module
    Introduce export functions in page_alloc.c needed by vendor module.
      free_hpage() based on __free_pages_ok()
      prep_new_hpage() based on prep_new_page()
    
    Bug: 351175506
    Change-Id: Iab9eea265b2c6b10e8868e8dfa365f5f62d58dc1
    Signed-off-by: Sooyong Suk <s.suk@samsung.corp-partner.google.com>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -4244,4 +4244,9 @@ static inline void pgalloc_tag_copy(struct folio *new, struct folio *old)
 }
 #endif /* CONFIG_MEM_ALLOC_PROFILING */
 
+#ifdef CONFIG_TRANSPARENT_HUGEPAGE
+void free_hpage(struct page *page, int __bitwise fpi_flags);
+void prep_new_hpage(struct page *page, gfp_t gfp_flags, unsigned int alloc_flags);
+#endif
+
 #endif /* _LINUX_MM_H */
