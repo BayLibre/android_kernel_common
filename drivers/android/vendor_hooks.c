79b6ef3245086b55b246f34e78e36235cd55a7af
Sooyong Suk <s.suk@samsung.corp-partner.google.com>
2024-07-15 15:03:23 +0900

    ANDROID: madvise: add vendor hook to bypass process madvise
    We'd like to let custom vendor module does madvise instead and bypass
    original madvise.
    
    Bug: 351175506
    Change-Id: I93aef1b34f38b548c9be33c4b34f5d4eb00407dc
    Signed-off-by: Sooyong Suk <s.suk@samsung.corp-partner.google.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -61,6 +61,7 @@
 #include <trace/hooks/power.h>
 #include <trace/hooks/dmabuf.h>
 #include <trace/hooks/signal.h>
+#include <trace/hooks/madvise.h>
 #include <trace/events/android_vendor_lmk.h>
 
 
@@ -312,3 +313,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_warn_alloc_tune_ratelimit);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_warn_alloc_show_mem_bypass);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_use_vm_swappiness);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_tune_scan_control);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_process_madvise_bypass);
