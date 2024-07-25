a40a2e06ccae25f10db8114b79e9e25f0314e944
Sebastian Ene <sebastianene@google.com>
2024-07-25 10:56:35 +0000

    ANDROID: KVM: arm64: Handle guest FF-A share/lend/reclaim
    Handle the guest FF-A memory operations and validate the handle returned
    from Trustzone which identifies an FF-A transaction.
    Allocate memory to store this handle as part of the endpoint's structure.
    On reclaim, verify if the handle is part of a known memory transaction and
    remove the transaction if we got the memory back from Trustzone.
    The handle verification is required by the hypervisor to keep track of the
    transaction's ownership and to prevent handle impersonation and memory
    leaking.
    
    Bug: 269285339
    Bug: 278749606
    Change-Id: I854bc97b8bafe3fbaa3a98e2d2e9635f256e2913
    Signed-off-by: Sebastian Ene <sebastianene@google.com>

--- arch/arm64/kvm/hyp/include/nvhe/ffa.h
+++ arch/arm64/kvm/hyp/include/nvhe/ffa.h
@@ -16,8 +16,14 @@
 #define FFA_HANDLE_FROM_HYP_VCPU(hyp_vcpu)	((hyp_vcpu) == NULL ? 0 :\
 	((((hyp_vcpu)->vcpu).kvm->arch.pkvm.handle) - HANDLE_OFFSET + 1))
 
+struct ffa_mem_transfer {
+	struct list_head node;
+	u64 ffa_handle;
+};
+
 int hyp_ffa_init(void *pages);
 bool kvm_host_ffa_handler(struct kvm_cpu_context *host_ctxt, u32 func_id);
 bool kvm_guest_ffa_handler(struct pkvm_hyp_vcpu *hyp_vcpu, u64 *exit_code);
+struct ffa_mem_transfer *find_transfer_by_handle(u64 ffa_handle, struct kvm_ffa_buffers *buf);
 
 #endif /* __KVM_HYP_FFA_H */
