fd4524e922a09d69446348902b98be58cdbdc0c1
Sooyong Suk <s.suk@samsung.corp-partner.google.com>
2024-07-25 16:52:04 +0900

    ANDROID: mm: add vendor hook for khugepaged
    Add a vendor hook to bypass recommended min_free_bytes by khugepaged.
    
    Bug: 351175506
    Change-Id: Ic36f3ae01545bc8025a55864749a9fbb8aa06c52
    Signed-off-by: Sooyong Suk <s.suk@samsung.corp-partner.google.com>

--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -332,6 +332,9 @@ DECLARE_HOOK(android_vh_split_large_folio_bypass,
 DECLARE_HOOK(android_vh_cma_alloc_set_max_retries,
 	TP_PROTO(int *max_retries),
 	TP_ARGS(max_retries));
+DECLARE_HOOK(android_vh_set_recommended_min_free_kbytes_bypass,
+	TP_PROTO(bool *bypass),
+	TP_ARGS(bypass));
 #endif /* _TRACE_HOOK_MM_H */
 
 /* This part must be outside protection */
