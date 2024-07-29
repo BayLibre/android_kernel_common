4f88bf29b803f77bb19a9f178cdc57f736b65b51
Patrick Rohr <prohr@google.com>
2024-07-29 15:00:59 -0700

    BACKPORT: Add support for PIO p flag
    draft-ietf-6man-pio-pflag is adding a new flag to the Prefix Information
    Option to signal that the network can allocate a unique IPv6 prefix per
    client via DHCPv6-PD (see draft-ietf-v6ops-dhcp-pd-per-device).
    
    When ra_honor_pio_pflag is enabled, the presence of a P-flag causes
    SLAAC autoconfiguration to be disabled for that particular PIO.
    
    An automated test has been added in Android (r.android.com/3195335) to
    go along with this change.
    
    Cc: Maciej Żenczykowski <maze@google.com>
    Cc: Lorenzo Colitti <lorenzo@google.com>
    Cc: David Lamparter <equinox@opensourcerouting.org>
    Cc: Simon Horman <horms@kernel.org>
    Signed-off-by: Patrick Rohr <prohr@google.com>
    Reviewed-by: Maciej Żenczykowski <maze@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Link: https://lore.kernel.org/all/20240729220059.3018247-1-prohr@google.com/
    Bug: 315069348
    (cherry picked from commit 990c304930138dcd7a49763417e6e5313b81293e)
    Change-Id: I212cfbd558d265885ec63d3c97182bc012ea34e4
    Signed-off-by: Patrick Rohr <prohr@google.com>

--- net/ipv6/addrconf.c
+++ net/ipv6/addrconf.c
@@ -240,6 +240,17 @@ static struct ipv6_devconf ipv6_devconf __read_mostly = {
 	.addr_gen_mode		= IN6_ADDR_GEN_MODE_EUI64,
 	.disable_policy		= 0,
 	.rpl_seg_enabled	= 0,
+<<<<<<< HEAD   (05f8a4 UPSTREAM: binder: fix potential UAF of target_{proc,thread})
+||||||| BASE
+	.ioam6_enabled		= 0,
+	.ioam6_id               = IOAM6_DEFAULT_IF_ID,
+	.ioam6_id_wide		= IOAM6_DEFAULT_IF_ID_WIDE,
+=======
+	.ioam6_enabled		= 0,
+	.ioam6_id               = IOAM6_DEFAULT_IF_ID,
+	.ioam6_id_wide		= IOAM6_DEFAULT_IF_ID_WIDE,
+	.ra_honor_pio_pflag	= 0,
+>>>>>>> CHANGE (c25625 BACKPORT: Add support for PIO p flag)
 };
 
 static struct ipv6_devconf ipv6_devconf_dflt __read_mostly = {
@@ -297,6 +308,17 @@ static struct ipv6_devconf ipv6_devconf_dflt __read_mostly = {
 	.addr_gen_mode		= IN6_ADDR_GEN_MODE_EUI64,
 	.disable_policy		= 0,
 	.rpl_seg_enabled	= 0,
+<<<<<<< HEAD   (05f8a4 UPSTREAM: binder: fix potential UAF of target_{proc,thread})
+||||||| BASE
+	.ioam6_enabled		= 0,
+	.ioam6_id               = IOAM6_DEFAULT_IF_ID,
+	.ioam6_id_wide		= IOAM6_DEFAULT_IF_ID_WIDE,
+=======
+	.ioam6_enabled		= 0,
+	.ioam6_id               = IOAM6_DEFAULT_IF_ID,
+	.ioam6_id_wide		= IOAM6_DEFAULT_IF_ID_WIDE,
+	.ra_honor_pio_pflag	= 0,
+>>>>>>> CHANGE (c25625 BACKPORT: Add support for PIO p flag)
 };
 
 /* Check if link is ready: is it up and is a valid qdisc available */
@@ -2734,6 +2756,7 @@ void addrconf_prefix_rcv(struct net_device *dev, u8 *opt, int len, bool sllao)
 	u32 addr_flags = 0;
 	struct inet6_dev *in6_dev;
 	struct net *net = dev_net(dev);
+	bool ignore_autoconf = false;
 
 	pinfo = (struct prefix_info *) opt;
 
@@ -2827,7 +2850,8 @@ void addrconf_prefix_rcv(struct net_device *dev, u8 *opt, int len, bool sllao)
 
 	/* Try to figure out our local address for this prefix */
 
-	if (pinfo->autoconf && in6_dev->cnf.autoconf) {
+	ignore_autoconf = READ_ONCE(in6_dev->cnf.ra_honor_pio_pflag) && pinfo->preferpd;
+	if (pinfo->autoconf && in6_dev->cnf.autoconf && !ignore_autoconf) {
 		struct in6_addr addr;
 		bool tokenized = false, dev_addr_generated = false;
 
@@ -6792,6 +6816,15 @@ static const struct ctl_table addrconf_sysctl[] = {
 		.maxlen		= sizeof(int),
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec,
+	},
+	{
+		.procname	= "ra_honor_pio_pflag",
+		.data		= &ipv6_devconf.ra_honor_pio_pflag,
+		.maxlen		= sizeof(u8),
+		.mode		= 0644,
+		.proc_handler	= proc_dou8vec_minmax,
+		.extra1		= SYSCTL_ZERO,
+		.extra2		= SYSCTL_ONE,
 	},
 #ifdef CONFIG_IPV6_ROUTER_PREF
 	{
