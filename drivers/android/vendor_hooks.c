6933be22a7c99bdab29a2e1c48f8fcd22cebbd22
liujinbao1 <liujinbao1@xiaomi.corp-partner.google.com>
2024-07-31 11:20:06 +0800

    ANDROID: erofs: create vendor hooks for decompress
    the trace_android_vh_lz4_decompress_bypass is added in
    the function z_erofs_lz4_decompress_mem() to bypass native kernel LZ4_decompress in some conditons.
    
    Bug: 355072745
    
    Change-Id: Ia4ae8b08e320bc21c27b5619d7da22a14e95c4d6
    Signed-off-by: liujinbao1 <liujinbao1@xiaomi.corp-partner.google.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -33,6 +33,7 @@
 #include <trace/hooks/pm_domain.h>
 #include <trace/hooks/cpuidle_psci.h>
 #include <trace/hooks/vmscan.h>
+#include <trace/hooks/lz4_decompress.h>
 #include <trace/hooks/avc.h>
 #include <trace/hooks/creds.h>
 #include <trace/hooks/selinux.h>
@@ -190,6 +191,7 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_set_balance_anon_file_reclaim);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_show_max_freq);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_slab_alloc_node);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_slab_free);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_lz4_decompress_bypass);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_free_unref_page_bypass);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_kvmalloc_node_use_vmalloc);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_should_alloc_pages_retry);
