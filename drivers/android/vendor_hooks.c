maltesermailo/android_kernel_pantah

ANDROID: vendor_hooks: mm: add hook to skip swapoff
When using zram writeback, swapoff takes too long
during reboot. So make zram skip the swapoff.

Bug:
Change-Id: I07856b4b73a02e90e437567dfb43d33caf67cadb
Signed-off-by: Haonan Wang <haonan.wang@unisoc.com>

drivers/android/vendor_hooks.c
@@ -75,6 +75,7 @@
 #include <trace/hooks/suspend.h>
 #include <trace/hooks/fsnotify.h>
 #include <trace/hooks/perf.h>
+#include <trace/hooks/swapfile.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -463,3 +464,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_get_folio_trylock_result);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_folio_trylock_clear);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_folio_trylock_set);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_handle_trylock_failed_folio);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_swapoff);

include/trace/hooks/swapfile.h
@@ -1,0 +1,19 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#undef TRACE_SYSTEM
+#define TRACE_SYSTEM swapfile
+#undef TRACE_INCLUDE_PATH
+
+#define TRACE_INCLUDE_PATH trace/hooks
+
+#if !defined(_TRACE_HOOK_SWAPFILE_H) || defined(TRACE_HEADER_MULTI_READ)
+#define _TRACE_HOOK_SWAPFILE_H
+
+#include <trace/hooks/vendor_hooks.h>
+
+DECLARE_HOOK(android_vh_swapoff,
+	TP_PROTO(struct filename *pathname, bool *skip),
+	TP_ARGS(pathname, skip));
+
+#endif /* _TRACE_HOOK_SWAPFILE_H */
+/* This part must be outside protection */
+#include <trace/define_trace.h>

mm/swapfile.c
@@ -48,6 +48,7 @@
 #include <linux/swap_cgroup.h>
 #include "internal.h"
 #include "swap.h"
+#include <trace/hooks/swapfile.h>
 
 #define CLUSTER_FLAG_FREE	1 /* This cluster is free */
 #define CLUSTER_FLAG_NONFULL	2 /* This cluster on nonfull list  */
@@ -2417,6 +2418,7 @@ SYSCALL_DEFINE1(swapoff, const char __user *, specialfile)
 	struct filename *pathname;
 	int err, found = 0;
 	unsigned int old_block_size;
+	bool skip = false;
 
 	if (!capable(CAP_SYS_ADMIN))
 		return -EPERM;
@@ -2426,6 +2428,10 @@ SYSCALL_DEFINE1(swapoff, const char __user *, specialfile)
 	pathname = getname(specialfile);
 	if (IS_ERR(pathname))
 		return PTR_ERR(pathname);
+
+	trace_android_vh_swapoff(pathname, &skip);
+	if (skip)
+		return 0;
 
 	victim = file_open_name(pathname, O_RDWR|O_LARGEFILE, 0);
 	err = PTR_ERR(victim);

BayLibre/android_kernel_common

Bug: 356521473

drivers/android/vendor_hooks.c
@@ -75,6 +75,7 @@
 #include <trace/hooks/suspend.h>
 #include <trace/hooks/fsnotify.h>
 #include <trace/hooks/perf.h>
+#include <trace/hooks/swapfile.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -463,3 +464,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_get_folio_trylock_result);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_folio_trylock_clear);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_folio_trylock_set);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_handle_trylock_failed_folio);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_swapoff_skip);

include/trace/hooks/swapfile.h
@@ -1,0 +1,19 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#undef TRACE_SYSTEM
+#define TRACE_SYSTEM swapfile
+#undef TRACE_INCLUDE_PATH
+
+#define TRACE_INCLUDE_PATH trace/hooks
+
+#if !defined(_TRACE_HOOK_SWAPFILE_H) || defined(TRACE_HEADER_MULTI_READ)
+#define _TRACE_HOOK_SWAPFILE_H
+
+#include <trace/hooks/vendor_hooks.h>
+
+DECLARE_HOOK(android_vh_swapoff_skip,
+	TP_PROTO(struct filename *pathname, bool *skip),
+	TP_ARGS(pathname, skip));
+
+#endif /* _TRACE_HOOK_SWAPFILE_H */
+/* This part must be outside protection */
+#include <trace/define_trace.h>

mm/swapfile.c
@@ -48,6 +48,7 @@
 #include <linux/swap_cgroup.h>
 #include "internal.h"
 #include "swap.h"
+#include <trace/hooks/swapfile.h>
 
 #define CLUSTER_FLAG_FREE	1 /* This cluster is free */
 #define CLUSTER_FLAG_NONFULL	2 /* This cluster on nonfull list  */
@@ -2417,6 +2418,7 @@ SYSCALL_DEFINE1(swapoff, const char __user *, specialfile)
 	struct filename *pathname;
 	int err, found = 0;
 	unsigned int old_block_size;
+	bool skip = false;
 
 	if (!capable(CAP_SYS_ADMIN))
 		return -EPERM;
@@ -2426,6 +2428,10 @@ SYSCALL_DEFINE1(swapoff, const char __user *, specialfile)
 	pathname = getname(specialfile);
 	if (IS_ERR(pathname))
 		return PTR_ERR(pathname);
+
+	trace_android_vh_swapoff_skip(pathname, &skip);
+	if (skip)
+		return 0;
 
 	victim = file_open_name(pathname, O_RDWR|O_LARGEFILE, 0);
 	err = PTR_ERR(victim);