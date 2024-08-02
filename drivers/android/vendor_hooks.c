d0a86641b42089a1f4eb88099397b546a0375274
Sooyong Suk <s.suk@samsung.corp-partner.google.com>
2024-08-02 17:24:10 +0900

    ANDROID: mm: add vendor hook for file read
    Add a vendor hook to get file read information.
    
    Bug: 351175506
    Change-Id: Ie00ff34d48db7ef85520576af8986c95d841112e
    Signed-off-by: Sooyong Suk <s.suk@samsung.corp-partner.google.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -478,3 +478,5 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_dmabuf_page_pool_free_bypass);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_do_read_fault);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_filemap_read);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_filemap_map_pages);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_file_read_start);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_file_read_end);
