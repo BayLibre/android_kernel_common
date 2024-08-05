5919fa106f07e69afdaa3ad83005c4fc68b4f13b
Sooyong Suk <s.suk@samsung.corp-partner.google.com>
2024-08-05 17:59:11 +0900

    ANDROID: mm: add vendor hook in zs_shrinker
    Add vendor hook to adjust zs_shrinker frequency.
    
    Bug: 351175506
    Change-Id: I1bcdf2df605a08b57546e5e3e3303cb2f10cb043
    Signed-off-by: Sooyong Suk <s.suk@samsung.corp-partner.google.com>
    (cherry picked from commit 5d275a0324e6f2922c50d14334ee665ed10dacbe)

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -493,3 +493,5 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_do_group_exit);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_init_adjust_zone_wmark);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_shrink_node_memcgs);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_customize_thp_gfp_orders);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_zs_shrinker_adjust);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_zs_shrinker_bypass);
