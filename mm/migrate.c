e1177ff25cb6e10802044f84b8b061fbd8a10b5d
Qian-Hao Huang <qhhuang@google.com>
2024-08-13 00:13:32 +0800

    [TEST ONLY][DO NOT SUBMIT] Revert "mm/migrate: set swap entry values of THP tail pages properly."
    This reverts commit 3372c3ffc34509cfd2f66761a1f568b78daed04f.
    
    Bug: 352181300
    Change-Id: I3c24ebaed7341934bfcef480aab8da3cc6744c4d
    Signed-off-by: Qian-Hao Huang <qhhuang@google.com>

--- mm/migrate.c
+++ mm/migrate.c
@@ -428,12 +428,8 @@ int folio_migrate_mapping(struct address_space *mapping,
 	if (folio_test_swapbacked(folio)) {
 		__folio_set_swapbacked(newfolio);
 		if (folio_test_swapcache(folio)) {
-			int i;
-
 			folio_set_swapcache(newfolio);
-			for (i = 0; i < nr; i++)
-				set_page_private(folio_page(newfolio, i),
-					page_private(folio_page(folio, i)));
+			newfolio->private = folio_get_private(folio);
 		}
 		entries = nr;
 	} else {
