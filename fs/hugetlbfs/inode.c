d2bbef509aece5df0df34b34bbadff8a1d293075
Qian-Hao Huang <qhhuang@google.com>
2024-08-13 01:09:13 +0800

    [TEST ONLY][DO NOT SUBMIT] Revert "fs,hugetlb: fix NULL pointer dereference in hugetlbs_fill_super"
    This reverts commit 2e2c07104b4904aed1389a59b25799b95a85b5b9.
    
    Bug: 352181300
    Change-Id: Ifbd0eff86bcba84a6000ddf1165a8cf392ad7ae8
    Signed-off-by: Qian-Hao Huang <qhhuang@google.com>

--- fs/hugetlbfs/inode.c
+++ fs/hugetlbfs/inode.c
@@ -1350,7 +1350,6 @@ static int hugetlbfs_parse_param(struct fs_context *fc, struct fs_parameter *par
 {
 	struct hugetlbfs_fs_context *ctx = fc->fs_private;
 	struct fs_parse_result result;
-	struct hstate *h;
 	char *rest;
 	unsigned long ps;
 	int opt;
@@ -1395,12 +1394,11 @@ static int hugetlbfs_parse_param(struct fs_context *fc, struct fs_parameter *par
 
 	case Opt_pagesize:
 		ps = memparse(param->string, &rest);
-		h = size_to_hstate(ps);
-		if (!h) {
+		ctx->hstate = size_to_hstate(ps);
+		if (!ctx->hstate) {
 			pr_err("Unsupported page size %lu MB\n", ps / SZ_1M);
 			return -EINVAL;
 		}
-		ctx->hstate = h;
 		return 0;
 
 	case Opt_min_size:
