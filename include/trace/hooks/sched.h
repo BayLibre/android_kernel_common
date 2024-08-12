5916f457400089842bcc31cc963eff4413f38df0
Park Bumgyu <bumgyu.park@samsung.com>
2024-08-12 10:09:31 +0900

    ANDROID: sched: add vendor hook for task wakeup
    Task waker-wakee relation is very useful information at task scheduling,
    vendors might utilize it effectively. To be aware of task waker-wakee
    relation, vendor hook android_rvh_sched_wakeup is added.
    
    Change-Id: Idcb245fd012fe9487e9be7235a1426feffd3b183
    Signed-off-by: Park Bumgyu <bumgyu.park@samsung.com>

--- include/trace/hooks/sched.h
+++ include/trace/hooks/sched.h
@@ -476,6 +476,11 @@ DECLARE_HOOK(android_vh_prio_restore,
 	TP_PROTO(int saved_prio),
 	TP_ARGS(saved_prio));
 
+DECLARE_RESTRICTED_HOOK(android_rvh_sched_wakeup,
+	TP_PROTO(struct task_struct *p),
+	TP_ARGS(p),1);
+
+
 /* macro versions of hooks are no longer required */
 
 #endif /* _TRACE_HOOK_SCHED_H */
--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -3851,6 +3851,7 @@ static inline void ttwu_do_wakeup(struct task_struct *p)
 {
 	WRITE_ONCE(p->__state, TASK_RUNNING);
 	trace_sched_wakeup(p);
+	trace_android_rvh_sched_wakeup(p);
 }
 
 static void
