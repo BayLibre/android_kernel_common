torvalds/linux

commit b9578c49456340ca4d3c7ddbaca054ffc2b51bc1

dma-buf/heaps: Correct the types of fd_flags and heap_flags
dma_heap_allocation_data defines the UAPI as follows:

 struct dma_heap_allocation_data {
        __u64 len;
        __u32 fd;
        __u32 fd_flags;
        __u64 heap_flags;
 };

But dma heaps are casting both fd_flags and heap_flags into
unsigned long. This patch makes dma heaps - cma heap and
system heap have consistent types with UAPI.

Signed-off-by: Barry Song <v-songbaohua@oppo.com>
Acked-by: John Stultz <jstultz@google.com>
Reviewed-by: Carlos Llamas <cmllamas@google.com>
Signed-off-by: Sumit Semwal <sumit.semwal@linaro.org>
Link: https://patchwork.freedesktop.org/patch/msgid/20240606020213.49854-1-21cnbao@gmail.com

drivers/dma-buf/heaps/system_heap.c
@@ -333,8 +333,8 @@ static struct page *alloc_largest_available(unsigned long size,
 
 static struct dma_buf *system_heap_allocate(struct dma_heap *heap,
 					    unsigned long len,
-					    unsigned long fd_flags,
-					    unsigned long heap_flags)
+					    u32 fd_flags,
+					    u64 heap_flags)
 {
 	struct system_heap_buffer *buffer;
 	DEFINE_DMA_BUF_EXPORT_INFO(exp_info);

drivers/dma-buf/heaps/cma_heap.c
@@ -274,8 +274,8 @@ static const struct dma_buf_ops cma_heap_buf_ops = {
 
 static struct dma_buf *cma_heap_allocate(struct dma_heap *heap,
 					 unsigned long len,
-					 unsigned long fd_flags,
-					 unsigned long heap_flags)
+					 u32 fd_flags,
+					 u64 heap_flags)
 {
 	struct cma_heap *cma_heap = dma_heap_get_drvdata(heap);
 	struct cma_heap_buffer *buffer;

include/linux/dma-heap.h
@@ -23,8 +23,8 @@ struct dma_heap;
 struct dma_heap_ops {
 	struct dma_buf *(*allocate)(struct dma_heap *heap,
 				    unsigned long len,
-				    unsigned long fd_flags,
-				    unsigned long heap_flags);
+				    u32 fd_flags,
+				    u64 heap_flags);
 };
 
 /**

BayLibre/android_kernel_common

commit 9831ee9b72a1b320218252785136605930964606

ANDROID: dma-buf: Follow function parameter type change
Commit b9578c494563 ("dma-buf/heaps: Correct the types of fd_flags and
heap_flags") changed the expected types of a bunch of 'allocate' related
function calls.  In order to avoid build failures, we need to follow
them.

Fixes the following build errors:

  ...unsigned long, unsigned int, unsigned long long)') with an expression of type
    'struct dma_buf *(struct dma_heap *, unsigned long, unsigned long, unsigned long)' [-Wincompatible-function-pointer-types]
    520 |         .allocate = system_uncached_heap_not_initialized,
        |                     ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  ...*)(struct dma_heap *, unsigned long, unsigned int, unsigned long long)') from
    'struct dma_buf *(struct dma_heap *, unsigned long, unsigned long, unsigned long)' [-Wincompatible-function-pointer-types]
    545 |         system_uncached_heap_ops.allocate = system_uncached_heap_allocate;

Fixes: b9578c494563 ("dma-buf/heaps: Correct the types of fd_flags and heap_flags")
Change-Id: Ic349064cc67e20dcd97a58995cdee010508e8f0e
Signed-off-by: Lee Jones <joneslee@google.com>

drivers/dma-buf/heaps/system_heap.c
@@ -500,8 +500,8 @@ static const struct dma_heap_ops system_heap_ops = {
 
 static struct dma_buf *system_uncached_heap_allocate(struct dma_heap *heap,
 						     unsigned long len,
-						     unsigned long fd_flags,
-						     unsigned long heap_flags)
+						     u32 fd_flags,
+						     u64 heap_flags)
 {
 	return system_heap_do_allocate(heap, len, fd_flags, heap_flags, true);
 }
@@ -509,8 +509,8 @@ static struct dma_buf *system_uncached_heap_allocate(struct dma_heap *heap,
 /* Dummy function to be used until we can call coerce_mask_and_coherent */
 static struct dma_buf *system_uncached_heap_not_initialized(struct dma_heap *heap,
 							    unsigned long len,
-							    unsigned long fd_flags,
-							    unsigned long heap_flags)
+							    u32 fd_flags,
+							    u64 heap_flags)
 {
 	return ERR_PTR(-EBUSY);
 }