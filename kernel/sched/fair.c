b22331b29965bdd2d162bfb7842f9e88a040b54d
Qais Yousef <qyousef@layalina.io>
2024-08-20 17:35:07 +0100

    sched/qos: Add rampup multiplier QoS
    Bursty tasks are hard to predict. To use resources efficiently, the
    system would like to be exact as much as possible. But this poses
    a challenge for these bursty tasks that need to get access to more
    resources quickly.
    
    The new SCHED_QOS_RAMPUP_MULTIPLIER allows userspace to do that. As the
    name implies, it only helps them to transition to a higher performance
    state when they get _busier_. That is perfectly periodic tasks by
    definition are not going through a transition and will run at a constant
    performance level. It is the tasks that need to transition from one
    periodic state to another periodic state that is at a higher level that
    this rampup_multiplier will help with. It also slows down the ewma decay
    of util_est which should help those bursty tasks to keep their faster
    rampup.
    
    This should work complimentary with uclamp. uclamp tells the system
    about min and max perf requirements which can be applied immediately.
    
    rampup_multiplier is about reactiveness of the task to change.
    Specifically to a change for a higher performance level. The task might
    necessary need to have a min perf requirements, but it can have sudden
    burst of changes that require higher perf level and it needs the system
    to provide this faster.
    
    TODO: update the sched_qos docs
    
    Change-Id: Ifd726b830144b2a904342bf4aa0fec7428bfca55
    Signed-off-by: Qais Yousef <qyousef@layalina.io>
    Signed-off-by: John Stultz <jstultz@google.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5037,7 +5037,7 @@ static inline void util_est_update(struct cfs_rq *cfs_rq,
 
 			do_div(delta_exec, 1000);
 			ewma &= ~UTIL_AVG_UNCHANGED;
-			ewma = approximate_util_avg(ewma, delta_exec);
+			ewma = approximate_util_avg(ewma, delta_exec * p->sched_qos.rampup_multiplier);
 			goto done;
 		}
 		return;
@@ -5105,6 +5105,8 @@ static inline void util_est_update(struct cfs_rq *cfs_rq,
 	 * 0.25, thus making w=1/4 ( >>= UTIL_EST_WEIGHT_SHIFT)
 	 */
 	ewma <<= UTIL_EST_WEIGHT_SHIFT;
+	if (p->sched_qos.rampup_multiplier)
+		last_ewma_diff /= p->sched_qos.rampup_multiplier;
 	ewma  -= last_ewma_diff;
 	ewma >>= UTIL_EST_WEIGHT_SHIFT;
 done:
@@ -10116,7 +10118,7 @@ static void update_cpu_capacity(struct sched_domain *sd, int cpu)
 	 * on TICK doesn't end up hurting it as it can happen after we would
 	 * have crossed this threshold.
 	 *
-	 * To ensure that invaraince is taken into account, we don't scale time
+	 * To ensure that invariance is taken into account, we don't scale time
 	 * and use it as-is, approximate_util_avg() will then let us know the
 	 * our threshold.
 	 */
