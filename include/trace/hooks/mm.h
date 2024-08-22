dc9d5a103cf7b42652ead24aa566bd1236926b29
chunpeng li <lichunpeng@xiaomi.com>
2024-08-22 15:47:58 +0800

    ANDROID: vendor_hook: customize gfp and orders to alloc large folio
    This hook is used to dynamic modify:
    1.gfp_mask, which control behavior for allocation.
    2.orders, which control highest order to try alloc folio.
    3.order, which need assign `order = highest_order(orders);`
    
    Bug: 359422098
    Change-Id: Id2e89a2c674acb7bea6867cdb9f84163c266d7db
    Signed-off-by: chunpeng li <lichunpeng@xiaomi.com>
    (cherry picked from commit d70733e0bc02ec19a301898a0ff493e65e8ad567)

--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -338,12 +338,49 @@ DECLARE_HOOK(android_vh_split_large_folio_bypass,
 DECLARE_HOOK(android_vh_cma_alloc_set_max_retries,
 	TP_PROTO(int *max_retries),
 	TP_ARGS(max_retries));
+<<<<<<< HEAD   (09d40a ANDROID: GKI: update symbol list for xiaomi)
 DECLARE_HOOK(android_vh_do_group_exit,
 	TP_PROTO(struct task_struct *tsk),
 	TP_ARGS(tsk));
 DECLARE_HOOK(android_vh_init_adjust_zone_wmark,
 	TP_PROTO(struct zone *zone, u64 interval),
 	TP_ARGS(zone, interval));
+||||||| BASE
+DECLARE_HOOK(android_vh_compact_finished,
+	TP_PROTO(bool *abort_compact),
+	TP_ARGS(abort_compact));
+DECLARE_HOOK(android_vh_madvise_cold_or_pageout_abort,
+	TP_PROTO(struct vm_area_struct *vma, bool *abort_madvise),
+	TP_ARGS(vma, abort_madvise));
+DECLARE_HOOK(android_vh_zs_shrinker_adjust,
+	TP_PROTO(unsigned long *pages_to_free),
+	TP_ARGS(pages_to_free));
+DECLARE_HOOK(android_vh_zs_shrinker_bypass,
+	TP_PROTO(bool *bypass),
+	TP_ARGS(bypass));
+DECLARE_HOOK(android_vh_customize_thp_pcp_order,
+	TP_PROTO(unsigned int *order),
+	TP_ARGS(order));
+=======
+DECLARE_HOOK(android_vh_compact_finished,
+	TP_PROTO(bool *abort_compact),
+	TP_ARGS(abort_compact));
+DECLARE_HOOK(android_vh_madvise_cold_or_pageout_abort,
+	TP_PROTO(struct vm_area_struct *vma, bool *abort_madvise),
+	TP_ARGS(vma, abort_madvise));
+DECLARE_HOOK(android_vh_zs_shrinker_adjust,
+	TP_PROTO(unsigned long *pages_to_free),
+	TP_ARGS(pages_to_free));
+DECLARE_HOOK(android_vh_zs_shrinker_bypass,
+	TP_PROTO(bool *bypass),
+	TP_ARGS(bypass));
+DECLARE_HOOK(android_vh_customize_thp_pcp_order,
+	TP_PROTO(unsigned int *order),
+	TP_ARGS(order));
+DECLARE_HOOK(android_vh_customize_thp_gfp_orders,
+	TP_PROTO(gfp_t *gfp_mask, unsigned long *orders, int *order),
+	TP_ARGS(gfp_mask, orders, order));
+>>>>>>> CHANGE (d70733 ANDROID: vendor_hook: customize gfp and orders to alloc larg)
 #endif /* _TRACE_HOOK_MM_H */
 
 /* This part must be outside protection */
