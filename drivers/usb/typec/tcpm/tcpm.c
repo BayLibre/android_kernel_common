e7c4911f8f9290d09273ae92f535bf6f955925b2
Tudor Ambarus <tudordana@google.com>
2024-08-22 11:03:16 +0000

    [WIP, DO NOT MERGE] Revert "usb: typec: tpcm: Fix PORT_RESET behavior for self powered devices"
    This reverts commit 197331b27ac890d0209232d5f669830cd00e8918.
    
    possibly solves devices being recognized with adb.
    
    Change-Id: I2a6050353c9b51be6b116963ec2382842b159129
    Signed-off-by: Tudor Ambarus <tudordana@google.com>

--- drivers/usb/typec/tcpm/tcpm.c
+++ drivers/usb/typec/tcpm/tcpm.c
@@ -5631,11 +5631,7 @@ static void run_state_machine(struct tcpm_port *port)
 	case PORT_RESET:
 		tcpm_reset_port(port);
 		port->pd_events = 0;
-		if (port->self_powered)
-			tcpm_set_cc(port, TYPEC_CC_OPEN);
-		else
-			tcpm_set_cc(port, tcpm_default_state(port) == SNK_UNATTACHED ?
-				    TYPEC_CC_RD : tcpm_rp_cc(port));
+		tcpm_set_cc(port, TYPEC_CC_OPEN);
 		tcpm_set_state(port, PORT_RESET_WAIT_OFF,
 			       PD_T_ERROR_RECOVERY);
 		break;
