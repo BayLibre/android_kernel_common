43d0661476064b3e4ced79f6acbc46dcf6e1060d
Jiewen Wang <jiewen.wang@vivo.com>
2024-09-02 14:36:56 +0800

    ANDROID: vendor_hooks: Add vendor hooks in __swap_writepage
    Add hook in __swap_writepage for reclaim synchronous or asynchronous in different scenarios.
    
    Bug: 363882594
    Change-Id: I06604454fa3019850a499912f6def97525960c0f
    Signed-off-by: Jiewen Wang <jiewen.wang@vivo.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -385,3 +385,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_new_ref);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_del_ref);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_percpu_rwsem_wq_add);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_try_to_unmap_one);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_swap_writepage);
