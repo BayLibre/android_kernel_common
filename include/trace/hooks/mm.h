602d54c2446423d7534f119ee976c9aa67ad55fe
Sooyong Suk <s.suk@samsung.corp-partner.google.com>
2024-09-04 10:15:40 +0900

    ANDROID: mm: add vendor hook for compaction
    Add a vendor hook to skip certain page block for compaction target.
    
    Bug: 391128435
    Bug: 351175506
    
    please review this patch after
    https://android-review.googlesource.com/c/kernel/common/+/3458179
    https://android-review.googlesource.com/c/kernel/common/+/3455141
    
    Change-Id: I81de4e5cd2c6d042ccb17f79264fa08b13af3915
    Signed-off-by: Sooyong Suk <s.suk@samsung.corp-partner.google.com>

--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -76,6 +76,10 @@ DECLARE_HOOK(android_vh_watermark_ok,
 DECLARE_HOOK(android_vh_gfp_to_alloc_flags,
 		TP_PROTO(gfp_t gfp_mask, unsigned int order, unsigned int *alloc_flags),
 		TP_ARGS(gfp_mask, order, alloc_flags));
+DECLARE_HOOK(android_vh_migration_target_bypass,
+	TP_PROTO(struct page *page, bool *bypass),
+	TP_ARGS(page, bypass));
+
 
 struct page_vma_mapped_walk;
 DECLARE_HOOK(android_vh_slab_alloc_node,
