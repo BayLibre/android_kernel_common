b196a0af5be8de3bf3f7249d8c7aef4772753f9d
yipeng xiang <yipengxiang@honor.corp-partner.google.com>
2024-09-11 16:18:33 +0800

    ANDROID: mm: add vendor hook to abort scan
    Add vendor hook to determine whether kswapd scan should be aborted
    based on the order of scan_control. Avoid excessive kswapd reclaim
    and to identify the intent to reclaim a specific memcg and bypass
    the !root_reclaim(sc) condition.
    
    combine https://android-review.googlesource.com/c/kernel/common/+/3261680
    and https://android-review.googlesource.com/c/kernel/common/+/3258173
    
    Bug: 365902592
    Change-Id: Iafd31ba629db2a65afc19e48e22ec8ad0bbe4cf2
    Signed-off-by: yipeng xiang <yipengxiang@honor.corp-partner.google.com>
    (cherry picked from commit 9d48e18a60d5ce476168ecddbb9ec56d955ac64c)
    (cherry picked from commit 621fb7eb3c71abbc52c0ebe93636e47a92f9ef23)

--- include/trace/hooks/vmscan.h
+++ include/trace/hooks/vmscan.h
@@ -12,6 +12,32 @@
 DECLARE_RESTRICTED_HOOK(android_rvh_set_balance_anon_file_reclaim,
 			TP_PROTO(bool *balance_anon_file_reclaim),
 			TP_ARGS(balance_anon_file_reclaim), 1);
+<<<<<<< HEAD   (b68cddb455684dd92a6f86a1dc93f9395fc1adc0 ANDROID: sched: Export cpu_busy_with_softirqs)
+||||||| BASE   (5fa4c0d2c0fc53593c70acb5876e2208ea0eba24 ANDROID: mm/cma: add vendor_hook in cma_alloc for retries)
+DECLARE_HOOK(android_vh_check_folio_look_around_ref,
+	TP_PROTO(struct folio *folio, int *skip),
+	TP_ARGS(folio, skip));
+DECLARE_HOOK(android_vh_tune_swappiness,
+	TP_PROTO(int *swappiness),
+	TP_ARGS(swappiness));
+DECLARE_HOOK(android_vh_async_psi_bypass,
+	TP_PROTO(bool *bypass),
+	TP_ARGS(bypass));
+=======
+DECLARE_HOOK(android_vh_check_folio_look_around_ref,
+	TP_PROTO(struct folio *folio, int *skip),
+	TP_ARGS(folio, skip));
+DECLARE_HOOK(android_vh_tune_swappiness,
+	TP_PROTO(int *swappiness),
+	TP_ARGS(swappiness));
+DECLARE_HOOK(android_vh_async_psi_bypass,
+	TP_PROTO(bool *bypass),
+	TP_ARGS(bypass));
+DECLARE_HOOK(android_vh_mglru_should_abort_scan,
+	TP_PROTO(unsigned long nr_reclaimed, unsigned long nr_to_reclaim,
+	unsigned int order, bool *bypass),
+	TP_ARGS(nr_to_reclaim, nr_reclaimed, order, bypass));
+>>>>>>> CHANGE (621fb7eb3c71abbc52c0ebe93636e47a92f9ef23 ANDROID: mm: add vendor hook to abort scan)
 #endif /* _TRACE_HOOK_VMSCAN_H */
 /* This part must be outside protection */
 #include <trace/define_trace.h>
