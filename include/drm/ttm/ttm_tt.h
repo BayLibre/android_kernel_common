395aeb4c5090ddc7fcefecf1d03d1df286ed6bbc
Thomas Hellström <thomas.hellstrom@linux.intel.com>
2024-09-11 14:18:58 +0200

    UPSTREAM: drm/ttm: Move swapped objects off the manager's LRU list
    Resources of swapped objects remains on the TTM_PL_SYSTEM manager's
    LRU list, which is bad for the LRU walk efficiency.
    
    Rename the device-wide "pinned" list to "unevictable" and move
    also resources of swapped-out objects to that list.
    
    An alternative would be to create an "UNEVICTABLE" priority to
    be able to keep the pinned- and swapped objects on their
    respective manager's LRU without affecting the LRU walk efficiency.
    
    v2:
    - Remove a bogus WARN_ON (Christian König)
    - Update ttm_resource_[add|del] bulk move (Christian König)
    - Fix TTM KUNIT tests (Intel CI)
    v3:
    - Check for non-NULL bo->resource in ttm_bo_populate().
    v4:
    - Don't move to LRU tail during swapout until the resource
      is properly swapped or there was a swapout failure.
      (Intel Ci)
    - Add a newline after checkpatch check.
    v5:
    - Introduce ttm_resource_is_swapped() to avoid a corner-case where
      a newly created resource was considered swapped. (Intel CI)
    v6:
    - Move an assert.
    
    Cc: Christian König <christian.koenig@amd.com>
    Cc: Matthew Brost <matthew.brost@intel.com>
    Cc: <dri-devel@lists.freedesktop.org>
    Signed-off-by: Thomas Hellström <thomas.hellstrom@linux.intel.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20240911121859.85387-2-thomas.hellstrom@linux.intel.com
    
    BUG: 417548663
    TEST: This patch is part of series fixing 3DMark Steel Nomad benchmark
    on PTL
    Change-Id: I1a6030702fe30b36740da8d29aa6113eecc59ce9
    Signed-off-by: Arselan Alvi <arselan.alvi@intel.corp-partner.google.com>

--- include/drm/ttm/ttm_tt.h
+++ include/drm/ttm/ttm_tt.h
@@ -145,6 +145,11 @@ static inline bool ttm_tt_is_populated(struct ttm_tt *tt)
 	return tt->page_flags & TTM_TT_FLAG_PRIV_POPULATED;
 }
 
+static inline bool ttm_tt_is_swapped(const struct ttm_tt *tt)
+{
+	return tt->page_flags & TTM_TT_FLAG_SWAPPED;
+}
+
 /**
  * ttm_tt_is_swapped() - Whether the ttm_tt is swapped out or backed up
  * @tt: The struct ttm_tt.
