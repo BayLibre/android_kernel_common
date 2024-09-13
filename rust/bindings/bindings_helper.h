d52ca4cfec03b748a189660dc2fb395d19f18c48
Alice Ryhl <aliceryhl@google.com>
2024-09-13 13:33:45 +0000

    ANDROID: ashmem_rust: add mmap ioctl
    The new `shmem.rs` file is a Rust abstraction around shmem files. It's
    important that we modify the fops to prevent users from calling mmap
    again, as they could otherwise modify the protection bits on the memory
    region in a way that bypasses the ashmem protection mask.
    
    Change-Id: Ia656744f06c951027eeed3ae7f5ab0da1fcf74c8
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>

--- rust/bindings/bindings_helper.h
+++ rust/bindings/bindings_helper.h
@@ -31,6 +31,7 @@
 #include <linux/sched.h>
 #include <linux/security.h>
 #include <linux/seq_file.h>
+#include <linux/shmem_fs.h>
 #include <linux/shrinker.h>
 #include <linux/slab.h>
 #include <linux/task_work.h>
@@ -41,6 +42,7 @@
 #include <trace/hooks/rust_binder.h>
 #include <uapi/linux/android/binder.h>
 #include <uapi/linux/android/binderfs.h>
+#include <uapi/linux/falloc.h>
 #include <uapi/linux/sched/types.h>
 
 /* `bindgen` gets confused at certain things. */
@@ -57,4 +59,6 @@ const blk_features_t RUST_CONST_HELPER_BLK_FEAT_ROTATIONAL = BLK_FEAT_ROTATIONAL
 
 #ifdef CONFIG_ASHMEM_RUST
 #include "../../drivers/staging/android/ashmem.h"
+const size_t RUST_CONST_HELPER_ASHMEM_NAME_PREFIX_LEN = ASHMEM_NAME_PREFIX_LEN;
+const size_t RUST_CONST_HELPER_ASHMEM_FULL_NAME_LEN = ASHMEM_FULL_NAME_LEN;
 #endif
