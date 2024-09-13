e4ba2e19e4780231d819a0a7137e81a435d1604e
Alice Ryhl <aliceryhl@google.com>
2024-09-13 13:01:48 +0000

    ANDROID: ashmem_rust: add GET/SIZE_SIZE
    Add the second ioctl. Once we add `mmap`, we must add a check that the
    size does not change after `mmap` is called.
    
    Change-Id: I62719c7ba4a2d55b3f64f5fa8c59ecf451c0e70c
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>

--- drivers/staging/android/ashmem_rust.rs
+++ drivers/staging/android/ashmem_rust.rs
@@ -58,6 +58,7 @@ struct Ashmem {
 }
 
 struct AshmemInner {
+    size: usize,
     /// If set, then this holds the ashmem name without the dev/ashmem/ prefix. No zero terminator.
     name: Option<Vec<u8>>,
 }
@@ -71,6 +72,7 @@ impl MiscDevice for Ashmem {
             try_pin_init! {
                 Ashmem {
                     inner <- new_mutex!(AshmemInner {
+                        size: 0,
                         name: None,
                     }),
                 }
@@ -84,6 +86,8 @@ impl MiscDevice for Ashmem {
         match cmd {
             bindings::ASHMEM_SET_NAME => me.set_name(UserSlice::new(arg, size).reader()),
             bindings::ASHMEM_GET_NAME => me.get_name(UserSlice::new(arg, size).writer()),
+            bindings::ASHMEM_SET_SIZE => me.set_size(arg),
+            bindings::ASHMEM_GET_SIZE => me.get_size(),
             _ => Err(EINVAL),
         }
     }
@@ -116,5 +120,16 @@ impl Ashmem {
 
         writer.write_slice(&local_name[..let_with_nul])?;
         Ok(0)
+    }
+
+    fn set_size(&self, size: usize) -> Result<c_long> {
+        let mut asma = self.inner.lock();
+        // TODO: fail if `mmap` is already called
+        asma.size = size;
+        Ok(0)
+    }
+
+    fn get_size(&self) -> Result<c_long> {
+        Ok(self.inner.lock().size as c_long)
     }
 }
