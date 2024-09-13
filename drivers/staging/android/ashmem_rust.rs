c82b92b2d4a74f5cc1bd04117f76aad86da715c6
Alice Ryhl <aliceryhl@google.com>
2024-09-13 14:44:39 +0000

    ANDROID: ashmem_rust: add memory shrinker
    Unlike the C driver, we do not need to release the lock when calling
    `punch_hole`. This is because the global mutex is only held here and in
    the PIN/UNPIN ioctl. That is to say, we never take the lock while
    holding any fs locks anywhere, so calling into fs code with the lock
    held is okay.
    
    That said, not unlocking the mutex after each iteration means that the
    shrinker will keep going even if there are PIN/UNPIN ioctls waiting for
    the mutex, possibly for a long time. To avoid this, a counter is
    introduced to have the shrinker exit early when a PIN/UNPIN ioctl starts
    waiting for the mutex.
    
    The lru_count field is moved to an atomic global so that `count_objects`
    can read it without taking the lock. It's still only modified with the
    lock held, so using load/store instead of an atomic increment will not
    lead to race conditions.
    
    Bug: 370906207
    Change-Id: I7c181a9c6acd6278333f57feb72c8af7dce61049
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>

--- drivers/staging/android/ashmem_rust.rs
+++ drivers/staging/android/ashmem_rust.rs
@@ -10,7 +10,11 @@
 //! It is, in theory, a good memory allocator for low-memory devices, because it can discard shared
 //! memory units when under memory pressure.
 
-use core::{ffi::c_int, pin::Pin};
+use core::{
+    ffi::c_int,
+    pin::Pin,
+    sync::atomic::{AtomicUsize, Ordering},
+};
 use kernel::{
     bindings::{self, ASHMEM_GET_PIN_STATUS, ASHMEM_PIN, ASHMEM_UNPIN},
     c_str,
@@ -22,6 +26,7 @@ use kernel::{
     page::{page_align, PAGE_MASK, PAGE_SIZE},
     prelude::*,
     seq_file::{seq_print, SeqFile},
+    shrinker::{self, ShrinkerBuilder, ShrinkerRegistration},
     sync::{new_mutex, Mutex, UniqueArc},
     task::Task,
     uaccess::{UserSlice, UserSliceReader, UserSliceWriter},
@@ -50,6 +55,12 @@ fn read_implies_exec(task: &Task) -> bool {
     (personality & bindings::READ_IMPLIES_EXEC) != 0
 }
 
+static NUM_PIN_IOCTLS_WAITING: AtomicUsize = AtomicUsize::new(0);
+
+fn shrinker_should_stop() -> bool {
+    NUM_PIN_IOCTLS_WAITING.load(Ordering::Relaxed) > 0
+}
+
 module! {
     type: AshmemModule,
     name: "ashmem_rust",
@@ -60,6 +71,7 @@ module! {
 
 struct AshmemModule {
     _misc: Pin<Box<MiscDeviceRegistration<Ashmem>>>,
+    _shrinker: ShrinkerRegistration<Self>,
 }
 
 impl kernel::Module for AshmemModule {
@@ -71,6 +83,9 @@ impl kernel::Module for AshmemModule {
 
         pr_info!("Using Rust implementation.");
 
+        let mut shrinker = ShrinkerBuilder::new(c_str!("android-ashmem"))?;
+        shrinker.set_seeks(4 * shrinker::DEFAULT_SEEKS);
+
         Ok(Self {
             _misc: Box::pin_init(
                 MiscDeviceRegistration::register(MiscDeviceOptions {
@@ -78,6 +93,7 @@ impl kernel::Module for AshmemModule {
                 }),
                 GFP_KERNEL,
             )?,
+            _shrinker: shrinker.register(()),
         })
     }
 }
@@ -356,9 +372,13 @@ impl Ashmem {
             Some(UniqueArc::new_uninit(GFP_KERNEL)?)
         };
 
+        NUM_PIN_IOCTLS_WAITING.fetch_add(1, Ordering::Relaxed);
         let mut guard = AshmemGuard(ASHMEM_MUTEX.lock());
+        NUM_PIN_IOCTLS_WAITING.fetch_sub(1, Ordering::Relaxed);
+
         // C ashmem waits for in-flight shrinkers here using a separate mechanism, but we don't
         // release the lock when calling `punch_hole` in the shrinker, so we don't need to do that.
+
         let asma = &mut *self.inner.lock();
         let mut new_range = match asma.file.as_ref() {
             Some(file) => new_range.map(|alloc| NewRange { file, alloc }),
