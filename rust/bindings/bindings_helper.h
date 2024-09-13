7d5c7f90017c83b8d06f0f84a712d6421e72920c
Alice Ryhl <aliceryhl@google.com>
2024-09-13 13:12:05 +0000

    ANDROID: ashmem_rust: add GET/SET_PROT_MASK
    It's really important for ashmem's security model that once you give up
    protection bits, you can't get them back. Another important factor is
    that this will not affect existing mmaps. This means that if you mmap a
    writable ashmem fd, and then give up all of your permissions, then even
    if you accidentally send the fd to another process, that other process
    will not be able to access the contents.
    
    The mman.h header is for the PROT_* constants.
    
    Bug: 370906207
    Change-Id: I5046126ffd1c2ac99936eaeaee89087961092649
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>

--- rust/bindings/bindings_helper.h
+++ rust/bindings/bindings_helper.h
@@ -19,6 +19,7 @@
 #include <linux/jiffies.h>
 #include <linux/mdio.h>
 #include <linux/miscdevice.h>
+#include <linux/mman.h>
 #include <linux/phy.h>
 #include <linux/pid_namespace.h>
 #include <linux/poll.h>
