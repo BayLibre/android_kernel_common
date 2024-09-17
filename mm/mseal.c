df502d61a61d1f8d0b2b76ab853928178d8f5d76
Howard Chen <howardsoc@google.com>
2024-09-17 11:42:04 +0800

    Revert "ANDROID: 16K: Ensure mseal start and len are 16kB multiples"
    This reverts commit 1c1c29c60a1ed7edf550a56f6bc75ed078e8e9fc.
    
    Change-Id: Ie08f24a802397f75e75844e22e5ac82fb1563af2
    Signed-off-by: Howard Chen <howardsoc@google.com>

--- mm/mseal.c
+++ mm/mseal.c
@@ -12,7 +12,6 @@
 #include <linux/mm.h>
 #include <linux/mm_inline.h>
 #include <linux/mmu_context.h>
-#include <linux/page_size_compat.h>
 #include <linux/syscalls.h>
 #include <linux/sched.h>
 #include "internal.h"
@@ -292,10 +291,10 @@ static int do_mseal(unsigned long start, size_t len_in, unsigned long flags)
 		return ret;
 
 	start = untagged_addr(start);
-	if (!__PAGE_ALIGNED(start))
+	if (!PAGE_ALIGNED(start))
 		return -EINVAL;
 
-	len = __PAGE_ALIGN(len_in);
+	len = PAGE_ALIGN(len_in);
 	/* Check to see whether len was rounded up from small -ve to zero. */
 	if (len_in && !len)
 		return -EINVAL;
