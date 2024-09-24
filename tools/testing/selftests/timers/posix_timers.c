b565be8e224c68b74cce4e8744b3586f5e8b4f08
Matthias Maennich <maennich@google.com>
2024-09-24 12:57:56 +0000

    ANDROID: Disable check_sig_ign within kselftest_timers_posix_timers
    This test does not compile as of 6.12 due to a missing `pthread_cancel`
    implementation in the underlying sysroot. Disable for now.
    
    Bug: 369330443
    Bug: 367265496
    Change-Id: I49537b25eddbf8c2502d809be06bd028f8f1e421
    Signed-off-by: Matthias Maennich <maennich@google.com>

--- tools/testing/selftests/timers/posix_timers.c
+++ tools/testing/selftests/timers/posix_timers.c
@@ -250,6 +250,8 @@ static void *ignore_thread(void *arg)
 	return NULL;
 }
 
+// TODO: b/369330443 - broken due to missing pthread_cancel in sysroot
+#if 0
 static void check_sig_ign(int thread)
 {
 	struct tmrsig tsig = { };
@@ -335,6 +337,7 @@ static void check_sig_ign(int thread)
 				 "check_sig_ign SIGEV_THREAD_ID\n");
 	}
 }
+#endif
 
 static void check_rearm(void)
 {
@@ -621,8 +624,11 @@ int main(int argc, char **argv)
 	check_timer_create(CLOCK_PROCESS_CPUTIME_ID, "CLOCK_PROCESS_CPUTIME_ID");
 	check_timer_distribution();
 
+// TODO: b/369330443 - broken due to missing pthread_cancel in sysroot
+#if 0
 	check_sig_ign(0);
 	check_sig_ign(1);
+#endif
 	check_rearm();
 	check_delete();
 	check_sigev_none(CLOCK_MONOTONIC, "CLOCK_MONOTONIC");
