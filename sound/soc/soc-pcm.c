7ca76553df428c3d9e7f9fbd4624fd64c17e166b
liuqi <liuqi20328@gmail.com>
2024-09-24 18:36:23 +0800

    UPSTREAM: ASoC: soc-pcm: test refcount before triggering
    [ Upstream commit 848aedfdc6ba25ad5652797db9266007773e44dd ]
    
    On start/pause_release/resume, when more than one FE is connected to
    the same BE, it's possible that the trigger is sent more than
    once. This is not desirable, we only want to trigger a BE once, which
    is straightforward to implement with a refcount.
    
    For stop/pause/suspend, the problem is more complicated: the check
    implemented in snd_soc_dpcm_can_be_free_stop() may fail due to a
    conceptual deadlock when we trigger the BE before the FE. In this
    case, the FE states have not yet changed, so there are corner cases
    where the TRIGGER_STOP is never sent - the dual case of start where
    multiple triggers might be sent.
    
    This patch suggests an unconditional trigger in all cases, without
    checking the FE states, using a refcount protected by the BE PCM
    stream lock.
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Reviewed-by: Kai Vehmanen <kai.vehmanen@linux.intel.com>
    Reviewed-by: Bard Liao <yung-chuan.liao@linux.intel.com>
    Reviewed-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
    Link: https://lore.kernel.org/r/20211207173745.15850-6-pierre-louis.bossart@linux.intel.com
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Fixes: aa9ff6a4955f ("ASoC: soc-compress: Reposition and add pcm_mutex")
    
    Bug: 353646905
    Link: https://lore.kernel.org/r/20211207173745.15850-6-pierre-louis.bossart@linux.intel.com
    Change-Id: I37f2b702c2b81cd3024f41bcb04381ee38005ace
    
    Change-Id: I575b95b4371d31314ff7430e1c39c09e947f2a6f
    Signed-off-by: liuqi <liuqi20328@gmail.com>

--- sound/soc/soc-pcm.c
+++ sound/soc/soc-pcm.c
@@ -1614,7 +1614,7 @@ int dpcm_be_dai_startup(struct snd_soc_pcm_runtime *fe, int stream)
 			be->dpcm[stream].state = SND_SOC_DPCM_STATE_CLOSE;
 			goto unwind;
 		}
-
+		be->dpcm_be_start[stream] = 0;
 		be->dpcm[stream].state = SND_SOC_DPCM_STATE_OPEN;
 		count++;
 	}
@@ -2227,14 +2227,21 @@ int dpcm_be_dai_trigger(struct snd_soc_pcm_runtime *fe, int stream,
 
 		switch (cmd) {
 		case SNDRV_PCM_TRIGGER_START:
-			if ((be->dpcm[stream].state != SND_SOC_DPCM_STATE_PREPARE) &&
+			if (!be->dpcm_be_start[stream] &&
+			    (be->dpcm[stream].state != SND_SOC_DPCM_STATE_PREPARE) &&
 			    (be->dpcm[stream].state != SND_SOC_DPCM_STATE_STOP) &&
 			    (be->dpcm[stream].state != SND_SOC_DPCM_STATE_PAUSED))
 				goto next;
 
-			ret = dpcm_do_trigger(dpcm, be_substream, cmd);
-			if (ret)
+			be->dpcm_be_start[stream]++;
+			if (be->dpcm_be_start[stream] != 1)
 				goto next;
+
+			ret = dpcm_do_trigger(dpcm, be_substream, cmd);
+			if (ret) {
+				be->dpcm_be_start[stream]--;
+				goto next;
+			}
 
 			be->dpcm[stream].state = SND_SOC_DPCM_STATE_START;
 			break;
@@ -2242,9 +2249,15 @@ int dpcm_be_dai_trigger(struct snd_soc_pcm_runtime *fe, int stream,
 			if ((be->dpcm[stream].state != SND_SOC_DPCM_STATE_SUSPEND))
 				goto next;
 
-			ret = dpcm_do_trigger(dpcm, be_substream, cmd);
-			if (ret)
+			be->dpcm_be_start[stream]++;
+			if (be->dpcm_be_start[stream]!= 1)
 				goto next;
+
+			ret = dpcm_do_trigger(dpcm, be_substream, cmd);
+			if (ret) {
+				be->dpcm_be_start[stream]--;
+				goto next;
+			}
 
 			be->dpcm[stream].state = SND_SOC_DPCM_STATE_START;
 			break;
@@ -2252,9 +2265,15 @@ int dpcm_be_dai_trigger(struct snd_soc_pcm_runtime *fe, int stream,
 			if ((be->dpcm[stream].state != SND_SOC_DPCM_STATE_PAUSED))
 				goto next;
 
-			ret = dpcm_do_trigger(dpcm, be_substream, cmd);
-			if (ret)
+			be->dpcm_be_start[stream]++;
+			if (be->dpcm_be_start[stream] != 1)
 				goto next;
+
+			ret = dpcm_do_trigger(dpcm, be_substream, cmd);
+			if (ret) {
+				be->dpcm_be_start[stream]--;
+				goto next;
+			}
 
 			be->dpcm[stream].state = SND_SOC_DPCM_STATE_START;
 			break;
@@ -2263,12 +2282,18 @@ int dpcm_be_dai_trigger(struct snd_soc_pcm_runtime *fe, int stream,
 			    (be->dpcm[stream].state != SND_SOC_DPCM_STATE_PAUSED))
 				goto next;
 
-			if (!snd_soc_dpcm_can_be_free_stop(fe, be, stream))
+			if (be->dpcm[stream].state == SND_SOC_DPCM_STATE_START)
+				be->dpcm_be_start[stream]--;
+
+			if (be->dpcm_be_start[stream] != 0)
 				goto next;
 
 			ret = dpcm_do_trigger(dpcm, be_substream, cmd);
-			if (ret)
+			if (ret) {
+				if (be->dpcm[stream].state == SND_SOC_DPCM_STATE_START)
+					be->dpcm_be_start[stream]++;
 				goto next;
+			}
 
 			be->dpcm[stream].state = SND_SOC_DPCM_STATE_STOP;
 			break;
@@ -2276,12 +2301,15 @@ int dpcm_be_dai_trigger(struct snd_soc_pcm_runtime *fe, int stream,
 			if (be->dpcm[stream].state != SND_SOC_DPCM_STATE_START)
 				goto next;
 
-			if (!snd_soc_dpcm_can_be_free_stop(fe, be, stream))
+			be->dpcm_be_start[stream]--;
+			if (be->dpcm_be_start[stream] != 0)
 				goto next;
 
 			ret = dpcm_do_trigger(dpcm, be_substream, cmd);
-			if (ret)
+			if (ret) {
+				be->dpcm_be_start[stream]++;
 				goto next;
+			}
 
 			be->dpcm[stream].state = SND_SOC_DPCM_STATE_SUSPEND;
 			break;
@@ -2289,12 +2317,15 @@ int dpcm_be_dai_trigger(struct snd_soc_pcm_runtime *fe, int stream,
 			if (be->dpcm[stream].state != SND_SOC_DPCM_STATE_START)
 				goto next;
 
-			if (!snd_soc_dpcm_can_be_free_stop(fe, be, stream))
+			be->dpcm_be_start[stream]--;
+			if (be->dpcm_be_start[stream] != 0)
 				goto next;
 
 			ret = dpcm_do_trigger(dpcm, be_substream, cmd);
-			if (ret)
+			if (ret) {
+				be->dpcm_be_start[stream]++;
 				goto next;
+			}
 
 			be->dpcm[stream].state = SND_SOC_DPCM_STATE_PAUSED;
 			break;
