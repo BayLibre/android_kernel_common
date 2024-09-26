a4a1361fc3426346289c28413b4eef25d69e9301
Barry Song <v-songbaohua@oppo.com>
2024-09-27 09:19:36 +1200

    UPSTREAM: mm: avoid unconditional one-tick sleep when swapcache_prepare fails
    Commit 13ddaf26be32 ("mm/swap: fix race when skipping swapcache")
    introduced an unconditional one-tick sleep when `swapcache_prepare()`
    fails, which has led to reports of UI stuttering on latency-sensitive
    Android devices.  To address this, we can use a waitqueue to wake up tasks
    that fail `swapcache_prepare()` sooner, instead of always sleeping for a
    full tick.  While tasks may occasionally be woken by an unrelated
    `do_swap_page()`, this method is preferable to two scenarios: rapid
    re-entry into page faults, which can cause livelocks, and multiple
    millisecond sleeps, which visibly degrade user experience.
    
    Oven's testing shows that a single waitqueue resolves the UI stuttering
    issue.  If a 'thundering herd' problem becomes apparent later, a waitqueue
    hash similar to `folio_wait_table[PAGE_WAIT_TABLE_SIZE]` for page bit
    locks can be introduced.
    
    [v-songbaohua@oppo.com: wake_up only when swapcache_wq waitqueue is active]
      Link: https://lkml.kernel.org/r/20241008130807.40833-1-21cnbao@gmail.com
    Link: https://lkml.kernel.org/r/20240926211936.75373-1-21cnbao@gmail.com
    Fixes: 13ddaf26be32 ("mm/swap: fix race when skipping swapcache")
    Change-Id: I6cd3d6ef318d660ee6290554b5e864d90a70b920
    Signed-off-by: Barry Song <v-songbaohua@oppo.com>
    Reported-by: Oven Liyang <liyangouwen1@oppo.com>
    Tested-by: Oven Liyang <liyangouwen1@oppo.com>
    Cc: Kairui Song <kasong@tencent.com>
    Cc: "Huang, Ying" <ying.huang@intel.com>
    Cc: Yu Zhao <yuzhao@google.com>
    Cc: David Hildenbrand <david@redhat.com>
    Cc: Chris Li <chrisl@kernel.org>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Matthew Wilcox (Oracle) <willy@infradead.org>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Minchan Kim <minchan@kernel.org>
    Cc: Yosry Ahmed <yosryahmed@google.com>
    Cc: SeongJae Park <sj@kernel.org>
    Cc: Kalesh Singh <kaleshsingh@google.com>
    Cc: Suren Baghdasaryan <surenb@google.com>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    (cherry picked from commit 01626a18230246efdcea322aa8f067e60ffe5ccd)
    Bug: 313807618
    Signed-off-by: Kalesh Singh <kaleshsingh@google.com>
    (cherry picked from commit 001d8a58c0f43a84c9e8df34d72c1f0319396cec)
    Signed-off-by: Yuanchao Xia <xiayuanchao@vivo.corp-partner.google.com>

--- mm/memory.c
+++ mm/memory.c
@@ -3951,6 +3951,8 @@ static vm_fault_t handle_pte_marker(struct vm_fault *vmf)
 	return VM_FAULT_SIGBUS;
 }
 
+static DECLARE_WAIT_QUEUE_HEAD(swapcache_wq);
+
 /*
  * We enter with non-exclusive mmap_lock (to exclude vma changes,
  * but allow concurrent faults), and pte mapped but not yet locked.
@@ -3963,6 +3965,7 @@ vm_fault_t do_swap_page(struct vm_fault *vmf)
 {
 	struct vm_area_struct *vma = vmf->vma;
 	struct folio *swapcache, *folio = NULL;
+	DECLARE_WAITQUEUE(wait, current);
 	struct page *page;
 	struct swap_info_struct *si = NULL;
 	rmap_t rmap_flags = RMAP_NONE;
@@ -4048,7 +4051,9 @@ vm_fault_t do_swap_page(struct vm_fault *vmf)
 			 */
 			if (swapcache_prepare(entry)) {
 				/* Relax a bit to prevent rapid repeated page faults */
+				add_wait_queue(&swapcache_wq, &wait);
 				schedule_timeout_uninterruptible(1);
+				remove_wait_queue(&swapcache_wq, &wait);
 				goto out;
 			}
 			need_clear_cache = true;
@@ -4065,6 +4070,25 @@ vm_fault_t do_swap_page(struct vm_fault *vmf)
 							vma->vm_mm, GFP_KERNEL,
 							entry)) {
 					ret = VM_FAULT_OOM;
+				nr_pages = folio_nr_pages(folio);
+				if (folio_test_large(folio))
+					entry.val = ALIGN_DOWN(entry.val, nr_pages);
+				/*
+				 * Prevent parallel swapin from proceeding with
+				 * the cache flag. Otherwise, another thread
+				 * may finish swapin first, free the entry, and
+				 * swapout reusing the same entry. It's
+				 * undetectable as pte_same() returns true due
+				 * to entry reuse.
+				 */
+				if (swapcache_prepare(entry, nr_pages)) {
+					/*
+					 * Relax a bit to prevent rapid
+					 * repeated page faults.
+					 */
+					add_wait_queue(&swapcache_wq, &wait);
+					schedule_timeout_uninterruptible(1);
+					remove_wait_queue(&swapcache_wq, &wait);
 					goto out_page;
 				}
 				mem_cgroup_swapin_uncharge_swap(entry);
@@ -4353,8 +4377,11 @@ unlock:
 		pte_unmap_unlock(vmf->pte, vmf->ptl);
 out:
 	/* Clear the swap cache pin for direct swapin after PTL unlock */
-	if (need_clear_cache)
-		swapcache_clear(si, entry);
+	if (need_clear_cache) {
+		swapcache_clear(si, entry, nr_pages);
+		if (waitqueue_active(&swapcache_wq))
+			wake_up(&swapcache_wq);
+	}
 	if (si)
 		put_swap_device(si);
 	return ret;
@@ -4369,8 +4396,11 @@ out_release:
 		folio_unlock(swapcache);
 		folio_put(swapcache);
 	}
-	if (need_clear_cache)
-		swapcache_clear(si, entry);
+	if (need_clear_cache) {
+		swapcache_clear(si, entry, nr_pages);
+		if (waitqueue_active(&swapcache_wq))
+			wake_up(&swapcache_wq);
+	}
 	if (si)
 		put_swap_device(si);
 	return ret;
