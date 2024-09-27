e0432701d9516dfabe4eff76ee29c5cf6f37c155
Sooyong Suk <s.suk@samsung.corp-partner.google.com>
2024-09-27 14:03:34 +0900

    ANDROID: mm: add vendor hook for direct reclaim
    We'd like to adjust cpu priority for some processes with lower priority
    during direct reclaim.
    
    Bug: 351175506
    Change-Id: Icdb613e99c8455f705d330aa90f65c908d43a6e3
    Signed-off-by: Sooyong Suk <s.suk@samsung.corp-partner.google.com>
    (cherry picked from commit 6508004f9ff26ee9949403b3dc0dd4ea27ba06f5)

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -440,3 +440,5 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_vmscan_kswapd_wake);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_vmscan_kswapd_done);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_customize_alloc_gfp);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mm_kcompactd_cpu_online);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_direct_reclaim_begin);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_direct_reclaim_end);
