ed2d5f40c2578600d8e35ac0cd9774da37ece165
yipeng xiang <yipengxiang@honor.corp-partner.google.com>
2024-09-27 15:53:28 +0800

    ANDROID: mm: add vendor hook to skip memcg reclaim by priority
    Add vendor hook to determine whether a memcg should be recalimed.
    Avoid import memcg file pages are frequently reclaimed.
    
    Bug: 369921205
    Change-Id: Iac4abcf3a59a9c26ef9895a8f0d35aa6b6c26776
    Signed-off-by: yipeng xiang <yipengxiang@honor.corp-partner.google.com>
    (cherry picked from commit 29fd5366b14faf617b4d99c25d04e7f325226396)
    (cherry picked from commit c9bbf5834a77bfd1089db26a1f581b8de26f2236)

--- include/trace/hooks/vmscan.h
+++ include/trace/hooks/vmscan.h
@@ -12,6 +12,39 @@
 DECLARE_RESTRICTED_HOOK(android_rvh_set_balance_anon_file_reclaim,
 			TP_PROTO(bool *balance_anon_file_reclaim),
 			TP_ARGS(balance_anon_file_reclaim), 1);
+<<<<<<< HEAD   (1e3041af01622a8956e6130548f6dfb66e10871c Reapply "FROMGIT: memfd,selinux: call security_inode_init_se)
+||||||| BASE   (51fe955bf6dfda4550402c3f4335814f77b4e297 ANDROID: GKI: Add hooks for TCP parameter management.)
+DECLARE_HOOK(android_vh_check_folio_look_around_ref,
+	TP_PROTO(struct folio *folio, int *skip),
+	TP_ARGS(folio, skip));
+DECLARE_HOOK(android_vh_tune_swappiness,
+	TP_PROTO(int *swappiness),
+	TP_ARGS(swappiness));
+DECLARE_HOOK(android_vh_async_psi_bypass,
+	TP_PROTO(bool *bypass),
+	TP_ARGS(bypass));
+DECLARE_HOOK(android_vh_mglru_should_abort_scan,
+	TP_PROTO(unsigned long nr_reclaimed, unsigned long nr_to_reclaim,
+	unsigned int order, bool *bypass),
+	TP_ARGS(nr_to_reclaim, nr_reclaimed, order, bypass));
+=======
+DECLARE_HOOK(android_vh_check_folio_look_around_ref,
+	TP_PROTO(struct folio *folio, int *skip),
+	TP_ARGS(folio, skip));
+DECLARE_HOOK(android_vh_tune_swappiness,
+	TP_PROTO(int *swappiness),
+	TP_ARGS(swappiness));
+DECLARE_HOOK(android_vh_async_psi_bypass,
+	TP_PROTO(bool *bypass),
+	TP_ARGS(bypass));
+DECLARE_HOOK(android_vh_mglru_should_abort_scan,
+	TP_PROTO(unsigned long nr_reclaimed, unsigned long nr_to_reclaim,
+	unsigned int order, bool *bypass),
+	TP_ARGS(nr_to_reclaim, nr_reclaimed, order, bypass));
+DECLARE_HOOK(android_vh_should_memcg_bypass,
+	TP_PROTO(struct mem_cgroup *memcg, int priority, bool *bypass),
+	TP_ARGS(memcg, priority, bypass));
+>>>>>>> CHANGE (c9bbf5834a77bfd1089db26a1f581b8de26f2236 ANDROID: mm: add vendor hook to skip memcg reclaim by priori)
 #endif /* _TRACE_HOOK_VMSCAN_H */
 /* This part must be outside protection */
 #include <trace/define_trace.h>
