d12ca087230bead053d9c63a4831c45df7ac82d4
Sebastian Ene <sebastianene@google.com>
2024-09-30 14:51:38 +0000

    ANDROID: KVM: Send VM availability direct messages to Trustzone
    Verify if there are secure partitions which are interested in receiving
    VM availability messages and store them in an array. Notify the secure
    partitions which are registered for receiving these direct messages
    about VM creation and teardown.
    
    Bug: 269285339
    Bug: 278749606
    Change-Id: Ic812ad8b6fb3f30c821085141900c4e22555af57
    Signed-off-by: Sebastian Ene <sebastianene@google.com>
    Signed-off-by: Andrei Homescu <ahomescu@google.com>

--- arch/arm64/kvm/hyp/nvhe/pkvm.c
+++ arch/arm64/kvm/hyp/nvhe/pkvm.c
@@ -396,6 +396,29 @@ int __pkvm_reclaim_dying_guest_ffa_resources(pkvm_handle_t handle)
 	return ret;
 }
 
+int __pkvm_notify_guest_vm_avail(pkvm_handle_t handle, u32 vm_availability)
+{
+	struct pkvm_hyp_vm *hyp_vm;
+	int ret = 0;
+
+	hyp_read_lock(&vm_table_lock);
+	hyp_vm = get_vm_by_handle(handle);
+	if (!hyp_vm || !hyp_vm->kvm.arch.pkvm.ffa_support) {
+		ret = -EBUSY;
+		goto unlock;
+	}
+
+	if (hyp_vm->is_dying && vm_availability != FFA_VM_DESTRUCTION_MSG) {
+		ret = -EINVAL;
+		goto unlock;
+	}
+
+	ret = kvm_guest_notify_availability(hyp_vm, vm_availability);
+unlock:
+	hyp_read_unlock(&vm_table_lock);
+	return ret;
+}
+
 struct pkvm_hyp_vcpu *pkvm_load_hyp_vcpu(pkvm_handle_t handle,
 					 unsigned int vcpu_idx)
 {
@@ -1794,6 +1817,14 @@ bool kvm_handle_pvm_hvc64(struct kvm_vcpu *vcpu, u64 *exit_code)
 	return true;
 }
 
+u32 vm_handle_to_ffa_handle(pkvm_handle_t vm_handle)
+{
+	if (!vm_handle)
+		return HOST_FFA_ID;
+	else
+		return vm_handle_to_idx(vm_handle) + 1;
+}
+
 u32 hyp_vcpu_to_ffa_handle(struct pkvm_hyp_vcpu *hyp_vcpu)
 {
 	pkvm_handle_t vm_handle;
@@ -1802,5 +1833,5 @@ u32 hyp_vcpu_to_ffa_handle(struct pkvm_hyp_vcpu *hyp_vcpu)
 		return HOST_FFA_ID;
 
 	vm_handle = hyp_vcpu->vcpu.kvm->arch.pkvm.handle;
-	return vm_handle_to_idx(vm_handle) + 1;
+	return vm_handle_to_ffa_handle(vm_handle);
 }
