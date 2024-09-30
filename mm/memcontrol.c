be81c97c784a2f9d9e76aae5b825e493fc44888c
liwei <liwei1234@oppo.com>
2024-09-30 16:35:06 +0800

    ANDROID: GKI: export symbol memcg_page_state
    Bug:370264832
    Change-Id: Iae359dc667d3609b91c221743479e92287f22c1a
    Signed-off-by: liwei <liwei1234@oppo.com>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -797,6 +797,7 @@ unsigned long memcg_page_state(struct mem_cgroup *memcg, int idx)
 #endif
 	return x;
 }
+EXPORT_SYMBOL_GPL(memcg_page_state);
 
 /**
  * __mod_memcg_state - update cgroup memory statistics
