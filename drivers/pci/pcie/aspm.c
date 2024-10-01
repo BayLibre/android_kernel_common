e85007d3faa9e49949c165d1cedddbd92bdaf24b
Sushrut Shree Trivedi <quic_sushruts@quicinc.com>
2024-10-01 14:10:17 +0530

    UPSTREAM: pci: aspm:  Always build aspm.c
    Some ASPM-related tasks, such as save and restore of LTR and L1SS
    capabilities, still need to be performed when CONFIG_PCIEASPM is not
    enabled. To prepare for these changes, wrap the current code in aspm.c
    with an #ifdef and always build the file.
    
    Link:
    https://lore.kernel.org/r/20240128233212.1139663-2-david.e.box@linux.intel.com
    [bhelgaas: split build change from function moves]
    Link:
    https://lore.kernel.org/r/20240223205851.114931-3-helgaas@kernel.org
    Signed-off-by: David E. Box <david.e.box@linux.intel.com>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    
    Change-Id: I019b972172b5749f9d9181f6f3eeee0f05841a99
    (cherry picked from commit f3994bba8200b49e3eacbe5914cd13f228e0db37)
    Signed-off-by: Sushrut Shree Trivedi <quic_sushruts@quicinc.com>

--- drivers/pci/pcie/aspm.c
+++ drivers/pci/pcie/aspm.c
@@ -20,6 +20,8 @@
 #include <linux/delay.h>
 #include "../pci.h"
 
+#ifdef CONFIG_PCIEASPM
+
 #ifdef MODULE_PARAM_PREFIX
 #undef MODULE_PARAM_PREFIX
 #endif
@@ -1442,3 +1444,5 @@ bool pcie_aspm_support_enabled(void)
 	return aspm_support_enabled;
 }
 EXPORT_SYMBOL(pcie_aspm_support_enabled);
+
+#endif /* CONFIG_PCIEASPM */
