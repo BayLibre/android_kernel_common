8ac8579977386bf023b830fbe4add7070c1e0094
Mostafa Saleh <smostafa@google.com>
2024-10-01 13:46:27 +0000

    ANDROID: KVM: arm64: Add HVC to donate assignable MMIO
    Devices assigned to guest has to transition first to hypervisor,
    this guarantees that there is a point of time that the device is
    neither accessible from the host or the guest, so the hypervisor
    can reset it and block it's IOMMU.
    The host will donate the whole device first to the hypervisor
    before the guest touches or requests any part of the device
    and upon the first request or access the hypervisor will ensure
    that the device is fully donated and then will execute the transition
    sequence before assigning to the guest.
    
    Bug: 357781595
    Bug: 348382247
    Change-Id: I4ae5fc888e2eb4cd98a036d42d090c84d865f6bd
    Signed-off-by: Mostafa Saleh <smostafa@google.com>

--- arch/arm64/kvm/hyp/nvhe/device/device.c
+++ arch/arm64/kvm/hyp/nvhe/device/device.c
@@ -12,6 +12,16 @@
 struct pkvm_device *registered_devices;
 unsigned long registered_devices_nr;
 
+/*
+ * This lock protects all devices in registered_devices when ctxt changes,
+ * this is overlocking and can be improved. However, the device context
+ * only changes at boot time and at teardown and in theory there shouldn't
+ * be congestion on that path.
+ * All changes/checks to MMIO state or IOMMU must be atomic with the ctxt
+ * of the device.
+ */
+static DEFINE_HYP_SPINLOCK(device_spinlock);
+
 int pkvm_init_devices(void)
 {
 	size_t dev_sz;
@@ -25,5 +35,58 @@ int pkvm_init_devices(void)
 				     dev_sz >> PAGE_SHIFT);
 	if (ret)
 		registered_devices_nr = 0;
+	return ret;
+}
+
+static struct pkvm_device *pkvm_get_device(u64 addr)
+{
+	struct pkvm_device *dev = NULL;
+	struct pkvm_dev_resource *res;
+	int i, j;
+
+	for (i = 0 ; i < registered_devices_nr ; ++i) {
+		dev = &registered_devices[i];
+		for (j = 0 ; j < dev->nr_resources; ++j) {
+			res = &dev->resources[j];
+			if ((addr >= res->base) && (addr < (res->base + res->size)))
+				return dev;
+		}
+	}
+
+	return NULL;
+}
+
+/*
+ * Devices assigned to guest has to transition first to hypervisor,
+ * this guarantees that there is a point of time that the device is
+ * neither accessible from the host or the guest, so the hypervisor
+ * can reset it and block it's IOOMU.
+ * The host will donate the whole device first to the hypervisor
+ * before the guest touches or requests any part of the device
+ * and upon the first request or access the hypervisor will ensure
+ * that the device is fully donated first.
+ */
+int pkvm_device_hyp_assign_mmio(u64 pfn)
+{
+	struct pkvm_device *dev = pkvm_get_device(hyp_pfn_to_phys(pfn));
+	int ret;
+
+	if (!dev)
+		return -ENODEV;
+
+	hyp_spin_lock(&device_spinlock);
+	/* A VM already have this device, no take backs. */
+	if (dev->ctxt) {
+		ret = -EBUSY;
+		goto out_unlock;
+	}
+
+	ret = ___pkvm_host_donate_hyp_prot(pfn, 1, true, PAGE_HYP_DEVICE);
+	/* Hyp have device mapping, while host may have issue cacheable writes.*/
+	if (!ret)
+		kvm_flush_dcache_to_poc(__hyp_va(pfn << PAGE_SHIFT), PAGE_SIZE);
+
+out_unlock:
+	hyp_spin_unlock(&device_spinlock);
 	return ret;
 }
