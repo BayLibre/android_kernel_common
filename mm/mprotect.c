59af7a3bafd9225c8e0baa1d9592bbff856d10e4
Jeff Xu <jeffxu@google.com>
2024-10-03 03:59:03 +0000

    (WIP) seal NX stack - test only
    Change-Id: I67e70144e8093cb6cbb0e7ab38da6ac586101bfb
    Signed-off-by: Jeff Xu<jeffxu@google.com>

--- mm/mprotect.c
+++ mm/mprotect.c
@@ -761,8 +761,11 @@ static int do_mprotect_pkey(unsigned long start, size_t len,
 	 * can_modify_mm assumes we have acquired the lock on MM.
 	 */
 	if (unlikely(!can_modify_mm(current->mm, start, end))) {
-		error = -EPERM;
-		goto out;
+		/* checking if this is trying to use PROT_NONE on initial stack */
+		if (!((prot & PROT_EXEC) && (start <= mm->start_stack && end >= start_stack))) {
+			error = -EPERM;
+			goto out;
+		}
 	}
 
 	prev = vma_prev(&vmi);
