8c75bd2379a0794c76db15f50e2706a8c041ca7a
Sai Harshini Nimmala <quic_snimmala@quicinc.com>
2024-10-03 14:02:50 -0700

    ANDROID: GKI: Dynamic additional task_struct size allocation
    To reduce the size of vendor data allocated in the task_struct, from 512
    bytes to a significantly lower 48 bytes, the move to a dynamically sized
    task_struct is being made.
    
    Enable the config option that allows for dynamic sizing of the
    task_struct. Once config option is enabled, ensure that
    arch_task_struct_size is initialized to hold a valid task_struct size.
    
    Add padding after the init_task structure.
    This has to be done because init_task is statically allocated and hence
    will not benefit from the dynamic addition of size to all task_structs.
    
    Provide means for vendors to pass a size value via kernel cmdline.
    Use the passed value to dynamically add to the task_struct size to
    accommodate vendor data.
    The cmdline parameter to be used is 'android_task_struct_vendor_size'.
    For eg., vendors can add the following to the bootargs section of their
    devicetree to add an extra 512 bytes to the task_struct:
    "android_task_struct_vendor_size=512"
    To access this additional memory, use the android_task_vendor_data
    function provided.
    
    This commit is a squash of reverts of the commits,
    bacaaecd0aaf9facc5461f42b9a179a7f4fa0973,
    8cabd3723d5b12a6c2eceaa03cebd74900b2c771, and
    306270f72908c0a98e3e0e8fea20a00371b36922. All the above commits are
    reverts, so their reverts will essentially bring back the original
    changes.
    
    Bug: 233921394
    Signed-off-by: Sai Harshini Nimmala <quic_snimmala@quicinc.com>
    Change-Id: I0423e13019906def9056b5835d6219c824eebc9b

--- init/main.c
+++ init/main.c
@@ -1001,6 +1001,13 @@ static void __init print_kernel_cmdline(const char *cmdline)
 		pr_notice("%s%s\n", KERNEL_CMDLINE_PREFIX, cmdline);
 }
 
+#ifdef CONFIG_ARCH_WANTS_DYNAMIC_TASK_STRUCT
+static void __init setup_arch_task_struct_size(void)
+{
+	arch_task_struct_size = sizeof(struct task_struct);
+}
+#endif
+
 asmlinkage __visible __init __no_sanitize_address __noreturn __no_stack_protector
 void start_kernel(void)
 {
@@ -1024,6 +1031,9 @@ void start_kernel(void)
 	boot_cpu_init();
 	page_address_init();
 	pr_notice("%s", linux_banner);
+#ifdef CONFIG_ARCH_WANTS_DYNAMIC_TASK_STRUCT
+        setup_arch_task_struct_size();
+#endif
 	setup_arch(&command_line);
 	/* Static keys and static calls are needed by LSMs */
 	jump_label_init();
