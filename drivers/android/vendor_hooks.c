bd02f3e209ec635a41a6f3d849d2f2bcd6ce06fa
Sangmoon Kim <sangmoon.kim@samsung.com>
2024-10-10 19:07:14 +0900

    ANDROID: add vendor hooks for debug information of synchronize_irq
    synchronize_irq() is sometimes stuck. It helps the bootloader produces
    debugging information on which irq the function was processing. It can
    be obtained through Ramdump or etc, and this information can be used
    to quickly classify and respond to problems.
    
    Bug: 372807424
    Change-Id: I22d66d3cf1124d744e37612ac3275c972c3394c8
    Signed-off-by: Sangmoon Kim <sangmoon.kim@samsung.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -80,6 +80,7 @@
 #include <trace/hooks/perf.h>
 #include <trace/hooks/dmabuf.h>
 #include <trace/hooks/dev_shutdown.h>
+#include <trace/hooks/irqmanage.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -584,3 +585,5 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_wq_queue_work);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_wq_wake_idle_worker);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_device_shutdown);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_unfrozen_task);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_synchronize_irq_start);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_synchronize_irq_finish);
