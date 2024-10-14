d1bedbe09a6c5d9dc335b939f9c70f6181925371
huangshaobo <huangshaobo3@xiaomi.com>
2024-10-14 15:24:00 +0800

    ANDROID: android: Add symbols to debug_symbols driver
    Add __start_ro_after_init and __end_ro_after_init symbol which
    will be used by the mini dump module to debug_symbols driver.
    
    Bug: 373192614
    
    Signed-off-by: huangshaobo <huangshaobo3@xiaomi.com>
    Change-Id: I2a1853ed6659c51f4b02009929adc16d920859f2

--- include/linux/android_debug_symbols.h
+++ include/linux/android_debug_symbols.h
@@ -22,6 +22,8 @@ enum android_debug_symbol {
 	ADS_TOTAL_CMA, /* for debugging total cma pages */
 	ADS_SLAB_CACHES, /* for debugging slab */
 	ADS_SLAB_MUTEX, /* for debugging slab */
+	ADS_START_RO_AFTER_INIT,
+	ADS_END_RO_AFTER_INIT,
 	ADS_END
 };
 
