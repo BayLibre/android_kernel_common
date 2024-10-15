8ffa4f233ea1a834b144b6cac46b9118a90f595a
Jianan Huang <huangjianan@xiaomi.com>
2024-10-15 14:47:56 +0800

    ANDROID: mm: add vendor hook to trace shrink_node
    This is to adjust parameters between different memcgs to achieve
    more accurate memory reclamation.
    
    Bug: 423742169
    
    Bug: 373540729
    Change-Id: Ifb97a144c057555c5f9181f357fa146f9509be3e
    Signed-off-by: Jianan Huang <huangjianan@xiaomi.com>
    Signed-off-by: zhaorou <v-zhaorou@oppo.com>
    (cherry picked from commit 9d6f981a89e6e289f114270e2f1738b2b6fdd2ab)
    (cherry picked from commit ff55f3e7ea69a22fdba2e415532d7bce00d15561)
    (cherry picked from commit ac2a74af548e798cbbbeb997276e7bfb6c84f758)

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -386,3 +386,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_del_ref);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_percpu_rwsem_wq_add);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rmqueue_pcplist_override_batch);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_tune_inactive_ratio);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_shrink_node);
