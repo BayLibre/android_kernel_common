ef84cf94c45ae4215cefa9d3a4a0e3f413f3210a
Jianan Huang <huangjianan@xiaomi.com>
2024-10-15 14:47:56 +0800

    ANDROID: mm: add vendor hook to trace shrink_node
    This is to adjust parameters between different memcgs to achieve
    more accurate memory reclamation.
    
    Bug: 373540729
    Change-Id: Ifb97a144c057555c5f9181f357fa146f9509be3e
    Signed-off-by: Jianan Huang <huangjianan@xiaomi.com>
    (cherry picked from commit 9d6f981a89e6e289f114270e2f1738b2b6fdd2ab)

--- include/trace/hooks/vmscan.h
+++ include/trace/hooks/vmscan.h
@@ -80,12 +80,25 @@ DECLARE_HOOK(android_vh_do_folio_trylock,
 	TP_PROTO(struct folio *folio, struct rw_semaphore *sem,
 		bool *got_lock, bool *skip),
 	TP_ARGS(folio, sem, got_lock, skip));
+<<<<<<< HEAD   (ca24c5 Revert "ANDROID: usb: Optimization the transfer rate of acce)
 DECLARE_HOOK(android_vh_page_referenced_check_bypass,
 	TP_PROTO(struct folio *folio, unsigned long nr_to_scan, int lru, bool *bypass),
 	TP_ARGS(folio, nr_to_scan, lru, bypass));
 DECLARE_HOOK(android_vh_folio_referenced_check_bypass,
 	TP_PROTO(struct folio *folio, s8 priority, unsigned long nr_to_scan, int lru, bool *bypass),
 	TP_ARGS(folio, priority, nr_to_scan, lru, bypass));
+||||||| BASE
+DECLARE_HOOK(android_vh_shrink_node_memcgs,
+	TP_PROTO(struct mem_cgroup *memcg, bool *skip),
+	TP_ARGS(memcg, skip));
+=======
+DECLARE_HOOK(android_vh_shrink_node,
+	TP_PROTO(pg_data_t *pgdat, struct mem_cgroup *memcg),
+	TP_ARGS(pgdat, memcg));
+DECLARE_HOOK(android_vh_shrink_node_memcgs,
+	TP_PROTO(struct mem_cgroup *memcg, bool *skip),
+	TP_ARGS(memcg, skip));
+>>>>>>> CHANGE (9d6f98 ANDROID: mm: add vendor hook to trace shrink_node)
 DECLARE_HOOK(android_vh_should_memcg_bypass,
 	TP_PROTO(struct mem_cgroup *memcg, int priority, bool *bypass),
 	TP_ARGS(memcg, priority, bypass));
