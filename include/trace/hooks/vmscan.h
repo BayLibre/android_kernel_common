c658685fe0ae62233ef3671f1b284f657569071e
Jianan Huang <huangjianan@xiaomi.com>
2024-10-15 14:47:56 +0800

    ANDROID: mm: add vendor hook to trace shrink_node
    This is to adjust parameters between different memcgs to achieve
    more accurate memory reclamation.
    
    Bug: 423742169
    
    Bug: 373540729
    Change-Id: Ifb97a144c057555c5f9181f357fa146f9509be3e
    Signed-off-by: Jianan Huang <huangjianan@xiaomi.com>
    (cherry picked from commit 9d6f981a89e6e289f114270e2f1738b2b6fdd2ab)
    (cherry picked from commit ff55f3e7ea69a22fdba2e415532d7bce00d15561)
    (cherry picked from commit ac2a74af548e798cbbbeb997276e7bfb6c84f758)

--- include/trace/hooks/vmscan.h
+++ include/trace/hooks/vmscan.h
@@ -105,7 +105,20 @@ DECLARE_HOOK(android_vh_direct_reclaim_end,
 DECLARE_HOOK(android_vh_throttle_direct_reclaim_bypass,
 	TP_PROTO(bool *bypass),
 	TP_ARGS(bypass));
+<<<<<<< HEAD   (5289c9 ANDROID: GKI: Add zap_page_range_single to symbol list for q)
 
+||||||| BASE
+DECLARE_HOOK(android_vh_shrink_node_memcgs,
+	TP_PROTO(struct mem_cgroup *memcg, bool *skip),
+	TP_ARGS(memcg, skip));
+=======
+DECLARE_HOOK(android_vh_shrink_node,
+	TP_PROTO(pg_data_t *pgdat, struct mem_cgroup *memcg),
+	TP_ARGS(pgdat, memcg));
+DECLARE_HOOK(android_vh_shrink_node_memcgs,
+	TP_PROTO(struct mem_cgroup *memcg, bool *skip),
+	TP_ARGS(memcg, skip));
+>>>>>>> CHANGE (ac2a74 ANDROID: mm: add vendor hook to trace shrink_node)
 #endif /* _TRACE_HOOK_VMSCAN_H */
 /* This part must be outside protection */
 #include <trace/define_trace.h>
