63a1d9a7987fba1825ba6eea3cb91e393d03721e
Heikki Krogerus <heikki.krogerus@linux.intel.com>
2024-10-16 16:18:32 +0300

    UPSTREAM: usb: typec: Add attribute file showing the USB Modes of the partner
    This attribute file shows the supported USB modes (USB 2.0,
    USB 3.0 and USB4) of the partner, and the currently active
    mode.
    
    The active mode is determined primarily by checking the
    speed of the enumerated USB device. When USB Power Delivery
    is supported, the active USB mode should be always the mode
    that was used with the Enter_USB Message, regardless of the
    result of the USB enumeration. The port drivers can
    separately assign the mode with a dedicated API.
    
    If USB Power Delivery Identity is supplied for the partner
    device, the supported modes are extracted from it.
    
    Change-Id: I75462db69c23583d3c3a1ce9f7ca7b8ee29aaa2b
    Signed-off-by: Heikki Krogerus <heikki.krogerus@linux.intel.com>
    Link: https://lore.kernel.org/r/20241016131834.898599-3-heikki.krogerus@linux.intel.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 2140a952c4e9c73993ae6d9c2cc674d263d4beab)
    
    Bug: 382001905
    Test: Across patch series:
    Write 'usb4' to /sys/class/typec/portN/portN-partner/usb_mode
    Verify the mode change by reading /sys/class/chromeos/cros_ec/usbpdmuxinfo
    Confirm the port capabilities via /sys/class/typec/portN/usb_capability
    
    Change-Id: Ic0bdd540978adb15d7ed3d290dad8b2c97d0283e
    Signed-off-by: Andrei Kuchynski <akuchynski@google.com>

--- include/linux/usb/typec.h
+++ include/linux/usb/typec.h
@@ -219,6 +219,7 @@ struct typec_cable_desc {
  * @accessory: Audio, Debug or none.
  * @identity: Discover Identity command data
  * @pd_revision: USB Power Delivery Specification Revision if supported
+ * @usb_capability: Supported USB Modes
  * @attach: Notification about attached USB device
  * @deattach: Notification about removed USB device
  *
@@ -236,6 +237,7 @@ struct typec_partner_desc {
 	enum typec_accessory	accessory;
 	struct usb_pd_identity	*identity;
 	u16			pd_revision; /* 0300H = "3.0" */
+	u8			usb_capability;
 
 	void (*attach)(struct typec_partner *partner, struct device *dev);
 	void (*deattach)(struct typec_partner *partner, struct device *dev);
@@ -251,6 +253,7 @@ struct typec_partner_desc {
  * @pd_get: Get available USB Power Delivery Capabilities.
  * @pd_set: Set USB Power Delivery Capabilities.
  * @default_usb_mode_set: USB Mode to be used by default with Enter_USB Message
+ * @enter_usb_mode: Change the active USB Mode
  */
 struct typec_operations {
 	int (*try_role)(struct typec_port *port, int role);
@@ -262,6 +265,7 @@ struct typec_operations {
 	struct usb_power_delivery **(*pd_get)(struct typec_port *port);
 	int (*pd_set)(struct typec_port *port, struct usb_power_delivery *pd);
 	int (*default_usb_mode_set)(struct typec_port *port, enum usb_mode mode);
+	int (*enter_usb_mode)(struct typec_port *port, enum usb_mode mode);
 
 	ANDROID_KABI_RESERVE(1);
 	ANDROID_KABI_RESERVE(2);
@@ -367,6 +371,7 @@ int typec_port_set_usb_power_delivery(struct typec_port *port, struct usb_power_
 int typec_partner_set_usb_power_delivery(struct typec_partner *partner,
 					 struct usb_power_delivery *pd);
 
+void typec_partner_set_usb_mode(struct typec_partner *partner, enum usb_mode usb_mode);
 void typec_port_set_usb_mode(struct typec_port *port, enum usb_mode mode);
 
 /**
