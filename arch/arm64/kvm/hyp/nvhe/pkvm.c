9871a60147ce6a19fed6b087863bb518a515801e
Sebastian Ene <sebastianene@google.com>
2024-10-17 10:44:35 +0000

    ANDROID: KVM: arm64: Handle guest FF-A map call
    Introduce a guest FF-A dispatcher and handle the memory
    map call, the version request and the supported features
    call. Only the FF-A map call requires special handling
    as we will need to map the guest page inside the
    hypervisor. To treat the case where the guest issues
    FF-A map on a page that wasn't faulted, we fake up a
    data abort and we go back to the host.
    If the call is not FF-A, go back to the guest an return
    not supported.
    
    Bug: 269285339
    Bug: 278749606
    Change-Id: I4bd6f482606e617a6cf59046cc1015ea2d9b491f
    Signed-off-by: Sebastian Ene <sebastianene@google.com>

--- arch/arm64/kvm/hyp/nvhe/pkvm.c
+++ arch/arm64/kvm/hyp/nvhe/pkvm.c
@@ -11,8 +11,11 @@
 #include <kvm/arm_psci.h>
 
 #include <asm/kvm_emulate.h>
+#include <hyp/adjust_pc.h>
 
 #include <nvhe/alloc.h>
+#include <nvhe/arm-smccc.h>
+#include <nvhe/ffa.h>
 #include <nvhe/mem_protect.h>
 #include <nvhe/memory.h>
 #include <nvhe/mm.h>
@@ -229,12 +232,6 @@ static int pkvm_vcpu_init_traps(struct pkvm_hyp_vcpu *hyp_vcpu)
 
 	return 0;
 }
-
-/*
- * Start the VM table handle at the offset defined instead of at 0.
- * Mainly for sanity checking and debugging.
- */
-#define HANDLE_OFFSET 0x1000
 
 static unsigned int vm_handle_to_idx(pkvm_handle_t handle)
 {
@@ -1339,8 +1336,31 @@ static bool pkvm_handle_psci(struct pkvm_hyp_vcpu *hyp_vcpu)
 	return pvm_psci_not_supported(hyp_vcpu);
 }
 
-static int pkvm_handle_empty_memcache(struct pkvm_hyp_vcpu *hyp_vcpu,
-				      u64 *exit_code)
+u64 __pkvm_memshare_page_req(struct pkvm_hyp_vcpu *hyp_vcpu, u64 ipa)
+{
+	struct kvm_vcpu *vcpu = &hyp_vcpu->vcpu;
+	u64 elr;
+
+	/* Fake up a data abort (Level 3 translation fault on write) */
+	vcpu->arch.fault.esr_el2 = (u32)ESR_ELx_EC_DABT_LOW << ESR_ELx_EC_SHIFT |
+				   ESR_ELx_WNR | ESR_ELx_FSC_FAULT |
+				   FIELD_PREP(ESR_ELx_FSC_LEVEL, 3);
+
+	/* Shuffle the IPA around into the HPFAR */
+	vcpu->arch.fault.hpfar_el2 = (ipa >> 8) & HPFAR_MASK;
+
+	/* This is a virtual address. 0's good. Let's go with 0. */
+	vcpu->arch.fault.far_el2 = 0;
+
+	/* Rewind the ELR so we return to the HVC once the IPA is mapped */
+	elr = read_sysreg(elr_el2);
+	elr -= 4;
+	write_sysreg(elr, elr_el2);
+
+	return ARM_EXCEPTION_TRAP;
+}
+
+int pkvm_handle_empty_memcache(struct pkvm_hyp_vcpu *hyp_vcpu, u64 *exit_code)
 {
 	struct kvm_hyp_req *req;
 
