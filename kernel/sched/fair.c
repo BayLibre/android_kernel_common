7ea5a01f84c8090338a8efb7e54b4866f6279e97
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2024-10-24 21:32:10 +0000

    Revert "ANDROID: sched: Add replace_next_task_fair vendor hook i..."
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/7eb51225-34d6-4f51-9df5-fba22262754a). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Bug: 375458897
    Change-Id: I18b349e4cf3502349ef87b2944216de3cc8d0887
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8960,8 +8960,6 @@ pick_next_task_fair(struct rq *rq, struct task_struct *prev, struct rq_flags *rf
 
 again:
 	p = pick_task_fair(rq);
-	trace_android_rvh_replace_next_task_fair(rq, &p, prev);
-
 	if (!p)
 		goto idle;
 	se = &p->se;
