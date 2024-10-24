7d218393573c546a10a040be7d6cba0c0fc7a394
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2024-10-24 21:32:10 +0000

    Revert "ANDROID: vendor_hooks: Add hooks for lookaround"
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/7eb51225-34d6-4f51-9df5-fba22262754a). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Bug: 375458897
    Change-Id: Id23f7e4f662adf73e3ec35b1ec292c5754b5c5d1
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com

--- mm/migrate.c
+++ mm/migrate.c
@@ -49,10 +49,6 @@
 
 #include <trace/events/migrate.h>
 
-#undef CREATE_TRACE_POINTS
-#include <trace/hooks/mm.h>
-#include <trace/hooks/vmscan.h>
-
 #include "internal.h"
 
 bool isolate_movable_page(struct page *page, isolate_mode_t mode)
@@ -688,8 +684,6 @@ void folio_migrate_flags(struct folio *newfolio, struct folio *folio)
 	 */
 	if (folio_test_mappedtodisk(folio))
 		folio_set_mappedtodisk(newfolio);
-
-	trace_android_vh_look_around_migrate_folio(folio, newfolio);
 
 	/* Move dirty on pages not done by folio_migrate_mapping() */
 	if (folio_test_dirty(folio))
