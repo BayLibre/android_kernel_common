a7f201649e4a6456c5b4e6539433d32567ebeb76
Srivatsa Vaddagiri <quic_svaddagi@quicinc.com>
2024-10-29 20:51:15 +0530

    ANDROID: pci/coco: Introduce cc_platform APIs
    Introduced APIs can be used by guest kernel to initialize/query various CC
    (confidential compute) capabilities.
    
    Efforts are ongoing in community [1] to add CC support for arm64 guests,
    which will provide the API proposed in this patch. Marking this patch
    ANDROID only until those efforts conclude and land upstream.
    
    1. https://lore.kernel.org/all/20240819131924.372366-17-steven.price@arm.com/
    
    Bug: 376380960
    Change-Id: I97a7a179531db4295b8685911f0954dcab8bf7b5
    Signed-off-by: Srivatsa Vaddagiri <quic_svaddagi@quicinc.com>

---
+++ arch/arm64/coco/core.c
@@ -0,0 +1,80 @@
+// SPDX-License-Identifier: GPL-2.0-only
+
+#include <linux/export.h>
+#include <linux/init.h>
+#include <linux/printk.h>
+#include <linux/cc_platform.h>
+
+static u64 arm64_cc_status;
+
+#define BITS_PER_U64 ((u32)(sizeof(u64) * 8))
+
+bool noinstr cc_platform_has(enum cc_attr attr)
+{
+	u64 mask;
+
+	if (attr >= BITS_PER_U64) {
+		pr_err("%s: Invalid attr %d\n", __func__, attr);
+		return false;
+	}
+
+	mask = 1 << attr;
+
+	return (arm64_cc_status & mask) != 0;
+}
+EXPORT_SYMBOL_GPL(cc_platform_has);
+
+void cc_platform_set(enum cc_attr attr)
+{
+	u64 mask;
+
+	if (attr >= BITS_PER_U64) {
+		pr_err("%s: Invalid attr %d\n", __func__, attr);
+		return;
+	}
+
+	mask = 1 << attr;
+
+	switch (attr) {
+#ifdef CONFIG_HARDENED_GUEST
+	case CC_ATTR_GUEST_HARDENED:
+		arm64_cc_status |= mask;
+		break;
+#endif
+	default:
+		break;
+	}
+}
+EXPORT_SYMBOL_GPL(cc_platform_set);
+
+void cc_platform_clear(enum cc_attr attr)
+{
+	u64 mask;
+
+	if (attr >= BITS_PER_U64) {
+		pr_err("%s: Invalid attr %d\n", __func__, attr);
+		return;
+	}
+
+	mask = 1 << attr;
+
+	switch (attr) {
+#ifdef CONFIG_HARDENED_GUEST
+	case CC_ATTR_GUEST_HARDENED:
+		arm64_cc_status &= ~mask;
+		break;
+#endif
+	default:
+		break;
+	}
+}
+EXPORT_SYMBOL_GPL(cc_platform_clear);
+
+static int __init aarch64_coco_init(void)
+{
+#ifdef CONFIG_HARDENED_GUEST
+	cc_platform_set(CC_ATTR_GUEST_HARDENED);
+#endif
+	return 0;
+}
+arch_initcall(aarch64_coco_init);
