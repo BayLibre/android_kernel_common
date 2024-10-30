55d2fac9ed5f1060e044871b4bb43166618e6fa8
Srivatsa Vaddagiri <quic_svaddagi@quicinc.com>
2024-10-30 11:07:16 +0530

    ANDROID: arm64/coco: Enable virtio restricted memory access
    virtio devices are usually emulated in an untrusted host and hence their
    access to (confidential) guest memory is restricted to only a portion of
    guest memory. Activate use of restricted memory access feature for
    virtio devices, which ensures that virtio devices are requested to
    access only regions of guest memory they have access to.
    
    (This patch is inspired from a similar patch present in commit
    38a4dbc101d28f598f8b39b07ce76ee9d1c5dea8
    https://github.com/intel/tdx.git guest-hardening-rebased)
    
    Bug: 376380960
    Change-Id: I157d8aa6151bf95868e77f43f5553a25222f807e
    Signed-off-by: Srivatsa Vaddagiri <quic_svaddagi@quicinc.com>

--- arch/arm64/coco/core.c
+++ arch/arm64/coco/core.c
@@ -1,10 +1,21 @@
 // SPDX-License-Identifier: GPL-2.0-only
 
 #include <linux/export.h>
+#include <linux/init.h>
 #include <linux/cc_platform.h>
+#include <linux/virtio_anchor.h>
 
 bool noinstr cc_platform_has(enum cc_attr attr)
 {
 	return false;
 }
 EXPORT_SYMBOL_GPL(cc_platform_has);
+
+static int __init aarch64_coco_init(void)
+{
+	/* Set restricted memory access for virtio. */
+	virtio_set_mem_acc_cb(virtio_require_restricted_mem_acc);
+
+	return 0;
+}
+arch_initcall(aarch64_coco_init);
