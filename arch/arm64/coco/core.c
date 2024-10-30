8e8defb5db3cb129f0219f14686fedc0c55a931f
Srivatsa Vaddagiri <quic_svaddagi@quicinc.com>
2024-10-30 11:07:16 +0530

    arm64/cc: Enable virtio restricted memory access
    Enforce use of restricted memory access for virtio devices
    
    Change-Id: I157d8aa6151bf95868e77f43f5553a25222f807e
    Signed-off-by: Srivatsa Vaddagiri <quic_svaddagi@quicinc.com>

--- arch/arm64/coco/core.c
+++ arch/arm64/coco/core.c
@@ -2,9 +2,17 @@
 
 #include <linux/export.h>
 #include <linux/cc_platform.h>
+#include <linux/virtio_anchor.h>
 
 bool noinstr cc_platform_has(enum cc_attr attr)
 {
 	return false;
 }
 EXPORT_SYMBOL_GPL(cc_platform_has);
+
+static int __init aarch64_coco_init(void)
+{
+	/* Set restricted memory access for virtio. */
+	virtio_set_mem_acc_cb(virtio_require_restricted_mem_acc);
+}
+arch_initcall(coco_init);
