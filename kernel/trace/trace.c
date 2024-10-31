278af9963bf953533d36ae19b5ab670cf50b7743
Prateek Sood <prsood@codeaurora.org>
2024-10-31 16:38:35 +0800

    ANDROID: ftrace: add more vendor hook for ftrace dump on oops
    The aosp/1562075 add the following vendor hooks in ftrace oops path
    to allow vendor modules to get ftrace buffer data.
    
    1) android_vh_ftrace_oops_enter
    2) android_vh_ftrace_size_check
    3) android_vh_ftrace_format_check
    4) android_vh_ftrace_dump_buffer
    5) android_vh_ftrace_oops_exit
    
    Ftrace dump on oops can be controlled with kernel command
    line parameter ftrace_dump_on_oops.
    
    However the commit b8e1f899fc6b ("ANDROID: ftrace: vendor hook for ftrace dump on oops")
    only cherry-pick android_vh_ftrace_format_check vh.
    
    But following vendor hooks in ftrace oops path was not added.
    
    1) android_vh_ftrace_oops_enter
    2) android_vh_ftrace_size_check
    3) android_vh_ftrace_dump_buffer
    4) android_vh_ftrace_oops_exit
    
    So add these to allow vendor modules to get ftrace buffer data.
    
    Bug: 177387994
    Signed-off-by: Prateek Sood <prsood@codeaurora.org>
    Signed-off-by: Mukesh Ojha <mojha@codeaurora.org>
    [xuewen: minor conflicts fixups]
    Change-Id: I00328101bc7e1092ce7a0b50c2d47ab8288e0067
    Signed-off-by: Xuewen Yan <xuewen.yan@unisoc.com>
    (cherry picked from commit fbbc48ef1854f36aba48538b3034c884878d9a65)

--- kernel/trace/trace.c
+++ kernel/trace/trace.c
@@ -10668,7 +10668,11 @@ static struct notifier_block trace_die_notifier = {
 static int trace_die_panic_handler(struct notifier_block *self,
 				unsigned long ev, void *unused)
 {
-	if (!ftrace_dump_on_oops_enabled())
+	bool ftrace_check = false;
+
+	trace_android_vh_ftrace_oops_enter(&ftrace_check);
+
+	if (!ftrace_dump_on_oops_enabled() || ftrace_check)
 		return NOTIFY_DONE;
 
 	/* The die notifier requires DIE_OOPS to trigger */
@@ -10677,6 +10681,7 @@ static int trace_die_panic_handler(struct notifier_block *self,
 
 	ftrace_dump(DUMP_PARAM);
 
+	trace_android_vh_ftrace_oops_exit(&ftrace_check);
 	return NOTIFY_DONE;
 }
 
@@ -10696,6 +10701,8 @@ static int trace_die_panic_handler(struct notifier_block *self,
 void
 trace_printk_seq(struct trace_seq *s)
 {
+	bool dump_printk = true;
+
 	/* Probably should print a warning here. */
 	if (s->seq.len >= TRACE_MAX_PRINT)
 		s->seq.len = TRACE_MAX_PRINT;
@@ -10711,7 +10718,9 @@ trace_printk_seq(struct trace_seq *s)
 	/* should be zero ended, but we are paranoid. */
 	s->buffer[s->seq.len] = 0;
 
-	printk(KERN_TRACE "%s", s->buffer);
+	trace_android_vh_ftrace_dump_buffer(s, &dump_printk);
+	if (dump_printk)
+		printk(KERN_TRACE "%s", s->buffer);
 
 	trace_seq_init(s);
 }
@@ -10752,8 +10761,10 @@ static void ftrace_dump_one(struct trace_array *tr, enum ftrace_dump_mode dump_m
 	static struct trace_iterator iter;
 	unsigned int old_userobj;
 	unsigned long flags;
-	int cnt = 0;
+	int cnt = 0, cpu;
 	bool ftrace_check = true;
+	bool ftrace_size_check = false;
+	unsigned long size;
 
 	/*
 	 * Always turn off tracing when we dump.
@@ -10773,10 +10784,18 @@ static void ftrace_dump_one(struct trace_array *tr, enum ftrace_dump_mode dump_m
 	/* While dumping, do not allow the buffer to be enable */
 	tracer_tracing_disable(tr);
 
+	for_each_tracing_cpu(cpu) {
+		size = ring_buffer_size(iter.array_buffer->buffer, cpu);
+		trace_android_vh_ftrace_size_check(size, &ftrace_size_check);
+	}
+
 	old_userobj = tr->trace_flags & TRACE_ITER_SYM_USEROBJ;
 
 	/* don't look at user memory in panic mode */
 	tr->trace_flags &= ~TRACE_ITER_SYM_USEROBJ;
+
+	if (ftrace_size_check)
+		goto out_enable;
 
 	if (dump_mode == DUMP_ORIG)
 		iter.cpu_file = raw_smp_processor_id();
@@ -10837,6 +10856,7 @@ static void ftrace_dump_one(struct trace_array *tr, enum ftrace_dump_mode dump_m
 	else
 		printk(KERN_TRACE "---------------------------------\n");
 
+out_enable:
 	tr->trace_flags |= old_userobj;
 
 	tracer_tracing_enable(tr);
