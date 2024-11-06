1e6ef6d1ec721b10ff7670d753351f3891e2d490
HyungJae Im <hj2.im@samsung.corp-partner.google.com>
2024-11-06 10:29:57 +0900

    ANDROID: USB: add vendor hook functions for USB
    Add vendor hooks functions to improve power consumption
    while USB accessory is attached.
    
    Bug: 377546266
    
    Change-Id: Ic381b9f65e783ec545a77f492d6ef14890202015
    Signed-off-by: HyungJae Im <hj2.im@samsung.corp-partner.google.com>
    (cherry picked from commit 1586d3cdc925ae9352529763ac1eabc69c09a15d)

--- include/trace/hooks/usb.h
+++ include/trace/hooks/usb.h
@@ -1,7 +1,11 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 #undef TRACE_SYSTEM
 #define TRACE_SYSTEM usb
+<<<<<<< HEAD   (871ceb6f212cc1380cfdce82017a75d15701a762 ANDROID: cpufreq: scmi: Fix scmi_limit_notify_cb handling)
 
+||||||| BASE   (5dc82090ccc0ec6118c0c09565932afb7c4e2e5e FROMGIT: wifi: cfg80211: Do not create BSS entries for unsup)
+=======
+>>>>>>> CHANGE (1586d3cdc925ae9352529763ac1eabc69c09a15d ANDROID: USB: add vendor hook functions for USB)
 #undef TRACE_INCLUDE_PATH
 #define TRACE_INCLUDE_PATH trace/hooks
 
@@ -23,6 +27,10 @@ DECLARE_RESTRICTED_HOOK(android_rvh_usb_dev_suspend,
 DECLARE_HOOK(android_vh_usb_dev_resume,
 	TP_PROTO(struct usb_device *udev, pm_message_t msg, bool *bypass),
 	TP_ARGS(udev, msg, bypass));
+
+DECLARE_HOOK(android_vh_usb_new_device_added,
+	TP_PROTO(struct usb_device *udev, int *err),
+	TP_ARGS(udev, err));
 
 #endif /*  _TRACE_HOOK_USB_H */
 /*  This part must be outside protection */
