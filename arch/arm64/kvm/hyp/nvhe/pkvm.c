b9552275b29a9644b2c9012b8feb09b60e7b5a57
Sebastian Ene <sebastianene@google.com>
2024-11-07 15:52:09 +0000

    ANDROID: KVM: arm64: Introduce KVM_CAP_ARM_FFA_SUPPORT uapi
    Restrict protected VMs from being able to talk to Trustzone unless
    userspace specifies the KVM_CAP_ARM_FFA_SUPPORT capability.
    
    Bug: 269285339
    Bug: 278749606
    Change-Id: I9f8adb47b03f076f17b32fdb280d5531b4578e25
    Signed-off-by: Sebastian Ene <sebastianene@google.com>

--- arch/arm64/kvm/hyp/nvhe/pkvm.c
+++ arch/arm64/kvm/hyp/nvhe/pkvm.c
@@ -555,6 +555,7 @@ static void init_pkvm_hyp_vm(struct kvm *host_kvm, struct pkvm_hyp_vm *hyp_vm,
 		pvmfw_load_addr = READ_ONCE(host_kvm->arch.pkvm.pvmfw_load_addr);
 	hyp_vm->kvm.arch.pkvm.pvmfw_load_addr = pvmfw_load_addr;
 
+	hyp_vm->kvm.arch.pkvm.ffa_support = READ_ONCE(host_kvm->arch.pkvm.ffa_support);
 	hyp_vm->kvm.arch.mmu.last_vcpu_ran = (int __percpu *)last_ran;
 	hyp_vm->kvm.arch.flags = 0;
 	memset(last_ran, -1, pkvm_get_last_ran_size());
