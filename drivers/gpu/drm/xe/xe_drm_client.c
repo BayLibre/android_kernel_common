5ed5601e9d1de665784707bf4c65099cfe5e5dfd
Lucas De Marchi <lucas.demarchi@intel.com>
2024-11-07 21:33:16 -0800

    UPSTREAM: drm/xe: Wait on killed exec queues
    When an exec queue is killed it triggers an async process of asking the
    GuC to schedule the context out. The timestamp in the context image is
    only updated when this process completes. In case a userspace process
    kills an exec and tries to read the timestamp, it may not get an updated
    runtime.
    
    Add synchronization between the process reading the fdinfo and the exec
    queue being killed. After reading all the timestamps, wait on exec
    queues in the process of being killed. When that wait is over,
    xe_exec_queue_fini() was already called and updated the timestamps.
    
    v2: Do not update pending_removal before validating user args
        (Matthew Auld)
    v3: Move wait on pending to be done before getting any timestamp
        so it's more likely for the gpu and exec queue timestamps to
        be closer together
    
    Closes: https://gitlab.freedesktop.org/drm/xe/kernel/-/issues/2667
    Reviewed-by: Jonathan Cavitt <jonathan.cavitt@intel.com>
    Reviewed-by: Umesh Nerlige Ramappa <umesh.nerlige.ramappa@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20241108053318.3483678-2-lucas.demarchi@intel.com
    Change-Id: Ic4eab24f3203a8759d4250f4a7f5e47cfb2a7064
    Signed-off-by: Lucas De Marchi <lucas.demarchi@intel.com>

--- drivers/gpu/drm/xe/xe_drm_client.c
+++ drivers/gpu/drm/xe/xe_drm_client.c
@@ -279,6 +279,13 @@ static void show_run_ticks(struct drm_printer *p, struct drm_file *file)
 	struct xe_exec_queue *q;
 	u64 gpu_timestamp;
 
+	/*
+	 * Wait for any exec queue going away: their cycles will get updated on
+	 * context switch out, so wait for that to happen
+	 */
+	wait_var_event(&xef->exec_queue.pending_removal,
+		       !atomic_read(&xef->exec_queue.pending_removal));
+
 	xe_pm_runtime_get(xe);
 
 	/* Accumulate all the exec queues from this client */
