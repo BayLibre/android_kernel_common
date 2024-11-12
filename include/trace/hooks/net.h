641ec060df5a8edefa9b405d4367bc1c723e5ad9
Chenghao Zhao <zhaochenghao@honor.com>
2024-11-12 21:17:37 +0800

    ANDROID: GKI: net: add vendor hook to check if out of order occurs
    1.android_vh_tcp_rcv_established_fast_path
    Check if there are received packets out of order for the fast path.
    2.android_vh_tcp_rcv_established_slow_path
    Check if there are received packets out of order for the slow path.
    
    Bug: 378600969
    Change-Id: Ifad16bd9523ac0c3cc0c0c98dfb0884635f3a537
    Signed-off-by: Chenghao Zhao <zhaochenghao@honor.com>
    (cherry picked from commit c6058890859531826c574aaee2ffd2ed95c38e59)
    (cherry picked from commit 0defa678590c9f83d74dd42a14f743425ab6f489)

--- include/trace/hooks/net.h
+++ include/trace/hooks/net.h
@@ -48,6 +48,26 @@ DECLARE_HOOK(android_vh_tcp_clean_rtx_queue,
 struct inet_connection_sock;
 DECLARE_HOOK(android_vh_tcp_rcv_synack,
 	TP_PROTO(struct inet_connection_sock *icsk), TP_ARGS(icsk));
+<<<<<<< HEAD   (171d963ed71fe5c19af40cf566db837920137e90 ANDROID: GKI: Add symbol to symbol list for unisoc)
+||||||| BASE   (cbd012971e3bfd1a24ca0e2df1055fd9b1246d52 ANDROID: Add vendor hooks to signal.)
+DECLARE_HOOK(android_vh_udp_unicast_rcv_skb,
+	TP_PROTO(struct sk_buff *skb, struct sock *sk),
+	TP_ARGS(skb, sk));
+DECLARE_HOOK(android_vh_udp6_unicast_rcv_skb,
+	TP_PROTO(struct sk_buff *skb, struct sock *sk),
+	TP_ARGS(skb, sk));
+=======
+DECLARE_HOOK(android_vh_udp_unicast_rcv_skb,
+	TP_PROTO(struct sk_buff *skb, struct sock *sk),
+	TP_ARGS(skb, sk));
+DECLARE_HOOK(android_vh_udp6_unicast_rcv_skb,
+	TP_PROTO(struct sk_buff *skb, struct sock *sk),
+	TP_ARGS(skb, sk));
+DECLARE_HOOK(android_vh_tcp_rcv_established_fast_path,
+	TP_PROTO(struct sock *sk), TP_ARGS(sk));
+DECLARE_HOOK(android_vh_tcp_rcv_established_slow_path,
+	TP_PROTO(struct sock *sk), TP_ARGS(sk));
+>>>>>>> CHANGE (0defa678590c9f83d74dd42a14f743425ab6f489 ANDROID: GKI: net: add vendor hook to check if out of order )
 /* macro versions of hooks are no longer required */
 
 #endif /* _TRACE_HOOK_NET_VH_H */
