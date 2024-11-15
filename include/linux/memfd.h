790bd8b20b122ee8d822a9343dbcba85880df415
Isaac J. Manjarres <isaacmanjarres@google.com>
2024-11-15 10:49:18 -0800

    ANDROID: mm/memfd: Expose memfd_file_seals_ptr()
    Ashmem is currently being replaced by memfd. When setting the size for
    a buffer, we can use file seals to prevent the size from being changed
    again. However, when doing so, the file's inode lock will be held to
    ensure atomicity with respect to mmap() operations. Therefore,
    memfd_fcntl() cannot be used to add file seals.
    
    Therefore, expose memfd_file_seals_ptr() so that it can be used to
    modify the file seals with the inode lock held.
    
    Bug: 111903542
    Change-Id: I5aeddbbe6100d49aed860718328de6ce45f2da9c
    Signed-off-by: Isaac J. Manjarres <isaacmanjarres@google.com>

--- include/linux/memfd.h
+++ include/linux/memfd.h
@@ -7,6 +7,7 @@
 #ifdef CONFIG_MEMFD_CREATE
 extern long memfd_fcntl(struct file *file, unsigned int cmd, unsigned int arg);
 struct folio *memfd_alloc_folio(struct file *memfd, pgoff_t idx);
+unsigned int *memfd_file_seals_ptr(struct file *file);
 #else
 static inline long memfd_fcntl(struct file *f, unsigned int c, unsigned int a)
 {
@@ -15,6 +16,10 @@ static inline long memfd_fcntl(struct file *f, unsigned int c, unsigned int a)
 static inline struct folio *memfd_alloc_folio(struct file *memfd, pgoff_t idx)
 {
 	return ERR_PTR(-EINVAL);
+}
+static inline unsigned int *memfd_file_seals_ptr(struct file *file)
+{
+	return NULL;
 }
 #endif
 
