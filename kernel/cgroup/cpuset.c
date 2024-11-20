c3906985d43e2ea4e7441944c720daf9b372450a
Jiss Jose <jiss.jose@samsung.com>
2024-11-20 12:19:59 +0530

    Revert "ANDROID: fix crc of cpuset_cpus_allowed()"
    This reverts commit e84a7dc5be664670c845574c5c323d6df28e6496.
    
    Need to revert 96226fbed566f3f686f53a489a29846f2d538080 ,
    so ANDROID: fix crc of cpuset_cpus_allowed() should be reverted to avoid
    merge conflict
    
    Bug: 379227997
    Change-Id: I1d73fbe1b48de1975af4af07cbb74f4cbf756f5c
    Signed-off-by: Jiss Jose <jiss.jose@samsung.com>

--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -21,9 +21,7 @@
  *  License.  See the file COPYING in the main directory of the Linux
  *  distribution for more details.
  */
-#ifndef __GENKSYMS__
 #include "cgroup-internal.h"
-#endif
 
 #include <linux/cpu.h>
 #include <linux/cpumask.h>
