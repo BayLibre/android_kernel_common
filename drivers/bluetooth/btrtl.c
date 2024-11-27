d082fd1be1ba0c7a99e940991c659663197ddf6d
Zhengping Jiang <jiangzp@google.com>
2024-11-26 21:21:49 -0800

    Revert "CHROMIUM: Bluetooth: Prevent Reset on Suspend for RTL8852"
    This reverts commit 37f57b5a695547db11cfeceed81afc63ba576fbe.
    
    Change-Id: Ib2c4401023da3301b3a3267d88933a82150b4bb8
    Signed-off-by: Zhengping Jiang <jiangzp@google.com>

--- drivers/bluetooth/btrtl.c
+++ drivers/bluetooth/btrtl.c
@@ -1306,8 +1306,7 @@ void btrtl_set_quirks(struct hci_dev *hdev, struct btrtl_device_info *btrtl_dev)
 	/* Force RTL8852A to enable remote wakeup in order to prevent it from
 	 * resetting itself and taking longer to resume from suspend
 	 */
-	if (btrtl_dev->project_id == CHIP_ID_8852A ||
-	    btrtl_dev->project_id == CHIP_ID_8852B)
+	if (btrtl_dev->project_id == CHIP_ID_8852A)
 		set_bit(HCI_QUIRK_FORCE_REMOTE_WAKE, &hdev->quirks);
 
 	if (!btrtl_dev->ic_info)
