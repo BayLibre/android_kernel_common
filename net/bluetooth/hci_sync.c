2ae8af038b58e89d4e53788391fbba5168972a19
Zhengping Jiang <jiangzp@google.com>
2024-11-26 21:21:25 -0800

    Revert "CHROMIUM: Bluetooth: Ignore global Connectable setting for extended adv"
    This reverts commit 3bf29bd6e6bc78d3c3e583988e9048cecf134077.
    
    Change-Id: I513aa7072e39996fe50b1b99428497c13a4c10f0
    Signed-off-by: Zhengping Jiang <jiangzp@google.com>

--- net/bluetooth/hci_sync.c
+++ net/bluetooth/hci_sync.c
@@ -1254,7 +1254,11 @@ int hci_setup_ext_adv_instance_sync(struct hci_dev *hdev, u8 instance)
 
 	flags = hci_adv_instance_flags(hdev, instance);
 
-	connectable = (flags & MGMT_ADV_FLAG_CONNECTABLE);
+	/* If the "connectable" instance flag was not set, then choose between
+	 * ADV_IND and ADV_NONCONN_IND based on the global connectable setting.
+	 */
+	connectable = (flags & MGMT_ADV_FLAG_CONNECTABLE) ||
+		      mgmt_get_connectable(hdev);
 
 	if (!is_advertising_allowed(hdev, connectable))
 		return -EPERM;
