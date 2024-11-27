52d42e16f3b91255b5dcbc508b8e31ab6f93b4f3
Zhengping Jiang <jiangzp@google.com>
2024-11-26 21:21:31 -0800

    Revert "FROMLIST: Bluetooth: hci_sync: Cancel AdvMonitor interleave scan when suspend"
    This reverts commit fbcf336f2be25eecfc8cdf79c0898234ef4bdb4a.
    
    Change-Id: I30d027b80727dd60d45d7dd7eadbf5fe4bccd080
    Signed-off-by: Zhengping Jiang <jiangzp@google.com>

--- net/bluetooth/hci_sync.c
+++ net/bluetooth/hci_sync.c
@@ -2240,9 +2240,6 @@ static bool is_interleave_scanning(struct hci_dev *hdev)
 
 static void cancel_interleave_scan(struct hci_dev *hdev)
 {
-	if (!is_interleave_scanning(hdev))
-		return;
-
 	bt_dev_dbg(hdev, "cancelling interleave scan");
 
 	cancel_delayed_work_sync(&hdev->interleave_scan);
@@ -6123,9 +6120,6 @@ int hci_suspend_sync(struct hci_dev *hdev)
 
 	/* Pause other advertisements */
 	hci_pause_advertising_sync(hdev);
-
-	/* Cancel interleaved scan */
-	cancel_interleave_scan(hdev);
 
 	/* Suspend monitor filters */
 	hci_suspend_monitor_sync(hdev);
