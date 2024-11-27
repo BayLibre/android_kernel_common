ac9730b635a7e01c751ca3d3de568ca0c7b6edea
Zhengping Jiang <jiangzp@google.com>
2024-11-26 21:21:36 -0800

    Revert "CHROMIUM: Skip disabling quality report if HCI is unregistered"
    This reverts commit 744ef24641694745bf6e1936e70032276b89c26f.
    
    Change-Id: I50d57c010ec46da5c8cecc22df7d41f76ee8183a
    Signed-off-by: Zhengping Jiang <jiangzp@google.com>

--- net/bluetooth/hci_sync.c
+++ net/bluetooth/hci_sync.c
@@ -5161,8 +5161,7 @@ int hci_dev_close_sync(struct hci_dev *hdev)
 	 * is called. Otherwise, some chips may panic.
 	 */
 	if (!hci_dev_test_flag(hdev, HCI_USER_CHANNEL)) {
-		if (!hci_dev_test_flag(hdev, HCI_UNREGISTER))
-			set_quality_report(hdev, false);
+		set_quality_report(hdev, false);
 
 		aosp_do_close(hdev);
 	}
