5ba2306a779312c35f912b86d8925713c532cbcd
Yang Yang <yang.yang@vivo.com>
2024-11-27 18:00:17 +0800

    ANDROID: vendor_hooks: add hook when adding or deleting folios
    Introducing vendor hooks during the addition or deletion of folios aims
    to manage specific folios within a dedicated list, thereby ensuring a
    more precise memory reclamation process.
    
    Bug: 381203903
    Bug: 411576684
    Bug: 491161822
    Change-Id: Id0a2cd558eff2bbcef6fd49d7615fce51a6856e3
    Signed-off-by: Yang Yang <yang.yang@vivo.com>
    (cherry picked from commit 83b91498d3574a5f5388f3f2babdac27e2b38685)
    Signed-off-by: ying zuxin <11154159@vivo.com>

--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -468,6 +468,14 @@ DECLARE_HOOK(android_vh_filemap_update_page,
 	TP_PROTO(struct address_space *mapping, struct folio *folio,
 		struct file *file),
 	TP_ARGS(mapping, folio, file));
+DECLARE_HOOK(android_vh_lruvec_add_folio,
+	TP_PROTO(struct lruvec *lruvec, struct folio *folio, enum lru_list lru,
+		bool tail, bool *skip),
+	TP_ARGS(lruvec, folio, lru, tail, skip));
+DECLARE_HOOK(android_vh_lruvec_del_folio,
+	TP_PROTO(struct lruvec *lruvec, struct folio *folio, enum lru_list lru,
+		bool *skip),
+	TP_ARGS(lruvec, folio, lru, skip));
 #endif /* _TRACE_HOOK_MM_H */
 
 /* This part must be outside protection */
