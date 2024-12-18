0bbe17404b52fcb36b99d6d07cbd398b3b510032
h00013079 <huangdezhi@hihonor.com>
2024-12-18 17:28:07 +0800

    ANDROID: add vendor hook in do_read_fault to tune fault_around_bytes
    with this vendor_hook, oem can dynamically adjust fault_around_bytes to
    balance memory usage and performance
    
    Bug: 340749845
    
    Change-Id: I429f4302caf44a769696ccec84e9cc13ea8892ea
    Signed-off-by: Dezhi Huang <huangdezhi@hihonor.com>

--- mm/memory.c
+++ mm/memory.c
@@ -5072,6 +5072,8 @@ vm_fault_t finish_fault(struct vm_fault *vmf)
 
 static unsigned long fault_around_pages __read_mostly =
 	65536 >> PAGE_SHIFT;
+static unsigned long fault_around_bytes __read_mostly =
+	65536 >> PAGE_SHIFT;
 
 #ifdef CONFIG_DEBUG_FS
 static int fault_around_bytes_get(void *data, u64 *val)
@@ -5185,6 +5187,7 @@ static vm_fault_t do_read_fault(struct vm_fault *vmf)
 	vm_fault_t ret = 0;
 	struct folio *folio;
 
+	trace_android_vh_tune_fault_around_bytes(&fault_around_bytes);
 	/*
 	 * Let's call ->map_pages() first and use ->fault() as fallback
 	 * if page by the offset is not ready to be mapped (cold cache or
