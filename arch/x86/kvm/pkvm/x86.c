dc073e0d6d6494ae3b33390110d0d0f471fc58bf
Chuanxiao Dong <chuanxiao.dong@intel.com>
2024-12-25 15:08:27 +0800

    ANDROID: pkvm: vmx: Handle force_immediate_exit
    Make the preemption timer as the must have feature for the pkvm
    hyperviso so that this can be used to support the force_immediate_exit.
    With preemption timer enabled, the pkvm hypervisor can use preemption
    timer to implement a force immeidate exit instead of relying on the host
    to send an ipi. For the vmexit due to the force_immediate_exit requested
    by the host KVM, breaks the pkvm vcpu run loop and switches back to the
    host.
    
    Bug: 391539939
    Test: pKVM works
    Upstream-Task: 402758258
    
    Change-Id: I445ca6eb7e8e7d9532de23ff90e32158c2471e62
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>
    Signed-off-by: Dmytro Maluka <dmaluka@google.com>

--- arch/x86/kvm/pkvm/x86.c
+++ arch/x86/kvm/pkvm/x86.c
@@ -915,6 +915,9 @@ fastpath_t kvm_vcpu_enter_guest(struct kvm_vcpu *vcpu, bool force_immediate_exit
 
 		/* Vmeixt is handled by the pkvm hypervisor */
 		exit_fastpath = EXIT_FASTPATH_EXIT_HANDLED;
+
+		if (unlikely(force_immediate_exit))
+			break;
 	}
 
 	vcpu->arch.host_debugctl = get_debugctlmsr();
