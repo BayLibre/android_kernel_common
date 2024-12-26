71632211894182cca36cae7930f44557edd7da59
Suren Baghdasaryan <surenb@google.com>
2024-12-26 09:07:08 -0800

    FROMGIT: mm: make vma cache SLAB_TYPESAFE_BY_RCU
    To enable SLAB_TYPESAFE_BY_RCU for vma cache we need to ensure that
    object reuse before RCU grace period is over will be detected by
    lock_vma_under_rcu(). Current checks are sufficient as long as vma
    is detached before it is freed. Implement this guarantee by calling
    vma_ensure_detached() before vma is freed and make vm_area_cachep
    SLAB_TYPESAFE_BY_RCU. This will facilitate vm_area_struct reuse and
    will minimize the number of call_rcu() calls.
    
    Link: https://lkml.kernel.org/r/20241226170710.1159679-17-surenb@google.com
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Cc: Christian Brauner <brauner@kernel.org>
    Cc: David Hildenbrand <david@redhat.com>
    Cc: David Howells <dhowells@redhat.com>
    Cc: Davidlohr Bueso <dave@stgolabs.net>
    Cc: Hillf Danton <hdanton@sina.com>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: Jann Horn <jannh@google.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Jonathan Corbet <corbet@lwn.net>
    Cc: kernel test robot <oliver.sang@intel.com>
    Cc: Klara Modin <klarasmodin@gmail.com>
    Cc: Liam R. Howlett <Liam.Howlett@Oracle.com>
    Cc: Lokesh Gidra <lokeshgidra@google.com>
    Cc: Lorenzo Stoakes <lorenzo.stoakes@oracle.com>
    Cc: Mateusz Guzik <mjguzik@gmail.com>
    Cc: Matthew Wilcox (Oracle) <willy@infradead.org>
    Cc: Mel Gorman <mgorman@techsingularity.net>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Minchan Kim <minchan@google.com>
    Cc: Oleg Nesterov <oleg@redhat.com>
    Cc: Pasha Tatashin <pasha.tatashin@soleen.com>
    Cc: Paul E. McKenney <paulmck@kernel.org>
    Cc: Peter Xu <peterx@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Shakeel Butt <shakeel.butt@linux.dev>
    Cc: Sourav Panda <souravpanda@google.com>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    (cherry picked from commit 1a67b3a3c64928df286fc1353b14b5455746174d
    https: //git.kernel.org/pub/scm/linux/kernel/git/akpm/mm.git mm-unstable)
    Bug: 322132947
    Change-Id: I410c6fbce2e0d87ed5f7c19dc1f8806b2556837a
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- include/linux/mm_types.h
+++ include/linux/mm_types.h
@@ -561,6 +561,12 @@ struct page_frag_cache {
 typedef unsigned long vm_flags_t;
 
 /*
+ * freeptr_t represents a SLUB freelist pointer, which might be encoded
+ * and not dereferenceable if CONFIG_SLAB_FREELIST_HARDENED is enabled.
+ */
+typedef struct { unsigned long v; } freeptr_t;
+
+/*
  * A region containing a mapping of a non-memory backed file under NOMMU
  * conditions.  These are held in a global tree and are pinned by the VMAs that
  * map parts of them.
@@ -673,9 +679,7 @@ struct vm_area_struct {
 			unsigned long vm_start;
 			unsigned long vm_end;
 		};
-#ifdef CONFIG_PER_VMA_LOCK
-		struct rcu_head vm_rcu;	/* Used for deferred freeing. */
-#endif
+		freeptr_t vm_freeptr; /* Pointer used by SLAB_TYPESAFE_BY_RCU */
 	};
 
 	/*
