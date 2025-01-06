c4ca8041956d56dfe06b0d32235357941ee3b40c
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2025-01-06 09:47:06 -0800

    Revert "ANDROID: fix incorrect TRACE_INCLUDE_PATH in sched/build..."
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/45db6536-6fc2-4d2b-9a68-c472480b5787). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: I8fa8f1669829ba359077e0a2b2ac395906eff135
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com

--- kernel/sched/cputime.c
+++ kernel/sched/cputime.c
@@ -4,7 +4,6 @@
  */
 #include <linux/cpufreq_times.h>
 #include <trace/hooks/sched.h>
-#undef TRACE_INCLUDE_PATH
 
 #ifdef CONFIG_VIRT_CPU_ACCOUNTING_NATIVE
  #include <asm/cputime.h>
--- kernel/sched/syscalls.c
+++ kernel/sched/syscalls.c
@@ -17,7 +17,6 @@
 #include "autogroup.h"
 
 #include <trace/hooks/sched.h>
-#undef TRACE_INCLUDE_PATH
 
 static inline int __normal_prio(int policy, int rt_prio, int nice)
 {
