68644b60e957328a465f3122f2473ac44bcd92c1
Vincent Donnefort <vdonnefort@google.com>
2025-01-13 15:42:07 +0000

    [DONOTMERGE] DEBUG!
    Change-Id: If38688af020a0d7d210878d5565789964af1cd23

--- arch/arm64/kvm/hyp/nvhe/mem_protect.c
+++ arch/arm64/kvm/hyp/nvhe/mem_protect.c
@@ -533,10 +533,10 @@ static bool range_is_allowed_memory(u64 start, u64 end)
 	if (!reg)
 		return false;
 
-	if (!is_in_mem_range(end - 1, &range))
+	if (reg->flags & MEMBLOCK_NOMAP)
 		return false;
 
-	return !(reg->flags & MEMBLOCK_NOMAP);
+	return range_is_allowed_memory(range->end, end);
 }
 
 static inline int __host_stage2_idmap(u64 start, u64 end,
@@ -1076,6 +1076,7 @@ static int guest_request_walker(const struct kvm_pgtable_visit_ctx *ctx,
 	u32 level = ctx->level;
 
 	state = guest_get_page_state(pte, 0);
+
 	if (data->desired_state != state)
 		return (state & PKVM_NOPAGE) ? -EFAULT : -EPERM;
 
@@ -1083,14 +1084,22 @@ static int guest_request_walker(const struct kvm_pgtable_visit_ctx *ctx,
 
 	if (!data->size) {
 		size_t size_orig = ctx->end - ctx->start;
-
-		if (kvm_pte_valid(pte) &&
-		    !range_is_allowed_memory(phys, phys + size_orig))
-			return -EINVAL;
+		phys_addr_t phys_orig;
 
 		data->pte_start = pte;
 		data->size = kvm_granule_size(level);
 		data->ipa_start = ctx->addr & ~(kvm_granule_size(level) - 1);
+
+		phys_orig = data->ipa_start - ctx->addr + phys;
+
+		trace_hyp_printk("guest_request_walker: ipa_start=0x%llx phys=0x%llx size_orig=%lu allowed=%d",
+				data->ipa_start, phys_orig, size_orig,
+				range_is_allowed_memory(phys_orig, phys_orig + size_orig));
+
+		if (kvm_pte_valid(pte) &&
+		    !range_is_allowed_memory(phys_orig, phys_orig + size_orig))
+			return -EINVAL;
+
 		goto end;
 	}
 
@@ -1249,6 +1258,9 @@ int __pkvm_guest_share_host(struct pkvm_hyp_vcpu *vcpu, u64 ipa, u64 nr_pages,
 unlock:
 	guest_unlock_component(vm);
 	host_unlock_component();
+
+	trace_hyp_printk("__pkvm_guest_share_host: ipa=0x%llx nr_pages=%llu ret=%d nr_shared=%llu",
+			 ipa, nr_pages, ret, *nr_shared);
 
 	return ret;
 }
