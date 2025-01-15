5efa95de72d414d7f08c2cc40c113b64ec8af8c7
Ilan Peer <ilan.peer@intel.com>
2025-01-15 14:54:10 +0530

    BACKPORT: wifi: cfg80211: Support association to AP MLD with disabled links
    An AP part of an AP MLD might be temporarily disabled, and might be
    enabled later. Such a link should be included in the association
    exchange, but should not be used until enabled.
    
    Extend the NL80211_CMD_ASSOCIATE to also indicate disabled links.
    
    Signed-off-by: Ilan Peer <ilan.peer@intel.com>
    Signed-off-by: Gregory Greenman <gregory.greenman@intel.com>
    Link: https://lore.kernel.org/r/20230608163202.c4c61ee4c4a5.I784ef4a0d619fc9120514b5615458fbef3b3684a@changeid
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Change-Id: I53d4364ecca3ff1bbbd68276569131b90341204d
    (cherry picked from commit 6cf963edbbd3b279185e28e4864c9698ffaa23c3)
    Signed-off-by: Krupali Dhanvijay <quic_kdhanvij@quicinc.com>

--- include/uapi/linux/nl80211.h
+++ include/uapi/linux/nl80211.h
@@ -11,7 +11,7 @@
  * Copyright 2008 Jouni Malinen <jouni.malinen@atheros.com>
  * Copyright 2008 Colin McCabe <colin@cozybit.com>
  * Copyright 2015-2017	Intel Deutschland GmbH
- * Copyright (C) 2018-2022 Intel Corporation
+ * Copyright (C) 2018-2023 Intel Corporation
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -2829,6 +2829,9 @@ enum nl80211_commands {
  *	index. If the userspace includes more RNR elements than number of
  *	MBSSID elements then these will be added in every EMA beacon.
  *
+ * @NL80211_ATTR_MLO_LINK_DISABLED: Flag attribute indicating that the link is
+ *	disabled.
+ *
  * @NUM_NL80211_ATTR: total number of nl80211_attrs available
  * @NL80211_ATTR_MAX: highest attribute number currently defined
  * @__NL80211_ATTR_AFTER_LAST: internal use
@@ -3368,7 +3371,8 @@ enum nl80211_attrs {
 
 	NL80211_ATTR_EMA_RNR_ELEMS,
 
-	NL80211_ATTR_ANDROID_KABI_RESERVED_4,
+	NL80211_ATTR_MLO_LINK_DISABLED,
+
 	NL80211_ATTR_ANDROID_KABI_RESERVED_5,
 	NL80211_ATTR_ANDROID_KABI_RESERVED_6,
 	NL80211_ATTR_ANDROID_KABI_RESERVED_7,
@@ -3393,7 +3397,7 @@ enum nl80211_attrs {
 
 	/* add attributes here, update the policy in nl80211.c */
 
-	__NL80211_ATTR_MAX_IMPLEMENTED = NL80211_ATTR_EMA_RNR_ELEMS,
+	__NL80211_ATTR_MAX_IMPLEMENTED = NL80211_ATTR_MLO_LINK_DISABLED,
 	__NL80211_ATTR_AFTER_LAST,
 	NUM_NL80211_ATTR = __NL80211_ATTR_AFTER_LAST,
 	NL80211_ATTR_MAX = __NL80211_ATTR_AFTER_LAST - 1
