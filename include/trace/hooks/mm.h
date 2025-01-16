7be6a3c33b70bf7425b0d91db841914cac721f4a
yipeng xiang <yipengxiang@honor.corp-partner.google.com>
2025-01-16 15:48:46 +0800

    ANDROID: mm: add vendor hook for vmalloc and vfree
    Add vendor hook to avoid waste associated with  vmalloc allocations smaller than the page size.
    
    Bug: 390314831
    Change-Id: Ic5ee975cd4c705a6634a07b18fed358c82682e45
    Signed-off-by: yipeng xiang <yipengxiang@honor.corp-partner.google.com>
    (cherry picked from commit 745b77b51f3e2b628597dd8661bea78252f3611c)

--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -29,6 +29,12 @@ DECLARE_RESTRICTED_HOOK(android_rvh_set_readahead_gfp_mask,
 			TP_ARGS(flags), 1);
 
 */
+DECLARE_RESTRICTED_HOOK(android_rvh_vmalloc_node_bypass,
+	TP_PROTO(unsigned long size, gfp_t gfp_mask, void **addr),
+	TP_ARGS(size, gfp_mask, addr), 1);
+DECLARE_RESTRICTED_HOOK(android_rvh_vfree_bypass,
+	TP_PROTO(const void *addr, bool *bypass),
+	TP_ARGS(addr, bypass), 1);
 #endif /* _TRACE_HOOK_MM_H */
 
 /* This part must be outside protection */
