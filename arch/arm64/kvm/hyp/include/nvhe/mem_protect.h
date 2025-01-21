8eb1f060ada8773db6064ab4a9c53a0f0d6403c8
Vincent Donnefort <vdonnefort@google.com>
2025-01-21 10:38:03 +0000

    ANDROID: KVM: arm64: Introduce __pkvm_host_donate_guest_sglist
    With the extension of the guest mem abort path to support ranges, it
    makes sense for the hypervisor to also support range. This will also
    later allow to group IOMMU operations within a single 'atomic' unmap and
    limit the number of necessary TLBI.
    
    For the non-protected guest case, the memory is simply shared. This
    operation is fast and no significant gain would be achieved from using a
    range version.
    
    Bug: 357781595
    Bug: 361026567
    Change-Id: I6fd59d023399743369ce2eec216cc155e15d8bb6
    Signed-off-by: Vincent Donnefort <vdonnefort@google.com>

--- arch/arm64/kvm/hyp/include/nvhe/mem_protect.h
+++ arch/arm64/kvm/hyp/include/nvhe/mem_protect.h
@@ -48,6 +48,7 @@ int __pkvm_hyp_donate_host(u64 pfn, u64 nr_pages);
 int __pkvm_host_share_ffa(u64 pfn, u64 nr_pages);
 int __pkvm_host_unshare_ffa(u64 pfn, u64 nr_pages);
 int __pkvm_host_donate_guest(u64 pfn, u64 gfn, struct pkvm_hyp_vcpu *vcpu, u64 nr_pages);
+int __pkvm_host_donate_sglist_guest(struct pkvm_hyp_vcpu *vcpu, unsigned long host_sglist);
 int __pkvm_host_share_guest(u64 pfn, u64 gfn, struct pkvm_hyp_vcpu *vcpu,
 			    enum kvm_pgtable_prot prot, u64 nr_pages);
 int __pkvm_host_unshare_guest(u64 gfn, struct pkvm_hyp_vm *vm, u64 nr_pages);
