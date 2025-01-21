d52db3648ab075d2ca6174a925bf37fcc67aec4a
Vincent Donnefort <vdonnefort@google.com>
2025-01-21 10:38:03 +0000

    ANDROID: KVM: arm64: Introduce __pkvm_host_donate_guest_sglist
    With the extension of the guest mem abort path to support ranges, it
    makes sense for the hypervisor to also support range. This will also
    later allow to group IOMMU operations within a single 'atomic' unmap and
    limit the number of necessary TLBI.
    
    For the non-protected guest case, the memory is simply shared. This
    operation is fast and no significant gain would be achieved from using a
    range version.
    
    Bug: 357781595
    Bug: 361026567
    Change-Id: I6fd59d023399743369ce2eec216cc155e15d8bb6
    Signed-off-by: Vincent Donnefort <vdonnefort@google.com>

--- arch/arm64/kvm/hyp/nvhe/mem_protect.c
+++ arch/arm64/kvm/hyp/nvhe/mem_protect.c
@@ -2012,12 +2012,35 @@ kvm_pte_t __pkvm_host_mkyoung_guest(u64 gfn, struct pkvm_hyp_vcpu *vcpu)
 	return pte;
 }
 
+static int __host_donate_guest(struct pkvm_hyp_vcpu *vcpu, u64 phys, u64 ipa, size_t size)
+{
+	struct pkvm_hyp_vm *vm = pkvm_hyp_vcpu_to_hyp_vm(vcpu);
+	u64 nr_pages = size >> PAGE_SHIFT;
+	enum kvm_pgtable_prot prot;
+	int ret;
+
+	WARN_ON(host_stage2_set_owner_locked(phys, size, PKVM_ID_GUEST));
+	psci_mem_protect_inc(nr_pages);
+	if (pkvm_ipa_range_has_pvmfw(vm, ipa, ipa + size)) {
+		ret = pkvm_load_pvmfw_pages(vm, ipa, phys, size);
+		if (WARN_ON(ret)) {
+			psci_mem_protect_dec(nr_pages);
+			return ret;
+		}
+	}
+
+	prot = pkvm_mkstate(KVM_PGTABLE_PROT_RWX, PKVM_PAGE_OWNED);
+	WARN_ON(kvm_pgtable_stage2_map(&vm->pgt, ipa, size, phys, prot,
+				       &vcpu->vcpu.arch.stage2_mc, 0));
+
+	return 0;
+}
+
 int __pkvm_host_donate_guest(u64 pfn, u64 gfn, struct pkvm_hyp_vcpu *vcpu, u64 nr_pages)
 {
 	struct pkvm_hyp_vm *vm = pkvm_hyp_vcpu_to_hyp_vm(vcpu);
 	u64 phys = hyp_pfn_to_phys(pfn);
 	u64 ipa = hyp_pfn_to_phys(gfn);
-	enum kvm_pgtable_prot prot;
 	size_t size;
 	int ret;
 
@@ -2034,22 +2057,83 @@ int __pkvm_host_donate_guest(u64 pfn, u64 gfn, struct pkvm_hyp_vcpu *vcpu, u64 n
 	if (ret)
 		goto unlock;
 
-	WARN_ON(host_stage2_set_owner_locked(phys, size, PKVM_ID_GUEST));
-	psci_mem_protect_inc(nr_pages);
-	if (pkvm_ipa_range_has_pvmfw(vm, ipa, ipa + size)) {
-		ret = pkvm_load_pvmfw_pages(vm, ipa, phys, size);
-		if (WARN_ON(ret)) {
-			psci_mem_protect_dec(nr_pages);
-			goto unlock;
-		}
-	}
-	prot = pkvm_mkstate(KVM_PGTABLE_PROT_RWX, PKVM_PAGE_OWNED);
-	WARN_ON(kvm_pgtable_stage2_map(&vm->pgt, ipa, size, phys, prot,
-				       &vcpu->vcpu.arch.stage2_mc, 0));
+	WARN_ON(__host_donate_guest(vcpu, phys, ipa, size));
 
 unlock:
 	guest_unlock_component(vm);
 	host_unlock_component();
+
+	return ret;
+}
+
+#define for_each_ppage(ppage, sglist) 					\
+	for (ppage = next_kvm_hyp_pinned_page(sglist, NULL, true);	\
+	     ppage;							\
+	     ppage = next_kvm_hyp_pinned_page(sglist, ppage, true))
+
+int __pkvm_host_donate_sglist_guest(struct pkvm_hyp_vcpu *vcpu, unsigned long host_sglist)
+{
+	struct pkvm_hyp_vm *vm = pkvm_hyp_vcpu_to_hyp_vm(vcpu);
+	struct kvm_hyp_pinned_page *ppage, *sglist;
+	bool is_memory;
+	int ret;
+
+	if (!PAGE_ALIGNED(host_sglist))
+		return -EINVAL;
+
+	sglist = (struct kvm_hyp_pinned_page *)kern_hyp_va(host_sglist);
+	ret = __pkvm_host_donate_hyp(hyp_virt_to_pfn(sglist), 1);
+	if (ret)
+		return ret;
+
+	host_lock_component();
+	guest_lock_component(vm);
+
+	ppage = next_kvm_hyp_pinned_page(sglist, NULL, true);
+	if (!ppage) {
+		ret = -EINVAL;
+		goto unlock;
+	}
+
+	is_memory = addr_is_memory(hyp_pfn_to_phys(ppage->pfn));
+
+	for_each_ppage(ppage, sglist) {
+		u64 phys = hyp_pfn_to_phys(ppage->pfn);
+		u64 ipa = hyp_pfn_to_phys(ppage->gfn);
+		size_t size;
+
+		if (check_shl_overflow(PAGE_SIZE, ppage->order, &size)) {
+			ret = -EINVAL;
+			goto unlock;
+		}
+
+		if (addr_is_memory(phys) != is_memory) {
+			ret = -EINVAL;
+			goto unlock;
+		}
+
+		ret = __host_check_page_state_range(phys, size, PKVM_PAGE_OWNED);
+		if (ret)
+			goto unlock;
+
+		ret = __guest_check_page_state_range(vcpu, ipa, size, PKVM_NOPAGE);
+		if (ret)
+			goto unlock;
+	}
+
+	for_each_ppage(ppage, sglist) {
+		size_t size = PAGE_SIZE << ppage->order;
+		u64 phys = hyp_pfn_to_phys(ppage->pfn);
+		u64 ipa = hyp_pfn_to_phys(ppage->gfn);
+
+		WARN_ON(__host_donate_guest(vcpu, phys, ipa, size));
+	}
+
+unlock:
+	guest_unlock_component(vm);
+	host_unlock_component();
+
+	WARN_ON(__pkvm_hyp_donate_host(hyp_virt_to_pfn(sglist), 1));
 
 	return ret;
 }
