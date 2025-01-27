382e5bede8f393fce0b9c22f8865f3f1dbedb3d3
Mostafa Saleh <smostafa@google.com>
2025-01-27 17:22:46 +0000

    ANDROID: KVM: arm64: Add  __pkvm_remove_ioguard_page()
    Bug: 357781595
    Bug: 348382247
    Change-Id: I461a33cb9b8275a3dcfe88ce8501e11ae49dd954
    Signed-off-by: Mostafa Saleh <smostafa@google.com>

--- arch/arm64/kvm/hyp/include/nvhe/mem_protect.h
+++ arch/arm64/kvm/hyp/include/nvhe/mem_protect.h
@@ -63,6 +63,7 @@ int __pkvm_guest_unshare_host(struct pkvm_hyp_vcpu *hyp_vcpu, u64 ipa,
 int __pkvm_install_ioguard_page(struct pkvm_hyp_vcpu *hyp_vcpu, u64 ipa,
 				u64 nr_pages, u64 *nr_guarded);
 bool __pkvm_check_ioguard_page(struct pkvm_hyp_vcpu *hyp_vcpu);
+int __pkvm_remove_ioguard_page(struct pkvm_hyp_vcpu *hyp_vcpu, u64 ipa);
 int __pkvm_guest_relinquish_to_host(struct pkvm_hyp_vcpu *vcpu,
 				    u64 ipa, u64 *ppa);
 int __pkvm_host_use_dma(u64 phys_addr, size_t size);
