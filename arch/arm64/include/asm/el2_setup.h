a934ccc59b6618a5a960e4f76edc80cf8200bca0
Fuad Tabba <tabba@google.com>
2025-02-04 16:03:54 +0000

    ANDROID: arm64: head.S: Do not trap access to MPAMSM_EL1
    Bit 50 of MPAM2_EL2, EnMPAMSM, traps when 0. Therefore, set it
    for the host to ensure that traps are disabled.
    
    Fixes: 42e68111456bb ("arm64: head.S: Initialise MPAM EL2 registers and disable traps")
    Fixes: dfec746328449 ("KVM: arm64: Fix missing traps of guest accesses to the MPAM registers")
    Change-Id: I10a322fb9b831a83201a26399c0e43c9b0e90088
    Signed-off-by: Fuad Tabba <tabba@google.com>
    Bug: 338172522
    Bug: 357781595
    
    -----
    
    Will submit it to the list soon.
    
    Change-Id: I10a322fb9b831a83201a26399c0e43c9b0e90088

--- arch/arm64/include/asm/el2_setup.h
+++ arch/arm64/include/asm/el2_setup.h
@@ -225,8 +225,9 @@
 	mrs	x1, id_aa64pfr0_el1
 	ubfx	x0, x1, #ID_AA64PFR0_EL1_MPAM_SHIFT, #4
 	cbz	x0, .Lskip_mpam_\@		// skip if no MPAM
-	msr_s	SYS_MPAM2_EL2, xzr		// use the default partition
-						// and disable lower traps
+	msr_s	SYS_MPAM2_EL2, #MPAM2_HOST_FLAGS	// use the default partition
+							// and disable lower traps
+							// don't trap access to MPAMSM_EL1
 	mrs_s	x0, SYS_MPAMIDR_EL1
 	tbz	x0, #MPAMIDR_EL1_HAS_HCR_SHIFT, .Lskip_mpam_\@	// skip if no MPAMHCR reg
 	msr_s	SYS_MPAMHCR_EL2, xzr		// clear TRAP_MPAMIDR_EL1 -> EL2
