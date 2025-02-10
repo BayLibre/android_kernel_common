7932d9ccf20f8b430a31e7dc3a28ced9257b0029
Mark Rutland <mark.rutland@arm.com>
2025-02-10 19:52:20 +0000

    BACKPORT: KVM: arm64: Remove host FPSIMD saving for non-protected KVM
    [ Upstream commit 8eca7f6d5100b6997df4f532090bc3f7e0203bef ]
    
    Now that the host eagerly saves its own FPSIMD/SVE/SME state,
    non-protected KVM never needs to save the host FPSIMD/SVE/SME state,
    and the code to do this is never used. Protected KVM still needs to
    save/restore the host FPSIMD/SVE state to avoid leaking guest state to
    the host (and to avoid revealing to the host whether the guest used
    FPSIMD/SVE/SME), and that code needs to be retained.
    
    Remove the unused code and data structures.
    
    To avoid the need for a stub copy of kvm_hyp_save_fpsimd_host() in the
    VHE hyp code, the nVHE/hVHE version is moved into the shared switch
    header, where it is only invoked when KVM is in protected mode.
    
    Bug: 357781595
    Bug: 411040189
    Change-Id: I0088db7c5f75c9331956867040b8eb69976aabf8
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Reviewed-by: Mark Brown <broonie@kernel.org>
    Tested-by: Mark Brown <broonie@kernel.org>
    Acked-by: Will Deacon <will@kernel.org>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Fuad Tabba <tabba@google.com>
    Cc: Marc Zyngier <maz@kernel.org>
    Cc: Oliver Upton <oliver.upton@linux.dev>
    Reviewed-by: Oliver Upton <oliver.upton@linux.dev>
    Link: https://lore.kernel.org/r/20250210195226.1215254-3-mark.rutland@arm.com
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/arm64/kvm/hyp/nvhe/hyp-main.c
+++ arch/arm64/kvm/hyp/nvhe/hyp-main.c
@@ -547,7 +547,7 @@ static void fpsimd_sve_sync(struct kvm_vcpu *vcpu)
 	if (system_supports_sve())
 		__hyp_sve_restore_host();
 	else
-		__fpsimd_restore_state(*host_data_ptr(fpsimd_state));
+		__fpsimd_restore_state(host_data_ptr(host_ctxt.fp_regs));
 
 	if (has_fpmr)
 		write_sysreg_s(*host_data_ptr(fpmr), SYS_FPMR);
@@ -915,7 +915,7 @@ static void fpsimd_host_restore(struct kvm_vcpu *vcpu)
 	else
 		__fpsimd_save_state(&vcpu->arch.ctxt.fp_regs);
 
-	__fpsimd_restore_state(*host_data_ptr(fpsimd_state));
+	__fpsimd_restore_state(host_data_ptr(host_ctxt.fp_regs));
 
 	*host_data_ptr(fp_owner) = FP_STATE_HOST_OWNED;
 
