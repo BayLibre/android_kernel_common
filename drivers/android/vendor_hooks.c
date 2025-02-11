321bd113464feffc6cd974ae0b26c1df05120feb
DANGJian <dangjian@honor.corp-partner.google.com>
2025-02-11 20:48:48 +0800

    ANDROID: mm: add vendor hook in file cache fault and read file
    We want to collect data of file cache accessing pattern in each
    performance-sensitive scenario like application start-up.
    With acquired cache accessing pattern for each concerned scenario,
    we can custom policy for file cache pre-fetch & protection as well
    as adjust file cache capacity for specific scenario.
    
    Bug: 395822957
    Change-Id: Iec01eea30ff44e93638b8d80cdcfc5238c0d383b
    Signed-off-by: DANGJian <dangjian@honor.corp-partner.google.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -304,3 +304,6 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_smaps_pte_entry);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_show_smap);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rebalance_anon_lru_bypass);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_trigger_vendor_lmk_kill);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_do_read_fault);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_filemap_read);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_filemap_map_pages);
--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -188,6 +188,16 @@ DECLARE_HOOK(android_vh_show_smap,
 		unsigned long swap_shared, unsigned long writeback,
 		unsigned long same, unsigned long huge),
 	TP_ARGS(m, swap_shared, writeback, same, huge));
+DECLARE_HOOK(android_vh_do_read_fault,
+	TP_PROTO(struct vm_fault *vmf, unsigned long fault_around_bytes),
+	TP_ARGS(vmf, fault_around_bytes));
+DECLARE_HOOK(android_vh_filemap_read,
+	TP_PROTO(struct file *file, loff_t pos, size_t size),
+	TP_ARGS(file, pos, size));
+DECLARE_HOOK(android_vh_filemap_map_pages,
+	TP_PROTO(struct file *file, pgoff_t first_pgoff,
+		pgoff_t last_pgoff, vm_fault_t ret),
+	TP_ARGS(file, first_pgoff, last_pgoff, ret));
 #endif /* _TRACE_HOOK_MM_H */
 
 /* This part must be outside protection */
