fa5dc249143c7996beaf12b8babb4216d0cf7596
Prakruthi Deepak Heragu <quic_pheragu@quicinc.com>
2025-02-24 15:02:59 -0800

    ANDROID: gunyah: vcpu_mgr: Add trace_hooks in the gunyah_vcpu driver
    As QTVMs use Gunyah emulated watchdog instead of vcpu_stall detector,
    provide trace_hooks to manage the watchdog of the QTVM.
    
    Bug: 399219478
    Change-Id: I6ec8a0236835dcc20c66681a747eae09c0c1a32d
    Signed-off-by: Prakruthi Deepak Heragu <quic_pheragu@quicinc.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -37,6 +37,7 @@
 #include <trace/hooks/syscall_check.h>
 #include <trace/hooks/gic.h>
 #include <trace/hooks/gic_v3.h>
+#include <trace/hooks/gunyah.h>
 #include <trace/hooks/remoteproc.h>
 #include <trace/hooks/reboot.h>
 #include <trace/hooks/psci.h>
@@ -132,6 +133,8 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_cpuidle_psci_enter);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_cpuidle_psci_exit);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_cpufreq_transition);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_gic_v3_set_affinity);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_gh_before_vcpu_run);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_gh_after_vcpu_run);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_proc_transaction_finish);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_sync_txn_recvd);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_set_balance_anon_file_reclaim);
