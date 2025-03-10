83c20db596d436a6bbe4156e0fe995d3dd8e7df6
Jaegeuk Kim <jaegeuk@google.com>
2025-03-10 10:20:12 -0700

    Revert "ANDROID: f2fs: introduce sanity_check sysfs entry"
    Revert submission 3516777-inconsistent_footer
    
    Reason for revert: Roll back after test
    
    Reverted changes: /q/submissionid:3516777-inconsistent_footer
    
    Change-Id: I8782583cca85438bd3a56f15940bc8753915cd2f

--- fs/f2fs/sysfs.c
+++ fs/f2fs/sysfs.c
@@ -1122,8 +1122,6 @@ F2FS_SBI_GENERAL_RW_ATTR(max_read_extent_count);
 F2FS_SBI_GENERAL_RO_ATTR(unusable_blocks_per_sec);
 F2FS_SBI_GENERAL_RW_ATTR(blkzone_alloc_policy);
 #endif
-/* enable sanity check to dump more metadata info */
-F2FS_SBI_GENERAL_RW_ATTR(sanity_check);
 
 /* STAT_INFO ATTR */
 #ifdef CONFIG_F2FS_STAT_FS
@@ -1311,7 +1309,6 @@ static struct attribute *f2fs_attrs[] = {
 	ATTR_LIST(warm_data_age_threshold),
 	ATTR_LIST(last_age_weight),
 	ATTR_LIST(max_read_extent_count),
-	ATTR_LIST(sanity_check),
 	NULL,
 };
 ATTRIBUTE_GROUPS(f2fs);
