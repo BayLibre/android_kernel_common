894577fc9a42cc65bf0e2d830048804f61f59d21
Masami Hiramatsu (Google) <mhiramat@kernel.org>
2025-03-11 10:30:03 +0900

    UPSTREAM: tracing: Skip update_last_data() if cleared and remove active check for save_mod()
    If the last boot data is already cleared, there is no reason to update it
    again. Skip if the TRACE_ARRAY_FL_LAST_BOOT is cleared.
    Also, for calling save_mod() when module loading, we don't need to check
    the trace is active or not because any module address can be on the
    stacktrace.
    
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Link: https://lore.kernel.org/174165660328.1173316.15529357882704817499.stgit@devnote2
    Signed-off-by: Masami Hiramatsu (Google) <mhiramat@kernel.org>
    Signed-off-by: Steven Rostedt (Google) <rostedt@goodmis.org>
    (cherry picked from commit 486fbcb3806c0c7a5dbeea326c4a146fd4ed4eff)
    
    Bug: 466891640
    Test: Build and boot test with persistent ring buffer
    
    Change-Id: I7f993c8804023571ed12569d8547331cb46e9da2
    Signed-off-by: Masami Hiramatsu <mhiramat@google.com>

--- kernel/trace/trace.c
+++ kernel/trace/trace.c
@@ -6059,6 +6059,12 @@ static void update_last_data(struct trace_array *tr)
 	if (!(tr->flags & TRACE_ARRAY_FL_BOOT))
 		return;
 
+	if (!(tr->flags & TRACE_ARRAY_FL_LAST_BOOT))
+		return;
+
+	/* Only if the buffer has previous boot data clear and update it. */
+	tr->flags &= ~TRACE_ARRAY_FL_LAST_BOOT;
+
 	/* Reset the module list and reload them */
 	tscratch = trace_array_scratch(tr, NULL);
 	if (tscratch) {
@@ -6069,9 +6075,6 @@ static void update_last_data(struct trace_array *tr)
 		guard(mutex)(&scratch_mutex);
 		module_for_each_mod(save_mod, tr);
 	}
-
-	if (!(tr->flags & TRACE_ARRAY_FL_LAST_BOOT))
-		return;
 
 	/*
 	 * Need to clear all CPU buffers as there cannot be events
@@ -6094,7 +6097,6 @@ static void update_last_data(struct trace_array *tr)
 #else
 	tscratch->kaslr_addr = 0;
 #endif
-	tr->flags &= ~TRACE_ARRAY_FL_LAST_BOOT;
 }
 
 /**
@@ -10093,15 +10095,6 @@ static void trace_module_remove_evals(struct module *mod)
 static inline void trace_module_remove_evals(struct module *mod) { }
 #endif /* CONFIG_TRACE_EVAL_MAP_FILE */
 
-static bool trace_array_active(struct trace_array *tr)
-{
-	if (tr->current_trace != &nop_trace)
-		return true;
-
-	/* 0 is no events, 1 is all disabled */
-	return trace_events_enabled(tr, NULL) > 1;
-}
-
 static void trace_module_record(struct module *mod)
 {
 	struct trace_array *tr;
@@ -10110,11 +10103,8 @@ static void trace_module_record(struct module *mod)
 		/* Update any persistent trace array that has already been started */
 		if ((tr->flags & (TRACE_ARRAY_FL_BOOT | TRACE_ARRAY_FL_LAST_BOOT)) ==
 		    TRACE_ARRAY_FL_BOOT) {
-			/* Only update if the trace array is active */
-			if (trace_array_active(tr)) {
-				guard(mutex)(&scratch_mutex);
-				save_mod(mod, tr);
-			}
+			guard(mutex)(&scratch_mutex);
+			save_mod(mod, tr);
 		}
 	}
 }
