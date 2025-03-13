f747a272b444e68e6d9c57268aab29f9b1736ec0
Alice Ryhl <aliceryhl@google.com>
2025-03-13 09:17:10 +0000

    ANDROID: rust_binder: move Rust Binder in preparation for GKI module
    This moves the code from the common-modules/binder branch verbatim. The
    code is not yet added to the build system as it requires some adjusments
    before it can built as a GKI module.
    
    Bug: 388786466
    Change-Id: I6b49b1b6ff0e35fbae2b9efc13d6bbde984b5196
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>

---
+++ drivers/android/binder/my_mod.rs
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-2.0
+
+use super::*;
+
+pub(crate) fn make_numbers() -> Result<Vec<i32>> {
+    let mut numbers = Vec::new();
+    numbers.push(72, GFP_KERNEL)?;
+    numbers.push(108, GFP_KERNEL)?;
+    numbers.push(200, GFP_KERNEL)?;
+    Ok(numbers)
+}
