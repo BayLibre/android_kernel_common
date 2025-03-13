aeb2d9a3164d522e9ff6bc46143025f0a1b0272b
Dao Huang <huangdao1@oppo.com>
2025-03-13 16:08:01 +0800

    ANDROID: OGKI: Upstream vendorhooks to enable sched_ext in OGKI kernel.
    As discussed in 401436689, Add some vendor hooks to facilitate
    enabling sched_ext in subsequent OGKI kernels.
    
    Bug: 401436689
    
    Change-Id: I78321c918e03ff28a2c74c668e86436c0e416d66
    Signed-off-by: Dao Huang <huangdao1@oppo.com>

--- include/trace/hooks/sched.h
+++ include/trace/hooks/sched.h
@@ -285,6 +285,28 @@ DECLARE_RESTRICTED_HOOK(android_rvh_after_dequeue_task,
 	TP_PROTO(struct rq *rq, struct task_struct *p, int flags, bool *dequeue_task_result),
 	TP_ARGS(rq, p, flags, dequeue_task_result), 1);
 
+DECLARE_HOOK(android_vh_task_should_scx,
+	TP_PROTO(int *should_scx, int policy, int prio),
+	TP_ARGS(should_scx, policy, prio));
+DECLARE_HOOK(android_vh_scx_ops_consider_migration,
+	TP_PROTO(bool *consider_migration),
+	TP_ARGS(consider_migration));
+DECLARE_HOOK(android_vh_fix_prev_keep_slice,
+	TP_PROTO(struct task_struct *p),
+	TP_ARGS(p));
+DECLARE_HOOK(android_vh_scx_ops_enable_state,
+	TP_PROTO(int state),
+	TP_ARGS(state));
+DECLARE_HOOK(android_vh_scx_enabled,
+	TP_PROTO(int enabled),
+	TP_ARGS(enabled));
+DECLARE_HOOK(android_vh_set_cpus_allowed_scx,
+	TP_PROTO(struct task_struct *p, struct affinity_context *ac, int *done),
+	TP_ARGS(p, ac, done));
+DECLARE_HOOK(android_vh_task_switch_finish,
+	TP_PROTO(struct task_struct *p, int enable),
+	TP_ARGS(p, enable));
+
 struct cfs_rq;
 struct sched_entity;
 struct rq_flags;
