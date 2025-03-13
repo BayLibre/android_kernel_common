742e8fabf2155785bf3f0f470381475933169eb0
Dmytro Maluka <dmaluka@google.com>
2025-03-13 14:37:20 +0000

    DO-NOT-MERGE: HACK: pkvm: x86: Handle guest EPT violation fully in host
    Now that KVM-high never adds any PTEs to its vEPT, the pKVM's logic for
    guest EPT violation handling always results in forwarding it to KVM-high
    anyway (since pkvm_handle_shadow_ept_violation() never finds the PTE in
    the vept), we can remove this logic i.e. just forward it to KVM-high
    right away.
    
    Jaszczyk: apply relevant changes to arch/x86/kvm/vmx/pkvm/hyp/ept.c
    instead of arch/x86/kvm/vmx/pkvm/hyp/nested.c. Also do not drop entire
    handle_ept_violation handler, just return 0 and pass it to host for
    further handling.
    
    Change-Id: Icea9a21f9f89f1f341955b8c42ee7c42c5b720bd
    Signed-off-by: Dmytro Maluka <dmaluka@google.com>
    Signed-off-by: Grzegorz Jaszczyk <jaszczyk@google.com>

--- arch/x86/kvm/vmx/pkvm/hyp/ept.h
+++ arch/x86/kvm/vmx/pkvm/hyp/ept.h
@@ -39,8 +39,6 @@ int pkvm_shadow_ept_init(struct shadow_ept_desc *desc);
 void pkvm_shadow_ept_deinit(struct shadow_ept_desc *desc);
 void pkvm_guest_ept_init(struct shadow_vcpu_state *shadow_vcpu, u64 guest_eptp);
 void pkvm_guest_ept_deinit(struct shadow_vcpu_state *shadow_vcpu);
-enum sept_handle_ret
-pkvm_handle_shadow_ept_violation(struct shadow_vcpu_state *shadow_vcpu, u64 l2_gpa, u64 exit_quali);
 void pkvm_invalidate_shadow_ept(struct shadow_ept_desc *desc);
 void pkvm_invalidate_shadow_ept_with_range(struct shadow_ept_desc *desc,
 					   unsigned long vaddr, unsigned long size);
@@ -67,7 +65,6 @@ static inline bool is_valid_eptp(u64 eptp)
 
 extern struct pkvm_pgtable_ops ept_ops;
 
-int pkvm_handle_guest_ept_violation(struct kvm_vcpu *vcpu, u64 gpa);
 void pkvm_setup_virtual_ept(struct kvm_vcpu *vcpu, u64 veptp);
 void pkvm_invalidate_guest_ept(int shadow_handle, u64 start_gpa, u64 size);
 
