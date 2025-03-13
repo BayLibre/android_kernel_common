b325d949d5dd1b4d27d29774724dea4984d0c3f0
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-03-13 14:39:32 +0800

    ANDROID: pkvm: x86: Handle IA32_MISC_ENABLE MSR vmexit
    Emulate the IA32_MISC_ENABLE MSR in the pkvm hypervisor.
    
    Bug: 391539939
    Test: pKVM works
    Upstream-Task: 402758258
    
    Change-Id: I366b7b880382c10f375be1bf4693c4405de55b69
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>
    Signed-off-by: Dmytro Maluka <dmaluka@google.com>

--- arch/x86/kvm/pkvm/x86.c
+++ arch/x86/kvm/pkvm/x86.c
@@ -8,6 +8,7 @@
 #include <uapi/asm/debugreg.h>
 #include <smm.h>
 #include <lapic.h>
+#include <pmu.h>
 #include "pkvm.h"
 
 #ifdef __PKVM_HYP__
@@ -1084,6 +1085,7 @@ int kvm_set_msr_common(struct kvm_vcpu *vcpu, struct msr_data *msr_info)
 			vcpu->arch.ia32_tsc_adjust_msr = data;
 		}
 		break;
+#endif
 	case MSR_IA32_MISC_ENABLE: {
 		u64 old_val = vcpu->arch.ia32_misc_enable_msr;
 
@@ -1108,6 +1110,7 @@ int kvm_set_msr_common(struct kvm_vcpu *vcpu, struct msr_data *msr_info)
 		}
 		break;
 	}
+#ifndef __PKVM_HYP__ /* FIXME: Leave to the host to emulate */
 	case MSR_IA32_SMBASE:
 		if (!IS_ENABLED(CONFIG_KVM_SMM) || !msr_info->host_initiated)
 			return 1;
@@ -1457,9 +1460,11 @@ int kvm_get_msr_common(struct kvm_vcpu *vcpu, struct msr_data *msr_info)
 	case MSR_IA32_TSC_ADJUST:
 		msr_info->data = (u64)vcpu->arch.ia32_tsc_adjust_msr;
 		break;
+#endif
 	case MSR_IA32_MISC_ENABLE:
 		msr_info->data = vcpu->arch.ia32_misc_enable_msr;
 		break;
+#ifndef __PKVM_HYP__ /* FIXME: Leave to the host to emulate */
 	case MSR_IA32_SMBASE:
 		if (!IS_ENABLED(CONFIG_KVM_SMM) || !msr_info->host_initiated)
 			return 1;
