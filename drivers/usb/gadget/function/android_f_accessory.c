1574f6f6e4b487f31ee098e6d121a82de9f3a4d9
Hongseock Kim <hongpooh.kim@samsung.com>
2025-03-17 15:32:57 +0900

    Revert "ANDROID: usb: Optimize the problem of slow transfer rate in USB accessory mode"
    This reverts commit 4fb48ede932a5510b38237daa4e38f62dd396a37.
    
    Reason for revert: CTS-accessary test fail (test not started)
    
    Bug: to be updated
    
    Signed-off-by: Hongseock Kim <hongpooh.kim@samsung.com>
    Change-Id: I58331b4b7e37f0906418589d12889bffdc26bcb0

--- drivers/usb/gadget/function/android_f_accessory.c
+++ drivers/usb/gadget/function/android_f_accessory.c
@@ -156,7 +156,7 @@ static struct usb_ss_ep_comp_descriptor acc_superspeedplus_comp_desc = {
 	.bDescriptorType        = USB_DT_SS_ENDPOINT_COMP,
 
 	/* the following 2 values can be tweaked if necessary */
-	.bMaxBurst              = 6,
+	/* .bMaxBurst           = 0, */
 	.bmAttributes           = 16,
 };
 
@@ -181,7 +181,7 @@ static struct usb_ss_ep_comp_descriptor acc_superspeed_comp_desc = {
 	.bDescriptorType        = USB_DT_SS_ENDPOINT_COMP,
 
 	/* the following 2 values can be tweaked if necessary */
-	.bMaxBurst              = 6,
+	/* .bMaxBurst           = 0, */
 	.bmAttributes           = 16,
 };
 
