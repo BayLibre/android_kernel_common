3df825b121f26874ef582b22572c96c40ddd9a89
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-03-25 13:20:09 +0800

    ANDROID: kvm: pkvm: Handle the uret MSRs switching
    The prepare_switch_to_guest/host have been implemented in the pkvm
    hypervisor, which are used before entering into the guest and after
    breaking out the vcpu run loop. The host KVM doesn't need to explicitly
    use the PV interface to explicitly call prepare_switch_to_guest/host.
    Although the uret MSRs are part of the switching, the emulation are
    still done by the host KVM. Due to this, the uret MSRs switching are
    left to the host KVM. The switching and emulation will be moved to the
    pkvm hypervisor together.
    
    Bug: 391539939
    Test: pKVM works
    Upstream-Task: 402758258
    
    Change-Id: I68f0f2744f298791a2221d98fb32404bed873de2
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>
    Signed-off-by: Dmytro Maluka <dmaluka@google.com>
    Signed-off-by: Grzegorz Jaszczyk <jaszczyk@google.com>

--- arch/x86/kvm/pkvm/x86.c
+++ arch/x86/kvm/pkvm/x86.c
@@ -890,6 +890,8 @@ fastpath_t kvm_vcpu_enter_guest(struct kvm_vcpu *vcpu, bool force_immediate_exit
 				kvm_vcpu_flush_tlb_current(vcpu);
 		}
 
+		kvm_x86_call(prepare_switch_to_guest)(vcpu);
+
 		/*
 		 * Make sure vcpu->mode is changed to IN_GUEST_MODE before
 		 * running to mark this vcpu should be kicked for any new
@@ -919,6 +921,8 @@ fastpath_t kvm_vcpu_enter_guest(struct kvm_vcpu *vcpu, bool force_immediate_exit
 		if (unlikely(force_immediate_exit))
 			break;
 	}
+
+	kvm_x86_call(prepare_switch_to_host)(vcpu);
 
 	vcpu->arch.host_debugctl = get_debugctlmsr();
 
