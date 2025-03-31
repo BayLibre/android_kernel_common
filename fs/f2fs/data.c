289dbfdaa9d4ab512d5b12da9324d7cd851a415d
Matthew Wilcox (Oracle) <willy@infradead.org>
2025-03-31 21:10:23 +0100

    UPSTREAM: f2fs: Introduce fio_inode()
    This helper returns the inode associated with the f2fs_io_info.  That's a
    relatively common thing to want, mildly awkward to get and provides one
    place to change if we decide to record it directly, or change fio->page
    to fio->folio.
    
    Change-Id: I077304d08fa8ad99c97959ea82dead6fa2f76008
    Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>
    [Jaegeuk Kim: fix wrong fio_inode conversion]
    Reviewed-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    (cherry picked from commit 4e5109c7c5f218d40e41f8b53bb96114b65a05d2)

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -1004,13 +1004,13 @@ next:
 	if (io->bio &&
 	    (!io_is_mergeable(sbi, io->bio, io, fio, io->last_block_in_bio,
 			      fio->new_blkaddr) ||
-	     !f2fs_crypt_mergeable_bio(io->bio, fio->page->mapping->host,
+	     !f2fs_crypt_mergeable_bio(io->bio, fio_inode(fio),
 				page_folio(bio_page)->index, fio)))
 		__submit_merged_bio(io);
 alloc_new:
 	if (io->bio == NULL) {
 		io->bio = __bio_alloc(fio, BIO_MAX_VECS);
-		f2fs_set_bio_crypt_ctx(io->bio, fio->page->mapping->host,
+		f2fs_set_bio_crypt_ctx(io->bio, fio_inode(fio),
 				page_folio(bio_page)->index, fio, GFP_NOIO);
 		io->fio = *fio;
 	}
@@ -2526,7 +2526,7 @@ static void f2fs_readahead(struct readahead_control *rac)
 
 int f2fs_encrypt_one_page(struct f2fs_io_info *fio)
 {
-	struct inode *inode = fio->page->mapping->host;
+	struct inode *inode = fio_inode(fio);
 	struct page *mpage, *page;
 	gfp_t gfp_flags = GFP_NOFS;
 
@@ -2656,7 +2656,7 @@ bool f2fs_should_update_outplace(struct inode *inode, struct f2fs_io_info *fio)
 
 static inline bool need_inplace_update(struct f2fs_io_info *fio)
 {
-	struct inode *inode = fio->page->mapping->host;
+	struct inode *inode = fio_inode(fio);
 
 	if (f2fs_should_update_outplace(inode, fio))
 		return false;
