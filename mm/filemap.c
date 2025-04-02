96fd7f299aef4368d8c1ec8adec6a7c97213b4fb
Kalesh Singh <kaleshsingh@google.com>
2025-04-01 17:50:57 -0700

    ANDROID: 16K: Emulate cachestat counters
    If emulating a larger page size adjust the cachestat page counters
    to match.
    
    Bug: 481846501
    Bug: 383389337
    Bug: 404958693
    Test: atest vts_ltp_test_x86_64:syscalls.cachestat01_64bit#syscalls.cachestat01_64bit
    Change-Id: I39581c7dce4b59d48096ede33730cc38dbc0680b
    Signed-off-by: Kalesh Singh <kaleshsingh@google.com>
    Signed-off-by: Adrian Barnaś <abarnas@google.com>

--- mm/filemap.c
+++ mm/filemap.c
@@ -42,6 +42,7 @@
 #include <linux/psi.h>
 #include <linux/ramfs.h>
 #include <linux/page_idle.h>
+#include <linux/page_size_compat.h>
 #include <linux/migrate.h>
 #include <linux/pipe_fs_i.h>
 #include <linux/splice.h>
@@ -4627,6 +4628,8 @@ resched:
 		}
 	}
 	rcu_read_unlock();
+
+	__adjust_cachestat_counters(cs);
 }
 
 /*
