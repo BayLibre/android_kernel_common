ecb4357462b59ad9b732a3720722c1d6af5e2009
Vineeth Pillai <vineethrp@google.com>
2025-04-07 09:01:58 -0400

    ANDROID: pkvm: x86: wrapper function for pkvm enabled check in host
    Implement a wrapper function for host to check if pkvm is enabled or
    not. This is useful and needed in iommu driver, but could be used in
    other places as well.
    
    We have a percpu flag to check is pkvm is active (deprivileged). This
    flag was in place to differentiate between deprivileged cpus and cpus
    that are not yet deprivileged because iommu initialization happens
    before or while deprivilege and iommu initialization needs to trap into
    pkvm if the cpu is deprivileged. With out this per-cpu check, iommu
    initialization would be error prone based on whether its running on a
    deprivileged cpu or not.
    
    Now that the iommu deprivilege happens after deprivilege and we have
    reprivilege mechanism to avoid partial deprivilege, we can replace the
    per-cpu flags with a global flag.
    
    Bug: 391542119
    Test: Build and Boot
    
    Change-Id: I3cfe68808337090e4e2d138fc4725ffb7dfa751c
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

--- arch/x86/kvm/vmx/pkvm/pkvm_host.c
+++ arch/x86/kvm/vmx/pkvm/pkvm_host.c
@@ -5,6 +5,7 @@
 
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/jump_label.h>
 #include <linux/dmar.h>
 #include <../drivers/iommu/intel/iommu.h>
 #include <linux/pci.h>
@@ -20,6 +21,8 @@
 #include "pkvm_constants.h"
 
 MODULE_LICENSE("GPL");
+
+DEFINE_STATIC_KEY_FALSE(pkvm_enabled_key);
 
 bool __read_mostly enable_pkvm = false;
 
@@ -66,7 +69,6 @@ struct pkvm_deprivilege_param {
 	struct pkvm_hyp *pkvm;
 	int ret;
 };
-DEFINE_PER_CPU_READ_MOSTLY(bool, pkvm_enabled);
 
 static const struct pkvm_iommu_driver *iommu_driver;
 
@@ -968,7 +970,6 @@ static __init void pkvm_host_reprivilege_cpu(void *data)
 	pkvm_host_clear_vmx(&hvcpu->vmx);
 	vcpu = &hvcpu->vmx.vcpu;
 	vcpu->mode = OUTSIDE_GUEST_MODE;
-	this_cpu_write(pkvm_enabled, false);
 
 	pr_info("%s: CPU%d back in host mode\n", __func__, cpu);
 
@@ -1049,11 +1050,7 @@ static __init int pkvm_host_deprivilege_cpus(struct pkvm_hyp *pkvm)
  */
 static int __this_cpu_do_finalise_hc(struct pkvm_section *sections, unsigned long size)
 {
-	int ret = kvm_hypercall2(PKVM_HC_INIT_FINALISE, (unsigned long)sections, size);
-	if (!ret)
-		this_cpu_write(pkvm_enabled, true);
-
-	return ret;
+	return kvm_hypercall2(PKVM_HC_INIT_FINALISE, (unsigned long)sections, size);
 }
 
 /* Called with preemption disabled but interrupts enabled. */
@@ -1462,6 +1459,13 @@ static void __init pkvm_init_rollback(void)
 	pkvm_firmware_rmem_clear();
 
 	/*
+	 * Rollback would have allowed host to access memory and MMIO
+	 * range, we can mark pkvm disabled so that host directly accesses
+	 * MMIO and memory instead of hypercall.
+	 */
+	static_branch_disable(&pkvm_enabled_key);
+
+	/*
 	 * Reprivilege cpus
 	 */
 	for_each_possible_cpu(cpu) {
@@ -1581,7 +1585,9 @@ int __init vmx_pkvm_init(void)
 
 	ret = __vmx_pkvm_init();
 
-	if (ret)
+	if (!ret)
+		static_branch_enable(&pkvm_enabled_key);
+	else
 		pkvm_init_rollback();
 
 	/*
