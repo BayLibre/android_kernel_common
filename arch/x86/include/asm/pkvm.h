43018e1d3167a18b1e75b9351a7308ac0a61b738
Vineeth Pillai <vineethrp@google.com>
2025-04-07 09:01:58 -0400

    ANDROID: pkvm: x86: wrapper function for pkvm enabled check in host
    Implement a wrapper function for host to check if pkvm is enabled or
    not. This is useful and needed in iommu driver, but could be used in
    other places as well.
    
    We have a percpu flag to check is pkvm is active (deprivileged). This
    flag was in place to differentiate between deprivileged cpus and cpus
    that are not yet deprivileged because iommu initialization happens
    before or while deprivilege and iommu initialization needs to trap into
    pkvm if the cpu is deprivileged. With out this per-cpu check, iommu
    initialization would be error prone based on whether its running on a
    deprivileged cpu or not.
    
    Now that the iommu deprivilege happens after deprivilege and we have
    reprivilege mechanism to avoid partial deprivilege, we can replace the
    per-cpu flags with a global flag.
    
    Bug: 391542119
    Test: Build and Boot
    
    Change-Id: I3cfe68808337090e4e2d138fc4725ffb7dfa751c
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

--- arch/x86/include/asm/pkvm.h
+++ arch/x86/include/asm/pkvm.h
@@ -40,15 +40,19 @@ struct pkvm_iommu_driver {
 
 #ifndef __PKVM_HYP__
 extern bool __read_mostly enable_pkvm;	/* kernel command-line flag */
-#endif
 
-DECLARE_PER_CPU_READ_MOSTLY(bool, pkvm_enabled);
+extern struct static_key_false pkvm_enabled_key;
+
+static inline bool pkvm_enabled(void)
+{
+	return static_branch_likely(&pkvm_enabled_key);
+}
 
 int pkvm_iommu_register_driver(const struct pkvm_iommu_driver *kern_ops);
 
 static inline long pkvm_dump_dmar_translation_struct(void)
 {
-	if (likely(this_cpu_read(pkvm_enabled)))
+	if (pkvm_enabled())
 		return kvm_hypercall0(PKVM_HC_DUMP_DMAR_TR_STRUCT);
 	return 0;
 }
@@ -56,7 +60,7 @@ static inline long pkvm_dump_dmar_translation_struct(void)
 static inline long pkvm_dump_domain_translation_struct(
 		unsigned long phys, unsigned long bdf, unsigned long pasid)
 {
-	if (likely(this_cpu_read(pkvm_enabled)))
+	if (pkvm_enabled())
 		return kvm_hypercall3(PKVM_HC_DUMP_DOMAIN_PGT, phys, bdf, pasid);
 	return 0;
 }
@@ -64,7 +68,7 @@ static inline long pkvm_dump_domain_translation_struct(
 static inline u64 pkvm_readq(void __iomem *reg, unsigned long reg_phys,
 			     unsigned long offset)
 {
-	if (likely(this_cpu_read(pkvm_enabled)))
+	if (pkvm_enabled())
 		return (u64)kvm_hypercall3(PKVM_HC_MMIO_ACCESS, true,
 					   sizeof(u64), reg_phys + offset);
 	else
@@ -74,7 +78,7 @@ static inline u64 pkvm_readq(void __iomem *reg, unsigned long reg_phys,
 static inline u32 pkvm_readl(void __iomem *reg, unsigned long reg_phys,
 			     unsigned long offset)
 {
-	if (likely(this_cpu_read(pkvm_enabled)))
+	if (pkvm_enabled())
 		return (u32)kvm_hypercall3(PKVM_HC_MMIO_ACCESS, true,
 					   sizeof(u32), reg_phys + offset);
 	else
@@ -84,7 +88,7 @@ static inline u32 pkvm_readl(void __iomem *reg, unsigned long reg_phys,
 static inline void pkvm_writeq(void __iomem *reg, unsigned long reg_phys,
 			       unsigned long offset, u64 val)
 {
-	if (likely(this_cpu_read(pkvm_enabled)))
+	if (pkvm_enabled())
 		kvm_hypercall4(PKVM_HC_MMIO_ACCESS, false, sizeof(u64),
 			       reg_phys + offset, val);
 	else
@@ -94,12 +98,13 @@ static inline void pkvm_writeq(void __iomem *reg, unsigned long reg_phys,
 static inline void pkvm_writel(void __iomem *reg, unsigned long reg_phys,
 			       unsigned long offset, u32 val)
 {
-	if (likely(this_cpu_read(pkvm_enabled)))
+	if (pkvm_enabled())
 		kvm_hypercall4(PKVM_HC_MMIO_ACCESS, false, sizeof(u32),
 			       reg_phys + offset, (u64)val);
 	else
 		writel(val, reg + offset);
 }
+#endif
 
 static inline void pkvm_update_iommu_virtual_caps(u64 *cap, u64 *ecap)
 {
@@ -165,6 +170,11 @@ static inline void pkvm_update_iommu_virtual_caps(u64 *cap, u64 *ecap)
 }
 #else
 #define enable_pkvm false
+
+static inline bool pkvm_enabled(void)
+{
+	return false;
+}
 
 static inline int pkvm_iommu_register_driver(const struct pkvm_iommu_driver *kern_ops)
 {
