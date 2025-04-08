8be8302e1dce1f90eb28684f80fcc4e87726b168
Grzegorz Jaszczyk <jaszczyk@google.com>
2025-04-08 10:59:23 +0000

    of: fdt: allow to register arch specific hook validating reserved region
    Add a possibility to register arch specific hook which will validate if
    reserved-memory region passed by DT is valid (e.g. in case of x86, which
    allows to boot with both ACPI and DT at the same time, registered hook
    will validate if reserved-memory region passed by DT is covered by E820
    reserved region entry).
    
    Bug: 399581144
    Change-Id: I242090842703d6be703c032992f0af8cdecae64c
    Signed-off-by: Grzegorz Jaszczyk <jaszczyk@google.com>

--- drivers/of/fdt.c
+++ drivers/of/fdt.c
@@ -480,6 +480,8 @@ int __initdata dt_root_size_cells;
 void *initial_boot_params __ro_after_init;
 phys_addr_t initial_boot_params_pa __ro_after_init;
 
+static bool __initdata (*arch_is_region_reserved)(phys_addr_t base, phys_addr_t size);
+
 #ifdef CONFIG_OF_EARLY_FLATTREE
 
 static u32 of_fdt_crc32;
@@ -487,6 +489,13 @@ static u32 of_fdt_crc32;
 static int __init early_init_dt_reserve_memory(phys_addr_t base,
 					       phys_addr_t size, bool nomap)
 {
+	if (arch_is_region_reserved && !arch_is_region_reserved(base, size)) {
+		phys_addr_t end = base + size - 1;
+
+		pr_err("mem %pa-%pa not arch reserved\n", &base, &end);
+		return -EINVAL;
+	}
+
 	if (nomap) {
 		/*
 		 * If the memory is already reserved (by another region), we
@@ -628,6 +637,12 @@ static void __init fdt_reserve_elfcorehdr(void)
 
 	pr_info("Reserving %llu KiB of memory at 0x%llx for elfcorehdr\n",
 		elfcorehdr_size >> 10, elfcorehdr_addr);
+}
+
+void __init early_init_set_rsv_region_verifier(bool (*is_mem_reserved)(phys_addr_t base,
+								       phys_addr_t size))
+{
+	arch_is_region_reserved = is_mem_reserved;
 }
 
 /**
