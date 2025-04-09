3d46a9a364bf272f946922990992780123ac2da8
Chinwen Chang <chinwen.chang@mediatek.com>
2025-04-09 15:46:19 +0800

    ANDROID: mm: Add vendor hook in swapon()
    This patch add a vendor hook in swapon() for changing swap
    info flags to meet the capability of certain swap devices.
    
    Bug: 368512254
    Change-Id: I009f039179db3d4ac23039c7ddd09fa852f3729f
    Signed-off-by: Chinwen Chang <chinwen.chang@mediatek.com>

--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -22,6 +22,10 @@ DECLARE_RESTRICTED_HOOK(android_rvh_set_readahead_gfp_mask,
 
 */
 
+DECLARE_HOOK(android_vh_adjust_swap_info_flags,
+	TP_PROTO(unsigned long *flags),
+	TP_ARGS(flags));
+
 DECLARE_HOOK(android_vh_rmqueue_pcplist_override_batch,
 	TP_PROTO(int *batch),
 	TP_ARGS(batch));
