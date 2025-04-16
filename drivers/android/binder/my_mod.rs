d52eea711e2ad3288da072483ef0f15c9d81d1c5
Alice Ryhl <aliceryhl@google.com>
2025-04-16 12:49:03 +0000

    ANDROID: rust_binder: fixups for 6.12.19 LTS
    The 6.12.19 LTS of the kernel contains some changes to the Rust kernel
    crate involving the integer types used for FFI and the removal of the
    alloc crate in favor of a custom alloc utilities.
    
    Bug: 388786466
    Change-Id: Ie4cab650deaf1f2ffc4ee64d367e52f0671791c1
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>

--- drivers/android/binder/my_mod.rs
+++ drivers/android/binder/my_mod.rs
@@ -2,8 +2,8 @@
 
 use super::*;
 
-pub(crate) fn make_numbers() -> Result<Vec<i32>> {
-    let mut numbers = Vec::new();
+pub(crate) fn make_numbers() -> Result<KVec<i32>> {
+    let mut numbers = KVec::new();
     numbers.push(72, GFP_KERNEL)?;
     numbers.push(108, GFP_KERNEL)?;
     numbers.push(200, GFP_KERNEL)?;
