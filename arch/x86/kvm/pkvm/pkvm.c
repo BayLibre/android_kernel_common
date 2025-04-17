ca8810fb02d6b1f7e1495d455f75ff4d931cce71
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-04-17 15:03:27 +0800

    ANDROID: pkvm: x86: Swap the FPU registers for the pVM
    To support the FPU isolation for the pVM, mark the pVM's fpstate as
    confidential and do the FPU registers swapping around the vcpu run loop.
    
    Bug: 420676925
    Upstream-Task: 402758258
    
    Change-Id: I6a6a2ea8b4a75c2d9455b601946aeb035d867ddd
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/pkvm/pkvm.c
+++ arch/x86/kvm/pkvm/pkvm.c
@@ -1616,6 +1616,11 @@ static unsigned long pkvm_vcpu_add_fpstate(struct pkvm_vcpu *pkvm_vcpu,
 	old = vcpu->arch.guest_fpu.fpstate;
 	vcpu->arch.guest_fpu.fpstate = new;
 	pkvm_init_guest_fpu(&vcpu->arch.guest_fpu);
+	/*
+	 * As only the pVM will be here, set the fpstate confidential
+	 * unconditionally
+	 */
+	fpstate_set_confidential(&vcpu->arch.guest_fpu);
 
 	if (!old)
 		return INVALID_PAGE;
