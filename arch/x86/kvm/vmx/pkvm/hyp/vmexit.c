f11ad46986cf6e0c08ea3028ecf6f0bd5e1a8c75
Vineeth Pillai <vineethrp@gmail.com>
2025-04-20 07:17:52 -0400

    ANDROID: pkvm: debugfs: dump pkvm dmar translation entries to dmesg
    Extend the intel iommu debugfs to show the dmar and domain translation
    structures from pkvm if pkvm is enabled. Currently pkvm debug outputs
    are directed to dmesg.
    
    Bug: 391542119
    Test: Build and Boot
    
    Change-Id: I5f62947e4769ae8c3df2f713b49e399a5f009a82
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

--- arch/x86/kvm/vmx/pkvm/hyp/vmexit.c
+++ arch/x86/kvm/vmx/pkvm/hyp/vmexit.c
@@ -13,9 +13,12 @@
 #include "pkvm_hyp.h"
 #include "vmsr.h"
 #include "iommu.h"
+#include "iommu_internal.h"
 #include "lapic.h"
 #include "debug.h"
 #include "init_finalise.h"
+
+#include <pkvm_debugfs.h>
 
 #define CR4	4
 
@@ -94,6 +97,12 @@ static unsigned long handle_vmcall(struct kvm_vcpu *vcpu)
 		break;
 	case PKVM_HC_DUMP_VMEXIT_TRACE:
 		pkvm_handle_dump_vmexit_trace(a0, a1, a2);
+		break;
+	case PKVM_HC_DUMP_DMAR_TR_STRUCT:
+		pkvm_dump_dmar_tr_struct();
+		break;
+	case PKVM_HC_DUMP_DOMAIN_PGT:
+		pkvm_dump_domain_pgt(a0, a1, a2);
 		break;
 	case PKVM_HC_MMIO_ACCESS:
 		ret = pkvm_access_iommu(a0, a1, a2, a3);
