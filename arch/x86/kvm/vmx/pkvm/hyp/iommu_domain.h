73fc92e03e516c85b7f64636a7a18afa6d391d5a
Vineeth Pillai <vineethrp@gmail.com>
2025-04-21 16:14:01 -0400

    pkvm: paravirtualize iommu page tables
    Implement hypercalls for map/unmap and iova to phys translation.
    The implementation is a copy of the existing intel iommu driver
    implementation. This code could be refactored as a library and shared
    between host and pkvm.
    
    Bug: 391542119
    Test: Build and Boot
    
    Change-Id: I5f4e29919c78f6dfa381e4e353ec166c775a3230
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

--- arch/x86/kvm/vmx/pkvm/hyp/iommu_domain.h
+++ arch/x86/kvm/vmx/pkvm/hyp/iommu_domain.h
@@ -27,8 +27,22 @@ struct pkvm_iommu_domain {
 	struct hlist_node hnode;
 };
 
+#define DEFAULT_DOMAIN_ADDRESS_WIDTH 57
+
+#define __DOMAIN_MAX_PFN(gaw)  ((((uint64_t)1) << ((gaw) - VTD_PAGE_SHIFT)) - 1)
+#define __DOMAIN_MAX_ADDR(gaw) ((((uint64_t)1) << (gaw)) - 1)
+
+/* We limit DOMAIN_MAX_PFN to fit in an unsigned long, and DOMAIN_MAX_ADDR
+   to match. That way, we can use 'unsigned long' for PFNs with impunity. */
+#define DOMAIN_MAX_PFN(gaw)	((unsigned long) min_t(uint64_t, \
+				__DOMAIN_MAX_PFN(gaw), (unsigned long)-1))
+#define DOMAIN_MAX_ADDR(gaw)	(((uint64_t)__DOMAIN_MAX_PFN(gaw)) << VTD_PAGE_SHIFT)
+
 struct pkvm_iommu_domain *pkvm_alloc_iommu_domain(u64 pgd);
 struct pkvm_iommu_domain *pkvm_get_iommu_domain(u64 pgd);
 void pkvm_put_iommu_domain(struct pkvm_iommu_domain *iommu_domain);
 
+unsigned long pkvm_iommu_domain_map(struct kvm_vcpu *hvcpu, unsigned long param_gva);
+unsigned long pkvm_iommu_domain_unmap(struct kvm_vcpu *hvcpu, unsigned long param_gva);
+unsigned long pkvm_iommu_domain_iova_to_phys(struct kvm_vcpu *hvcpu, unsigned long param_gva);
 #endif
