0f13824401f09e6ec238e6f3fff7d85a0f1bdae7
Monika A <monika.a@intel.com>
2025-04-24 10:09:25 +0530

    ANDROID: Fatcat-Fix to enable Ocelot ALC256 HDA codec
    Add soft dependency of snd_hda_codec_realtek module in the kernel to fix
    the codec probe error while loading HDA codec.
    
    Bug: 412215393
    Upstream-Task: 416430112
    Test: Build kernel and check for this Kconfig in the generated config
    Test: Verify if sound card is listed using cat /proc/asound/pcm
    
    Change-Id: I7cf6ffae3474b1cc2eaef150eaf54f13c63266d0
    Signed-off-by: Monika A <monika.a@intel.com>
    Signed-off-by: Jairaj Arava <jairaj.arava@intel.corp-partner.google.com>
    Signed-off-by: Uday M Bhat <uday.m.bhat@intel.com>

--- sound/soc/sof/intel/hda-codec.c
+++ sound/soc/sof/intel/hda-codec.c
@@ -454,6 +454,8 @@ int hda_codec_i915_exit(struct snd_sof_dev *sdev)
 }
 EXPORT_SYMBOL_NS_GPL(hda_codec_i915_exit, SND_SOC_SOF_HDA_AUDIO_CODEC_I915);
 
+MODULE_SOFTDEP("pre: snd_hda_codec_realtek");
+
 #endif
 
 MODULE_LICENSE("Dual BSD/GPL");
