f897824c0724973cd56e30be1fd2367277861ca2
Per Larsen <perlarsen@google.com>
2025-04-25 07:17:36 +0000

    ANDROID: KVM: arm64: Restrict FF-A version downgrades
    FF-A versions 1.x are intended to be backwards compatible but this is
    difficult to support in practice as FF-A 1.2 relies on SMC calling
    convention 1.2 which is not backwards compatible with SMCCC 1.1.
    
    This makes it difficult if not impossible to support cases where the
    hypervisor negotiates FF-A 1.2 with the firmware and the host only
    supports earlier FF-A versions. To avoid this case, return NOT_SUPPORTED
    instead of downgrading the hypervisor version.
    
    Bug: 269285339
    Change-Id: I18e26a1a467f89ce421f31f577b3f15c37dfcf1a
    Signed-off-by: Per Larsen <perlarsen@google.com>

--- arch/arm64/kvm/hyp/nvhe/ffa.c
+++ arch/arm64/kvm/hyp/nvhe/ffa.c
@@ -1076,7 +1076,28 @@ static void do_ffa_version(struct arm_smccc_res *res,
 
 	hyp_spin_lock(&version_lock);
 	if (has_version_negotiated) {
-		res->a0 = hyp_ffa_version;
+		/*
+		 * FF-A implementations with the same major version must interoperate
+		 * with earlier minor per DEN0077A 1.2 REL0 13.2.1 but FF-A v1.2 makes a
+		 * breaking change by switching the calling convention to SMCCC 1.2.
+		 *
+		 * According to DEN0028 1.6 G BET0 Appendix F:
+		 * * SMCCC 1.1: Mandates preservation of registers X4–X17 across a SMC/HVC call.
+		 * * SMCCC 1.2: Permits calls to use X4–X17 as return and argument registers.
+		 *
+		 * However, the hypervisor has already negotiated a version with the firmware
+		 * and cannot be changed for the lifetime of the hypervisor (DEN0077A 1.2 REL0
+		 * 13.2.1).
+		 *
+		 * Do not let the host downgrade the hypervisor when it requires a switch
+		 * between incompatible calling conventions.
+		 */
+		if (FFA_MINOR_VERSION(ffa_req_version) < FFA_MINOR_VERSION(hyp_ffa_version) &&
+			FFA_MINOR_VERSION(ffa_req_version) < 2) {
+			res->a0 = FFA_RET_NOT_SUPPORTED;
+		} else {
+			res->a0 = hyp_ffa_version;
+		}
 		goto unlock;
 	}
 
