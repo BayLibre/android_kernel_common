8ed94b2f864a506bfd3fb1e52b696dc1badcf2e2
Per Larsen <perlarsen@google.com>
2025-04-26 03:29:47 +0000

    ANDROID: KVM: arm64: Make FF-A 1.2 the minimum version for guests
    While guests on older FF-A versions can be supported in principle, FF-A
    version 1.2 and 1.1 each made non-trivial breaking changes that would
    necessitate compatibility paths to handle properly. It would also be
    necessary to track what version each guest requested if less than the
    hypervisor version.
    
    Make FF-A 1.2 the minimum required guest version because it is otherwise
    necessary add non-trivial 1.0 and 1.1 compatibility paths.
    
    If the host negotiated a version not supported by the guest, don't enable
    the KVM_CAP_ARM_PROTECTED_VM_FLAGS_SET_FFA capability.
    
    Bug: 280886201
    Change-Id: I6da1419aa950449f77ffcb4b4e59560c5d455efd
    Signed-off-by: Per Larsen <perlarsen@google.com>

--- arch/arm64/kvm/hyp/nvhe/ffa.c
+++ arch/arm64/kvm/hyp/nvhe/ffa.c
@@ -1506,8 +1506,24 @@ static void do_ffa_guest_version(struct arm_smccc_1_2_regs *res,
 		return;
 	}
 
+	/*
+	 * For now, FF-A 1.2 is the minimum required guest version because it is
+	 * otherwise necessary add non-trivial 1.0 and 1.1 compatibility paths.
+	 */
+	if (FFA_MINOR_VERSION(ffa_req_version) < 2) {
+		res->a0 = FFA_RET_NOT_SUPPORTED;
+		return;
+	}
+
 	hyp_spin_lock(&version_lock);
-	if (has_version_negotiated)
+	/*
+	 * If the hypervisor adds support for a new FF-A version that is not
+	 * backwards compatible with 1.2 without adding compatibility paths to
+	 * paper over differences, clients that requested a version less than
+	 * the hypervisor must be tracked and compatibility paths must be added
+	 * as needed.
+	 */
+	if (has_version_negotiated && FFA_MINOR_VERSION(hyp_ffa_version) >= 2)
 		res->a0 = hyp_ffa_version;
 	else
 		res->a0 = FFA_RET_NOT_SUPPORTED;
@@ -1903,8 +1919,10 @@ int hyp_ffa_init(void *pages)
 	if (FFA_MAJOR_VERSION(res.a0) != 1)
 		return -EOPNOTSUPP;
 
+	/* See do_ffa_guest_version before bumping maximum supported version. */
 	if (FFA_MINOR_VERSION(res.a0) < FFA_MINOR_VERSION(FFA_VERSION_1_1))
 		hyp_ffa_version = res.a0;
+
 	else
 		hyp_ffa_version = FFA_VERSION_1_1;
 
