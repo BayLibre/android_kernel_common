e44f753e19bce0aec3cc426c4cfaad0db1df1a86
suntao9 <suntao9@xiaomi.com>
2025-04-28 20:26:25 +0800

    ANDROID: modifies the order of log commits after calling the hook
    If commit the log to the ringbuffer first, there may be a case
    where logcat fetches the log before the hook is called,
    which will cause the timestamp to be out of order.
    
    Bug: 414341879
    Fixes: Ib37f4373d3511aaf765838bf581c84ed22992a77
           I706824aeb566c09ecaf4b5900973d6cee8a2f35b
    Change-Id: Idc9e91e4857644f450d5955a1cd4d9cbf36af99b
    Signed-off-by: suntao9 <suntao9@xiaomi.com>
    (cherry picked from commit 7bf8c86a5c6b8e7201bc0a8f061a7c2a6e835b54)

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -2389,6 +2389,9 @@ int vprintk_store(int facility, int level,
 		memcpy(&r.info->dev_info, dev_info, sizeof(r.info->dev_info));
 	printk_store_execution_ctx(r.info);
 
+	trace_android_rvh_logbuf(prb, &r);
+	trace_android_vh_logbuf(prb, &r);
+
 	/* A message without a trailing newline can be continued. */
 	if (!(flags & LOG_NEWLINE))
 		prb_commit(&e);
