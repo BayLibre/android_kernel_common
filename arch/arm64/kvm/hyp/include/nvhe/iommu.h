863a9ebbcd064da215de7491fa8b07603fd9af71
Isaac J. Manjarres <isaacmanjarres@google.com>
2025-05-02 18:39:14 -0700

    ANDROID: KVM: arm64: iommu: Add hypercalls for managing nested domains
    Introduce two hypercalls to allow a nested domain to be attached and
    detached to and from a device respectively. Nested domains are domains
    that use two stages of translation, where the translation context for
    each stage is represented by an IOMMU domain.
    
    The caller is only aware of stage-1 translations, and thus is only
    expected to provide the domain ID for the stage-1 domain that will be
    used for stage-1 translations for the domain. The hypervisor is free
    to determine what translation context should be used for stage-2, and
    currently only supports using the idmap domain as the stage-2 context
    for nested domains.
    
    Bug: 399886316
    Change-Id: I146bad95b8c363d1a6cb5d815d1e00ae39b3612a
    Signed-off-by: Isaac J. Manjarres <isaacmanjarres@google.com>

--- arch/arm64/kvm/hyp/include/nvhe/iommu.h
+++ arch/arm64/kvm/hyp/include/nvhe/iommu.h
@@ -38,8 +38,12 @@ int kvm_iommu_alloc_domain(pkvm_handle_t domain_id, u32 type);
 int kvm_iommu_free_domain(pkvm_handle_t domain_id);
 int kvm_iommu_attach_dev(pkvm_handle_t iommu_id, pkvm_handle_t domain_id,
 			 u32 endpoint_id, u32 pasid, u32 pasid_bits);
+int kvm_iommu_attach_dev_nested(pkvm_handle_t iommu_id, pkvm_handle_t domain_id, u32 endpoint_id,
+				u32 pasid, u32 pasid_bits);
 int kvm_iommu_detach_dev(pkvm_handle_t iommu_id, pkvm_handle_t domain_id,
 			 u32 endpoint_id, u32 pasid);
+int kvm_iommu_detach_dev_nested(pkvm_handle_t iommu_id, pkvm_handle_t domain_id, u32 endpoint_id,
+				u32 pasid);
 int kvm_iommu_map_pages(pkvm_handle_t domain_id, unsigned long iova,
 			phys_addr_t paddr, size_t pgsize,
 			size_t pgcount, int prot, unsigned long *mapped);
@@ -123,8 +127,14 @@ struct kvm_iommu_ops {
 	void (*free_domain)(struct kvm_hyp_iommu_domain *domain);
 	int (*attach_dev)(struct kvm_hyp_iommu *iommu, struct kvm_hyp_iommu_domain *domain,
 			  u32 endpoint_id, u32 pasid, u32 pasid_bits);
+	int (*attach_dev_nested)(struct kvm_hyp_iommu *iommu, struct kvm_hyp_iommu_domain *domain,
+				 struct kvm_hyp_iommu_domain *s2_domain, u32 endpoint_id, u32 pasid,
+				 u32 pasid_bits);
 	int (*detach_dev)(struct kvm_hyp_iommu *iommu, struct kvm_hyp_iommu_domain *domain,
 			  u32 endpoint_id, u32 pasid);
+	int (*detach_dev_nested)(struct kvm_hyp_iommu *iommu, struct kvm_hyp_iommu_domain *domain,
+				 struct kvm_hyp_iommu_domain *s2_domain, u32 endpoint_id,
+				 u32 pasid);
 
 	bool (*dabt_handler)(struct kvm_cpu_context *host_ctxt, u64 esr, u64 addr);
 	int (*suspend)(struct kvm_hyp_iommu *iommu);
