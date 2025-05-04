8a6f870aaa9185ad7971ce5450620b8d47794fe2
Vineeth Pillai <vineethrp@google.com>
2025-05-04 13:29:13 -0400

    ANDROID: pkvm: x86: hypercall to allocate pkvm_iommu_domain
    Currently we allocate a domain only with update_ce hypercall. But that
    is a bit late was host driver may do some direct mapping before device
    attach. So we need to allocate domain early inline with host driver.
    
    Bug: 391542119
    Test: Build and Boot
    
    Change-Id: I2fb6f063e0259ea588eaaf85c7e938c13845b8f7
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

--- arch/x86/kvm/vmx/pkvm/hyp/pv_iommu.c
+++ arch/x86/kvm/vmx/pkvm/hyp/pv_iommu.c
@@ -179,6 +179,7 @@ unsigned long pkvm_iommu_update_ce(struct kvm_vcpu *hvcpu, unsigned long phys, u
 		}
 		if (__valid_paging_pgd(new_ce_pgd)) {
 			domain = pkvm_get_iommu_domain(new_ce_pgd);
+			PKVM_ASSERT(domain);
 			if (domain) {
 				PKVM_ASSERT(domain->iommu_coherency == iommu_coherency(iommu->iommu.ecap));
 				PKVM_ASSERT(domain->iommu_superpage == param.iommu_superpage);
