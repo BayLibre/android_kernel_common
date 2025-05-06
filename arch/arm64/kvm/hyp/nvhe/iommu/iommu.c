bbe5981246c5c65769dc80a1f0768518eec22da7
Isaac J. Manjarres <isaacmanjarres@google.com>
2025-05-06 00:01:01 -0700

    ANDROID: KVM: arm64: iommu: Add a hypercall to retry/terminate faults
    Expose a hypercall to allow stalled transactions to be retried or aborted.
    This is useful in a scenario where a device stalls
    due to a pagefault, and the kernel handles the pagefault and wants to
    inform the hypervisor of how the transaction should be handled.
    
    Bug: 399886316
    Change-Id: I521967603b9f508db20a6508d479f0d415814630
    Signed-off-by: Isaac J. Manjarres <isaacmanjarres@google.com>

--- arch/arm64/kvm/hyp/nvhe/iommu/iommu.c
+++ arch/arm64/kvm/hyp/nvhe/iommu/iommu.c
@@ -600,6 +600,19 @@ out_unpin_mem:
 	return ret;
 }
 
+int kvm_iommu_page_response(pkvm_handle_t drv_id, pkvm_handle_t iommu_id, u32 endpoint_id,
+			    u32 pasid, u32 grpid, u32 status_code)
+{
+	struct kvm_iommu_ops *kvm_iommu_ops;
+
+	kvm_iommu_ops = get_drv(drv_id);
+	if (!kvm_iommu_ops || !kvm_iommu_ops->page_response)
+		return -ENODEV;
+
+	kvm_iommu_ops->page_response(iommu_id, endpoint_id, pasid, grpid, status_code);
+	return 0;
+}
+
 int kvm_iommu_attach_dev(pkvm_handle_t iommu_id, pkvm_handle_t domain_id,
 			 u32 endpoint_id, u32 pasid, u32 pasid_bits, unsigned long flags)
 {
