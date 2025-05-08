97d873f31a1a4d13855de0d073e6c346710964e2
Vineeth Pillai <vineethrp@google.com>
2025-05-08 13:25:40 -0400

    ANDROID: pkvm: x86: host to hyp donation api without iotlb flush
    __pkvm_host_donate_hyp currently flushes iotlb after taking iommu lock.
    Sometimes we do need this as the caller may be explicitly flushing iotlb
    after this call. Also it may cause deadlocks if we take iommu lock when
    donating pages. So create a new api __pkvm_host_donate_hyp_flush which
    takes a new boolean argument to check whether to flush or not.
    
    Bug: 391542119
    Test: Build and Boot
    
    Change-Id: I80061203dbfd3bb69867c95966c2804b8c96fefa
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

--- arch/x86/kvm/vmx/pkvm/hyp/mem_protect.c
+++ arch/x86/kvm/vmx/pkvm/hyp/mem_protect.c
@@ -361,7 +361,7 @@ static int do_donate(const struct pkvm_mem_transition *donation)
 	return WARN_ON(__do_donate(donation));
 }
 
-int __pkvm_host_donate_hyp(u64 hpa, u64 size)
+int __pkvm_host_donate_hyp_noflush(u64 hpa, u64 size)
 {
 	int ret;
 	u64 hyp_addr = (u64)__pkvm_va(hpa);
@@ -387,10 +387,13 @@ int __pkvm_host_donate_hyp(u64 hpa, u64 size)
 
 	host_ept_unlock();
 
-	/*
-	 * Also need to flush the IOTLB as host EPT is used
-	 * as second-stage IOMMU page table for some devices.
-	 */
+	return ret;
+}
+
+int __pkvm_host_donate_hyp(u64 hpa, u64 size)
+{
+	int ret =  __pkvm_host_donate_hyp_noflush(hpa, size);
+
 	pkvm_iommu_flush_iotlb(pkvm_hyp->host_vm.ept, hpa, size);
 
 	return ret;
