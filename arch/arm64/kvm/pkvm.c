f789a866f86ab52914f3804c87db207e0b40e875
Vincent Donnefort <vdonnefort@google.com>
2025-05-08 15:52:11 +0100

    ANDROID: KVM: arm64: Redirect modprobe to /dev/console
    When using early loading of pKVM modules, redirect modprobe
    stdout/stderr to /dev/console to get logs when something goes wrong.
    
    Bug: 357781595
    Bug: 254835242
    Bug: 415374827
    Change-Id: I9d435071f5ec7b47a702ce5e9802b882a4ea9cb4
    Signed-off-by: Vincent Donnefort <vdonnefort@google.com>

--- arch/arm64/kvm/pkvm.c
+++ arch/arm64/kvm/pkvm.c
@@ -927,9 +927,15 @@ static int __init early_pkvm_modules_cfg(char *arg)
 }
 early_param("kvm-arm.protected_modules", early_pkvm_modules_cfg);
 
-static void free_modprobe_argv(struct subprocess_info *info)
+static void __init free_modprobe_argv(struct subprocess_info *info)
 {
 	kfree(info->argv);
+}
+
+static int __init init_modprobe(struct subprocess_info *info, struct cred *new)
+{
+	console_on_rootfs();
+	return 0;
 }
 
 /*
@@ -974,7 +980,7 @@ static int __init __pkvm_request_early_module(char *module_name,
 	argv[idx++] = NULL;
 
 	info = call_usermodehelper_setup(modprobe_path, argv, envp, GFP_KERNEL,
-					 NULL, free_modprobe_argv, NULL);
+					 init_modprobe, free_modprobe_argv, NULL);
 	if (!info)
 		goto err;
 
