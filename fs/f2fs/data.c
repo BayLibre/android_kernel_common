01b4d8135fad86e098bcfcda121f0a1ccd33cbd0
Christoph Hellwig <hch@lst.de>
2025-05-08 07:14:30 +0200

    UPSTREAM: f2fs: always unlock the page in f2fs_write_single_data_page
    Consolidate the code to unlock the page in f2fs_write_single_data_page
    instead of leaving it to the callers for the AOP_WRITEPAGE_ACTIVATE case.
    Replace AOP_WRITEPAGE_ACTIVATE with a positive return of 1 as this case
    now doesn't match the historic ->writepage special return code that is
    on it's way out now that ->writepage has been removed.
    
    Change-Id: I82e8a57b3ae1e489f8100272c8a43c985f21cb1f
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    (cherry picked from commit 84c5d16711a300949cfec83e76299a75b4cdf04a)

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -2942,9 +2942,9 @@ redirty_out:
 	 * file_write_and_wait_range() will see EIO error, which is critical
 	 * to return value of fsync() followed by atomic_write failure to user.
 	 */
-	if (!err)
-		return AOP_WRITEPAGE_ACTIVATE;
 	folio_unlock(folio);
+	if (!err)
+		return 1;
 	return err;
 }
 
@@ -3158,8 +3158,6 @@ continue_unlock:
 			ret = f2fs_write_single_data_page(folio,
 					&submitted, &bio, &last_block,
 					wbc, io_type, 0, true);
-			if (ret == AOP_WRITEPAGE_ACTIVATE)
-				folio_unlock(folio);
 #ifdef CONFIG_F2FS_FS_COMPRESSION
 result:
 #endif
@@ -3171,7 +3169,7 @@ result:
 				 * keep nr_to_write, since vfs uses this to
 				 * get # of written pages.
 				 */
-				if (ret == AOP_WRITEPAGE_ACTIVATE) {
+				if (ret == 1) {
 					ret = 0;
 					goto next;
 				} else if (ret == -EAGAIN) {
