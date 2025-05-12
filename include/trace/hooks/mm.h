5bd92af06115345566602599738d80588a0af017
Tangquan Zheng <zhengtangquan@oppo.com>
2025-05-12 16:14:31 +0800

    ANDROID: madvise: add vendor hook to activate/unactivate folios
    Add hooks for folio activation and deactivation.
    The vendor hook helps us determine whether to activate folios in certain cases.
    
    Bug: 417133503
    Change-Id: I5cecd1661a8817a0aa06f4d72271dd7b82d75f6a
    Signed-off-by: Tangquan Zheng <zhengtangquan@oppo.com>

--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -577,6 +577,18 @@ DECLARE_HOOK(android_vh_alloc_swap_slot_cache,
 DECLARE_HOOK(android_vh_calculate_totalreserve_pages,
 	TP_PROTO(bool *skip),
 	TP_ARGS(skip));
+DECLARE_HOOK(android_vh_folio_add_lru_folio_activate,
+	TP_PROTO(struct folio *folio, bool *bypass),
+	TP_ARGS(folio, bypass));
+DECLARE_HOOK(trace_android_vh_filemap_fault_folio_locked,
+	TP_PROTO(struct folio *folio),
+	TP_ARGS(folio));
+DECLARE_HOOK(trace_android_vh_filemap_folio_mapped,
+	TP_PROTO(struct folio *folio),
+	TP_ARGS(folio));
+DECLARE_HOOK(android_vh_folio_remove_rmap_ptes,
+	TP_PROTO(struct folio *folio),
+	TP_ARGS(folio));
 #endif /* _TRACE_HOOK_MM_H */
 
 /* This part must be outside protection */
