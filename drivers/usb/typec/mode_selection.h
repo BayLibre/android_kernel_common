f07561150464f5fbc85ec470e4e705fb0b7e3873
Andrei Kuchynski <akuchynski@chromium.org>
2025-05-16 13:00:26 +0000

    BACKPORT: FROMLIST: usb: typec: Implement automated mode selection
    This patch introduces new sysfs attributes to enable user control over
    Type-C automated mode selection and provide negotiation feedback.
    
    `mode_selection` attribute shows a prioritized list of supported modes
    with the currently entered mode bracketed. Writing boolean 0 or 1 to
    this attribute starts or stops the mode selection process,
    respectively.
    
    Signed-off-by: Andrei Kuchynski <akuchynski@chromium.org>
    
    Bug: 382333876
    Upstream-Task: 441243047
    Link: https://lore.kernel.org/all/20250804090340.3062182-6-akuchynski@chromium.org/
    Change-Id: Ib3d41583ea8bcafd22d634c2aed134a78ccfac24
    Signed-off-by: Andrei Kuchynski <akuchynski@google.com>

--- drivers/usb/typec/mode_selection.h
+++ drivers/usb/typec/mode_selection.h
@@ -4,3 +4,26 @@
 
 void typec_mode_set_priority(struct typec_altmode *alt,
 		const unsigned int priority);
+
+/**
+ * The mode selection process follows a lifecycle tied to the USB-C partner
+ * device. The API is designed to first build a set of desired modes and then
+ * trigger the selection process. The expected sequence of calls is as follows:
+ *
+ * Creation and Configuration:
+ * call typec_mode_selection_add_partner() when the partner device is being set
+ * up.
+ *
+ * Execution:
+ * Call typec_mode_selection_start() to trigger the mode selection.
+ * Call typec_mode_selection_reset() to prematurely stop the selection
+ * process and clear any stored results.
+ *
+ * Destruction:
+ * Before destroying a partner, call typec_mode_selection_remove_partner()
+ */
+void typec_mode_selection_add_partner(struct typec_partner *partner);
+void typec_mode_selection_remove_partner(struct typec_partner *partner);
+int typec_mode_selection_start(struct typec_partner *partner);
+int typec_mode_selection_is_pending(struct typec_partner *partner);
+int typec_mode_selection_reset(struct typec_partner *partner);
