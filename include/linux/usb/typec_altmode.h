553c26983c7ffcf91fa26ac6eb9f7b66593701f7
Andrei Kuchynski <akuchynski@chromium.org>
2025-05-16 13:00:26 +0000

    BACKPORT: FROMLIST: usb: typec: Implement mode selection
    This patch adds APIs for managing the alternate mode selection process,
    enabling the initiation and termination of mode entry based on each
    mode's priority.
    
    Signed-off-by: Andrei Kuchynski <akuchynski@chromium.org>
    
    Bug: 382333876
    Upstream-Task: 441243047
    Link: https://lore.kernel.org/all/20250909123028.2127449-2-akuchynski@chromium.org/
    Change-Id: Ib3d41583ea8bcafd22d634c2aed134a78ccfac24
    Signed-off-by: Andrei Kuchynski <akuchynski@google.com>

--- include/linux/usb/typec_altmode.h
+++ include/linux/usb/typec_altmode.h
@@ -221,4 +221,15 @@ void typec_altmode_unregister_driver(struct typec_altmode_driver *drv);
 	module_driver(__typec_altmode_driver, typec_altmode_register_driver, \
 		      typec_altmode_unregister_driver)
 
+/**
+ * typec_altmode_entry_complete - Complete an alternate mode entry request
+ * @altmode: Handle to the alternate mode.
+ * @result: Result of the entry operation.
+ *
+ * This function should be called by a driver to report the final result of
+ * an asynchronous alternate mode entry request.
+ */
+void typec_altmode_entry_complete(struct typec_altmode *altmode,
+				const int result);
+
 #endif /* __USB_TYPEC_ALTMODE_H */
