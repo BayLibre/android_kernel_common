7c2336814f657f93d2457adccd4937f872d2750b
Chungkai Mei <chungkai@google.com>
2025-05-19 03:12:34 +0000

    ANDROID: vendor_hook: add trace_android_rvh_setscheduler
    To modify priority of specific tasks, add this existing vendor hook in rt_mutex_setprio
    
    Bug: 409176857
    Change-Id: Id5a2309378f1a8c3ecc1de71c20f44f73b3f7557
    Signed-off-by: Chungkai Mei <chungkai@google.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -7220,6 +7220,7 @@ void rt_mutex_setprio(struct task_struct *p, struct task_struct *pi_task)
 	}
 
 	__setscheduler_prio(p, prio);
+	trace_android_rvh_setscheduler(p);
 
 	if (queued)
 		enqueue_task(rq, p, queue_flag);
