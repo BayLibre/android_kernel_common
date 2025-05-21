f9f4e65743d0eb7c5c9eaf35a4110b402defc4bd
Kuniyuki Iwashima <kuniyu@amazon.com>
2025-05-21 14:45:15 +0000

    UPSTREAM: af_unix: Allocate struct unix_vertex for each inflight AF_UNIX fd.
    commit 1fbfdfaa590248c1d86407f578e40e5c65136330 upstream.
    
    We will replace the garbage collection algorithm for AF_UNIX, where
    we will consider each inflight AF_UNIX socket as a vertex and its file
    descriptor as an edge in a directed graph.
    
    This patch introduces a new struct unix_vertex representing a vertex
    in the graph and adds its pointer to struct unix_sock.
    
    When we send a fd using the SCM_RIGHTS message, we allocate struct
    scm_fp_list to struct scm_cookie in scm_fp_copy().  Then, we bump
    each refcount of the inflight fds' struct file and save them in
    scm_fp_list.fp.
    
    After that, unix_attach_fds() inexplicably clones scm_fp_list of
    scm_cookie and sets it to skb.  (We will remove this part after
    replacing GC.)
    
    Here, we add a new function call in unix_attach_fds() to preallocate
    struct unix_vertex per inflight AF_UNIX fd and link each vertex to
    skb's scm_fp_list.vertices.
    
    When sendmsg() succeeds later, if the socket of the inflight fd is
    still not inflight yet, we will set the preallocated vertex to struct
    unix_sock.vertex and link it to a global list unix_unvisited_vertices
    under spin_lock(&unix_gc_lock).
    
    If the socket is already inflight, we free the preallocated vertex.
    This is to avoid taking the lock unnecessarily when sendmsg() could
    fail later.
    
    In the following patch, we will similarly allocate another struct
    per edge, which will finally be linked to the inflight socket's
    unix_vertex.edges.
    
    And then, we will count the number of edges as unix_vertex.out_degree.
    
    Bug: 404256079
    Signed-off-by: Kuniyuki Iwashima <kuniyu@amazon.com>
    Acked-by: Paolo Abeni <pabeni@redhat.com>
    Link: https://lore.kernel.org/r/20240325202425.60930-2-kuniyu@amazon.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Lee Jones <lee@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 9e98ba0c735292cde35696ac841cbf7beceb306b)
    Signed-off-by: Lee Jones <joneslee@google.com>
    Change-Id: Ibba6b1f4cbd5bfa7a53b8f297b4c0d057237de16

--- net/core/scm.c
+++ net/core/scm.c
@@ -93,6 +93,9 @@ static int scm_fp_copy(struct cmsghdr *cmsg, struct scm_fp_list **fplp)
 		fpl_ext->count_unix = 0;
 		fpl->max = SCM_MAX_FD;
 		fpl->user = NULL;
+#if IS_ENABLED(CONFIG_UNIX)
+		INIT_LIST_HEAD(&fpl->vertices);
+#endif
 	}
 	fpp = &fpl->fp[fpl->count];
 
@@ -411,6 +414,9 @@ struct scm_fp_list *scm_fp_dup(struct scm_fp_list *fpl)
 		new_fpl = &new_fpl_ext->fpl;
 		new_fpl->max = new_fpl->count;
 		new_fpl->user = get_uid(fpl->user);
+#if IS_ENABLED(CONFIG_UNIX)
+		INIT_LIST_HEAD(&new_fpl->vertices);
+#endif
 	}
 	return new_fpl;
 }
