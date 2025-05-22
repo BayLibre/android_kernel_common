93276483ce07881724b1e4c092f9db8b3f69e39c
Andrei Homescu <ahomescu@xwf.google.com>
2025-05-22 01:56:37 +0000

    ANDROID: KVM: Add a total timeout for VM availability messages
    After a few initial retries, sleep between each attempt
    at sending the VM availability messages. When a total timeout and
    number of retries are hit, return an error to user space instead.
    
    Bug: 269285339
    Bug: 278749606
    Change-Id: I35fb3f5ad42975ce9bc84b7a89cb0fdc4a20668e
    Signed-off-by: Andrei Homescu <ahomescu@xwf.google.com>

--- arch/arm64/kvm/pkvm.c
+++ arch/arm64/kvm/pkvm.c
@@ -4,6 +4,7 @@
  * Author: Quentin Perret <qperret@google.com>
  */
 
+#include <linux/delay.h>
 #include <linux/init.h>
 #include <linux/initrd.h>
 #include <linux/interval_tree_generic.h>
@@ -38,6 +39,15 @@
 #include "hyp_trace.h"
 
 #define PKVM_DEVICE_ASSIGN_COMPAT	"pkvm,device-assignment"
+
+/*
+ * Retry the VM creation message for the host for a maximul total
+ * amount of times, with sleeps in between. For the first few attempts,
+ * do a faster reschedule instead of a full sleep.
+ */
+#define VM_AVAILABILITY_FAST_RETRIES	5
+#define VM_AVAILABILITY_TOTAL_RETRIES	500
+#define VM_AVAILABILITY_RETRY_SLEEP_MS	10
 
 DEFINE_STATIC_KEY_FALSE(kvm_protected_mode_initialized);
 
@@ -363,22 +373,35 @@ static int __reclaim_dying_guest_page_call(u64 pfn, u64 gfn, u8 order, void *arg
 
 static int __pkvm_notify_guest_vm_avail_retry(struct kvm *host_kvm)
 {
-	int ret, retry_availability_msg = 5;
+	int ret, retries;
+	long timeout;
 
 	if (!host_kvm->arch.pkvm.ffa_support)
 		return 0;
 
-	do {
+	for (retries = 0; retries < VM_AVAILABILITY_TOTAL_RETRIES; retries++) {
 		ret = kvm_call_hyp_nvhe(__pkvm_notify_guest_vm_avail,
 					host_kvm->arch.pkvm.handle);
 		if (!ret)
-			break;
-		else if (ret == -EINTR || ret == -EAGAIN) {
-			retry_availability_msg--;
+			return 0;
+		else if (ret != -EINTR && ret != -EAGAIN)
+			return ret;
+
+		if (retries < VM_AVAILABILITY_FAST_RETRIES) {
 			cond_resched();
-		} else
-			break;
-	} while (retry_availability_msg >= 0);
+		} else {
+			timeout = msecs_to_jiffies(VM_AVAILABILITY_RETRY_SLEEP_MS);
+			timeout = schedule_timeout_killable(timeout);
+			if (timeout) {
+				/*
+				 * The timer did not expire,
+				 * most likely because the
+				 * process was killed.
+				 */
+				return ret;
+			}
+		}
+	}
 
 	return ret;
 }
