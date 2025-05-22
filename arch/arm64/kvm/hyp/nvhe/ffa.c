b5e790608f3bdb358336a05a2720e8b1c9795313
Per Larsen <perlarsen@google.com>
2025-05-22 23:18:42 +0000

    ANDROID: KVM: arm64: Release rx buf after FFA_PARTITION_INFO_GET
    do_ffa_part_get_response calls FFA_PARTITION_INFO_GET (which can take
    ownership of the rx buffer) and expects callers to release rx buf. One
    of the callers (do_ffa_part_get) did not release the rx buffer.
    
    The rules for when rx buf must be released are non-trivial so releasing
    rx buf in do_ffa_part_get_reponse is less brittle than inserting the
    missing release operation in do_ffa_part_get.
    
    Bug: 269285339
    Bug: 278749606
    Change-Id: Ib6e4f74000a5d2ca5147c7acfa41bf5db1790be8
    Signed-off-by: Per Larsen <perlarsen@google.com>

--- arch/arm64/kvm/hyp/nvhe/ffa.c
+++ arch/arm64/kvm/hyp/nvhe/ffa.c
@@ -1108,6 +1108,7 @@ static void do_ffa_part_get_response(struct arm_smccc_res *res,
 {
 	int ret;
 	u32 count, partition_sz, copy_sz;
+	struct arm_smccc_res rx_res;
 
 	arm_smccc_1_1_smc(FFA_PARTITION_INFO_GET, uuid0, uuid1,
 			  uuid2, uuid3, flags, 0, 0,
@@ -1118,11 +1119,13 @@ static void do_ffa_part_get_response(struct arm_smccc_res *res,
 
 	count = res->a2;
 	if (!count)
+		/* rx buf ownership not transferred */
 		return;
 
 	if (hyp_ffa_version > FFA_VERSION_1_0) {
 		/* Get the number of partitions deployed in the system */
 		if (flags & 0x1)
+			/* rx buf ownership not transferred */
 			return;
 
 		partition_sz  = res->a3;
@@ -1133,7 +1136,7 @@ static void do_ffa_part_get_response(struct arm_smccc_res *res,
 	copy_sz = partition_sz * count;
 	if (copy_sz > KVM_FFA_MBOX_NR_PAGES * PAGE_SIZE) {
 		ffa_to_smccc_res(res, FFA_RET_ABORTED);
-		return;
+		goto unlock;
 	}
 
 	ret = parse_vm_availability_resp(partition_sz, count);
@@ -1142,6 +1145,9 @@ static void do_ffa_part_get_response(struct arm_smccc_res *res,
 
 	if (ffa_buf)
 		memcpy(ffa_buf->rx, hyp_buffers.rx, copy_sz);
+unlock:
+	/* Errors intentionally ignored */
+	ffa_rx_release(&rx_res);
 }
 
 static int hyp_ffa_post_init(void)
@@ -1298,8 +1304,6 @@ static int kvm_host_ffa_signal_availability(void)
 	do_ffa_part_get_response(&res, 0, 0, 0, 0, 0, NULL);
 	if (res.a0 != FFA_SUCCESS)
 		return ffa_to_linux_errno(ret);
-
-	ffa_rx_release(&res);
 
 	return kvm_notify_vm_availability(HOST_FFA_ID, &host_buffers, FFA_VM_CREATION_MSG);
 }
