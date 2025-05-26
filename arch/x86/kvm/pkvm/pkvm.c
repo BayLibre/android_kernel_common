629e1892a23d97fe074bfc526f46c39e18c4dcdb
Zhiyuan Lv <zhiyuan.lv@intel.com>
2025-05-26 15:33:11 +0800

    ANDROID: pkvm: x86: reuse retpoline.S in kernel for pkvm
    Just reuse retpoline.S in kernel to compile into pkvm hypervisor module
    instead of maintaining a copied file in pkvm.
    
    Change-Id: I4ba7abadc606a3d208fdb5c6416b8ae9be8bbee0
    Signed-off-by: Zhiyuan Lv <zhiyuan.lv@intel.com>

--- arch/x86/kvm/pkvm/pkvm.c
+++ arch/x86/kvm/pkvm/pkvm.c
@@ -1521,3 +1521,8 @@ void pkvm_x86_ops_init(struct pkvm_x86_ops *ops)
 {
 	memcpy(&pkvm_x86_ops, ops, sizeof(struct pkvm_x86_ops));
 }
+
+void warn_thunk_thunk(void)
+{
+	WARN_ONCE(1, "pkvm: Unpatched return thunk in use. This should not happen!\n");
+}
