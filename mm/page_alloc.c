4aabb7aa476f18cea382ff2fd089dcc8df872a3e
Yang Yang <yang.yang@vivo.com>
2025-06-04 12:04:42 +0800

    ANDROID: mm: Reset unused page flag bits on free
    Add vendor hook to reset unused bits in page flags to zero when
    releasing a page. This reserves space for OEMs to define
    vendor-specific features.
    
    Bug: 420361650
    Bug: 426115925
    Bug: 491161822
    Change-Id: Ife2b96de64eea670d257f1a8d9fd07d16c0ddc83
    Signed-off-by: Yang Yang <yang.yang@vivo.com>
    (cherry picked from commit 8fb77f6f9d20be56cd76b579b2830eb287e2e8bb)
    Signed-off-by: ying zuxin <11154159@vivo.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -1499,6 +1499,7 @@ __always_inline bool __free_pages_prepare(struct page *page,
 				}
 			}
 			(page + i)->flags.f &= ~PAGE_FLAGS_CHECK_AT_PREP;
+			trace_android_vh_mm_free_page(page + i);
 		}
 	}
 	if (folio_test_anon(folio)) {
@@ -1518,7 +1519,11 @@ __always_inline bool __free_pages_prepare(struct page *page,
 
 	page_cpupid_reset_last(page);
 	page->flags.f &= ~PAGE_FLAGS_CHECK_AT_PREP;
+<<<<<<< HEAD
 	page->private = 0;
+=======
+	trace_android_vh_mm_free_page(page);
+>>>>>>> 04816273feb0 (ANDROID: mm: Reset unused page flag bits on free)
 	reset_page_owner(page, order);
 	page_table_check_free(page, order);
 	pgalloc_tag_sub(page, 1 << order);
