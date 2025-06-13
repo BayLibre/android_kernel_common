cf7b28e289703eda0cccc9094efdec6c851c5f44
liulu liu <liulu.liu@honor.corp-partner.google.com>
2025-06-13 11:09:20 +0800

    ANDROID: vendor_hooks: add one hook for lazy preemption
    add some changes to achieve the lazy preemption feature in our baseline.
    - android_vh_set_tsk_need_resched_lazy
    
    Bug: 424627098
    
    Change-Id: I2c59f6ba7ef0fe4513e2febaa0f50f00dab8f3bc
    Signed-off-by: liulu liu <liulu.liu@honor.corp-partner.google.com>
    (cherry picked from commit 75547173ff9c2ebe5002749b07c5a1e45c87eb24)

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -547,6 +547,25 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_oom_swapmem_gather_finish);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_set_gfp_zone_flags);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_set_readahead_gfp_mask);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_shrink_node_memcgs);
+<<<<<<< HEAD   (3b53c0 FROMGIT: mm: page_alloc: tighten up find_suitable_fallback())
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mem_cgroup_charge);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_filemap_add_folio);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_shrink_node);
+||||||| BASE
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mmap_region);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_try_to_unmap_one);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_resume_begin);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_resume_end);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_early_resume_begin);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_use_amu_fie);
+
+=======
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mmap_region);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_try_to_unmap_one);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_resume_begin);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_resume_end);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_early_resume_begin);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_use_amu_fie);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_set_tsk_need_resched_lazy);
+
+>>>>>>> CHANGE (755471 ANDROID: vendor_hooks: add one hook for lazy preemption)
