19b02df49438a4b876ce7a47316f37abdfb27591
Per Larsen <perlarsen@google.com>
2025-06-17 02:45:06 +0000

    ANDROID: KVM: arm64: Use SMCCC 1.2 in guest handler
    SMCCC 1.1 and prior allows four registers to be sent back as a result
    of an FF-A interface. SMCCC 1.2 increases the number of results that can
    be sent back to 8 and 16 for 32-bit and 64-bit SMC/HVCs respectively.
    
    Update guest ffa handler and related subroutines to use SMCCC 1.2.
    
    Bug: 280886201
    Change-Id: Ia7fb3184a5e5803f3b705b7cf3d0cb3de51a0344
    Signed-off-by: Per Larsen <perlarsen@google.com>

--- arch/arm64/kvm/hyp/nvhe/ffa.c
+++ arch/arm64/kvm/hyp/nvhe/ffa.c
@@ -1279,7 +1279,8 @@ out_handled:
 	return true;
 }
 
-static void do_ffa_guest_features(struct arm_smccc_res *res, struct kvm_cpu_context *ctxt)
+static void do_ffa_guest_features(struct arm_smccc_1_2_regs *res,
+				  struct kvm_cpu_context *ctxt)
 {
 	DECLARE_REG(u32, id, ctxt, 1);
 	u64 prop = 0;
@@ -1450,7 +1451,8 @@ unlock:
 	hyp_spin_unlock(&version_lock);
 }
 
-static void do_ffa_guest_version(struct arm_smccc_res *res, struct kvm_cpu_context *ctxt,
+static void do_ffa_guest_version(struct arm_smccc_1_2_regs *res,
+				 struct kvm_cpu_context *ctxt,
 				 struct pkvm_hyp_vcpu *hyp_vcpu)
 {
 	DECLARE_REG(u32, ffa_req_version, ctxt, 1);
@@ -1468,7 +1470,7 @@ static void do_ffa_guest_version(struct arm_smccc_res *res, struct kvm_cpu_conte
 	hyp_spin_unlock(&version_lock);
 }
 
-static void do_ffa_part_get(struct arm_smccc_res *res,
+static void do_ffa_part_get(struct arm_smccc_1_2_regs *res,
 			    struct kvm_cpu_context *ctxt,
 			    struct pkvm_hyp_vcpu *hyp_vcpu)
 {
@@ -1647,7 +1649,7 @@ bool kvm_guest_ffa_handler(struct pkvm_hyp_vcpu *hyp_vcpu, u64 *exit_code)
 {
 	struct kvm_vcpu *vcpu = &hyp_vcpu->vcpu;
 	struct kvm_cpu_context *ctxt = &vcpu->arch.ctxt;
-	struct arm_smccc_res res;
+	struct arm_smccc_1_2_regs res;
 	int ret, hyp_alloc_ret;
 	struct kvm_hyp_req *req;
 
@@ -1751,7 +1753,7 @@ static void kvm_guest_try_reclaim_transfer(struct ffa_mem_transfer *transfer,
 					   struct pkvm_hyp_vm *vm)
 {
 	struct ffa_translation *translation, *tmp;
-	struct arm_smccc_res res;
+	struct arm_smccc_1_2_regs res;
 
 	ffa_mem_reclaim(&res, HANDLE_LOW(transfer->ffa_handle),
 			HANDLE_HIGH(transfer->ffa_handle), 0);
