4ccf6b327d556f9dd9fb7b627d66cda61c95cc0f
Lee Jones <joneslee@google.com>
2025-06-18 10:43:01 +0100

    [DO NOT MERGE] Hacky debug code
    Change-Id: I0977b0ad50f067400c3efb2f2762efa8bdec4ce2

--- net/core/scm.c
+++ net/core/scm.c
@@ -368,8 +368,20 @@ struct scm_fp_list *scm_fp_dup(struct scm_fp_list *fpl)
 	struct scm_fp_list *new_fpl;
 	int i;
 
+	static unsigned int count = 0;
+	static u64 total_saved = 0;
+	unsigned int whole, chopped, saved;
+
 	if (!fpl)
 		return NULL;
+
+	whole = sizeof(*fpl);
+	chopped = offsetof(struct scm_fp_list, fp[fpl->count]);
+	saved = whole - chopped;
+	total_saved += saved;
+
+	printk("LEE: %s %s()[%d]: [%u] whole: %u chopped: %u saved: %u total_saved: %llu\n",
+		__FILE__, __func__, __LINE__, count, whole, chopped, saved, total_saved);
 
 	new_fpl = kmemdup(fpl, offsetof(struct scm_fp_list, fp[fpl->count]),
 			  GFP_KERNEL_ACCOUNT);
