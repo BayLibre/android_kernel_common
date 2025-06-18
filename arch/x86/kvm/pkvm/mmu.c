64015ba057caa3eb537da5079c59f47a4067a5d6
Dmytro Maluka <dmaluka@google.com>
2025-06-18 15:31:27 +0000

    ANDROID: pkvm: x86: Implement PV MMU map hypercall
    With PV MMU, the host shall use this hypercall to explicitly ask pKVM
    to map pages for the guest, instead of using virtual EPT page tables
    shadowed by pKVM.
    
    Bug: 391541180
    Test: compile
    
    Change-Id: I6fbf8648d94995e22bf82b5086d92272530a78c2
    Signed-off-by: Dmytro Maluka <dmaluka@google.com>

--- arch/x86/kvm/pkvm/mmu.c
+++ arch/x86/kvm/pkvm/mmu.c
@@ -8,6 +8,7 @@
 #include <vmx/pkvm/hyp/pgtable.h>
 #include <vmx/pkvm/hyp/gfp.h>
 #include <vmx/pkvm/hyp/ept.h>	//FIXME
+#include <vmx/pkvm/hyp/mem_protect.h>
 
 /*
  * FIXME: temporarily reusing the shadow pgt memory pool.
@@ -50,6 +51,53 @@ int pkvm_vm_mmu_init(struct pkvm_vm *pkvm_vm)
 
 	return pkvm_pgtable_init(&pkvm_vm->pgt, &pkvm_vm->pgt_mm_ops, pgt_ops,
 				 &cap, true);
+}
+
+static int guest_pgt_map_leaf(struct pkvm_pgtable *pgt, unsigned long vaddr, int level,
+			      void *ptep, struct pgt_flush_data *flush_data, void *arg)
+{
+	struct pkvm_pgtable_map_data *data = arg;
+	u64 size = pgt->pgt_ops->pgt_level_to_size(level);
+	struct kvm *kvm = pgt_to_kvm(pgt);
+	int ret;
+
+	/*
+	 * It is possible that another CPU has just created the same mapping
+	 * when multiple CPUs touch the same page simultaneously.
+	 * TODO: check this on the host side, before making the hypercall.
+	 */
+	if (pgt->pgt_ops->pgt_entry_present(ptep))
+		return 0;
+
+	/*
+	 * TODO: use a more suitable API than the existing page state API. Why walk
+	 * the page table once again to reach this PTE if we are already at it?
+	 * We could combine these 2 layers (MMU and page state API) into a single layer.
+	 */
+	if (pkvm_is_protected_vm(kvm))
+		ret = __pkvm_host_donate_guest(data->phys, pgt, vaddr, size, data->prot);
+	else
+		ret = __pkvm_host_share_guest(data->phys, pgt, vaddr, size, data->prot);
+
+	return ret;
+}
+
+int pkvm_vm_mmu_map(int vm_handle, u64 gpa, u64 hpa, u64 size)
+{
+	u64 prot = HOST_EPT_DEF_MEM_PROT;	/* FIXME */
+	struct pkvm_vm *pkvm_vm;
+	int ret;
+
+	pkvm_vm = get_pkvm_vm(vm_handle);
+	if (!pkvm_vm)
+		return -EINVAL;
+
+	pkvm_spin_lock(&pkvm_vm->pgt_lock);
+	ret = pkvm_pgtable_map(&pkvm_vm->pgt, gpa, hpa, size, 0, prot, guest_pgt_map_leaf);
+	pkvm_spin_unlock(&pkvm_vm->pgt_lock);
+
+	put_pkvm_vm(pkvm_vm);
+	return ret;
 }
 
 void pkvm_vm_mmu_destroy(struct pkvm_vm *pkvm_vm)
