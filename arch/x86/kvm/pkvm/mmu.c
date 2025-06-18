a0e1bbe02cefdd44218de0f8b4a153f40c70ee07
Dmytro Maluka <dmaluka@google.com>
2025-06-18 15:31:27 +0000

    WIP: pkvm: x86: Implement PV MMU map hypercall
    Change-Id: I6fbf8648d94995e22bf82b5086d92272530a78c2
    Signed-off-by: Dmytro Maluka <dmaluka@google.com>

--- arch/x86/kvm/pkvm/mmu.c
+++ arch/x86/kvm/pkvm/mmu.c
@@ -6,6 +6,7 @@
 #include <vmx/pkvm/hyp/pkvm_hyp.h>
 #include <vmx/pkvm/hyp/pgtable.h>
 #include <vmx/pkvm/hyp/ept.h>
+#include <vmx/pkvm/hyp/mem_protect.h>
 
 int pkvm_vm_mmu_init(struct pkvm_vm *pkvm_vm)
 {
@@ -21,6 +22,29 @@ int pkvm_vm_mmu_init(struct pkvm_vm *pkvm_vm)
 	 */
 	return pkvm_pgtable_init(&pkvm_vm->pgt, &shadow_ept_mm_ops, &ept_ops,
 				 &pkvm_hyp->ept_cap, true);
+}
+
+int pkvm_vm_mmu_map(int handle, u64 gpa, u64 hpa, u64 size)
+{
+	u64 prot = HOST_EPT_DEF_MEM_PROT;	/* FIXME */
+	struct pkvm_vm *pkvm_vm;
+	int ret;
+
+	pkvm_vm = get_pkvm_vm(handle);
+	if (!pkvm_vm)
+		return -EINVAL;
+
+	pkvm_spin_lock(&pkvm_vm->lock);
+
+	if (pkvm_is_protected_vm(to_kvm(pkvm_vm)))
+		ret = __pkvm_host_donate_guest(hpa, &pkvm_vm->pgt, gpa, size, prot);
+	else
+		ret = __pkvm_host_share_guest(hpa, &pkvm_vm->pgt, gpa, size, prot);
+
+	pkvm_spin_unlock(&pkvm_vm->lock);
+
+	put_pkvm_vm(pkvm_vm);
+	return ret;
 }
 
 void pkvm_vm_mmu_destroy(struct pkvm_vm *pkvm_vm)
