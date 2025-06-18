696a7d01f9339575c22f5999b61de4134abb085a
Dmytro Maluka <dmaluka@google.com>
2025-06-18 15:36:38 +0000

    WIP: pkvm: x86: Implement removing PV MMU mappings on VM teardown
    Based on the existing implementation for shadow EPT, see e.g. Jason's
    commit 5cbc19b43eba ("pkvm: x86: add shadow EPT invalidation & destroy
    support for protected VM").
    
    Change-Id: Ibd4ea7c5d576c268b23f8776a0003c786d770eba
    Signed-off-by: Dmytro Maluka <dmaluka@google.com>

--- arch/x86/kvm/pkvm/mmu.c
+++ arch/x86/kvm/pkvm/mmu.c
@@ -7,6 +7,7 @@
 #include <vmx/pkvm/hyp/pgtable.h>
 #include <vmx/pkvm/hyp/ept.h>
 #include <vmx/pkvm/hyp/mem_protect.h>
+#include <vmx/pkvm/hyp/memory.h>
 
 int pkvm_vm_mmu_init(struct pkvm_vm *pkvm_vm)
 {
@@ -47,7 +48,47 @@ int pkvm_vm_mmu_map(int handle, u64 gpa, u64 hpa, u64 size)
 	return ret;
 }
 
+static int pkvm_vm_pgt_free_leaf(struct pkvm_pgtable *pgt, unsigned long vaddr, int level,
+				 void *ptep, struct pgt_flush_data *flush_data, void *arg)
+{
+	unsigned long phys = pgt->pgt_ops->pgt_entry_to_phys(ptep);
+	unsigned long size = pgt->pgt_ops->pgt_level_to_size(level);
+	struct pkvm_vm *pkvm_vm = container_of(pgt, struct pkvm_vm, pgt);
+
+	/* TODO: this actually happens, why? */
+	if (WARN_ON_ONCE(!pgt->pgt_ops->pgt_entry_present(ptep)))
+		return 0;
+
+	/*
+	 * The pgtable_free_cb in this current page walker is still walking
+	 * the page state table so cannot allow the  __pkvm_host_unshare_guest()
+	 * or __pkvm_host_undonate_guest() release the page table pages. So
+	 * we shall get_page before these APIs called, then put_page to allow
+	 * pgtable_free_cb free table pages with correct refcount.
+	 */
+	if (pkvm_is_protected_vm(to_kvm(pkvm_vm))) {
+		void *virt = pgt->mm_ops->phys_to_virt(phys);
+
+		/*
+		 * Before returning to host, the memory page previously owned by
+		 * protected VM shall be memset to 0 to avoid secret leakage.
+		 */
+		memset(virt, 0, size);
+		pkvm_clflush_cache_range(virt, size);
+
+		pgt->mm_ops->get_page(ptep);
+		WARN_ON_ONCE(__pkvm_host_undonate_guest(phys, pgt, vaddr, size));
+		pgt->mm_ops->put_page(ptep);
+	} else {
+		pgt->mm_ops->get_page(ptep);
+		WARN_ON_ONCE(__pkvm_host_unshare_guest(phys, pgt, vaddr, size));
+		pgt->mm_ops->put_page(ptep);
+	}
+
+	return 0;
+}
+
 void pkvm_vm_mmu_destroy(struct pkvm_vm *pkvm_vm)
 {
-	pkvm_pgtable_destroy(&pkvm_vm->pgt, NULL);
+	pkvm_pgtable_destroy(&pkvm_vm->pgt, pkvm_vm_pgt_free_leaf);
 }
