7e30f89907a47fcde4a41464934778fd4af360c7
Dmytro Maluka <dmaluka@google.com>
2025-06-18 16:40:07 +0000

    ANDROID: pkvm: x86: Switch to using PV MMU instead of emulated EPT
    Bug: 391541180
    Test: run various VMs
    
    Change-Id: I79fe91b6953e21da0a79268795e3909abeb2d1c2
    Signed-off-by: Dmytro Maluka <dmaluka@google.com>

--- arch/x86/kvm/pkvm/x86.c
+++ arch/x86/kvm/pkvm/x86.c
@@ -2711,9 +2711,15 @@ EXPORT_SYMBOL_GPL(kvm_emulate_halt);
 int kvm_arch_vcpu_create(struct kvm_vcpu *vcpu)
 {
 #ifdef __PKVM_HYP__
+	struct pkvm_vm *pkvm_vm = to_pkvm(vcpu->kvm);
+
 	vcpu->arch.last_vmentry_cpu = -1;
 	vcpu->arch.regs_avail = ~0;
 	vcpu->arch.regs_dirty = ~0;
+
+	vcpu->arch.root_mmu.root_role.level = pkvm_vm->pgt.level;
+	vcpu->arch.root_mmu.root.hpa = pkvm_vm->pgt.root_pa;
+	vcpu->arch.mmu = &vcpu->arch.root_mmu;
 
 	vcpu->arch.pat = MSR_IA32_CR_PAT_DEFAULT;
 
