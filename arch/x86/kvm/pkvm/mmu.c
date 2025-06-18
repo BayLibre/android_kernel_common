97d4a701fad9492b4ea2402e7631f08db4b24683
Dmytro Maluka <dmaluka@google.com>
2025-06-18 15:24:39 +0000

    ANDROID: pkvm: x86: Add PV MMU page table
    Add page table which shall be used for guest VMs instead of the existing
    shadow page tables. Implement is as a generic VMX-agnostic (and thus
    EPT-agnostic) MMU layer which uses guest_mmu_ops and guest_mmu_cap
    pre-initialized by the VMX/EPT layer.
    
    Temporarily reuse the same static memory pool that is currently used for
    shadow page tables, until we implement a memcache for allocating pKVM's
    pgtable pages by the host.
    
    Bug: 391541180
    Test: compile and boot
    Upstream-Task: 402758258
    
    Change-Id: I813fc0f81cc7a3879b2da188a014c29bd7fe0a8e
    Signed-off-by: Dmytro Maluka <dmaluka@google.com>

--- arch/x86/kvm/pkvm/mmu.c
+++ arch/x86/kvm/pkvm/mmu.c
@@ -1,8 +1,57 @@
 // SPDX-License-Identifier: GPL-2.0
+#include <asm/kvm_pkvm.h>
+#include <asm/pkvm_spinlock.h>
+#include "pkvm.h"
 #include "mmu.h"
 //FIXME: clean up the header files
+#include <vmx/pkvm/hyp/pkvm_hyp.h>
 #include <vmx/pkvm/hyp/pgtable.h>
+#include <vmx/pkvm/hyp/gfp.h>
+#include <vmx/pkvm/hyp/ept.h>	//FIXME
 #include <pkvm.h>
 
 struct pkvm_pgtable_ops guest_mmu_ops;
 struct pkvm_pgtable_cap guest_mmu_cap;
+
+/*
+ * FIXME: temporarily reusing the shadow pgt memory pool.
+ * Replace it with a memcache supplied by KVM-high.
+ */
+
+static void *guest_mmu_zalloc_page(void)
+{
+	return hyp_alloc_pages(&shadow_pgt_pool, 0);
+}
+
+static void guest_mmu_get_page(void *vaddr)
+{
+	hyp_get_page(&shadow_pgt_pool, vaddr);
+}
+
+static void guest_mmu_put_page(void *vaddr)
+{
+	hyp_put_page(&shadow_pgt_pool, vaddr);
+}
+
+int pkvm_vm_mmu_init(struct pkvm_vm *pkvm_vm)
+{
+	pkvm_vm->pgt_mm_ops = (struct pkvm_mm_ops) {
+		.phys_to_virt = pkvm_phys_to_virt,
+		.virt_to_phys = pkvm_virt_to_phys,
+		.zalloc_page = guest_mmu_zalloc_page,
+		.get_page = guest_mmu_get_page,
+		.put_page = guest_mmu_put_page,
+		.page_count = hyp_page_count,
+		.flush_tlb = NULL,
+		.flush_cache = NULL,
+	};
+	pkvm_vm->pgt_lock = __PKVM_SPINLOCK_UNLOCKED;
+
+	return pkvm_pgtable_init(&pkvm_vm->pgt, &pkvm_vm->pgt_mm_ops, &guest_mmu_ops,
+				 &guest_mmu_cap, true);
+}
+
+void pkvm_vm_mmu_destroy(struct pkvm_vm *pkvm_vm)
+{
+	pkvm_pgtable_destroy(&pkvm_vm->pgt, NULL);
+}
