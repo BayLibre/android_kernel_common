9fc04a296356c7bff4600f03c49e9ca0e6a3ea1f
Dmytro Maluka <dmaluka@google.com>
2025-06-23 16:07:54 +0000

    ANDROID: pkvm: vmx: Implement EPT support for PV MMU
    Implement a small EPT-specific layer for pre-initializing guest EPT page
    table ops, capabilities and various PTE bit masks, which shall be used
    by the generic VMX-agnostic PV MMU layer.
    
    The implementation of ept_ops is almost copied from the existing shadow
    EPT code. One difference is that the default PTE value is set to all
    zeroes, without the SUPPRESS_VE bit, since we are not using #VE anymore
    anyway, and it makes our life easier as we don't need to initialize all
    PTEs with SUPPRESS_VE when allocating pgtable pages.
    
    Also extend the pkvm_pgtable_cap struct to let this EPT layer provide
    the needed EPT PTE bit masks (permissions, memory type and access bit)
    to the generic PV MMU layer.
    
    Bug: 391541180
    Test: compile
    
    Change-Id: I624c422fb86d81de3cdd7787a9825e77e073bb38
    Signed-off-by: Dmytro Maluka <dmaluka@google.com>

---
+++ arch/x86/kvm/pkvm/vmx/ept.c
@@ -0,0 +1,115 @@
+// SPDX-License-Identifier: GPL-2.0
+#include <asm/kvm_pkvm.h>
+#include <mmu.h>
+#include <mmu/spte.h>
+
+#include <pkvm/mmu.h>
+#include "ept.h"
+//FIXME: clean up the header files
+#include <vmx/pkvm/hyp/pgtable.h>
+#include <pkvm.h>
+
+static bool ept_entry_present(void *ptep)
+{
+	u64 val = *(u64 *)ptep;
+
+	return !!(val & VMX_EPT_RWX_MASK);
+}
+
+static bool ept_entry_mapped(void *ptep)
+{
+	/*
+	 * Both present and non-zero non-present entries are counted as
+	 * mapped because a non-present entry with a non-zero value may
+	 * contain page state and ownership information created through
+	 * a map operation. So simply count non-zero entry as mapped to
+	 * cover both cases.
+	 */
+	return !!(*(u64 *)ptep);
+}
+
+static bool ept_entry_huge(void *ptep)
+{
+	return is_large_pte(*(u64 *)ptep);
+}
+
+static void ept_entry_mkhuge(void *ptep)
+{
+	*(u64 *)ptep |= PT_PAGE_SIZE_MASK;
+}
+
+static unsigned long ept_entry_to_phys(void *ptep)
+{
+	return *(u64 *)ptep & SPTE_BASE_ADDR_MASK;
+}
+
+static u64 ept_entry_to_prot(void *ptep)
+{
+	u64 prot = *(u64 *)ptep & ~(SPTE_BASE_ADDR_MASK);
+
+	return prot & ~PT_PAGE_SIZE_MASK;
+}
+
+static int ept_entry_to_index(unsigned long vaddr, int level)
+{
+	return SPTE_INDEX(vaddr, level);
+}
+
+static bool ept_entry_is_leaf(void *ptep, int level)
+{
+	return level == PG_LEVEL_4K ||
+	       !ept_entry_present(ptep) ||
+	       ept_entry_huge(ptep);
+}
+
+static int ept_level_entry_size(int level)
+{
+	return PAGE_SIZE / SPTE_ENT_PER_PAGE;
+}
+
+static int ept_level_to_entries(int level)
+{
+	return SPTE_ENT_PER_PAGE;
+}
+
+static u64 ept_level_page_mask(int level)
+{
+	return ~((1UL << SPTE_LEVEL_SHIFT(level)) - 1);
+}
+
+static unsigned long ept_level_to_size(int level)
+{
+	return KVM_HPAGE_SIZE(level);
+}
+
+static void ept_set_entry(void *sptep, u64 spte)
+{
+	WRITE_ONCE(*(u64 *)sptep, spte);
+}
+
+static struct pkvm_pgtable_ops ept_ops = {
+	.pgt_entry_present = ept_entry_present,
+	.pgt_entry_mapped = ept_entry_mapped,
+	.pgt_entry_huge = ept_entry_huge,
+	.pgt_entry_mkhuge = ept_entry_mkhuge,
+	.pgt_entry_to_phys = ept_entry_to_phys,
+	.pgt_entry_to_prot = ept_entry_to_prot,
+	.pgt_entry_to_index = ept_entry_to_index,
+	.pgt_level_page_mask = ept_level_page_mask,
+	.pgt_entry_is_leaf = ept_entry_is_leaf,
+	.pgt_level_entry_size = ept_level_entry_size,
+	.pgt_level_to_entries = ept_level_to_entries,
+	.pgt_level_to_size = ept_level_to_size,
+	.pgt_set_entry = ept_set_entry,
+};
+
+void ept_set_mmu_caps(void)
+{
+	guest_pgt_cap = pkvm_hyp->ept_cap;
+	guest_pgt_cap.prot_mask = VMX_EPT_RWX_MASK;
+	guest_pgt_cap.prot_rwx = guest_pgt_cap.prot_mask;
+	guest_pgt_cap.prot_rx = guest_pgt_cap.prot_rwx & ~VMX_EPT_WRITABLE_MASK;
+	guest_pgt_cap.mt_memory = MTRR_TYPE_WRBACK << VMX_EPT_MT_EPTE_SHIFT;
+
+	guest_pgt_ops = &ept_ops;
+}
---
+++ arch/x86/kvm/pkvm/vmx/ept.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __PKVM_X86_EPT_H
+#define __PKVM_X86_EPT_H
+
+void ept_set_mmu_caps(void);
+
+#endif /* __PKVM_X86_EPT_H */
