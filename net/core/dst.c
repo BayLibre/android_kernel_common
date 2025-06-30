c5189e186c7baa3631ebd394f5bc22781b47c489
Eric Dumazet <edumazet@google.com>
2025-06-30 12:19:30 +0000

    UPSTREAM: net: dst: add four helpers to annotate data-races around dst->dev
    dst->dev is read locklessly in many contexts,
    and written in dst_dev_put().
    
    Fixing all the races is going to need many changes.
    
    We probably will have to add full RCU protection.
    
    Add three helpers to ease this painful process.
    
    static inline struct net_device *dst_dev(const struct dst_entry *dst)
    {
           return READ_ONCE(dst->dev);
    }
    
    static inline struct net_device *skb_dst_dev(const struct sk_buff *skb)
    {
           return dst_dev(skb_dst(skb));
    }
    
    static inline struct net *skb_dst_dev_net(const struct sk_buff *skb)
    {
           return dev_net(skb_dst_dev(skb));
    }
    
    static inline struct net *skb_dst_dev_net_rcu(const struct sk_buff *skb)
    {
           return dev_net_rcu(skb_dst_dev(skb));
    }
    
    Fixes: 4a6ce2b6f2ec ("net: introduce a new function dst_dev_put()")
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Reviewed-by: Kuniyuki Iwashima <kuniyu@google.com>
    Link: https://patch.msgid.link/20250630121934.3399505-7-edumazet@google.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Bug: 445592499
    (cherry picked from commit 88fe14253e181878c2ddb51a298ae8c468a63010)
    Signed-off-by: zhoumin <zhoumin@oppo.corp-partner.google.com>
    Change-Id: I2a857663bf151e618e30865ed2e687cd05171fd5
    (cherry picked from commit 41bb88adcf1e38b3fbe0f76e93e84f22a14943e4)

--- net/core/dst.c
+++ net/core/dst.c
@@ -150,9 +150,19 @@ void dst_dev_put(struct dst_entry *dst)
 	dst->obsolete = DST_OBSOLETE_DEAD;
 	if (dst->ops->ifdown)
 		dst->ops->ifdown(dst, dev);
+<<<<<<< HEAD   (e07b65c574a94aaeca2eb4529e74240dd32d222b UPSTREAM: tls: make sure to abort the stream if headers are )
 	dst->input = dst_discard;
 	dst->output = dst_discard_out;
 	dst->dev = blackhole_netdev;
+||||||| BASE   (c88d5c40babac0f1b146bd7a9a71ca76c0bbc20e UPSTREAM: arm64: insn: Simulate nop instruction for better u)
+	WRITE_ONCE(dst->input, dst_discard);
+	WRITE_ONCE(dst->output, dst_discard_out);
+	dst->dev = blackhole_netdev;
+=======
+	WRITE_ONCE(dst->input, dst_discard);
+	WRITE_ONCE(dst->output, dst_discard_out);
+	WRITE_ONCE(dst->dev, blackhole_netdev);
+>>>>>>> CHANGE (41bb88adcf1e38b3fbe0f76e93e84f22a14943e4 UPSTREAM: net: dst: add four helpers to annotate data-races )
 	netdev_ref_replace(dev, blackhole_netdev, &dst->dev_tracker,
 			   GFP_ATOMIC);
 }
@@ -265,7 +275,7 @@ unsigned int dst_blackhole_mtu(const struct dst_entry *dst)
 {
 	unsigned int mtu = dst_metric_raw(dst, RTAX_MTU);
 
-	return mtu ? : dst->dev->mtu;
+	return mtu ? : dst_dev(dst)->mtu;
 }
 EXPORT_SYMBOL_GPL(dst_blackhole_mtu);
 
