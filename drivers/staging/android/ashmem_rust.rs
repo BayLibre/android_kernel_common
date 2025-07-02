e123a88f04a30dc19993d87cc23f71f9e2c6ce19
Isaac J. Manjarres <isaacmanjarres@google.com>
2025-07-02 22:46:26 +0000

    ANDROID: ashmem_rust: Add support for querying the size of an ashmem region
    Given a pointer to a file structure that represents an ashmem region,
    return the size of the region. This should only be used for dumping
    information about ashmem regions in debug scenarios.
    
    Bug: 193397560
    Change-Id: I24e6a2f7a469d667dc37ec8714e591b911043ee4
    Signed-off-by: Isaac J. Manjarres <isaacmanjarres@google.com>

--- drivers/staging/android/ashmem_rust.rs
+++ drivers/staging/android/ashmem_rust.rs
@@ -708,3 +708,20 @@ unsafe extern "C" fn ashmem_area_name(
         Err(err) => err.to_errno() as c_int,
     }
 }
+
+/// # Safety
+///
+/// The caller must ensure that `file` is valid for the duration of this function.
+#[no_mangle]
+unsafe extern "C" fn ashmem_area_size(file: *mut bindings::file) -> isize {
+    // SAFETY: file is valid for the duration of this function.
+    let ashmem = unsafe { get_ashmem_area(file) };
+    if ashmem.is_err() {
+        return 0;
+    }
+
+    match ashmem.unwrap().get_size() {
+        Ok(size) => size,
+        Err(_) => 0,
+    }
+}
