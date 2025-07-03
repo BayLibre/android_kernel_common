ec0f712ed5ba78e40b71e8a5e98738db11484272
Grzegorz Jaszczyk <jaszczyk@google.com>
2025-07-03 07:43:28 +0000

    ANDROID: pkvm: x86: hyp: prepare infrastructure to use memcache
    As a preparation for using memcache instead of preallocated memory pool
    for page allocation introduce relevant changes in pkvm hypervisor:
    - introduce new memcache filed in pagtable map structure and set it in
      __pkvm_pgtable_map, so it can be later used during page allocation
    - pass memcache pointer to zalloc_page
    
    No functional change so far as NULL is passed to pkvm_pgtable_map.
    
    Bug: 391541180
    Test: Run various VMs on Brya and Qemu
    
    Change-Id: Ib48c6929acf94c912833ebb859648d42334f6467
    Signed-off-by: Grzegorz Jaszczyk <jaszczyk@google.com>

--- arch/x86/kvm/pkvm/mmu.c
+++ arch/x86/kvm/pkvm/mmu.c
@@ -158,7 +158,7 @@ int pkvm_vm_mmu_map(int vm_handle, u64 gpa, u64 hpa, u64 size)
 		return -EINVAL;
 
 	pkvm_spin_lock(&pkvm_vm->pgt_lock);
-	ret = pkvm_pgtable_map(&pkvm_vm->pgt, gpa, hpa, size, 0, prot, guest_pgt_map_leaf);
+	ret = pkvm_pgtable_map(&pkvm_vm->pgt, gpa, hpa, size, 0, prot, guest_pgt_map_leaf, NULL);
 	pkvm_spin_unlock(&pkvm_vm->pgt_lock);
 
 	put_pkvm_vm(pkvm_vm);
