8f31964129a965df28a646f6bb3b11edcf27ed22
Dmytro Maluka <dmaluka@google.com>
2025-07-04 17:15:38 +0000

    ANDROID: pkvm: x86: Implement PV MMU unmap hypercall
    With PV MMU, this hypercall shall be used by the host KVM's MMU
    notifiers to ask pKVM to unmap guest pages and return them back to the
    host, for swapping, page migration, ballooning etc. This hypercall is
    allowed for non-protected VMs only, for security reasons.
    
    Bug: 391541180
    Test: compile
    
    Change-Id: I8af835acb7613dd9455b2aa8d008d2871ecde97a
    Signed-off-by: Dmytro Maluka <dmaluka@google.com>

--- arch/x86/kvm/pkvm/mmu.h
+++ arch/x86/kvm/pkvm/mmu.h
@@ -13,5 +13,6 @@ extern struct pkvm_pgtable_ops *guest_pgt_ops;
 int pkvm_vm_mmu_init(struct pkvm_vm *pkvm_vm);
 void pkvm_vm_mmu_destroy(struct pkvm_vm *pkvm_vm);
 int pkvm_vm_mmu_map(int handle, u64 gpa, u64 hpa, u64 size, bool writable);
+int pkvm_vm_mmu_unmap(int handle, u64 gpa, u64 size);
 
 #endif /* __PKVM_X86_MMU_H */
