4c49642230f457b518d79ebf2c9903fcdcdfa8a2
Aashish Sharma <shraash@google.com>
2025-07-05 17:02:41 -0700

    ANDROID: pkvm: x86: Verify page ownership before mapping
    Before host IOVA addresses are mapped in pKVM to physical pages,
    ensure that the host actually has ownership of those physical pages.
    
    Use host_check_page_state_range() to verify that the intended physical
    address range is currently in the PKVM_PAGE_OWNED state. As part of
    this change, make host_check_page_state_range() public by removing the
    static qualifier so it can be called from the mapping path.
    
    Bug: 391542119
    Upstream-Task: 402758258
    Test: Build and Boot
    
    Change-Id: Ice3a81ead3ce4a7ac77f2020a759a91da8270364
    Signed-off-by: Aashish Sharma <shraash@google.com>

--- arch/x86/kvm/vmx/pkvm/hyp/iommu_domain.c
+++ arch/x86/kvm/vmx/pkvm/hyp/iommu_domain.c
@@ -7,6 +7,7 @@
 #include <pkvm.h>
 #include "pkvm_hyp.h"
 #include "debug.h"
+#include "ept.h"
 #include "iommu_internal.h"
 #include "iommu.h"
 #include "iommu_domain.h"
@@ -468,6 +469,12 @@ static int domain_map(struct pkvm_iommu_domain *domain, struct pkvm_iommu_map_pa
 	if ((prot & (DMA_PTE_READ|DMA_PTE_WRITE)) == 0)
 		return -EINVAL;
 
+	host_ept_lock();
+	if (host_check_page_state_range(NULL, (phys_addr_t)phys_pfn << VTD_PAGE_SHIFT,
+				nr_pages * VTD_PAGE_SIZE, PKVM_PAGE_OWNED))
+		return -EPERM;
+	host_ept_unlock();
+
 	attr = prot & (DMA_PTE_READ | DMA_PTE_WRITE | DMA_PTE_SNP);
 	attr |= DMA_FL_PTE_PRESENT;
 
@@ -509,6 +516,7 @@ static int domain_map(struct pkvm_iommu_domain *domain, struct pkvm_iommu_map_pa
 			}
 
 		}
+
 		/* We don't need lock here, nobody else touches the iova range. */
 		tmp = 0ULL;
 		if (!try_cmpxchg64_local(&pte->val, &tmp, pteval) && (tmp != pteval)) {
