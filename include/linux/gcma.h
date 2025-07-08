d63ed0e31b97d7f9edde1a8704cd0b7392dc0b08
Sooyong Suk <s.suk@samsung.corp-partner.google.com>
2025-07-08 17:40:01 +0900

    ANDROID: mm: merge mm/gcma_sysfs.h into include/linux/gcma.h
    For furthur commits, merge mm/gcma_sysfs.h into include/linux/gcma.h.
    
    Bug: 410432482
    Change-Id: I1e4fd209c8a3037fba69ab2ed190f4c308477835
    Signed-off-by: Sooyong Suk <s.suk@samsung.corp-partner.google.com>

--- include/linux/gcma.h
+++ include/linux/gcma.h
@@ -5,6 +5,26 @@
 #include <linux/types.h>
 
 #ifdef CONFIG_GCMA
+enum gcma_stat_type {
+	STORED_PAGE,
+	LOADED_PAGE,
+	EVICTED_PAGE,
+	CACHED_PAGE,
+	DISCARDED_PAGE,
+	NUM_OF_GCMA_STAT,
+};
+
+#ifdef CONFIG_GCMA_SYSFS
+void gcma_stat_inc(enum gcma_stat_type type);
+void gcma_stat_dec(enum gcma_stat_type type);
+void gcma_stat_add(enum gcma_stat_type type, unsigned long delta);
+#else /* CONFIG_GCMA_SYSFS */
+static inline void gcma_stat_inc(enum gcma_stat_type type) {}
+static inline void gcma_stat_dec(enum gcma_stat_type type) {}
+static inline void gcma_stat_add(enum gcma_stat_type type,
+				 unsigned long delta) {}
+#endif /* CONFIG_GCMA_SYSFS */
+
 extern void gcma_alloc_range(unsigned long start_pfn, unsigned long end_pfn);
 extern void gcma_free_range(unsigned long start_pfn, unsigned long end_pfn);
 extern int register_gcma_area(const char *name, phys_addr_t base,
