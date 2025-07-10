5af378e65a26ddcc3e1033537ddd3e7247ba6ab0
Mostafa Saleh <smostafa@google.com>
2025-07-10 11:48:17 +0000

    ANDROID: iommu: Fix error path for deferred map_sg
    When the core IOMMU code fails to add_deferred_map_sg an entry,
    it will try to unmap the whole added entries, which might not be
    mapped at this point.
    Make the core code just return the error, and it’s the driver's
    responsibility to fail this cleanly.
    For SMMUv3, an add operation can only fail from a map because the
    list is full, so we need to track how many pages are in the list,
    and check if the map call have mapped less, which signals a failure,
    then we would unmap the whole mapped entries till this point and
    return an error.
    
    Bug: 428939924
    Change-Id: I91529fd9f043d37dde5d1ed2450dbba59d7857d1
    Signed-off-by: Mostafa Saleh <smostafa@google.com>

--- drivers/iommu/iommu.c
+++ drivers/iommu/iommu.c
@@ -2703,11 +2703,15 @@ ssize_t iommu_map_sg(struct iommu_domain *domain, unsigned long iova,
 		phys_addr_t s_phys = sg_phys(sg);
 
 		if (len && s_phys != start + len) {
-			if (deferred_sg)
+			if (deferred_sg) {
 				ret = __iommu_add_sg(cookie_sg, iova + mapped, start, len);
-			else
+				/* Don't unmap on add failure as it shouldn't map */
+				if (ret)
+					return ret;
+			} else {
 				ret = iommu_map_nosync(domain, iova + mapped, start,
 						  len, prot, gfp);
+			}
 			if (ret)
 				goto out_err;
 
