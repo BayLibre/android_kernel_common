7c1ce6dde5dd19c6222556f3a12d0de3aa0cd70d
Tudor Ambarus <tudordana@google.com>
2025-07-10 11:28:00 +0000

    Revert "PM: sleep: Make suspend of devices more asynchronous"
    This reverts commit 443046d1ad66607f324c604b9fbdf11266fa8aad.
    
    Bug: 425699034
    Change-Id: I352b0c7d3d0cedff8bcce237432eccc5c85e8cf9
    Signed-off-by: Tudor Ambarus <tudordana@google.com>

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -1310,8 +1310,6 @@ static void dpm_superior_set_must_resume(struct device *dev)
 	device_links_read_unlock(idx);
 }
 
-static void async_suspend_noirq(void *data, async_cookie_t cookie);
-
 /**
  * device_suspend_noirq - Execute a "noirq suspend" callback for given device.
  * @dev: Device to handle.
@@ -1392,13 +1390,7 @@ Skip:
 Complete:
 	complete_all(&dev->power.completion);
 	TRACE_SUSPEND(error);
-
-	if (error || async_error)
-		return error;
-
-	dpm_async_suspend_parent(dev, async_suspend_noirq);
-
-	return 0;
+	return error;
 }
 
 static void async_suspend_noirq(void *data, async_cookie_t cookie)
@@ -1412,7 +1404,6 @@ static void async_suspend_noirq(void *data, async_cookie_t cookie)
 static int dpm_noirq_suspend_devices(pm_message_t state)
 {
 	ktime_t starttime = ktime_get();
-	struct device *dev;
 	int error = 0;
 
 	trace_suspend_resume(TPS("dpm_suspend_noirq"), state.event, true);
@@ -1422,21 +1413,12 @@ static int dpm_noirq_suspend_devices(pm_message_t state)
 
 	mutex_lock(&dpm_list_mtx);
 
-	/*
-	 * Start processing "async" leaf devices upfront so they don't need to
-	 * wait for the "sync" devices they don't depend on.
-	 */
-	list_for_each_entry_reverse(dev, &dpm_late_early_list, power.entry) {
-		dpm_clear_async_state(dev);
-		if (dpm_leaf_device(dev))
-			dpm_async_with_cleanup(dev, async_suspend_noirq);
-	}
-
 	while (!list_empty(&dpm_late_early_list)) {
-		dev = to_device(dpm_late_early_list.prev);
+		struct device *dev = to_device(dpm_late_early_list.prev);
 
 		list_move(&dev->power.entry, &dpm_noirq_list);
 
+		dpm_clear_async_state(dev);
 		if (dpm_async_fn(dev, async_suspend_noirq))
 			continue;
 
@@ -1450,14 +1432,8 @@ static int dpm_noirq_suspend_devices(pm_message_t state)
 
 		mutex_lock(&dpm_list_mtx);
 
-		if (error || async_error) {
-			/*
-			 * Move all devices to the target list to resume them
-			 * properly.
-			 */
-			list_splice(&dpm_late_early_list, &dpm_noirq_list);
+		if (error || async_error)
 			break;
-		}
 	}
 
 	mutex_unlock(&dpm_list_mtx);
@@ -1509,8 +1485,6 @@ static void dpm_propagate_wakeup_to_parent(struct device *dev)
 
 	spin_unlock_irq(&parent->power.lock);
 }
-
-static void async_suspend_late(void *data, async_cookie_t cookie);
 
 /**
  * device_suspend_late - Execute a "late suspend" callback for given device.
@@ -1590,13 +1564,7 @@ Skip:
 Complete:
 	TRACE_SUSPEND(error);
 	complete_all(&dev->power.completion);
-
-	if (error || async_error)
-		return error;
-
-	dpm_async_suspend_parent(dev, async_suspend_late);
-
-	return 0;
+	return error;
 }
 
 static void async_suspend_late(void *data, async_cookie_t cookie)
@@ -1614,7 +1582,6 @@ static void async_suspend_late(void *data, async_cookie_t cookie)
 int dpm_suspend_late(pm_message_t state)
 {
 	ktime_t starttime = ktime_get();
-	struct device *dev;
 	int error = 0;
 
 	trace_suspend_resume(TPS("dpm_suspend_late"), state.event, true);
@@ -1626,21 +1593,12 @@ int dpm_suspend_late(pm_message_t state)
 
 	mutex_lock(&dpm_list_mtx);
 
-	/*
-	 * Start processing "async" leaf devices upfront so they don't need to
-	 * wait for the "sync" devices they don't depend on.
-	 */
-	list_for_each_entry_reverse(dev, &dpm_suspended_list, power.entry) {
-		dpm_clear_async_state(dev);
-		if (dpm_leaf_device(dev))
-			dpm_async_with_cleanup(dev, async_suspend_late);
-	}
-
 	while (!list_empty(&dpm_suspended_list)) {
-		dev = to_device(dpm_suspended_list.prev);
+		struct device *dev = to_device(dpm_suspended_list.prev);
 
 		list_move(&dev->power.entry, &dpm_late_early_list);
 
+		dpm_clear_async_state(dev);
 		if (dpm_async_fn(dev, async_suspend_late))
 			continue;
 
@@ -1654,14 +1612,8 @@ int dpm_suspend_late(pm_message_t state)
 
 		mutex_lock(&dpm_list_mtx);
 
-		if (error || async_error) {
-			/*
-			 * Move all devices to the target list to resume them
-			 * properly.
-			 */
-			list_splice(&dpm_suspended_list, &dpm_late_early_list);
+		if (error || async_error)
 			break;
-		}
 	}
 
 	mutex_unlock(&dpm_list_mtx);
