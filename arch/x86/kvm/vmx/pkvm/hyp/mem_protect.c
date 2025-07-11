c676c32b095f1191681ad60569044298deb03ee9
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-07-11 15:59:24 +0800

    ANDROID: pkvm: x86: Add host memory pages sharing/unsharing with pKVM
    Add two new page state APIs to share/unshare host memory pages with the
    pKVM hypervisor.
    
    With sharing a host memory page with the pKVM hypervisor, the host page
    state is transferred from OWNED to SHARED_OWNED. After unsharing, the page
    state is transferred from SHARED_OWNED to OWNED.
    
    These two APIs will be exposed to the host via new PV interfaces so that
    the host can lead this sharing/unsharing by the subsequent patch when it
    is necessary.
    
    Upstream-Task: 402758258
    
    Fixes: ed3834af0771 ("ANDROID: kvm: pkvm: Use PV interface for vm/vcpu life cycle management")
    Change-Id: Ia321be7bdd5b23befbf00290436683a5d5d9ecc7
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/vmx/pkvm/hyp/mem_protect.c
+++ arch/x86/kvm/vmx/pkvm/hyp/mem_protect.c
@@ -526,6 +526,15 @@ static int guest_request_share(const struct pkvm_mem_transition *tx)
 					      addr, size, PKVM_PAGE_OWNED);
 }
 
+static int hyp_ack_share(const struct pkvm_mem_transition *tx)
+{
+	/*
+	 * Only allow host to share normal memory pages with the pKVM
+	 * hypervisor, not MMIO pages.
+	 */
+	return is_mem_range(tx->completer.hyp.addr, tx->size) ? 0 : -EPERM;
+}
+
 static int host_ack_share(const struct pkvm_mem_transition *tx)
 {
 	u64 addr = tx->completer.host.addr;
@@ -563,6 +572,9 @@ static int check_share(const struct pkvm_mem_transition *tx)
 		return ret;
 
 	switch (tx->completer.id) {
+	case PKVM_ID_HYP:
+		ret = hyp_ack_share(tx);
+		break;
 	case PKVM_ID_HOST:
 		ret = host_ack_share(tx);
 		break;
@@ -636,6 +648,9 @@ static int __do_share(const struct pkvm_mem_transition *tx)
 		return ret;
 
 	switch (tx->completer.id) {
+	case PKVM_ID_HYP:
+		ret = 0;
+		break;
 	case PKVM_ID_HOST:
 		ret = host_complete_share(tx);
 		break;
@@ -779,6 +794,15 @@ static int guest_request_unshare(const struct pkvm_mem_transition *tx)
 					      addr, size, PKVM_PAGE_SHARED_OWNED);
 }
 
+static int hyp_ack_unshare(const struct pkvm_mem_transition *tx)
+{
+	/*
+	 * Only allow host to unshare normal memory pages with the pKVM
+	 * hypervisor, not MMIO pages.
+	 */
+	return is_mem_range(tx->completer.hyp.addr, tx->size) ? 0 : -EPERM;
+}
+
 static int host_ack_unshare(const struct pkvm_mem_transition *tx)
 {
 	u64 addr = tx->completer.host.addr;
@@ -816,6 +840,9 @@ static int check_unshare(const struct pkvm_mem_transition *tx)
 		return ret;
 
 	switch (tx->completer.id) {
+	case PKVM_ID_HYP:
+		ret = hyp_ack_unshare(tx);
+		break;
 	case PKVM_ID_HOST:
 		ret = host_ack_unshare(tx);
 		break;
@@ -887,6 +914,9 @@ static int __do_unshare(struct pkvm_mem_transition *tx)
 		return ret;
 
 	switch (tx->completer.id) {
+	case PKVM_ID_HYP:
+		ret = 0;
+		break;
 	case PKVM_ID_HOST:
 		ret = host_complete_unshare(tx);
 		break;
@@ -1002,6 +1032,68 @@ int __pkvm_guest_unshare_host(struct pkvm_pgtable *guest_pgt,
 
 	host_ept_unlock();
 	guest_mmu_unlock(guest_pgt);
+
+	return ret;
+}
+
+int __pkvm_host_share_hyp(u64 phys, u64 size)
+{
+	u64 start = PAGE_ALIGN_DOWN(phys);
+	u64 end = PAGE_ALIGN(phys + size);
+	struct pkvm_mem_transition share = {
+		.size = end - start,
+		.initiator	= {
+			.id	= PKVM_ID_HOST,
+			.host = {
+				.addr	= start,
+			},
+			.prot	= HOST_EPT_DEF_MEM_PROT,
+		},
+		.completer	= {
+			.id	= PKVM_ID_HYP,
+			.hyp	= {
+				.addr = start,
+			},
+		},
+	};
+	int ret;
+
+	host_ept_lock();
+
+	ret = do_share(&share);
+
+	host_ept_unlock();
+
+	return ret;
+}
+
+int __pkvm_host_unshare_hyp(u64 phys, u64 size)
+{
+	u64 start = PAGE_ALIGN_DOWN(phys);
+	u64 end = PAGE_ALIGN(phys + size);
+	struct pkvm_mem_transition unshare = {
+		.size = end - start,
+		.initiator	= {
+			.id	= PKVM_ID_HOST,
+			.host = {
+				.addr	= start,
+			},
+			.prot	= HOST_EPT_DEF_MEM_PROT,
+		},
+		.completer	= {
+			.id	= PKVM_ID_HYP,
+			.hyp	= {
+				.addr = start,
+			},
+		},
+	};
+	int ret;
+
+	host_ept_lock();
+
+	ret = do_unshare(&unshare);
+
+	host_ept_unlock();
 
 	return ret;
 }
