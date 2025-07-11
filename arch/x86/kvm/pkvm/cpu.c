3861d1d213abf877c9c1674f179bceeada67ad5c
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-07-11 09:17:21 +0800

    ANDROID: pkvm: x86: Add percpu fpu
    Add a percpu fpu to facilitate the FPU isolation. The percpu fpu is
    implemented by leveraging the pcpu_hot->current_task->thread.fpu and
    initialized with the initial fpstate, which is used to restore the FPU
    to the initial state to isolation the FPU from the host for the pVM. The
    permission is set to the maximum features to allow the guest to use all
    the supported features.
    
    Bug: 420676925
    Upstream-Task: 402758258
    
    Change-Id: I4f5922dad058272de34e0fa206f0649e71742334
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/pkvm/cpu.c
+++ arch/x86/kvm/pkvm/cpu.c
@@ -11,6 +11,7 @@ unsigned long __per_cpu_offset[NR_CPUS];
 DEFINE_PER_CPU_READ_MOSTLY(unsigned long, this_cpu_off);
 DEFINE_PER_CPU_ALIGNED(struct pcpu_hot, pcpu_hot);
 DEFINE_PER_CPU(u64, x86_spec_ctrl_current);
+DEFINE_PER_CPU(struct task_struct, cur_task);
 
 struct cpumask __cpu_possible_mask __ro_after_init;
 struct cpuinfo_x86 boot_cpu_data;
@@ -28,6 +29,8 @@ unsigned int pkvm_per_cpu_nr_pages(void)
 
 int setup_pkvm_per_cpu(int cpu, unsigned long base)
 {
+	struct task_struct *task;
+
 	if (cpu >= ARRAY_SIZE(__per_cpu_offset))
 		return -EINVAL;
 
@@ -39,6 +42,10 @@ int setup_pkvm_per_cpu(int cpu, unsigned long base)
 #endif
 	per_cpu(this_cpu_off, cpu) = __per_cpu_offset[cpu];
 	per_cpu(pcpu_hot.cpu_number, cpu) = cpu;
+
+	task = per_cpu_ptr(&cur_task, cpu);
+	task->group_leader = task;
+	per_cpu(pcpu_hot.current_task, cpu) = task;
 
 	return 0;
 }
