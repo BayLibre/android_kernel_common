f33e22b40b8503683aee9d836c319639ad483896
Dmytro Maluka <dmaluka@google.com>
2025-07-14 20:49:13 +0000

    ANDROID: pkvm: x86,vmx: Implement PV MMU age hypercall
    With PV MMU, this hypercall shall be used by the host KVM's MMU
    notifiers for both testing and clearing the 'accessed' bit in the guest
    PTEs, which is essential for letting the host kernel efficiently perform
    swapping and other memory reclaiming tasks.
    
    Like with the unmap hypercall, allow it for non-protected VMs only
    (similarly to pKVM-ARM). Even if it is ok security-wise to allow it for
    pVMs as well, it wouldn't make a lot of sense, since the host kernel
    cannot reclaim pVM pages on its own initiative anyway.
    
    The implementation relies on the EPT A/D bits capability, so for
    simplicity just require this capability for pKVM.
    
    The implementation of the pgtable part is inspired by the KVM arm64
    implementation [1][2].
    
    [1] https://lore.kernel.org/all/20200911132529.19844-11-will@kernel.org/
    [2] https://lore.kernel.org/all/20230627235405.4069823-1-oliver.upton@linux.dev/
    
    Bug: 391541180
    Test: compile, boot and run VMs
    
    Change-Id: I635930af6196b3cb6966f72ca7a5da0d4d3309d6
    Signed-off-by: Dmytro Maluka <dmaluka@google.com>

--- arch/x86/kvm/pkvm/mmu.h
+++ arch/x86/kvm/pkvm/mmu.h
@@ -14,5 +14,6 @@ int pkvm_vm_mmu_init(struct pkvm_vm *pkvm_vm);
 void pkvm_vm_mmu_destroy(struct pkvm_vm *pkvm_vm);
 int pkvm_vm_mmu_map(int handle, u64 gpa, u64 hpa, u64 size, bool writable);
 int pkvm_vm_mmu_unmap(int handle, u64 gpa, u64 size);
+int pkvm_vm_mmu_age(int handle, u64 gpa, u64 size, bool mkold);
 
 #endif /* __PKVM_X86_MMU_H */
