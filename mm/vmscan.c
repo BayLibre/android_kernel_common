f123c2f464859ae68b4b8d015e7cc566aee79a00
Pengfei Li <pengfei.kernel@vivo.corp-partner.google.com>
2025-07-14 16:05:00 +0800

    ANDROID: vendor_hook: Added hook for memory reclaim tuning
    The following hooks are added for memory reclaim tuning:
    
    android_vh_mm_customize_file_is_tiny - For a specific process, we want only
    anonymous pages to be reclaimed. This hook allows vendors to modify the value
    of file_is_tiny to affect the reclaim behavior.
    
    android_vh_mm_customize_pgdat_balanced - When there are multiple zones, it is
    not always reasonable to perform indirect reclaim only when all zones do not
    meet the watermark. This hook allows vendors to customize pgdat_balanced to use
    their own strategy to determine whether to perform and stop indirect collection.
    
    android_vh_mm_customize_reclaim_idx - When there are multiple zones, vendors use
    this hook to customize which zones can be reclaimed.
    
    android_vh_mm_customize_zone_can_compact - When there are multiple zones, vendors
    use this hook to customize which zones can be compacted.
    
    Bug: 431672372
    Bug: 505636392
    Change-Id: Ia4ce31ccc23da0e88187d1d550fcb20037e3025b
    Signed-off-by: Pengfei Li <pengfei.kernel@vivo.corp-partner.google.com>
    (cherry picked from commit 6c25b93a26385e5ab85750db953d2fedf5754e0e)

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -2425,6 +2425,15 @@ static bool inactive_is_low(struct lruvec *lruvec, enum lru_list inactive_lru)
 	return inactive * inactive_ratio < active;
 }
 
+static void customize_sc_file_is_tiny(struct scan_control *sc)
+{
+	bool file_is_tiny = sc->file_is_tiny;
+
+	trace_android_vh_mm_customize_file_is_tiny(sc->may_swap, sc->order,
+						   sc->reclaim_idx, &file_is_tiny);
+	sc->file_is_tiny = file_is_tiny;
+}
+
 enum scan_balance {
 	SCAN_EQUAL,
 	SCAN_FRACT,
@@ -2534,6 +2543,8 @@ static void prepare_scan_control(pg_data_t *pgdat, struct scan_control *sc)
 			!(sc->may_deactivate & DEACTIVATE_ANON) &&
 			anon >> sc->priority;
 	}
+
+	customize_sc_file_is_tiny(sc);
 }
 
 static inline void calculate_pressure_balance(struct scan_control *sc,
@@ -6425,6 +6436,9 @@ static void shrink_zones(struct zonelist *zonelist, struct scan_control *sc)
 		sc->reclaim_idx = gfp_zone(sc->gfp_mask);
 	}
 
+	trace_android_vh_mm_customize_reclaim_idx(sc->order, sc->gfp_mask,
+						  &sc->reclaim_idx, NULL);
+
 	for_each_zone_zonelist_nodemask(zone, z, zonelist,
 					sc->reclaim_idx, sc->nodemask) {
 		/*
@@ -6960,7 +6974,14 @@ static bool pgdat_balanced(pg_data_t *pgdat, int order, int highest_zoneidx)
 {
 	int i;
 	unsigned long mark = -1;
+	bool customized = false;
+	bool balanced = false;
 	struct zone *zone;
+
+	trace_android_vh_mm_customize_pgdat_balanced(order, highest_zoneidx,
+						     &balanced, &customized);
+	if (customized)
+		return balanced;
 
 	/*
 	 * Check watermarks bottom-up as lower zones are more likely to
@@ -7586,6 +7607,9 @@ void wakeup_kswapd(struct zone *zone, gfp_t gfp_flags, int order,
 
 	if (!cpuset_zone_allowed(zone, gfp_flags))
 		return;
+
+	trace_android_vh_mm_customize_reclaim_idx(order, gfp_flags, NULL,
+						  &highest_zoneidx);
 
 	pgdat = zone->zone_pgdat;
 	curr_idx = READ_ONCE(pgdat->kswapd_highest_zoneidx);
