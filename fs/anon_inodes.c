0434fda84fbead273cbe9ff3590f07a17caaeac1
Thiébaud Weksteen <tweek@google.com>
2025-07-25 11:17:23 +1000

    lsm: add type to security_inode_init_security_anon
    Introduce a new enum (lsm_anon_inode_id) used to identify the type of
    anonymous inode being created. This enum is passed down to the
    security_inode_init_security_anon LSM hook.
    
    Change-Id: Ie1e278df82a41eb600cef6dd3a73a45317bfcf76
    Signed-off-by: Thiébaud Weksteen <tweek@google.com>

--- fs/anon_inodes.c
+++ fs/anon_inodes.c
@@ -21,6 +21,7 @@
 #include <linux/magic.h>
 #include <linux/anon_inodes.h>
 #include <linux/pseudo_fs.h>
+#include <linux/security.h>
 
 #include <linux/uaccess.h>
 
@@ -121,8 +122,8 @@ struct inode *anon_inode_make_secure_inode(struct super_block *sb, const char *n
 		return inode;
 	inode->i_flags &= ~S_PRIVATE;
 	inode->i_op = &anon_inode_operations;
-	error =	security_inode_init_security_anon(inode, &QSTR(name),
-						  context_inode);
+	error = security_inode_init_security_anon(inode, LSM_ANON_INODE_GENERIC,
+						  &QSTR(name), context_inode);
 	if (error) {
 		iput(inode);
 		return ERR_PTR(error);
