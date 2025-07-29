7dac2f83200b93ca5ccc2cb4900f71fa4642d961
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-07-30 07:59:21 +0800

    ANDROID: pkvm: x86: Passthrough the PCI configuration space
    The pkvm monitors the PCI configuration space for the purpose of
    supporting passthrough device to the pVM, which can prevent the host to
    change the BAR in the passthrough device's pci configuration space.
    
    Now as doing so introduced thundbolt issues on PTL, and there is no
    requirement to passthrough device to the pVM. Passthrough the PCI
    configuration space to the host by removing the monitoring code which
    was implemented via MMIO and PIO. As well as the io_bitmap and io
    emulation support.
    
    Bug: 434032928
    Upstream-Task: 402758258
    
    Change-Id: I69d8961f49efa86a16e1c78bf0c659cf89b99b2f
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/vmx/pkvm/include/pkvm.h
+++ arch/x86/kvm/vmx/pkvm/include/pkvm.h
@@ -42,7 +42,6 @@ struct pkvm_host_vcpu {
 	struct vmcs *vmxarea;
 
 	bool pending_nmi;
-	u8 *io_bitmap;
 };
 
 struct pkvm_pci_info {
@@ -55,7 +54,6 @@ struct pkvm_host_vm {
 	struct pkvm_pgtable *ept;
 	struct pkvm_pgtable *ept_notlbflush;
 	struct pkvm_pci_info pci_info;
-	u8 *io_bitmap;
 };
 
 struct pkvm_iommu_info {
@@ -147,7 +145,6 @@ extern phys_addr_t pkvm_sym(pvmfw_size);
 PKVM_DECLARE(void, __pkvm_vmexit_entry, (void));
 PKVM_DECLARE(bool, pkvm_vmexit_main, (struct kvm_vcpu *vcpu));
 PKVM_DECLARE(void, pkvm_init_host_state_area, (struct pkvm_pcpu *pcpu, int cpu));
-PKVM_DECLARE(int, init_pci, (struct pkvm_hyp *pkvm));
 
 PKVM_DECLARE(void *, pkvm_early_alloc_contig, (unsigned int nr_pages));
 PKVM_DECLARE(void *, pkvm_early_alloc_page, (void));
