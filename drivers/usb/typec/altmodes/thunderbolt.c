e639314606b1339d632fd5165715bbeff179e4b2
Andrei Kuchynski <akuchynski@chromium.org>
2025-08-05 09:18:25 +0000

    test
    Change-Id: I533f9f6729f504972cb1b0cc16c84d218efd0f19
    Signed-off-by: Andrei Kuchynski <akuchynski@google.com>

--- drivers/usb/typec/altmodes/thunderbolt.c
+++ drivers/usb/typec/altmodes/thunderbolt.c
@@ -307,6 +307,7 @@ static int tbt_altmode_probe(struct typec_altmode *alt)
 	typec_altmode_set_drvdata(alt, tbt);
 	typec_altmode_set_ops(alt, &tbt_altmode_ops);
 
+#if 0
 	if (tbt_ready(alt)) {
 		if (tbt->plug[TYPEC_PLUG_SOP_P])
 			tbt->state = TBT_STATE_SOP_P_ENTER;
@@ -316,6 +317,7 @@ static int tbt_altmode_probe(struct typec_altmode *alt)
 			tbt->state = TBT_STATE_ENTER;
 		schedule_work(&tbt->work);
 	}
+#endif
 
 	return 0;
 }
