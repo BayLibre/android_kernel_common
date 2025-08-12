3923a505941825c0f774cbc989ff7ea20e5fd3f6
Zong Jiang <quic_zongjian@quicinc.com>
2025-08-12 13:48:19 +0800

    serial: qcom-geni: Make UART port count configurable via Kconfig
    Replace the hardcoded GENI_UART_PORTS macro with a new Kconfig option
    SERIAL_QCOM_GENI_UART_PORTS to allow platforms to configure the maximum
    number of UART ports supported by the driver at build time.
    
    This improves flexibility for platforms that require more than the
    previously fixed number of UART ports, and avoids unnecessary allocation
    for unused ports.
    
    BUG : 490975684
    Change-Id: I545fb8bbb50188f71ba81d50d34b68babaf88ae3
    Signed-off-by: Zong Jiang <quic_zongjian@quicinc.com>
    Link: https://lore.kernel.org/r/20250812054819.3748649-3-quic_zongjian@quicinc.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/qcom_geni_serial.c
+++ drivers/tty/serial/qcom_geni_serial.c
@@ -77,7 +77,6 @@
 #define STALE_TIMEOUT			16
 #define DEFAULT_BITS_PER_CHAR		10
 #define GENI_UART_CONS_PORTS		1
-#define GENI_UART_PORTS			3
 #define DEF_FIFO_DEPTH_WORDS		16
 #define DEF_TX_WM			2
 #define DEF_FIFO_WIDTH_BITS		32
@@ -252,7 +251,7 @@ static const char *qcom_geni_serial_get_type(struct uart_port *uport)
 static struct qcom_geni_serial_port *get_port_from_line(int line, bool console)
 {
 	struct qcom_geni_serial_port *port;
-	int nr_ports = console ? GENI_UART_CONS_PORTS : GENI_UART_PORTS;
+	int nr_ports = console ? GENI_UART_CONS_PORTS : CONFIG_SERIAL_QCOM_GENI_UART_PORTS;
 
 	if (line < 0 || line >= nr_ports)
 		return ERR_PTR(-ENXIO);
@@ -1570,7 +1569,7 @@ static struct uart_driver qcom_geni_uart_driver = {
 	.owner = THIS_MODULE,
 	.driver_name = "qcom_geni_uart",
 	.dev_name = "ttyHS",
-	.nr =  GENI_UART_PORTS,
+	.nr = CONFIG_SERIAL_QCOM_GENI_UART_PORTS,
 };
 
 static void qcom_geni_serial_pm(struct uart_port *uport,
