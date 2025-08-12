b8fefbbef20cd0fe5f0baa45b2362ae1b66bcaf2
Dmytro Maluka <dmaluka@google.com>
2025-08-12 16:09:10 +0000

    ANDROID: pkvm: vmx: Add vmx_get_mt_mask()
    Copy vmx_get_mt_mask() from KVM's vmx.c to the pKVM's copy of it, to
    allow the pKVM hypervisor's MMU code to reuse it for getting the desired
    values of memory type bits when creating guest page table entries.
    
    Bug: 391541180
    Test: compile
    Upstream-Task: 402758258
    
    Change-Id: Ie84e48cb02736dd572822824161f1a0ef1994492
    Signed-off-by: Dmytro Maluka <dmaluka@google.com>

--- arch/x86/kvm/pkvm/x86.c
+++ arch/x86/kvm/pkvm/x86.c
@@ -3253,6 +3253,12 @@ void kvm_set_rflags(struct kvm_vcpu *vcpu, unsigned long rflags)
 }
 EXPORT_SYMBOL_GPL(kvm_set_rflags);
 
+bool kvm_arch_has_noncoherent_dma(struct kvm *kvm)
+{
+	return atomic_read(&kvm->arch.noncoherent_dma_count);
+}
+EXPORT_SYMBOL_GPL(kvm_arch_has_noncoherent_dma);
+
 int kvm_spec_ctrl_test_value(u64 value)
 {
 	/*
