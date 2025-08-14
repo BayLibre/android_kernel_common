7f333e4da94fe1fc8d4bd184abc56362a9f5ab08
Marcus Ma <maminghui5@xiaomi.corp-partner.google.com>
2025-08-14 14:43:24 +0800

    ANDROID: vendor_hooks: Add hooks for pcp related optimization.
    When allocating memory in pcp, add the alloc_factor adaptive adjustment
    factor to dynamically adjust the batch and optimize memory allocation.
    
    Bug: 438620264
    
    Change-Id: Ib96ce74687abce26e5fda3e86621273c31c927b5
    Signed-off-by: Marcus Ma <maminghui5@xiaomi.corp-partner.google.com>

--- include/linux/mmzone.h
+++ include/linux/mmzone.h
@@ -714,6 +714,7 @@ struct per_cpu_pages {
 
 	/* Lists of pages, one per migrate type stored on the pcp-lists */
 	struct list_head lists[NR_PCP_LISTS];
+	ANDROID_VENDOR_DATA(1);
 } ____cacheline_aligned_in_smp;
 
 struct per_cpu_zonestat {
