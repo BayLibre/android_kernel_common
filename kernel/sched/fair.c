d4055b32709b89edbb3b1756d1d6b3c1126ccceb
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2025-08-15 02:16:06 -0700

    Revert "BACKPORT: sched/fair: Fix NO_RUN_TO_PARITY case"
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/92a52002-723e-4809-9196-02b3e94e8fd9). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: Id34d00e2eab9d4696e5deb5c6feb62926448bdcb
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -917,35 +917,23 @@ struct sched_entity *__pick_first_entity(struct cfs_rq *cfs_rq)
 }
 
 /*
- * Set the vruntime up to which an entity can run before looking
- * for another entity to pick.
- * In case of run to parity, we protect the entity up to its deadline.
- * When run to parity is disabled, we give a minimum quantum to the running
- * entity to ensure progress.
+ * HACK, stash a copy of deadline at the point of pick in vlag,
+ * which isn't used until dequeue.
  */
 static inline void set_protect_slice(struct sched_entity *se)
 {
-	u64 slice = se->slice;
-	u64 vprot = se->deadline;
-
-	if (!sched_feat(RUN_TO_PARITY))
-		slice = min(slice, normalized_sysctl_sched_base_slice);
-
-	if (slice != se->slice)
-		vprot = min_vruntime(vprot, se->vruntime + calc_delta_fair(slice, se));
-
-	se->vprot = vprot;
+	se->vlag = se->deadline;
 }
 
 static inline bool protect_slice(struct sched_entity *se)
 {
-	return ((s64)(se->vprot - se->vruntime) > 0);
+	return se->vlag == se->deadline;
 }
 
 static inline void cancel_protect_slice(struct sched_entity *se)
 {
 	if (protect_slice(se))
-		se->vprot = se->vruntime;
+		se->vlag = se->deadline + 1;
 }
 
 /*
@@ -984,7 +972,7 @@ static struct sched_entity *pick_eevdf(struct cfs_rq *cfs_rq)
 	if (curr && (!curr->on_rq || !entity_eligible(cfs_rq, curr)))
 		curr = NULL;
 
-	if (curr && protect_slice(curr))
+	if (sched_feat(RUN_TO_PARITY) && curr && protect_slice(curr))
 		return curr;
 
 	/* Pick the leftmost entity if it's eligible */
@@ -1240,8 +1228,11 @@ static s64 update_se(struct rq *rq, struct sched_entity *se)
 	return delta_exec;
 }
 
-static inline bool resched_next_slice(struct cfs_rq *cfs_rq, struct sched_entity *curr)
+static inline bool did_preempt_short(struct cfs_rq *cfs_rq, struct sched_entity *curr)
 {
+	if (!sched_feat(PREEMPT_SHORT))
+		return false;
+
 	if (protect_slice(curr))
 		return false;
 
@@ -1324,7 +1315,7 @@ static void update_curr(struct cfs_rq *cfs_rq)
 	if (cfs_rq->nr_running == 1)
 		return;
 
-	if (resched || resched_next_slice(cfs_rq, curr)) {
+	if (resched || did_preempt_short(cfs_rq, curr)) {
 		resched_curr(rq);
 		clear_buddies(cfs_rq, curr);
 	}
