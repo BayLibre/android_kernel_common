98e1dc59711330730e052ba6b553f0165158ca79
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2025-08-15 02:16:06 -0700

    Revert "UPSTREAM: sched/fair: Use protect_slice() instead of dir..."
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/92a52002-723e-4809-9196-02b3e94e8fd9). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: I8d8751341831f3c22d39bcf99014ebc0654be6d3
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -1233,7 +1233,7 @@ static inline bool did_preempt_short(struct cfs_rq *cfs_rq, struct sched_entity 
 	if (!sched_feat(PREEMPT_SHORT))
 		return false;
 
-	if (protect_slice(curr))
+	if (curr->vlag == curr->deadline)
 		return false;
 
 	return !entity_eligible(cfs_rq, curr);
