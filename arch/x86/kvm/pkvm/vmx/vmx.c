7771b67e8c9527853bf7a3951b913a9c88846e8a
Grzegorz Jaszczyk <jaszczyk@google.com>
2025-08-18 16:22:48 +0000

    ANDROID: pkvm: x86: introduce s2stage memcache and use it
    Extend kvm_vcpu_arch about memcache dedicated for stage-2 page table
    allocation and plumb the pKVM to properly propagate it to
    pkvm_pgtable_map where it will be used.
    
    Note: the memcache is still empty and old preallocated poll is used
    (will be changed in following commits).
    
    Bug: 391541180
    Test: Build, boot and run various VMs
    Upstream-Task: 402758258
    
    Change-Id: I437fe8ccb610fa5473f779dcebfaee48a39de34b
    Signed-off-by: Grzegorz Jaszczyk <jaszczyk@google.com>

--- arch/x86/kvm/pkvm/vmx/vmx.c
+++ arch/x86/kvm/pkvm/vmx/vmx.c
@@ -5121,7 +5121,8 @@ static int kvm_pkvm_hypercall(struct kvm_vcpu *vcpu)
 
 	switch (nr) {
 	case PKVM_GHC_SHARE_MEM:
-		ret = __pkvm_guest_share_host(&pkvm_vm->mmu, a0, a1);
+		ret = __pkvm_guest_share_host(&pkvm_vm->mmu, a0, a1,
+					      &vcpu->arch.stage2_mc);
 		break;
 	case PKVM_GHC_UNSHARE_MEM:
 		ret = __pkvm_guest_unshare_host(&pkvm_vm->mmu, a0, a1);
