20c714bf28e026ec4048fe0205b650183c8b8157
Richard Chang <richardycc@google.com>
2025-08-20 08:52:13 +0000

    ANDROID: zram: Add ioctl infrastructure
    Add the basic infrastructure for handling ioctl commands in the zram
    driver. This provides a channel for device-specific operations between
    userspace and the driver.
    
    This change is a preparatory step for introducing more advanced zram
    features. The new ioctl interface will be used to implement per-process
    page operations in a subsequent patch.
    
    Bug: 421065577
    Change-Id: I9d34b88fb4d98df0bf7e5b04d4820bb6ef0a0c24
    Signed-off-by: Richard Chang <richardycc@google.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -2588,10 +2588,17 @@ static int zram_open(struct gendisk *disk, blk_mode_t mode)
 	return 0;
 }
 
+static int zram_ioctl(struct block_device *bdev, blk_mode_t mode,
+		      unsigned int cmd, unsigned long arg)
+{
+	return -EINVAL;
+}
+
 static const struct block_device_operations zram_devops = {
 	.open = zram_open,
 	.submit_bio = zram_submit_bio,
 	.swap_slot_free_notify = zram_slot_free_notify,
+	.ioctl = zram_ioctl,
 	.owner = THIS_MODULE
 };
 
