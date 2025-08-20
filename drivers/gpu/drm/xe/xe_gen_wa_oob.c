9849857cb071fbb658e4c976f44aaf7b0ea6c1b6
Carlos Llamas <cmllamas@google.com>
2025-08-20 20:16:11 +0000

    FROMLIST: drm/xe: replace basename() with portable strrchr()
    Commit b0a2ee5567ab ("drm/xe: prepare xe_gen_wa_oob to be multi-use")
    introduced a call to basename(). The GNU version of this function is not
    portable and fails to build with alternative libc implementations like
    musl or bionic. This causes the following build error:
    
      drivers/gpu/drm/xe/xe_gen_wa_oob.c:130:12: error: assignment to ‘const char *’ from ‘int’ makes pointer from integer without a cast [-Wint-conversion]
        130 |         fn = basename(fn);
            |            ^
    
    While a POSIX version of basename() could be used, it would require a
    separate header plus the behavior differs from GNU version in that it
    might modify its argument. Not great.
    
    Instead replace basename() with a strrchr() based implementation which
    provides the same functionality and avoid portability issues.
    
    Fixes: b0a2ee5567ab ("drm/xe: prepare xe_gen_wa_oob to be multi-use")
    Signed-off-by: Carlos Llamas <cmllamas@google.com>
    
    Link: https://lore.kernel.org/all/20250820201612.2549797-1-cmllamas@google.com/
    Signed-off-by: Carlos Llamas <cmllamas@google.com>
    (cherry picked from https://android-review.googlesource.com/q/commit:c71ecb422115b1a9571b3e36cf14eb06c8de99c5)
    Merged-In: Icb43fd8af1fe25dd8828d3c6d20ec96d8768932f
    Change-Id: Icb43fd8af1fe25dd8828d3c6d20ec96d8768932f

--- drivers/gpu/drm/xe/xe_gen_wa_oob.c
+++ drivers/gpu/drm/xe/xe_gen_wa_oob.c
@@ -118,6 +118,10 @@ static int parse(FILE *input, FILE *csource, FILE *cheader, char *prefix)
 	if (idx)
 		fprintf(csource, ") },\n");
 
+<<<<<<< PATCH SET (cad44959b8109050e50f78ac8907f144e9d4ad9e FROMLIST: drm/xe: replace basename() with portable strrchr())
+<<<<<<< HEAD   (35eeb4d42d50d732250a138559350b81b075f738 ANDROID: sched: Export set_task_cpu inside of ifdef am: 37a6)
+	fprintf(cheader, "\t_XE_WA_OOB_COUNT = %u\n", idx);
+||||||| BASE   (40226d7997b8d45a9d587c8ce70f68d50d4c21c7 Merge 260f6f4fda93 ("Merge tag 'drm-next-2025-07-30' of http)
 	fprintf(cheader, "\t_%s_COUNT = %u\n", prefix, idx);
 
 	return 0;
@@ -145,6 +149,68 @@ static int fn_to_prefix(const char *fn, char *prefix, size_t size)
 			break;
 		}
 	}
+=======
+	fprintf(cheader, "\t_%s_COUNT = %u\n", prefix, idx);
+
+	return 0;
+}
+
+static int fn_to_prefix(const char *fn, char *prefix, size_t size)
+{
+	const char *base;
+	size_t len;
+
+	base = strrchr(fn, '/');
+	fn = base ? base + 1 : fn;
+	len = strlen(fn);
+
+	if (len > size - 1)
+		return -ENAMETOOLONG;
+
+	memcpy(prefix, fn, len + 1);
+
+	for (char *p = prefix; *p; p++) {
+		switch (*p) {
+		case '.':
+			*p = '\0';
+			return 0;
+		default:
+			*p = toupper(*p);
+			break;
+		}
+	}
+>>>>>>> CHANGE (c71ecb422115b1a9571b3e36cf14eb06c8de99c5 FROMLIST: drm/xe: replace basename() with portable strrchr())
+||||||| BASE      (35eeb4d42d50d732250a138559350b81b075f738 ANDROID: sched: Export set_task_cpu inside of ifdef am: 37a6)
+	fprintf(cheader, "\t_XE_WA_OOB_COUNT = %u\n", idx);
+=======
+	fprintf(cheader, "\t_%s_COUNT = %u\n", prefix, idx);
+
+	return 0;
+}
+
+static int fn_to_prefix(const char *fn, char *prefix, size_t size)
+{
+	size_t len;
+
+	fn = basename(fn);
+	len = strlen(fn);
+
+	if (len > size - 1)
+		return -ENAMETOOLONG;
+
+	memcpy(prefix, fn, len + 1);
+
+	for (char *p = prefix; *p; p++) {
+		switch (*p) {
+		case '.':
+			*p = '\0';
+			return 0;
+		default:
+			*p = toupper(*p);
+			break;
+		}
+	}
+>>>>>>> BASE      (e7342b72fc916ab6801a63ea26820c5d5b9185ed [conflict] Merge 260f6f4fda93 ("Merge tag 'drm-next-2025-07-)
 
 	return 0;
 }
