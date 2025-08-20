29b9cfbd35c36060a4f985baf647a80059a3d596
Richard Chang <richardycc@google.com>
2025-08-20 09:37:27 +0000

    ANDROID: zram: Add ioctl for per-process writeback operation
    Introduce a new ioctl command `ZRAM_ANDROID_IOC_PROCESS_WRITEBACK`.
    Operate on a target process identified by a pidfd, and then iterates
    through the virtual memory areas (VMAs) of the target process,
    identifies pages that are swapped to the zram device, and then invokes
    the corresponding writeback operation.
    
    Use CONFIG_ZRAM_ANDROID_IOCTL to enable this feature.
    
    Bug: 421065577
    Change-Id: I84cedb0c18a8088114582e7f54637e119fad1564
    Signed-off-by: Richard Chang <richardycc@google.com>

---
+++ drivers/block/zram/zram_ioctl.h
@@ -0,0 +1,20 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#ifndef _ZRAM_IOCTL_H_
+#define _ZRAM_IOCTL_H_
+
+#include <uapi/linux/zram_ioctl.h>
+
+#if IS_ENABLED(CONFIG_ZRAM_ANDROID_IOCTL)
+int zram_ioctl_process_writeback_scan(struct zram *zram,
+	struct zram_android_ioc_data *ioc_data, struct zram_pp_ctl *ctl);
+#else
+inline int zram_ioctl_process_writeback_scan(struct zram *zram,
+	struct zram_android_ioc_data *ioc_data, struct zram_pp_ctl *ctl)
+{
+	return -EINVAL;
+}
+#endif
+
+#endif /* _ZRAM_IOCTL_H_ */
+
