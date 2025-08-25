2e1dbb424f6a2b0972abbf83949e92091abde401
Nikhil V <nikhil.v@oss.qualcomm.com>
2025-08-25 12:09:51 +0530

    ANDROID: gunyah: Vendor hooks for reclaiming PVMFW memory
    With the migration of QTVM alongside PVMFW to the latest Qualcomm SoCs,
    there's a potential memory overhead of 4MB allocated for PVMFW.
    Currently, this memory is shared with QTVM as a separate parcel. Once
    QTVM has successfully booted, the associated memory parcel can be
    released and reclaimed by Android through the Resource Manager RPC
    interface.
    
    To facilitate this, a vendor-specific hook has been added to the ACK
    Gunyah driver, enabling reclamation of PVMFW memory after QTVM startup.
    
    Bug: 431672484
    Change-Id: Ic3c739e3324233c4bcf063a9cc8a8262f43d1f89
    Signed-off-by: Nikhil V <nikhil.v@oss.qualcomm.com>

--- drivers/virt/gunyah/vm_mgr.c
+++ drivers/virt/gunyah/vm_mgr.c
@@ -915,6 +915,10 @@ static int gunyah_vm_start(struct gunyah_vm *ghvm)
 	if (ret)
 		goto err;
 
+	if (ghvm->fw.config.size > 0)
+		trace_android_rvh_gh_note_fw_parcel(ghvm, ghvm->vmid,
+						    ghvm->fw.parcel.parcel.mem_handle);
+
 	ret = gunyah_rm_vm_start(ghvm->rm, ghvm->vmid);
 	if (ret) {
 		dev_warn(ghvm->parent, "Failed to start VM: %d\n", ret);
