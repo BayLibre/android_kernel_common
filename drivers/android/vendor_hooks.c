275466f199ca2fc2a13f0a21bda111c887d4b177
hoai.tt5 <hoai.tt5@samsung.com>
2025-08-27 03:37:54 -0400

    ANDROID: vendor_hooks: Add hooks for dmv
    By these vhs, we can obtain the informations when dmv corrupt happend
    such as 4k want_digest, runtime_digest, 4k block is compared, etc.
    - android_vh_handle_add_fec_mismatch_blks
    
    - android_vh_handle_error
    
    - android_vh_handle_add_skipped_blks
    
    - android_vh_handle_add_blks_map
    
    - android_vh_handle_get_b_info
    
    Bug: 439737442
    
    Change-Id: Ibb33e6bbca93ae37b0a992edbae126778702953a
    Signed-off-by: hoai.tt5 <hoai.tt5@samsung.corp-partner.google.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -77,6 +77,7 @@
 #include <trace/hooks/user.h>
 #include <trace/hooks/gzvm.h>
 #include <trace/hooks/usb.h>
+#include <trace/hooks/dmv_debug.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -616,3 +617,8 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mm_remove_migration_pte_bypass);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mm_split_huge_page_bypass);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mm_try_split_folio_bypass);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_scmi_limit_notify_cb);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_handle_add_fec_mismatch_blks);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_handle_error);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_handle_add_skipped_blks);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_handle_add_blks_map);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_handle_get_b_info);
