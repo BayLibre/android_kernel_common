e3c24e90592dc20d7322bcb320e3c0cc05c68639
Kalesh Singh <kaleshsingh@google.com>
2025-09-02 16:28:41 -0700

    ANDROID: mm: Centralize VMA map count limit checking
    The check against the VMA limit (sysctl_max_map_count) was
    open-coded in several places. This made the code repetitive and
    prevented centralized handling, such as logging when the limit is
    hit. The sysctl variable was also unnecessarily global.
    
    Introduce a new helpeer function, would_exceed_max_nr_vmas(), to
    consolidate this logic. All open-coded checks are replaced with
    calls to this new function. To improve encapsulation,
    sysctl_max_map_count is now static to mm/mmap.c. A rate-limited
    warning is now issued from this function when the VMA limit is
    exceeded.
    
    This results in cleaner, more maintainable code. Debugging
    applications that exhaust their VMA limit is now easier thanks to
    the new warning. Encapsulation is also improved by restricting the
    scope of the sysctl variable.
    
    Bug: 440427562
    Signed-off-by: Kalesh Singh <kaleshsingh@google.com>
    Change-Id: I1badb19a2ef4cb40af9bea971298bb53b09225d2

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -192,7 +192,17 @@ static inline void __mm_zero_struct_page(struct page *page)
 #define MAPCOUNT_ELF_CORE_MARGIN	(5)
 #define DEFAULT_MAX_MAP_COUNT	(USHRT_MAX - MAPCOUNT_ELF_CORE_MARGIN)
 
-extern int sysctl_max_map_count;
+/**
+ * would_exceed_max_nr_vmas - check if a VMA operation would exceed max_map_count
+ * @mm: The mm_struct of the target process.
+ * @new_vmas: The number of new VMAs the operation will create.
+ *
+ * Returns true if the operation would cause the number of VMAs to exceed
+ * the sysctl_max_map_count limit, false otherwise. A rate-limited warning
+ * is logged if the limit is exceeded.
+ */
+extern bool would_exceed_max_nr_vmas(struct mm_struct *mm,
+					    unsigned int new_vmas);
 
 extern unsigned long sysctl_user_reserve_kbytes;
 extern unsigned long sysctl_admin_reserve_kbytes;
