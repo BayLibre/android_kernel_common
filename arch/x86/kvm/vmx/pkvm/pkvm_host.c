fefb5ba8e5bfb4f558a6ca43a731966ce845c84c
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-09-04 13:59:12 +0800

    ANDROID: pkvm: x86: Add kernel command line to control cpu_bug_relax
    Add a kernel command line parameter to control cpu_bug_relax, which
    determines whether the pKVM hypervisor should run when it cannot
    mitigate all CPU vulnerabilities. By default, cpu_bug_relax is true to
    allow pKVM to run even when some CPU vulnerabilities remain unmitigated
    to enable pKVM usage for development purposes. For production, this
    parameter should be set to false.
    
    Bug: 420681877
    Upstream-Task: 402758258
    
    Suggested-by: Kevin Tian <kevin.tian@intel.com>
    Change-Id: I80c5f895be11f87ed3e6b11ce509678668eee471
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/vmx/pkvm/pkvm_host.c
+++ arch/x86/kvm/vmx/pkvm/pkvm_host.c
@@ -38,6 +38,12 @@ static int __init early_pkvm_parse_cmdline(char *buf)
 }
 early_param("kvm-intel.pkvm", early_pkvm_parse_cmdline);
 
+static int __init early_pkvm_relax_cpu_bugs_parse_cmdline(char *buf)
+{
+	return kstrtobool(buf, &relax_cpu_bugs);
+}
+early_param("kvm-intel.pkvm_relax_cpu_bugs", early_pkvm_relax_cpu_bugs_parse_cmdline);
+
 static int __init early_pvmfw_parse_cmdline(char *buf)
 {
 	u64 start, size;
@@ -1706,6 +1712,8 @@ static int __init __vmx_pkvm_init(void)
 	if (pkvm_has_unmitigated_cpu_bugs()) {
 		if (relax_cpu_bugs) {
 			pr_warn("pkvm: Allow pkvm to run with unmitigated CPU bugs\n");
+			pr_warn("pkvm: To prevent pkvm running on such CPU,");
+			pr_cont("reboot with kvm-intel.pkvm_relax_cpu_bugs=false\n");
 		} else {
 			pr_err("pkvm: Prevent pkvm to run due to unmitigated CPU bugs\n");
 			ret = -EOPNOTSUPP;
