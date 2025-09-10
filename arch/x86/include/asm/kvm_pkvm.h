c7ec47177158bdc7e2f56e23dc9f5ca6c8b2d929
Vineeth Pillai <vineethrp@google.com>
2025-09-09 22:07:17 -0400

    ANDROID: pkvm: x86: Hypercalls for legacy mode context tables
    Implement hypercalls for updating legacy context tables.
    
    Bug: 391542119
    Upstream-Task: 402758258
    Test: Build and Boot
    
    Change-Id: I5c09d1f4e521ad9919bc2cce469f6d80984615a2
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

--- arch/x86/include/asm/kvm_pkvm.h
+++ arch/x86/include/asm/kvm_pkvm.h
@@ -241,6 +241,10 @@ union pkvm_pv_param {
 	struct msr_data msr;
 	struct desc_ptr desc;
 	u64 eoi_exit_bitmap[4];
+
+	// IOMMU hypercall params
+	struct pkvm_lm_context_param lm_context_param;
+	struct pkvm_clear_translation_param clear_translation_param;
 } __aligned(PAGE_SIZE);
 
 #ifdef __PKVM_HYP__
--- arch/x86/kvm/vmx/pkvm/hyp/vmexit.c
+++ arch/x86/kvm/vmx/pkvm/hyp/vmexit.c
@@ -119,6 +119,12 @@ static unsigned long handle_vmcall(struct kvm_vcpu *vcpu)
 	case __pkvm__iommu_disable:
 		ret = pkvm_iommu_disable(a0);
 		break;
+	case __pkvm__iommu_clear_ce:
+		ret = pkvm_iommu_clear_ce(a0);
+		break;
+	case __pkvm__iommu_set_lm_ce:
+		ret = pkvm_iommu_set_lm_ce(a0);
+		break;
 #endif
 	case __pkvm__add_ptdev:
 		ret = pkvm_add_ptdev(a0, a1, a2);
