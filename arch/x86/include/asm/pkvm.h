59ffe52667bfa5b4c0b1418125125ba379906eb9
Vineeth Pillai <vineethrp@google.com>
2025-09-09 22:07:17 -0400

    ANDROID: pkvm: x86: hypercalls for legacy mode context tables
    Implement hypercalls for updating legacy context tables.
    
    Bug: 391542119
    Upstream-Task: 402758258
    Test: Build and Boot
    
    Change-Id: I5c09d1f4e521ad9919bc2cce469f6d80984615a2
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

--- arch/x86/include/asm/pkvm.h
+++ arch/x86/include/asm/pkvm.h
@@ -17,6 +17,8 @@
 
 #define PKVM_HC_ENABLE_IOMMU		11
 #define PKVM_HC_DISABLE_IOMMU		12
+#define PKVM_HC_IOMMU_CLEAR_CE		13
+#define PKVM_HC_IOMMU_SET_LM_CE		14
 
 /*
  * Internal hypercall to commit the pkvm initialization
@@ -46,6 +48,39 @@ struct pkvm_iommu_driver {
 };
 
 #ifdef CONFIG_PKVM_INTEL
+
+/*
+ * Generic hypercall parameter for clearing legacy
+ * and scalable mode context entries and pasid table
+ * entries.
+ */
+struct pkvm_clear_translation_param {
+	/*
+	 * Input: bdf to indentify entry
+	 *        to be cleared.
+	 */
+	u16 bdf;
+	/*
+	 * Input: pasid to indentify pasid table
+	 *        entry to be cleared.
+	 *        Ignored for context teardown.
+	 */
+	u32 pasid;
+	/*
+	 * Output: did of the cleared entry
+	 *         Ignored for scalable mode
+	 *         context entry.
+	 */
+	u16 did;
+};
+
+struct pkvm_lm_context_param {
+	u16 bdf;
+	u16 did;
+	u16 domain_agaw;
+	u64 domain_pgd_gpa;
+	u64 context_gpa;
+};
 
 #ifndef __PKVM_HYP__
 
@@ -130,6 +165,30 @@ static inline long pkvm_hc_disable_iommu(unsigned long reg_phys)
 
 	if (pkvm_pviommu_enabled())
 		ret = kvm_hypercall1(PKVM_HC_DISABLE_IOMMU, reg_phys);
+
+	return ret;
+}
+
+static inline long pkvm_hc_iommu_clear_ce(unsigned long reg_phys,
+		struct pkvm_clear_translation_param *param)
+{
+	long ret = 0;
+
+	if (pkvm_pviommu_enabled())
+		ret = kvm_hypercall2(PKVM_HC_IOMMU_CLEAR_CE,
+				reg_phys, virt_to_phys(param));
+
+	return ret;
+}
+
+static inline long pkvm_hc_iommu_set_lm_ce(unsigned long reg_phys,
+		struct pkvm_lm_context_param *param)
+{
+	long ret = 0;
+
+	if (pkvm_pviommu_enabled())
+		ret = kvm_hypercall2(PKVM_HC_IOMMU_SET_LM_CE, reg_phys,
+				virt_to_phys(param));
 
 	return ret;
 }
