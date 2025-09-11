31eba1bb16bde20b2f5d3fb4be26eb9f5a961400
Vineeth Pillai <vineethrp@google.com>
2025-09-11 11:28:16 -0400

    ANDROID: pkvm: x86: refactor ptdev implementation
    struct pkvm_ptdev is intended to be representing a device that is passed
    through to a virtual machine and houses details like bdf, pasid, iommu
    pagetable etc. Multiple pkvm_ptdev can represent the underlying physical
    device if scalable mode support is enabled with multiple pasids.
    
    It would be beneficial to have a more generic representation of physical
    device to store the device specific details and iommu specific details
    of the device. Currently there is no such representation in pkvm.
    
    Introduce a new structure pkvm_device representing generic device and
    iommu information specific to the device. pkvm_device also houses
    pkvm_ptdev instances for the passthrough representation of this device.
    
    Bug: 391542119
    Upstream-Task: 402758258
    Test: Build and Boot
    
    Change-Id: Ia1be50389e55e2973f3de0e8c3eec53793cbdf73
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

--- arch/x86/kvm/vmx/pkvm/hyp/ptdev.c
+++ arch/x86/kvm/vmx/pkvm/hyp/ptdev.c
@@ -13,75 +13,190 @@
 #include "memory.h"
 #include <pkvm/vmx/vmx.h>
 
+static DECLARE_BITMAP(devs_bitmap, PKVM_MAX_PDEV_NUM);
+static DEFINE_HASHTABLE(dev_hasht, 8);
+static struct pkvm_device pkvm_dev[PKVM_MAX_PDEV_NUM];
+static pkvm_spinlock_t dev_lock = __PKVM_SPINLOCK_UNLOCKED;
+
 #define MAX_PTDEV_NUM	(PKVM_MAX_PDEV_NUM + PKVM_MAX_PASID_PDEV_NUM)
-static DEFINE_HASHTABLE(ptdev_hasht, 8);
 static DECLARE_BITMAP(ptdevs_bitmap, MAX_PTDEV_NUM);
 static struct pkvm_ptdev pkvm_ptdev[MAX_PTDEV_NUM];
-static pkvm_spinlock_t ptdev_lock = __PKVM_SPINLOCK_UNLOCKED;
 
-struct pkvm_ptdev *pkvm_alloc_ptdev(u16 bdf, u32 pasid, bool coherency)
+static struct pkvm_device *__pkvm_alloc_device_locked(u16 bdf, bool coherency)
+{
+	struct pkvm_device *dev = NULL;
+	unsigned long index;
+
+	index = find_next_zero_bit(devs_bitmap, PKVM_MAX_PDEV_NUM, 0);
+	if (index < PKVM_MAX_PDEV_NUM) {
+		__set_bit(index, devs_bitmap);
+		dev = &pkvm_dev[index];
+		dev->bdf = bdf;
+		dev->iommu_coherency = coherency;
+		dev->index = index;
+		atomic_set(&dev->refcount, 1);
+		pkvm_spin_lock_init(&dev->lock);
+		hash_init(dev->ptdev_hash);
+		hash_add(dev_hasht, &dev->hnode, bdf);
+	}
+	return dev;
+}
+
+struct pkvm_device *pkvm_alloc_device(u16 bdf, bool coherency)
+{
+	struct pkvm_device *dev;
+
+	pkvm_spin_lock(&dev_lock);
+	dev = __pkvm_alloc_device_locked(bdf, coherency);
+	pkvm_spin_unlock(&dev_lock);
+
+	return dev;
+}
+
+static struct pkvm_device *__pkvm_get_device_locked(u16 bdf)
+{
+	struct pkvm_device *dev = NULL, *tmp;
+
+	hash_for_each_possible(dev_hasht, tmp, hnode, bdf) {
+		if (match_pkvm_device(tmp, bdf)) {
+			dev = atomic_inc_not_zero(&tmp->refcount) ? tmp : NULL;
+			if (dev)
+				break;
+		}
+	}
+
+	return dev;
+}
+
+struct pkvm_device *pkvm_get_device(u16 bdf)
+{
+	struct pkvm_device *dev;
+
+	pkvm_spin_lock(&dev_lock);
+	dev = __pkvm_get_device_locked(bdf);
+	pkvm_spin_unlock(&dev_lock);
+	return dev;
+}
+
+void pkvm_put_device(struct pkvm_device *dev)
+{
+	if (!atomic_dec_and_test(&dev->refcount))
+		return;
+
+	pkvm_spin_lock(&dev_lock);
+
+	hlist_del(&dev->hnode);
+
+	__clear_bit(dev->index, devs_bitmap);
+
+	memset(dev, 0, sizeof(struct pkvm_device));
+
+	pkvm_spin_unlock(&dev_lock);
+}
+
+static struct pkvm_ptdev *alloc_ptdev(struct pkvm_device *dev, u32 pasid)
 {
 	struct pkvm_ptdev *ptdev = NULL;
 	unsigned long index;
-
-	pkvm_spin_lock(&ptdev_lock);
 
 	index = find_next_zero_bit(ptdevs_bitmap, MAX_PTDEV_NUM, 0);
 	if (index < MAX_PTDEV_NUM) {
 		__set_bit(index, ptdevs_bitmap);
 		ptdev = &pkvm_ptdev[index];
-		ptdev->bdf = bdf;
+		ptdev->dev = dev;
 		ptdev->pasid = pasid;
-		ptdev->iommu_coherency = coherency;
 		ptdev->index = index;
 		ptdev->pgt = pkvm_hyp->host_vm.ept;
 		INIT_LIST_HEAD(&ptdev->iommu_node);
 		INIT_LIST_HEAD(&ptdev->vm_node);
 		atomic_set(&ptdev->refcount, 1);
 		pkvm_spin_lock_init(&ptdev->lock);
-		hash_add(ptdev_hasht, &ptdev->hnode, bdf);
+		hash_add(dev->ptdev_hash, &ptdev->hnode, pasid);
 	}
 
-	pkvm_spin_unlock(&ptdev_lock);
+	return ptdev;
+}
 
+struct pkvm_ptdev *pkvm_alloc_ptdev(u16 bdf, u32 pasid, bool coherency)
+{
+	struct pkvm_device *dev;
+	struct pkvm_ptdev *ptdev = NULL;
+
+	pkvm_spin_lock(&dev_lock);
+	dev = __pkvm_get_device_locked(bdf);
+	if (!dev) {
+		dev = __pkvm_alloc_device_locked(bdf, coherency);
+		if (!dev)
+			goto out;
+	}
+
+	hash_for_each_possible(dev->ptdev_hash, ptdev, hnode, pasid) {
+		if (match_pkvm_ptdev(ptdev, pasid)) {
+			pkvm_err("pkvm: %s: ptdev(%x:%x) already allocated!",
+					__func__, bdf, pasid);
+			ptdev = NULL;
+			goto out;
+		}
+	}
+
+	ptdev = alloc_ptdev(dev, pasid);
+	if (!ptdev) {
+		goto out;
+	}
+
+out:
+	pkvm_spin_unlock(&dev_lock);
+	if (!ptdev && dev)
+		pkvm_put_device(dev);
 	return ptdev;
 }
 
 struct pkvm_ptdev *pkvm_get_ptdev(u16 bdf, u32 pasid)
 {
 	struct pkvm_ptdev *ptdev = NULL, *tmp;
+	struct pkvm_device *dev;
 
-	pkvm_spin_lock(&ptdev_lock);
+	dev = pkvm_get_device(bdf);
+	if (!dev)
+		return NULL;
 
-	hash_for_each_possible(ptdev_hasht, tmp, hnode, bdf) {
-		if (match_ptdev(tmp, bdf, pasid)) {
+	pkvm_spin_lock(&dev->lock);
+
+	hash_for_each_possible(dev->ptdev_hash, tmp, hnode, pasid) {
+		if (match_pkvm_ptdev(tmp, pasid)) {
 			ptdev = atomic_inc_not_zero(&tmp->refcount) ? tmp : NULL;
 			if (ptdev)
 				break;
 		}
 	}
 
-	pkvm_spin_unlock(&ptdev_lock);
+	pkvm_spin_unlock(&dev->lock);
+	pkvm_put_device(dev);
+
 	return ptdev;
 }
 
 void pkvm_put_ptdev(struct pkvm_ptdev *ptdev)
 {
+	struct pkvm_device *dev;
+
 	if (!atomic_dec_and_test(&ptdev->refcount))
 		return;
 
-	pkvm_spin_lock(&ptdev_lock);
+	dev = ptdev->dev;
+	pkvm_spin_lock(&dev->lock);
 
 	hlist_del(&ptdev->hnode);
 
 	__clear_bit(ptdev->index, ptdevs_bitmap);
 
 	if (ptdev->pgt != pkvm_hyp->host_vm.ept)
-		pkvm_put_host_iommu_spgt(ptdev->pgt, ptdev->iommu_coherency);
+		pkvm_put_host_iommu_spgt(ptdev->pgt, dev->iommu_coherency);
 
 	memset(ptdev, 0, sizeof(struct pkvm_ptdev));
+	pkvm_spin_unlock(&dev->lock);
 
-	pkvm_spin_unlock(&ptdev_lock);
+	pkvm_put_device(dev);
 }
 
 void pkvm_setup_ptdev_vpgt(struct pkvm_ptdev *ptdev, unsigned long root_gpa,
@@ -89,12 +204,15 @@ void pkvm_setup_ptdev_vpgt(struct pkvm_ptdev *ptdev, unsigned long root_gpa,
 			   const struct pkvm_pgtable_ops *paging_ops,
 			   const struct pkvm_pgtable_cap *cap, bool shadowed)
 {
+	struct pkvm_device *dev;
+
 	pkvm_spin_lock(&ptdev->lock);
+	dev = ptdev->dev;
 
 	if (ptdev->pgt != pkvm_hyp->host_vm.ept &&
 			(!shadowed || root_gpa != ptdev->vpgt.root_pa) &&
 			!ptdev_attached_to_vm(ptdev)) {
-		pkvm_put_host_iommu_spgt(ptdev->pgt, ptdev->iommu_coherency);
+		pkvm_put_host_iommu_spgt(ptdev->pgt, dev->iommu_coherency);
 		ptdev->pgt = pkvm_hyp->host_vm.ept;
 	}
 
@@ -107,7 +225,7 @@ void pkvm_setup_ptdev_vpgt(struct pkvm_ptdev *ptdev, unsigned long root_gpa,
 	PKVM_ASSERT(pkvm_pgtable_init(&ptdev->vpgt, mm_ops, paging_ops, cap, false) == 0);
 
 	if (shadowed && ptdev->pgt == pkvm_hyp->host_vm.ept) {
-		ptdev->pgt = pkvm_get_host_iommu_spgt(root_gpa, ptdev->iommu_coherency);
+		ptdev->pgt = pkvm_get_host_iommu_spgt(root_gpa, dev->iommu_coherency);
 		PKVM_ASSERT(ptdev->pgt);
 	}
 out:
@@ -135,8 +253,8 @@ void pkvm_detach_ptdev(struct pkvm_ptdev *ptdev, struct pkvm_shadow_vm *vm)
 	pkvm_spin_unlock(&ptdev->lock);
 
 	pkvm_shadow_vm_unlink_ptdev(vm, &ptdev->vm_node,
-				    ptdev->iommu_coherency);
-	pkvm_iommu_sync(ptdev->bdf, ptdev->pasid);
+				    ptdev->dev->iommu_coherency);
+	pkvm_iommu_sync(ptdev->dev->bdf, ptdev->pasid);
 
 	pkvm_put_ptdev(ptdev);
 }
@@ -162,16 +280,17 @@ void pkvm_detach_ptdev(struct pkvm_ptdev *ptdev, struct pkvm_shadow_vm *vm)
 int pkvm_attach_ptdev(u16 bdf, u32 pasid, struct pkvm_shadow_vm *vm)
 {
 	struct pkvm_ptdev *ptdev = pkvm_get_ptdev(bdf, pasid);
+	struct pkvm_device *dev;
 	int vm_handle;
 
 	if (!ptdev) {
-		ptdev = pkvm_alloc_ptdev(bdf, pasid,
-					 pkvm_iommu_coherency(bdf, pasid));
+		ptdev = pkvm_alloc_ptdev(bdf, pasid, pkvm_iommu_coherency(bdf));
 		if (!ptdev)
 			return -ENODEV;
 	}
 
 	pkvm_spin_lock(&ptdev->lock);
+	dev = ptdev->dev;
 
 	vm_handle = shadow_to_kvm(vm)->arch.pkvm.pkvm_vm_handle;
 	if (cmpxchg(&ptdev->shadow_vm_handle, 0, vm_handle) != 0) {
@@ -184,7 +303,7 @@ int pkvm_attach_ptdev(u16 bdf, u32 pasid, struct pkvm_shadow_vm *vm)
 
 	PKVM_ASSERT(ptdev->pgt != &vm->pgstate_pgt);
 	if (ptdev->pgt != pkvm_hyp->host_vm.ept)
-		pkvm_put_host_iommu_spgt(ptdev->pgt, ptdev->iommu_coherency);
+		pkvm_put_host_iommu_spgt(ptdev->pgt, dev->iommu_coherency);
 
 	/*
 	 * Reset pgt of this ptdev to VM's pgstate_pgt so need to update
@@ -195,8 +314,8 @@ int pkvm_attach_ptdev(u16 bdf, u32 pasid, struct pkvm_shadow_vm *vm)
 	pkvm_spin_unlock(&ptdev->lock);
 
 	pkvm_shadow_vm_link_ptdev(vm, &ptdev->vm_node,
-				  ptdev->iommu_coherency);
-	if (pkvm_iommu_sync(ptdev->bdf, ptdev->pasid)) {
+				  dev->iommu_coherency);
+	if (pkvm_iommu_sync(dev->bdf, ptdev->pasid)) {
 		pkvm_detach_ptdev(ptdev, vm);
 		return -ENODEV;
 	}
--- arch/x86/kvm/vmx/pkvm/hyp/ptdev.h
+++ arch/x86/kvm/vmx/pkvm/hyp/ptdev.h
@@ -7,30 +7,48 @@
 #include "pkvm_hyp.h"
 #include "pgtable.h"
 
-struct pkvm_ptdev {
+struct pkvm_device {
 	atomic_t refcount;
 	struct hlist_node hnode;
-	u16 did;
 	u16 bdf;
-	u32 pasid;
 	unsigned long index;
-	struct list_head iommu_node;
 	bool iommu_coherency;
+
+	pkvm_spinlock_t lock;
+
+	DECLARE_HASHTABLE(ptdev_hash, 8);
+
+	u32 max_pasid;
+	void *pasid_table;
+};
+
+struct pkvm_ptdev {
+	atomic_t refcount;
+	unsigned long index;
+	pkvm_spinlock_t lock;
+	u32 pasid;
+	u16 did;
+	int shadow_vm_handle;
+	struct list_head vm_node;
+	struct list_head iommu_node;
 
 	/* Represents the page table maintained by primary VM */
 	struct pkvm_pgtable vpgt;
 	/* Represents the page table maintained by pKVM */
 	struct pkvm_pgtable *pgt;
 
-	pkvm_spinlock_t lock;
-
-	int shadow_vm_handle;
-	struct list_head vm_node;
+	struct hlist_node hnode;
+	struct pkvm_device *dev;
 };
+
+struct pkvm_device *pkvm_alloc_device(u16 bdf, bool coherency);
+struct pkvm_device *pkvm_get_device(u16 bdf);
+void pkvm_put_device(struct pkvm_device *dev);
 
 struct pkvm_ptdev *pkvm_alloc_ptdev(u16 bdf, u32 pasid, bool coherency);
 struct pkvm_ptdev *pkvm_get_ptdev(u16 bdf, u32 pasid);
 void pkvm_put_ptdev(struct pkvm_ptdev *ptdev);
+
 void pkvm_setup_ptdev_vpgt(struct pkvm_ptdev *ptdev, unsigned long root_gpa,
 			   const struct pkvm_mm_ops *mm_ops,
 			   const struct pkvm_pgtable_ops *paging_ops,
@@ -39,9 +57,14 @@ void pkvm_setup_ptdev_did(struct pkvm_ptdev *ptdev, u16 did);
 void pkvm_detach_ptdev(struct pkvm_ptdev *ptdev, struct pkvm_shadow_vm *vm);
 int pkvm_attach_ptdev(u16 bdf, u32 pasid, struct pkvm_shadow_vm *vm);
 
-static inline bool match_ptdev(struct pkvm_ptdev *ptdev, u16 bdf, u32 pasid)
+static inline bool match_pkvm_ptdev(struct pkvm_ptdev *ptdev, u32 pasid)
 {
-	return ptdev && (ptdev->bdf == bdf) && (ptdev->pasid == pasid);
+	return ptdev && (ptdev->pasid == pasid);
+}
+
+static inline bool match_pkvm_device(struct pkvm_device *dev, u16 bdf)
+{
+	return dev && (dev->bdf == bdf);
 }
 
 static inline bool ptdev_attached_to_vm(struct pkvm_ptdev *ptdev)
