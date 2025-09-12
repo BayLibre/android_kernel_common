01fc4f06bb21e6393d9598bc58816d4b8b4889fb
Aashish Sharma <shraash@google.com>
2025-09-12 14:21:25 -0400

    ANDROID: pkvm: x86: pasid table update hypercalls
    Hypercalls for updating pasid table entries.
    
    Bug: 391542119
    Upstream-Task: 402758258
    Test: Build and Boot
    
    Change-Id: I8756d74cb205cb1e1dabb8902681dc7fce3a5950
    Signed-off-By: Aashish Sharma <shraash@google.com>

--- arch/x86/include/asm/pkvm.h
+++ arch/x86/include/asm/pkvm.h
@@ -24,6 +24,10 @@
 #define PKVM_HC_IOMMU_SET_LM_CE		14
 #define PKVM_HC_IOMMU_SET_LM_PTCE	15
 #define PKVM_HC_IOMMU_SET_SM_CE		16
+#define PKVM_HC_IOMMU_CLEAR_PASID_ENTRY	17
+#define PKVM_HC_IOMMU_SET_PASID_FL	18
+#define PKVM_HC_IOMMU_SET_PASID_SL	19
+#define PKVM_HC_IOMMU_SET_PASID_FT	20
 
 /*
  * Internal hypercall to commit the pkvm initialization
@@ -102,6 +106,20 @@ struct pkvm_sm_context_param {
 	u32 max_pasid;
 	u64 pasid_dir_gpa;
 	u64 context_gpa;
+};
+
+struct pkvm_pasid_table_param {
+	u16 bdf;
+	u16 did;
+	u32 pasid;
+	u8 force_snooping:1;
+	u8 dirty_tracking:1;
+	u32 max_pasid;
+	u64 pasid_dir_gpa;
+	u64 pasid_table_gpa;
+	u16 domain_gaw;
+	u16 domain_agaw;
+	u64 domain_pgd_gpa;
 };
 
 #ifndef __PKVM_HYP__
@@ -228,6 +246,54 @@ static inline long pkvm_hc_iommu_set_sm_ce(unsigned long reg_phys,
 
 	if (pkvm_enabled())
 		ret = kvm_hypercall2(PKVM_HC_IOMMU_SET_SM_CE, reg_phys,
+				virt_to_phys(param));
+
+	return ret;
+}
+
+static inline long pkvm_hc_iommu_clear_pasid_entry(unsigned long reg_phys,
+		struct pkvm_block_translation_param *param)
+{
+	long ret = 0;
+
+	if (pkvm_enabled())
+		ret = kvm_hypercall2(PKVM_HC_IOMMU_CLEAR_PASID_ENTRY, reg_phys,
+				virt_to_phys(param));
+
+	return ret;
+}
+
+static inline long pkvm_hc_iommu_set_pasid_fl(unsigned long reg_phys,
+		struct pkvm_pasid_table_param *param)
+{
+	long ret = 0;
+
+	if (pkvm_enabled())
+		ret = kvm_hypercall2(PKVM_HC_IOMMU_SET_PASID_FL, reg_phys,
+				virt_to_phys(param));
+
+	return ret;
+}
+
+static inline long pkvm_hc_iommu_set_pasid_sl(unsigned long reg_phys,
+		struct pkvm_pasid_table_param *param)
+{
+	long ret = 0;
+
+	if (pkvm_enabled())
+		ret = kvm_hypercall2(PKVM_HC_IOMMU_SET_PASID_SL, reg_phys,
+				virt_to_phys(param));
+
+	return ret;
+}
+
+static inline long pkvm_hc_iommu_set_pasid_pt(unsigned long reg_phys,
+		struct pkvm_pasid_table_param *param)
+{
+	long ret = 0;
+
+	if (pkvm_enabled())
+		ret = kvm_hypercall2(PKVM_HC_IOMMU_SET_PASID_FT, reg_phys,
 				virt_to_phys(param));
 
 	return ret;
