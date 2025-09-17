51afcc034de0874420da6d4055c20eadf62977e8
Vineeth Pillai <vineethrp@google.com>
2025-09-17 15:29:37 -0400

    ANDROID: pkvm: x86: Read only sharing of pages with host
    pkvm should revoke host access to iommu translation tables for security
    purposes.  But host driver may need read access to iommu translation
    tables which is not a security threat.
    
    Implement APIs that pviommu can use to downgrade a page to read only
    when host iommu driver donates it to pkvm for constructing translation
    tables.
    
    Bug: 391542119
    Upstream-Task: 402758258
    Test: Build and Boot
    
    Change-Id: I218229d77f0140c4758b0ddd809273e61d7975c9
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

--- arch/x86/kvm/vmx/pkvm/hyp/mem_protect.c
+++ arch/x86/kvm/vmx/pkvm/hyp/mem_protect.c
@@ -12,6 +12,7 @@
 #include "ept.h"
 #include "iommu.h"
 #include "memory.h"
+#include "debug.h"
 #include <pkvm/vmx/vmx.h>
 
 DEFINE_PER_CPU(struct pkvm_vm *, __current_vm);
@@ -1236,4 +1237,59 @@ void __pkvm_unpin_shared_mem(u64 phys, u64 size)
 	host_ept_lock();
 	WARN_ON(pin_unpin_shared_mem_range(phys, size, false));
 	host_ept_unlock();
+}
+
+static int update_host_ept_prot(u64 addr, u64 size, u64 exp_prot, u64 new_prot)
+{
+	struct mem_range range;
+	unsigned long paddr;
+	int ret = 0;
+	u64 prot;
+
+	/*
+	 * Only allow updating protection bits of normal memory (tracked in the hyp vmemmap),
+	 * not reserved memory, MMIO etc.
+	 */
+	if (!find_mem_range(addr, &range))
+		return -EPERM;
+
+	/* Prevent updating protection bits for pages used by the host for DMA. */
+	if (hyp_page_count(__hyp_va(addr)))
+		return -EBUSY;
+
+	host_ept_lock();
+	pkvm_host_ept_lookup(addr, &paddr, &prot, NULL);
+	if (paddr == INVALID_ADDR) {
+		pkvm_err("pkvm: %s: requested memory(%llx) not mapped in ept!\n", __func__, addr);
+		ret = -EPERM;
+		goto out;
+	} else if (prot != new_prot && prot != exp_prot) {
+		pkvm_err("pkvm: %s: requested memory(%llx) prot expected(%llx), actual(%llx)!\n",
+				__func__, addr, exp_prot, prot);
+		ret = -EINVAL;
+		goto out;
+	}
+
+	ret = pkvm_host_ept_map(addr, addr, size, 0, new_prot);
+
+out:
+	host_ept_unlock();
+
+	return ret;
+}
+
+int pkvm_switch_host_ept_ro(u64 addr, u64 size)
+{
+	u64 new_prot = pkvm_mkstate(HOST_EPT_RO_MEM_PROT, PKVM_PAGE_OWNED);
+	u64 exp_prot = pkvm_mkstate(HOST_EPT_DEF_MEM_PROT, PKVM_PAGE_OWNED);
+
+	return update_host_ept_prot(addr, size, exp_prot, new_prot);
+}
+
+int pkvm_switch_host_ept_default(u64 addr, u64 size)
+{
+	u64 new_prot = pkvm_mkstate(HOST_EPT_DEF_MEM_PROT, PKVM_PAGE_OWNED);
+	u64 exp_prot = pkvm_mkstate(HOST_EPT_RO_MEM_PROT, PKVM_PAGE_OWNED);
+
+	return update_host_ept_prot(addr, size, exp_prot, new_prot);
 }
