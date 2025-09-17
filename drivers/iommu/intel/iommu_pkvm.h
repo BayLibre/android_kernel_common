30536226f93794a9c1d1ddd7e6920cf54cd76efe
Vineeth Pillai <vineethrp@google.com>
2025-09-17 12:40:03 -0400

    ANDROID: iommu/vt-d: Paravirtualize sm context PRE update
    Bug: 391542119
    Upstream-Task: 402758258
    Test: Build and Boot
    
    Change-Id: I736e795d224af6f0f5829abe687ded67774d627e
    Signed-off-By: Vineeth Pillai <vineethrp@google.com>

--- drivers/iommu/intel/iommu_pkvm.h
+++ drivers/iommu/intel/iommu_pkvm.h
@@ -48,6 +48,12 @@ static inline long pkvm_hc_iommu_set_sm_ce(struct pkvm_sm_context_param *param)
 	return pkvm_iommu_hypercall(iommu_set_sm_ce, sm_context_param, param);
 }
 
+static inline long pkvm_hc_iommu_set_sm_ce_pre(struct pkvm_sm_context_pre_param *param)
+{
+	return pkvm_iommu_hypercall(iommu_set_sm_ce_pre,
+			sm_context_pre_param, param);
+}
+
 static inline long pkvm_hc_iommu_clear_pasid_entry(struct pkvm_clear_translation_param *param)
 {
 	return pkvm_iommu_hypercall(iommu_clear_pasid_entry,
