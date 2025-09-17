15f131d473d63fb6d15371c28e4c98fd0bd8732d
Vineeth Pillai <vineethrp@google.com>
2025-09-17 12:37:37 -0400

    ANDROID: pkvm: x86: Hypercall for updating sm context PRE
    Bug: 391542119
    Upstream-Task: 402758258
    Test: Build and Boot
    
    Change-Id: Ief933cbf01f567091cd756b3306652f12c3d21fa
    Signed-off-By: Vineeth Pillai <vineethrp@google.com>

--- arch/x86/kvm/vmx/pkvm/hyp/pv_iommu.c
+++ arch/x86/kvm/vmx/pkvm/hyp/pv_iommu.c
@@ -489,3 +489,50 @@ out_unlock:
 
 	return ret;
 }
+
+unsigned long pkvm_iommu_set_sm_ce_pre(u64 param_va)
+{
+	struct pkvm_sm_context_pre_param *param;
+	struct context_entry *context;
+	struct pkvm_iommu *hyp_iommu;
+	struct intel_iommu *iommu;
+	u8 bus, devfn;
+	int ret = 0;
+
+	if (!param_va)
+		return -EINVAL;
+
+	param = (struct pkvm_sm_context_pre_param *)kern_pkvm_va((void *)param_va);
+	if (WARN_ON_ONCE(param != this_pv_param(sm_context_pre_param)))
+		return -EINVAL;
+
+	hyp_iommu = find_iommu_by_reg_phys(param->phys);
+	if (!hyp_iommu)
+		return -EINVAL;
+
+	bus = PCI_BUS_NUM(param->bdf);
+	devfn = PCI_DEV_FN(param->bdf);
+
+	pkvm_spin_lock(&hyp_iommu->lock);
+	iommu = &hyp_iommu->iommu;
+
+	context = pkvm_iommu_context_addr(iommu, bus, devfn, 0);
+	if (!context) {
+		ret = -ENODEV;
+		goto out_unlock;
+	}
+	param->did = context_domain_id(context);
+
+	if (param->val)
+		context_set_sm_pre(context);
+	else
+		context_clear_sm_pre(context);
+
+	if (!ecap_coherent(iommu->ecap))
+		iommu_flush_cache(context, sizeof(*context));
+
+out_unlock:
+	pkvm_spin_unlock(&hyp_iommu->lock);
+
+	return ret;
+}
