android/kernel_common

commit 5fd327389a0653f9d031282e0bc0f7ddabbbae67

ANDROID: kvm: pkvm: Add union pkvm_pv_param
The pkvm_pv_param can be used as the shared memory between the host and
the pkvm hypervisor for the PV interfaces. Although the PV interface can
use the register to pass data between the two sides, some PV interface
may need to pass more. Instead of extending the PV interface to use more
registers, defines a union pkvm_pv_param for this purpose so that there
will be no limitation by the register number. Currently it contains
kvm_segment structure and will be used for the get/set_segment PV
interface.

The union pkvm_pv_param is allocated as a percpu page by the host KVM
and shared with the pkvm hypervisor via the hardware_enable PV
interface. So each physical CPU has its own pkvm_pv_param page. Define
macros get/put_this_pv_param for the host and this_pv_param for the pkvm
hypervisor to use the percpu pkvm_pv_param page.

Bug: 391539939
Test: pKVM works
Upstream-Task: 402758258

Change-Id: If07188260d0a9b257ea47dac4293d7a6c9744003
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>
Signed-off-by: Dmytro Maluka <dmaluka@google.com>

arch/x86/kvm/pkvm/pkvm.c
@@ -30,6 +30,33 @@ static pkvm_spinlock_t pkvm_vms_lock = __PKVM_SPINLOCK_UNLOCKED;
 static struct pkvm_vm_ref pkvm_vms_ref[MAX_PKVM_VMS];
 struct pkvm_x86_ops pkvm_x86_ops __read_mostly;
 
+static DEFINE_PER_CPU(union pkvm_pv_param *, pv_param);
+
+#define this_pv_param(f)	(&this_cpu_read(pv_param)->f)
+
+static int pkvm_enable_virtualization_cpu(unsigned long pv_param_pa)
+{
+	int r = kvm_arch_enable_virtualization_cpu();
+
+	if (!r)
+		/*
+		 * TODO: Assume host is already share the pv_param structure
+		 * with pkvm. Pin the pv_param_pa so that it won't be re-used
+		 * as guest memory.
+		 */
+		this_cpu_write(pv_param, __pkvm_va(pv_param_pa));
+
+	return r;
+}
+
+static void pkvm_disable_virtualization_cpu(void)
+{
+	kvm_arch_disable_virtualization_cpu();
+
+	/* TODO: unpin the shared pv_param memory */
+	this_cpu_write(pv_param, NULL);
+}
+
 #define HANDLE_OFFSET 1
 
 static int idx_to_vm_handle(int idx)
@@ -568,10 +595,10 @@ unsigned long handle_kvm_call(unsigned long fn, unsigned long p1,
 
 	switch (fn) {
 	case __pkvm__enable_virtualization_cpu:
-		ret = kvm_arch_enable_virtualization_cpu();
+		ret = pkvm_enable_virtualization_cpu(p1);
 		break;
 	case __pkvm__disable_virtualization_cpu:
-		kvm_arch_disable_virtualization_cpu();
+		pkvm_disable_virtualization_cpu();
 		break;
 	case __pkvm__check_processor_compatibility:
 		ret = kvm_x86_call(check_processor_compatibility)();

arch/x86/kvm/vmx/pkvm_high.c
@@ -15,6 +15,16 @@
 #include <trace/events/ipi.h>
 #include "trace.h"
 
+static DEFINE_PER_CPU(union pkvm_pv_param, *pv_param);
+
+#define get_this_pv_param(f)		(&per_cpu(pv_param, get_cpu())->f)
+#define put_this_pv_param(ptr)		\
+({					\
+	memset(ptr, 0, sizeof(*ptr));	\
+	ptr = NULL;			\
+	put_cpu();			\
+})
+
 static void pkvm_mc_free_fn(void *addr, void *unused)
 {
 	free_page((unsigned long)addr);
@@ -117,19 +127,36 @@ static int pkvm_check_processor_compat(void)
 
 static int pkvm_enable_virtualization_cpu(void)
 {
-	int r = kvm_call_pkvm(enable_virtualization_cpu);
+	size_t size = sizeof(union pkvm_pv_param);
+	void *page;
+	int r;
 
-	if (r)
+	page = alloc_pages_exact(size, GFP_KERNEL_ACCOUNT | __GFP_ZERO);
+	if (!page)
+		return -ENOMEM;
+
+	/* TODO: share union pkvm_pv_param with pkvm */
+	r = kvm_call_pkvm(enable_virtualization_cpu, __pa(page));
+	if (r) {
+		free_pages_exact(page, size);
 		return r;
+	}
 
+	this_cpu_write(pv_param, page);
 	intel_pt_handle_vmx(1);
 	return 0;
 }
 
 static void pkvm_disable_virtualization_cpu(void)
 {
+	size_t size = sizeof(union pkvm_pv_param);
+	void *page = this_cpu_read(pv_param);
+
 	intel_pt_handle_vmx(0);
 	kvm_call_pkvm(disable_virtualization_cpu);
+	/* TODO: unshare union pkvm_pv_param with pkvm */
+	free_pages_exact(page, size);
+	this_cpu_write(pv_param, NULL);
 }
 
 static void pkvm_emergency_disable_virtualization_cpu(void) { /* TODO */ }

arch/x86/include/asm/kvm_pkvm.h
@@ -268,4 +268,8 @@ enum pkvm_fn {
 	__pkvm__vcpu_after_set_cpuid,
 };
 
+union pkvm_pv_param {
+	struct kvm_segment seg;
+} __aligned(PAGE_SIZE);
+
 #endif

BayLibre/android_kernel_common

commit 155fb7a010ceef77c877ebe5b8a01a30612699d7

ANDROID: pkvm: x86: Share/pin and unshare/unpin pkvm_pv_param page
The pkvm_pv_param page is a per-cpu page between host and pkvm to share
the input and output for some PV interfaces. Share and pin this per-cpu
page when enables the virtualization on each CPU before pkvm can use it.
It will also be unpinned and unshared when disables the virtualization
on each CPU.

Upstream-Task: 402758258

Fixes: 5fd327389a06 ("ANDROID: kvm: pkvm: Add union pkvm_pv_param")
Change-Id: I6d70d5cebfeb26fcd752fab43a7d4224b70a534e
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/pkvm.c
@@ -41,27 +41,60 @@ static DEFINE_PER_CPU(union pkvm_pv_param *, pv_param);
 
 #define this_pv_param(f)	(&this_cpu_read(pv_param)->f)
 
-static int pkvm_enable_virtualization_cpu(unsigned long pv_param_pa)
+static int pkvm_enable_virtualization_cpu(unsigned long pv_param_gpa)
 {
-	int r = kvm_arch_enable_virtualization_cpu();
+	size_t size = sizeof(union pkvm_pv_param);
+	int r;
 
-	if (!r)
-		/*
-		 * TODO: Assume host is already share the pv_param structure
-		 * with pkvm. Pin the pv_param_pa so that it won't be re-used
-		 * as guest memory.
-		 */
-		this_cpu_write(pv_param, __pkvm_va(pv_param_pa));
+	/*
+	 * The kvm host can follow the way of handling the shared kvm/kvm_vcpu
+	 * by trigger page sharing via PV interface, and the pkvm hypervisor to
+	 * pin the shared pages. But then the kvm host will need to unshare the
+	 * pv_param in the pkvm_disable_virtualization_cpu() in the irq disabled
+	 * context, which conflicts with using the mutex hyp_shared_pfns_lock.
+	 *
+	 * Alternatively, request the pv_param memory to be page aligned so that
+	 * the pkvm hypervisor can share/unshare the pv_param memory page by its
+	 * own.
+	 */
+	r = __pkvm_host_share_hyp(pv_param_gpa, size);
+	if (r)
+		return r;
 
+	r = hyp_pin_shared_mem(pv_param_gpa, size);
+	if (r)
+		goto unshare;
+
+	r = kvm_arch_enable_virtualization_cpu();
+	if (r)
+		goto unpin;
+
+	this_cpu_write(pv_param, __pkvm_va(host_gpa2hpa(pv_param_gpa)));
+	return 0;
+
+unpin:
+	hyp_unpin_shared_mem(pv_param_gpa, size);
+unshare:
+	__pkvm_host_unshare_hyp(pv_param_gpa, size);
 	return r;
 }
 
 static void pkvm_disable_virtualization_cpu(void)
 {
+	void *pv_param_va = this_cpu_read(pv_param);
+	size_t size = sizeof(union pkvm_pv_param);
+	unsigned long pv_param_gpa;
+
+	if (!pv_param_va)
+		return;
+
+	this_cpu_write(pv_param, NULL);
+
 	kvm_arch_disable_virtualization_cpu();
 
-	/* TODO: unpin the shared pv_param memory */
-	this_cpu_write(pv_param, NULL);
+	pv_param_gpa = pkvm_virt_to_host_gpa(pv_param_va);
+	hyp_unpin_shared_mem(pv_param_gpa, size);
+	__pkvm_host_unshare_hyp(pv_param_gpa, size);
 }
 
 #define HANDLE_OFFSET 1

arch/x86/kvm/vmx/pkvm_high.c
@@ -865,7 +865,6 @@ static int pkvm_enable_virtualization_cpu(void)
 	unsigned long pv_param_pa = __pa(this_cpu_ptr(&pv_param));
 	int r;
 
-	/* TODO: share union pkvm_pv_param with pkvm */
 	r = kvm_call_pkvm(enable_virtualization_cpu, pv_param_pa);
 	if (r)
 		return r;
@@ -878,7 +877,6 @@ static void pkvm_disable_virtualization_cpu(void)
 {
 	intel_pt_handle_vmx(0);
 	kvm_call_pkvm(disable_virtualization_cpu);
-	/* TODO: unshare union pkvm_pv_param with pkvm */
 }
 
 static void pkvm_emergency_disable_virtualization_cpu(void) { /* TODO */ }