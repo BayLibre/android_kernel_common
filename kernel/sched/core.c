61c8a28f5c30f96cd9ce30e3407c82dbaa408717
Sai Harshini Nimmala <quic_snimmala@quicinc.com>
2025-10-03 14:19:24 -0700

    ANDROID: sched: Re-apply patch to export a few scheduler symbols for vendor modules
    This reverts commit 3e977a9bed4b72f5dfca97469fec585eaaf7fa3e.
    
    As migrate_swap function is needed by Qualcomm vendor module, reverting
    the revert to restore the original patch. Essentially,
    CONFIG_NUMA_BALANCING protection is removed and function is exported.
    Subsequent patch addresses the lines of code that need to be protected
    by the above config.
    
    Change-Id: I4ccab7bde99b328317cf3fd10aedf15e314fe09c
    Signed-off-by: Sai Harshini Nimmala <quic_snimmala@quicinc.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -3435,7 +3435,6 @@ void set_task_cpu(struct task_struct *p, unsigned int new_cpu)
 EXPORT_SYMBOL_GPL(set_task_cpu);
 #endif /* CONFIG_SMP */
 
-#ifdef CONFIG_NUMA_BALANCING
 static void __migrate_swap_task(struct task_struct *p, int cpu)
 {
 	if (task_on_rq_queued(p)) {
@@ -3539,7 +3538,7 @@ int migrate_swap(struct task_struct *cur, struct task_struct *p,
 out:
 	return ret;
 }
-#endif /* CONFIG_NUMA_BALANCING */
+EXPORT_SYMBOL_GPL(migrate_swap);
 
 /***
  * kick_process - kick a running thread to enter/exit the kernel
