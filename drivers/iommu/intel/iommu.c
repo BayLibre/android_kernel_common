9d0a33fa0db1323b0351a4b9aca2924d8c000681
Vineeth Pillai <vineethrp@google.com>
2025-10-09 10:22:15 -0400

    ANDROID: iommu/vt-d: Avoid pagetable walk during dmar fault logging
    When pviommu enabled, identity mapping for passthrough devices are
    changed to DMA mapping in pkvm and uses host ept as second level page
    table. Since this is not accessible for host, avoid walking page tables.
    
    Bug: 391542119
    Upstream-Task: 402758258
    Test: Build and Boot
    
    Change-Id: I33a9d4e8d098e31f5e7a8f5d9113766555c0e946
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

--- drivers/iommu/intel/iommu.c
+++ drivers/iommu/intel/iommu.c
@@ -588,6 +588,18 @@ void dmar_fault_dump_ptes(struct intel_iommu *iommu, u16 source_id,
 			pr_info("legacy mode page table is not present\n");
 			return;
 		}
+
+		if (pkvm_pviommu_enabled() &&
+				 context_domain_id(ctx_entry) == FLPT_DEFAULT_DID) {
+			/*
+			 * Do not try to walk the page table with pviommu if
+			 * device is identity mapped. pkvm converts identity
+			 * to DMA mapping and uses host ept which the host do
+			 * not have access.
+			 */
+			pr_info("passthrough mode is shadowed and write protected by pkvm\n");
+			return;
+		}
 		level = agaw_to_level(ctx_entry->hi & 7);
 		pgtable = phys_to_virt(ctx_entry->lo & VTD_PAGE_MASK);
 		goto pgtable_walk;
@@ -622,6 +634,17 @@ void dmar_fault_dump_ptes(struct intel_iommu *iommu, u16 source_id,
 
 	if (!pasid_pte_is_present(pte)) {
 		pr_info("scalable mode page table is not present\n");
+		return;
+	}
+
+	if (pkvm_pviommu_enabled() && pasid_get_domain_id(pte) == FLPT_DEFAULT_DID) {
+		/*
+		 * Do not try to walk the page table with pviommu if
+		 * device is identity mapped. pkvm converts identity
+		 * to DMA mapping and uses host ept which the host do
+		 * not have access.
+		 */
+		pr_info("passthrough mode is shadowed and write protected by pkvm\n");
 		return;
 	}
 
