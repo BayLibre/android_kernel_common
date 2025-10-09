d9f4e57361ef52377874bb6557cc4cbc25df7187
Vineeth Pillai <vineethrp@google.com>
2025-10-09 05:52:20 -0400

    ANDROID: pkvm: x86: hypercalls for iommu domain management
    Bug: 391542119
    Upstream-Task: 402758258
    Test: Build and Boot
    
    Change-Id: Id16d43d0120bf6b31b2a381aaedb97209c7b8f4c
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

--- arch/x86/kvm/vmx/pkvm/hyp/iommu_internal.h
+++ arch/x86/kvm/vmx/pkvm/hyp/iommu_internal.h
@@ -73,6 +73,8 @@ extern const struct pkvm_mm_ops iommu_pw_noncoherency_mm_ops;
 extern const struct pkvm_pgtable_ops iommu_lm_id_ops;
 extern const struct pkvm_pgtable_ops iommu_sm_id_ops;
 
+#define __DOMAIN_MAX_ADDR(gaw) ((((uint64_t)1) << (gaw)) - 1)
+
 #define LAST_LEVEL(level)	\
 	(((level) == 1) ? true : false)
 
@@ -244,6 +246,8 @@ unsigned long pkvm_iommu_clear_ce(u64 phys, u64 param_gpa);
 unsigned long pkvm_iommu_set_lm_ce(u64 phys, u64 param_gpa);
 unsigned long pkvm_iommu_set_sm_ce(u64 phys, u64 param_gpa);
 unsigned long pkvm_iommu_set_sm_ce_pre(u64 phys, u64 param_gpa);
+unsigned long pkvm_iommu_domain_alloc(u64 phys, u64 param_gpa);
+unsigned long pkvm_iommu_domain_free(u64 pgd_gpa);
 #else
 int handle_descriptor(struct pkvm_iommu *iommu, struct qi_desc *desc);
 int free_shadow_id(struct pkvm_iommu *iommu, unsigned long vaddr,
