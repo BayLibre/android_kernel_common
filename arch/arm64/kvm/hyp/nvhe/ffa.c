6d8ddc9666ec38b7124508135583b7da367aa395
Per Larsen <perlarsen@google.com>
2025-10-10 05:32:44 +0000

    ANDROID: KVM: arm64: Make FF-A 1.1 the minimum version for guests
    Make FF-A 1.1 the minimum required guest version because it is otherwise
    necessary to add non-trivial FF-A 1.0 compatibility paths.
    
    Prevent the guest from making FF-A calls by disabling the
    KVM_CAP_ARM_PROTECTED_VM_FLAGS_SET_FFA capability if the hypervisor
    negotiated FF-A 1.0.
    
    Bug: 449927652
    Change-Id: I4a097ff6af6c520fa39ce96302af04d7539a50cd
    Signed-off-by: Per Larsen <perlarsen@google.com>

--- arch/arm64/kvm/hyp/nvhe/ffa.c
+++ arch/arm64/kvm/hyp/nvhe/ffa.c
@@ -1506,13 +1506,13 @@ static void do_ffa_guest_version(struct arm_smccc_1_2_regs *res,
 {
 	DECLARE_REG(u32, ffa_req_version, ctxt, 1);
 
-	if (FFA_MAJOR_VERSION(ffa_req_version) != 1) {
+	if (FFA_MAJOR_VERSION(ffa_req_version) != 1 || FFA_MINOR_VERSION(ffa_req_version) < 1) {
 		res->a0 = FFA_RET_NOT_SUPPORTED;
 		return;
 	}
 
 	hyp_spin_lock(&version_lock);
-	if (has_version_negotiated)
+	if (has_version_negotiated && FFA_MINOR_VERSION(hyp_ffa_version) >= 1)
 		res->a0 = hyp_ffa_version;
 	else
 		res->a0 = FFA_RET_NOT_SUPPORTED;
