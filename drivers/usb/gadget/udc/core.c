c11be75f4f6bf05ad6a7b5ef4f43115759992555
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2025-10-11 18:51:36 -0700

    Revert "FROMGIT: usb: udc: Add trace event for usb_gadget_set_state"
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/db4f0ef6-a6b2-4874-9e5b-7572cbd6e7ce). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: I9df2b449bbc7cdafa93a29639f0d3153d57d14dd
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com
    Bug: 427760780

--- drivers/usb/gadget/udc/core.c
+++ drivers/usb/gadget/udc/core.c
@@ -1093,7 +1093,6 @@ void usb_gadget_set_state(struct usb_gadget *gadget,
 {
 	gadget->state = state;
 	schedule_work(&gadget->work);
-	trace_usb_gadget_set_state(gadget, 0);
 }
 EXPORT_SYMBOL_GPL(usb_gadget_set_state);
 
