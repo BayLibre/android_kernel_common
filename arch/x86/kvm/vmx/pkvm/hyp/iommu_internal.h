7f2bf27d19bb3f14428a96556ab50bd032e91ba7
Aashish Sharma <shraash@google.com>
2025-10-12 23:15:06 -0400

    ANDROID: pkvm: x86: Hypercalls for scalable mode context update.
    Implement hypercalls for updating scalable mode context entries. Similar
    to legacy context updates, host donates the pages needed for the context
    table and pkvm writes protect the pages.
    
    Unlike shadow iommu, we need not worry about memory constraints as the
    host donates pages as needed. So remove the limit on maximum supported
    PASID.
    
    Bug: 391542119
    Upstream-Task: 402758258
    Test: Build and Boot
    
    Change-Id: I1128b0daac299d6300b126db8ec911f4431c327b
    Signed-off-by: Aashish Sharma <shraash@google.com>
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

--- arch/x86/kvm/vmx/pkvm/hyp/iommu_internal.h
+++ arch/x86/kvm/vmx/pkvm/hyp/iommu_internal.h
@@ -396,6 +396,7 @@ struct pkvm_iommu *find_iommu_by_reg_phys(unsigned long phys);
 unsigned long pkvm_iommu_iec_flush(u64 phys, u64 global, u64 index, u64 mask);
 unsigned long pkvm_iommu_clear_ce(u64 param_va);
 unsigned long pkvm_iommu_set_lm_ce(u64 param_va);
+unsigned long pkvm_iommu_set_sm_ce(u64 param_va);
 #else
 int initialize_iommu_pgt(struct pkvm_iommu *iommu);
 int handle_descriptor(struct pkvm_iommu *iommu, struct qi_desc *desc);
