7a824f55381267ef85095b879b5a06ae37318b23
Vineeth Pillai <vineethrp@google.com>
2025-10-12 23:13:37 -0400

    ANDROID: iommu/vt-d: Paravirtualize legacy context entry updates
    Bug: 391542119
    Upstream-Task: 402758258
    Test: Build and Boot
    
    Change-Id: I0feae6a72c79a744e00859774ec17d7218cd0dd4
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

--- drivers/iommu/intel/iommu_pkvm.h
+++ drivers/iommu/intel/iommu_pkvm.h
@@ -6,7 +6,18 @@
 #ifndef _INTEL_IOMMU_PKVM_H_
 #define _INTEL_IOMMU_PKVM_H_
 
-#include <asm/pkvm.h>
+#include <asm/kvm_pkvm.h>
+
+#define pkvm_iommu_hypercall(hc, param_name, param)			\
+	({									\
+		struct pkvm_##param_name *p = get_this_pv_param(param_name);	\
+		int ret;							\
+		*p = *(param);							\
+		ret = kvm_hypercall1((hc), (unsigned long)p);		\
+		*(param) = *p;							\
+		put_this_pv_param(p);						\
+		ret;								\
+	})
 
 static inline long pkvm_hc_enable_iommu(unsigned long reg_phys,
 		unsigned long root_gpa)
@@ -20,5 +31,15 @@ static inline long pkvm_hc_disable_iommu(unsigned long reg_phys)
 {
 	return kvm_hypercall1(PKVM_HC_DISABLE_IOMMU, reg_phys);
 
+}
+
+static inline long pkvm_hc_iommu_clear_ce(struct pkvm_clear_translation_param *param)
+{
+	return pkvm_iommu_hypercall(PKVM_HC_IOMMU_CLEAR_CE, clear_translation_param, param);
+}
+
+static inline long pkvm_hc_iommu_set_lm_ce(struct pkvm_lm_context_param *param)
+{
+	return pkvm_iommu_hypercall(PKVM_HC_IOMMU_SET_LM_CE, lm_context_param, param);
 }
 #endif
