8519e669d1bf13acae5f61e43b23932fab07d635
Aashish Sharma <shraash@google.com>
2025-10-12 23:15:06 -0400

    ANDROID: pkvm: x86: Hypercalls for scalable mode context update.
    Implement hypercalls for updating scalable mode context entries.
    
    Bug: 391542119
    Upstream-Task: 402758258
    Test: Build and Boot
    
    Change-Id: I1128b0daac299d6300b126db8ec911f4431c327b
    Signed-off-by: Aashish Sharma <shraash@google.com>
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

--- arch/x86/kvm/vmx/pkvm/hyp/pv_iommu.c
+++ arch/x86/kvm/vmx/pkvm/hyp/pv_iommu.c
@@ -246,3 +246,127 @@ unsigned long pkvm_iommu_set_lm_ce(u64 param_va)
 
 	return ret;
 }
+
+/*
+ * Get the PASID directory size for scalable mode context entry.
+ * Value of X in the PDTS field of a scalable mode context entry
+ * indicates PASID directory with 2^(X + 7) entries.
+ *
+ * Copied from drivers/iommu/intel/pasid.c:context_get_sm_pds()
+ */
+static unsigned long context_get_sm_pds(u32 max_pasid)
+{
+	unsigned long pds, max_pde;
+
+	max_pde = max_pasid >> PASIDDIR_SHIFT;
+	pds = find_first_bit(&max_pde, MAX_NR_PASID_BITS);
+	if (pds < 7)
+		return 0;
+
+	return pds - 7;
+}
+
+/*
+ * Size of pasid directory in bytes, given the max pasid number
+ * A pasid directory entry can address 64 pasids and a pasid
+ * directory page holds 512 entries, hence one page can address
+ * 64 * 512 entries.
+ * So pasid_dir_size = (max_pasid / (64 * 512)) * PAGE_SIZE
+ *                   = (max_pasid >> 15) << PAGE_SHIFT
+ */
+#define pasid_dir_size(max_pasid) ((max_pasid) >> (15 - PAGE_SHIFT))
+
+unsigned long pkvm_iommu_set_sm_ce(u64 param_va)
+{
+	struct pkvm_sm_context_param *param;
+	struct context_entry *context;
+	struct pkvm_iommu *hyp_iommu;
+	struct intel_iommu *iommu;
+	unsigned long pds;
+	u8 bus, devfn;
+	int ret = 0;
+
+	if (!param_va)
+		return -EINVAL;
+
+	param = (struct pkvm_sm_context_param *)kern_pkvm_va((void *)param_va);
+	if (WARN_ON_ONCE(param != this_pv_param(sm_context_param)))
+		return -EINVAL;
+
+	hyp_iommu = find_iommu_by_reg_phys(param->phys);
+	if (!hyp_iommu)
+		return -EINVAL;
+
+	if (!param->pasid_dir_gpa)
+		return -EINVAL;
+
+	if (param->ats_supported && !is_dev_in_satc(param->bdf)) {
+		pkvm_err("pkvm: %s host reports ats supported for device[%x], but not in satc\n",
+				__func__, param->bdf);
+		return -EPERM;
+	}
+
+	pkvm_spin_lock(&hyp_iommu->lock);
+	iommu = &hyp_iommu->iommu;
+
+	pkvm_dbg("pkvm: %s: set_sm_ce: dev[%x] max_pasid: %u, pasid_dir: %llx\n",
+			__func__, param->bdf, param->max_pasid, param->pasid_dir_gpa);
+
+	if (!sm_supported(iommu)) {
+		pkvm_err("pkvm: %s: iommu%d doesn't support scalable mode!\n",
+				__func__, iommu->seq_id);
+		return -EINVAL;
+	};
+
+	bus = PCI_BUS_NUM(param->bdf);
+	devfn = PCI_DEV_FN(param->bdf);
+	context = pkvm_iommu_context_addr(iommu, bus, devfn, &param->context_gpa);
+	if (!context) {
+		pkvm_spin_unlock(&hyp_iommu->lock);
+		return -ENOMEM;
+	}
+
+	if (context_present(context)) {
+		pkvm_spin_unlock(&hyp_iommu->lock);
+		return 0;
+	}
+
+	if (__pkvm_host_donate_hyp_share_ro(param->pasid_dir_gpa,
+				pasid_dir_size(param->max_pasid))) {
+		pkvm_spin_unlock(&hyp_iommu->lock);
+		pkvm_err("pkvm: %s: failed to write protect pasid dir pages!\n",
+				__func__);
+		return -EFAULT;
+	}
+
+	context_clear_entry(context);
+
+	pds = context_get_sm_pds(param->max_pasid);
+	context->lo = param->pasid_dir_gpa | context_pdts(pds);
+	context_set_sm_rid2pasid(context, IOMMU_NO_PASID);
+
+	if (param->ats_supported)
+		context_set_sm_dte(context);
+	if (ecap_pasid(iommu->ecap))
+		context_set_pasid(context);
+
+	context_set_fault_enable(context);
+	context_set_present(context);
+
+	__pkvm_iommu_flush_cache(iommu, context, sizeof(*context));
+	pkvm_spin_unlock(&hyp_iommu->lock);
+
+	/*
+	 * It's a non-present to present mapping. If hardware doesn't cache
+	 * non-present entry we don't need to flush the caches. If it does
+	 * cache non-present entries, then it does so in the special
+	 * domain #0, which we have to flush:
+	 */
+	if (cap_caching_mode(iommu->cap)) {
+		flush_context_cache(hyp_iommu, 0, param->bdf,
+				DMA_CCMD_MASK_NOBIT, DMA_CCMD_DEVICE_INVL);
+		flush_iotlb(hyp_iommu, 0, 0, 0, DMA_TLB_DSI_FLUSH);
+	}
+
+	return ret;
+}
