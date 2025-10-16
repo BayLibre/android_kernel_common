373585514b40c3b233d0fe78f51a948045c51310
Bartłomiej Grzesik <bgrzesik@google.com>
2025-10-16 13:41:48 +0200

    ANDROID: gic-v3-its-pkvm: Add support for 64 bit access
    This change modifies trap and emulate to support 64 bit register access.
    This is necessary for GICR support.
    
    Bug: 444189132
    Bug: 448036963
    Bug: 378381203
    Test: build
    Change-Id: Ibecc9ec0bba752e47b35035785443068bac6ef87
    Signed-off-by: Bartłomiej Grzesik <bgrzesik@google.com>

--- drivers/irqchip/gic-v3-its-pkvm/hyp/its.c
+++ drivers/irqchip/gic-v3-its-pkvm/hyp/its.c
@@ -26,14 +26,21 @@ static DEFINE_HYP_SPINLOCK(its_devs_lock);
 	for (__its = its_devs; __its != &its_devs[its_dev_count]; __its++)
 
 static int its_emulate_handler(struct emulate *emulate, u64 offset, bool write,
-			       u32 *reg)
+			       u64 *reg, int reg_size)
 {
 	struct hyp_gic_v3_its *its = emulate->priv;
 
-	if (write)
-		writel_relaxed(*reg, its->base + offset);
-	else
-		*reg = readl_relaxed(its->base + offset);
+	if (reg_size == sizeof(u32)) {
+		if (write)
+			writel_relaxed(*reg, its->base + offset);
+		else
+			*reg = readl_relaxed(its->base + offset);
+	} else {
+		if (write)
+			writeq_relaxed(*reg, its->base + offset);
+		else
+			*reg = readq_relaxed(its->base + offset);
+	}
 
 	return 0;
 }
