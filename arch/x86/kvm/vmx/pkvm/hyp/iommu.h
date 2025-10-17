4087cd52614f03eba0ee7bf1d82f91df7897c7d8
Aashish Sharma <shraash@google.com>
2025-10-16 19:14:01 -0700

    ANDROID: pkvm: x86: Flush did FLPT_DEFAULT_DID for host_ept flush
    Whenever a page is donated to/from hyp from/to host, iotlb needs
    to be flushed for the the host EPT. Only the domains which are
    configured passthrough in the host, use host EPT as their second
    level page table in pkvm and use did as FLPT_DEFAULT_DID. So we
    only need to flush did FLPT_DEFAULT_DID when doing an iotlb flush
    for the host EPT.
    
    Bug: 391542119
    Upstream-Task: 402758258
    Test: Build and Boot
    
    Signed-off-by: Aashish Sharma <shraash@google.com>
    Change-Id: I2e2f0dd8067beddb946b6cdd4e4c74bc9b07b22a

--- arch/x86/kvm/vmx/pkvm/hyp/iommu.h
+++ arch/x86/kvm/vmx/pkvm/hyp/iommu.h
@@ -11,6 +11,6 @@ unsigned long pkvm_access_iommu(bool is_read, int len, unsigned long reg, unsign
 bool is_mem_range_overlap_iommu(unsigned long start, unsigned long end);
 int pkvm_iommu_sync(u16 bdf, u32 pasid);
 bool pkvm_iommu_coherency(u16 bdf, u32 pasid);
-void pkvm_iommu_flush_iotlb(struct pkvm_pgtable *pgt, unsigned long addr, unsigned long size);
+void pkvm_iommu_flush_iotlb(unsigned long addr, unsigned long size);
 
 #endif
