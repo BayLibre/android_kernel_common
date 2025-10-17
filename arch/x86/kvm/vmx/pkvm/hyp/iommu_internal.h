6e66e87dd46eb51bfc39c1996f6988a7e1703a49
Aashish Sharma <shraash@google.com>
2025-10-16 19:14:01 -0700

    ANDROID: pkvm: x86: Flush did FLPT_DEFAULT_DID for host_ept flush
    Whenever a page is donated to/from hyp from/to host, iotlb needs
    to be flushed for the the host EPT. Only the domains which are
    configured passthrough in the host, use host EPT as their second
    level page table in pkvm and use did as FLPT_DEFAULT_DID. So we
    only need to flush did FLPT_DEFAULT_DID when doing an iotlb flush
    for the host EPT.
    
    Bug: 391542119
    Upstream-Task: 402758258
    Test: Build and Boot
    
    Signed-off-by: Aashish Sharma <shraash@google.com>
    Change-Id: I2e2f0dd8067beddb946b6cdd4e4c74bc9b07b22a

--- arch/x86/kvm/vmx/pkvm/hyp/iommu_internal.h
+++ arch/x86/kvm/vmx/pkvm/hyp/iommu_internal.h
@@ -135,6 +135,13 @@ do {									\
 #define QI_DESC_IOTLB_ADDR(qw)		((qw) & VTD_PAGE_MASK)
 #define QI_DESC_IOTLB_AM(qw)		((qw) & GENMASK_ULL(5, 0))
 
+/*
+ * Domain ID reserved for pasid entries programmed for first-level
+ * only and pass-through transfer modes.
+ * (copied from drivers/iommu/intel/pasid.h)
+ */
+#define FLPT_DEFAULT_DID		1
+
 #define pgt_to_pkvm_iommu(_pgt) container_of(_pgt, struct pkvm_iommu, pgt)
 
 struct pasid_dir_entry {
