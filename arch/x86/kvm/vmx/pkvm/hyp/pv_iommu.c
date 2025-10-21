4f5ab52bd5fc8f16875ff12717b79572e3c300d5
Aashish Sharma <shraash@google.com>
2025-10-20 20:08:43 -0700

    ANDROID: pkvm: x86: Return pasid dir and tables back to host
    On context clear with scalable mode on, pasid dir and pasid
    tables should be returned back to the host, so it can free
    them.
    
    Bug: 391542119
    Upstream-Task: 402758258
    Test: Build and Boot
    
    Signed-off-by: Aashish Sharma <shraash@google.com>
    Change-Id: Ieb3bc3d7d3fe77aea249a5e6d6c3a7b9af380e64

--- arch/x86/kvm/vmx/pkvm/hyp/pv_iommu.c
+++ arch/x86/kvm/vmx/pkvm/hyp/pv_iommu.c
@@ -83,6 +83,7 @@ unsigned long pkvm_iommu_clear_ce(u64 param_va)
 	struct pkvm_clear_translation_param *param;
 	struct context_entry *context;
 	struct pkvm_iommu *hyp_iommu;
+	int ret = 0;
 
 	if (!param_va)
 		return -EINVAL;
@@ -102,6 +103,14 @@ unsigned long pkvm_iommu_clear_ce(u64 param_va)
 	if (!context)
 		goto out;
 
+	if (sm_supported(&hyp_iommu->iommu) && context_present(context)) {
+		ret = pkvm_pasid_free_table(
+				pkvm_phys_to_virt(context->lo & VTD_PAGE_MASK),
+				1 << (((context->lo >> 9) & 0x7) + 7));
+		if (ret)
+			goto out;
+	}
+
 	/*
 	 * Pass the did back to host for iommu cache flush.
 	 */
@@ -112,7 +121,7 @@ unsigned long pkvm_iommu_clear_ce(u64 param_va)
 	__pkvm_iommu_flush_cache(&hyp_iommu->iommu, context, sizeof(*context));
 out:
 	pkvm_spin_unlock(&hyp_iommu->lock);
-	return 0;
+	return ret;
 }
 
 static void pkvm_context_present_cache_flush(struct pkvm_iommu *iommu, u16 bdf, u16 did)
