2fe450ccbd21d03f036e11a07862e5b89915ee80
Aashish Sharma <shraash@google.com>
2025-10-20 20:08:43 -0700

    ANDROID: pkvm: x86: Return pasid dir and tables back to host
    On context clear with scalable mode on, pasid dir and pasid
    tables should be returned back to the host, so it can free
    them.
    
    Bug: 391542119
    Upstream-Task: 402758258
    Test: Build and Boot
    
    Signed-off-by: Aashish Sharma <shraash@google.com>
    Change-Id: Ieb3bc3d7d3fe77aea249a5e6d6c3a7b9af380e64

--- arch/x86/kvm/vmx/pkvm/hyp/pv_pasid.c
+++ arch/x86/kvm/vmx/pkvm/hyp/pv_pasid.c
@@ -54,6 +54,31 @@ static void __pasid_setup_sl(struct intel_iommu *iommu, struct pasid_entry *pe, 
 	pasid_set_present(pe);
 }
 
+int pkvm_pasid_free_table(struct pasid_dir_entry *dir, int max_pde)
+{
+	struct pasid_entry *table;
+	int i, ret = 0;
+
+	for (i = 0; i < max_pde; i++) {
+		table = get_pasid_table_from_pde(&dir[i]);
+		if (!table)
+			continue;
+		ret = __pkvm_hyp_release_host(pkvm_virt_to_phys(table), VTD_PAGE_SIZE);
+		if (ret) {
+			pkvm_err("pkvm: %s: failed to remove write protect pasid entry: %llx!\n",
+					__func__, pkvm_virt_to_phys(table));
+			return ret;
+		}
+	}
+
+	ret = __pkvm_hyp_release_host(pkvm_virt_to_phys(dir), ALIGN(max_pde * 8, VTD_PAGE_SIZE));
+	if (ret)
+		pkvm_err("pkvm: %s: failed to remove write protect pasid dir: %llx!\n",
+				__func__, pkvm_virt_to_phys(dir));
+
+	return ret;
+}
+
 static int pkvm_pasid_get_entry(struct intel_iommu *iommu, u32 pasid, u16 bdf,
 		u64 *ptable_gpa, struct pasid_entry **pte)
 {
