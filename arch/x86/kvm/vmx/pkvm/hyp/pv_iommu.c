89c94cbd66190325f9f2e4dfbfd26fe002397604
Aashish Sharma <shraash@google.com>
2025-10-20 20:08:43 -0700

    ANDROID: pkvm: x86: Return pasid dir and tables back to host
    On context clear with scalable mode on, pasid dir and pasid
    tables should be returned back to the host, so it can free
    them.
    
    Bug: 391542119
    Upstream-Task: 402758258
    Test: Build and Boot
    
    Signed-off-by: Aashish Sharma <shraash@google.com>
    Change-Id: Ieb3bc3d7d3fe77aea249a5e6d6c3a7b9af380e64

--- arch/x86/kvm/vmx/pkvm/hyp/pv_iommu.c
+++ arch/x86/kvm/vmx/pkvm/hyp/pv_iommu.c
@@ -138,6 +138,7 @@ unsigned long pkvm_iommu_clear_ce(u64 param_va)
 	struct pkvm_clear_translation_param *param;
 	struct context_entry *context;
 	struct pkvm_iommu *hyp_iommu;
+	int ret = 0;
 	u16 did;
 
 	if (!param_va)
@@ -160,6 +161,14 @@ unsigned long pkvm_iommu_clear_ce(u64 param_va)
 		return 0;
 	}
 
+	if (sm_supported(&hyp_iommu->iommu) && context_present(context)) {
+		ret = pkvm_pasid_free_table(pkvm_phys_to_virt(context->lo & VTD_PAGE_MASK),
+					    get_pasid_dir_size(context));
+		if (ret)
+			pkvm_err("pkvm: %s: failed to free pasid table for device: %x\n",
+				 __func__, param->bdf);
+	}
+
 	did = context_domain_id(context);
 	pkvm_dbg("pkvm: %s: clear_ce: dev[%x] did: %u\n", __func__, param->bdf, did);
 
@@ -169,7 +178,7 @@ unsigned long pkvm_iommu_clear_ce(u64 param_va)
 
 	context_flush_present_no_pasid(hyp_iommu, did, param->bdf,
 				       param->ats_qdep, param->ats_enabled);
-	return 0;
+	return ret;
 }
 
 /* Copied from drivers/iommu/intel/iommu.c:context_present_cache_flush() */
