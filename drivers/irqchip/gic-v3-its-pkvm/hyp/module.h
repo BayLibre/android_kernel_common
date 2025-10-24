306fcd365309957827920313c30adf00e360c850
Bartłomiej Grzesik <bgrzesik@google.com>
2025-10-24 16:29:14 +0200

    ANDROID: gic-v3-its-pkvm: Add GICR LPI sanitization
    Sanitize the GICR register access from the host.
    
    Bug: 444189132
    Bug: 448036963
    Bug: 378381203
    Test: build
    Change-Id: I8b30ad90dcb0fa7516ae2c23fd8f3fbf74994568
    Signed-off-by: Bartłomiej Grzesik <bgrzesik@google.com>

--- drivers/irqchip/gic-v3-its-pkvm/hyp/module.h
+++ drivers/irqchip/gic-v3-its-pkvm/hyp/module.h
@@ -2,7 +2,6 @@
 #ifndef __GIC_V3_ITS_PKVM_MODULES__
 #define __GIC_V3_ITS_PKVM_MODULES__
 
-
 #if defined(__KVM_NVHE_HYPERVISOR__) && defined(MODULE)
 
 #include <asm/kvm_pkvm_module.h>
@@ -22,6 +21,13 @@ extern const struct pkvm_module_ops *mod_ops;
 
 #define create_private_mapping(phys, size, prot, haddr) \
 	CALL_FROM_OPS(create_private_mapping, phys, size, prot, haddr)
+
+#define host_share_hyp(pfn) CALL_FROM_OPS(host_share_hyp, pfn)
+
+#define host_unshare_hyp(pfn) CALL_FROM_OPS(host_unshare_hyp, pfn)
+
+#define host_stage2_get_leaf(phys, ptep, level) \
+	CALL_FROM_OPS(host_stage2_get_leaf, phys, ptep, level)
 
 #endif /* defined(__KVM_NVHE_HYPERVISOR__) && defined(MODULE) */
 
