8f7b579ce4e38bfe07936dba0ab9d892ec0f068c
Bartłomiej Grzesik <bgrzesik@google.com>
2025-10-24 16:29:14 +0200

    ANDROID: gic-v3-its-pkvm: Add GICR LPI sanitization
    Sanitize the GICR register access from the host.
    
    Bug: 444189132
    Bug: 448036963
    Bug: 378381203
    Test: build
    Change-Id: I8b30ad90dcb0fa7516ae2c23fd8f3fbf74994568
    Signed-off-by: Bartłomiej Grzesik <bgrzesik@google.com>

--- drivers/irqchip/gic-v3-its-pkvm/hyp/gic-v3-its.h
+++ drivers/irqchip/gic-v3-its-pkvm/hyp/gic-v3-its.h
@@ -8,6 +8,11 @@ int hyp_gic_v3_its_init(const struct pkvm_module_ops *ops);
 
 #define hyp_gic_v3_its_protect_hvc kvm_nvhe_sym(hyp_gic_v3_its_protect_hvc)
 void hyp_gic_v3_its_protect_hvc(struct user_pt_regs *regs);
+
+#define hyp_gic_v3_redist_protect_hvc \
+	kvm_nvhe_sym(hyp_gic_v3_redist_protect_hvc)
+void hyp_gic_v3_redist_protect_hvc(struct user_pt_regs *regs);
+
 #endif /* MODULE */
 
 #endif /* __GIC_V3_ITS_PKVM_GIC_V3_ITS__ */
