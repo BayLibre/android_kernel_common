e4ef7c0138666e2a956204df7aecd707a7ae82c6
Vineeth Pillai <vineethrp@google.com>
2025-11-06 10:34:26 -0500

    ANDROID: pkvm: x86: make pv_param framework generic for hypercalls
    pv_param is a page allocated by host and used to pass data to pkvm as
    hypercall arguements. Currently it is available only to host kvm for
    calling into pkvm. Refactor the implementation to make it available
    generically to the host and pkvm. This would be used by pviommu
    hypercalls in later patches.
    
    Bug: 391542119
    Upstream-Task: 402758258
    Test: Build and Boot
    
    Change-Id: I029512c9433eae9ba9d279c2553ed86aacf9ac55
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

--- arch/x86/kvm/vmx/pkvm/hyp/vmexit.c
+++ arch/x86/kvm/vmx/pkvm/hyp/vmexit.c
@@ -116,7 +116,7 @@ static unsigned long handle_vmcall(struct kvm_vcpu *vcpu)
 	 * invoked after pkvm initialization.
 	 */
 	case PKVM_HC_INIT_FINALISE:
-		ret = __pkvm_init_finalise(vcpu, (struct pkvm_section *)a0, a1);
+		ret = __pkvm_init_finalise(vcpu, (struct pkvm_section *)a0, a1, a2);
 		break;
 	case __PKVM_HC_COMMIT_FINALISE:
 		ret = pkvm_commit_finalise(a0);
