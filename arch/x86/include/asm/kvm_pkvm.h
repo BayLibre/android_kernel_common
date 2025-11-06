f8dec86e50f7208ee699b9b2fab46fa22879a16c
Aashish Sharma <shraash@google.com>
2025-11-06 18:45:44 -0500

    ANDROID: pkvm: x86: Hypercalls for managing cache tags
    Whenever a cache tag is created in host, an hypercall
    will be made to pKVM to add the same in it's own data
    structures. This will allow pKVM to do QI flushing on
    its own for the domain map. Also skip the corresponding
    flushing for the domain map in the host as it's taken
    care for in pKVM now.
    
    Bug: 391542119
    Upstream-Task: 402758258
    Test: Build and Boot
    
    Change-Id: Iaf893f86a0a07c388f2329f083c6d2e03fb6ad45
    Signed-off-by: Aashish Sharma <shraash@google.com>

--- arch/x86/include/asm/kvm_pkvm.h
+++ arch/x86/include/asm/kvm_pkvm.h
@@ -248,6 +248,7 @@ union pkvm_pv_param {
 	struct pkvm_sm_context_param sm_context_param;
 	struct pkvm_pasid_table_param pasid_table_param;
 	struct pkvm_domain_param domain_param;
+	struct pkvm_cache_tag_param cache_tag_param;
 } __aligned(PAGE_SIZE);
 
 #ifdef __PKVM_HYP__
