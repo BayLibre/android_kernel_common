cba10e6b20202d510f9b0bc347c8e689fbdf5e4a
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2025-11-06 18:39:51 -0800

    Revert "Merge 66cd8e9cb810 ("Merge tag 'slab-for-6.18-rc3' of gi..."
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/8eaf22e9-8350-48b1-a220-83cfe42129b1). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: I1f6239bf600c11bc163bfa175184559c73b999f7
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com

--- mm/slub.c
+++ mm/slub.c
@@ -2081,9 +2081,9 @@ static inline void mark_objexts_empty(struct slabobj_ext *obj_exts)
 	}
 }
 
-static inline bool mark_failed_objexts_alloc(struct slab *slab)
+static inline void mark_failed_objexts_alloc(struct slab *slab)
 {
-	return cmpxchg(&slab->obj_exts, 0, OBJEXTS_ALLOC_FAIL) == 0;
+	slab->obj_exts = OBJEXTS_ALLOC_FAIL;
 }
 
 static inline void handle_failed_objexts_alloc(unsigned long obj_exts,
@@ -2105,7 +2105,7 @@ static inline void handle_failed_objexts_alloc(unsigned long obj_exts,
 #else /* CONFIG_MEM_ALLOC_PROFILING_DEBUG */
 
 static inline void mark_objexts_empty(struct slabobj_ext *obj_exts) {}
-static inline bool mark_failed_objexts_alloc(struct slab *slab) { return false; }
+static inline void mark_failed_objexts_alloc(struct slab *slab) {}
 static inline void handle_failed_objexts_alloc(unsigned long obj_exts,
 			struct slabobj_ext *vec, unsigned int objects) {}
 
@@ -2153,14 +2153,8 @@ int alloc_slab_obj_exts(struct slab *slab, struct kmem_cache *s,
 				   slab_nid(slab));
 	}
 	if (!vec) {
-		/*
-		 * Try to mark vectors which failed to allocate.
-		 * If this operation fails, there may be a racing process
-		 * that has already completed the allocation.
-		 */
-		if (!mark_failed_objexts_alloc(slab) &&
-		    slab_obj_exts(slab))
-			return 0;
+		/* Mark vectors which failed to allocate */
+		mark_failed_objexts_alloc(slab);
 
 		return -ENOMEM;
 	}
@@ -2171,7 +2165,6 @@ int alloc_slab_obj_exts(struct slab *slab, struct kmem_cache *s,
 #ifdef CONFIG_MEMCG
 	new_exts |= MEMCG_DATA_OBJEXTS;
 #endif
-retry:
 	old_exts = READ_ONCE(slab->obj_exts);
 	handle_failed_objexts_alloc(old_exts, vec, objects);
 	if (new_slab) {
@@ -2181,7 +2174,8 @@ retry:
 		 * be simply assigned.
 		 */
 		slab->obj_exts = new_exts;
-	} else if (old_exts & ~OBJEXTS_FLAGS_MASK) {
+	} else if ((old_exts & ~OBJEXTS_FLAGS_MASK) ||
+		   cmpxchg(&slab->obj_exts, old_exts, new_exts) != old_exts) {
 		/*
 		 * If the slab is already in use, somebody can allocate and
 		 * assign slabobj_exts in parallel. In this case the existing
@@ -2193,9 +2187,6 @@ retry:
 		else
 			kfree(vec);
 		return 0;
-	} else if (cmpxchg(&slab->obj_exts, old_exts, new_exts) != old_exts) {
-		/* Retry if a racing thread changed slab->obj_exts from under us. */
-		goto retry;
 	}
 
 	if (allow_spin)
@@ -3457,6 +3448,7 @@ static void *alloc_single_from_new_slab(struct kmem_cache *s, struct slab *slab,
 
 	if (!allow_spin && !spin_trylock_irqsave(&n->list_lock, flags)) {
 		/* Unlucky, discard newly allocated slab */
+		slab->frozen = 1;
 		defer_deactivate_slab(slab, NULL);
 		return NULL;
 	}
@@ -6505,12 +6497,9 @@ static void free_deferred_objects(struct irq_work *work)
 		struct slab *slab = container_of(pos, struct slab, llnode);
 
 #ifdef CONFIG_SLUB_TINY
-		free_slab(slab->slab_cache, slab);
+		discard_slab(slab->slab_cache, slab);
 #else
-		if (slab->frozen)
-			deactivate_slab(slab->slab_cache, slab, slab->flush_freelist);
-		else
-			free_slab(slab->slab_cache, slab);
+		deactivate_slab(slab->slab_cache, slab, slab->flush_freelist);
 #endif
 	}
 }
