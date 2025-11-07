4c8ba2d31599886b2dea3a42fa99221aeee98030
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2025-11-06 18:49:36 -0800

    Revert "Merge 818444a61bd8 ("Merge tag 'firewire-fixes-6.18-rc3'..."
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/8eaf22e9-8350-48b1-a220-83cfe42129b1). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: I03a7a8c1de3bbb8059a11c8e4ab5d879602abeb7
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com

--- drivers/gpu/drm/xe/xe_pt.c
+++ drivers/gpu/drm/xe/xe_pt.c
@@ -2022,7 +2022,7 @@ static int op_prepare(struct xe_vm *vm,
 	case DRM_GPUVA_OP_MAP:
 		if ((!op->map.immediate && xe_vm_in_fault_mode(vm) &&
 		     !op->map.invalidate_on_bind) ||
-		    (op->map.vma_flags & XE_VMA_SYSTEM_ALLOCATOR))
+		    op->map.is_cpu_addr_mirror)
 			break;
 
 		err = bind_op_prepare(vm, tile, pt_update_ops, op->map.vma,
@@ -2252,7 +2252,7 @@ static void op_commit(struct xe_vm *vm,
 	switch (op->base.op) {
 	case DRM_GPUVA_OP_MAP:
 		if ((!op->map.immediate && xe_vm_in_fault_mode(vm)) ||
-		    (op->map.vma_flags & XE_VMA_SYSTEM_ALLOCATOR))
+		    op->map.is_cpu_addr_mirror)
 			break;
 
 		bind_op_commit(vm, tile, pt_update_ops, op->map.vma, fence,
