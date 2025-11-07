2fdb71f9e279af9b92079bac68ef557c6db3de1d
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2025-11-06 18:39:51 -0800

    Revert "Merge cb7f9fc3725a ("Merge tag 'kbuild-fixes-6.18-2' of ..."
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/8eaf22e9-8350-48b1-a220-83cfe42129b1). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: If6205844ef87f4952a94fda303176f1b3bf23ba3
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com

--- arch/s390/pci/pci_event.c
+++ arch/s390/pci/pci_event.c
@@ -188,7 +188,7 @@ static pci_ers_result_t zpci_event_attempt_error_recovery(struct pci_dev *pdev)
 	 * is unbound or probed and that userspace can't access its
 	 * configuration space while we perform recovery.
 	 */
-	device_lock(&pdev->dev);
+	pci_dev_lock(pdev);
 	if (pdev->error_state == pci_channel_io_perm_failure) {
 		ers_res = PCI_ERS_RESULT_DISCONNECT;
 		goto out_unlock;
@@ -257,7 +257,7 @@ static pci_ers_result_t zpci_event_attempt_error_recovery(struct pci_dev *pdev)
 		driver->err_handler->resume(pdev);
 	pci_uevent_ers(pdev, PCI_ERS_RESULT_RECOVERED);
 out_unlock:
-	device_unlock(&pdev->dev);
+	pci_dev_unlock(pdev);
 	zpci_report_status(zdev, "recovery", status_str);
 
 	return ers_res;
