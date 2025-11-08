038b37dbe7a243b54d1b40e68e668153b53dba17
Aashish Sharma <shraash@google.com>
2025-11-07 22:13:27 -0500

    ANDROID: pkvm: x86: Hypercalls for queued invalidation
    Bug: 391542119
    Upstream-Task: 402758258
    Test: Build and Boot
    
    Change-Id: I311a10c94f0ba05baa8921cdf6d56d541b598780
    Signed-off-by: Aashish Sharma <shraash@google.com>

--- arch/x86/kvm/vmx/pkvm/hyp/iommu_internal.h
+++ arch/x86/kvm/vmx/pkvm/hyp/iommu_internal.h
@@ -360,9 +360,12 @@ extern void root_tbl_walk(struct pkvm_iommu *iommu);
 
 bool is_dev_in_satc(u16 bdf);
 
-#ifndef CONFIG_PKVM_INTEL_PVIOMMU
+#ifdef CONFIG_PKVM_INTEL_PVIOMMU
+unsigned long pkvm_iommu_submit_qi(u64 reg, u64 desc_base, int count);
+#else
 int initialize_iommu_pgt(struct pkvm_iommu *iommu);
 int handle_descriptor(struct pkvm_iommu *iommu, struct qi_desc *desc);
+int handle_qi_invalidation(struct pkvm_iommu *iommu, unsigned long val);
 int free_shadow_id(struct pkvm_iommu *iommu, unsigned long vaddr,
 		       unsigned long vaddr_end);
 int sync_shadow_id(struct pkvm_iommu *iommu, unsigned long vaddr,
