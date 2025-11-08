9dd42a776b650654e71c3b3456b0dc71cf42e428
Bart Van Assche <bvanassche@google.com>
2025-11-08 15:44:32 -0800

    ANDROID: linux/bits.h: Improve CodeSonar support
    The GENMASK_INPUT_CHECK() macro arguments may include statement
    expressions. CodeSonar does not support passing statement expressions to
    __is_constexpr(). Hence disable GENMASK() input checking when analyzing
    with CodeSonar.
    
    Bug: 459484659
    Change-Id: I56d7525992083e928fe3eaf3585812c030aaf627
    Signed-off-by: Bart Van Assche <bvanassche@google.com>

--- include/linux/bits.h
+++ include/linux/bits.h
@@ -18,7 +18,7 @@
  * position @h. For example
  * GENMASK_ULL(39, 21) gives us the 64bit vector 0x000000ffffe00000.
  */
-#if !defined(__ASSEMBLY__)
+#if !defined(__ASSEMBLY__) && !defined(__CODESONAR__)
 #include <linux/build_bug.h>
 #define GENMASK_INPUT_CHECK(h, l) \
 	(BUILD_BUG_ON_ZERO(__builtin_choose_expr( \
