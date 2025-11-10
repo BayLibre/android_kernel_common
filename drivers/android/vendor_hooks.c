96d232ff344d77b4fc0743b22319a85d11f7bc3d
jiayingrui <jiayingrui@oppo.com>
2025-11-10 23:20:17 +0800

    ANDROID: vendor_hooks: add hook in loop_queue_work
    This hook is used to optimize the performance of some important IOs.
    
    Bug: 459346147
    Change-Id: I8b0bfdfc54fa16cba34849dbd1383644c8ee63f3
    Signed-off-by: jiayingrui <jiayingrui@oppo.com>
    (cherry picked from commit 8c6fbee830490fab45da6ef6c408a851d9cff88e)

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -694,4 +694,15 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mm_customize_lru_add_dst);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mm_isolate_priv_lru);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mm_customize_pgdat_balanced);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mm_customize_file_is_tiny);
+<<<<<<< HEAD   (e1dbe1ef2a9168e477c1aeeb5dd8f98b7efdb197 UPSTREAM: tracing: Add down_write(trace_event_sem) when addi)
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_bio_add_page_merge_bypass);
+||||||| BASE   (aa8a9034d8a0e18d37fff6c6053f470be77637ae ANDROID: loop: export loop_process_cmd_list)
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_nr_pcp_alloc);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_pcp_alloc_factor_adjust);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_fas_gpu_qos_update_tracer);
+=======
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_nr_pcp_alloc);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_pcp_alloc_factor_adjust);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_fas_gpu_qos_update_tracer);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_loop_skip_queue_work);
+>>>>>>> CHANGE (8c6fbee830490fab45da6ef6c408a851d9cff88e ANDROID: vendor_hooks: add hook in loop_queue_work)
--- include/trace/hooks/blk.h
+++ include/trace/hooks/blk.h
@@ -51,9 +51,17 @@ DECLARE_HOOK(android_vh_check_set_ioprio,
 	TP_PROTO(struct bio *bio),
 	TP_ARGS(bio));
 
+<<<<<<< HEAD   (e1dbe1ef2a9168e477c1aeeb5dd8f98b7efdb197 UPSTREAM: tracing: Add down_write(trace_event_sem) when addi)
 DECLARE_HOOK(android_vh_bio_add_page_merge_bypass,
 	TP_PROTO(struct bio *bio, bool *skip),
 	TP_ARGS(bio, skip));
+||||||| BASE   (aa8a9034d8a0e18d37fff6c6053f470be77637ae ANDROID: loop: export loop_process_cmd_list)
+=======
+struct request;
+DECLARE_HOOK(android_vh_loop_skip_queue_work,
+        TP_PROTO(struct request *req, bool *skip),
+        TP_ARGS(req, skip));
+>>>>>>> CHANGE (8c6fbee830490fab45da6ef6c408a851d9cff88e ANDROID: vendor_hooks: add hook in loop_queue_work)
 
 #endif /* _TRACE_HOOK_BLK_H */
 /* This part must be outside protection */
