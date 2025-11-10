f8ec0281ab0207013d961fdad0c20c5eb04d1a0f
Jaewon Kim <jaewon31.kim@samsung.com>
2025-11-10 22:51:23 +0900

    ANDROID: mm: add vendor hook to oom victim and disable
    The oom killer disable timeout may happen if the oom victim tasks did
    not die. To get some clues, add vendor hook to leave oom victim
    information.
    
    Bug: 459380738
    Change-Id: I2ac17a839bcb678bb241f6eb0d1eb3ae3dbbbb6d
    Signed-off-by: Jaewon Kim <jaewon31.kim@samsung.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -661,3 +661,5 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_keep_reclaimed_folio);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_clear_reclaimed_folio);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_evict_folios_bypass);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_may_unmap_folio);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_exit_oom_victim);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_oom_killer_disable);
