907e1883182257a1f9336db10a8d6049fe213b71
Henry Dong <donghenry@google.com>
2025-11-11 09:17:32 +0000

    ANDROID: add gup_longterm_locked vendor hook
    Add vendor hook for pin long term pin pagie fail
    debug.
    
    Bug: 457541695
    
    Change-Id: Icc0a172d51815811fbce77a566c55a8923cf5a41
    Signed-off-by: Henry Dong <donghenry@google.com>

--- mm/gup.c
+++ mm/gup.c
@@ -2141,6 +2141,9 @@ static long __gup_longterm_locked(struct mm_struct *mm,
 		rc = check_and_migrate_movable_pages(nr_pinned_pages, pages);
 	} while (rc == -EAGAIN);
 	memalloc_pin_restore(flags);
+	/* Debugging for pin user pages in memory fail with FOLL_LONGTERM flag */
+	if (rc)
+		trace_android_rvh_gup_longterm_locked(rc, nr_pinned_pages, start, nr_pages, pages);
 	return rc ? rc : nr_pinned_pages;
 }
 
