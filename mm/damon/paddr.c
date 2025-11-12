544389327363c42d80f6f612e7a0ec636256426f
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2025-11-12 13:53:54 -0800

    Revert "BACKPORT: FROMGIT: mm: always call rmap_walk() on locked..."
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/393c7528-da63-4ed0-bcf8-8a86793fdf4b). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: Ie925284d5b479ef394aaffb4239773741b36d84b
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com
    Bug: 422561794

--- mm/damon/paddr.c
+++ mm/damon/paddr.c
@@ -40,17 +40,22 @@ static void damon_folio_mkold(struct folio *folio)
 		.rmap_one = damon_folio_mkold_one,
 		.anon_lock = folio_lock_anon_vma_read,
 	};
+	bool need_lock;
 
 	if (!folio_mapped(folio) || !folio_raw_mapping(folio)) {
 		folio_set_idle(folio);
 		return;
 	}
 
-	if (!folio_trylock(folio))
+	need_lock = !folio_test_anon(folio) || folio_test_ksm(folio);
+	if (need_lock && !folio_trylock(folio))
 		return;
 
 	rmap_walk(folio, &rwc);
-	folio_unlock(folio);
+
+	if (need_lock)
+		folio_unlock(folio);
+
 }
 
 static void damon_pa_mkold(unsigned long paddr)
@@ -122,6 +127,7 @@ static bool damon_folio_young(struct folio *folio)
 		.rmap_one = damon_folio_young_one,
 		.anon_lock = folio_lock_anon_vma_read,
 	};
+	bool need_lock;
 
 	if (!folio_mapped(folio) || !folio_raw_mapping(folio)) {
 		if (folio_test_idle(folio))
@@ -130,11 +136,14 @@ static bool damon_folio_young(struct folio *folio)
 			return true;
 	}
 
-	if (!folio_trylock(folio))
+	need_lock = !folio_test_anon(folio) || folio_test_ksm(folio);
+	if (need_lock && !folio_trylock(folio))
 		return false;
 
 	rmap_walk(folio, &rwc);
-	folio_unlock(folio);
+
+	if (need_lock)
+		folio_unlock(folio);
 
 	return accessed;
 }
