4251bdf06be477db6d94b267a0c62f9c0ef894af
Aashish Sharma <shraash@google.com>
2025-11-13 00:05:14 -0800

    ANDROID: pkvm: x86: Don't reserve memory for pv-iommu
    With paravirtualized implementation for IOMMU, host
    dynamically allocates and donates memory to pKVM for
    IOMMU's structures. So we don't need to reserve a
    fixed memory during the initial setup.
    
    This change reduce the pKVM footprint about 2GB on
    16GB systems.
    E.g. Without this change:
    [    0.026284] kvm [0]: Reserved 214 MiB at 0x3d1600000
    With this change:
    [    0.026387] kvm [0]: Reserved 114 MiB at 0x469600000
    
    Bug: 391542119
    Upstream-Task: 402758258
    Test: Build and Boot
    
    Change-Id: I120c35288957aa5e0b18eb9279b34fcd6f8dd37b
    Signed-off-by: Aashish Sharma <shraash@google.com>

--- arch/x86/kvm/vmx/pkvm/hyp/iommu.c
+++ arch/x86/kvm/vmx/pkvm/hyp/iommu.c
@@ -121,10 +121,13 @@ int pkvm_init_iommu(unsigned long mem_base, unsigned long nr_pages)
 {
 	struct pkvm_iommu_info *info = &pkvm_hyp->iommu_infos[0];
 	struct pkvm_iommu *piommu = &iommus[0];
-	int i, ret = hyp_pool_init(&iommu_pool, mem_base >> PAGE_SHIFT, nr_pages, 0);
+	int i, ret;
 
+#ifndef CONFIG_PKVM_INTEL_PVIOMMU
+	ret =  hyp_pool_init(&iommu_pool, mem_base >> PAGE_SHIFT, nr_pages, 0);
 	if (ret)
 		return ret;
+#endif
 
 	for (i = 0; i < PKVM_MAX_IOMMU_NUM; piommu++, info++, i++) {
 		struct viommu_reg *vreg = &piommu->viommu.vreg;
