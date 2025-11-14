7c5c18ead59eaf6934b945fed9664d81462ce6dc
Vincent Donnefort <vdonnefort@google.com>
2025-11-14 15:55:53 +0000

    ANDROID: KVM: arm64: device_register_power_handler for pKVM modules
    When a device is assigned to a protected VM, it is expected from the
    firmware to lock the power management to prevent any attempt from a
    malicious host to tamper the device. In preparation for runtime PM
    support for assigned device, add a power handler for pKVM modules.
    
    This handler will be called whenever the p-guest is querying a power-on
    or off for the device, described by the physical base address of its
    MMIO region.
    
    Bug: 458241298
    Bug: 460349550
    Bug: 461757236
    Change-Id: I7a5cc24c9254460c4d8c3c358a92a7f7dc2a57e5
    Signed-off-by: Vincent Donnefort <vdonnefort@google.com>

--- arch/arm64/kvm/hyp/nvhe/modules.c
+++ arch/arm64/kvm/hyp/nvhe/modules.c
@@ -318,6 +318,7 @@ const struct pkvm_module_ops module_ops = {
 	.pkvm_unuse_dma = iommu_pkvm_unuse_dma,
 	.init_hvc_pd = pkvm_init_hvc_pd,
 	.device_register_reset = pkvm_device_register_reset,
+	.device_register_power_handler = pkvm_device_register_power_handler,
 	.iommu_register_pviommu_drv = kvm_iommu_register_pviommu_drv,
 	.register_guest_trng_ops = __register_guest_trng_ops,
 };
