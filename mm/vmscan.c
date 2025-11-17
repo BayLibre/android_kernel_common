009a20e9c11b0c10745dea778e927b60e72c884e
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2025-11-17 15:26:47 -0800

    Revert "ANDROID: Fix shrink_many UAF of memcg"
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/a7e72f7e-c766-4331-8d12-ba84b3667698). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: I8800b692ebf629977904c92ce1f343c3bbb37ef0
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -5507,7 +5507,6 @@ static void shrink_many(struct pglist_data *pgdat, struct scan_control *sc)
 	bin = first_bin = get_random_u32_below(MEMCG_NR_BINS);
 restart:
 	op = 0;
-	lruvec = NULL;
 	memcg = NULL;
 	gen = get_memcg_gen(READ_ONCE(pgdat->memcg_lru.seq));
 
@@ -5549,12 +5548,10 @@ restart:
 	if (op)
 		lru_gen_rotate_memcg(lruvec, op);
 
-	if (lruvec && should_abort_scan(lruvec, sc)) {
-		mem_cgroup_put(memcg);
-		return;
-	}
-
 	mem_cgroup_put(memcg);
+
+	if (lruvec && should_abort_scan(lruvec, sc))
+		return;
 
 	/* restart if raced with lru_gen_rotate_memcg() */
 	if (gen != get_nulls_value(pos))
