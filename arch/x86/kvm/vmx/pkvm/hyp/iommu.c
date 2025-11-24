4e5c34efe844f85e07359b19e2e1ecb40352697d
Vineeth Pillai <vineethrp@google.com>
2025-11-24 16:19:17 -0500

    ANDROID: pkvm: x86: Introduce CONFIG_PKVM_INTEL_PVIOMMU
    Introduce for pv iommu implementation framework and wrap it in
    CONFIG_PKVM_INTEL_PVIOMMU. This patch just uses the translation
    structures created by host iommu driver and not protection is enforced.
    Future patches in this series builds the protection mechanism by using
    hypercalls.
    
    Shadow iommu implementation is kept in place for testing and
    performance comparison. Might be removed in future.
    
    Bug: 391542119
    Upstream-Task: 402758258
    Test: Build and Boot
    
    Change-Id: I9b46552605173996cfae2da597793f71a1eca63f
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

--- arch/x86/kvm/vmx/pkvm/hyp/iommu.c
+++ arch/x86/kvm/vmx/pkvm/hyp/iommu.c
@@ -119,6 +119,12 @@ int pkvm_init_iommu(unsigned long mem_base, unsigned long nr_pages)
 	if (ret)
 		return ret;
 
+#ifdef CONFIG_PKVM_INTEL_PVIOMMU
+	pkvm_dbg("pkvm: %s: Initializing iommus in paravirt mode\n", __func__);
+#else
+	pkvm_dbg("pkvm: %s: Initializing iommus in shadow mode\n", __func__);
+#endif
+
 	for (i = 0; i < PKVM_MAX_IOMMU_NUM; piommu++, info++, i++) {
 		struct viommu_reg *vreg = &piommu->viommu.vreg;
 
@@ -574,24 +580,29 @@ static void enable_translation(struct pkvm_iommu *iommu)
  */
 static int activate_iommu(struct pkvm_iommu *iommu)
 {
-	unsigned long vaddr = 0, vaddr_end = IOMMU_MAX_VADDR;
 	int ret;
 
+#ifndef CONFIG_PKVM_INTEL_PVIOMMU
 	ret = initialize_iommu_pgt(iommu);
 	if (ret)
 		return ret;
 
-	ret = sync_shadow_id(iommu, vaddr, vaddr_end, 0);
+	ret = sync_shadow_id(iommu, 0, IOMMU_MAX_VADDR, 0);
 	if (ret)
 		return ret;
+#endif
 
 	set_root_table(iommu);
 
 	ret = pkvm_host_ept_unmap((unsigned long)iommu->iommu.reg_phys,
 			     (unsigned long)iommu->iommu.reg_phys,
 			     iommu->iommu.reg_size);
-	if (ret)
-		goto free_shadow;
+	if (ret) {
+#ifndef CONFIG_PKVM_INTEL_PVIOMMU
+		free_shadow_id(iommu, 0, IOMMU_MAX_VADDR);
+#endif
+		return ret;
+	}
 
 	iommu->activated = true;
 	root_tbl_walk(iommu);
@@ -599,10 +610,6 @@ static int activate_iommu(struct pkvm_iommu *iommu)
 	pkvm_dbg("pkvm: %s: iommu%d activated\n", __func__, iommu->iommu.seq_id);
 
 	return 0;
-
-free_shadow:
-	free_shadow_id(iommu, vaddr, vaddr_end);
-	return ret;
 }
 
 static void handle_qi_submit(struct pkvm_iommu *iommu, void *vdesc, int vhead, int count)
@@ -661,7 +668,7 @@ static int handle_qi_invalidation(struct pkvm_iommu *iommu, unsigned long val)
 	int len = IQ_DESC_LEN(viommu_iqa);
 	int shift = IQ_DESC_SHIFT(viommu_iqa);
 	int head = viommu->vreg.iq_head >> shift;
-	int count, i, ret = 0;
+	int count, ret = 0;
 	int *desc_status;
 	void *desc;
 
@@ -669,12 +676,14 @@ static int handle_qi_invalidation(struct pkvm_iommu *iommu, unsigned long val)
 	desc = pkvm_phys_to_virt(IQ_DESC_BASE_PHYS(viommu_iqa));
 	count = ((val >> shift) + len - head) % len;
 
-	for (i = 0; i < count; i++) {
+#ifndef CONFIG_PKVM_INTEL_PVIOMMU
+	for (int i = 0; i < count; i++) {
 		viommu->vreg.iq_head = ((head + i) % len) << shift;
 		ret = handle_descriptor(iommu, desc + viommu->vreg.iq_head);
 		if (ret)
 			break;
 	}
+#endif
 	/* update iq_head */
 	viommu->vreg.iq_head = val;
 
@@ -702,16 +711,17 @@ static int handle_qi_invalidation(struct pkvm_iommu *iommu, unsigned long val)
 
 static void handle_gcmd_te(struct pkvm_iommu *iommu, bool en)
 {
-	unsigned long vaddr = 0, vaddr_end = MAX_NUM_OF_ADDRESS_SPACE(iommu);
 	struct pkvm_viommu *viommu = &iommu->viommu;
 
 	if (en) {
 		viommu->vreg.gsts |= DMA_GSTS_TES;
+#ifndef CONFIG_PKVM_INTEL_PVIOMMU
 		/*
 		 * Sync shadow id table to emulate Translation enable.
 		 */
-		if (sync_shadow_id(iommu, vaddr, vaddr_end, 0))
+		if (sync_shadow_id(iommu, 0, MAX_NUM_OF_ADDRESS_SPACE(iommu), 0))
 			return;
+#endif
 
 		enable_translation(iommu);
 
@@ -719,13 +729,15 @@ static void handle_gcmd_te(struct pkvm_iommu *iommu, bool en)
 		goto out;
 	}
 
+#ifndef CONFIG_PKVM_INTEL_PVIOMMU
 	/*
 	 * Free shadow to emulate Translation disable.
 	 *
 	 * Not really disable translation as still
 	 * need to protect agains the device.
 	 */
-	free_shadow_id(iommu, vaddr, vaddr_end);
+	free_shadow_id(iommu, 0, MAX_NUM_OF_ADDRESS_SPACE(iommu));
+#endif
 	viommu->vreg.gsts &= ~DMA_GSTS_TES;
 	pkvm_dbg("pkvm: %s: disable TE\n", __func__);
 out:
@@ -741,6 +753,7 @@ static void handle_gcmd_srtp(struct pkvm_iommu *iommu)
 {
 	struct viommu_reg *vreg = &iommu->viommu.vreg;
 	struct pkvm_pgtable *vpgt = &iommu->viommu.pgt;
+	struct pkvm_pgtable *pgt = &iommu->pgt;
 
 	if (!iommu->viommu.vreg.rta) {
 		pkvm_err("pkvm: %s: iommu%d: host RTADDR_REG not set",
@@ -755,7 +768,11 @@ static void handle_gcmd_srtp(struct pkvm_iommu *iommu)
 	vreg->gsts &= ~DMA_GSTS_RTPS;
 
 	/* Set the root table phys address from vreg */
-	vpgt->root_pa = vreg->rta & VTD_PAGE_MASK;
+	if (IS_ENABLED(CONFIG_PKVM_INTEL_PVIOMMU)) {
+		pgt->root_pa = vreg->rta & VTD_PAGE_MASK;
+	} else {
+		vpgt->root_pa = vreg->rta & VTD_PAGE_MASK;
+	}
 
 	pkvm_dbg("pkvm: %s: set SRTP val 0x%llx\n", __func__, vreg->rta);
 
@@ -1041,6 +1058,7 @@ static struct pkvm_iommu *bdf_pasid_to_iommu(u16 bdf, u32 pasid)
 	return find;
 }
 
+#ifndef CONFIG_PKVM_INTEL_PVIOMMU
 /*
  * pkvm_iommu_sync() - Sync IOMMU context/pasid entry according to a ptdev
  *
@@ -1096,6 +1114,7 @@ int pkvm_iommu_sync(u16 bdf, u32 pasid)
 	pkvm_put_ptdev(ptdev);
 	return ret;
 }
+#endif
 
 bool pkvm_iommu_coherency(u16 bdf, u32 pasid)
 {
