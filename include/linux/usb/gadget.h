b6751a077cf09fe6b15636d37f5d61d8e88bc5ba
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2025-11-24 07:29:21 -0800

    Revert "ANDROID: KABI: fix up abi change in struct usb_request"
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/d9a60577-5c7b-46a4-8964-ff66a9a620e7). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: I0edc809597cb3174606ed5aa1b1aa9d8bdd69b6a
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com

--- include/linux/usb/gadget.h
+++ include/linux/usb/gadget.h
@@ -97,6 +97,7 @@ struct usb_ep;
  */
 
 struct usb_request {
+	struct usb_ep		*ep;
 	void			*buf;
 	unsigned		length;
 	dma_addr_t		dma;
@@ -122,7 +123,7 @@ struct usb_request {
 	int			status;
 	unsigned		actual;
 
-	ANDROID_KABI_USE(1, struct usb_ep *ep);
+	ANDROID_KABI_RESERVE(1);
 };
 
 /*-------------------------------------------------------------------------*/
