0a278708b2494f345ac6b268d4ca78ff0251a3a6
daiyang5 <daiyang5@xiaomi.com>
2025-12-05 15:23:08 +0800

    ANDROID: vendor_hook: Add hooks to maintain folios that are under writeback separately.
    two Vendor hooks add:
        android_vh_handle_folio_writeback
        android_vh_reclaim_before_kswapd
    
    Add vendor hook point in shrink_folio_list to maintain folios that are under writeback separately and not return them to the inactive list, so as to quick recycling later in kswapd.
    
    Change-Id: Ie7dfcc4f4119046a6be71abcefb3ba36f07b4c9f
    Signed-off-by: daiyang5 <daiyang5@xiaomi.com>

--- include/trace/hooks/vmscan.h
+++ include/trace/hooks/vmscan.h
@@ -12,6 +12,12 @@
 DECLARE_RESTRICTED_HOOK(android_rvh_set_balance_anon_file_reclaim,
 			TP_PROTO(bool *balance_anon_file_reclaim),
 			TP_ARGS(balance_anon_file_reclaim), 1);
+DECLARE_HOOK(android_vh_handle_folio_writeback,
+	TP_PROTO(struct folio *folio, bool *bypass),
+	TP_ARGS(folio, bypass));
+DECLARE_HOOK(android_vh_reclaim_before_kswapd,
+	TP_PROTO(unsigned long *nr_reclaimed),
+	TP_ARGS(nr_reclaimed));
 #endif /* _TRACE_HOOK_VMSCAN_H */
 /* This part must be outside protection */
 #include <trace/define_trace.h>
