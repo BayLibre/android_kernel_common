de2e35902d9d6c527beea640c804616d7b8add6b
Andrei Kuchynski <akuchynski@google.com>
2025-12-10 14:07:17 +0000

    Revert "ANDROID: usb: typec: Implement mode selection"
    This reverts commit f49f8d90b43e2a0aa7a011c88529f38c409fbf0b.
    The mode selection feature operates effectively in TCPM-based systems but
    results in deadlock problems within UCSI systems. The updated version of
    mode selection functions correctly across both TCPM and UCSI systems.
    
    Bug: 382333169
    Upstream-Task: 441243047
    Change-Id: I5346cac6e4df453c5fe76ff0a3b069e9b8ce0107
    Signed-off-by: Andrei Kuchynski <akuchynski@google.com>

--- drivers/usb/typec/class.h
+++ drivers/usb/typec/class.h
@@ -10,7 +10,6 @@
 struct typec_mux;
 struct typec_switch;
 struct usb_device;
-struct altmode_selection;
 
 struct typec_plug {
 	struct device			dev;
@@ -43,7 +42,6 @@ struct typec_partner {
 	u8				usb_capability;
 
 	struct usb_power_delivery	*pd;
-	struct altmode_selection	*sel;
 
 	void (*attach)(struct typec_partner *partner, struct device *dev);
 	void (*deattach)(struct typec_partner *partner, struct device *dev);
