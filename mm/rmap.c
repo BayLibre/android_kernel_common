5031b585f43ce4a9a9c4735efb9b9cb2f6be36df
zhanyuan hu <huzhanyuan@xiaomi.com>
2025-12-12 00:48:41 -0800

    move trace_android_vh_look_around to right place
    acording the change: https://android-review.googlesource.com/c/kernel/common/+/3315014
    
    trace_android_vh_look_around should work on lru_gen disable situation.
    
    Bug: 468214566
    Change-Id: I0675f686aa5ad182591663b7fce1a0ace020f23a
    Signed-off-by: huzhanyuan  <huzhanyuan@xiaomi.com>

--- mm/rmap.c
+++ mm/rmap.c
@@ -867,10 +867,10 @@ static bool folio_referenced_one(struct folio *folio,
 		}
 
 		if (lru_gen_enabled() && pvmw.pte) {
-			trace_android_vh_look_around(&pvmw, folio, vma, &referenced);
 			if (lru_gen_look_around(&pvmw))
 				referenced++;
 		} else if (pvmw.pte) {
+            trace_android_vh_look_around(&pvmw, folio, vma, &referenced);
 			if (ptep_clear_flush_young_notify(vma, address,
 						pvmw.pte))
 				referenced++;
