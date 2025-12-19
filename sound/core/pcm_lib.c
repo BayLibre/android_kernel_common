8907f5ea28a1e1b98eb80385e04c8fd93d49907b
Isha Padiyar <padiyar@qti.qualcomm.com>
2025-12-19 16:39:10 +0530

    ANDROID: ASoC: core - add hostless DAI support using vendor hook
    Allow DAI's to be hostless so that no PCM data is sent between DAI
    and CPU. This allows for power savings as there is no DMA or CPU
    interaction required.
    
    [Re-implemented commit 7fe5afb29120d4c6d57b19bd62bd8b1ad3eaaa61
    using vendor hooks to avoid core kernel modifications]
    
    Bug: 487170912
    Change-Id: I1520266ddb31e2576b03ebb6c3c3dc128b5d5db3
    Signed-off-by: Isha Padiyar <padiyar@qti.qualcomm.com>

--- sound/core/pcm_lib.c
+++ sound/core/pcm_lib.c
@@ -2197,7 +2197,7 @@ static int pcm_sanity_check(struct snd_pcm_substream *substream)
 	struct snd_pcm_runtime *runtime;
 	if (PCM_RUNTIME_CHECK(substream))
 		return -ENXIO;
-	runtime = substream->runtime;
+        runtime = substream->runtime;
 	if (snd_BUG_ON(!substream->ops->copy && !runtime->dma_area))
 		return -EINVAL;
 	if (runtime->state == SNDRV_PCM_STATE_OPEN)
