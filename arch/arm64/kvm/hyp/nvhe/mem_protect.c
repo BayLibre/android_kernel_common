690e1e63fa2cb8c1dc7d6a675e9ebb4ce2078171
Mostafa Saleh <smostafa@google.com>
2025-12-30 11:58:16 +0000

    ANDRIOD: KVM: arm64: Don't use S2FWB for VM with devices
    In case the VM uses non-coherent DMA, it will not be able to map
    memory as NC because of FWB.
    Workaround this for now, by removing the feature from such VMs.
    
    Bug: 448427307
    Bug: 368611573
    Change-Id: Iffe14e5db89a0c1ff115090b9338bca6eddfd702
    Signed-off-by: Mostafa Saleh <smostafa@google.com>

--- arch/arm64/kvm/hyp/nvhe/mem_protect.c
+++ arch/arm64/kvm/hyp/nvhe/mem_protect.c
@@ -335,7 +335,7 @@ static void invalidate_icache_guest_page(void *va, size_t size)
 	}
 }
 
-int kvm_guest_prepare_stage2(struct pkvm_hyp_vm *vm, void *pgd)
+int kvm_guest_prepare_stage2(struct pkvm_hyp_vm *vm, void *pgd, bool has_devices)
 {
 	struct kvm_s2_mmu *mmu = &vm->kvm.arch.mmu;
 	unsigned long nr_pages;
@@ -362,7 +362,8 @@ int kvm_guest_prepare_stage2(struct pkvm_hyp_vm *vm, void *pgd)
 	};
 
 	guest_lock_component(vm);
-	ret = __kvm_pgtable_stage2_init(mmu->pgt, mmu, &vm->mm_ops, 0,
+	ret = __kvm_pgtable_stage2_init(mmu->pgt, mmu, &vm->mm_ops,
+					has_devices ? KVM_PGTABLE_S2_NOFWB : 0,
 					&guest_s2_pte_ops);
 	guest_unlock_component(vm);
 	if (ret)
@@ -2990,7 +2991,7 @@ static void init_selftest_vm(void *virt)
 	int i;
 
 	selftest_vm.kvm.arch.mmu.vtcr = host_mmu.arch.mmu.vtcr;
-	WARN_ON(kvm_guest_prepare_stage2(&selftest_vm, virt));
+	WARN_ON(kvm_guest_prepare_stage2(&selftest_vm, virt, false));
 
 	for (i = 0; i < pkvm_selftest_pages(); i++) {
 		if (p[i].refcount)
--- arch/arm64/kvm/hyp/nvhe/pkvm.c
+++ arch/arm64/kvm/hyp/nvhe/pkvm.c
@@ -843,6 +843,7 @@ int __pkvm_init_vm(struct kvm *host_kvm, unsigned long pgd_hva)
 	void *pgd = NULL;
 	size_t pgd_size;
 	int ret;
+	bool has_devices = false;
 
 	ret = hyp_pin_shared_mem(host_kvm, host_kvm + 1);
 	if (ret)
@@ -883,11 +884,11 @@ int __pkvm_init_vm(struct kvm *host_kvm, unsigned long pgd_hva)
 	if (ret < 0)
 		goto err_unlock;
 
-	ret = kvm_guest_prepare_stage2(hyp_vm, pgd);
+	ret = pkvm_pviommu_finalise(hyp_vm, &has_devices);
 	if (ret)
 		goto err_remove_vm_table_entry;
 
-	ret = pkvm_pviommu_finalise(hyp_vm);
+	ret = kvm_guest_prepare_stage2(hyp_vm, pgd, has_devices);
 	if (ret)
 		goto err_remove_vm_table_entry;
 
