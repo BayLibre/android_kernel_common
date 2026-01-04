b9a5102adfb9091e3c889775eedbbce5fb82a23e
Chao Yu <chao@kernel.org>
2026-01-04 10:07:18 +0800

    FROMGIT: f2fs: trace elapsed time for cp_rwsem lock
    Use f2fs_{down,up}_read_trace for cp_rwsem to trace lock elapsed time.
    
    Bug: 408202989
    (cherry picked from commit 66e9e0d55d117a7de2c00a9a06fb943ead56e1c2
    https://git.kernel.org/pub/scm/linux/kernel/git/jaegeuk/f2fs.git dev)
    Link: https://lore.kernel.org/linux-f2fs-devel/20260104020729.1064529-3-chao@kernel.org
    Signed-off-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    (cherry picked from commit 66e9e0d55d117a7de2c00a9a06fb943ead56e1c2)
    [Chao: fix code conflict]
    Signed-off-by: Chao Yu <chao@kernel.org>
    Cherrypick-From: https://android-review.googlesource.com/q/commit:bce1234d1f7a2f18a444373a0fe8156b798bd3af
    Merged-In: I56d82c2dacd11aaf4d1bd9f4cb27a33683995fcc
    Change-Id: I56d82c2dacd11aaf4d1bd9f4cb27a33683995fcc

--- fs/f2fs/compress.c
+++ fs/f2fs/compress.c
@@ -1283,6 +1283,7 @@ static int f2fs_write_compressed_pages(struct compress_ctx *cc,
 	struct dnode_of_data dn;
 	struct node_info ni;
 	struct compress_io_ctx *cic;
+	struct f2fs_lock_context lc;
 	pgoff_t start_idx = start_idx_of_cluster(cc);
 	unsigned int last_index = cc->cluster_size - 1;
 	loff_t psize;
@@ -1302,7 +1303,7 @@ static int f2fs_write_compressed_pages(struct compress_ctx *cc,
 		 * the below discard race condition.
 		 */
 		f2fs_down_read(&sbi->node_write);
-	} else if (!f2fs_trylock_op(sbi)) {
+	} else if (!f2fs_trylock_op(sbi, &lc)) {
 		goto out_free;
 	}
 
@@ -1428,7 +1429,7 @@ unlock_continue:
 	if (quota_inode)
 		f2fs_up_read(&sbi->node_write);
 	else
-		f2fs_unlock_op(sbi);
+		f2fs_unlock_op(sbi, &lc);
 
 	spin_lock(&fi->i_size_lock);
 	if (fi->last_disk_size < psize)
@@ -1457,7 +1458,7 @@ out_unlock_op:
 	if (quota_inode)
 		f2fs_up_read(&sbi->node_write);
 	else
-		f2fs_unlock_op(sbi);
+		f2fs_unlock_op(sbi, &lc);
 out_free:
 	for (i = 0; i < cc->valid_nr_cpages; i++) {
 		f2fs_compress_free_page(cc->cpages[i]);
@@ -1504,6 +1505,7 @@ static int f2fs_write_raw_pages(struct compress_ctx *cc,
 {
 	struct address_space *mapping = cc->inode->i_mapping;
 	struct f2fs_sb_info *sbi = F2FS_M_SB(mapping);
+	struct f2fs_lock_context lc;
 	int submitted, compr_blocks, i;
 	int ret = 0;
 
@@ -1522,7 +1524,7 @@ static int f2fs_write_raw_pages(struct compress_ctx *cc,
 
 	/* overwrite compressed cluster w/ normal cluster */
 	if (compr_blocks > 0)
-		f2fs_lock_op(sbi);
+		f2fs_lock_op(sbi, &lc);
 
 	for (i = 0; i < cc->cluster_size; i++) {
 		if (!cc->rpages[i])
@@ -1577,7 +1579,7 @@ continue_unlock:
 
 out:
 	if (compr_blocks > 0)
-		f2fs_unlock_op(sbi);
+		f2fs_unlock_op(sbi, &lc);
 
 	f2fs_balance_fs(sbi, true);
 	return ret;
