373f3943476534298dc4a6667adbdcefeae857fb
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2026-01-09 10:00:51 -0800

    Revert "Merge 9551a26f17d9 ("Merge tag 'loongarch-6.19' of git:/..."
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/71e4e09b-5a1b-4236-9489-db3b67ee3161). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: I54b7934972b531aeba1859a53b48488868f2c867
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com

--- arch/loongarch/include/asm/vdso/gettimeofday.h
+++ arch/loongarch/include/asm/vdso/gettimeofday.h
@@ -12,8 +12,6 @@
 #include <asm/unistd.h>
 #include <asm/vdso/vdso.h>
 
-#ifdef CONFIG_GENERIC_GETTIMEOFDAY
-
 #define VDSO_HAS_CLOCK_GETRES		1
 
 static __always_inline long gettimeofday_fallback(
@@ -90,8 +88,6 @@ static inline bool loongarch_vdso_hres_capable(void)
 	return true;
 }
 #define __arch_vdso_hres_capable loongarch_vdso_hres_capable
-
-#endif /* CONFIG_GENERIC_GETTIMEOFDAY */
 
 #endif /* !__ASSEMBLER__ */
 
