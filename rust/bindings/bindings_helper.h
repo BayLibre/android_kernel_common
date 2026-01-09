b36e7a4444250d16f307a0ae07b03f2bb30653fa
Alice Ryhl <aliceryhl@google.com>
2026-01-09 10:06:38 +0000

    netlink
    Change-Id: I03f99459767b937cbc060374b442d3f86bb1f860
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>
    (cherry picked from commit 11b9adec67d6b743511cdc1038e5a66e8148cbf7)
    Signed-off-by: Carlos Llamas <cmllamas@google.com>

--- rust/bindings/bindings_helper.h
+++ rust/bindings/bindings_helper.h
@@ -89,6 +89,8 @@
 #include <linux/wait.h>
 #include <linux/workqueue.h>
 #include <linux/xarray.h>
+#include <net/genetlink.h>
+#include <net/netlink.h>
 #include <trace/events/rust_sample.h>
 #include <uapi/linux/falloc.h>
 #include <uapi/linux/sched/types.h>
