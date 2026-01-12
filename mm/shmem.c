56d7d1fa0bab8c8eb16f9760f1dbe9affc6fab10
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2026-01-12 08:20:33 -0800

    Revert "Merge b69492161c05 ("tpm: Cap the number of PCR banks") ..."
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/e3e55299-5326-4f0f-87dd-778b2fa1fce5). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: Ice2d07a186c8ec12754bb6631ee8a4478c66685a
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com

--- mm/shmem.c
+++ mm/shmem.c
@@ -3777,7 +3777,6 @@ static int shmem_rename2(struct mnt_idmap *idmap,
 {
 	struct inode *inode = d_inode(old_dentry);
 	int they_are_dirs = S_ISDIR(inode->i_mode);
-	bool had_offset = false;
 	int error;
 
 	if (flags & ~(RENAME_NOREPLACE | RENAME_EXCHANGE | RENAME_WHITEOUT))
@@ -3790,23 +3789,16 @@ static int shmem_rename2(struct mnt_idmap *idmap,
 	if (!simple_empty(new_dentry))
 		return -ENOTEMPTY;
 
-	error = simple_offset_add(shmem_get_offset_ctx(new_dir), new_dentry);
-	if (error == -EBUSY)
-		had_offset = true;
-	else if (unlikely(error))
-		return error;
-
 	if (flags & RENAME_WHITEOUT) {
 		error = shmem_whiteout(idmap, old_dir, old_dentry);
-		if (error) {
-			if (!had_offset)
-				simple_offset_remove(shmem_get_offset_ctx(new_dir),
-						     new_dentry);
+		if (error)
 			return error;
-		}
 	}
 
-	simple_offset_rename(old_dir, old_dentry, new_dir, new_dentry);
+	error = simple_offset_rename(old_dir, old_dentry, new_dir, new_dentry);
+	if (error)
+		return error;
+
 	if (d_really_is_positive(new_dentry)) {
 		(void) shmem_unlink(new_dir, new_dentry);
 		if (they_are_dirs) {
--- net/handshake/request.c
+++ net/handshake/request.c
@@ -325,11 +325,7 @@ bool handshake_req_cancel(struct sock *sk)
 
 	hn = handshake_pernet(net);
 	if (hn && remove_pending(hn, req)) {
-		/* Request hadn't been accepted - mark cancelled */
-		if (test_and_set_bit(HANDSHAKE_F_REQ_COMPLETED, &req->hr_flags)) {
-			trace_handshake_cancel_busy(net, req, sk);
-			return false;
-		}
+		/* Request hadn't been accepted */
 		goto out_true;
 	}
 	if (test_and_set_bit(HANDSHAKE_F_REQ_COMPLETED, &req->hr_flags)) {
