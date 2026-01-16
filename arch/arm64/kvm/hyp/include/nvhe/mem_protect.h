c491614b151f8271ff87103ca17d5f744c8b044f
Vincent Donnefort <vdonnefort@google.com>
2026-01-16 16:50:52 +0000

    ANDROID: KVM: arm64: Use a separated pool for host IOMMU mappings
    Change-Id: Ia0b045511cbfdd81b55147817bd3146924537b49
    Signed-off-by: Vincent Donnefort <vdonnefort@google.com>

--- arch/arm64/kvm/hyp/include/nvhe/mem_protect.h
+++ arch/arm64/kvm/hyp/include/nvhe/mem_protect.h
@@ -90,10 +90,10 @@ unsigned long __pkvm_host_stage2_reclaimable(u8 order);
 s64 __pkvm_host_stage2_reclaim(u8 order);
 
 bool addr_is_memory(phys_addr_t phys);
-int host_stage2_idmap_locked(phys_addr_t addr, u64 size, enum kvm_pgtable_prot prot);
+int host_stage2_idmap_locked(phys_addr_t addr, u64 size, enum kvm_pgtable_prot prot, bool is_memory);
 int host_stage2_set_owner_locked(phys_addr_t addr, u64 size, u8 owner_id);
 int host_stage2_unmap_reg_locked(phys_addr_t start, u64 size);
-int kvm_host_prepare_stage2(void *pgt_pool_base);
+int kvm_host_prepare_stage2(void *pgt_pool_base, void *mmio_pool_base);
 int kvm_guest_prepare_stage2(struct pkvm_hyp_vm *vm, void *pgd);
 void handle_host_mem_abort(struct kvm_cpu_context *host_ctxt);
 
