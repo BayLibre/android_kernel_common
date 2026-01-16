b8ba61f05fa4c7bea00ddf19f7eea37fa23490e5
Zhipeng Wang <zhipeng.wang_1@nxp.com>
2026-01-16 17:10:35 +0900

    Revert "net: phy: declare package-related struct members only if CONFIG_PHY_PACKAGE is enabled"
    This reverts commit c523058713abac66b0d83ae12a0574d76cd7df2b.
    
    The CONFIG_PHY_PACKAGE conditional compilation breaks KMI when
    vendor modules like micrel.ko need to access these shared pointers.
    CONFIG_PHY_PACKAGE is disabled in GKI by default, but vendor modules
    built against the kernel headers expect consistent struct layouts.
    
    Bug: 476288035
    Change-Id: I0f37f23ec921bebb279730acf8769e2193098184
    Signed-off-by: Zhipeng Wang <zhipeng.wang_1@nxp.com>

--- include/linux/phy.h
+++ include/linux/phy.h
@@ -423,10 +423,8 @@ struct mii_bus {
 	/** @shared_lock: protect access to the shared element */
 	struct mutex shared_lock;
 
-#if IS_ENABLED(CONFIG_PHY_PACKAGE)
 	/** @shared: shared state across different PHYs */
 	struct phy_package_shared *shared[PHY_MAX_ADDR];
-#endif
 };
 #define to_mii_bus(d) container_of(d, struct mii_bus, dev)
 
@@ -734,11 +732,9 @@ struct phy_device {
 	/* For use by PHYs to maintain extra state */
 	void *priv;
 
-#if IS_ENABLED(CONFIG_PHY_PACKAGE)
 	/* shared data pointer */
 	/* For use by PHYs inside the same package that need a shared state. */
 	struct phy_package_shared *shared;
-#endif
 
 	/* Reporting cable test results */
 	struct sk_buff *skb;
