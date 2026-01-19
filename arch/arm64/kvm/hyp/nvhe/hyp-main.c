cd2f52f125f2278bec13cfaf6db59be329b13b7d
Vincent Donnefort <vdonnefort@google.com>
2026-01-19 13:06:13 +0000

    ANDROID: KVM: arm64: Use a specific code for hyp_alloc ENOMEM
    ENOMEM is heavily overloaded into the hypervisor. To avoid conflicts,
    use a specific code whenever hyp_alloc() fails: ENOMEMHYPALLOC.
    
    Bug: 458241298
    Change-Id: I5cc8452d2978e922bee607eb3daa031a02d55082
    Signed-off-by: Vincent Donnefort <vdonnefort@google.com>

--- arch/arm64/kvm/hyp/nvhe/hyp-main.c
+++ arch/arm64/kvm/hyp/nvhe/hyp-main.c
@@ -19,8 +19,9 @@
 
 #include <nvhe/alloc.h>
 #include <nvhe/alloc_mgt.h>
-#include <nvhe/iommu.h>
+#include <nvhe/errno.h>
 #include <nvhe/ffa.h>
+#include <nvhe/iommu.h>
 #include <nvhe/mem_protect.h>
 #include <nvhe/modules.h>
 #include <nvhe/mm.h>
@@ -1035,7 +1036,7 @@ static int errno_to_smccc(int ret, struct kvm_cpu_context *host_ctxt)
 	struct kvm_hyp_req *req = this_cpu_ptr(&host_hyp_reqs);
 
 	switch (ret) {
-	case -ENOMEM:
+	case -ENOMEMHYPALLOC:
 		req->type = KVM_HYP_REQ_TYPE_HYP_ALLOC;
 		req->mem.nr_pages = hyp_alloc_missing_donations();
 		break;
