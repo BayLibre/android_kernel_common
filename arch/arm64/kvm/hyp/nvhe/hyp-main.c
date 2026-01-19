03da736c737480860db46a3db631de83cc92f989
Vincent Donnefort <vdonnefort@google.com>
2026-01-19 15:03:59 +0000

    ANDROID: KVM: arm64: Raise MEM_HOST_S2 hyp_req
    Repaint the ENOMEM from a failed host stage-2 mapping into a host
    stage-2 specific error ENOMEMHOSTS2 that then will raise a MEM_HOST_S2
    hyp request for the host to fulfill.
    
    Bug: 458241298
    Change-Id: Ib6fcbfefd1ff62aee85f8a460612e7eb1400b979
    Signed-off-by: Vincent Donnefort <vdonnefort@google.com>

--- arch/arm64/kvm/hyp/nvhe/hyp-main.c
+++ arch/arm64/kvm/hyp/nvhe/hyp-main.c
@@ -1029,6 +1029,9 @@ static u64 errno_to_smccc(int ret, struct kvm_cpu_context *host_ctxt)
 		req->type = KVM_HYP_REQ_TYPE_HYP_ALLOC;
 		req->mem.nr_pages = hyp_alloc_missing_donations();
 		break;
+	case -ENOMEMHOSTS2:
+		req->type = KVM_HYP_REQ_TYPE_MEM_HOST_S2;
+		break;
 	}
 
 	return hyp_req_to_smccc(host_ctxt, req, ret);
@@ -1058,8 +1061,9 @@ static void handle___pkvm_host_map_guest(struct kvm_cpu_context *host_ctxt)
 		ret = __pkvm_host_donate_guest(pfn, gfn, nr_pages, hyp_vcpu);
 	else
 		ret = __pkvm_host_share_guest(pfn, gfn, nr_pages, hyp_vcpu, prot);
+
 out:
-	cpu_reg(host_ctxt, 1) =  ret;
+	cpu_reg(host_ctxt, 1) =  errno_to_smccc(ret, host_ctxt);
 }
 
 static void handle___pkvm_host_donate_guest_sglist(struct kvm_cpu_context *host_ctxt)
@@ -1081,7 +1085,7 @@ static void handle___pkvm_host_donate_guest_sglist(struct kvm_cpu_context *host_
 	ret = __pkvm_host_donate_sglist_guest(hyp_vcpu);
 
 out:
-	cpu_reg(host_ctxt, 1) =  ret;
+	cpu_reg(host_ctxt, 1) = errno_to_smccc(ret, host_ctxt);
 }
 
 static void handle___pkvm_host_unshare_guest(struct kvm_cpu_context *host_ctxt)
@@ -1648,7 +1652,7 @@ static void handle___pkvm_hyp_alloc_mgt_refill(struct kvm_cpu_context *host_ctxt
 		.nr_pages	= nr_pages,
 	};
 
-	cpu_reg(host_ctxt, 1) = hyp_alloc_mgt_refill(id, &mc);
+	cpu_reg(host_ctxt, 1) = errno_to_smccc(hyp_alloc_mgt_refill(id, &mc), host_ctxt);
 
 	/*
 	 * We also need to return the updated memcache. Luckily here, the only request we will have
@@ -1687,7 +1691,7 @@ static void handle___pkvm_ptdump(struct kvm_cpu_context *host_ctxt)
 	if (op == PKVM_PTDUMP_GET_LEVEL || op == PKVM_PTDUMP_GET_RANGE)
 		cpu_reg(host_ctxt, 1) = __pkvm_ptdump_get_config(handle, op);
 	else if (op == PKVM_PTDUMP_WALK_RANGE)
-		cpu_reg(host_ctxt, 1) = __pkvm_ptdump_walk_range(handle, log);
+		cpu_reg(host_ctxt, 1) = errno_to_smccc(__pkvm_ptdump_walk_range(handle, log), host_ctxt);
 	else
 		cpu_reg(host_ctxt, 0) = SMCCC_RET_NOT_SUPPORTED;
 }
