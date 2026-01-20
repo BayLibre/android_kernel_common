708e78b279c3a7785fc6659ca9e4afdc21090ee5
Vincent Donnefort <vdonnefort@google.com>
2026-01-20 15:44:07 +0000

    ANDROID: KVM: arm64: Back the host stage-2 memory pool by a CMA region
    In order to reclaim the unused memory for the host stage-2 pool, we need
    to be able to top it up. But to add memory to that pool, it needs to be
    first unmaped from the host stage-2... operation which might need
    memory. To break this circle, we can use memory from a specific region
    where we can keep the host stage-2 mapping at the PTE level ensuring us
    that admitting memory from this region into the hypervisor will never
    cause an allocation from the host stage-2 pool.
    
    Back the host stage-2 pool with a CMA region so that reclaimed memory
    can be re-used by the system while the hypervisor can easily keep this
    region always mapped at PTE-level (done in a later patch).
    
    To init the hypervisor the entire pool is allocated. Later reclaim will
    be possible. Also due to huge alignment requirements (most likely
    PMD_SIZE) the CMA region covers the entire carveout even though in
    practice, only the host stage-2 part will be reclaimable.
    
    Bug: 458241298
    Change-Id: I22b4b4c8dc714b2b9025416dba963ebcadd40ec4
    Signed-off-by: Vincent Donnefort <vdonnefort@google.com>

--- arch/arm64/include/asm/kvm_host.h
+++ arch/arm64/include/asm/kvm_host.h
@@ -1695,6 +1695,11 @@ int kvm_trng_call(struct kvm_vcpu *vcpu);
 extern phys_addr_t hyp_mem_base;
 extern phys_addr_t hyp_mem_size;
 void __init kvm_hyp_reserve(void);
+#ifdef CONFIG_CMA
+int __init pkvm_host_stage2_reserve(void);
+#else
+static inline int __init pkvm_host_stage2_reserve(void) { return 0; }
+#endif
 #else
 static inline void kvm_hyp_reserve(void) { }
 #endif
