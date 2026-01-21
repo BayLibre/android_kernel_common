ad4ad401d0c9c5b2df00556d62b501570d200966
Richard Chang <richardycc@google.com>
2026-01-21 12:00:06 +0000

    ANDROID: zram: Add ZRAM_ANDROID_IOC_PROCESS_PREFETCH ioctl
    Introduce a new IOCTL command, ZRAM_ANDROID_IOC_PROCESS_PREFETCH, which
    allows userspace to trigger the prefetching of pages from the backing
    device into zram for a specific process.
    
    When invoked, the zram driver scans the target process's page tables to
    identify entries that are currently stored on the backing device. These
    pages are then read from the backing device and written back into zram.
    This mechanism is intended to reduce page fault swap-in latency by
    restoring data from storage before it is accessed.
    
    Bug: 340137613
    Change-Id: Ieebbf506663c09cb355636c8f5a81b7bd9f6d3fd
    Signed-off-by: Richard Chang <richardycc@google.com>

--- include/uapi/linux/zram_ioctl.h
+++ include/uapi/linux/zram_ioctl.h
@@ -36,10 +36,15 @@ struct zram_android_ioc_data_process_writeback {
 	__u64		written_bytes;
 };
 
+struct zram_android_ioc_data_process_prefetch {
+	__aligned_u64	pidfd;
+};
+
 struct zram_android_ioc_data {
 	__u32 version;
 	union {
 		struct zram_android_ioc_data_process_writeback process_writeback;
+		struct zram_android_ioc_data_process_prefetch process_prefetch;
 	} data;
 };
 
@@ -55,6 +60,9 @@ struct zram_android_ioc_data {
 
 #define ZRAM_ANDROID_IOC_PROCESS_WRITEBACK \
 	_IOWR(ZRAM_ANDROID_IOC_MAGIC, 1, struct zram_android_ioc_data)
+
+#define ZRAM_ANDROID_IOC_PROCESS_PREFETCH \
+	_IOW(ZRAM_ANDROID_IOC_MAGIC, 2, struct zram_android_ioc_data)
 
 #endif /* _UAPI_LINUX_ZRAM_IOCTL_H */
 
