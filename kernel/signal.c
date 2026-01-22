6909692d1f99360e16470ab6e722aea5848c9761
Yinchu Chen <chenyc5@motorola.com>
2026-01-22 14:54:40 +0800

    ANDROID: vendor_hooks: Add a hook to record signal generated
    Add a hook to record the signal generated source and target info
    
    Bug: 477782439
    Change-Id: If8ee81ab275f3577eb874d57ef51ec1cc12d8d64
    Signed-off-by: Yinchu Chen <chenyc5@motorola.com>

--- kernel/signal.c
+++ kernel/signal.c
@@ -1204,6 +1204,7 @@ out_set:
 	complete_signal(sig, t, type);
 ret:
 	trace_signal_generate(sig, info, t, type != PIDTYPE_PID, result);
+	trace_android_vh_signal_generate(sig, info, t, type != PIDTYPE_PID, result);
 	return ret;
 }
 
