d0da9dc0eae2eb8d9b3a66a09c74292d73efc5d0
Aashish Sharma <shraash@google.com>
2026-01-24 19:07:31 -0500

    ANDROID: iommu/vt-d: pKVM: Hypercall for submitting QI descriptors
    Have this hypercall to submit invalidation request so that host does not
    directly access QI registers.
    
    TODO: Once pvIOMMU is feature complete, this hypercall would be needed
    only for IEC descriptors as rest of the invalidation logic will be fully
    taken care of by pKVM. This hypercall would be changed to handle only
    IEC flushing.
    
    Bug: 473885291
    Upstream-Task: 402758258
    Test: Build and Boot
    
    Change-Id: I3ca498715a5d55c39e44f651575094a25b89cb9d
    Signed-off-by: Aashish Sharma <shraash@google.com>

---
+++ drivers/iommu/intel/pkvm/iommu_hc.c
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright © 2026 Google.
+ *
+ */
+#include <asm/kvm_pkvm.h>
+#include "pkvm/mmu.h"
+#include "pkvm/memory.h"
+#include "pkvm/pkvm.h"
+#include "pkvm/debug.h"
+#include "../iommu.h"
+
+int pkvm_iommu_qi_submit(u64 phys, u64 desc_gpa, u32 count, u32 options)
+{
+	struct intel_iommu *iommu = iommu_from_phys(phys);
+
+	if (!iommu)
+		return -EINVAL;
+
+	BUG_ON(!iommu->qi);
+
+	return qi_submit_sync(iommu, pkvm_host_gpa_to_virt(desc_gpa),
+			      count, options);
+}
