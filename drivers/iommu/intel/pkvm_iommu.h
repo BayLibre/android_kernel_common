055841bd7fa2cac45df36856272383468a1e927b
Aashish Sharma <shraash@google.com>
2026-01-29 10:58:39 -0800

    ANDROID: iommu/vt-d: pKVM: Flag to denote if iotlb sync needed after map
    pKVM needs to flush iotlb if hardware requires a flush after a page
    table map operation. If CM(Caching Mode) in capability register is set
    to 1, hardware caches invalid or non-present entries for non first-level
    translations and hence software is required to flush iotlb when mappings
    are created. This is also needed for hardware that mandates write buffer
    flushing, but we don't support very old hardware where write buffer
    flushing was required.
    
    Determine this during domain allocation so that pKVM can conditionally
    flush iotlb during map operation.
    
    Bug: 470050354
    
    Change-Id: I0655b54ee2cbe4a3bb7f6c0950f88041880f6626
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>
    Signed-off-by: Aashish Sharma <shraash@google.com>

--- drivers/iommu/intel/pkvm_iommu.h
+++ drivers/iommu/intel/pkvm_iommu.h
@@ -181,7 +181,8 @@ void host_ept_flush_needed_inc(void);
 void host_ept_flush_needed_dec(void);
 bool host_ept_flush_needed(void);
 
-struct dmar_domain *pkvm_alloc_iommu_domain(struct alloc_domain_data *data);
+struct dmar_domain *pkvm_alloc_iommu_domain(struct alloc_domain_data *data,
+					    bool need_iotlb_sync_map);
 struct dmar_domain *pkvm_get_iommu_domain(void *pgd);
 struct dmar_domain *pkvm_get_iommu_domain_noref(void *pgd);
 void pkvm_put_iommu_domain(struct dmar_domain *domain);
