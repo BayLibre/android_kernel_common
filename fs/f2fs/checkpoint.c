a02a9cc85d233f2e233493510fbb76c1353d0e17
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2026-01-29 13:51:39 -0800

    Revert "FROMGIT: f2fs: trace elapsed time for cp_global_sem lock"
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/3ec151c8-dc66-41bc-80b1-8b208db1de8d). Please review this revert and approve if it looks good to you.
    
    Change-Id: I1c2d75b75d445f52e2df396de58bb13d0c98670b
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com
    Bug: 408202989

--- fs/f2fs/checkpoint.c
+++ fs/f2fs/checkpoint.c
@@ -527,7 +527,6 @@ static int f2fs_write_meta_pages(struct address_space *mapping,
 				struct writeback_control *wbc)
 {
 	struct f2fs_sb_info *sbi = F2FS_M_SB(mapping);
-	struct f2fs_lock_context lc;
 	long diff, written;
 
 	if (unlikely(is_sbi_flag_set(sbi, SBI_POR_DOING)))
@@ -540,13 +539,13 @@ static int f2fs_write_meta_pages(struct address_space *mapping,
 		goto skip_write;
 
 	/* if locked failed, cp will flush dirty pages instead */
-	if (!f2fs_down_write_trylock_trace(&sbi->cp_global_sem, &lc))
+	if (!f2fs_down_write_trylock(&sbi->cp_global_sem))
 		goto skip_write;
 
 	trace_f2fs_writepages(mapping->host, wbc, META);
 	diff = nr_pages_to_write(sbi, META, wbc);
 	written = f2fs_sync_meta_pages(sbi, META, wbc->nr_to_write, FS_META_IO);
-	f2fs_up_write_trace(&sbi->cp_global_sem, &lc);
+	f2fs_up_write(&sbi->cp_global_sem);
 	wbc->nr_to_write = max((long)0, wbc->nr_to_write - written - diff);
 	return 0;
 
@@ -1767,7 +1766,6 @@ static int do_checkpoint(struct f2fs_sb_info *sbi, struct cp_control *cpc)
 int f2fs_write_checkpoint(struct f2fs_sb_info *sbi, struct cp_control *cpc)
 {
 	struct f2fs_checkpoint *ckpt = F2FS_CKPT(sbi);
-	struct f2fs_lock_context lc;
 	unsigned long long ckpt_ver;
 	int err = 0;
 
@@ -1780,7 +1778,7 @@ int f2fs_write_checkpoint(struct f2fs_sb_info *sbi, struct cp_control *cpc)
 		f2fs_warn(sbi, "Start checkpoint disabled!");
 	}
 	if (cpc->reason != CP_RESIZE)
-		f2fs_down_write_trace(&sbi->cp_global_sem, &lc);
+		f2fs_down_write(&sbi->cp_global_sem);
 
 	if (!is_sbi_flag_set(sbi, SBI_IS_DIRTY) &&
 		((cpc->reason & CP_FASTBOOT) || (cpc->reason & CP_SYNC) ||
@@ -1862,7 +1860,7 @@ stop:
 	trace_f2fs_write_checkpoint(sbi->sb, cpc->reason, "finish checkpoint");
 out:
 	if (cpc->reason != CP_RESIZE)
-		f2fs_up_write_trace(&sbi->cp_global_sem, &lc);
+		f2fs_up_write(&sbi->cp_global_sem);
 	return err;
 }
 
