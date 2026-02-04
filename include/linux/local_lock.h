9f8f2ab494bdf297065ceb9949c9649c9fb000df
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2026-02-04 12:22:46 -0800

    Revert "BACKPORT: locking/local_lock, mm: replace localtry_ help..."
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/ce0223ae-2f3e-4ae7-91ff-80493ad50c7d). Please review this revert and approve if it looks good to you.
    
    Change-Id: I4ce670ba037322d4d9fda7d5b9b1baf55559e046
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com
    Bug: 470809823

--- include/linux/local_lock.h
+++ include/linux/local_lock.h
@@ -52,23 +52,44 @@
 	__local_unlock_irqrestore(lock, flags)
 
 /**
- * local_lock_init - Runtime initialize a lock instance
+ * localtry_lock_init - Runtime initialize a lock instance
  */
-#define local_trylock_init(lock)	__local_trylock_init(lock)
+#define localtry_lock_init(lock)		__localtry_lock_init(lock)
 
 /**
- * local_trylock - Try to acquire a per CPU local lock
+ * localtry_lock - Acquire a per CPU local lock
+ * @lock:	The lock variable
+ */
+#define localtry_lock(lock)		__localtry_lock(lock)
+
+/**
+ * localtry_lock_irq - Acquire a per CPU local lock and disable interrupts
+ * @lock:	The lock variable
+ */
+#define localtry_lock_irq(lock)		__localtry_lock_irq(lock)
+
+/**
+ * localtry_lock_irqsave - Acquire a per CPU local lock, save and disable
+ *			 interrupts
+ * @lock:	The lock variable
+ * @flags:	Storage for interrupt flags
+ */
+#define localtry_lock_irqsave(lock, flags)				\
+	__localtry_lock_irqsave(lock, flags)
+
+/**
+ * localtry_trylock - Try to acquire a per CPU local lock.
  * @lock:	The lock variable
  *
  * The function can be used in any context such as NMI or HARDIRQ. Due to
  * locking constrains it will _always_ fail to acquire the lock in NMI or
  * HARDIRQ context on PREEMPT_RT.
  */
-#define local_trylock(lock)		__local_trylock(lock)
+#define localtry_trylock(lock)		__localtry_trylock(lock)
 
 /**
- * local_trylock_irqsave - Try to acquire a per CPU local lock, save and disable
- *			   interrupts if acquired
+ * localtry_trylock_irqsave - Try to acquire a per CPU local lock, save and disable
+ *			      interrupts if acquired
  * @lock:	The lock variable
  * @flags:	Storage for interrupt flags
  *
@@ -76,8 +97,29 @@
  * locking constrains it will _always_ fail to acquire the lock in NMI or
  * HARDIRQ context on PREEMPT_RT.
  */
-#define local_trylock_irqsave(lock, flags)			\
-	__local_trylock_irqsave(lock, flags)
+#define localtry_trylock_irqsave(lock, flags)				\
+	__localtry_trylock_irqsave(lock, flags)
+
+/**
+ * local_unlock - Release a per CPU local lock
+ * @lock:	The lock variable
+ */
+#define localtry_unlock(lock)		__localtry_unlock(lock)
+
+/**
+ * local_unlock_irq - Release a per CPU local lock and enable interrupts
+ * @lock:	The lock variable
+ */
+#define localtry_unlock_irq(lock)		__localtry_unlock_irq(lock)
+
+/**
+ * localtry_unlock_irqrestore - Release a per CPU local lock and restore
+ *			      interrupt flags
+ * @lock:	The lock variable
+ * @flags:      Interrupt flags to restore
+ */
+#define localtry_unlock_irqrestore(lock, flags)			\
+	__localtry_unlock_irqrestore(lock, flags)
 
 DEFINE_GUARD(local_lock, local_lock_t __percpu*,
 	     local_lock(_T),
