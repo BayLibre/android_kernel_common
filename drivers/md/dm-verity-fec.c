f482da8413bef74f0637fe186e21ee3bc3c9cd3e
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2026-02-05 09:46:41 -0800

    Revert "Merge 9551a26f17d9 ("Merge tag 'loongarch-6.19' of git:/..."
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/eed7e194-016f-4eb7-af5a-a7aa619c66d0). Please review this revert and approve if it looks good to you.
    
    Bug: 481980385
    Change-Id: I40eb5f2e3822896c68666680f5c0fc909d9a5558
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com

--- drivers/md/dm-verity-fec.c
+++ drivers/md/dm-verity-fec.c
@@ -177,11 +177,9 @@ error:
 	if (r < 0 && neras)
 		DMERR_LIMIT("%s: FEC %llu: failed to correct: %d",
 			    v->data_dev->name, (unsigned long long)rsb, r);
-	else if (r > 0) {
+	else if (r > 0)
 		DMWARN_LIMIT("%s: FEC %llu: corrected %d errors",
 			     v->data_dev->name, (unsigned long long)rsb, r);
-		atomic64_inc(&v->fec->corrected);
-	}
 
 	return r;
 }
@@ -190,13 +188,14 @@ error:
  * Locate data block erasures using verity hashes.
  */
 static int fec_is_erasure(struct dm_verity *v, struct dm_verity_io *io,
-			  const u8 *want_digest, const u8 *data)
+			  u8 *want_digest, u8 *data)
 {
 	if (unlikely(verity_hash(v, io, data, 1 << v->data_dev_block_bits,
-				 io->tmp_digest)))
+				 verity_io_real_digest(v, io))))
 		return 0;
 
-	return memcmp(io->tmp_digest, want_digest, v->digest_size) != 0;
+	return memcmp(verity_io_real_digest(v, io), want_digest,
+		      v->digest_size) != 0;
 }
 
 /*
@@ -329,7 +328,7 @@ static int fec_alloc_bufs(struct dm_verity *v, struct dm_verity_fec_io *fio)
 		if (fio->bufs[n])
 			continue;
 
-		fio->bufs[n] = kmem_cache_alloc(v->fec->cache, GFP_NOWAIT);
+		fio->bufs[n] = mempool_alloc(&v->fec->extra_pool, GFP_NOWAIT);
 		/* we can manage with even one buffer if necessary */
 		if (unlikely(!fio->bufs[n]))
 			break;
@@ -363,7 +362,7 @@ static void fec_init_bufs(struct dm_verity *v, struct dm_verity_fec_io *fio)
  */
 static int fec_decode_rsb(struct dm_verity *v, struct dm_verity_io *io,
 			  struct dm_verity_fec_io *fio, u64 rsb, u64 offset,
-			  const u8 *want_digest, bool use_erasures)
+			  bool use_erasures)
 {
 	int r, neras = 0;
 	unsigned int pos;
@@ -389,11 +388,12 @@ static int fec_decode_rsb(struct dm_verity *v, struct dm_verity_io *io,
 
 	/* Always re-validate the corrected block against the expected hash */
 	r = verity_hash(v, io, fio->output, 1 << v->data_dev_block_bits,
-			io->tmp_digest);
+			verity_io_real_digest(v, io));
 	if (unlikely(r < 0))
 		return r;
 
-	if (memcmp(io->tmp_digest, want_digest, v->digest_size)) {
+	if (memcmp(verity_io_real_digest(v, io), verity_io_want_digest(v, io),
+		   v->digest_size)) {
 		DMERR_LIMIT("%s: FEC %llu: failed to correct (%d erasures)",
 			    v->data_dev->name, (unsigned long long)rsb, neras);
 		return -EILSEQ;
@@ -404,8 +404,7 @@ static int fec_decode_rsb(struct dm_verity *v, struct dm_verity_io *io,
 
 /* Correct errors in a block. Copies corrected block to dest. */
 int verity_fec_decode(struct dm_verity *v, struct dm_verity_io *io,
-		      enum verity_block_type type, const u8 *want_digest,
-		      sector_t block, u8 *dest)
+		      enum verity_block_type type, sector_t block, u8 *dest)
 {
 	int r;
 	struct dm_verity_fec_io *fio = fec_io(io);
@@ -414,8 +413,10 @@ int verity_fec_decode(struct dm_verity *v, struct dm_verity_io *io,
 	if (!verity_fec_is_enabled(v))
 		return -EOPNOTSUPP;
 
-	if (fio->level)
+	if (fio->level >= DM_VERITY_FEC_MAX_RECURSION) {
+		DMWARN_LIMIT("%s: FEC: recursion too deep", v->data_dev->name);
 		return -EIO;
+	}
 
 	fio->level++;
 
@@ -446,9 +447,9 @@ int verity_fec_decode(struct dm_verity *v, struct dm_verity_io *io,
 	 * them first. Do a second attempt with erasures if the corruption is
 	 * bad enough.
 	 */
-	r = fec_decode_rsb(v, io, fio, rsb, offset, want_digest, false);
+	r = fec_decode_rsb(v, io, fio, rsb, offset, false);
 	if (r < 0) {
-		r = fec_decode_rsb(v, io, fio, rsb, offset, want_digest, true);
+		r = fec_decode_rsb(v, io, fio, rsb, offset, true);
 		if (r < 0)
 			goto done;
 	}
@@ -478,8 +479,7 @@ void verity_fec_finish_io(struct dm_verity_io *io)
 		mempool_free(fio->bufs[n], &f->prealloc_pool);
 
 	fec_for_each_extra_buffer(fio, n)
-		if (fio->bufs[n])
-			kmem_cache_free(f->cache, fio->bufs[n]);
+		mempool_free(fio->bufs[n], &f->extra_pool);
 
 	mempool_free(fio->output, &f->output_pool);
 }
@@ -531,6 +531,7 @@ void verity_fec_dtr(struct dm_verity *v)
 
 	mempool_exit(&f->rs_pool);
 	mempool_exit(&f->prealloc_pool);
+	mempool_exit(&f->extra_pool);
 	mempool_exit(&f->output_pool);
 	kmem_cache_destroy(f->cache);
 
@@ -780,6 +781,12 @@ int verity_fec_ctr(struct dm_verity *v)
 				     f->cache);
 	if (ret) {
 		ti->error = "Cannot allocate FEC buffer prealloc pool";
+		return ret;
+	}
+
+	ret = mempool_init_slab_pool(&f->extra_pool, 0, f->cache);
+	if (ret) {
+		ti->error = "Cannot allocate FEC buffer extra pool";
 		return ret;
 	}
 
