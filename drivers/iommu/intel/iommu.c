7780b2c8bd95d8349cda4cc07ef10d3bf23c3dee
Vineeth Pillai <vineethrp@google.com>
2026-02-10 11:37:20 -0500

    ANDROID: iommu/vt-d: pKVM: Hypercalls for domain pagetable management
    Implement hypercalls for mapping and unmapping of IOVA addresses in the
    IOMMU page tables.
    
    When pvIOMMU enabled, use hypercalls for iommu map and unmap instead of
    directly updating page tables as they are write protected by pKVM.
    
    Security Note:
    The host IOMMU driver typically optimizes performance by delaying IOTLB
    cache flushes on unmap. However, this violates pKVM security guarantees:
    pages freed by an unmap might be immediately donated to another VM.
    Stale IOTLB entries could allow a device to access sensitive data in
    the newly repurposed pages. Consequently, this implementation enforces
    unconditional IOTLB flushing on every unmap operation.
    
    Bug: 473885291
    Upstream-Task: 402758258
    Test: Build and Boot
    
    Change-Id: If0e0ab9ea73de4e34e744c88a8869429a72d3738
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>
    Signed-off-by: Aashish Sharma <shraash@google.com>

--- drivers/iommu/intel/iommu.c
+++ drivers/iommu/intel/iommu.c
@@ -3868,6 +3868,11 @@ static int intel_iommu_map(struct iommu_domain *domain,
 	/* Round up size to next multiple of PAGE_SIZE, if it and
 	   the low bits of hpa would take us onto the next page */
 	size = aligned_nrpages(hpa, size);
+
+	if (pkvm_enabled())
+		return pkvm_domain_mapping(dmar_domain, iova >> VTD_PAGE_SHIFT,
+					   hpa >> VTD_PAGE_SHIFT, size, prot, gfp);
+
 	return domain_map(dmar_domain, iova >> VTD_PAGE_SHIFT,
 				hpa >> VTD_PAGE_SHIFT, size, prot, gfp);
 }
@@ -3913,6 +3918,15 @@ static size_t intel_iommu_unmap(struct iommu_domain *domain,
 
 	start_pfn = iova >> VTD_PAGE_SHIFT;
 	last_pfn = (iova + size - 1) >> VTD_PAGE_SHIFT;
+
+	if (pkvm_enabled()) {
+		int ret = pkvm_domain_unmapping(dmar_domain, start_pfn, last_pfn);
+
+		if (ret)
+			pr_err("%s: domain unmap IOVA[start: %lx, end: %lx] failed (err=%d)\n",
+			       __func__, start_pfn, last_pfn, ret);
+		return size;
+	}
 
 	domain_unmap(dmar_domain, start_pfn, last_pfn, &gather->freelist);
 
