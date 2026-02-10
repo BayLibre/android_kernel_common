7db2edbdecd2206e354d6a2f9935e680286e0922
Aashish Sharma <shraash@google.com>
2026-02-10 11:09:19 -0500

    ANDROID: iommu/vt-d: pKVM: Release the pagetable pages when freeing domain
    Now that we have  domain unmap implemented in pKVM, and unmap logic
    collects all freed pages from page table in the memcache, release those
    pages back to host when freeing the domain.
    
    Bug: 473885291
    Upstream-Task: 402758258
    Test: Build and Boot
    
    Change-Id: Ia8bb58fbb7f125bfb9e7faf6532129b0dfc938a9
    Signed-off-by: Aashish Sharma <shraash@google.com>
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>

--- drivers/iommu/intel/pkvm/iommu_domain.h
+++ drivers/iommu/intel/pkvm/iommu_domain.h
@@ -11,6 +11,6 @@ struct dmar_domain *pkvm_alloc_iommu_domain(struct alloc_domain_data *data);
 struct dmar_domain *pkvm_get_iommu_domain(void *pgd);
 struct dmar_domain *pkvm_get_iommu_domain_noref(void *pgd);
 void pkvm_put_iommu_domain(struct dmar_domain *domain);
-int pkvm_free_iommu_domain(struct dmar_domain *domain);
+int pkvm_free_iommu_domain(struct dmar_domain *domain, struct pkvm_memcache *teardown_mc);
 #endif
 
