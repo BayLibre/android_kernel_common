59de9db9aa9ed3caef31cb581d8b835aec628c65
Suren Baghdasaryan <surenb@google.com>
2026-02-11 19:12:22 +0000

    ANDROID: drivers/wrapfd: avoid changing vma attributes when mapping wrapfd
    Changing vma attributes when mapping a wrapfd would cause conflicts if
    two threads are mapping and modifying them concurrently. Stop using
    vma->vm_private_data for storing wrap_ctx and replace it with an xarray
    hashing vma to the wrap_ctx.
    Also verify that no vm_ops other than the ones wrap_vm_ops overrides are
    used by the wrapped content.
    
    Bug: 480234490
    Change-Id: I0e1a91ecc4a38d0a9df775a80def4d9f5feb1887
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- drivers/android/wrapfd.c
+++ drivers/android/wrapfd.c
@@ -22,6 +22,7 @@
 #include <linux/spinlock.h>
 #include <linux/uaccess.h>
 #include <linux/uio.h>
+#include <linux/xarray.h>
 #include <uapi/linux/wrapfd.h>
 
 #include "wrapfd.h"
@@ -40,8 +41,6 @@ struct wrap_content_operations {
 	int (*mmap_prepare)(struct wrap_content *content,
 			    struct vm_area_struct *vma);
 	int (*mmap)(struct wrap_content *content, struct vm_area_struct *vma);
-	vm_fault_t (*fault)(struct wrap_content *content,
-			    struct vm_fault *vmf);
 	void (*free)(struct wrap_content *content);
 	struct wrap_content *(*make_writable)(struct wrap_content *content,
 			      bool writable);
@@ -202,12 +201,6 @@ static int dmabuf_content_mmap(struct wrap_content *content,
 	return 0;
 }
 
-static vm_fault_t dmabuf_content_fault(struct wrap_content *content,
-				       struct vm_fault *vmf)
-{
-	return vmf->vma->vm_ops->fault(vmf);
-}
-
 static void dmabuf_content_free(struct wrap_content *content)
 {
 	struct wrap_content_dmabuf *dmabuf_content;
@@ -294,7 +287,6 @@ static struct wrap_content_operations dmabuf_content_ops = {
 	.load			= dmabuf_content_load,
 	.mmap_prepare		= dmabuf_content_mmap_prepare,
 	.mmap			= dmabuf_content_mmap,
-	.fault			= dmabuf_content_fault,
 	.make_writable		= dmabuf_content_make_writable,
 	.is_writable		= dmabuf_content_is_writable,
 	.free			= dmabuf_content_free,
@@ -326,18 +318,13 @@ struct wrap_owner {
 	struct device *dev;
 };
 
-struct wrap_ctx_mapping {
-	struct wrap_ctx *ctx;
-	const struct vm_operations_struct *vm_ops;
-	void *vm_private_data;
-};
-
 struct wrap_ctx {
 	struct wrap_content *content;
 	spinlock_t lock; /* protects all fields below */
 	struct wrap_owner owner;
 	bool allow_guests;
 	int map_count;
+	const struct vm_operations_struct *vm_ops;
 };
 
 static struct wrap_ctx *create_wrap_ctx(void)
@@ -398,44 +385,75 @@ static int can_access(struct wrap_ctx *ctx, struct task_struct *task,
 }
 
 static const struct vm_operations_struct wrap_vm_ops;
+static DEFINE_XARRAY_ALLOC(ctx_xa);
+
+static inline int hash_ctx(struct vm_area_struct *vma, struct wrap_ctx *ctx)
+{
+	return xa_insert(&ctx_xa, (unsigned long)vma, ctx, GFP_KERNEL);
+}
+
+static inline void unhash_ctx(struct vm_area_struct *vma)
+{
+	xa_erase(&ctx_xa, (unsigned long)vma);
+}
+
+static inline struct wrap_ctx *find_wrap_ctx(struct vm_area_struct *vma)
+{
+	/*
+	 * No need to lock ctx_xa because the callers are vm_ops which
+	 * don't race with hash modifiers in wrap_vm_close(), wrap_mmap()
+	 * or with wrap_ctx destruction in wrap_release().
+	 */
+	return xa_load(&ctx_xa, (unsigned long)vma);
+}
+
+static void wrap_vm_open(struct vm_area_struct *vma)
+{
+	struct wrap_ctx *ctx = find_wrap_ctx(vma);
+
+	if (WARN_ON(!ctx))
+		return;
+
+	if (ctx->vm_ops && ctx->vm_ops->open)
+		ctx->vm_ops->open(vma);
+
+	spin_lock(&ctx->lock);
+	ctx->map_count++;
+	spin_unlock(&ctx->lock);
+}
 
 static void wrap_vm_close(struct vm_area_struct *vma)
 {
-	struct wrap_ctx_mapping *mapping = vma->vm_private_data;
-	struct wrap_ctx *ctx = mapping->ctx;
+	struct wrap_ctx *ctx = find_wrap_ctx(vma);
 
-	if (mapping->vm_ops && mapping->vm_ops->close) {
-		vma->vm_private_data = mapping->vm_private_data;
-		vma->vm_ops = mapping->vm_ops;
-		vma->vm_ops->close(vma);
-	}
+	if (WARN_ON(!ctx))
+		return;
+
+	if (ctx->vm_ops && ctx->vm_ops->close)
+		ctx->vm_ops->close(vma);
 
 	spin_lock(&ctx->lock);
 	ctx->map_count--;
+	vma->vm_ops = ctx->vm_ops;
 	spin_unlock(&ctx->lock);
-
-	kfree(mapping);
+	unhash_ctx(vma);
 }
 
 static vm_fault_t wrap_vm_fault(struct vm_fault *vmf)
 {
-	struct wrap_ctx_mapping *mapping = vmf->vma->vm_private_data;
-	struct wrap_ctx *ctx = mapping->ctx;
-	vm_fault_t ret;
+	struct wrap_ctx *ctx = find_wrap_ctx(vmf->vma);
 
-	if (!mapping->vm_ops || !mapping->vm_ops->fault)
+	if (WARN_ON(!ctx))
 		return VM_FAULT_SIGBUS;
 
-	vmf->vma->vm_private_data = mapping->vm_private_data;
-	vmf->vma->vm_ops = mapping->vm_ops;
-	ret = ctx->content->ops->fault(ctx->content, vmf);
-	vmf->vma->vm_ops = &wrap_vm_ops;
-	vmf->vma->vm_private_data = ctx;
+	if (ctx->vm_ops && ctx->vm_ops->fault)
+		return ctx->vm_ops->fault(vmf);
 
-	return ret;
+	return VM_FAULT_SIGBUS;
 }
 
 static const struct vm_operations_struct wrap_vm_ops = {
+	.open		= wrap_vm_open,
 	.close		= wrap_vm_close,
 	.fault		= wrap_vm_fault,
 };
@@ -443,7 +461,6 @@ static const struct vm_operations_struct wrap_vm_ops = {
 static int wrap_mmap(struct file *file, struct vm_area_struct *vma)
 {
 	struct wrap_ctx *ctx = file->private_data;
-	struct wrap_ctx_mapping *mapping;
 	struct wrap_content *content;
 	int ret = 0;
 
@@ -476,25 +493,51 @@ unlock:
 		goto err;
 
 	/* If we reached here then ctx->map_count has been incremented */
-	mapping = kmalloc(sizeof(*mapping), GFP_KERNEL);
-	if (!mapping) {
-		ret = -ENOMEM;
-		goto err_dec;
-	}
-
 	ret = content->ops->mmap(content, vma);
-	if (ret) {
-		kfree(mapping);
+	if (ret)
 		goto err_dec;
-	}
 
-	mapping->ctx = ctx;
-	mapping->vm_ops = vma->vm_ops;
-	mapping->vm_private_data = vma->vm_private_data;
+	/*
+	 * Vma can't be used yet, so we can hash the context before taking
+	 * the lock to allow sleepable allocations while hashing.
+	 */
+	ret = hash_ctx(vma, ctx);
+	if (ret)
+		goto err_undo_map;
+
+	spin_lock(&ctx->lock);
+	if (!ctx->vm_ops) {
+		if (vma->vm_ops) {
+			/* Confirm no extra ops are defined */
+			struct vm_operations_struct cmp_vm_ops = {
+				.open		= vma->vm_ops->open,
+				.close		= vma->vm_ops->close,
+				.fault		= vma->vm_ops->fault,
+			};
+			if (memcmp(&cmp_vm_ops, vma->vm_ops,
+				   sizeof(cmp_vm_ops))) {
+				ret = -EINVAL;
+				goto err_unlock;
+			}
+		}
+		ctx->vm_ops = vma->vm_ops;
+	} else {
+		/* We expect content to use the same vm_ops for all its mappings. */
+		if (ctx->vm_ops != vma->vm_ops) {
+			ret = -EINVAL;
+			goto err_unlock;
+		}
+	}
 	vma->vm_ops = &wrap_vm_ops;
-	vma->vm_private_data = mapping;
+	spin_unlock(&ctx->lock);
 
 	return 0;
+err_unlock:
+	spin_unlock(&ctx->lock);
+	unhash_ctx(vma);
+err_undo_map:
+	if (vma->vm_ops && vma->vm_ops->close)
+		vma->vm_ops->close(vma);
 err_dec:
 	spin_lock(&ctx->lock);
 	ctx->map_count--;
@@ -698,7 +741,6 @@ restore_content:
 out:
 	return ret;
 }
-
 
 static int wrap_file_empty(struct wrap_ctx *ctx)
 {
