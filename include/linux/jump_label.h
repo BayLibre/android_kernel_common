59f9a756067e2c1eb4a7da4f0e7f251c379a6944
Eric Biggers <ebiggers@google.com>
2026-02-12 03:49:32 +0000

    ANDROID: jump_label: allow fips140.ko to change static keys
    Start allowing static keys to be changed by fips140.ko.  This is needed
    because some of the arm64-optimized crypto code has started using static
    keys internally.
    
    Previously, support for changing static keys was intentionally not
    provided to fips140.ko, first because it was unneeded, and second
    because it could allow static keys to get out of sync with the
    corresponding jump labels (considering that fips140.ko has jump labels
    disabled but uses the jump-label-enabled struct for ABI compatibility).
    
    However, the latter is not an issue for static keys used only internally
    in fips140.ko, which is what this is needed for.
    
    Note: for the next forward port after 6.18, fold this into
    "ANDROID: jump_label: disable jump labels in fips140.ko"
    and update the commit message accordingly.
    
    Bug: 188620248
    Bug: 469464730
    Change-Id: I9dd09d9b29c9048aadcd75e55100172ab82ef803
    Signed-off-by: Eric Biggers <ebiggers@google.com>

--- include/linux/jump_label.h
+++ include/linux/jump_label.h
@@ -109,6 +109,16 @@ struct static_key {
 
 #endif /* __ASSEMBLY__ */
 
+/*
+ * Since the fips140 module doesn't support code patching, it uses the
+ * CONFIG_JUMP_LABEL=n implementation of static keys.
+ *
+ * However, to allow the fips140 module to use static keys that are exported by
+ * the core kernel, it uses the CONFIG_JUMP_LABEL=y definition of the struct.
+ *
+ * Care should be taken not to export any static keys from the fips140 module.
+ * That would not work as expected, due to the mismatched implementations.
+ */
 #if defined(CONFIG_JUMP_LABEL) && !defined(BUILD_FIPS140_KO)
 #include <asm/jump_label.h>
 
@@ -196,30 +206,7 @@ enum jump_label_type {
 
 struct module;
 
-#ifdef BUILD_FIPS140_KO
-
-#include <linux/atomic.h>
-
-static inline int static_key_count(struct static_key *key)
-{
-	return raw_atomic_read(&key->enabled);
-}
-
-static __always_inline bool static_key_false(struct static_key *key)
-{
-	if (unlikely(static_key_count(key) > 0))
-		return true;
-	return false;
-}
-
-static __always_inline bool static_key_true(struct static_key *key)
-{
-	if (likely(static_key_count(key) > 0))
-		return true;
-	return false;
-}
-
-#elif defined(CONFIG_JUMP_LABEL)
+#if defined(CONFIG_JUMP_LABEL) && !defined(BUILD_FIPS140_KO)
 
 #define JUMP_TYPE_FALSE		0UL
 #define JUMP_TYPE_TRUE		1UL
