7e1e9ec9614335ec30f30082900c3a54b7c55bcf
Suren Baghdasaryan <surenb@google.com>
2026-02-18 17:16:00 +0000

    ANDROID: drivers/wrapfd: remove vm_flags modification when mapping the vma
    Remove vm_flags modifications when mapping a wrapfd file. Setting of the
    vm_flags is left up to the dmabuf heap it's allocated from.
    
    Bug: 480234490
    Change-Id: Ibfa2465dff5ab3e7516406231449b731cf64679d
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- drivers/android/wrapfd.c
+++ drivers/android/wrapfd.c
@@ -152,8 +152,6 @@ static int dmabuf_content_mmap_prepare(struct wrap_content *content,
 			return -EINVAL;
 	}
 
-	vm_flags_set(vma, VM_SHARED | VM_PFNMAP | VM_DONTEXPAND | VM_DONTDUMP);
-
 	return 0;
 }
 
