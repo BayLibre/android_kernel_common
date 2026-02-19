403f683ce626830b9c9b60a5c182f4dfedd56b9c
Richard Chang <richardycc@google.com>
2026-02-19 16:05:06 +0000

    ANDROID: zram: cache backing device block index for prefetched pages
    When a page is prefetched from the backing device, zram currently
    releases the associated backing device block immediately. This can lead
    to race conditions if a page fault and prefetch occur simultaneously.
    
    To address this, introduce a 'prefetch_cache' using an xarray to track
    backing device block indices (blk_idx) for prefetched pages. Instead of
    releasing the block in zram_populate_table(), the blk_idx is stored in
    the prefetch_cache. The block is only released when
    zram_slot_free_notify() is called, ensuring data validity on the backing
    device as long as the swap slot is active.
    
    This mechanism also enables an optimization: if a prefetched page is
    selected for writeback again, zram can reuse the existing blk_idx from
    the prefetch_cache, eliminating redundant write operations.
    
    Additionally, skip recompression for prefetched slots to prevent
    potential decompression algorithm mismatches during subsequent page
    faults.
    
    Bug: 340137613
    Change-Id: I8de78bb0a83465a1986f45074620166b482eb9b1
    Signed-off-by: Richard Chang <richardycc@google.com>

--- drivers/block/zram/zram_drv.h
+++ drivers/block/zram/zram_drv.h
@@ -18,6 +18,10 @@
 #include <linux/rwsem.h>
 #include <linux/zsmalloc.h>
 
+#if IS_ENABLED(CONFIG_ZRAM_ANDROID_IOCTL)
+#include <linux/xarray.h>
+#endif
+
 #include "zcomp.h"
 
 #define SECTORS_PER_PAGE_SHIFT	(PAGE_SHIFT - SECTOR_SHIFT)
@@ -135,6 +139,9 @@ struct zram {
 	unsigned long *bitmap;
 	unsigned long nr_pages;
 #endif
+#if IS_ENABLED(CONFIG_ZRAM_ANDROID_IOCTL)
+	struct xarray prefetch_cache;
+#endif
 #ifdef CONFIG_ZRAM_MEMORY_TRACKING
 	struct dentry *debugfs_dir;
 #endif
@@ -183,6 +190,21 @@ int zram_writeback_slots(struct zram *zram,
 int scan_slot_for_prefetch(struct zram *zram, unsigned long index,
 			   struct zram_pp_ctl *ctl);
 int zram_prefetch_slots(struct zram *zram, struct zram_pp_ctl *ctl);
+#endif
+
+#if IS_ENABLED(CONFIG_ZRAM_ANDROID_IOCTL)
+int zram_prefetch_cache_reuse(struct zram *zram, u32 index);
+int zram_prefetch_cache_drop(struct zram *zram, u32 index);
+#else
+static inline int zram_prefetch_cache_repuse(struct zram *zram, u32 index)
+{
+	return -ENOTSUPP;
+}
+
+static inline int zram_prefetch_cache_drop(struct zram *zram, u32 index)
+{
+	return -ENOTSUPP;
+}
 #endif
 
 #endif
