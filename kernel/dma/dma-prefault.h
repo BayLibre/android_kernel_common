6bf27233d697c023ee4707b478b13bd04e78d9c8
Yuvraj Sakshith <yuvraj.sakshith@oss.qualcomm.com>
2026-02-20 01:13:38 -0800

    ANDROID: dma: add dma_prefault_range()
    dma_prefault_range() when called, makes a READ_ONCE() on all
    these pages which are meant to be DMA-ed into.
    This method takes in an address and the size of the region.
    This way, all these pages can be faulted into memory in a demand paged environment.
    
    Bug: 497094521
    Change-Id: Ieae51afef56b22ff5297b238b9e5b99372c09ba4
    Signed-off-by: Yuvraj Sakshith <yuvraj.sakshith@oss.qualcomm.com>

--- kernel/dma/dma-prefault.h
+++ kernel/dma/dma-prefault.h
@@ -24,4 +24,30 @@ static inline void dma_prefault_page_range(struct page *page,
 	}
 }
 
+static inline void dma_prefault_range(const void *addr, size_t size)
+{
+	unsigned long start = ALIGN_DOWN((unsigned long)addr, PAGE_SIZE);
+	unsigned long end   = ALIGN_DOWN((unsigned long)addr + size - 1, PAGE_SIZE);
+
+	for (unsigned long a = start; a <= end; a += PAGE_SIZE) {
+		if (is_vmalloc_addr((void *)a)) {
+			struct page *p = vmalloc_to_page((void *)a);
+			if (!p)
+				continue;
+
+			void *vaddr = kmap_local_page(p);
+
+			READ_ONCE(*(volatile char *)vaddr);
+
+			kunmap_local(vaddr);
+		} else if (virt_addr_valid((void *)a)) {
+			unsigned long vaddr = a;
+
+			READ_ONCE(*(volatile char *)vaddr);
+		} else {
+			continue;
+		}
+	}
+}
+
 #endif /* _LINUX_DMA_PREFAULT_H */
