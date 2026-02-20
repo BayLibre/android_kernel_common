ad2637dee223632c72dd7f2824b0df7327005903
Suren Baghdasaryan <surenb@google.com>
2026-02-20 01:34:20 +0000

    ANDROID: drivers/wrapfd: move access permission check
    Move generic access permission check out of content-specific
    mmap_prepare() and into generic wrap_mmap(). Fix the returned
    error code and clear VM_MAYWRITE for mappings of read-only content
    so that they can't be made writable later.
    
    Bug: 480234490
    Change-Id: I6baa31141bf0277016c9df9d663f4e51b72dc386
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- drivers/android/wrapfd.c
+++ drivers/android/wrapfd.c
@@ -186,21 +186,6 @@ err_detach:
 	return 0;
 }
 
-static int dmabuf_content_mmap_prepare(struct wrap_content *content,
-				       struct vm_area_struct *vma)
-{
-	struct wrap_content_dmabuf *dmabuf_content;
-
-	dmabuf_content = container_of(content, struct wrap_content_dmabuf,
-				      content);
-	if (vma->vm_flags & VM_MAYWRITE) {
-		if (!dmabuf_content->writable)
-			return -EINVAL;
-	}
-
-	return 0;
-}
-
 static int dmabuf_content_mmap(struct wrap_content *content,
 			       struct vm_area_struct *vma)
 {
@@ -301,7 +286,6 @@ static int dmabuf_content_ioctl(struct wrap_content *content,
 static struct wrap_content_operations dmabuf_content_ops = {
 	.create_wrap		= dmabuf_content_create_wrap,
 	.load			= dmabuf_content_load,
-	.mmap_prepare		= dmabuf_content_mmap_prepare,
 	.mmap			= dmabuf_content_mmap,
 	.make_writable		= dmabuf_content_make_writable,
 	.is_writable		= dmabuf_content_is_writable,
@@ -481,6 +465,7 @@ static int wrap_mmap(struct file *file, struct vm_area_struct *vma)
 	struct wrap_ctx *ctx = file->private_data;
 	struct wrap_ctx_mapping *mapping;
 	struct wrap_content *content;
+	bool make_rdonly = false;
 	int ret = 0;
 
 	spin_lock(&ctx->lock);
@@ -496,15 +481,29 @@ static int wrap_mmap(struct file *file, struct vm_area_struct *vma)
 		goto unlock;
 	}
 
-	ret = content->ops->mmap_prepare(content, vma);
-	if (!ret) {
-		/*
-		 * Increased map_count prevents changes in the ownership,
-		 * rewrapping or emptying the content. Therefore content
-		 * is stable.
-		 */
-		ctx->map_count++;
+	/* Handle read-only content */
+	if (content->ops->is_writable &&
+	    !content->ops->is_writable(content)) {
+		if (vma->vm_flags & VM_WRITE) {
+			ret = -EACCES;
+			goto unlock;
+		}
+		make_rdonly = !!(vma->vm_flags & VM_MAYWRITE);
 	}
+
+	if (content->ops->mmap_prepare) {
+		ret = content->ops->mmap_prepare(content, vma);
+		if (ret) {
+			ret = -EINVAL;
+			goto unlock;
+		}
+	}
+	/*
+	 * Increased map_count prevents changes in the
+	 * ownership, rewrapping or emptying the content.
+	 * Therefore content is stable.
+	 */
+	ctx->map_count++;
 unlock:
 	spin_unlock(&ctx->lock);
 
@@ -522,6 +521,16 @@ unlock:
 	if (ret) {
 		kfree(mapping);
 		goto err_dec;
+	}
+
+	if (make_rdonly) {
+		/*
+		 * content->ops->mmap should not be mapping read-only content
+		 * as writable. Either content->ops->is_writable reports
+		 * incorrect value or content->ops->mmap is misbehaving.
+		 */
+		BUG_ON(vma->vm_flags & VM_WRITE);
+		vm_flags_clear(vma, VM_MAYWRITE);
 	}
 
 	spin_lock(&ctx->lock);
