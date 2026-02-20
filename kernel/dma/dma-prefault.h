f8a4198a2a791103ace7e71b7f1c71d7f5f0730e
Yuvraj Sakshith <yuvraj.sakshith@oss.qualcomm.com>
2026-02-20 02:03:43 -0800

    ANDROID: dma: add dma_prefault_sgt()
    dma_prefault_sgt() when called, makes a READ_ONCE() on all
    these pages which are meant to be DMA-ed into passed as a
    scatterlist. This way, all these pages can be faulted into
    memory in a demand paged environment.
    
    Bug: 497094521
    Change-Id: I52b8d01b9f0597162708a3cfa22762b62dac28ac
    Signed-off-by: Yuvraj Sakshith <yuvraj.sakshith@oss.qualcomm.com>

--- kernel/dma/dma-prefault.h
+++ kernel/dma/dma-prefault.h
@@ -18,7 +18,7 @@ static inline void dma_prefault_page_range(struct page *page,
 
 		void *vaddr = kmap_local_page(p);
 
-        READ_ONCE(*(volatile char *)vaddr);
+        READ_ONCE(*(char *)vaddr);
 
         kunmap_local(vaddr);
 	}
@@ -31,23 +31,50 @@ static inline void dma_prefault_range(const void *addr, size_t size)
 
 	for (unsigned long a = start; a <= end; a += PAGE_SIZE) {
 		if (is_vmalloc_addr((void *)a)) {
-			struct page *p = vmalloc_to_page((void *)a);
-			if (!p)
-				continue;
-
-			void *vaddr = kmap_local_page(p);
-
-			READ_ONCE(*(volatile char *)vaddr);
-
-			kunmap_local(vaddr);
-		} else if (virt_addr_valid((void *)a)) {
 			unsigned long vaddr = a;
 
 			READ_ONCE(*(volatile char *)vaddr);
+
+		} else if (virt_addr_valid((void *)a)) {
+			unsigned long vaddr = a;
+
+			READ_ONCE(*(char *)vaddr);
 		} else {
 			continue;
 		}
 	}
 }
+
+
+static inline void dma_prefault_sgt(struct sg_table *sgt)
+{
+	struct scatterlist *sg;
+	int i;
+
+	for_each_sg(sgt->sgl, sg, sgt->nents, i) {
+		struct page *p = sg_page(sg);
+		size_t off = sg->offset;
+		size_t len = sg->length;
+
+		while (len) {
+			size_t off_in_page = off & (PAGE_SIZE - 1);
+			size_t chunk = min_t(size_t, len,
+						PAGE_SIZE - off_in_page);
+
+			void *vaddr = kmap_local_page(p);
+
+			dma_prefault_range(vaddr + off_in_page, chunk);
+
+			kunmap_local(vaddr);
+
+			off += chunk;
+			len -= chunk;
+
+			if (!(off & (PAGE_SIZE - 1)))
+				p = nth_page(p, 1);
+		}
+	}
+}
+
 
 #endif /* _LINUX_DMA_PREFAULT_H */
