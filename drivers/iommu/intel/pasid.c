vineethrp/pKVM-IA

ANDROID: iommu/vt-d: pKVM: Pasid table entry teardown hypercall
Implement hypercalls for clearing pasid table entries and
host driver changes to use the hypercall.

Bug: 473885291
Upstream-Task: 402758258
Test: Build and Boot

Change-Id: Ic32fa5009df5bc31366d096e364ff9a71c57b604
Signed-off-by: Vineeth Pillai <vineethrp@google.com>

drivers/iommu/intel/pasid.c
@@ -263,6 +263,23 @@ void intel_pasid_tear_down_entry(struct intel_iommu *iommu, struct device *dev,
 	struct pasid_entry *pte;
 	u16 did, pgtt;
 
+#ifndef __PKVM_HYP__
+	if (pkvm_enabled()) {
+		struct device_domain_info *info = dev_iommu_priv_get(dev);
+		int ret;
+
+		if (WARN_ON(!info || !info->pasid_table))
+			return;
+
+		ret = pv_pasid_teardown(info, pasid);
+		if (ret)
+			pr_err("iommu%d: pv_pasid_teardown failed(err=%d)\n",
+			       iommu->seq_id, ret);
+
+		return;
+	}
+#endif
+
 	iommu_lock(iommu);
 	pte = intel_pasid_get_entry(dev, pasid);
 	if (WARN_ON(IS_ERR(pte))) {

BayLibre/android_kernel_common

Bug: 470050354

drivers/iommu/intel/pasid.c
@@ -292,6 +292,23 @@ void intel_pasid_tear_down_entry(struct intel_iommu *iommu, struct pkvm_device *
 	struct pasid_entry *pte;
 	u16 did, pgtt;
 
+#ifndef __PKVM_HYP__
+	if (pkvm_enabled()) {
+		struct device_domain_info *info = dev_iommu_priv_get(dev);
+		int ret;
+
+		if (WARN_ON(!info || !info->pasid_table))
+			return;
+
+		ret = pkvm_pasid_teardown(info, pasid);
+		if (ret)
+			pr_err("iommu%d: pkvm_pasid_teardown failed(err=%d)\n",
+			       iommu->seq_id, ret);
+
+		return;
+	}
+#endif
+
 	spin_lock(&iommu->lock);
 	pte = intel_pasid_get_entry(dev, pasid);
 	if (WARN_ON(IS_ERR(pte))) {