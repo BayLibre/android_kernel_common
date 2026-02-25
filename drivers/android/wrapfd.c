dd0206ba4e61286324e6d5732fb286467d8372d6
Isaac J. Manjarres <isaacmanjarres@google.com>
2026-02-25 21:36:38 +0000

    ANDROID: drivers/wrapfd: Validate ioctl command structures' reserved fields
    Reserved fields for ioctl command structures should be checked to ensure
    that they are zero. Otherwise, when it comes time to use them, userspace
    applications that passed in garbage via those reserved fields will
    break.
    
    Bug: 480234490
    Change-Id: I21ecee97ef47e34a034430a988ddc326e54238fd
    Signed-off-by: Isaac J. Manjarres <isaacmanjarres@google.com>

--- drivers/android/wrapfd.c
+++ drivers/android/wrapfd.c
@@ -633,6 +633,9 @@ static int wrap_file_load(struct wrap_ctx *ctx,
 	if (!PAGE_ALIGNED(wrapfd_load.buf_offs))
 		return -EINVAL;
 
+	if (wrapfd_load.reserved)
+		return -EINVAL;
+
 	file = fget(wrapfd_load.fd);
 	if (!file)
 		return -EBADF;
@@ -700,6 +703,9 @@ static int wrap_file_rewrap(struct wrap_ctx *ctx,
 		return -EFAULT;
 
 	if (wrapfd_rewrap.prot & ~(PROT_WRITE | PROT_READ))
+		return -EINVAL;
+
+	if (wrapfd_rewrap.reserved)
 		return -EINVAL;
 
 	spin_lock(&ctx->lock);
@@ -995,6 +1001,9 @@ static int wrap_file(struct wrap_ctx *ctx,
 	if (wrapfd_wrap.prot & ~(PROT_WRITE | PROT_READ))
 		return -EINVAL;
 
+	if (wrapfd_wrap.reserved)
+		return -EINVAL;
+
 	content = create_content_for(wrapfd_wrap.fd, wrapfd_wrap.prot);
 	if (IS_ERR(content))
 		return PTR_ERR(content);
@@ -1017,6 +1026,9 @@ static int get_wrap_state(struct wrapfd_get_state __user *user_wrapfd_get_state)
 	if (copy_from_user(&wrapfd_get_state, user_wrapfd_get_state,
 			   sizeof(wrapfd_get_state)))
 		return -EFAULT;
+
+	if (wrapfd_get_state.reserved)
+		return -EINVAL;
 
 	file = fget(wrapfd_get_state.fd);
 	if (!file)
