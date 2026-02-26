6eb11a413a80c567328de18f3ff4fb1ee00fda09
Eric Biggers <ebiggers@google.com>
2026-02-26 23:03:14 +0000

    ANDROID: fips140: move lib override support into kconfig option
    Commit 1aa0ebd06571 ("ANDROID: lib/crypto: Support overriding crypto
    library functions") added support for library function overrides
    unconditionally.  Since I didn't correctly handle the case where the
    library files are built as modules, several fixes were needed:
    
      - commit bd41fa5ccb8c
        ("ANDROID: lib/crypto: fix build failures for crypto modules")
      - commit 8ed4fbecf30a
        ("ANDROID: lib/crypto: Select CRYPTO_LIB_UTILS with SHA256")
    
    However, that configuration does not reflect the intended use case for
    fips140.ko.  Instead, let's make the support for library function
    overrides be conditional on its own kconfig option, which depends on the
    corresponding functions being built into the core kernel.  Enable that
    kconfig option in the arm64 gki_defconfig.
    
    Bug: 469464730
    Change-Id: I265e1ddbce585abc865c28b4b47d9f8351926893
    Signed-off-by: Eric Biggers <ebiggers@google.com>

---
+++ lib/crypto/fips140.c
@@ -0,0 +1,12 @@
+#include <crypto/fips140-lib-overrides.h>
+
+struct fips140_lib_funcs fips140_lib_funcs;
+DEFINE_STATIC_KEY_FALSE(fips140_lib_funcs_loaded);
+
+void register_fips140_lib_funcs(const struct fips140_lib_funcs *funcs)
+{
+	WARN_ON(static_key_enabled(&fips140_lib_funcs_loaded));
+	fips140_lib_funcs = *funcs;
+	static_branch_enable(&fips140_lib_funcs_loaded);
+}
+EXPORT_SYMBOL_GPL(register_fips140_lib_funcs);
