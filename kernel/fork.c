5372107063efff52776559ce3217564d7c2649f7
Suren Baghdasaryan <surenb@google.com>
2026-02-26 11:22:52 -0800

    ANDROID: mm: store mm while exiting to help process_mrelease find it
    When a process is exiting it resets current->mm in exit_mm() which can
    happen much earlier than the last mm user drops the last mm refcount
    and mm gets reclaimed. This prevents process_mrelease() from finding
    the mm to reap once current->mm gets reset, which results in mm reclaim
    being delayed potentially for a long time (until the last mm reference
    gets dropped). Help process_mrelease() to find the mm even after exit_mm()
    by storing task-to-mm association into a side xarray which is used by
    process_mrelease() to find the right mm. The association is removed once
    exit_mmap() is over and mm areas are released.
    
    Bug: 486894305
    Change-Id: I88681f4462d599f9c6298323e2cfa4c3e4492de3
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -1351,6 +1351,7 @@ static inline void __mmput(struct mm_struct *mm)
 	ksm_exit(mm);
 	khugepaged_exit(mm); /* must run before exit_mmap */
 	exit_mmap(mm);
+	erase_exiting_mm(mm);
 	mm_put_huge_zero_folio(mm);
 	set_mm_exe_file(mm, NULL);
 	if (!list_empty(&mm->mmlist)) {
@@ -1659,6 +1660,7 @@ static void mm_release(struct task_struct *tsk, struct mm_struct *mm)
 
 void exit_mm_release(struct task_struct *tsk, struct mm_struct *mm)
 {
+	store_exiting_mm(tsk, mm);
 	futex_exit_release(tsk);
 	mm_release(tsk, mm);
 }
