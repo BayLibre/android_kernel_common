71201062f6d8841952cdd34bb2fc79401c8c9c02
Suren Baghdasaryan <surenb@google.com>
2026-02-28 02:31:24 +0000

    ANDROID: mm: remove mm from LRU gen list on exit
    Remove process mm from the MGLRU gen list upon exit to prevent kswapd
    from trying to reclaim it and pinning it in the process, preventing
    exit_mmap() from doing the final cleanup.
    
    Bug: 486894305
    Change-Id: I32e6eb861c87224ad5619359bc1ac0089270bb2b
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -3052,6 +3052,12 @@ void lru_gen_del_mm(struct mm_struct *mm)
 
 	spin_lock(&mm_list->lock);
 
+	/* Confirm under lock */
+	if (list_empty(&mm->lru_gen.list)) {
+		spin_unlock(&mm_list->lock);
+		return;
+	}
+
 	for_each_node(nid) {
 		struct lruvec *lruvec = get_lruvec(memcg, nid);
 		struct lru_gen_mm_state *mm_state = get_mm_state(lruvec);
