183f7e74247d140a2f9da727c1b2bbdb0cbb5da0
Richard Chang <richardycc@google.com>
2026-03-03 07:30:24 +0000

    ANDROID: zram: support address range and size limits for process writeback
    Add the ZRAM_ANDROID_IOC_PROCESS_RANGE_WRITEBACK IOCTL to allow more
    granular control over memory regions scanned and data processed.
    
    Add 'start_addr' and 'size' to support a limited size of process
    writeback. The userspace can use the 'next_addr' as the new 'start_addr'
    for the next ioctl calls.
    
    This enhancement allows userspace to manage zram writeback more
    efficiently by processing large address spaces in smaller, manageable
    chunks without holding locks for excessive periods.
    
    For kernel backward compatibility, we maintained for the legacy
    ZRAM_ANDROID_IOC_PROCESS_WRITEBACK IOCTL by
    mapping the legacy ioctl call into an unlimited range of
    ZRAM_ANDROID_IOC_PROCESS_RANGE_WRITEBACK ioctl call internally.
    
    Bug: 340137613
    Change-Id: I8c41115d45ab17d628120e160d6af0815bbcd7b1
    Signed-off-by: Richard Chang <richardycc@google.com>

--- include/uapi/linux/zram_ioctl.h
+++ include/uapi/linux/zram_ioctl.h
@@ -6,19 +6,49 @@
 #include <linux/types.h>
 #include <linux/ioctl.h>
 
+/* Legacy */
 struct zram_android_ioc_data_process_writeback {
 	__aligned_u64	pidfd;
+	__u64		written_bytes;
+};
+
+struct zram_android_ioc_data_process_range_writeback {
+	/* The pidfd of the process to scan for writeback candidates */
+	__aligned_u64	pidfd;
+	/* The starting virtual address for the scan */
+	__u64		start_addr;
+	/**
+	 * Maximum number of candidate bytes to scan for writeback. The scan
+	 * process will iterate the process address space from the @start_addr,
+	 * and stop when the number of writeback candidates is larger than
+	 * @size. If the @size is zero, the scan range is unlimited.
+	 */
+	__u64		size;
+	/**
+	 * The address where the scan stopped exclusively. Can be used as the
+	 * @start_addr for the next call. If the scan reaches the end of the
+	 * address space, it will be set to zero.
+	 */
+	__u64		next_addr;
+	/* Total number of bytes successfully written back */
 	__u64		written_bytes;
 };
 
 struct zram_android_ioc_data {
 	union {
 		struct zram_android_ioc_data_process_writeback process_writeback;
+		struct zram_android_ioc_data_process_range_writeback process_range_writeback;
 	} data;
 };
 
 #define ZRAM_ANDROID_IOC_MAGIC 0xBB
-#define ZRAM_ANDROID_IOC_PROCESS_WRITEBACK _IOWR(ZRAM_ANDROID_IOC_MAGIC, 1, struct zram_android_ioc_data)
+
+/* Legacy API: keep it in 6.12 for compatibility */
+#define ZRAM_ANDROID_IOC_PROCESS_WRITEBACK \
+	_IOWR(ZRAM_ANDROID_IOC_MAGIC, 1, struct zram_android_ioc_data)
+
+#define ZRAM_ANDROID_IOC_PROCESS_RANGE_WRITEBACK \
+	_IOWR(ZRAM_ANDROID_IOC_MAGIC, 2, struct zram_android_ioc_data)
 
 #endif /* _UAPI_LINUX_ZRAM_IOCTL_H */
 
