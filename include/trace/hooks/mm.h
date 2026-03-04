7a11949e19cc0c066b47a51c0e533384f1225485
Elena Yu <elena.yu@mediatek.corp-partner.google.com>
2026-03-04 13:30:27 +0800

    ANDROID: mm: Add vendor hooks for compaction and __alloc_pages_slowpath
    To monitor the efficiency of memory compaction and direct reclaim actions.
    For compaction, add vendor_hook functions and call them in kcompactd_do_work() and try_to_compact_pages() to monitor each compaction action:
    android_vh_compaction_exit(int, int, const int)
    android_vh_compaction_try_to_compact_exit(enum compact_result *)
    
    For __alloc_pages_slowpath, add vendor hooks in each stage, including __alloc_pages_may_oom and __alloc_pages_direct_reclaim, to monitor memory reclaim efficiency:
    android_vh_mm_direct_reclaim_enter()
    android_vh_mm_direct_reclaim_exit(unsigned long, int)
    android_vh_mm_may_oom_exit(struct oom_control *, unsigned long)
    
    Change-Id: I9b6a00817b5d5ee51ca5ca9ffbf4821674119741
    Signed-off-by: Elena Yu <elena.yu@mediatek.corp-partner.google.com>

--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -437,6 +437,23 @@ DECLARE_HOOK(android_vh_folios_put_refs_direct_free_extent,
 DECLARE_HOOK(android_vh_free_pages_and_swap_cache,
 	TP_PROTO(struct folio *folio),
 	TP_ARGS(folio));
+DECLARE_HOOK(android_vh_compaction_exit,
+	TP_PROTO(int node_id, int order, const int highest_zoneidx),
+	TP_ARGS(node_id, order, highest_zoneidx));
+enum compact_result;
+DECLARE_HOOK(android_vh_compaction_try_to_compact_exit,
+        TP_PROTO(enum compact_result *compact_result),
+        TP_ARGS(compact_result));
+DECLARE_HOOK(android_vh_mm_direct_reclaim_enter,
+	TP_PROTO(unsigned int order),
+	TP_ARGS(order));
+DECLARE_HOOK(android_vh_mm_direct_reclaim_exit,
+	TP_PROTO(unsigned long did_some_progress, int retry_times),
+	TP_ARGS(did_some_progress, retry_times));
+struct oom_control;
+DECLARE_HOOK(android_vh_mm_may_oom_exit,
+	TP_PROTO(struct oom_control *oc, unsigned long did_some_progress),
+	TP_ARGS(oc, did_some_progress));
 #endif /* _TRACE_HOOK_MM_H */
 
 /* This part must be outside protection */
