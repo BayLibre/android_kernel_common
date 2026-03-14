4a6e3b87d3d9100d28eca9c1965f59dc90996b99
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2026-03-13 20:52:37 -0700

    Revert "FROMLIST: blk-zoned: Add an argument to blk_zone_plug_bio()"
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/e074eced-0a3b-4be3-8812-8bdb2b6dc354). Please review this revert and approve if it looks good to you.
    
    Change-Id: Ie91de844ade9e6b65280a5ee9de3aabea5bd4b01
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com
    Bug: 234829282
    Bug: 449729210

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -922,7 +922,7 @@ static inline bool bio_needs_zone_write_plugging(struct bio *bio)
 	}
 }
 
-bool blk_zone_plug_bio(struct bio *bio, unsigned int nr_segs, int rq_cpu);
+bool blk_zone_plug_bio(struct bio *bio, unsigned int nr_segs);
 
 /**
  * disk_zone_capacity - returns the zone capacity of zone containing @sector
@@ -962,8 +962,7 @@ static inline bool bio_needs_zone_write_plugging(struct bio *bio)
 	return false;
 }
 
-static inline bool blk_zone_plug_bio(struct bio *bio, unsigned int nr_segs,
-				     int rq_cpu)
+static inline bool blk_zone_plug_bio(struct bio *bio, unsigned int nr_segs)
 {
 	return false;
 }
