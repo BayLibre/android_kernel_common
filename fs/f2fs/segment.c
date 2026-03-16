87f394f1525382b3fe50b5e8b4fa664d20acedec
Daeho Jeong <daehojeong@google.com>
2026-03-16 11:59:54 -0700

    BACKPORT: f2fs: fix to freeze GC and discard threads quickly
    Suspend can fail if kernel threads do not freeze for a while.
    f2fs_gc and f2fs_discard threads can perform long-running operations
    that prevent them from reaching a freeze point in a timely manner.
    
    This patch adds explicit freezing checks in the following locations:
    1. f2fs_gc: Added a check at the 'retry' label to exit the loop quickly
       if freezing is requested, especially during heavy GC rounds.
    2. __issue_discard_cmd: Added a 'suspended' flag to break both inner and
       outer loops during discard command issuance if freezing is detected
       after at least one command has been issued.
    3. __issue_discard_cmd_orderly: Added a similar check for orderly discard
       to ensure responsiveness.
    
    These checks ensure that the threads release locks safely and enter the
    frozen state.
    
    Change-Id: I0d93c434022e3e6501645273d2013e71d5791df5
    Signed-off-by: Daeho Jeong <daehojeong@google.com>
    Reviewed-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    
    Bug: 486579857
    (cherry picked from commit 62b45b0ad4cddebbba1ad5e7c48cf7b5e42c2377
    https: //git.kernel.org/pub/scm/linux/kernel/git/jaegeuk/f2fs.git dev)
    Link: https://lore.kernel.org/linux-f2fs-devel/20260316185954.2185806-1-daeho43@gmail.com
    Change-Id: Ia2c9de504800b7588fa05da4fd2102d9e7cafb0d
    Signed-off-by: Daeho Jeong <daehojeong@google.com>

--- fs/f2fs/segment.c
+++ fs/f2fs/segment.c
@@ -1621,6 +1621,9 @@ static void __issue_discard_cmd_orderly(struct f2fs_sb_info *sbi,
 		if (dc->state != D_PREP)
 			goto next;
 
+		if (*issued > 0 && unlikely(freezing(current)))
+			break;
+
 		if (dpolicy->io_aware && !is_idle(sbi, DISCARD_TIME)) {
 			io_interrupted = true;
 			break;
@@ -1660,6 +1663,7 @@ static int __issue_discard_cmd(struct f2fs_sb_info *sbi,
 	struct blk_plug plug;
 	int i, issued;
 	bool io_interrupted = false;
+	bool suspended = false;
 
 	if (dpolicy->timeout)
 		f2fs_update_time(sbi, UMOUNT_DISCARD_TIMEOUT);
@@ -1690,6 +1694,11 @@ retry:
 		list_for_each_entry_safe(dc, tmp, pend_list, list) {
 			f2fs_bug_on(sbi, dc->state != D_PREP);
 
+			if (issued > 0 && unlikely(freezing(current))) {
+				suspended = true;
+				break;
+			}
+
 			if (dpolicy->timeout &&
 				f2fs_time_over(sbi, UMOUNT_DISCARD_TIMEOUT))
 				break;
@@ -1709,7 +1718,8 @@ retry:
 next:
 		mutex_unlock(&dcc->cmd_lock);
 
-		if (issued >= dpolicy->max_requests || io_interrupted)
+		if (issued >= dpolicy->max_requests || io_interrupted ||
+					suspended)
 			break;
 	}
 
