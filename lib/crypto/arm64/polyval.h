dc8c73430fe503fb4d317bfa78b75376db232217
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2026-03-18 23:28:43 -0700

    Revert "BACKPORT: lib/crypto: arm64: Move remaining algorithms t..."
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/08fb23d1-9c97-4d9a-8e93-ced4ca7f3cc9). Please review this revert and approve if it looks good to you.
    
    Change-Id: I6848f6eb84cf45be3154e41f54144a7e05cf4239
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com
    Bug: 469786543

--- lib/crypto/arm64/polyval.h
+++ lib/crypto/arm64/polyval.h
@@ -4,6 +4,7 @@
  *
  * Copyright 2025 Google LLC
  */
+#include <asm/neon.h>
 #include <asm/simd.h>
 #include <linux/cpufeature.h>
 
@@ -23,14 +24,13 @@ static void polyval_preparekey_arch(struct polyval_key *key,
 	static_assert(ARRAY_SIZE(key->h_powers) == NUM_H_POWERS);
 	memcpy(&key->h_powers[NUM_H_POWERS - 1], raw_key, POLYVAL_BLOCK_SIZE);
 	if (static_branch_likely(&have_pmull) && may_use_simd()) {
-		scoped_ksimd() {
-			for (int i = NUM_H_POWERS - 2; i >= 0; i--) {
-				key->h_powers[i] = key->h_powers[i + 1];
-				polyval_mul_pmull(
-					&key->h_powers[i],
-					&key->h_powers[NUM_H_POWERS - 1]);
-			}
+		kernel_neon_begin();
+		for (int i = NUM_H_POWERS - 2; i >= 0; i--) {
+			key->h_powers[i] = key->h_powers[i + 1];
+			polyval_mul_pmull(&key->h_powers[i],
+					  &key->h_powers[NUM_H_POWERS - 1]);
 		}
+		kernel_neon_end();
 	} else {
 		for (int i = NUM_H_POWERS - 2; i >= 0; i--) {
 			key->h_powers[i] = key->h_powers[i + 1];
@@ -44,8 +44,9 @@ static void polyval_mul_arch(struct polyval_elem *acc,
 			     const struct polyval_key *key)
 {
 	if (static_branch_likely(&have_pmull) && may_use_simd()) {
-		scoped_ksimd()
-			polyval_mul_pmull(acc, &key->h_powers[NUM_H_POWERS - 1]);
+		kernel_neon_begin();
+		polyval_mul_pmull(acc, &key->h_powers[NUM_H_POWERS - 1]);
+		kernel_neon_end();
 	} else {
 		polyval_mul_generic(acc, &key->h_powers[NUM_H_POWERS - 1]);
 	}
@@ -61,8 +62,9 @@ static void polyval_blocks_arch(struct polyval_elem *acc,
 			size_t n = min_t(size_t, nblocks,
 					 4096 / POLYVAL_BLOCK_SIZE);
 
-			scoped_ksimd()
-				polyval_blocks_pmull(acc, key, data, n);
+			kernel_neon_begin();
+			polyval_blocks_pmull(acc, key, data, n);
+			kernel_neon_end();
 			data += n * POLYVAL_BLOCK_SIZE;
 			nblocks -= n;
 		} while (nblocks);
