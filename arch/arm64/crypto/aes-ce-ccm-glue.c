74c4316d455cb09b675601605c154635a5200b35
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2026-03-18 19:34:12 -0700

    Revert "UPSTREAM: crypto/arm64: aes-ccm - Switch to 'ksimd' scop..."
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/8dd7b307-147c-46cb-a979-5b1c718d0cfc). Please review this revert and approve if it looks good to you.
    
    Change-Id: I52a9e918ee8e39c8a3c155cfc6602e452f5e588b
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com
    Bug: 469786543

--- arch/arm64/crypto/aes-ce-ccm-glue.c
+++ arch/arm64/crypto/aes-ce-ccm-glue.c
@@ -8,14 +8,13 @@
  * Author: Ard Biesheuvel <ardb@kernel.org>
  */
 
+#include <asm/neon.h>
 #include <linux/unaligned.h>
 #include <crypto/aes.h>
 #include <crypto/scatterwalk.h>
 #include <crypto/internal/aead.h>
 #include <crypto/internal/skcipher.h>
 #include <linux/module.h>
-
-#include <asm/simd.h>
 
 #include "aes-ce-setkey.h"
 
@@ -185,38 +184,40 @@ static int ccm_encrypt(struct aead_request *req)
 	if (unlikely(err))
 		return err;
 
-	scoped_ksimd() {
-		if (req->assoclen)
-			ccm_calculate_auth_mac(req, mac);
+	kernel_neon_begin();
 
-		do {
-			u32 tail = walk.nbytes % AES_BLOCK_SIZE;
-			const u8 *src = walk.src.virt.addr;
-			u8 *dst = walk.dst.virt.addr;
-			u8 buf[AES_BLOCK_SIZE];
-			u8 *final_iv = NULL;
+	if (req->assoclen)
+		ccm_calculate_auth_mac(req, mac);
 
-			if (walk.nbytes == walk.total) {
-				tail = 0;
-				final_iv = orig_iv;
-			}
+	do {
+		u32 tail = walk.nbytes % AES_BLOCK_SIZE;
+		const u8 *src = walk.src.virt.addr;
+		u8 *dst = walk.dst.virt.addr;
+		u8 buf[AES_BLOCK_SIZE];
+		u8 *final_iv = NULL;
 
-			if (unlikely(walk.nbytes < AES_BLOCK_SIZE))
-				src = dst = memcpy(&buf[sizeof(buf) - walk.nbytes],
-						   src, walk.nbytes);
+		if (walk.nbytes == walk.total) {
+			tail = 0;
+			final_iv = orig_iv;
+		}
 
-			ce_aes_ccm_encrypt(dst, src, walk.nbytes - tail,
-					   ctx->key_enc, num_rounds(ctx),
-					   mac, walk.iv, final_iv);
+		if (unlikely(walk.nbytes < AES_BLOCK_SIZE))
+			src = dst = memcpy(&buf[sizeof(buf) - walk.nbytes],
+					   src, walk.nbytes);
 
-			if (unlikely(walk.nbytes < AES_BLOCK_SIZE))
-				memcpy(walk.dst.virt.addr, dst, walk.nbytes);
+		ce_aes_ccm_encrypt(dst, src, walk.nbytes - tail,
+				   ctx->key_enc, num_rounds(ctx),
+				   mac, walk.iv, final_iv);
 
-			if (walk.nbytes) {
-				err = skcipher_walk_done(&walk, tail);
-			}
-		} while (walk.nbytes);
-	}
+		if (unlikely(walk.nbytes < AES_BLOCK_SIZE))
+			memcpy(walk.dst.virt.addr, dst, walk.nbytes);
+
+		if (walk.nbytes) {
+			err = skcipher_walk_done(&walk, tail);
+		}
+	} while (walk.nbytes);
+
+	kernel_neon_end();
 
 	if (unlikely(err))
 		return err;
@@ -250,38 +251,40 @@ static int ccm_decrypt(struct aead_request *req)
 	if (unlikely(err))
 		return err;
 
-	scoped_ksimd() {
-		if (req->assoclen)
-			ccm_calculate_auth_mac(req, mac);
+	kernel_neon_begin();
 
-		do {
-			u32 tail = walk.nbytes % AES_BLOCK_SIZE;
-			const u8 *src = walk.src.virt.addr;
-			u8 *dst = walk.dst.virt.addr;
-			u8 buf[AES_BLOCK_SIZE];
-			u8 *final_iv = NULL;
+	if (req->assoclen)
+		ccm_calculate_auth_mac(req, mac);
 
-			if (walk.nbytes == walk.total) {
-				tail = 0;
-				final_iv = orig_iv;
-			}
+	do {
+		u32 tail = walk.nbytes % AES_BLOCK_SIZE;
+		const u8 *src = walk.src.virt.addr;
+		u8 *dst = walk.dst.virt.addr;
+		u8 buf[AES_BLOCK_SIZE];
+		u8 *final_iv = NULL;
 
-			if (unlikely(walk.nbytes < AES_BLOCK_SIZE))
-				src = dst = memcpy(&buf[sizeof(buf) - walk.nbytes],
-						   src, walk.nbytes);
+		if (walk.nbytes == walk.total) {
+			tail = 0;
+			final_iv = orig_iv;
+		}
 
-			ce_aes_ccm_decrypt(dst, src, walk.nbytes - tail,
-					   ctx->key_enc, num_rounds(ctx),
-					   mac, walk.iv, final_iv);
+		if (unlikely(walk.nbytes < AES_BLOCK_SIZE))
+			src = dst = memcpy(&buf[sizeof(buf) - walk.nbytes],
+					   src, walk.nbytes);
 
-			if (unlikely(walk.nbytes < AES_BLOCK_SIZE))
-				memcpy(walk.dst.virt.addr, dst, walk.nbytes);
+		ce_aes_ccm_decrypt(dst, src, walk.nbytes - tail,
+				   ctx->key_enc, num_rounds(ctx),
+				   mac, walk.iv, final_iv);
 
-			if (walk.nbytes) {
-				err = skcipher_walk_done(&walk, tail);
-			}
-		} while (walk.nbytes);
-	}
+		if (unlikely(walk.nbytes < AES_BLOCK_SIZE))
+			memcpy(walk.dst.virt.addr, dst, walk.nbytes);
+
+		if (walk.nbytes) {
+			err = skcipher_walk_done(&walk, tail);
+		}
+	} while (walk.nbytes);
+
+	kernel_neon_end();
 
 	if (unlikely(err))
 		return err;
