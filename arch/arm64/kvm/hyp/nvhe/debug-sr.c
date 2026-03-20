63792e5c133058e380c30a839be6e62aa4401540
Fuad Tabba <tabba@google.com>
2026-03-20 13:28:33 +0000

    ANDROID: KVM: arm64: Safely handle TRBE and SPE state for protected VMs
    The hypervisor currently relies on the host's DEBUG_STATE_SAVE_SPE and
    DEBUG_STATE_SAVE_TRBE iflags to determine if hardware profiling is
    running and needs to be disabled before entering a guest.
    
    For protected VMs, the hypervisor deliberately does not copy the
    host's iflags to protect the guest from spurious exceptions.
    Consequently, it fails to disable TRBE/SPE.
    
    Furthermore, unconditionally copying these specific debug iflags is
    unsafe. A malicious host can enable tracing but clear the flags,
    tricking the hypervisor into entering the guest with tracing active.
    
    Even if the hypervisor attempted to disable the trace units, it
    architecturally failed to do so. The legacy implementation only zeroed
    the trace filter control registers (TRFCR_EL1 and PMSCR_EL1). As
    identified upstream, simply stopping trace generation leaves the master
    hardware buffer units (TRBLIMITR_EL1.E and PMBLIMITR_EL1.E) enabled.
    Because enabled units can speculatively perform translation table walks
    using the active Stage-2 MMU context, running a protected guest while
    the host's TRBE is enabled causes severe translation fault storms and
    subsequent hardware lockups during TLB invalidations.
    
    Fix this by:
    1. Securely bypassing the host's iflags by checking the hardware
       registers directly. We expose the sanitized ID_AA64DFR0_EL1 to EL2
       to check for hardware support, unconditionally triggering the buffer
       disablement logic if the feature is supported.
    2. Explicitly saving, zeroing, and restoring the master enable bits for
       both TRBE (TRBLIMITR_EL1) and SPE (PMBLIMITR_EL1) during the guest
       world switch, ensuring the buffer units are physically disabled.
    
    The architectural limit register disable logic is heavily inspired by
    and partially picks the fixes from Will Deacon's upstream patch series:
    Link: https://lore.kernel.org/all/20260227212136.7660-1-will@kernel.org/
    
    Bug: 473214797
    Change-Id: I1bd58bd5a2283ad6cf87934a3f633000076bd110
    Signed-off-by: Fuad Tabba <tabba@google.com>

--- arch/arm64/kvm/hyp/nvhe/debug-sr.c
+++ arch/arm64/kvm/hyp/nvhe/debug-sr.c
@@ -14,12 +14,13 @@
 #include <asm/kvm_hyp.h>
 #include <asm/kvm_mmu.h>
 
-static void __debug_save_spe(u64 *pmscr_el1)
+static void __debug_save_spe(u64 *pmscr_el1, u64 *pmblimitr_el1)
 {
 	u64 reg;
 
 	/* Clear pmscr in case of early return */
 	*pmscr_el1 = 0;
+	*pmblimitr_el1 = 0;
 
 	/*
 	 * At this point, we know that this CPU implements
@@ -37,9 +38,14 @@ static void __debug_save_spe(u64 *pmscr_el1)
 
 	/* Now drain all buffered data to memory */
 	psb_csync();
+
+	*pmblimitr_el1 = reg;
+	write_sysreg_s(0, SYS_PMBLIMITR_EL1);
+	dsb(nsh);
+	isb();
 }
 
-static void __debug_restore_spe(u64 pmscr_el1)
+static void __debug_restore_spe(u64 pmscr_el1, u64 pmblimitr_el1)
 {
 	if (!pmscr_el1)
 		return;
@@ -47,16 +53,23 @@ static void __debug_restore_spe(u64 pmscr_el1)
 	/* The host page table is installed, but not yet synchronised */
 	isb();
 
+	write_sysreg_s(pmblimitr_el1, SYS_PMBLIMITR_EL1);
+	isb();
+
 	/* Re-enable data generation */
 	write_sysreg_el1(pmscr_el1, SYS_PMSCR);
 }
 
-static void __debug_save_trace(u64 *trfcr_el1)
+static void __debug_save_trace(u64 *trfcr_el1, u64 *trblimitr_el1)
 {
+	u64 reg;
+
 	*trfcr_el1 = 0;
+	*trblimitr_el1 = 0;
 
 	/* Check if the TRBE is enabled */
-	if (!(read_sysreg_s(SYS_TRBLIMITR_EL1) & TRBLIMITR_EL1_E))
+	reg = read_sysreg_s(SYS_TRBLIMITR_EL1);
+	if (!(reg & TRBLIMITR_EL1_E))
 		return;
 	/*
 	 * Prohibit trace generation while we are in guest.
@@ -68,12 +81,29 @@ static void __debug_save_trace(u64 *trfcr_el1)
 	isb();
 	/* Drain the trace buffer to memory */
 	tsb_csync();
+
+	*trblimitr_el1 = reg;
+	write_sysreg_s(0, SYS_TRBLIMITR_EL1);
+	dsb(nsh);
+
+	if (cpus_have_final_cap(ARM64_WORKAROUND_2064142)) {
+		tsb_csync();
+		dsb(nsh);
+	}
+
+	isb();
 }
 
-static void __debug_restore_trace(u64 trfcr_el1)
+static void __debug_restore_trace(u64 trfcr_el1, u64 trblimitr_el1)
 {
 	if (!trfcr_el1)
 		return;
+
+	write_sysreg_s(trblimitr_el1, SYS_TRBLIMITR_EL1);
+	isb();
+
+	if (cpus_have_final_cap(ARM64_WORKAROUND_2038923))
+		isb();
 
 	/* Restore trace filter controls */
 	write_sysreg_el1(trfcr_el1, SYS_TRFCR);
@@ -83,10 +113,12 @@ void __debug_save_host_buffers_nvhe(struct kvm_vcpu *vcpu)
 {
 	/* Disable and flush SPE data generation */
 	if (vcpu_get_flag(vcpu, DEBUG_STATE_SAVE_SPE))
-		__debug_save_spe(host_data_ptr(host_debug_state.pmscr_el1));
+		__debug_save_spe(host_data_ptr(host_debug_state.pmscr_el1),
+				 host_data_ptr(host_debug_state.pmblimitr_el1));
 	/* Disable and flush Self-Hosted Trace generation */
 	if (vcpu_get_flag(vcpu, DEBUG_STATE_SAVE_TRBE))
-		__debug_save_trace(host_data_ptr(host_debug_state.trfcr_el1));
+		__debug_save_trace(host_data_ptr(host_debug_state.trfcr_el1),
+				   host_data_ptr(host_debug_state.trblimitr_el1));
 }
 
 void __debug_switch_to_guest(struct kvm_vcpu *vcpu)
@@ -97,9 +129,11 @@ void __debug_switch_to_guest(struct kvm_vcpu *vcpu)
 void __debug_restore_host_buffers_nvhe(struct kvm_vcpu *vcpu)
 {
 	if (vcpu_get_flag(vcpu, DEBUG_STATE_SAVE_SPE))
-		__debug_restore_spe(*host_data_ptr(host_debug_state.pmscr_el1));
+		__debug_restore_spe(*host_data_ptr(host_debug_state.pmscr_el1),
+				    *host_data_ptr(host_debug_state.pmblimitr_el1));
 	if (vcpu_get_flag(vcpu, DEBUG_STATE_SAVE_TRBE))
-		__debug_restore_trace(*host_data_ptr(host_debug_state.trfcr_el1));
+		__debug_restore_trace(*host_data_ptr(host_debug_state.trfcr_el1),
+				      *host_data_ptr(host_debug_state.trblimitr_el1));
 }
 
 void __debug_switch_to_host(struct kvm_vcpu *vcpu)
