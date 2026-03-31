d9e55667a332dcbab82d17da05505a26e26a1206
ningbo1 <ningbo1@xiaomi.corp-partner.google.com>
2026-03-31 19:21:34 +0800

    ANDROID: vendor_hooks: vmalloc alloc and mmap count
    Add 'trace_android_vh_vmalloc_node_range_start' and 'trace_android_vh_vmalloc_node_range_end'
    in the __vmalloc_node_range_noprof function to collect vmalloc allocation information;
    also add 'trace_android_vh_do_mmap_map_count' in the do_mmap function to collect mmap information
    
    Bug: 498084353
    Change-Id: I57e574e16c9c2ed279ed3fec0b91c4fae2433a17
    Signed-off-by: Bo Ning <ningbo1@xiaomi.corp-partner.google.com>

--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -553,6 +553,17 @@ DECLARE_RESTRICTED_HOOK(android_rvh_gup_longterm_locked,
 		unsigned long start, unsigned long nr_pages,
 		struct page **pages),
 	TP_ARGS(rc, nr_pinned_pages, start, nr_pages, pages), 5);
+DECLARE_HOOK(android_vh_vmalloc_node_range_start,
+	TP_PROTO(unsigned long size),
+	TP_ARGS(size));
+DECLARE_HOOK(android_vh_vmalloc_node_range_end,
+	TP_PROTO(unsigned long size, const void *addr),
+	TP_ARGS(size, addr));
+DECLARE_HOOK(android_vh_do_mmap_map_count,
+	TP_PROTO(struct file *file, unsigned long addr,
+			unsigned long len, unsigned long prot,
+			unsigned long flags),
+	TP_ARGS(file, addr, len, prot, flags));
 #endif /* _TRACE_HOOK_MM_H */
 
 /* This part must be outside protection */
