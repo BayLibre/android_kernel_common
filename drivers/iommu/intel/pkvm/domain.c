vineethrp/pKVM-IA

ANDROID: pKVM: iommu/vt-d: Hypercall for flushing domain IOMMU cache
Introduce a hypercall to flush IOMMU caches for a given domain
identified by its page table. Currently pKVM do not support lazy
flushing of IOMMU caches and flushing happens synchronously during an
unmap operation, hence this hypercall was not needed. In preparation for
lazy mode support and nested translation support, we need this hypercall
so that host can flush the caches explicitly.

Change-Id: I1ebeb76d7af14a3676e700682000a3dac74ffa24
Signed-off-by: Vineeth Pillai <vineethrp@google.com>

drivers/iommu/intel/pkvm/domain.c
@@ -323,3 +323,21 @@ void pkvm_intel_iommu_tlb_flush(unsigned long paddr, unsigned long size)
 	if (pt_domain.qi_batch)
 		cache_tag_flush_range(&pt_domain, paddr, paddr + size - 1, 0);
 }
+
+ /* Flush IOMMU caches for the domain identified by the given pgd_gpa. */
+int pkvm_iommu_domain_flush(u64 pgd_gpa, u64 start, u64 last, int ih)
+{
+	struct dmar_domain *domain;
+
+	domain = pkvm_get_iommu_domain(pkvm_host_gpa_to_virt(pgd_gpa));
+	if (!domain) {
+		pkvm_err("%s, failed to get the domain [pgd:%llx]\n",
+			 __func__, pgd_gpa);
+		return -EINVAL;
+	}
+
+	domain_flush_range(domain, start, last, ih);
+	pkvm_put_iommu_domain(domain);
+
+	return 0;
+}

BayLibre/android_kernel_common

Bug: 498629006

drivers/iommu/intel/pkvm/domain.c
@@ -321,3 +321,21 @@ void pkvm_intel_iommu_tlb_flush(unsigned long paddr, unsigned long size)
 	if (pt_domain.qi_batch)
 		cache_tag_flush_range(&pt_domain, paddr, paddr + size - 1, 0);
 }
+
+ /* Flush IOMMU caches for the domain identified by the given pgd_gpa. */
+int pkvm_iommu_domain_flush(u64 pgd_gpa, u64 start, u64 last, int ih)
+{
+	struct dmar_domain *domain;
+
+	domain = pkvm_get_iommu_domain(pkvm_host_gpa_to_virt(pgd_gpa));
+	if (!domain) {
+		pkvm_err("%s, failed to get the domain [pgd:%llx]\n",
+			 __func__, pgd_gpa);
+		return -EINVAL;
+	}
+
+	cache_tag_flush_range(domain, start, last, ih);
+	pkvm_put_iommu_domain(domain);
+
+	return 0;
+}