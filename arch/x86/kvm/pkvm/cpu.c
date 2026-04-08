76d728415d02d46aa9d49a827be59e49d8110f0a
Chuanxiao Dong <chuanxiao.dong@intel.com>
2026-04-08 10:09:35 +0800

    ANDROID: pKVM: x86: Index host_vcpus and pcpus via consecutive number
    The host_vcpus and pcpus array in the struct pkvm_hyp were indexed by
    the CPU id. This is not necessary for the pKVM hypervisor as there are
    per cpu pointer pointing the host_vcpu and pcpu. Another potential issue
    is that, if the CPU id is discrete, then iterating over the host_vcpus
    and pcpus array via using pkvm_hyp->num_cpus is incorrect. So change to
    index these two array via consecutive number from 0..num_cpus-1.
    
    Fixes: 6977faba61b7 ("ANDROID: pKVM: x86: Add pkvm_hyp instance")
    Change-Id: I9e396e997e8380bb0c9d0e26ab19d618dd0ad16c
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/pkvm/cpu.c
+++ arch/x86/kvm/pkvm/cpu.c
@@ -47,19 +47,15 @@ unsigned int pkvm_per_cpu_nr_pages(void)
 #endif
 }
 
-int pkvm_setup_per_cpu(int cpu, unsigned long base)
+int pkvm_setup_per_cpu(int cpu, unsigned long base, struct pkvm_pcpu *pcpu, struct kvm_vcpu *vcpu)
 {
 	struct task_struct *task;
-	struct pkvm_pcpu *pcpu;
-	struct kvm_vcpu *vcpu;
 
 	if (cpu >= ARRAY_SIZE(__per_cpu_offset))
 		return -EINVAL;
-	pcpu = pkvm_hyp->pcpus[cpu];
-	if (!pcpu)
+	if (!pcpu || pcpu->cpu != cpu)
 		return -EINVAL;
-	vcpu = pkvm_hyp->host_vcpus[cpu];
-	if (!vcpu)
+	if (!vcpu || vcpu->cpu != cpu)
 		return -EINVAL;
 
 #ifndef CONFIG_PKVM_X86_DEBUG
