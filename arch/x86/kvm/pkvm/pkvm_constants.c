676269d55b93bcce3415cb65290b1d9573ebbffc
Grzegorz Jaszczyk <jaszczyk@google.com>
2026-04-08 22:01:17 +0000

    ANDROID: pKVM: x86: Dynamically extract persistent_ram_buffer definitions
    The pKVM hypervisor requires access to 'struct persistent_ram_buffer'
    and the 'PERSISTENT_RAM_SIG' definition to format and write panic logs
    directly to the host's ramoops-console buffer. However, these
    definitions are strictly private to the host's fs/pstore/ram_core.c.
    
    Rather than duplicating these definitions in the hypervisor or modifying
    the host's public headers, dynamically extract them using the
    hypervisor's pkvm_constants generator.
    
    By including fs/pstore/ram_core.c directly into
    arch/x86/kvm/pkvm/pkvm_constants.c (and undefining pr_fmt to avoid macro
    collisions), the build system automatically generates the exact byte
    offsets for all persistent_ram_buffer fields and extracts the
    PERSISTENT_RAM_SIG value into pkvm_constants.h.
    
    Bug: 502087662
    Change-Id: I13ba452eb820bc025cc1a8c21a2087e376228c20
    Signed-off-by: Grzegorz Jaszczyk <jaszczyk@google.com>

--- arch/x86/kvm/pkvm/pkvm_constants.c
+++ arch/x86/kvm/pkvm/pkvm_constants.c
@@ -4,6 +4,9 @@
 #include "memory.h"
 #include "pkvm.h"
 
+#undef pr_fmt
+#include "../../../../fs/pstore/ram_core.c"
+
 int main(void)
 {
 	DEFINE(PKVM_VMEMMAP_ENTRY_SIZE, sizeof(struct pkvm_page));
@@ -11,5 +14,13 @@ int main(void)
 	DEFINE(PKVM_VMX_VM_SIZE, PKVM_VM_BASE_SIZE + sizeof(struct kvm_vmx));
 	DEFINE(PKVM_VMX_VCPU_SIZE, PKVM_VCPU_BASE_SIZE + sizeof(struct vcpu_vmx));
 #endif
+
+	BLANK();
+	DEFINE(PKVM_RAMOOPS_SIG_VAL, PERSISTENT_RAM_SIG);
+	DEFINE(PKVM_RAMOOPS_BUFFER_SIG_OFFSET, offsetof(struct persistent_ram_buffer, sig));
+	DEFINE(PKVM_RAMOOPS_BUFFER_START_OFFSET, offsetof(struct persistent_ram_buffer, start));
+	DEFINE(PKVM_RAMOOPS_BUFFER_SIZE_OFFSET, offsetof(struct persistent_ram_buffer, size));
+	DEFINE(PKVM_RAMOOPS_BUFFER_DATA_OFFSET, offsetof(struct persistent_ram_buffer, data));
+
 	return 0;
 }
