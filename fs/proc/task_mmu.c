2c8683b4dd9b856aa75e1e1a8db51fe5a122d2ff
Adrian Barnaś <abarnas@google.com>
2026-04-14 09:00:57 +0000

    Revert "ANDROID: 16K: procfs: maps: Don't show fixup VMAs"
    This reverts commit dfc869aff621895c7710e04f2eeb00bd29f5b2a9.
    
    Change-Id: Ieccff349ff0f034b58780bc2986ec1fa363343b0
    Signed-off-by: Adrian Barnaś <abarnas@google.com>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -10,7 +10,6 @@
 #include <linux/ptrace.h>
 #include <linux/slab.h>
 #include <linux/pagemap.h>
-#include <linux/page_size_compat.h>
 #include <linux/pgsize_migration.h>
 #include <linux/mempolicy.h>
 #include <linux/rmap.h>
@@ -19,7 +18,7 @@
 #include <linux/swapops.h>
 #include <linux/mmu_notifier.h>
 #include <linux/page_idle.h>
-#include <linux/page_size_compat.h>
+#include <linux/page_size_compat_defs.h>
 #include <linux/shmem_fs.h>
 #include <linux/uaccess.h>
 #include <linux/pkeys.h>
@@ -486,10 +485,6 @@ show_map_vma(struct seq_file *m, struct vm_area_struct *vma)
 
 	start = vma->vm_start;
 	end = VMA_PAD_START(vma);
-
-	/* Skip page size fixup VMAs */
-	if (flags & __VM_NO_COMPAT)
-		return;
 
 	show_vma_header_prefix(m, start, end, flags, pgoff, dev, ino);
 
