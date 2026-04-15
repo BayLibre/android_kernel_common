1329cce462b4d131fa436420ce74cbd95b0dc724
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2026-04-15 16:28:40 -0700

    Revert "ANDROID: vendor_hooks: add a field in pglist_data"
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/888e4b56-20cc-4dae-b53d-9e75d966cbf4). Please review this revert and approve if it looks good to you.
    
    Change-Id: I3e199285c36da8429ed7d1f7a46224d2c72820ff
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com
    Bug: 413593563
    Bug: 498107435

--- include/linux/mmzone.h
+++ include/linux/mmzone.h
@@ -1539,7 +1539,6 @@ typedef struct pglist_data {
 
 	ANDROID_KABI_RESERVE(1);
 	ANDROID_VENDOR_DATA(1);
-	ANDROID_OEM_DATA(1);
 } pg_data_t;
 
 #define node_present_pages(nid)	(NODE_DATA(nid)->node_present_pages)
