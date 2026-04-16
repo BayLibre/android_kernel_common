385e7884faad701047b573a27e0e6ea2a18906f8
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2026-04-15 18:49:40 -0700

    Revert "ANDROID: iommu/arm-smmu-v3-kvm-pv: Add flags to set_identity"
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/888e4b56-20cc-4dae-b53d-9e75d966cbf4). Please review this revert and approve if it looks good to you.
    
    Change-Id: I17f9940bd09def0e89ec809bf5c1fd26ea81cacd
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com
    Bug: 500737833

--- arch/arm64/kvm/iommu.c
+++ arch/arm64/kvm/iommu.c
@@ -305,10 +305,10 @@ int pkvm_iommu_resume(int device_id)
 EXPORT_SYMBOL(pkvm_iommu_resume);
 
 int kvm_iommu_set_identity(pkvm_handle_t drv_id, pkvm_handle_t iommu,
-			   pkvm_handle_t dev, bool on, unsigned long flags)
+			   pkvm_handle_t dev, bool on)
 {
 	return kvm_call_hyp_nvhe_mc(__pkvm_host_iommu_set_identity, drv_id,
-				    iommu, dev, on, flags);
+				    iommu, dev, on);
 }
 EXPORT_SYMBOL(kvm_iommu_set_identity);
 
