b60d067478971573d7bd90195fb4dfa9b06942aa
Mostafa Saleh <smostafa@google.com>
2026-04-16 12:41:47 +0000

    ANDROID: KVM: vfio: Take a reference on kvm struct.
    The pviommu ops will use the kvm struct but they don't take any
    reference. Which means that the kvm fd can be closed while keeping
    the pviommu pd alive accessible causing UAF from the pviommu fd fops.
    
    Bug: 503387966
    Change-Id: I7934dae79e7a366549ac7cb1388775a26c4065eb
    Signed-off-by: Mostafa Saleh <smostafa@google.com>

--- virt/kvm/vfio.c
+++ virt/kvm/vfio.c
@@ -439,6 +439,7 @@ static int pviommufd_release(struct inode *i, struct file *filp)
 {
 	struct kvm_pviommu *pviommu = filp->private_data;
 
+	kvm_put_kvm(pviommu->dev->kvm);
 	kfree(pviommu);
 	return 0;
 }
@@ -461,8 +462,10 @@ static int kvm_vfio_pviommu_attach(struct kvm_device *dev)
 
 	pviommu->dev = dev;
 
+	kvm_get_kvm(dev->kvm);
 	filep = anon_inode_getfile("kvm-pviommu", &pviommu_fops, pviommu, O_CLOEXEC);
 	if (IS_ERR(filep)) {
+		kvm_put_kvm(dev->kvm);
 		kfree(pviommu);
 		return PTR_ERR(filep);
 	}
