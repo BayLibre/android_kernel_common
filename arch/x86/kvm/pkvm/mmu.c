thesofproject/linux

commit ae56783d9b5502f3a0a4582ec50fa824fc9e5b13

ANDROID: pKVM: x86: add support for filling memcache with host backed pages
When using pages provided by the host in the per-vCPU memcache for guest
MMU, those pages must be donated from the host to the hypervisor first.
That is achieved the following way: pKVM will use its own per-vCPU
memcache (in pKVM's instance of struct kvm_vcpu for the given vCPU),
refilling this memcache with pages from the host's memcache (in the
host's struct kvm_vcpu instance) and donating those pages to the
hypevisor before using them. The host is expected to provide enough
pages in its memcache prior to that.

[ Dmytro: adjust to new code structure when porting to v6.18 ]

Bug: 470050354
Change-Id: I47b6458855b266f70247582fc9af5044c1559218
Signed-off-by: Grzegorz Jaszczyk <jaszczyk@google.com>
Signed-off-by: Dmytro Maluka <dmaluka@google.com>

arch/x86/kvm/pkvm/mmu.c
@@ -403,6 +403,35 @@ static void drain_pool(struct pkvm_pool *pool, struct pkvm_memcache *host_mc)
 	}
 }
 
+static void *admit_host_page(void *arg)
+{
+	struct pkvm_memcache *host_mc = arg;
+	void *page;
+
+	page = pop_pkvm_memcache_page(host_mc, pkvm_host_gpa_to_virt);
+	if (!page)
+		return NULL;
+
+	/*
+	 * The page will be cleared by zalloc_page when allocating it from
+	 * the memcache, thus no need to clear it now.
+	 */
+	if (WARN_ON(pkvm_host_donate_hyp(__pkvm_pa(page), PAGE_SIZE, false))) {
+		push_pkvm_memcache_page(host_mc, page, pkvm_virt_to_host_gpa);
+		return NULL;
+	}
+
+	return page;
+}
+
+/* Refill our local memcache by popping pages from the one provided by the host. */
+static int refill_memcache(struct pkvm_memcache *mc, unsigned long min_pages,
+			   struct pkvm_memcache *host_mc)
+{
+	return topup_pkvm_memcache(mc, min_pages, admit_host_page,
+				   pkvm_virt_to_phys, host_mc);
+}
+
 int pkvm_hyp_mmu_init(void *pool_base, unsigned long pool_pages)
 {
 	struct pkvm_pgtable_cap cap = {
@@ -577,6 +606,17 @@ void pkvm_guest_mmu_destroy(struct pkvm_vm *pkvm_vm)
 	 * contains all pages freed during that step.
 	 */
 	drain_pool(&pkvm_vm->mmu_pool, &shared_pkvm->guest_mmu_teardown_mc);
+}
+
+int pkvm_guest_mmu_refill_memcache(struct pkvm_vcpu *pkvm_vcpu)
+{
+	struct kvm_vcpu *vcpu = &pkvm_vcpu->vcpu;
+	struct pkvm_memcache *host_mc;
+
+	host_mc = &pkvm_vcpu->shared_vcpu->arch.pkvm.guest_mmu_memcache;
+
+	return refill_memcache(&vcpu->arch.pkvm.guest_mmu_memcache,
+			       host_mc->count, host_mc);
 }
 
 void pkvm_guest_mmu_free_memcache(struct pkvm_vcpu *pkvm_vcpu)

arch/x86/kvm/pkvm/mmu.h
@@ -37,6 +37,7 @@ void pkvm_guest_mmu_setup(const struct pkvm_pgtable_ops *pgt_ops,
 			  struct pkvm_pgtable_cap pgt_cap);
 int pkvm_guest_mmu_init(struct pkvm_vm *pkvm_vm, phys_addr_t pgd_pa);
 void pkvm_guest_mmu_destroy(struct pkvm_vm *pkvm_vm);
+int pkvm_guest_mmu_refill_memcache(struct pkvm_vcpu *pkvm_vcpu);
 void pkvm_guest_mmu_free_memcache(struct pkvm_vcpu *pkvm_vcpu);
 
 #endif /* __PKVM_X86_MMU_H */

BayLibre/android_kernel_common

commit e14c9c6285677062d9e682217154930588a94868

ANDROID: pKVM: x86: Fix TOCTOU and donate ordering in memcache refill
The host memcache (host_mc) resides in shared memory that the host
retains write access to. As pop_pkvm_memcache_page() is called before
pkvm_host_donate_hyp(), the host_mc->head.addr will be read when popping
up and the data will be output to the host_mc. If the host_mc->head.addr
points to a private memory, this will cause data leakage to the host. To
prevent this, snapshot the host_mc to local and donate the page before
pop, so that when popping up, the pKVM can guarantee it reads from a page
which is not used by any pVM or itself to prevent the data leakage.

Fixes: ae56783d9b55 ("ANDROID: pKVM: x86: add support for filling memcache with host backed pages")
Change-Id: I30114b9b0601791645f09151c553c6ce133abaa5
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/mmu.c
@@ -476,18 +476,28 @@ static void *admit_host_page(void *arg)
 	struct pkvm_memcache *host_mc = arg;
 	void *page;
 
-	page = pop_pkvm_memcache_page(host_mc, pkvm_host_gpa_to_virt);
-	if (!page)
+	if (!host_mc->count || WARN_ON_ONCE(host_mc->head.nr_pages != 1))
 		return NULL;
 
 	/*
+	 * Should donate the page from the host memcache before pop it out
+	 * because pop will need to read this page to get the next head.
+	 * Without donating, reading this page may leak sensitive data as this
+	 * page may be a private page of a pVM or the hypervisor.
+	 *
 	 * The page will be cleared by zalloc_page when allocating it from
 	 * the memcache, thus no need to clear it now.
 	 */
-	if (WARN_ON(pkvm_host_donate_hyp(__pkvm_pa(page), PAGE_SIZE, false))) {
-		push_pkvm_memcache_page(host_mc, page, pkvm_virt_to_host_gpa);
+	if (WARN_ON(pkvm_host_donate_hyp(pkvm_host_gpa_to_phys(host_mc->head.addr),
+					 PAGE_SIZE, false)))
 		return NULL;
-	}
+
+	page = pop_pkvm_memcache_page(host_mc, pkvm_host_gpa_to_virt);
+	/*
+	 * The head page is already donated so the pop has no reason to fail
+	 * unless there is a code bug.
+	 */
+	BUG_ON(!page);
 
 	return page;
 }
@@ -496,8 +506,21 @@ static void *admit_host_page(void *arg)
 static int refill_memcache(struct pkvm_memcache *mc, unsigned long min_pages,
 			   struct pkvm_memcache *host_mc)
 {
-	return topup_pkvm_memcache(mc, min_pages, admit_host_page,
-				   pkvm_virt_to_phys, host_mc);
+	/*
+	 * The host mc is shared and can be modified by the host while popping.
+	 * Copy it to local and use the local one to prevent the host from
+	 * changing it with unexpected values, e.g., the host change the
+	 * host_mc->head.addr to a private page address after addr is donated
+	 * but before popping up, making the pop_pkvm_memcache_page reading
+	 * from the private page and leaking data to the host.
+	 */
+	struct pkvm_memcache tmp_mc = *host_mc;
+	int ret;
+
+	ret = topup_pkvm_memcache(mc, min_pages, admit_host_page,
+				  pkvm_virt_to_phys, &tmp_mc);
+	*host_mc = tmp_mc;
+	return ret;
 }
 
 static int host_reclaim_guest_walker(struct pkvm_pgtable_visit_ctx *ctx,