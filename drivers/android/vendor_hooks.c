maltesermailo/android_kernel_pantah

ANDROID: vendor_hook: Added hook for memory allocation tuning
The following hooks are added for memory allocation tuning:

android_vh_mm_customize_alloc_anon_thp - When allocating anonymous folios, this
hook will set different orders based on the importance of the processes. In
particular, some critical processes will allocate folios directly in this hook
to speed up memory allocation.

android_vh_mm_customize_ac - In __alloc_pages_noprof, this hook provides the
ability to modify alloc_context and alloc_flags to accommodate processes of
different importance.

android_vh_mm_customize_rmqueue - In rmqueue, this hook provides the ability to
modify alloc_flags and migratetype, which will be used by vendors to customize
alloc_flags and migratetype to speed up memory allocation.

android_vh_mm_customize_suitable_zone - For some specific gfp_mask and order
requests, this hook is used to skip zones that should not be allocated, and for
some critical processes, the wmark limit is skipped and jumps to try_this_zone
for allocation to prevent it from being stuck.

android_vh_mm_customize_zone_max_order - This hook is used to limit the maximum
order allowed for ZONE_NOSPLIT. It reduces a lot of unnecessary repeated merges
and splits (because large orders are rarely allocated).

android_vh_mm_customize_zone_pageset - This hook allows vendors to customize
the high and batch of pagesets.

Bug: 431672372
Change-Id: I181cb092f3763afbaad3fc7cde79f6a7530e96de
Signed-off-by: Pengfei Li <pengfei.kernel@vivo.corp-partner.google.com>

drivers/android/vendor_hooks.c
@@ -584,3 +584,9 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_gzvm_handle_demand_page_post);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_gzvm_destroy_vm_post_process);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_usb_dev_suspend);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_usb_dev_resume);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mm_customize_alloc_anon_thp);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mm_customize_ac);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mm_customize_rmqueue);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mm_customize_suitable_zone);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mm_customize_zone_max_order);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mm_customize_zone_pageset);

BayLibre/android_kernel_common

android_rvh_mm_customize_alloc_anon_thp - When allocating anonymous folios, this
android_vh_mm_customize_wmark_ok - Vendors use this hook to customize the return
value of wmark_ok based on different payloads.
order allowed for special zone. It reduces a lot of unnecessary repeated merges
Bug: 431730501
Bug: 505636392
Change-Id: I65d6b2cd63688291e6360597bc2a4157fa228df7
[ Resolved minor conflicts and change mm_customize_alloc_anon_thp to
RESTRICTED_HOOK - Pengfei Li ]
(cherry picked from commit 73c426b11f28690a937e5293fad0b22bd042733c)

drivers/android/vendor_hooks.c
@@ -133,6 +133,7 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_kmalloc_large_fallback_cma);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_kswapd_shrink_node);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_kswapd_shrink_node_bypass);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_logbuf);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_mm_customize_alloc_anon_thp);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_panic_unhandled);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_percpu_rwsem_wait_complete);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_perform_reclaim);
@@ -398,7 +399,13 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mglru_should_abort_scan_ex);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_migration_target_bypass);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mm_compaction_begin);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mm_compaction_end);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mm_customize_ac);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mm_customize_longterm_pinnable);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mm_customize_rmqueue);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mm_customize_suitable_zone);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mm_customize_wmark_ok);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mm_customize_zone_max_order);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mm_customize_zone_pageset);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mm_direct_reclaim_enter);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mm_direct_reclaim_exit);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mm_do_madvise_bypass);